b45756a532920cf98e0ffd5d7169951de513757e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:24 +0000

    Add package picoapi/0.0.2/picoapi-0.0.2.tar.gz

---
+++ packages/picoapi/picoapi-0.0.2.tar.gz/picoapi-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="picoapi",
+    version="0.0.2",
+    author="Patrick Coffey",
+    author_email="patrickcoffey91@gmail.com",
+    description="An opinionated wrapper around FastAPI with custom microservice registration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/schlerp/picoapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
