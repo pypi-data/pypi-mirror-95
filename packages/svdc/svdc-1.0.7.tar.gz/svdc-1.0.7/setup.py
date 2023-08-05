8a1705eca8ec30051782dead54fdf8fa9116fbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:20 +0000

    Add package svdc/1.0.7/svdc-1.0.7.tar.gz

---
+++ packages/svdc/svdc-1.0.7.tar.gz/svdc-1.0.7/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+    name='svdc',
+    packages=['svdc'],
+    version='1.0.7',
+    license='MIT',
+    # Give a short description about your library
+    description='Stefano Valentino: Test Dependency Confusion vulnerability',
+    author='Stefano Valentino',                   # Type in your name
+    author_email='stefano.valentino@outlook.com',      # Type in your E-Mail
+    # url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+    # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
---
+++ packages/svdc/svdc-1.0.7.tar.gz/svdc-1.0.7/svdc/__main__.py
@@ -0,0 +1,5 @@
+def main():
+    print("svdc: 1.0.7: PYPI")
+
+if __name__ == '__main__':
+    main()
