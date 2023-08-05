87cd9656572ef8ecf0e6c9c2ed583963632085a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:24 +0000

    Add package ehelply-python-sdk/0.1.10/ehelply-python-sdk-0.1.10.tar.gz

---
+++ packages/ehelply-python-sdk/ehelply-python-sdk-0.1.10.tar.gz/ehelply-python-sdk-0.1.10/ehelply_python_sdk/sdk.py
@@ -0,0 +1,58 @@
+from typing import Union, Type, TypeVar
+
+import requests
+
+from ehelply_python_sdk.utils import SDKConfiguration, make_requests
+from ehelply_python_sdk.services import services
+from ehelply_python_sdk.services.service_schemas import is_response_error, ErrorResponse
+
+genericSDKBase = TypeVar('genericSDKBase', bound=services.SDKBase)
+
+
+class eHelplySDK:
+    """
+    eHelply SDK
+    """
+    def __init__(self, sdk_configuration: SDKConfiguration) -> None:
+        self.sdk_configuration: SDKConfiguration = sdk_configuration
+        self.requests_session: requests.Session = make_requests(sdk_configuration=sdk_configuration)
+
+    def _make_client(
+            self,
+            client: str,
+            sdk_configuration: SDKConfiguration = None,
+            request_session: requests.Session = None
+    ) -> genericSDKBase:
+        if not sdk_configuration:
+            sdk_configuration = self.sdk_configuration
+
+        if not request_session:
+            request_session = self.requests_session
+
+        if client == "access":
+            return services.AccessSDK(sdk_configuration=sdk_configuration, requests_session=request_session)
+
+        if client == "security":
+            return services.SecuritySDK(sdk_configuration=sdk_configuration, requests_session=request_session)
+
+    def make_access(
+            self,
+            sdk_configuration: SDKConfiguration = None,
+            request_session: requests.Session = None
+    ) -> services.AccessSDK:
+        return self._make_client(
+            client="access",
+            sdk_configuration=sdk_configuration,
+            request_session=request_session
+        )
+
+    def make_security(
+            self,
+            sdk_configuration: SDKConfiguration = None,
+            request_session: requests.Session = None
+    ) -> services.SecuritySDK:
+        return self._make_client(
+            client="security",
+            sdk_configuration=sdk_configuration,
+            request_session=request_session
+        )
