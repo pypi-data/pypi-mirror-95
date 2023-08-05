c91acbb0f18198f423705da648423fa56d5203db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:46 +0000

    Add package pyinfluxql/0.1.3/pyinfluxql-0.1.3.tar.gz

---
+++ packages/pyinfluxql/pyinfluxql-0.1.3.tar.gz/pyinfluxql-0.1.3/README.rst
@@ -0,0 +1,87 @@
+##########
+PyInfluxQL
+##########
+
+.. image:: https://github.com/daq-tools/pyinfluxql/workflows/Tests/badge.svg
+    :target: https://github.com/daq-tools/pyinfluxql/actions?workflow=Tests
+
+.. image:: https://img.shields.io/pypi/pyversions/pyinfluxql.svg
+    :target: https://python.org
+
+.. image:: https://img.shields.io/pypi/v/pyinfluxql.svg
+    :target: https://pypi.org/project/pyinfluxql/
+
+.. image:: https://img.shields.io/pypi/dm/pyinfluxql.svg
+    :target: https://pypi.org/project/pyinfluxql/
+
+.. image:: https://img.shields.io/pypi/status/pyinfluxql.svg
+    :target: https://pypi.org/project/pyinfluxql/
+
+.. image:: https://img.shields.io/pypi/l/pyinfluxql.svg
+    :target: https://pypi.org/project/pyinfluxql/
+
+
+*****
+About
+*****
+A query generator for the SQL dialect of the `Influx Query Language (InfluxQL)`_.
+Like SQLAlchemy but for InfluxDB. Consider this an experimental WIP.
+
+.. _Influx Query Language (InfluxQL): https://docs.influxdata.com/influxdb/v1.8/query_language/
+
+
+********
+Synopsis
+********
+.. code-block:: python
+
+    from influxdb import InfluxDBClient
+    from pyinfluxql import Engine, Query, Mean
+
+    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
+    engine = Engine(client)
+    query = Query(Mean('value')).from_('cpu_load') \
+        .where(time__gt=datetime.now() - timedelta(1))
+        .group_by(time=timedelta(hours=1))
+    engine.execute(query)
+
+
+*****
+Tests
+*****
+How to invoke the test suite.
+
+Using Tox
+=========
+Run InfluxDB within Docker::
+
+    docker run -it --rm --publish 8086:8086 influxdb:1.8.3
+
+Run ``tox``::
+
+    tox
+
+
+Using sandbox
+=============
+Alternatively, setup package in development mode::
+
+    python3 -mvenv .venv
+    source .venv/bin/activate
+    pip install --editable=.[test]
+
+Run ``pytest``::
+
+    pytest tests -vvv
+
+
+*******
+Credits
+*******
+Thanks a stack to the original author `Jeremiah Malina`_ for his excellent
+work as well as `Michael Pérez`_ for adding integration tests, timezone support
+and for improving the documentation.
+
+
+.. _Jeremiah Malina: https://github.com/jjmalina
+.. _Michael Pérez: https://github.com/puhrez
---
+++ packages/pyinfluxql/pyinfluxql-0.1.3.tar.gz/pyinfluxql-0.1.3/setup.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    if sys.version_info > (3,):
+        with open('README.rst', encoding='utf-8') as f:
+            readme = f.read()
+    else:
+        with open('README.rst') as f:
+            readme = f.read().decode("utf-8")
+    return readme
+
+
+setup(
+    name='pyinfluxql',
+    version='0.1.3',
+    url='https://github.com/daq-tools/pyinfluxql',
+    author='Jeremiah Malina <me@jerem.io>, Michael Pérez <mpuhrez@gmail.com>',
+    author_email='me@jerem.io',
+    maintainer='Andreas Motl',
+    maintainer_email='andreas.motl@getkotori.org',
+    description='A query generator for the SQL dialect of the Influx Query Language (InfluxQL)',
+    long_description=get_long_description(),
+    keywords='influxdb influxql sql query generator acquisition graphing export engine',
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Manufacturing",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Communications",
+        "Topic :: Database",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Topic :: Scientific/Engineering :: Human Machine Interfaces",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Archiving",
+        "Topic :: System :: Networking :: Monitoring",
+        "Topic :: Text Processing",
+        "Topic :: Utilities",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms=['any'],
+    install_requires=[
+        "six>=1.9.0,<2",
+        "influxdb>=2.8.0,<6",
+    ],
+    extras_require={
+        "test": [
+            "pytest>=4.6.11,<7",
+        ],
+    },
+)
