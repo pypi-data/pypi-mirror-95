4b765d4e657df41c03f1a8ce96d4544b5c499fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:00 +0000

    Add package hello-world-from-karlis/0.1.0/hello_world_from_karlis-0.1.0.tar.gz

---
+++ packages/hello-world-from-karlis/hello_world_from_karlis-0.1.0.tar.gz/hello_world_from_karlis-0.1.0/README.md
@@ -0,0 +1,1 @@
+This is my first try to create a pypi package!
---
+++ packages/hello-world-from-karlis/hello_world_from_karlis-0.1.0.tar.gz/hello_world_from_karlis-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+        name='hello_world_from_karlis',
+        version='0.1.0',
+        author='Karlis from LV',
+        packages=find_packages(),
+        include_package_data=True,
+        description='Hello World form LV. My first pypi package'
+        )
