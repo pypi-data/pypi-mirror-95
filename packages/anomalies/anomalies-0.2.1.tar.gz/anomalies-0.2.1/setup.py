736667de6185144c102139d80df28a165ece1b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:47 +0000

    Add package anomalies/0.2.1/anomalies-0.2.1.tar.gz

---
+++ packages/anomalies/anomalies-0.2.1.tar.gz/anomalies-0.2.1/setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Copyright (c) Colav.
+# Distributed under the terms of the Modified BSD License.
+
+# -----------------------------------------------------------------------------
+# Minimal Python version sanity check (from IPython)
+# -----------------------------------------------------------------------------
+
+# See https://stackoverflow.com/a/26737258/2268280
+# sudo pip3 install twine
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
+# For test purposes
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+import os
+import sys
+
+
+v = sys.version_info
+
+shell = False
+if os.name in ('nt', 'dos'):
+    shell = True
+    warning = "WARNING: Windows is not officially supported"
+    print(warning, file=sys.stderr)
+
+
+def main():
+    setup(
+        # Application name:
+        name="anomalies",
+
+        # Version number (initial):
+        version="0.2.1",
+
+        # Application author details:
+        author="restrepo",
+        author_email="restrepo@udea.edu.co",
+
+        # Packages
+        packages=find_packages(exclude=['tests']),
+
+        # Include additional files into the package
+        include_package_data=True,
+
+        # Details
+        url="https://github.com/restrepo/anomalies",
+
+        #
+        license="BSD",
+
+        description="Anomaly cancellation",
+
+        long_description=open("README.md").read(),
+
+        long_description_content_type="text/markdown",
+
+        # Dependent packages (distributions)
+        # See: https://github.com/pypa/pipenv/issues/2171
+        install_requires=[
+            'numpy==1.16.2; python_version=="3.6"',
+            'numpy>=1.16.2; python_version>="3.7"',
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()
