0a4d3469bbdb35da4e652c49c0bd8ed1572b2d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:34 +0000

    Add package pydraw/0.1.4/pydraw-0.1.4.tar.gz

---
+++ packages/pydraw/pydraw-0.1.4.tar.gz/pydraw-0.1.4/pydraw/__init__.py
@@ -0,0 +1,5 @@
+from pydraw.errors import *;
+from pydraw.color import Color;
+from pydraw.location import Location;
+from pydraw.screen import Screen;
+from pydraw.objects import Object, Renderable, CustomRenderable, Rectangle, Oval, Triangle, Polygon, CustomPolygon, Image, Text, Line;
