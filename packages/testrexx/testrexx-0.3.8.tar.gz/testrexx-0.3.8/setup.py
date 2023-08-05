2f6d051ed30051d20dd1835fbef68a4f45eaf0f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:50 +0000

    Add package testrexx/0.3.8/testrexx-0.3.8.tar.gz

---
+++ packages/testrexx/testrexx-0.3.8.tar.gz/testrexx-0.3.8/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='testrexx',
+    version='0.3.8',
+    author='Guilherme Cartier',
+    description='Python library for REXX automated testing',
+    packages=['trexx',
+              'trexx.utilities',
+              'trexx.rexx_lib'],
+    install_requires=['pyrexx',
+                      'python-decouple',
+                      'zowe-zos-files-for-zowe-sdk',
+                      'zowe-zos-jobs-for-zowe-sdk'],
+    package_data={'': ['*.rex']},
+    include_package_data=True,
+    python_requires='>=3.6'
+)
