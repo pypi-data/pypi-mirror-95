ac7ff0db706b5ef39464c9d0bef900c61ed4f693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:44 +0000

    Add package standard-statistical-distributions-gauss/0.1/standard_statistical_distributions_gauss-0.1.tar.gz

---
+++ packages/standard-statistical-distributions-gauss/standard_statistical_distributions_gauss-0.1.tar.gz/standard_statistical_distributions_gauss-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='standard_statistical_distributions_gauss',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['standard_statistical_distributions_gauss'],
+      author='Greg Jones',
+      zip_safe=False)
