3b1e804fcfd59074d9cfe6d18a65ad76496fe32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:31 +0000

    Add package pretty-poly/0.2.0a2/pretty_poly-0.2.0a2.tar.gz

---
+++ packages/pretty-poly/pretty_poly-0.2.0a2.tar.gz/pretty_poly-0.2.0a2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pretty_poly']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['test = run_tests:test']}
+
+setup_kwargs = {
+    'name': 'pretty-poly',
+    'version': '0.2.0a2',
+    'description': 'Display polyominos and polyomino tilings in various pretty formats.',
+    'long_description': None,
+    'author': 'Jack Grahl',
+    'author_email': 'jack.grahl@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
