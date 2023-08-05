6a6699ae546750f3c01297d81616bddb47ba6b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:49 +0000

    Add package akerbp.mlops/0.20210215182238/akerbp.mlops-0.20210215182238-py3-none-any.whl

---
+++ packages/akerbp.mlops/akerbp.mlops-0.20210215182238-py3-none-any.whl/akerbp/mlops/services/test_service.py
@@ -0,0 +1,37 @@
+"""
+test_service.py
+
+Generic test for services (training or prediction)
+"""
+import os
+import sys
+from importlib import import_module
+
+from akerbp.mlops.core import config, logger
+locals().update(config.getenv)
+logging=logger.get_logger(name='MLOps')
+
+
+api_keys = config.api_keys
+
+def mock_saver(*args, **kargs):
+    pass
+
+
+def test_service(model_name):
+   logging.info(f"Test {service_name} service")
+   # Add deployment folder to path so that service can load the settings file
+   sys.path.append(os.getcwd())
+   service = import_module(f"akerbp.mlops.services.{service_name}").service
+   c = config.get_model_config(model_name)
+   
+   if service_name == 'training':
+      response = service(data=c.input, secrets=api_keys, saver=mock_saver)
+   elif service_name == 'prediction':
+      response = service(data=c.input, secrets=api_keys)
+   else:
+      raise Exception("Unknown service name")
+   
+   assert response['status'] == 'ok'
+   assert c.check(response[service_name])
+   
