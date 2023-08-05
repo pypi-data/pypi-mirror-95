a9c979fb26703a4cc795a5af4a5d1c27b005bcc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:27 +0000

    Add package poethepoet/0.11.0b1/poethepoet-0.11.0b1-py3-none-any.whl

---
+++ packages/poethepoet/poethepoet-0.11.0b1-py3-none-any.whl/poethepoet/task/cmd.py
@@ -0,0 +1,92 @@
+from glob import glob
+import re
+import shlex
+from typing import (
+    Dict,
+    MutableMapping,
+    Sequence,
+    Type,
+    TYPE_CHECKING,
+)
+from .base import PoeTask
+
+if TYPE_CHECKING:
+    from ..config import PoeConfig
+    from ..context import RunContext
+
+_GLOBCHARS_PATTERN = re.compile(r".*[\*\?\[]")
+_QUOTED_TOKEN_PATTERN = re.compile(r"(^\".*\"$|^'.*'$)")
+
+
+class CmdTask(PoeTask):
+    """
+    A task consisting of a reference to a shell command
+    """
+
+    content: str
+
+    __key__ = "cmd"
+    __options__: Dict[str, Type] = {}
+
+    def _handle_run(
+        self,
+        context: "RunContext",
+        extra_args: Sequence[str],
+        env: MutableMapping[str, str],
+    ) -> int:
+        env, has_named_args = self._add_named_args_to_env(extra_args, env)
+        if has_named_args:
+            # If named arguments are defined then it doesn't make sense to pass extra
+            # args to the command, because they've already been parsed
+            cmd = self._resolve_args(context, env)
+        else:
+            cmd = (*self._resolve_args(context, env), *extra_args)
+        self._print_action(" ".join(cmd), context.dry)
+        return context.get_executor(env, self.options.get("executor")).execute(cmd)
+
+    def _add_named_args_to_env(
+        self, extra_args: Sequence[str], env: MutableMapping[str, str]
+    ):
+        named_args = self.parse_named_args(extra_args)
+        if named_args is None:
+            return env, False
+        return dict(env, **named_args), bool(named_args)
+
+    def _resolve_args(
+        self, context: "RunContext", env: MutableMapping[str, str],
+    ):
+        # Parse shell command tokens and check if they're quoted
+        if self._is_windows:
+            cmd_tokens = (
+                (compat_token, bool(_QUOTED_TOKEN_PATTERN.match(compat_token)))
+                for compat_token in shlex.split(
+                    self._resolve_envvars(self.content, context, env),
+                    posix=False,
+                    comments=True,
+                )
+            )
+        else:
+            cmd_tokens = (
+                (posix_token, bool(_QUOTED_TOKEN_PATTERN.match(compat_token)))
+                for (posix_token, compat_token) in zip(
+                    shlex.split(
+                        self._resolve_envvars(self.content, context, env),
+                        posix=True,
+                        comments=True,
+                    ),
+                    shlex.split(
+                        self._resolve_envvars(self.content, context, env),
+                        posix=False,
+                        comments=True,
+                    ),
+                )
+            )
+        # Resolve any unquoted glob pattern paths
+        result = []
+        for (cmd_token, is_quoted) in cmd_tokens:
+            if not is_quoted and _GLOBCHARS_PATTERN.match(cmd_token):
+                # looks like a glob path so resolve it
+                result.extend(glob(cmd_token, recursive=True))
+            else:
+                result.append(cmd_token)
+        return result
---
+++ packages/poethepoet/poethepoet-0.11.0b1-py3-none-any.whl/poethepoet/task/script.py
@@ -0,0 +1,111 @@
+import re
+from typing import (
+    Any,
+    Dict,
+    Optional,
+    MutableMapping,
+    Tuple,
+    Type,
+    TYPE_CHECKING,
+    Sequence,
+    Union,
+)
+from .base import PoeTask
+
+if TYPE_CHECKING:
+    from ..config import PoeConfig
+    from ..context import RunContext
+
+
+_FUNCTION_CALL_PATTERN = re.compile(r"^(.+)\((.*)\)\s*;?\s*$")
+
+
+class ScriptTask(PoeTask):
+    """
+    A task consisting of a reference to a python script
+    """
+
+    content: str
+
+    __key__ = "script"
+    __options__: Dict[str, Type] = {}
+
+    def _handle_run(
+        self,
+        context: "RunContext",
+        extra_args: Sequence[str],
+        env: MutableMapping[str, str],
+    ) -> int:
+        # TODO: check whether the project really does use src layout, and don't do
+        #       sys.path.append('src') if it doesn't
+        target_module, target_callable, call_params = self._parse_content(self.content)
+        named_args = self.parse_named_args(extra_args)
+        if named_args is not None:
+            if call_params is None:
+                call_params = f"**({named_args!r})"
+            else:
+                call_params = self._resolve_args(call_params, named_args)
+
+        argv = [
+            self.name,
+            *(self._resolve_envvars(token, context, env) for token in extra_args),
+        ]
+        cmd = (
+            "python",  # TODO: pre-locate python from the target env?
+            "-c",
+            "import sys; "
+            "from importlib import import_module; "
+            f"sys.argv = {argv!r}; sys.path.append('src');"
+            f"import_module('{target_module}').{target_callable}({call_params or ''})",
+        )
+        self._print_action(" ".join(argv), context.dry)
+        return context.get_executor(env, self.options.get("executor")).execute(cmd)
+
+    @classmethod
+    def _parse_content(
+        cls, call_ref: str
+    ) -> Union[Tuple[str, str, Optional[str]], Tuple[None, None, None]]:
+        """
+        Parse module and callable call from a string like one of:
+         - "some_module:main"
+         - "some.module:main(foo='bar')"
+        """
+        try:
+            target_module, target_ref = call_ref.split(":")
+        except ValueError:
+            return None, None, None
+
+        if target_ref.isidentifier():
+            return target_module, f"{target_ref}", None
+
+        call_match = _FUNCTION_CALL_PATTERN.match(target_ref)
+        if call_match:
+            callable_name, call_params = call_match.groups()
+            return target_module, callable_name, call_params
+
+        return None, None, None
+
+    @classmethod
+    def _validate_task_def(
+        cls, task_name: str, task_def: Dict[str, Any], config: "PoeConfig"
+    ) -> Optional[str]:
+        target_module, target_callable, _ = cls._parse_content(task_def["script"])
+        if not target_module or not target_callable:
+            return (
+                f"Task {task_name!r} contains invalid callable reference "
+                f"{task_def['script']!r} (expected something like `module:callable()`)"
+            )
+        return None
+
+    @classmethod
+    def _resolve_args(cls, call_params: str, named_args: Dict[str, str]):
+        def resolve_param(param: str):
+            if "=" in param:
+                keyword, value = (token.strip() for token in param.split(r"=", 1))
+                return f"{keyword}={named_args.get(value, value)!r}"
+            else:
+                return named_args.get(param, param)
+
+        return ", ".join(
+            resolve_param(param.strip()) for param in call_params.strip().split(",")
+        )
