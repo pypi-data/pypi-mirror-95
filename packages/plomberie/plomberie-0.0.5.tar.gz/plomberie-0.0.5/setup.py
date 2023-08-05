70ab4d9c5e995d872b902c150a52215d52bb2781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:08 +0000

    Add package plomberie/0.0.5/plomberie-0.0.5.tar.gz

---
+++ packages/plomberie/plomberie-0.0.5.tar.gz/plomberie-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="plomberie",
+    version="0.0.5",
+    author="Aaron Mamparo",
+    author_email="aaronmamparo@gmail.com",
+    description="Create simple pipelines by defining task dependencies in code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amamparo/plomberie",
+    packages=find_packages("."),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">= 3.8",
+)
