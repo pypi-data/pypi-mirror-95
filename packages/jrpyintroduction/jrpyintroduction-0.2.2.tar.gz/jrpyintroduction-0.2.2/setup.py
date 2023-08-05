0fe6782232b629636bbafa24b26c1b03b2255202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:05 +0000

    Add package jrpyintroduction/0.2.2/jrpyintroduction-0.2.2.tar.gz

---
+++ packages/jrpyintroduction/jrpyintroduction-0.2.2.tar.gz/jrpyintroduction-0.2.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jrpyintroduction']
+
+package_data = \
+{'': ['*'], 'jrpyintroduction': ['data/*', 'vignettes/*']}
+
+install_requires = \
+['matplotlib>=3.0', 'numpy>=1.19', 'openpyxl>=3.0', 'pandas>=1']
+
+setup_kwargs = {
+    'name': 'jrpyintroduction',
+    'version': '0.2.2',
+    'description': 'Jumping Rivers: Introduction to Python',
+    'long_description': None,
+    'author': 'Jamie',
+    'author_email': 'jamie@jumpingrivers.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
