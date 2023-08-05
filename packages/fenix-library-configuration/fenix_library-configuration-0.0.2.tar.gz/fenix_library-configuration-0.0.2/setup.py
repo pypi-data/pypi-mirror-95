7319ac0ddef364f845234e51a4ac3ed412242c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:29 +0000

    Add package fenix-library-configuration/0.0.2/fenix_library-configuration-0.0.2.tar.gz

---
+++ packages/fenix-library-configuration/fenix_library-configuration-0.0.2.tar.gz/fenix_library-configuration-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fenix_library-configuration",
+    version="0.0.2",
+    author="Shivanand Pattanshetti",
+    author_email="shivanand.pattanshetti@gmail.com",
+    description="The configuration library for the Fenix Installer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/rebornos-team/fenix/libraries/configuration",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    packages=setuptools.find_namespace_packages(include=['fenix_library.*']),
+    python_requires='>=3.6',
+)
