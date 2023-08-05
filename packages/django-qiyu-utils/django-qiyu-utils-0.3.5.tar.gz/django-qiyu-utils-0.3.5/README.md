9d5eabdcc7eab2c1c4e469368f5648130edd597f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:06 +0000

    Add package django-qiyu-utils/0.3.5/django-qiyu-utils-0.3.5.tar.gz

---
+++ packages/django-qiyu-utils/django-qiyu-utils-0.3.5.tar.gz/django-qiyu-utils-0.3.5/README.md
@@ -0,0 +1,12 @@
+# 奇遇科技 Django 开发辅助工具箱
+
+![PyPI - Version](https://img.shields.io/pypi/v/django-qiyu-utils)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/django-qiyu-utils)
+![PyPI - Downloads](https://img.shields.io/pypi/dm/django-qiyu-utils)
+![PyPI - Wheel](https://img.shields.io/pypi/wheel/django-qiyu-utils)
+![GitHub repo size](https://img.shields.io/github/repo-size/qiyutechdev/django-qiyu-utils)
+![Lines of code](https://img.shields.io/tokei/lines/github/qiyutechdev/django-qiyu-utils)
+
+# WARNING
+
+    USE IT AT YOUR OWN RISK!!
---
+++ packages/django-qiyu-utils/django-qiyu-utils-0.3.5.tar.gz/django-qiyu-utils-0.3.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['django_qiyu_utils', 'django_qiyu_utils.div_node']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django>=3.1,<3.2']
+
+setup_kwargs = {
+    'name': 'django-qiyu-utils',
+    'version': '0.3.5',
+    'description': 'Django 辅助工具类',
+    'long_description': '# 奇遇科技 Django 开发辅助工具箱\n\n![PyPI - Version](https://img.shields.io/pypi/v/django-qiyu-utils)\n![PyPI - Python Version](https://img.shields.io/pypi/pyversions/django-qiyu-utils)\n![PyPI - Downloads](https://img.shields.io/pypi/dm/django-qiyu-utils)\n![PyPI - Wheel](https://img.shields.io/pypi/wheel/django-qiyu-utils)\n![GitHub repo size](https://img.shields.io/github/repo-size/qiyutechdev/django-qiyu-utils)\n![Lines of code](https://img.shields.io/tokei/lines/github/qiyutechdev/django-qiyu-utils)\n\n# WARNING\n\n    USE IT AT YOUR OWN RISK!!\n',
+    'author': 'dev',
+    'author_email': 'dev@qiyutech.tech',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://www.qiyutech.tech/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
