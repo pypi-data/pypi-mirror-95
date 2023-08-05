95fd57b2784518a2a12503ea0832c9034964810c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:22 +0000

    Add package toxine/1.0.47/toxine-1.0.47.tar.gz

---
+++ packages/toxine/toxine-1.0.47.tar.gz/toxine-1.0.47/toxine/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Toxine project
+#
+# Copyright (C) 2019-present by Sergei Ternovykh
+# License: BSD, see LICENSE for details
+"""
+Toxine is a part of the RuMor project. It is a pipeline of the text
+preprocessing, preliminary entity tagging, and tokenization.
+"""
+from toxine._version import __version__
+from toxine.text_preprocessor import TextPreprocessor
