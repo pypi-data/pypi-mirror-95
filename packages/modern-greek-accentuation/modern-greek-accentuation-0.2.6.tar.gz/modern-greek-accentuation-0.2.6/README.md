93e6fd6ebfa550fc777eac6d40c62a316151d41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:03 +0000

    Add package modern-greek-accentuation/0.2.6/modern-greek-accentuation-0.2.6.tar.gz

---
+++ packages/modern-greek-accentuation/modern-greek-accentuation-0.2.6.tar.gz/modern-greek-accentuation-0.2.6/README.md
@@ -0,0 +1,31 @@
+ # Modern-greek-accentuation
+
+Python 3 library for analyzing, accenting, syllabification, augmentation and transcription of Modern Greek Words
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install modern-greek-accentuation.
+
+```bash
+pip install modern-greek-accentuation
+```
+
+## Usage
+
+see [docs.rst](https://github.com/PicusZeus/modern_greek_accentuation/blob/master/docs.rst)
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Change Log
+ * 0.2.6 Fixed issue with converting to monotonic function, when it would strip word also from dieresis
+ * 0.2.5 Small improvements to augmentify module, now it deals correctly with accented prefixes when creating augmented stems
+ * 0.2.4 Added convert_to_monotonic function
+ * 0.2.3 Fixed issue with syllabification, where iota with diaeresis is not an independent vowel (roloiou), also fixed a secondary issue with accentuation, where previously redundant diaeresis was not removed in above cases.
+ * 0.2.2 Fixed issue with internal augmentation with unaccented augment, now it gives such an option
+ * 0.2.1 Added transcription module (simple, Erasmian and modern for Polish readers)
+ * 0.1.1 Initial release
+
---
+++ packages/modern-greek-accentuation/modern-greek-accentuation-0.2.6.tar.gz/modern-greek-accentuation-0.2.6/modern_greek_accentuation/syllabify.py
@@ -0,0 +1,138 @@
+from .resources import *
+
+
+def _cut_off_syllable(word, true_syllabification=False):
+    """
+    it is more complicated than it should be, but modern Greek rules on accentuation are somewhat convoluted
+    :param word:
+    :param true_syllabification: if true i before vowels treated as consonants
+    :return: returns tuple syllable and rest of the word
+    """
+    all_letters = list(word)
+    all_letters.reverse()
+
+    for index, letter in enumerate(all_letters):
+
+        if letter in vowels:
+            border = index
+            # if there is sth else
+            if len(all_letters[border:]) > 1:
+
+                # check for diphtongs
+                # problem with diph αη in κάηκα
+                if all_letters[border + 1] + letter in diphtongs and true_syllabification:
+                    border += 1
+                elif all_letters[border + 1] + letter in [d for d in diphtongs if d not in ['αη', 'οη', 'άη', 'όη',
+                                                                                            'άι', 'αϊ', 'όι', 'οϊ'
+                                                                                            ]] and not \
+                        true_syllabification:
+
+                    border += 1
+                # check if sth else
+                if len(all_letters[border + 1:]) > 0:
+                    # check for diphtongs with i
+                    if true_syllabification:
+                        # check if this i is a part of a digraph
+                        if len(all_letters[border + 1:]) > 1 and all_letters[border + 2] + \
+                                all_letters[border + 1] in un_digraph_i:
+                            # is oi, ei....
+                            border += 2
+                        # check if single i
+
+                        elif all_letters[border + 1] in un_single_i and len(all_letters[border + 1:]) > 1 and \
+                                (all_letters[border + 2] + all_letters[border + 1]) not in ['ευ', 'εύ', 'αυ', 'αύ']:
+
+                            border += 1
+
+                    rest = all_letters[border + 1:]
+                    cons = ''
+                    for let in rest:
+                        # if consonant shift border one step
+                        if let not in vowels:
+                            cons = let + cons
+                            border += 1
+                        else:
+                            # but if there are more then one consonant, than check
+                            # if the consonant cluster can be a start for a greek word
+
+                            if len(cons) > 1 and cons[:2] in valid_cons_cluster:
+                                difference = len(cons) - 2
+                                border -= difference
+
+                            elif len(cons) > 1:
+                                border -= 1
+                            # border one step back
+
+                            result = all_letters[border + 1:]
+                            result.reverse()
+                            result = ''.join(result)
+
+                            syllable = all_letters[:border + 1]
+                            syllable.reverse()
+                            syllable = ''.join(syllable)
+                            return result, syllable
+
+    if has_vowel(word):
+        syllable = word
+        return None, syllable
+
+    # if only consonant given
+    return word, None
+
+
+def _divide_into_syllables(word, syllables=[], true_syllabification=False):
+    # helping function, returns word chopped into syllables in reverse order,
+    # if true_syllabification off, it takes into consideration unaccented 'i'.
+
+    rest, syllable = _cut_off_syllable(word, true_syllabification=true_syllabification)
+    if syllable:
+        syllables.append(syllable)
+    if rest and len(syllables) > 0:
+
+        rest, syllables = _divide_into_syllables(rest, syllables, true_syllabification=true_syllabification)
+    elif rest and len(syllables) == 0:
+        # for verbs where root consists of a single consonant
+        syllables.append(rest)
+
+        return None, syllables
+
+    return rest, syllables
+
+
+def modern_greek_syllabify(word, true_syllabification=True):
+    """
+    There is a problem as to how treat 'i' before vowels, as it is inconsistent across Modern Greek
+    (χρη-σι-μο-ποι-ώ, υ-γι-ής, κα-τα-πιώ, και-νού-ργιο, Γιε-ώ-ργι-ος κτλ.), which mainly comes from the injection of
+    katarevousians words into dimotic vocabulary. It's impossible to know it without a db of all "logies lekseis".
+    For the sake of accentuation, you can use true_syllabification flag, which if False, will divide words treating
+    'i' always as vowel. Still the result of this function should be most of the time correct
+    :param word: word in Greek chars, works best if the word is accented
+    :param true_syllabification: if you want to divide word treating 'i' always as vowel, give it a False value
+    :return: Array with syllables
+    """
+    rest, syllables = _divide_into_syllables(word, [], true_syllabification=true_syllabification)
+    syllables.reverse()
+
+    return syllables
+
+
+def has_vowel(word):
+    for letter in word:
+        if letter in vowels:
+            return True
+    return False
+
+
+def is_vowel(letter):
+    if letter in vowels:
+        return True
+    return False
+
+
+def count_syllables(word, true_syllabification=True):
+    # modern greek only
+
+    syllables = modern_greek_syllabify(word, true_syllabification=true_syllabification)
+
+    length = len(syllables)
+    return length
