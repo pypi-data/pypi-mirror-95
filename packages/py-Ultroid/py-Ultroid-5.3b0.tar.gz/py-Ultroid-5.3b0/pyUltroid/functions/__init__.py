95bd5ddd9735585a9215ea240670b14add63dc64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:45 +0000

    Add package py-Ultroid/5.3b0/py-Ultroid-5.3b0.tar.gz

---
+++ packages/py-Ultroid/py-Ultroid-5.3b0.tar.gz/py-Ultroid-5.3b0/pyUltroid/functions/__init__.py
@@ -0,0 +1,20 @@
+from pyUltroid import *
+from ..dB.database import Var
+
+DANGER = [
+    "APP_ID",
+    "API_HASH",
+    "SESSION",
+    "BOT_TOKEN",
+    "HEROKU_API",
+    "base64",
+    "base32",
+    "get_me",
+    "NewMessage",
+    "addons/",
+    "cryptography",
+    "herokuapp.com",
+    "phone",
+    "load_addons",
+    "load_plugins",
+]
---
+++ packages/py-Ultroid/py-Ultroid-5.3b0.tar.gz/py-Ultroid-5.3b0/pyUltroid/functions/stats.py
@@ -0,0 +1,14 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+
+async def stats(event):
+    stattext = """
+Uptime - #todo
+#todo
+"""
+    await event.answer(stattext, alert=True)
