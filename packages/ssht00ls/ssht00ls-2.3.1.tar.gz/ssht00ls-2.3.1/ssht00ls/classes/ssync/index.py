b477f1cc1de63bba70a4ec45093c9521f3ef62d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:28 +0000

    Add package ssht00ls/2.3.1/ssht00ls-2.3.1.tar.gz

---
+++ packages/ssht00ls/ssht00ls-2.3.1.tar.gz/ssht00ls-2.3.1/ssht00ls/classes/ssync/index.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+import os, cl1
+from fil3s import Files, Formats
+from r3sponse import r3sponse
+
+# arguments.
+path = cl1.get_argument("--path")
+json = cl1.arguments_present(["--json", "-j"])
+
+# checks.
+if not os.path.exists(path):
+	r3sponse.log(json=json, response=r3sponse.error_response(f"Path [{path}] does not exist."))
+elif not os.path.isdir(path):
+	r3sponse.log(json=json, response=r3sponse.error_response(f"Path [{path}] is not a directory."))
+
+# index.
+index = Files.Dictionary(path=False, dictionary={})
+for _path_ in Files.Directory(path=path).paths(recursive=True):
+	fp = Formats.FilePath(_path_)
+	id = _path_
+	if fp.directory(): id += " (d)"
+	base, known = Formats.FilePath(fp.base()), True
+	if base.exists() and base not in path:
+		try: index.dictionary[base.path]
+		except: known = False
+	if base.path not in index:
+		index[base.path+" (d)"] = base.mtime(format="seconds")
+	index[id] = fp.mtime(format="seconds")
+index.dictionary = index.sort(alphabetical=True)
+
+# handler.
+r3sponse.log(json=json, response=r3sponse.success_response(f"Successfully indexed {len(index.dictionary)} files from directory [{path}].", {
+	"index":index.dictionary,
+}))
