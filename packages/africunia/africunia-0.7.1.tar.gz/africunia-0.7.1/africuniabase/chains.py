f77cf4aef893e7db94909d8ddbabc1f8a5cf27fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:30 +0000

    Add package africunia/0.7.1/africunia-0.7.1.tar.gz

---
+++ packages/africunia/africunia-0.7.1.tar.gz/africunia-0.7.1/africuniabase/chains.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+known_chains = {
+    "AFCASH": {
+        "chain_id": "f02248928ea2e639dee78804f588d1109ffc1fca5c4c7fd54b4f1a030bff7314",
+        "core_symbol": "AFCASH",
+        "prefix": "AFCASH",
+    },
+    "TEST": {
+        "chain_id": "f02248928ea2e639dee78804f588d1109ffc1fca5c4c7fd54b4f1a030bff7314",
+        "core_symbol": "TEST",
+        "prefix": "TEST",
+    },
+}
