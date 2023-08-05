e97fd2091526a7c939c7ff4874b3f877daf30656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:04 +0000

    Add package vandermonde/0.1.3/vandermonde-0.1.3.tar.gz

---
+++ packages/vandermonde/vandermonde-0.1.3.tar.gz/vandermonde-0.1.3/vandermonde/__init__.py
@@ -0,0 +1,10 @@
+from .__about__ import __version__
+from .main import det, matrix, solve, solve_transpose
+
+__all__ = [
+    "__version__",
+    "matrix",
+    "det",
+    "solve",
+    "solve_transpose",
+]
---
+++ packages/vandermonde/vandermonde-0.1.3.tar.gz/vandermonde-0.1.3/vandermonde/main.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+def matrix(x):
+    return np.vander(x, increasing=True)
+
+
+def det(x):
+    return np.prod([np.prod(x[i + 1 :] - x[i]) for i in range(len(x))])
+
+
+def solve(alpha, b):
+    assert len(alpha) == len(b)
+    n = len(b)
+
+    x = b.copy()
+
+    for k in range(1, n):
+        x[k:n] -= x[k - 1 : n - 1]
+        x[k:n] /= alpha[k:n] - alpha[0 : n - k]
+
+    for k in range(n - 1, 0, -1):
+        x[k - 1 : n - 1] -= alpha[k - 1] * x[k:n]
+
+    return x
+
+
+def solve_transpose(alpha, b):
+    assert len(alpha) == len(b)
+    n = len(b)
+
+    x = b.copy()
+
+    for k in range(n):
+        x[k + 1 : n] -= alpha[k] * x[k : n - 1]
+
+    for k in range(n - 1, 0, -1):
+        x[k:n] /= alpha[k:n] - alpha[: n - k]
+        x[k - 1 : n - 1] -= x[k:n]
+
+    return x
