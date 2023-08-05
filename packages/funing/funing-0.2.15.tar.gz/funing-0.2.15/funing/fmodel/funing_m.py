fe7c14b61238716216414c9f6f25dd53917da297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:51 +0000

    Add package funing/0.2.15/funing-0.2.15.tar.gz

---
+++ packages/funing/funing-0.2.15.tar.gz/funing-0.2.15/funing/fmodel/funing_m.py
@@ -0,0 +1,38 @@
+
+from pony.orm import *
+import os
+import sys
+
+from uuid import UUID
+from datetime import date
+
+from setting import base_dir, data_file_path
+from pony.orm.dbapiprovider import OperationalError
+
+db = Database()
+
+
+
+# https://docs.ponyorm.org/firststeps.html#database-binding
+db.bind(provider='sqlite', filename = data_file_path )
+
+class Person( db.Entity ):
+    id = PrimaryKey( str )
+    name = Optional( str )
+    dob = Optional( str )
+    address = Optional( str )
+    note = Optional( str )
+
+# additional informations
+class PersonInfo( db.Entity ):
+    id = PrimaryKey( str )
+    person_id = Required( str )
+    label = Required( str )
+    value = Required( str )
+    note = Optional( str )
+    
+try:
+    db.generate_mapping( create_tables = True )
+except OperationalError as e:
+    print( e )
+    print('\nAdd specific column to database (^_^)\n' )
