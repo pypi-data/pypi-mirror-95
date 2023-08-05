a3347d43a3c9644d53997f259deaee8556b5ef12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:51 +0000

    Add package pizza-slice/1.0.9/pizza_slice-1.0.9-py3-none-any.whl

---
+++ packages/pizza-slice/pizza_slice-1.0.9-py3-none-any.whl/pizza_slice/module1.py
@@ -0,0 +1,9 @@
+def my_sum(x, y):     
+    """calculates the sum of x and y.     
+    Parameters:     
+       x (int): a number
+       y (int): another number    
+    Returns:     
+       int: sum of x and y
+    """      
+    return x+y
