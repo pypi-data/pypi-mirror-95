b151c96a4560c1b99b6554fe8564669cec716d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:53 +0000

    Add package gFunctionLibrary/0.1.3/gFunctionLibrary-0.1.3.tar.gz

---
+++ packages/gFunctionLibrary/gFunctionLibrary-0.1.3.tar.gz/gFunctionLibrary-0.1.3/setup.py
@@ -0,0 +1,32 @@
+# Jack C. Cook
+# Tuesday, February 2, 2021
+
+from setuptools import setup
+
+
+def getreqs(fname):
+    """
+    Get the requirements list from the text file
+    JCC 03.10.2020
+    :param fname: the name of the requirements text file
+    :return: a list of requirements
+    """
+    file = open(fname)
+    data = file.readlines()
+    file.close()
+    return [data[i].replace('\n', '') for i in range(len(data))]
+    
+
+# pull_first()
+
+setup(name='gFunctionLibrary',
+      install_requires=['matplotlib', 'numpy', 'Pillow', 'scipy', 'pandas', 'natsort'],
+      url='https://github.com/j-c-cook/gFunctionLibrary',
+      download_url='https://github.com/j-c-cook/gFunctionLibrary/archive/v0.1.3.tar.gz',
+      version='0.1.3',
+      packages=['gFunctionLibrary'],
+      include_package_data=True,
+      author='Jack C. Cook',
+      author_email='jack.cook@okstate.edu',
+      description='A submodule of the GLHE Design Tool, containing libraries of '
+                  'g-functions, access and accurate interpolation')
