53546d35af5d9e9c8808d31566f9805be5e2171f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:26 +0000

    Add package ftdpack/0.1/ftdpack-0.1.tar.gz

---
+++ packages/ftdpack/ftdpack-0.1.tar.gz/ftdpack-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='ftdpack',
+version='0.1',
+description='package to access failure to deliver data',
+url='https://github.com/jc22dora/ftdpack',
+author='John Doran',
+author_email='test@gmail.com',
+license='MIT',
+packages=['ftd'],
+zip_safe=False)
+
+
