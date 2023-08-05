3d30e9b50307469ffde2bd023ae6482e91eaf67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:02 +0000

    Add package krx-hj3415/0.1.0/krx_hj3415-0.1.0.tar.gz

---
+++ packages/krx-hj3415/krx_hj3415-0.1.0.tar.gz/krx_hj3415-0.1.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="krx_hj3415",
+    version="0.1.0",
+    author="hyungjin kim",
+    author_email="hj3415@gmail.com",
+    description="Get entire corp data from https://kind.krx.co.kr",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://localhost",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'pandas>=1.2.2',
+        'sqlalchemy>=1.3.23',
+        'selenium>=3.141.0',
+        'lxml>=4.6.2',
+        'webdriver_manager>=3.3.0',
+    ],
+)
