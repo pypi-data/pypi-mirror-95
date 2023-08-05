5bcf6cab25d8bfc2c802f1b8916f53d52c96e0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:56 +0000

    Add package pytryfi/0.0.9/pytryfi-0.0.9.tar.gz

---
+++ packages/pytryfi/pytryfi-0.0.9.tar.gz/pytryfi-0.0.9/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+if sys.argv[-1] == 'compile':
+    os.system('python setup.py bdist_wheel')
+    sys.exit()
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+pkg = find_packages("pytryfi")
+print(pkg)
+
+setup(
+    name="pytryfi", # Replace with your own username
+    version="0.0.9",
+    author="Steve Babcock",
+    author_email="steve.w.babcock@gmail.com",
+    description="Python Interface for TryFi Dog Collars",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sbabcock23/pytryfi",
+    packages={'pytryfi', 'pytryfi/common'},
+    include_package_data=True,
+    classifiers=[ "Programming Language :: Python :: 3",
+                    "License :: OSI Approved :: Apache Software License",
+                    "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
