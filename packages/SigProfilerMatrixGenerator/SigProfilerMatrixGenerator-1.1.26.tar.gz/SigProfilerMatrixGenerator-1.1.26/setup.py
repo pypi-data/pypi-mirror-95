8f6b921e475f05f8c4d6de20df52a739b0935d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:41 +0000

    Add package SigProfilerMatrixGenerator/1.1.26/SigProfilerMatrixGenerator-1.1.26.tar.gz

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.1.26.tar.gz/SigProfilerMatrixGenerator-1.1.26/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import os
+import shutil
+
+
+#remove the dist folder first if exists
+if os.path.exists("dist"):
+	shutil.rmtree("dist")
+
+def readme():
+	this_directory = os.path.abspath(os.path.dirname(__file__))
+	with open(os.path.join(this_directory, 'README.md'), encoding='latin-1') as f:
+		long_description = f.read()
+		return(long_description)
+	# with open('README.rst') as f:
+	# 	return(f.read())
+
+VERSION = '1.1.26'
+
+def write_version_py(filename='SigProfilerMatrixGenerator/version.py'):
+	# Copied from numpy setup.py
+	cnt = """
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+	
+	"""
+	fh = open(filename, 'w')
+	fh.write(cnt % {'version': VERSION,})
+	fh.close()
+
+write_version_py()
+
+setup(name='SigProfilerMatrixGenerator',
+		version=VERSION,
+		description='SigProfiler matrix generator tool',
+		long_description= readme(),
+		long_description_content_type='text/markdown',
+		url='',
+		author='Erik Bergstrom',
+		author_email='ebergstr@eng.ucsd.edu',
+		license='UCSD',
+		packages=find_packages(),#['SigProfilerMatrixGenerator'],
+		install_requires =[
+			"matplotlib>=2.2.2",
+			"sigProfilerPlotting>=1.0.1",
+			"statsmodels>=0.9.0",
+			"scipy>=1.1.0",
+			"numpy>=1.18.5",
+			"pandas>=0.23.4"],
+		include_package_data=True,
+		zip_safe=False)
