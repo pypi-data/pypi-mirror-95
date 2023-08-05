288de1a71ec479abcbb1bd510d912e30868df2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:46 +0000

    Add package copyxnat/0.4.1/copyxnat-0.4.1-py2.py3-none-any.whl

---
+++ packages/copyxnat/copyxnat-0.4.1-py2.py3-none-any.whl/copyxnat/pyreporter/pyreporter.py
@@ -0,0 +1,119 @@
+# coding=utf-8
+
+"""Logging and user I/O"""
+
+
+class PyReporterCodes:
+    """ANSI sequences for terminal output operations"""
+    WARNING = '\33[93m'
+    END = '\33[0m'
+    CLEAR = '\33[K'
+
+
+class PyReporter(object):
+    """Class for custom reporting actions"""
+    _WARN_PREFIX = 'WARNING'
+    _INFO_PREFIX = 'INFO'
+    _VERBOSE_PREFIX = 'VERBOSE INFO (verbose)'
+    _SEPARATOR = ': '
+
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+        self._handlers = [self._print_handler]
+        self._iter_num = None
+        self._max_iter = None
+        self._message = None
+        self._last_progress_text = None
+
+    def info(self, message):
+        """Status message to show to end user"""
+        self._output(prefix=self._INFO_PREFIX, message=message)
+
+    def message(self, message):
+        """A message to the user"""
+        self._output(prefix='', message=message)
+
+    def output(self, message):
+        """A message to the user"""
+        self._output(prefix=None, message=message)
+
+    def warning(self, message):
+        """Warning message to report to end user"""
+        self._output(prefix=self._WARN_PREFIX, message=message,
+                     colour=PyReporterCodes.WARNING)
+
+    def log(self, message):
+        """Message for logs but need not report to end user"""
+        self._output(prefix=self._INFO_PREFIX, message=message)
+
+    def verbose_log(self, message):
+        """Message for logs only in verbose mode"""
+        if self.verbose:
+            self._output(prefix=self._VERBOSE_PREFIX, message=message)
+
+    def _output(self, prefix, message, colour=None):
+        combined_prefix = prefix + self._SEPARATOR if prefix is not None \
+            else ''
+        if colour:
+            colour_prefix = colour
+            colour_suffix = PyReporterCodes.END
+        else:
+            colour_prefix = ''
+            colour_suffix = ''
+
+        self._print_handler(colour_prefix + combined_prefix + message +
+                            colour_suffix)
+
+    def start_progress(self, message, max_iter):
+        """
+        Display a progress bar
+        @param message: message to display in the progress bar
+        @param max_iter: total number of iterations
+        """
+        self._max_iter = max_iter
+        self._iter_num = 0
+        self._message = message
+        self.next_progress()
+
+    def next_progress(self):
+        """
+        Display a progress bar
+        """
+        if self._iter_num is not None and self._max_iter is not None:
+            width = 50
+            num_bars = int(width*self._iter_num//self._max_iter) if \
+                self._max_iter >= 1 else width
+            bar_str = '█'*num_bars + '-'*(width - num_bars)
+            progress_text = '\r{} |{}| {:3d}/{} done'.format(self._message,
+                                                             bar_str,
+                                                             self._iter_num,
+                                                             self._max_iter
+                                                             )
+            self._iter_num = self._iter_num + 1
+        else:
+            progress_text = self._message
+
+        print('\r' + progress_text, end='', flush=True)
+        self._print_progress(progress_text)
+
+    def _print_progress(self, progress_text):
+        self._last_progress_text = progress_text
+        self._reprint_progress()
+
+    def _reprint_progress(self):
+        if self._last_progress_text:
+            print(self._last_progress_text, end='\r', flush=True)
+
+    def complete_progress(self):
+        """
+        Complete a progress bar
+        """
+        self._iter_num = self._max_iter
+        self.next_progress()
+        print()
+        self._message = None
+        self._last_progress_text = None
+
+    def _print_handler(self, message):
+        print(message + PyReporterCodes.CLEAR)
+        self._reprint_progress()
---
+++ packages/copyxnat/copyxnat-0.4.1-py2.py3-none-any.whl/copyxnat/xnat/xml_cleaner.py
@@ -0,0 +1,231 @@
+# coding=utf-8
+
+"""Remove and remap XML tags for xml transfer between XNAT servers"""
+
+
+from enum import Enum
+from xml.etree.cElementTree import register_namespace, XML
+
+
+class XnatType(Enum):
+    """Describe the type of XNAT item so cleaning can be performed"""
+
+    server = 'server'
+    project = 'project'
+    subject = 'subject'
+    experiment = 'experiment'
+    scan = 'scan'
+    assessor = 'assessor'
+    reconstruction = 'reconstruction'
+    resource = 'resource'
+    in_resource = 'in_resource'
+    out_resource = 'out_resource'
+    file = 'file'
+
+
+class XmlCleaner:
+    """Remove and remap XML tags for xml transfer between XNAT servers"""
+
+    XNAT_MODALITY_TAG = '{http://nrg.wustl.edu/xnat}modality'
+    XNAT_PROJECT_NAME_TAG = '{http://nrg.wustl.edu/xnat}name'
+
+    ATTR_SECONDARY_PROJECT_ID = 'secondary_ID'
+
+    XNAT_IMAGE_SCAN_DATA_TAG = '{http://nrg.wustl.edu/xnat}imageScanData'
+    XNAT_OTHER_SCAN = 'xnat:OtherDicomScan'
+
+    # Index names for the maps which store the before->after values
+    PROJECT_ID_MAP = 'PROJECT_ID_MAP'
+    SUBJECT_ID_MAP = 'SUBJECT_ID_MAP'
+    SESSION_ID_MAP = 'SESSION_ID_MAP'
+    SCAN_ID_MAP = 'SCAN_ID_MAP'
+    ASSESSOR_ID_MAP = 'ASSESSOR_ID_MAP'
+    RECONSTRUCTION_ID_MAP = 'RECONSTRUCTION_ID_MAP'
+
+    TAGS_TO_REMAP = {
+        '{http://icr.ac.uk/icr}subjectID': SUBJECT_ID_MAP,
+        '{http://nrg.wustl.edu/xnat}subject_ID': SUBJECT_ID_MAP,
+        '{http://nrg.wustl.edu/xnat}image_session_ID': SESSION_ID_MAP,
+        '{http://nrg.wustl.edu/xnat}imageSession_ID': SESSION_ID_MAP,
+        '{http://nrg.wustl.edu/xnat}session_id': SESSION_ID_MAP,
+        '{http://nrg.wustl.edu/xnat}scanID': SCAN_ID_MAP,
+        '{http://nrg.wustl.edu/xnat}imageScan_ID': SCAN_ID_MAP
+    }
+
+    ATTRS_TO_REMAP = {
+        XnatType.project: {
+            'ID': PROJECT_ID_MAP
+        },
+        XnatType.subject: {
+            'ID': SUBJECT_ID_MAP,
+            'project': PROJECT_ID_MAP
+        },
+        XnatType.experiment: {
+            'ID': SESSION_ID_MAP,
+            'project': PROJECT_ID_MAP
+        },
+        XnatType.scan: {
+            'ID': SCAN_ID_MAP,
+            'project': PROJECT_ID_MAP
+        },
+        XnatType.assessor: {
+            'ID': ASSESSOR_ID_MAP,
+            'project': PROJECT_ID_MAP
+        },
+        XnatType.reconstruction: {
+            'ID': RECONSTRUCTION_ID_MAP,
+            'project': PROJECT_ID_MAP
+        },
+        XnatType.resource: {
+        },
+        XnatType.in_resource: {
+        },
+        XnatType.out_resource: {
+        },
+        XnatType.file: {
+        }
+    }
+
+    def get_tags_to_remap(self, xnat_type):
+        """Return a map of attribute mappings for this xnat item type"""
+        return self.ATTRS_TO_REMAP[xnat_type]
+
+    TAGS_TO_DELETE = {
+        '{http://nrg.wustl.edu/xnat}out',
+        '{http://nrg.wustl.edu/xnat}experiments',
+        '{http://nrg.wustl.edu/xnat}scans',
+        '{http://nrg.wustl.edu/xnat}resources',
+        '{http://nrg.wustl.edu/xnat}assessors',
+        '{http://nrg.wustl.edu/xnat}prearchivePath',
+        '{http://nrg.wustl.edu/xnat}sharing',
+        '{http://nrg.wustl.edu/xnat}file'
+    }
+
+    MODALITY_TO_SCAN = {
+        'MR': '{http://nrg.wustl.edu/xnat}MRScan',
+        'CT': '{http://nrg.wustl.edu/xnat}CTScan',
+        'US': '{http://nrg.wustl.edu/xnat}USScan',
+        'PT': '{http://nrg.wustl.edu/xnat}PETScan'
+    }
+
+    NAMESPACES = {
+        'xnat': 'http://nrg.wustl.edu/xnat',
+        'prov': 'http://www.nbirn.net/prov',
+        'xdat': 'http://nrg.wustl.edu/xdat',
+        'xs': 'http://www.w3.org/2001/XMLSchema',
+        'proc': 'http://nrg.wustl.edu/proc',
+        'fs': 'http://nrg.wustl.edu/fs',
+        'icr': 'http://icr.ac.uk/icr'
+    }
+
+    def __init__(self, reporter):
+        self._reporter = reporter
+        self.tag_values = {}
+
+    @staticmethod
+    def xml_from_string(xml_string):
+        """Convert XML string to ElementTree with XNAT namespaces"""
+
+        root = XML(xml_string)
+        for prefix, namespace in XmlCleaner.NAMESPACES.items():
+            register_namespace(prefix, namespace)
+        return root
+
+    def make_project_names_unique(self, xml_root, disallowed_names,
+                                  disallowed_ids):
+        """
+        Update project XML tags and attributes to ensure unique values
+
+        @param xml_root: ElementTree root of the XML to remap
+        @param disallowed_names: names already in use by other projects
+        @param disallowed_ids: secondary IDs already in use by other projects
+        """
+
+        # Get current values of secondary ID and name
+        secondary_id = xml_root.attrib[self.ATTR_SECONDARY_PROJECT_ID]
+        name = xml_root.find(self.XNAT_PROJECT_NAME_TAG, self.NAMESPACES).text
+
+        # Find new values that are not already present on the server, by adding
+        # a " copy x" to the name and ID string. We keep the index x the
+        # same for both to avoid confusion
+        new_id = secondary_id
+        new_name = name
+        index = 0
+        while new_id in disallowed_ids or new_name in disallowed_names:
+            index += 1
+            new_id = secondary_id + ' copy {}'.format(index)
+            new_name = name + ' copy {}'.format(index)
+
+        # Update the XML values
+        xml_root.attrib[self.ATTR_SECONDARY_PROJECT_ID] = new_id
+        for child in xml_root.findall(self.XNAT_PROJECT_NAME_TAG,
+                                      self.NAMESPACES):
+            child.text = new_name
+        return xml_root
+
+    def clean(self, xml_root, xnat_type, fix_scan_types):
+        """
+        Remove or XML remap tags that change between XNAT servers
+
+        @param xml_root: ElementTree root of the XML to remap
+        @param xnat_type: Enumeration describing the type of XNAT item
+        @param fix_scan_types: set to True to correct ambiguous scan types
+        @return:
+        """
+        attr_to_tag_map = self.get_tags_to_remap(xnat_type)
+
+        # Delete all attributes that change on dest server
+        for attr in attr_to_tag_map:
+            if attr in xml_root.attrib:
+                del xml_root.attrib[attr]
+
+        if fix_scan_types:
+            if xml_root.tag == self.XNAT_IMAGE_SCAN_DATA_TAG:
+                new_tag = self.XNAT_OTHER_SCAN
+                modalities = [mod.text for mod in
+                              xml_root.findall(self.XNAT_MODALITY_TAG,
+                                               self.NAMESPACES)]
+                if len(modalities) == 1:
+                    new_tag = self.MODALITY_TO_SCAN.get(modalities[0],
+                                                        self.XNAT_OTHER_SCAN)
+                self._reporter.warning('Modifying scan type {} to {}'.format(
+                    self.XNAT_IMAGE_SCAN_DATA_TAG, new_tag))
+                xml_root.tag = new_tag
+
+        # Remove tags to delete
+        for tag in self.TAGS_TO_DELETE:
+            for child in xml_root.findall(tag, self.NAMESPACES):
+                xml_root.remove(child)
+
+        # Remap all tags that change on dest server
+        # for tag, values in self.tag_values.items():
+        for tag, tag_id in self.TAGS_TO_REMAP.items():
+            for child in xml_root.findall(tag, self.NAMESPACES):
+                tag_remap_dict = self.tag_values[tag_id]
+                src_value = child.text
+                if src_value not in tag_remap_dict:
+                    raise ValueError('Tag {}: no new value for {} found'.format(
+                                                                   tag,
+                                                                   src_value))
+
+                dst_value = tag_remap_dict[src_value]
+                child.text = dst_value
+
+        return xml_root
+
+    def add_tag_remaps(self, src_xml_root, dst_xml_root, xnat_type):
+        """
+        Update the remapping dictionary to add add before and after values
+        for the tags in the map
+        @param src_xml_root: the original XML
+        @param dst_xml_root: the XML after XNAT has assigned variables
+        @param xnat_type: Enumeration describing the type of XNAT item
+        """
+        attr_to_tag_map = self.get_tags_to_remap(xnat_type)
+        for attr, global_attr_name in attr_to_tag_map.items():
+            if attr in src_xml_root.attrib:
+                attr_dict = self.tag_values.get(global_attr_name, {})
+                src_value = src_xml_root.attrib[attr]
+                dst_value = dst_xml_root.attrib[attr]
+                attr_dict[src_value] = dst_value
+                self.tag_values[global_attr_name] = attr_dict
