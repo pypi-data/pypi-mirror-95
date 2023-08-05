b72e4f711b2e998286c6e2376227701f9eeb3b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:17 +0000

    Add package pymusiclooper/1.2.1/pymusiclooper-1.2.1-py3.7.egg

---
+++ packages/pymusiclooper/pymusiclooper-1.2.1-py3.7.egg/pymusiclooper/__main__.py
@@ -0,0 +1,265 @@
+#!/usr/bin/python3
+# coding=utf-8
+""" PyMusicLooper
+    Copyright (C) 2020  Hazem Nabil
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+
+import argparse
+import logging
+import os
+import sys
+import warnings
+from multiprocessing import Process
+
+from tqdm import tqdm
+
+from .core import MusicLooper
+
+
+def loop_track(filename, min_duration_multiplier):
+    try:
+        # Load the file
+        logging.info("Loading {}...".format(filename))
+
+        track = MusicLooper(filename, min_duration_multiplier)
+
+        loop_pair_list = track.find_loop_pairs()
+
+        if len(loop_pair_list) == 0:
+            logging.error(f"No suitable loop point found for '{filename}'.")
+            sys.exit(1)
+
+        loop_start = loop_pair_list[0]["loop_start"]
+        loop_end = loop_pair_list[0]["loop_end"]
+        score = loop_pair_list[0]["score"]
+
+        print(
+            "Playing with loop from {} back to {}; similarity: {:.1%}".format(
+                track.frames_to_ftime(loop_end),
+                track.frames_to_ftime(loop_start),
+                score if score is not None else 0,
+            )
+        )
+        print("(press Ctrl+C to exit)")
+
+        track.play_looping(loop_start, loop_end)
+
+    except (TypeError, FileNotFoundError) as e:
+        logging.error("Error: {}".format(e))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="python -m pymusiclooper",
+        description="A script for repeating music seamlessly and endlessly, by automatically finding the best loop points.",
+    )
+    parser.add_argument("path", type=str, help="path to music file.")
+
+    play_options = parser.add_argument_group("Play")
+    export_options = parser.add_argument_group("Export")
+    parameter_options = parser.add_argument_group("Parameter adjustment")
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="enable verbose logging output",
+    )
+
+    play_options.add_argument(
+        "-p",
+        "--play",
+        action="store_true",
+        default=True,
+        help="play the song on repeat with the best discovered loop point (default).",
+    )
+    export_options.add_argument(
+        "-e",
+        "--export",
+        action="store_true",
+        default=False,
+        help="export the song into intro, loop and outro files (WAV format).",
+    )
+    export_options.add_argument(
+        "-j",
+        "--json",
+        action="store_true",
+        default=False,
+        help="export the loop points (in samples) to a JSON file in the song's directory.",
+    )
+    export_options.add_argument(
+        "-b",
+        "--batch",
+        action="store_true",
+        default=False,
+        help="batch process all the files within the given path (usage with export args [-e] or [-j] only).",
+    )
+    export_options.add_argument(
+        "-r",
+        "--recursive",
+        action="store_true",
+        default=False,
+        help="process directories and their contents recursively (usage with [-b/--batch] only).",
+    )
+    export_options.add_argument(
+        "-n",
+        "--n-jobs",
+        type=int,
+        default=1,
+        help="number of parallel jobs to use for batch processing; specify -1 to use all cores (default: 1). WARNING: changing the value will also result in higher memory consumption.",
+    )
+    parameter_options.add_argument(
+        "-o",
+        "--output-dir",
+        type=str,
+        default=os.path.join(os.getcwd(), "looper_output"),
+        help="specify a different output directory.",
+    )
+
+    def bounded_float(x):
+        try:
+            x = float(x)
+        except ValueError:
+            raise argparse.ArgumentTypeError("%r not a floating-point literal" % (x,))
+
+        if x <= 0.0 or x >= 1.0:
+            raise argparse.ArgumentTypeError(
+                "%r not in range (0.0, 1.0) exclusive" % (x,)
+            )
+        return x
+
+    parameter_options.add_argument(
+        "-m",
+        "--min-duration-multiplier",
+        type=bounded_float,
+        default=0.35,
+        help="specify minimum loop duration as a multiplier of song duration (default: 0.35)",
+    )
+
+    args = parser.parse_args()
+
+    if args.batch and not args.verbose:
+        warnings.filterwarnings("ignore")
+        logging.basicConfig(level=logging.ERROR)
+    elif args.verbose:
+        warnings.filterwarnings("ignore")
+        logging.basicConfig(level=logging.ERROR)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    output_dir = args.output_dir
+
+    def export_handler(file_path):
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+
+        output_path = os.path.join(output_dir, os.path.split(file_path)[1])
+
+        try:
+            track = MusicLooper(file_path, args.min_duration_multiplier)
+        except TypeError as e:
+            logging.error(f"Skipping '{file_path}'. {e}")
+            return
+
+        logging.info("Loaded '{}'".format(file_path))
+
+        loop_pair_list = track.find_loop_pairs()
+        if len(loop_pair_list) == 0:
+            logging.error(f"No suitable loop point found for '{file_path}'.")
+            return
+        loop_start = loop_pair_list[0]["loop_start"]
+        loop_end = loop_pair_list[0]["loop_end"]
+        score = loop_pair_list[0]["score"]
+
+        if args.json:
+            track.export_json(loop_start, loop_end, score, output_dir=output_dir)
+            logging.info(
+                f"Successfully exported loop points to '{output_path}.loop_points.json'"
+            )
+        if args.export:
+            track.export(loop_start, loop_end, output_dir=output_dir)
+            logging.info(
+                f"Successfully exported intro/loop/outro sections to '{output_dir}'"
+            )
+        logging.info("")
+
+    if args.batch:
+        if not args.export or not args.json:
+            raise parser.error("Export mode not specified. -e or -j required.")
+
+        if args.n_jobs < -1 or args.n_jobs == 0:
+            raise parser.error(
+                f"n_jobs can either be -1 (for all cores), or any positive integer; n_jobs provided: {args.n_jobs}"
+            )
+
+        if args.recursive:
+            files = []
+            for directory, sub_dir_list, file_list in os.walk(args.path):
+                for filename in file_list:
+                    files.append(os.path.join(directory, filename))
+        else:
+            files = [
+                f
+                for f in os.listdir(args.path)
+                if os.path.isfile(os.path.join(args.path, f))
+            ]
+
+        if len(files) == 0:
+            logging.error(f"No files found in '{args.path}'")
+
+        num_files = len(files)
+
+        if args.n_jobs == 1:
+            tqdm_files = tqdm(files)
+            for file in tqdm_files:
+                tqdm_files.set_description(f"Processing '{file}'")
+                export_handler(file)
+            sys.exit(0)
+
+        affinity = len(os.sched_getaffinity(0)) if args.n_jobs == -1 else args.n_jobs
+
+        processes = []
+        file_idx = 0
+
+        with tqdm(total=num_files) as pbar:
+            while file_idx < num_files:
+                for pid in range(affinity):
+                    p = Process(
+                        target=export_handler,
+                        kwargs={"file_path": files[file_idx]},
+                        daemon=True,
+                    )
+                    processes.append(p)
+                    p.start()
+                    file_idx += 1
+                    if file_idx >= num_files:
+                        break
+
+                # Wait till current batch finishes
+                for process in processes:
+                    process.join()
+                    process.terminate()
+                    pbar.update()
+
+                processes = []
+
+        sys.exit(0)
+
+    if args.export or args.json:
+        export_handler(args.path)
+
+    if args.play and not (args.export or args.json or args.batch):
+        loop_track(args.path, args.min_duration_multiplier)
---
+++ packages/pymusiclooper/pymusiclooper-1.2.1-py3.7.egg/pymusiclooper/core.py
@@ -0,0 +1,282 @@
+#!/usr/bin/python3
+# coding=utf-8
+""" PyMusicLooper
+    Copyright (C) 2020  Hazem Nabil
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+
+import json
+import os
+
+import librosa
+import numpy as np
+import soundfile
+
+
+class MusicLooper:
+    def __init__(self, filename, min_duration_multiplier=0.35):
+        # Load the file if it exists
+        if os.path.exists(filename) and os.path.isfile(filename):
+            try:
+                audio, sr = librosa.load(filename, sr=None, mono=False)
+            except Exception:
+                raise TypeError("Unsupported file type.")
+        else:
+            raise FileNotFoundError("Specified file not found.")
+
+        self.filename = filename
+        mono_signal = librosa.core.to_mono(audio)
+        self.audio, self.trim_offset = librosa.effects.trim(mono_signal)
+        self.rate = sr
+        self.playback_audio = audio
+        self.min_duration_multiplier = min_duration_multiplier
+
+        # Initialize parameters for playback
+        self.channels = self.playback_audio.shape[0]
+
+    def _loop_finding_routine(
+        self, beats, i_start, i_stop, chroma, power_db, min_duration, candidate_pairs
+    ):
+        for i in range(i_start, i_stop):
+            deviation = np.linalg.norm(chroma[..., beats[i]] * 0.1)
+            for j in range(i):
+                # Since the beats array is sorted
+                # any j >= current_j will only decrease in duration
+                if beats[i] - beats[j] < min_duration:
+                    break
+                dist = np.linalg.norm(chroma[..., beats[i]] - chroma[..., beats[j]])
+                if dist <= deviation:
+                    avg_db_diff = self.db_diff(
+                        power_db[..., beats[i]], power_db[..., beats[j]]
+                    )
+                    if avg_db_diff <= 10:
+                        candidate_pairs.append(
+                            {
+                                "loop_start": beats[j],
+                                "loop_end": beats[i],
+                                "dB_diff": avg_db_diff,
+                            }
+                        )
+
+    def db_diff(self, power_db_f1, power_db_f2):
+        average_diff = np.average(np.abs(power_db_f1 - power_db_f2))
+        return average_diff
+
+    def find_loop_pairs(self, combine_beat_plp=False, concurrency=False):
+        S = librosa.core.stft(y=self.audio)
+        S_power = np.abs(S) ** 2
+        S_weighed = librosa.core.perceptual_weighting(
+            S=S_power, frequencies=librosa.fft_frequencies(sr=self.rate)
+        )
+        mel_spectrogram = librosa.feature.melspectrogram(S=S_weighed)
+        onset_env = librosa.onset.onset_strength(S=mel_spectrogram)
+        bpm, beats = librosa.beat.beat_track(onset_envelope=onset_env)
+
+        beats = np.sort(beats)
+
+        chroma = librosa.feature.chroma_stft(S=S_power)
+
+        power_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
+        min_duration = int(chroma.shape[-1] * self.min_duration_multiplier)
+
+        def loop_subroutine(combine_beat_plp=combine_beat_plp, beats=beats):
+            if combine_beat_plp:
+                onset_env = librosa.onset.onset_strength(S=mel_spectrogram)
+                pulse = librosa.beat.plp(onset_envelope=onset_env)
+                beats_plp = np.flatnonzero(librosa.util.localmax(pulse))
+                beats = np.union1d(beats, beats_plp)
+
+            candidate_pairs = []
+
+            self._loop_finding_routine(
+                beats, 1, beats.size, chroma, power_db, min_duration, candidate_pairs
+            )
+
+            candidate_pairs = sorted(
+                candidate_pairs, reverse=False, key=lambda x: x["dB_diff"]
+            )
+
+            db_diff_array = np.array(
+                [candidate_pairs[i]["dB_diff"] for i in range(len(candidate_pairs))]
+            )
+            db_diff_avg = np.average(db_diff_array)
+            db_diff_std = np.std(db_diff_array)
+
+            i = 0
+            one_stddev = db_diff_avg - db_diff_std
+            while i < len(candidate_pairs):
+                if candidate_pairs[i]["dB_diff"] > one_stddev:
+                    break
+                i += 1
+
+            pruned_list = candidate_pairs[: i if i >= 10 else 10]
+
+            test_offset = librosa.samples_to_frames(
+                np.amax([int((bpm / 60) * 0.1 * self.rate), self.rate * 1.5])
+            )
+
+            subseq_beat_sim = [
+                self._subseq_beat_similarity(
+                    pruned_list[i]["loop_start"],
+                    pruned_list[i]["loop_end"],
+                    chroma,
+                    test_duration=test_offset,
+                )
+                for i in range(len(pruned_list))
+            ]
+
+            # Add cosine similarity as score
+            for i in range(len(pruned_list)):
+                pruned_list[i]["score"] = subseq_beat_sim[i]
+
+            # re-sort based on new score
+            pruned_list = sorted(pruned_list, reverse=True, key=lambda x: x["score"])
+            return pruned_list
+
+        pruned_list = loop_subroutine()
+
+        # Retry will trigger when:
+        # (a) there is no beat sequence with <5dB difference and >90% similarity
+        # (b) list is empty
+        retry = True
+        for i in range(len(pruned_list)):
+            if pruned_list[i]["dB_diff"] <= 5.0 and pruned_list[i]["score"] >= 0.90:
+                retry = False
+                break
+
+        if retry and not combine_beat_plp:
+            pruned_list = loop_subroutine(combine_beat_plp=True)
+
+        if self.trim_offset[0] > 0:
+            for i in range(len(pruned_list)):
+                pruned_list[i]["loop_start"] = self.apply_trim_offset(
+                    pruned_list[i]["loop_start"]
+                )
+                pruned_list[i]["loop_end"] = self.apply_trim_offset(
+                    pruned_list[i]["loop_end"]
+                )
+
+        return pruned_list
+
+    def apply_trim_offset(self, frame):
+        return librosa.samples_to_frames(
+            librosa.frames_to_samples(frame) + self.trim_offset[0]
+        )
+
+    def _subseq_beat_similarity(self, b1, b2, chroma, test_duration=None):
+        if test_duration is None:
+            test_duration = librosa.samples_to_frames(self.rate * 3)
+
+        testable_offset = np.amin(
+            [
+                test_duration,
+                chroma[..., b1 : b1 + test_duration].shape[1],
+                chroma[..., b2 : b2 + test_duration].shape[1],
+            ]
+        )
+
+        cosine_sim = np.zeros(test_duration)
+
+        for i in range(testable_offset):
+            dot_prod = np.dot(chroma[..., b1 + i], chroma[..., b2 + i])
+            b1_norm = np.linalg.norm(chroma[..., b1 + i])
+            b2_norm = np.linalg.norm(chroma[..., b2 + i])
+            cosine_sim[i] = dot_prod / (b1_norm * b2_norm)
+
+        return np.average(cosine_sim)
+
+    def samples_to_frames(self, samples):
+        return librosa.core.samples_to_frames(samples)
+
+    def frames_to_samples(self, frame):
+        return librosa.core.frames_to_samples(frame)
+
+    def frames_to_ftime(self, frame):
+        time_sec = librosa.core.frames_to_time(frame, sr=self.rate)
+        return "{:02.0f}:{:06.3f}".format(time_sec // 60, time_sec % 60)
+
+    def play_looping(self, loop_start, loop_end):
+        from mpg123 import ENC_FLOAT_32
+        from mpg123 import Out123
+
+        out = Out123()
+        encoding = ENC_FLOAT_32
+
+        out.start(self.rate, self.channels, encoding)
+
+        playback_frames = librosa.util.frame(self.playback_audio.flatten(order="F"))
+        adjusted_loop_start = loop_start * self.channels
+        adjusted_loop_end = loop_end * self.channels
+
+        i = 0
+        loop_count = 0
+        try:
+            while True:
+                out.play(playback_frames[..., i])
+                i += 1
+                if i == adjusted_loop_end:
+                    i = adjusted_loop_start
+                    loop_count += 1
+                    print("Currently on loop #{}".format(loop_count), end="\r")
+
+        except KeyboardInterrupt:
+            print()  # so that the program ends on a newline
+
+    def export(self, loop_start, loop_end, format="WAV", output_dir=None):
+        if output_dir is not None:
+            filename = os.path.split(self.filename)[1]
+            out_path = os.path.join(output_dir, filename)
+        else:
+            out_path = os.path.abspath(self.filename)
+
+        loop_start = self.frames_to_samples(loop_start)
+        loop_end = self.frames_to_samples(loop_end)
+
+        soundfile.write(
+            out_path + "-intro." + format.lower(),
+            self.playback_audio[..., :loop_start].T,
+            self.rate,
+            format=format,
+        )
+        soundfile.write(
+            out_path + "-loop." + format.lower(),
+            self.playback_audio[..., loop_start:loop_end].T,
+            self.rate,
+            format=format,
+        )
+        soundfile.write(
+            out_path + "-outro." + format.lower(),
+            self.playback_audio[..., loop_end:].T,
+            self.rate,
+            format=format,
+        )
+
+    def export_json(self, loop_start, loop_end, score, output_dir=None):
+        if output_dir is not None:
+            filename = os.path.split(self.filename)[1]
+            out_path = os.path.join(output_dir, filename)
+        else:
+            out_path = os.path.abspath(self.filename)
+
+        loop_start = self.frames_to_samples(loop_start)
+        loop_end = self.frames_to_samples(loop_end)
+
+        out = {
+            "loop_start": int(loop_start),
+            "loop_end": int(loop_end),
+            "score": float(f"{score:.4}"),
+        }
+
+        with open(out_path + ".loop_points.json", "w") as file:
+            json.dump(out, fp=file)
