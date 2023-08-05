4a6b18495a134eff31cd1ba6800a261c65f5570e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:40 +0000

    Add package voicy/0.2.0.0/voicy-0.2.0.0.tar.gz

---
+++ packages/voicy/voicy-0.2.0.0.tar.gz/voicy-0.2.0.0/voicy/http/core.py
@@ -0,0 +1,11 @@
+import requests
+
+
+class Request:
+    @staticmethod
+    def make(
+        method: str, url: str, data: dict = None, params: dict = None, json: dict = None
+    ) -> requests.Response:
+        return requests.request(
+            method=method, url=url, data=data, params=params, json=json
+        )
