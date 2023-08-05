b57fc918dcfaec7435f47c6364335450cc57250d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:56 +0000

    Add package jsontrips/0.1.48/jsontrips-0.1.48-py3-none-any.whl

---
+++ packages/jsontrips/jsontrips-0.1.48-py3-none-any.whl/jsontrips/__init__.py
@@ -0,0 +1,25 @@
+from .code import get_file as __get_file
+
+def lexicon():
+    return __get_file("words.json")
+
+def feature_lists():
+    return __get_file("featurelists.json")
+
+def feature_types():
+    return __get_file("featuretypes.json")
+
+def lexicon_pos():
+    return __get_file("lexicon_lf.json")
+
+def ontology():
+    return __get_file("ontology.json")
+
+def syntax_templates():
+    return __get_file("syntax_templates.json")
+
+def stoplist():
+    return __get_file("stoplist.txt", as_json=False)
+
+def golist():
+    return __get_file("golist.txt", as_json=False)
