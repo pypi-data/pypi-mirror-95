a82902d92963735e7884a188b26096eb3cf7f6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:54 +0000

    Add package datazen/1.5.1/datazen-1.5.1.tar.gz

---
+++ packages/datazen/datazen-1.5.1.tar.gz/datazen-1.5.1/tests/classes/test_command_environment.py
@@ -0,0 +1,30 @@
+
+"""
+datazen - Tests for the 'CommandEnvironment' class mixin.
+"""
+
+# module under test
+from datazen.classes.environment import from_manifest
+
+# internal
+from ..resources import get_resource
+
+
+def test_command_basic():
+    """ Test basic commanding functionality. """
+
+    manifest = get_resource("manifest.yaml", True)
+    env = from_manifest(manifest)
+    env.clean_cache()
+
+    # run a command
+    assert env.command("a") == (True, True)
+    assert env.command("b") == (True, True)
+    assert env.command("a") == (True, False)
+    assert env.command("false") == (False, True)
+
+    env.write_cache()
+    env = from_manifest(manifest)
+    assert env.command("a") == (True, False)
+
+    env.clean_cache()
