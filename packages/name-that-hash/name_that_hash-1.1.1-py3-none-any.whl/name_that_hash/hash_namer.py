2c736fc0ecfc695d1b38fddffb2eb3ddeb20c4c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:16 +0000

    Add package name-that-hash/1.1.1/name_that_hash-1.1.1-py3-none-any.whl

---
+++ packages/name-that-hash/name_that_hash-1.1.1-py3-none-any.whl/name_that_hash/hash_namer.py
@@ -0,0 +1,22 @@
+import re
+from loguru import logger
+
+
+class Name_That_Hash:
+    """
+    The main class which deals with hash identification
+    """
+
+    def __init__(self, prototypes: list):
+        self.prototypes = list(prototypes)
+
+    def identify(self, chash: str):
+        logger.debug(chash)
+        chash = chash.strip()
+        output = []
+        for prototype in self.prototypes:
+            if prototype.regex.match(chash):
+                for mode in prototype.modes:
+                    output.append(mode)
+        print("identified hashes")
+        return output
