b3f25e4f1468333298fb962309bd2412f06789dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:45 +0000

    Add package ehelply-microservice-library/1.2.19/eHelply_Microservice_Library-1.2.19-py3-none-any.whl

---
+++ packages/ehelply-microservice-library/eHelply_Microservice_Library-1.2.19-py3-none-any.whl/ehelply_microservice_library/cli/clis.py
@@ -0,0 +1,33 @@
+import typer
+from typing import List, Dict, Union, Type
+from ehelply_microservice_library.cli import cli_release, cli_update, cli_cryptography, cli_database, cli_service
+from ehelply_microservice_library.service_bootstrap import ServiceBootstrap
+from ehelply_microservice_library.cli.cli_state import CLIState
+
+
+def make_clis(service: Type[ServiceBootstrap]) -> List[Dict[str, Union[str, typer.Typer]]]:
+    CLIState.service = service
+
+    return [
+        {
+            "name": "release",
+            "cli": cli_release.cli
+        },
+        {
+            "name": "update",
+            "cli": cli_update.cli
+        },
+        {
+            "name": "cryptography",
+            "cli": cli_cryptography.cli
+        },
+        {
+            "name": "database",
+            "cli": cli_database.cli
+        },
+        {
+            "name": "service",
+            "cli": cli_service.cli
+        }
+
+    ]
