241c286d4248bf7ae7b92ca91e3a0ca501c2e814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:02 +0000

    Add package hexucf/0.0.9a0/hexucf-0.0.9a0-py3-none-any.whl

---
+++ packages/hexucf/hexucf-0.0.9a0-py3-none-any.whl/datasets/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(name='hexucf',
+      version='0.0.9-alpha',
+      description='UCF Dataset',
+      long_description='UCF',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+      ],
+      keywords='ucf ai datasets',
+      url='',
+      author='Vladislav Ovchinnikov',
+      author_email='vladovchinnikov950@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'patool',
+          'pandas',
+          'Pillow',
+          'opencv-python',
+          'numpy',
+          'hex_dynamic_images'
+      ],
+      include_package_data=True,
+      zip_safe=False)
