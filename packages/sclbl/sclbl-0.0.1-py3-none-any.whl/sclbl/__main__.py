bb1942f02dabd890fccadd0595ec6c6e9c547d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:04 +0000

    Add package sclbl/0.0.1/sclbl-0.0.1-py3-none-any.whl

---
+++ packages/sclbl/sclbl-0.0.1-py3-none-any.whl/sclbl/__main__.py
@@ -0,0 +1,5 @@
+# Executed when running
+# > python sclbl
+# from terminal.
+if __name__ == '__main__':
+    print("After installation using pip install, you can use the sclbl package without invoking 'python'.")
