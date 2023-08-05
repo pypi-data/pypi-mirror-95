e9024781cd423ff79fe892572e7c8c362973f5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:55 +0000

    Add package pyeplan/0.3/pyeplan-0.3-py3-none-any.whl

---
+++ packages/pyeplan/pyeplan-0.3-py3-none-any.whl/pyeplan/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Python library for planning and operation of resilient microgrids."""
+
+__name__ = "pyeplan"
+__version__ = '0.3'
+__author__ = "Shahab Dehghan and Petros Aristidou"
+__copyright__ = ""
+__license__ ='Apache License 2.0',
+__maintainer__ = "s.dehghan@ieee.org"
+__email__ = "s.dehghan@ieee.org"
+__url__ = "https://apetros.github.io/pyeplan/"
+__status__ = "3 - Alpha"
+
+
+from .operation import opersys
+from .investment import invsys
