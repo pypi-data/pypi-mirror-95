98afdef3284741638629d6f51fa1be8bd7263bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:34 +0000

    Add package workoutizer/0.11.0/workoutizer-0.11.0-py3-none-any.whl

---
+++ packages/workoutizer/workoutizer-0.11.0-py3-none-any.whl/wizer/configuration.py
@@ -0,0 +1,44 @@
+# supported for parsing
+supported_formats = [".gpx", ".fit"]
+
+# cannot be edited nor deleted
+protected_sports = ["unknown"]
+
+# will create a time series plot for these list attributes
+attributes_to_create_time_series_plot_for = [
+    "heart_rate_list",
+    "altitude_list",
+    "cadence_list",
+    "speed_list",
+    "temperature_list",
+]
+
+min_max_attributes = [
+    "heart_rate_list",
+    "altitude_list",
+    "cadence_list",
+    "speed_list",
+    "temperature_list",
+]
+
+# configuration of best sections
+rank_limit = 3
+
+# fastest section to parse in activities (in kilometer) (only integers allowed)
+fastest_sections = [
+    1,
+    2,
+    3,
+    5,
+    7,
+    10,
+    20,
+    30,
+    50,
+    75,
+    100,
+    150,
+    200,
+    250,
+    300,
+]
