9e33dae2ba032ec70d3571668afa8375bf7ea677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:13 +0000

    Add package distributions-julio-ml/1/distributions_julio_ml-1.tar.gz

---
+++ packages/distributions-julio-ml/distributions_julio_ml-1.tar.gz/distributions_julio_ml-1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_julio_ml',
+      version='1',
+      description='Gaussian distributions',
+      author = 'Julio Daniel Pérez Cabrera',
+      packages = ['distributions_julio_ml'],
+      author_email = 'jdanielperez@outlook.es',
+      zip_safe=False)
