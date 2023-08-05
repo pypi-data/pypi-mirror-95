18bbce8f7804b1d00520646345fe42d6bbd025cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:14 +0000

    Add package ethmeet/1.5.3b3/ethmeet-1.5.3b3-py3-none-any.whl

---
+++ packages/ethmeet/ethmeet-1.5.3b3-py3-none-any.whl/ethmeet/attend/attendMeet.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class AttendMeet(ABC):
+    def __init__(self, **kwargs):
+        self.meet_url = None
+        try:
+            self.driver = kwargs["driver"].driver
+        except (AttributeError, KeyError): pass
+
+        print(kwargs["code"])
+        try: self.set_meeting_url(kwargs["code"])
+        except KeyError: pass
+
+
+    @abstractmethod
+    def goto_meet(self): raise NotImplementedError
+
+    @abstractmethod
+    def set_meeting_url(self, code): raise NotImplementedError
---
+++ packages/ethmeet/ethmeet-1.5.3b3-py3-none-any.whl/ethmeet/zoomMeet.py
@@ -0,0 +1,29 @@
+import selenium.common.exceptions
+
+from .googleMeet import GoogleMeet
+
+class ZoomMeet(GoogleMeet):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def goto_meet(self):
+        try:
+            self.driver.get(self.meet_url)
+            self.driver.find_elements_by_tag_name("a")[4].click()
+        except (selenium.common.exceptions.InvalidArgumentException, selenium.common.exceptions.NoSuchElementException):
+            print("ERROR ****** Meeting code was not properly set. Please, provide a valid one and try again! ******")
+            raise
+        except selenium.common.exceptions.InvalidSessionIdException:
+            print("ERROR ****** INVALID SESSION! ******")
+            raise
+        except AttributeError:
+            print("ERROR ****** WEB DRIVER NOT FOUND! ******")
+            raise
+
+        self.driver.find_element_by_id("joinBtn").click()
+        return
+
+    def set_meeting_url(self, code):
+        if "https://zoom.us/j/" in code:
+            self.meet_url = code
+        else: self.meet_url="https://zoom.us/j/%s" % code
