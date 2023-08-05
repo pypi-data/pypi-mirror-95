e697c3d197820998e2297822b0d9e6ac9f401804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:41 +0000

    Add package billocode/0.0.0/billocode-0.0.0.tar.gz

---
+++ packages/billocode/billocode-0.0.0.tar.gz/billocode-0.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['billocode']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'billocode',
+    'version': '0.0.0',
+    'description': 'internal use',
+    'long_description': '# internal use',
+    'author': 'Billogram',
+    'author_email': 'platform@billogram.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/billogram',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
