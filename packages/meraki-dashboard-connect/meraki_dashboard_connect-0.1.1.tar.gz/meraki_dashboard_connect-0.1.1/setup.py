a271db6ffc3d600c0fed7a3d956517348cf2397d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:04 +0000

    Add package meraki-dashboard-connect/0.1.1/meraki_dashboard_connect-0.1.1.tar.gz

---
+++ packages/meraki-dashboard-connect/meraki_dashboard_connect-0.1.1.tar.gz/meraki_dashboard_connect-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['meraki_dashboard_connect']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['meraki-env>=0.1.1,<0.2.0', 'meraki>=1.4.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'meraki-dashboard-connect',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Thomas Christory',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
