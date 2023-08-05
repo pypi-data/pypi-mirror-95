fa8d6022daf678bba92a759b4429a81d33e1b1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:18 +0000

    Add package fem.py/0.0.1/fem.py-0.0.1.tar.gz

---
+++ packages/fem.py/fem.py-0.0.1.tar.gz/fem.py-0.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '.'}
+
+packages = \
+['fem']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.20.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'fem.py',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Tiago Vilela',
+    'author_email': 'tiagovla@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
