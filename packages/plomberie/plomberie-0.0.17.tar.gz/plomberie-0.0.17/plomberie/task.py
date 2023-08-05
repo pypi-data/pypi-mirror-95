aa3e09824049e6d7872d728587a92bc63d6b660f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:25 +0000

    Add package plomberie/0.0.17/plomberie-0.0.17.tar.gz

---
+++ packages/plomberie/plomberie-0.0.17.tar.gz/plomberie-0.0.17/plomberie/task.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from typing import List, Awaitable, Optional
+
+from tqdm import tqdm
+
+
+class Task(ABC):
+    def __init__(self):
+        self.__dependencies: List[Task] = []
+        self.__progress_bar: Optional[tqdm] = None
+
+    def depends_on(self, *dependencies: 'Task') -> 'Task':
+        self.__dependencies.extend(dependencies)
+        return self
+
+    def post_run(self):
+        if self.__progress_bar:
+            self.__progress_bar.update(100 - self.__progress_bar.n)
+            self.__progress_bar.close()
+
+    @property
+    def dependencies(self) -> List['Task']:
+        return self.__dependencies
+
+    def set_progress_percent(self, out_of_100: float) -> None:
+        if not self.__progress_bar:
+            self.__progress_bar = tqdm(
+                desc=self.__class__.__name__,
+                total=100,
+                ascii='#',
+                bar_format='{desc}: [{bar}] {percentage:.2f}% ({elapsed}<{remaining})'
+            )
+        increment = out_of_100 - self.__progress_bar.n
+        if increment <= 0:
+            self.__progress_bar.reset(total=100)
+        else:
+            self.__progress_bar.update(increment)
+
+    @abstractmethod
+    def run(self) -> Awaitable:
+        pass
