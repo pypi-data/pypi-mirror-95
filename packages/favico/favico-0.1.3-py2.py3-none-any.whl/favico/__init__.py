ab11e32311b5c0782a3a21a1d5277ca1990fd8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:49 +0000

    Add package favico/0.1.3/favico-0.1.3-py2.py3-none-any.whl

---
+++ packages/favico/favico-0.1.3-py2.py3-none-any.whl/favico/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for favico."""
+
+from favico.favico import generate
+
+__author__ = """romnn"""
+__email__ = "contact@romnn.com"
+__version__ = "0.1.3"
+
+__all__ = ["generate"]
