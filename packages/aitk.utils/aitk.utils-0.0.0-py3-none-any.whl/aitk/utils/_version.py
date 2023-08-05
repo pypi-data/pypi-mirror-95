408aa9f78a75d4fb316fa6378773d12055abcba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:56 +0000

    Add package aitk.utils/0.0.0/aitk.utils-0.0.0-py3-none-any.whl

---
+++ packages/aitk.utils/aitk.utils-0.0.0-py3-none-any.whl/aitk/utils/_version.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# *************************************
+# jyrobot: Python robot simulator
+#
+# Copyright (c) 2021 Calysto Developers
+#
+# https://github.com/Calysto/jyrobot
+#
+# *************************************
+
+version_info = (0, 0, 0)
+__version__ = ".".join(map(str, version_info))
