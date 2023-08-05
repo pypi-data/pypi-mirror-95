42cb9b26d9acef51c8845d11a45ccd3558084a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:02 +0000

    Add package lab/6.3/lab-6.3.tar.gz

---
+++ packages/lab/lab-6.3.tar.gz/lab-6.3/README.rst
@@ -0,0 +1,28 @@
+Lab and Downward Lab
+====================
+
+**Lab** is a Python package for evaluating solvers on benchmark sets.
+Experiments can run on a single machine or on a computer cluster. The
+package also contains code for parsing results and creating reports.
+
+The **Downward Lab** Python package facilitates running experiments for
+the `Fast Downward <http://www.fast-downward.org>`_ planning system. It
+uses the generic experimentation package **Lab**. Currently, Lab and
+Downward Lab are distributed together.
+
+**Code**: https://github.com/aibasel/lab
+
+**Documentation**: https://lab.readthedocs.io
+
+**Cite**: please cite Downward Lab by using
+
+::
+
+    @Misc{seipp-et-al-zenodo2017,
+      author =       "Jendrik Seipp and Florian Pommerening and
+                      Silvan Sievers and Malte Helmert",
+      title =        "{Downward} {Lab}",
+      publisher =    "Zenodo",
+      year =         "2017",
+      howpublished = "\url{https://doi.org/10.5281/zenodo.790461}"
+    }
---
+++ packages/lab/lab-6.3.tar.gz/lab-6.3/setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+from lab import __version__ as version
+
+
+with open("README.rst") as f1, open("INSTALL.rst") as f2:
+    long_description = f1.read() + "\n\n" + f2.read()
+
+
+setup(
+    name="lab",
+    version=version.rstrip("+"),
+    description="Benchmark your code",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    keywords="benchmarks cluster grid",
+    author="Jendrik Seipp",
+    author_email="jendrikseipp@gmail.com",
+    url="https://github.com/aibasel/lab",
+    license="GPL3+",
+    packages=["downward", "downward.reports", "lab", "lab.calls", "lab.reports"],
+    package_data={"downward": ["scripts/*.py"], "lab": ["data/*", "scripts/*.py"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering",
+    ],
+    install_requires=[
+        "matplotlib",  # for scatter plots
+        "simplejson",  # optional, speeds up reading properties files
+        "txt2tags>=3.6",  # for HTML and Latex reports
+    ],
+    python_requires=">=3.6",
+)
