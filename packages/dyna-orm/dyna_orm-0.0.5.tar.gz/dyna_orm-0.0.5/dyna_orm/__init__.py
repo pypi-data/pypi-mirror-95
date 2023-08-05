fed7637112b1c1d366ed8a1c64b43a00ca3c4a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:32 +0000

    Add package dyna-orm/0.0.5/dyna_orm-0.0.5.tar.gz

---
+++ packages/dyna-orm/dyna_orm-0.0.5.tar.gz/dyna_orm-0.0.5/dyna_orm/__init__.py
@@ -0,0 +1,2 @@
+from dyna_orm.db.table import Table
+from dyna_orm.utils.exceptions import IncorrectDataError, InstanceNotFound
---
+++ packages/dyna-orm/dyna_orm-0.0.5.tar.gz/dyna_orm-0.0.5/dyna_orm/utils/exceptions.py
@@ -0,0 +1,2 @@
+class IncorrectDataError(Exception): pass
+class InstanceNotFound(Exception): pass
