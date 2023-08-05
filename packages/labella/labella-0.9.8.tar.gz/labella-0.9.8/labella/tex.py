2bafca24f1a755be9d1737cb5e0846f60cde4ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:12 +0000

    Add package labella/0.9.8/labella-0.9.8.tar.gz

---
+++ packages/labella/labella-0.9.8.tar.gz/labella-0.9.8/labella/tex.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of labella.py.
+
+Author: G.J.J. van den Burg
+License: Apache-2.0
+"""
+
+import os
+import shutil
+import subprocess
+import tempfile
+import unicodedata
+
+
+def uni2tex(text):
+    # Courtesy of https://tex.stackexchange.com/q/23410
+    accents = {
+        0x0300: "`",
+        0x0301: "'",
+        0x0302: "^",
+        0x0308: '"',
+        0x030B: "H",
+        0x0303: "~",
+        0x0327: "c",
+        0x0328: "k",
+        0x0304: "=",
+        0x0331: "b",
+        0x0307: ".",
+        0x0323: "d",
+        0x030A: "r",
+        0x0306: "u",
+        0x030C: "v",
+    }
+    out = ""
+    txt = tuple(text)
+    i = 0
+    while i < len(txt):
+        char = text[i]
+        code = ord(char)
+
+        # combining marks
+        if unicodedata.category(char) in ("Mn", "Mc") and code in accents:
+            out += "\\%s{%s}" % (accents[code], txt[i + 1])
+            i += 1
+        # precomposed characters
+        elif unicodedata.decomposition(char):
+            base, acc = unicodedata.decomposition(char).split()
+            acc = int(acc, 16)
+            base = int(base, 16)
+            if acc in accents:
+                out += "\\%s{%s}" % (accents[acc], chr(base))
+            else:
+                out += char
+        else:
+            out += char
+        i += 1
+    return out
+
+
+def get_latex_fontdoc(text, fontsize="11pt", preamble=""):
+    tex = r"""\documentclass[preview, {fontsize}]{{standalone}}
+{preamble}%
+\begin{{document}}
+{text}%
+\newlength{{\lblwidth}}%
+\newlength{{\lblheight}}%
+\settowidth{{\lblwidth}}{{{text}}}%
+\settoheight{{\lblheight}}{{{text}}}%
+\typeout{{LABELWIDTH: \the\lblwidth}}%
+\typeout{{LABELHEIGHT: \the\lblheight}}%
+\end{{document}}
+""".format(
+        fontsize=fontsize, text=uni2tex(text), preamble=uni2tex(preamble)
+    )
+    return tex
+
+def compile_latex(fname, tmpdirname, latexmk_options, silent=True):
+    compiler = 'latexmk'
+    if latexmk_options:
+        compiler_args = latexmk_options +  ['--outdir=' + tmpdirname,
+                '--interaction=nonstopmode', fname]
+    else:
+        compiler_args = ['--pdf','--outdir=' + tmpdirname,
+                '--interaction=nonstopmode', fname]
+    command = [compiler] + compiler_args
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT)
+    except (OSError, IOError) as e:
+        raise (e)
+    except subprocess.CalledProcessError as e:
+        print(e.output.decode())
+        raise (e)
+    else:
+        if not silent:
+            print(output.decode())
+
+def get_latex_dims(tex, latexmk_options, silent=True):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        basename = "labella_text"
+        fname = os.path.join(tmpdirname, basename + ".tex")
+        with open(fname, "w") as fid:
+            fid.write(tex)
+
+        compile_latex(fname, tmpdirname, latexmk_options, silent=silent)
+
+        logname = os.path.join(tmpdirname, basename + ".log")
+        with open(logname, "r") as fid:
+            lines = fid.readlines()
+
+        line_width = next(
+            (l for l in lines if l.startswith("LABELWIDTH")), None
+        )
+        line_height = next(
+            (l for l in lines if l.startswith("LABELHEIGHT")), None
+        )
+
+        width = line_width.strip().split(":")[-1].strip().rstrip("pt")
+        height = line_height.strip().split(":")[-1].strip().rstrip("pt")
+    return float(width), float(height)
+
+def build_latex_doc(tex, latexmk_options, output_name=None, silent=True):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        basename = "labella_text"
+        fname = os.path.join(tmpdirname, basename + ".tex")
+        with open(fname, "w") as fid:
+            fid.write(tex)
+
+        compile_latex(fname, tmpdirname, latexmk_options, silent=silent)
+
+        pdfname = os.path.join(tmpdirname, basename + ".pdf")
+        if output_name:
+            shutil.copy2(pdfname, output_name)
+
+def text_dimensions(text, fontsize='11pt', preamble='', silent=True,
+                    latexmk_options=None):
+    tex = get_latex_fontdoc(text, fontsize=fontsize, preamble=preamble)
+    width, height = get_latex_dims(tex, silent=silent, 
+                     latexmk_options=latexmk_options)
+    return width, height
---
+++ packages/labella/labella-0.9.8.tar.gz/labella-0.9.8/setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
+import os
+
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = "labella"
+DESCRIPTION = "Python clone of Labella.js that supports TikZ output"
+URL = "https://github.com/GjjvdBurg/labella.py"
+EMAIL = "gertjanvandenburg@gmail.com"
+AUTHOR = "Gertjan van den Burg"
+REQUIRES_PYTHON = ">=3.5.0"
+VERSION = None
+
+# What packages are required for this module to be executed?
+REQUIRED = ["intervaltree>=3.0.0"]
+
+docs_require = []
+test_require = []
+dev_require = ["green"]
+
+# What packages are optional?
+EXTRAS = {
+    "docs": docs_require,
+    "tests": test_require,
+    "dev": docs_require + test_require + dev_require,
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
+        exec(f.read(), about)
+else:
+    about["__version__"] = VERSION
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "tests.*"]
+    ),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="Apache-2.0",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
