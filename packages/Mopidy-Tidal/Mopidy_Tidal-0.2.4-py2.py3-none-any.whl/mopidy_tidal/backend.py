a4a43961541f619710e6dca6627d0e10fcc8a7e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:32 +0000

    Add package Mopidy-Tidal/0.2.4/Mopidy_Tidal-0.2.4-py2.py3-none-any.whl

---
+++ packages/Mopidy-Tidal/Mopidy_Tidal-0.2.4-py2.py3-none-any.whl/mopidy_tidal/backend.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+
+import logging
+
+from mopidy import backend
+
+from pykka import ThreadingActor
+
+from tidalapi import Config, Session, Quality
+
+from mopidy_tidal import library, playback, playlists
+
+
+logger = logging.getLogger(__name__)
+
+
+class TidalBackend(ThreadingActor, backend.Backend):
+    def __init__(self, config, audio):
+        super(TidalBackend, self).__init__()
+        self._session = None
+        self._config = config
+        self._username = config['tidal']['username']
+        self._password = config['tidal']['password']
+        self.playback = playback.TidalPlaybackProvider(audio=audio,
+                                                       backend=self)
+        self.library = library.TidalLibraryProvider(backend=self)
+        self.playlists = playlists.TidalPlaylistsProvider(backend=self)
+        self.uri_schemes = ['tidal']
+
+    def on_start(self):
+        quality = self._config['tidal']['quality']
+        logger.info("Connecting to TIDAL.. Quality = %s" % quality)
+        config = Config(quality=Quality(quality))
+        self._session = Session(config)
+        if self._session.login(self._username, self._password):
+            logger.info("TIDAL Login OK")
+        else:
+            logger.info("TIDAL Login KO")
