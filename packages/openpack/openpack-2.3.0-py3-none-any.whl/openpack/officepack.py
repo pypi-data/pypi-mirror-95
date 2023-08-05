992d2d19f5c601206af0e1025913950734be3a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:05 +0000

    Add package openpack/2.3.0/openpack-2.3.0-py3-none-any.whl

---
+++ packages/openpack/openpack-2.3.0-py3-none-any.whl/openpack/officepack.py
@@ -0,0 +1,16 @@
+from .zippack import ZipPackage
+
+
+class OfficePackage(ZipPackage):
+    """
+    A Microsoft Office OOXML package.
+    """
+
+    main_rel = (
+        "http://schemas.openxmlformats.org"
+        "/officeDocument/2006/relationships/officeDocument"
+    )
+
+    @property
+    def start_part(self):
+        return self.related(self.main_rel)[0]
