f31d1f12582d3efa2ca4a2744a0671124d5dcfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:52 +0000

    Add package favico/0.1.4/favico-0.1.4.tar.gz

---
+++ packages/favico/favico-0.1.4.tar.gz/favico-0.1.4/favico/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for favico."""
+
+from favico.favico import generate
+
+__author__ = """romnn"""
+__email__ = "contact@romnn.com"
+__version__ = "0.1.4"
+
+__all__ = ["generate"]
