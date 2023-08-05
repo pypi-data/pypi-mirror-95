5190dd99e351a78afa36661a121e841c07ee204c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:06 +0000

    Add package abna/0.3/abna-0.3.tar.gz

---
+++ packages/abna/abna-0.3.tar.gz/abna-0.3/README.md
@@ -0,0 +1,41 @@
+# ABN Amro mutations retrieval
+
+This Python library enables retrieval of mutations from the Dutch ABN Amro
+banking site using the "soft token" (5-digit pass code).
+
+Should work with Python 2.7 or 3.x; requires requests and cryptography.
+
+This library was created by and is maintained by Dirkjan Ochtman. If you are
+in a position to support ongoing maintenance and further development or use it
+in a for-profit context, please consider supporting my open source work on
+[Patreon](https://www.patreon.com/dochtman).
+
+## Example
+
+Here is a minimal example demonstrating how to use the library:
+
+```python
+import abna, json
+
+sess = abna.Session('NL01ABNA0123456789')
+sess.login(123, '12345')
+print(json.dumps(sess.mutations('NL01ABNA0123456789'), indent=2))
+```
+
+## Change log
+### 0.3 (2020-02-04)
+
+- Add User-Agent to fake browser behavior
+
+### 0.2 (2018-07-15)
+
+- Allow retrieval of mutations from different accounts
+  ([#1](https://github.com/djc/abna/pull/1), thanks to
+  [@ivasic](https://github.com/ivasic)). Note that this changes the signature
+  of the `Session.mutations()` method to take the account IBAN as a mandatory
+  first argument.
+
+## Alternatives
+
+[abnamro-tx](https://github.com/mkrcah/abnamro-tx) is a docker-based solution
+to run a headless Chrome instance that can download mutation files for you.
---
+++ packages/abna/abna-0.3.tar.gz/abna-0.3/abna/__init__.py
@@ -0,0 +1,114 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicNumbers
+import copy, random, requests, sys
+
+BASE = 'https://www.abnamro.nl'
+START = BASE + '/portalserver/mijn-abnamro/mijn-overzicht/overzicht/index.html'
+SERVICE_VERSION = {'x-aab-serviceversion': 'v3'}
+COMMON_USER_AGENTS = 'https://gist.githubusercontent.com/fijimunkii/952acac988f2d25bef7e0284bc63c406/raw/190452518c6bcc856b751333a0556588da0daf45/ua.json'
+
+class Session(object):
+    def __init__(self, iban):
+        self.iban = iban
+        self.session = requests.Session()
+        ua = random.choice(self.session.get(COMMON_USER_AGENTS).json())
+        self.session.headers.update({'User-Agent': ua})
+
+    def login(self, card, token):
+        card = int(card)
+        token = str(token)
+
+        base = {
+            'accessToolUsage': 'SOFTTOKEN',
+            'accountNumber': int(self.iban[8:]),
+            'appId': 'SIMPLE_BANKING',
+            'cardNumber': card,
+        }
+
+        self.session.get(START)
+        rsp = self.session.get(BASE + '/session/loginchallenge', params=base)
+        challenge = rsp.json()['loginChallenge']
+
+        response = calculate_response(challenge['challenge'], challenge['userId'], token)
+        payload = copy.copy(base)
+        payload['response'] = response
+        for k in {'challengeHandle', 'challengeDeviceDetails'}:
+            payload[k] = challenge[k]
+
+        url = BASE + '/session/loginresponse'
+        rsp = self.session.put(url, json=payload, headers=SERVICE_VERSION)
+        if not rsp.ok:
+            raise Exception(rsp.json())
+
+    def mutations(self, iban, last_key=None):
+        params = {
+            'accountNumber': self.iban,
+            'includeActions': 'EXTENDED',
+        }
+        if last_key is not None:
+            params['lastMutationKey'] = last_key
+
+        url = BASE + '/mutations/' + iban
+        rsp = self.session.get(url, headers=SERVICE_VERSION, params=params)
+        if rsp.ok:
+            return rsp.json()
+        else:
+            raise Exception(rsp.json())
+
+def calculate_response(challenge, user_id, password):
+    obj = decode(challenge)
+    out = {
+        1: [49],
+        2: obj[2],
+        3: obj[3],
+        8: [ord(c) for c in user_id],
+        9: [ord(c) for c in password],
+    }
+    encoded = encode(out)
+    pub_key = RSAPublicNumbers(ba2num(obj[5]), ba2num(obj[4])).public_key(default_backend())
+    encrypted = pub_key.encrypt(ba2bytes(encoded), PKCS1v15())
+    return bytes2hex(encrypted)
+
+def ba2num(v):
+    res = 0
+    size = len(v) - 1
+    for (i, v) in enumerate(v):
+        res += v << ((size - i) * 8)
+    return res
+
+def decode(challenge):
+    bytes = hex2ba(challenge)
+    res = {}
+    cur = 0
+    while cur < len(bytes):
+        key = bytes[cur]
+        size = (bytes[cur + 1] << 8) + bytes[cur + 2]
+        res[key] = bytes[cur + 3:cur + 3 + size]
+        cur += 3 + size
+    return res
+
+def encode(obj):
+    res = []
+    for k, v in sorted(obj.items()):
+        res.append(k)
+        res.append((len(v) >> 8) & 255)
+        res.append(len(v) & 255)
+        res += v
+    res += [0, 0, 0]
+    return res
+
+if sys.version_info[0] < 3:
+    def hex2ba(s):
+        return [ord(c) for c in s.decode('hex')]
+    def ba2bytes(s):
+        return ''.join(chr(i) for i in s)
+    def bytes2hex(s):
+        return s.encode('hex')
+else:
+    def hex2ba(s):
+        return [i for i in s.encode('ascii').fromhex(s)]
+    def ba2bytes(s):
+        return bytes(s)
+    def bytes2hex(s):
+        return s.hex()
