4d4299d9cc47085715469c50cc41dd37aff3f3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:58 +0000

    Add package molo.core/11.1.0/molo.core-11.1.0.tar.gz

---
+++ packages/molo.core/molo.core-11.1.0.tar.gz/molo.core-11.1.0/molo/core/context_processors.py
@@ -0,0 +1,16 @@
+from django.utils.translation import get_language_from_request
+from molo.core.utils import get_locale_code
+from molo.core.models import Languages
+from django.conf import settings
+
+
+def locale(request):
+    locale_code = get_locale_code(
+        get_language_from_request(request, check_path=True))
+    site = settings.site
+    languages = Languages.for_site(site).languages.filter(
+        is_active=True)
+    return {
+        'locale_code': locale_code,
+        'languages': languages,
+        'selected_language': languages.filter(locale=locale_code).first()}
