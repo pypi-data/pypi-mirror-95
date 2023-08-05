5bcb2a499452b34f76b3f6dd655064bde3ae3b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:01 +0000

    Add package pytest-blender/0.0.4/pytest_blender-0.0.4-py3-none-any.whl

---
+++ packages/pytest-blender/pytest_blender-0.0.4-py3-none-any.whl/pytest_blender/__main__.py
@@ -0,0 +1,58 @@
+import argparse
+import sys
+
+from pytest_blender import __version__
+from pytest_blender.run_pytest import get_blender_binary_path_python
+from pytest_blender.utils import which_blender_by_os
+
+
+__description__ = "Show a Blender builtin Python interpreter location."
+
+
+def build_parser():
+    parser = argparse.ArgumentParser(description=__description__)
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version="%(prog)s " + __version__,
+        help="Show program version number and exit.",
+    )
+    parser.add_argument(
+        "--blender-executable",
+        dest="blender_executable",
+        nargs=1,
+        default=which_blender_by_os,
+        help="Blender executable location.",
+    )
+    return parser
+
+
+def parse_args(args):
+    parser = build_parser()
+    if "-h" in args or "--help" in args:
+        parser.print_help()
+        sys.exit(0)
+    opts = parser.parse_args(args)
+
+    if hasattr(opts.blender_executable, "__call__"):
+        opts.blender_executable = opts.blender_executable()
+    else:
+        opts.blender_executable = opts.blender_executable[0]
+
+    return opts
+
+
+def run(args):
+    opts = parse_args(args)
+    sys.stdout.write(f"{get_blender_binary_path_python(opts.blender_executable)}\n")
+
+    return 0
+
+
+def main():
+    sys.exit(run(args=sys.argv[1:]))
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pytest-blender/pytest_blender-0.0.4-py3-none-any.whl/pytest_blender/plugin.py
@@ -0,0 +1,67 @@
+"""pytest-blender plugin"""
+
+import io
+import os
+import subprocess
+import sys
+
+import pytest
+
+from pytest_blender.utils import which_blender_by_os
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--blender-executable",
+        nargs=1,
+        default=which_blender_by_os,
+        help="Blender executable location.",
+    )
+
+
+def _get_blender_executable(config):
+    blender_executable = config.getoption("--blender-executable")
+    if hasattr(blender_executable, "__call__"):
+        return blender_executable()
+    return blender_executable[0]
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    # build propagated CLI args
+    propagated_cli_args = []
+    _inside_root_invocation_arg = False
+    for arg in sys.argv[1:]:
+        if _inside_root_invocation_arg:
+            _inside_root_invocation_arg = False
+            continue
+        elif arg == "--blender-executable":
+            _inside_root_invocation_arg = True
+            continue
+        propagated_cli_args.append(arg)
+
+    blender_executable = _get_blender_executable(config)
+
+    # run pytest using blender
+    proc = subprocess.Popen(
+        [
+            blender_executable,
+            "-b",
+            "--python",
+            os.path.join(
+                os.path.abspath(os.path.dirname(__file__)),
+                "run_pytest.py",
+            ),
+            "--",
+            "--pytest-blender-executable",
+            blender_executable,
+            *propagated_cli_args,  # propagate command line arguments
+        ],
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
+    proc.communicate()
+
+    # hide "Exit:" message shown by pytest on exit
+    sys.stderr = io.StringIO()
+    pytest.exit("", returncode=proc.returncode)
