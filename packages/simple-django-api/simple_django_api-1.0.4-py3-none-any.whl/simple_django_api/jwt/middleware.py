514355e645892301b5fd1d592f8b8e527c95e098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:07 +0000

    Add package simple-django-api/1.0.4/simple_django_api-1.0.4-py3-none-any.whl

---
+++ packages/simple-django-api/simple_django_api-1.0.4-py3-none-any.whl/simple_django_api/jwt/middleware.py
@@ -0,0 +1,19 @@
+from django.utils.functional import SimpleLazyObject
+from . import auth
+
+
+def get_user(request):
+    if not hasattr(request, '_cached_user'):
+        request._cached_user = auth.get_user(request)
+    return request._cached_user
+
+
+class AuthenticationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if not getattr(request, 'user'):
+            request.user = SimpleLazyObject(lambda: get_user(request))
+        response = self.get_response(request)
+        return response
