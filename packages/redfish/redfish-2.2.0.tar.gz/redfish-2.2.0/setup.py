379a935ba6f11bf525815fc13418f906140ccf30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:01 +0000

    Add package redfish/2.2.0/redfish-2.2.0.tar.gz

---
+++ packages/redfish/redfish-2.2.0.tar.gz/redfish-2.2.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='redfish',
+      version='2.2.0',
+      description='Redfish Python Library',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      author = 'DMTF, https://www.dmtf.org/standards/feedback',
+      license='BSD 3-clause "New" or "Revised License"',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Topic :: Communications'
+      ],
+      keywords='Redfish',
+      url='https://github.com/DMTF/python-redfish-library',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      install_requires=[
+          'jsonpath_rw',
+          'jsonpointer',
+      ],
+      extras_require={
+          ':python_version == "3.4"': [
+              'jsonpatch<=1.24'
+          ],
+          ':python_version >= "3.5" or python_version == "2.7"': [
+              'jsonpatch'
+          ]
+      })
