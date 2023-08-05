1e84610d7b2c207a2a885e1040fdfce8029a0ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:56 +0000

    Add package DIRAC/7.2.0a34/DIRAC-7.2.0a34.tar.gz

---
+++ packages/DIRAC/DIRAC-7.2.0a34.tar.gz/DIRAC-7.2.0a34/src/DIRAC/Core/scripts/dirac_version.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+########################################################################
+# File :   dirac-version
+# Author : Ricardo Graciani
+########################################################################
+"""
+Print version of current DIRAC installation
+
+Usage:
+  dirac-version [option]
+
+Example:
+  $ dirac-version
+
+"""
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+__RCSID__ = "$Id$"
+
+import argparse
+
+parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.parse_known_args()
+
+import DIRAC
+from DIRAC.Core.Utilities.DIRACScript import DIRACScript
+
+
+@DIRACScript()
+def main():
+  print(DIRAC.version)
+
+
+if __name__ == "__main__":
+  main()
