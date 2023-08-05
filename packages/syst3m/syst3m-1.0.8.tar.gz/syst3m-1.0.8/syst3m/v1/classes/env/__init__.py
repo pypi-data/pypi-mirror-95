c682ed1c8588d658b2bf8d09c7cc65a64fd5310c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:54 +0000

    Add package syst3m/1.0.8/syst3m-1.0.8.tar.gz

---
+++ packages/syst3m/syst3m-1.0.8.tar.gz/syst3m-1.0.8/syst3m/v1/classes/env/__init__.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from syst3m.v1.classes.config import *
+
+# the env object class.
+class Env(object):
+	def __init__(self):
+		a=1
+		#
+	def fill(self, string):
+		c = 0
+		for i in range(string.count("$")):
+			c += 1
+			if string[0] == "$" and c == 1:
+				variable_id, _ = Formats.String(string.split("$")[1]).before_after_first_occurence(slicer=["/","+",">","<","?",";",":","|","\\","]","[","{","}","=","+","(",")","!","@","#","$","%","^","&","*","±","~","`","'",'"'," "])
+			else:
+				variable_id, _ = Formats.String(string.split("$")[c]).before_after_first_occurence(slicer=["/","+",">","<","?",";",":","|","\\","]","[","{","}","=","+","(",")","!","@","#","$","%","^","&","*","±","~","`","'",'"'," "])
+			variable = os.environ.get(variable_id)
+			if variable == None: raise ValueError(f"Required environment variable {variable_id} is undefined.")
+			variable = get_string(variable)
+			string = string.replace(f"${variable_id}", variable)
+		string = string.replace("~",HOME)
+		return string
+	def get_string(self, id, default=None):
+		e = os.environ.get(id)
+		if e == None: 
+			return default
+		else:
+			return e
+	def get_boolean(self, id, default=None):
+		e = os.environ.get(id)
+		if e == None: 
+			return default
+		elif e in [True, "True", "true", "TRUE"]:
+			return True
+		else: 
+			return False
+	def get_integer(self, id, default=None):
+		e = os.environ.get(id)
+		try:
+			return int(e)
+		except:
+			return default
+	def get_array(self, id, default=None):
+		e = os.environ.get(id)
+		if e == None: 
+			return default
+		else: 
+			a = []
+			for i in e.split(","):
+				a.append(self.__remove_whitespace__(i))
+			return a
+	def get_tuple(self, id, default=None):
+		e = os.environ.get(id)
+		if e == None: 
+			return default
+		else: 
+			a = []
+			for i in e.split(","):
+				a.append(self.__remove_whitespace__(i))
+			return tuple(a)
+	def get_dictionary(self, id, default=None):
+		e = os.environ.get(id)
+		if e == None: 
+			return default
+		else: 
+			a = []
+			for i in e.split(","):
+				a.append(self.__remove_whitespace__(i))
+			d = {}
+			for i in a:
+				try:
+					key,value = i.split(":")
+					d[self.__remove_whitespace__(key)] = self.__remove_whitespace__(value)
+				except: a=1
+			return tuple(a)
+	def __remove_whitespace__(self, string):
+		if isinstance(string, str):
+			for attempts in range(101):
+				if string[0] == " ":
+					string = string[1:]
+				elif string[len(string)-1] == " ":
+					string = string[:-1]
+				else: break
+		return string
+	
+
+# initialized classes.
+env = Env()
