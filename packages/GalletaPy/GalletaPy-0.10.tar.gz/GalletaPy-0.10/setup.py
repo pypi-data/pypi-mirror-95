25a348761ca844c7a343f4479eeb889ffc42fb3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:04 +0000

    Add package GalletaPy/0.10/GalletaPy-0.10.tar.gz

---
+++ packages/GalletaPy/GalletaPy-0.10.tar.gz/GalletaPy-0.10/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("READMEpy.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GalletaPy",
+    version="0.10",
+    author="Juan Felipe Huan Lew Yee",
+    author_email="felipe.lew.yee@gmail.com",
+    description="Paquete con código para ERIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/felipelewyee/galleta",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_data={'': ['basis/*.bas']},
+    include_package_data=True,    
+    install_requires=['numpy','scipy','pubchempy'],
+)
+
