1ee6b0010df3c9d6ae3187bda342a0d165dea71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:06:57 +0000

    Add package bloggo/1.0.2/bloggo-1.0.2.tar.gz

---
+++ packages/bloggo/bloggo-1.0.2.tar.gz/bloggo-1.0.2/bloggo/__main__.py
@@ -0,0 +1,3 @@
+import bloggo
+
+bloggo.run()
---
+++ packages/bloggo/bloggo-1.0.2.tar.gz/bloggo-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as readme:
+    long_description = readme.read()
+
+setup(name='bloggo',
+      version='1.0.2',
+      description='A blog-oriented static site generator',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/askonomm/bloggo',
+      author='Asko Nõmm',
+      author_email='asko@askonomm.com',
+      license='MIT',
+      packages=['bloggo'],
+      zip_safe=False)
