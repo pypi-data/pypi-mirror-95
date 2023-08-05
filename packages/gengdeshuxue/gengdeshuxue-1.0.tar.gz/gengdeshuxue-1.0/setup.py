81f99b28b8401a9dbd8f4481e7bc4781d357b999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:58 +0000

    Add package gengdeshuxue/1.0/gengdeshuxue-1.0.tar.gz

---
+++ packages/gengdeshuxue/gengdeshuxue-1.0.tar.gz/gengdeshuxue-1.0/setup.py
@@ -0,0 +1,10 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+name='gengdeshuxue', # 对外我们模块的名字
+version='1.0', # 版本号
+description='这是第一个对外发布的模块，测试哦', #描述
+author='小耿', # 作者
+author_email='1113474592@qq.com', py_modules=['baizhanMath2.demo1','baizhanMath2.demo2'] # 要发布的模块
+)
