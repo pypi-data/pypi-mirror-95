6074250ef466e892a5f770fff1f7fd38cfe34fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:31 +0000

    Add package NEMtropy/2.0.0/NEMtropy-2.0.0-py3-none-any.whl

---
+++ packages/NEMtropy/NEMtropy-2.0.0-py3-none-any.whl/NEMtropy/__init__.py
@@ -0,0 +1,8 @@
+from .graph_classes import *
+from .matrix_generator import *
+from .ensemble_generator import *
+from .models_functions import *
+
+__version__ = "1.0.5"
+__author__ = """Nicolo' Vallarano (nicolo.vallarano@imtlucca.it)
+Emiliano Marchese (emiliano.marchese@imtulucca.it)"""
