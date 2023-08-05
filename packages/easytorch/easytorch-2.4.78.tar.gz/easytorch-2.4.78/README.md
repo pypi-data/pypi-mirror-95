2a7ddd235bfb407be77a89eebe5791f17041b8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:28 +0000

    Add package easytorch/2.4.78/easytorch-2.4.78.tar.gz

---
+++ packages/easytorch/easytorch-2.4.78.tar.gz/easytorch-2.4.78/README.md
@@ -0,0 +1,161 @@
+![Logo](assets/easytorch.png)
+### A quick and easy way to start running pytorch experiments within few minutes.
+[![YourActionName Actions Status](https://github.com/sraashis/easytorch/workflows/build/badge.svg)](https://github.com/sraashis/easytorch/actions)
+![versions](https://img.shields.io/pypi/pyversions/pybadges.svg)
+
+<hr/>
+
+## Installation
+1. Install latest pytorch and torchvision from [Pytorch official website](https://pytorch.org/)
+2. pip install easytorch
+
+## `'How to use?' you ask!`
+
+### 1. Define your trainer
+
+```python
+from easytorch import ETTrainer
+
+
+class MyTrainer(ETTrainer):
+  def __init__(self, args):
+    super().__init__(args)
+
+  def _init_nn_model(self):
+    self.nn['model'] = NeuralNetModel(self.args['num_channel'], self.args['num_class'])
+
+  def iteration(self, batch):
+    inputs = batch['input'].to(self.device['gpu']).float()
+    labels = batch['label'].to(self.device['gpu']).long()
+
+    out = self.nn['model'](inputs)
+    loss = F.cross_entropy(out, labels)
+    out = F.softmax(out, 1)
+
+    _, pred = torch.max(out, 1)
+    sc = self.new_metrics()
+    sc.add(pred, labels)
+
+    avg = self.new_averages()
+    avg.add(loss.item(), len(inputs))
+
+    return {'loss': loss, 'averages': avg, 'output': out, 'metrics': sc, 'predictions': pred}
+  
+   def init_experiment_cache(self):
+      r"""What scores you want to plot."""
+      r"""By default it plots Loss,Accuracy,F1,Precision,Recall."""
+      self.cache['log_header'] = 'Loss,Accuracy,F1,Precision,Recall'
+
+      r"""This is for best model selection: """
+      r"""It tells which metrics to monitor and either to maximize(F1 score), minimize(MSE)"""
+      self.cache.update(monitor_metric='f1', metric_direction='maximize')
+
+````
+
+### 2. Use custom or pytorch based Datasets class.
+ ***Define specification for your datasets:***
+```python
+import os
+sep = os.sep
+MYDATA = {
+    'name': 'mydata',
+    'data_dir': 'MYDATA' + sep + 'images',
+    'label_dir': 'MYDATA' + sep + 'labels',
+    'label_getter': lambda file_name: file_name.split('_')[0] + 'label.csv'
+}
+
+MyOTHERDATA = {
+    'name': 'otherdata',
+    'data_dir': 'OTHERDATA' + sep + 'images',
+    'label_dir': 'OTHERDATA' + sep + 'labels',
+    'label_getter': lambda file_name: file_name.split('_')[0] + 'label.csv'
+}
+```
+
+***Define how to load each data item***
+```python
+from easytorch import ETDataset
+import torchvision
+class MyDataset(ETDataset):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def __getitem__(self, index):
+        dataset_name, file = self.indices[index]
+        dataspec = self.dataspecs[dataset_name]
+        
+        """
+        All the info. (data_dir, label_dir, label_getter...) defined above will be in dataspec.
+        """
+        image = #Todo # Load file/Image. 
+        label = #Todo # Load corresponding label.
+        # Extra preprocessing, if needed.
+        # Apply transforms.
+        
+        return {'indices': self.indices[index],
+                'input': image,
+                'label': label}
+    @property
+    def transforms(self):
+        return torchvision.transforms.Compose(["""List of transforms"""])
+```
+
+### 3. Entry point
+
+```python
+from easytorch import EasyTorch
+runner = EasyTorch([MYDATA, MyOTHERDATA],
+                   phase="train", batch_size=4, epochs=21,
+                   num_channel=1, num_class=2)
+
+if __name__ == "__main__":
+    runner.run(MyDataset, MyTrainer)
+    runner.run_pooled(MyDataset, MyTrainer)
+```
+
+
+<hr />
+
+### Complete Examples
+* **[Vessel segmentation with U-NET example.](https://github.com/sraashis/unet-vessel-segmentation-easytorch)**
+* **[Healthy/Pneumonia/Covid-19 chest x-ray (Multi-label/Binary)classification example.](https://github.com/sraashis/covidxfactory)**
+* **[DCGAN Example.](https://github.com/sraashis/gan-easytorch-celeb-faces)**
+
+### `Feature Higlights`
+* **For advanced training with multiple networks, and complex training steps, click [here](assets/AdvancedTraining.md):**
+* **Implement custom metrics as [here](assets/CustomMetrics.md).**
+* Minimal configuration to setup a new experiment.
+* Use your choice of Neural Network architecture.
+* Automatic k-fold cross validation/Auto dataset split.
+* Automatic logging/plotting, and model checkpointing.
+[..more features](assets/Features.md)
+
+### Default arguments[default-value]. [Easily add custom arguments.](assets/DefaultArgs.md)
+* **-ph/--phase** [Required]
+    * Which phase to run? 'train' (runs all train, validation, test steps) OR 'test' (runs only test step).
+* **-b/--batch_size** [4]
+* **-ep/--epochs** [51]
+* **-lr/--learning_rate** [0.001]
+* -**gpus/--gpus** [0]
+    * List of gpus to be used. Eg. [0], [1], [0, 1]
+* **-nw/--num_workers** [4]
+    * Number of workers for data loading so that cpu can keep-up with GPU speed when loading mini-batches.
+* **-lim/--load-limit**[max]
+    * Specifies a limit on images/files to load for debug purpose for pipeline debugging.
+* **-nf/--num_folds** [None]
+    * Number of folds in k-fold cross validation(Integer value like 5, 10).
+* **-spl/--split_ratio** [0.6 0.2 0.2]
+    * Split ratio for train, validation, test set if two items given| train, test if three items given| train only if one item given.
+* [...see more](assets/DefaultArgs.md)
+## All the best! for whatever you are working on. Cheers!
+#### Please star or cite if you find it useful.
+```
+@misc{easytorch,
+  author = {Khanal, Aashis},
+  title = {Easy Torch}
+  year = {2020},
+  publisher = {GitHub},
+  journal = {GitHub repository},
+  url = {https://github.com/sraashis/easytorch}
+}
+```
---
+++ packages/easytorch/easytorch-2.4.78.tar.gz/easytorch-2.4.78/easytorch/trainer.py
@@ -0,0 +1,376 @@
+r"""
+The main core of EasyTorch
+"""
+
+import os as _os
+from collections import OrderedDict as _ODict
+
+import torch as _torch
+
+import easytorch.data as _etdata
+import easytorch.utils as _etutils
+from easytorch.config.status import *
+from easytorch.metrics import metrics as _base_metrics
+from easytorch.utils.logger import *
+from easytorch.utils.tensorutils import initialize_weights as _init_weights
+from .vision import plotter as _log_utils
+import math as _math
+
+_sep = _os.sep
+
+
+class ETTrainer:
+    def __init__(self, args: dict):
+        r"""
+        args: receives the arguments passed by the ArgsParser.
+        cache: Initialize all immediate things here. Like scores, loss, accuracies...
+        nn:  Initialize our models here.
+        optimizer: Initialize our optimizers.
+        """
+        self.args = _etutils.FrozenDict(args)
+        self.cache = _ODict()
+        self.nn = _ODict()
+        self.device = _ODict()
+        self.optimizer = _ODict()
+
+    def init_nn(self, init_models=True, init_weights=True, init_optimizer=True, set_device=True):
+        r"""
+        Call to user implementation of:
+            Initialize models.
+            Initialize random/pre-trained weights.
+            Initialize/Detect GPUS.
+            Initialize optimizer.
+        """
+
+        if init_models: self._init_nn_model()
+        # Print number of parameters in all models.
+        if init_models and self.args['verbose']:
+            for k, m in [(_k, _m) for _k, _m in self.nn.items() if isinstance(_m, _torch.nn.Module)]:
+                success(f'Total params in {k}:' f' {sum(p.numel() for p in m.parameters() if p.requires_grad)}')
+
+        if init_weights: self._init_nn_weights()
+        if init_optimizer: self._init_optimizer()
+        if set_device: self._set_device()
+
+    def _init_nn_weights(self):
+        r"""
+        By default, will initialize network with Kaimming initialization.
+        If path to pretrained weights are given, it will be used instead.
+        """
+        if self.args['pretrained_path'] is not None:
+            self.load_checkpoint(self.args['pretrained_path'])
+        elif self.args['phase'] == 'train':
+            _torch.manual_seed(self.args['seed'])
+            for mk in self.nn:
+                _init_weights(self.nn[mk])
+
+    def load_checkpoint(self, full_path, src=MYSELF):
+        r"""
+        Load checkpoint from the given path:
+            If it is an easytorch checkpoint, try loading all the models.
+            If it is not, assume it's weights to a single model and laod to first model.
+        """
+        try:
+            chk = _torch.load(full_path)
+        except:
+            chk = _torch.load(full_path, map_location='cpu')
+
+        if chk.get('_its_origin_', 'Unknown').lower() == src:
+            for m in chk['models']:
+                try:
+                    self.nn[m].module.load_state_dict(chk['models'][m])
+                except:
+                    self.nn[m].load_state_dict(chk['models'][m])
+
+            for m in chk['optimizers']:
+                try:
+                    self.optimizer[m].module.load_state_dict(chk['optimizers'][m])
+                except:
+                    self.optimizer[m].load_state_dict(chk['optimizers'][m])
+        else:
+            mkey = list(self.nn.keys())[0]
+            try:
+                self.nn[mkey].module.load_state_dict(chk)
+            except:
+                self.nn[mkey].load_state_dict(chk)
+
+    def _init_nn_model(self):
+        r"""
+        User cam override and initialize required models in self.nn dict.
+        """
+        raise NotImplementedError('Must be implemented in child class.')
+
+    def _set_device(self):
+        r"""
+        Initialize GPUs based on whats provided in args(Default [0])
+        Expects list of GPUS as [0, 1, 2, 3]., list of GPUS will make it use DataParallel.
+        If no GPU is present, CPU is used.
+        """
+        self.device['gpu'] = _torch.device("cpu")
+        if CUDA_AVAILABLE and len(self.args['gpus']) >= 1:
+            self.device['gpu'] = _torch.device(f"cuda:{self.args['gpus'][0]}")
+            if len(self.args['gpus']) >= 2:
+                for model_key in self.nn:
+                    self.nn[model_key] = _torch.nn.DataParallel(self.nn[model_key], self.args['gpus'])
+        for model_key in self.nn:
+            self.nn[model_key] = self.nn[model_key].to(self.device['gpu'])
+
+    def _init_optimizer(self):
+        r"""
+        Initialize required optimizers here. Default is Adam,
+        """
+        first_model = list(self.nn.keys())[0]
+        self.optimizer['adam'] = _torch.optim.Adam(self.nn[first_model].parameters(),
+                                                   lr=self.args['learning_rate'])
+
+    def new_metrics(self):
+        r"""
+        User can override to supply desired implementation of easytorch.metrics.ETMetrics().
+            Example: easytorch.metrics.Pr11a() will work with precision, recall, F1, Accuracy, IOU scores.
+        """
+        return _base_metrics.Prf1a()
+
+    def new_averages(self):
+        r""""
+        Should supply an implementation of easytorch.metrics.ETAverages() that can keep track of multiple averages.
+            Example: multiple loss, or any other values.
+        """
+        return _base_metrics.ETAverages(num_averages=1)
+
+    def save_checkpoint(self, full_path, src=MYSELF):
+        checkpoint = {'_its_origin_': src}
+        for k in self.nn:
+            checkpoint['models'] = {}
+            try:
+                checkpoint['models'][k] = self.nn[k].module.state_dict()
+            except:
+                checkpoint['models'][k] = self.nn[k].state_dict()
+        for k in self.optimizer:
+            checkpoint['optimizers'] = {}
+            try:
+                checkpoint['optimizers'][k] = self.optimizer[k].module.state_dict()
+            except:
+                checkpoint['optimizers'][k] = self.optimizer[k].state_dict()
+        _torch.save(checkpoint, full_path)
+
+    def init_experiment_cache(self):
+        r"""What scores you want to plot."""
+        r"""By default it plots Loss,Accuracy,F1,Precision,Recall."""
+        self.cache['log_header'] = 'Loss,Accuracy,F1,Precision,Recall'
+
+        r"""This is for best model selection: """
+        r"""It tells which metrics to monitor and either to maximize(F1 score), minimize(MSE)"""
+        self.cache.update(monitor_metric='f1', metric_direction='maximize')
+
+    def save_if_better(self, epoch, metrics):
+        r"""
+        Save the current model as best if it has better validation scores.
+        """
+        sc = getattr(metrics, self.cache['monitor_metric'])
+        if callable(sc):
+            sc = sc()
+
+        if (self.cache['metric_direction'] == 'maximize' and sc >= self.cache['best_score']) or (
+                self.cache['metric_direction'] == 'minimize' and sc <= self.cache['best_score']):
+            self.save_checkpoint(self.cache['log_dir'] + _sep + self.cache['checkpoint'])
+            self.cache['best_score'] = sc
+            self.cache['best_epoch'] = epoch
+            success(f"Best Model Saved!!! : {self.cache['best_score']}", self.args['verbose'])
+        else:
+            warn(f"Not best: {sc}, {self.cache['best_score']} in ep: {self.cache['best_epoch']}", self.args['verbose'])
+
+    def iteration(self, batch):
+        r"""
+        Left for user to implement one mini-bath iteration:
+        Example:{
+                    inputs = batch['input'].to(self.device['gpu']).float()
+                    labels = batch['label'].to(self.device['gpu']).long()
+                    out = self.nn['model'](inputs)
+                    loss = F.cross_entropy(out, labels)
+                    out = F.softmax(out, 1)
+                    _, pred = torch.max(out, 1)
+                    sc = self.new_metrics()
+                    sc.add(pred, labels)
+                    avg = self.new_averages()
+                    avg.add(loss.item(), len(inputs))
+                    return {'loss': loss, 'averages': avg, 'output': out, 'metrics': sc, 'predictions': pred}
+                }
+        Note: loss, averages, and metrics are required, whereas others are optional
+            -we will have to do backward on loss
+            -we need to keep track of loss
+            -we need to keep track of metrics
+        """
+        return {}
+
+    def save_predictions(self, dataset, its):
+        r"""
+        If one needs to save complex predictions result like predicted segmentations.
+         -Especially with U-Net architectures, we split images and train.
+        Once the argument --sp/-sparse-load is set to True,
+        the argument 'its' will receive all the patches of single image at a time.
+        From there, we can recreate the whole image.
+        """
+        pass
+
+    def evaluation(self, epoch=0, mode='eval', dataset_list=None, save_pred=False):
+        info(f'{mode} ...', self.args['verbose'])
+
+        for k in self.nn:
+            self.nn[k].eval()
+
+        eval_avg, eval_metrics = self.new_averages(), self.new_metrics()
+
+        loaders = []
+        _args = {**self.args}
+        _args['shuffle'] = False
+        for dataset in dataset_list:
+            loaders.append(_etdata.ETDataLoader.new(mode=mode, dataset=dataset, **_args))
+        with _torch.no_grad():
+            for loader in loaders:
+                its = []
+                metrics = self.new_metrics()
+                avg = self.new_averages()
+                for i, batch in enumerate(loader, 1):
+
+                    it = self.iteration(batch)
+                    if not it.get('metrics'):
+                        it['metrics'] = _base_metrics.ETMetrics()
+
+                    if not it.get('averages'):
+                        it['averages'] = _base_metrics.ETAverages()
+
+                    metrics.accumulate(it['metrics']), avg.accumulate(it['averages'])
+                    if save_pred:
+                        its.append(it)
+
+                    if self.args['verbose'] and len(dataset_list) <= 1 and lazy_debug(i, add=epoch):
+                        info(f" Itr:{i}/{len(loader)},{it['averages'].get()},{it['metrics'].get()}")
+
+                eval_metrics.accumulate(metrics)
+                eval_avg.accumulate(avg)
+                if self.args['verbose'] and len(dataset_list) > 1:
+                    info(f" {mode}, {avg.get()}, {metrics.get()}")
+                if save_pred:
+                    self.save_predictions(loader.dataset, self._reduce_iteration(its))
+
+        info(f" {self.cache['experiment_id']} {mode} metrics: {eval_avg.get()}, {eval_metrics.get()}",
+             self.args['verbose'])
+        return eval_avg, eval_metrics
+
+    def _reduce_iteration(self, its):
+        reduced = {}.fromkeys(its[0].keys(), None)
+        for key in reduced:
+            if isinstance(its[0][key], _base_metrics.ETAverages):
+                reduced[key] = self.new_averages()
+                [reduced[key].accumulate(ik[key]) for ik in its]
+
+            elif isinstance(its[0][key], _base_metrics.ETMetrics):
+                reduced[key] = self.new_metrics()
+                [reduced[key].accumulate(ik[key]) for ik in its]
+            else:
+                def collect(k=key, src=its):
+                    _data = []
+                    is_tensor = isinstance(src[0][k], _torch.Tensor)
+                    is_tensor = is_tensor and not src[0][k].requires_grad and src[0][k].is_leaf
+                    for ik in src:
+                        if is_tensor:
+                            _data.append(ik[k] if len(ik[k].shape) > 0 else ik[k].unsqueeze(0))
+                        else:
+                            _data.append(ik[k])
+                    if is_tensor:
+                        return _torch.cat(_data)
+                    return _data
+
+                reduced[key] = collect
+
+        return reduced
+
+    def _on_epoch_end(self, **kw):
+        r"""
+        Any logic to run after an epoch ends.
+        """
+        pass
+
+    def _on_iteration_end(self, i, epoch, it):
+        r"""
+        Any logic to run after an iteration ends.
+        """
+        pass
+
+    def _stop_early(self, **kw):
+        r"""
+        Stop the training based on some criteria.
+         For example: the implementation below will stop training if the validation
+         scores does not improve within a 'patience' number of epochs.
+        """
+        return kw.get('epoch') - self.cache['best_epoch'] >= self.args.get('patience', 'epochs')
+
+    def _save_progress(self, epoch):
+        _log_utils.plot_progress(self.cache, experiment_id=self.cache['experiment_id'],
+                                 plot_keys=[LogKey.TRAIN_LOG, LogKey.VALIDATION_LOG], epoch=epoch)
+
+    def training_iteration(self, i, batch):
+        r"""
+        Learning step for one batch.
+        We decoupled it so that user could implement any complex/multi/alternate training strategies.
+        """
+        it = self.iteration(batch)
+        it['loss'].backward()
+        if i % self.args.get('num_iterations', 1) == 0:
+            for optim in self.optimizer:
+                self.optimizer[optim].step()
+                self.optimizer[optim].zero_grad()
+        return it
+
+    def train(self, dataset, val_dataset):
+        info('Training ...', self.args['verbose'])
+
+        local_iter = self.args.get('num_iterations', 1)
+        _args = {**self.args}
+        _args['shuffle'] = True
+        train_loader = _etdata.ETDataLoader.new(mode='train', dataset=dataset, **_args)
+        tot_iter = len(train_loader) // local_iter
+
+        for epoch in range(1, self.args['epochs'] + 1):
+            for k in self.nn:
+                self.nn[k].train()
+
+            _metrics, _avg, its = self.new_metrics(), self.new_averages(), []
+            ep_avg, ep_metrics = self.new_averages(), self.new_metrics()
+
+            for i, batch in enumerate(train_loader, 1):
+                its.append(self.training_iteration(i, batch))
+                if i % local_iter == 0:
+                    it = self._reduce_iteration(its)
+                    if not it.get('metrics'):
+                        it['metrics'] = _base_metrics.ETMetrics()
+                    if not it.get('averages'):
+                        it['averages'] = _base_metrics.ETAverages()
+
+                    ep_avg.accumulate(it['averages']), ep_metrics.accumulate(it['metrics'])
+                    _avg.accumulate(it['averages']), _metrics.accumulate(it['metrics'])
+
+                    _i, its = i // local_iter, []
+                    if lazy_debug(_i, add=epoch) or _i == tot_iter:
+                        info(f"Ep:{epoch}/{self.args['epochs']},Itr:{_i}/{tot_iter},{_avg.get()},{_metrics.get()}",
+                             self.args['verbose'])
+                        self.cache[LogKey.TRAIN_LOG].append([*_avg.get(), *_metrics.get()])
+                        _metrics.reset(), _avg.reset()
+                    self._on_iteration_end(i=_i, epoch=epoch, it=it)
+
+            val_averages, val_metric = self.evaluation(epoch=epoch, mode='validation', dataset_list=[val_dataset])
+            self.cache[LogKey.VALIDATION_LOG].append([*val_averages.get(), *val_metric.get()])
+            self.save_if_better(epoch, val_metric)
+
+            self._on_epoch_end(epoch=epoch, epoch_averages=ep_avg, epoch_metrics=ep_metrics,
+                               validation_averages=val_averages, validation_metric=val_metric)
+
+            if lazy_debug(epoch, _math.log(epoch)):
+                self._save_progress(epoch=epoch)
+
+            if self._stop_early(epoch=epoch, epoch_averages=ep_avg, epoch_metrics=ep_metrics,
+                                validation_averages=val_averages, validation_metric=val_metric):
+                break
+
+        """Plot at the end regardless."""
+        self._save_progress(epoch=epoch)
