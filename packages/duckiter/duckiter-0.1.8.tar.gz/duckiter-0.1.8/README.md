6d3016c37ad2cca86df3914e43eeb639daf4419a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:12 +0000

    Add package duckiter/0.1.8/duckiter-0.1.8.tar.gz

---
+++ packages/duckiter/duckiter-0.1.8.tar.gz/duckiter-0.1.8/README.md
@@ -0,0 +1,49 @@
+<p align="center">
+<br>
+<img src="https://raw.githubusercontent.com/coci/duckiter/main/assets/logo.png">
+</p>   
+
+# Duckiter
+Duckiter will Automatically dockerize your Django projects.
+
+### Requirements :
+
+    - python version : python version 3.6 or upper version
+    - OS : linux, MacOS, windows
+    - docker engine installed in your machine and already run through the usage of package
+
+
+### Installation :
+clone project:
+```
+pip install duckiter
+```
+
+
+### Usage :
+Duckiter has two individual steps:
+    1- initialize Dockerfile
+    2- build image from created Dockerfile
+
+#### To initialize Dockerfile (step 1) :
+in terminal hit to your project directory ( where manage.py is in root ):
+```
+duckiter --init
+```
+This will create Dockerfile and config.cfg in the root of the project, and you can manipulate those files before Duckiter creates an image.
+If you are ok with our configurations, you can just pass '-build' to immediately build the image right after creating the Dockerfile.
+```
+duckiter --init -build
+```
+#### To build image (step 2) :
+To build an image from the Dockerfile that has been created in the last step, you need enter:
+```
+duckiter main.py -build
+```
+this will look for config.cfg in project directory and then build the image.
+
+
+
+### Contribution :
+
+feel free to contribute to this project, but first contact me about the idea :)
---
+++ packages/duckiter/duckiter-0.1.8.tar.gz/duckiter-0.1.8/duckiter/utility.py
@@ -0,0 +1,125 @@
+import configparser
+import os
+import random
+import string
+
+import docker
+from jinja2 import Template
+
+from duckiter.template.config_cfg import config_cfg as config_cfg_template
+from duckiter.template.docker_file import dockerfile as docker_file_template
+
+
+def get_django_project_name(project_path) -> str:
+    """
+            get project name
+
+    :param project_path: path of project
+    """
+    # list of all files and dirs in root
+    project_dirs = {}
+    for r, d, f in os.walk(project_path):
+        for file in f:
+            project_dirs[file] = os.path.join(r, file)
+
+    # get the path of settings.py file
+    project_main_dir = str(project_dirs['settings.py'])
+
+    return project_main_dir.split('/')[-2]
+
+
+def get_project_server(project_path, project_name) -> str:
+    """
+            get project server ( runserver , gunicorn , daphne ,..... )
+            it looks through requirements.txt file
+
+    :param project_path: path of project
+    :param project_name: name of django project
+    """
+
+    project_dirs = {}
+    for r, d, f in os.walk(project_path):
+        for file in f:
+            project_dirs[file] = os.path.join(r, file)
+
+    req_file_path = str(project_dirs['requirements.txt'])
+
+    is_gunicorn = False
+    is_daphne = False
+
+    with open(req_file_path, 'r') as file:
+        for line in file:
+            if 'gunicorn' in line:
+                is_gunicorn = True
+            if 'daphne' in line:
+                is_daphne = True
+
+    if is_gunicorn:
+        return f'CMD ["gunicorn"  , "-b", "0.0.0.0:8000", "{project_name}.wsgi"]'
+    elif is_daphne:
+
+        return f'CMD ["daphne","-b", "0.0.0.0", "-p","8000", "--access-log", "-","--proxy-headers", "{project_name}.asgi:application"]'
+    else:
+        return 'CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]'
+
+
+def create_docker_configuration_file(
+        config, project_name, project_path) -> None:
+    """
+            create config.cfg file that Dockerfile read settings from this
+    :param project_path: path of project
+    :param config: configuration that user entered
+    :param project_name: name of django project
+    """
+    project_server = get_project_server(
+        project_path=project_path,
+        project_name=project_name)
+
+    project_name = str(project_name)
+    project_server = str(project_server)
+
+    config_file = Template(config_cfg_template)
+    config_file = config_file.render(
+        project_name=project_name,
+        python_version=config['python_version'],
+        is_migration=config['is_migration'],
+        project_server=project_server,
+    )
+
+    with open(project_path + '/config.cfg', 'w+') as file:
+        file.write(config_file)
+
+
+def create_dockerfile(project_path) -> None:
+    """
+            create dockerfile from config.cfg
+    :param project_path: path of project
+    """
+    config = configparser.RawConfigParser()
+    config.read(f'{project_path}/config.cfg')
+
+    project_info = dict(config.items('project_info'))
+    project_server = dict(config.items('project_server'))
+    migrate = 'CMD ["python3","manage.py","migrate"]' if project_info['is_migration'] == 'True' else ""
+
+    dockerfile = Template(docker_file_template)
+    python_version = project_info['python_version']
+    python_server = project_server['project_server']
+    
+    dockerfile = dockerfile.render(
+        python_version=python_version,
+        migrate=migrate,
+        project_server=python_server,
+    )
+
+    with open(project_path + '/Dockerfile', 'w+') as file:
+        file.write(dockerfile)
+
+
+def random_string() -> str:
+    """
+            create random string with 5 character
+    """
+    letter = string.ascii_lowercase
+    random_string = ''.join(random.choice(letter) for i in range(5))
+    return random_string
