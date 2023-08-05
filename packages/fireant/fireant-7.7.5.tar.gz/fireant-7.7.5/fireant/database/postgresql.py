0b75c909d6f07ca8c588c2ded2523183e45cddc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:39 +0000

    Add package fireant/7.7.5/fireant-7.7.5.tar.gz

---
+++ packages/fireant/fireant-7.7.5.tar.gz/fireant-7.7.5/fireant/database/postgresql.py
@@ -0,0 +1,64 @@
+from pypika import (
+    Parameter,
+    PostgreSQLQuery,
+    Table,
+    terms,
+    Dialects,
+)
+
+from .base import Database
+
+
+class DateTrunc(terms.Function):
+    """
+    Wrapper for the PostgreSQL date_trunc function
+    """
+
+    def __init__(self, field, date_format, alias=None):
+        super(DateTrunc, self).__init__('DATE_TRUNC', date_format, field, alias=alias)
+
+
+class PostgreSQLDatabase(Database):
+    """
+    PostgreSQL client that uses the psycopg module.
+    """
+
+    # The pypika query class to use for constructing queries
+    query_cls = PostgreSQLQuery
+
+    def __init__(self, host="localhost", port=5432, database=None, user=None, password=None, **kwargs):
+        super().__init__(host, port, database, **kwargs)
+        self.user = user
+        self.password = password
+
+    def connect(self):
+        import psycopg2
+
+        return psycopg2.connect(
+            host=self.host,
+            port=self.port,
+            dbname=self.database,
+            user=self.user,
+            password=self.password,
+        )
+
+    def trunc_date(self, field, interval):
+        return DateTrunc(field, str(interval))
+
+    def date_add(self, field, date_part, interval):
+        interval_term = terms.Interval(**{f'{str(date_part)}s': interval, 'dialect': Dialects.POSTGRESQL})
+        return field + interval_term
+
+    def get_column_definitions(self, schema, table, connection=None):
+        columns = Table("columns", schema="information_schema")
+
+        columns_query = (
+            PostgreSQLQuery.from_(columns, immutable=False)
+            .select(columns.column_name, columns.data_type)
+            .where(columns.table_schema == Parameter('%(schema)s'))
+            .where(columns.field("table_name") == Parameter('%(table)s'))
+            .distinct()
+            .orderby(columns.column_name)
+        )
+
+        return self.fetch(str(columns_query), parameters=dict(schema=schema, table=table), connection=connection)
