15e272e81244eeb3c060484ef54bd51956fa2052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:56 +0000

    Add package snakefmt/0.3.1/snakefmt-0.3.1.tar.gz

---
+++ packages/snakefmt/snakefmt-0.3.1.tar.gz/snakefmt-0.3.1/snakefmt/exceptions.py
@@ -0,0 +1,46 @@
+class DuplicateKeyWordError(Exception):
+    pass
+
+
+class InvalidPython(Exception):
+    pass
+
+
+class StopParsing(Exception):
+    pass
+
+
+class EmptyContextError(Exception):
+    pass
+
+
+class NamedKeywordError(Exception):
+    pass
+
+
+class InvalidParameterSyntax(Exception):
+    pass
+
+
+class InvalidParameter(Exception):
+    pass
+
+
+class NoParametersError(Exception):
+    pass
+
+
+class TooManyParameters(Exception):
+    pass
+
+
+class UnsupportedSyntax(Exception):
+    pass
+
+
+class InvalidBlackConfiguration(Exception):
+    pass
+
+
+class MalformattedToml(Exception):
+    pass
