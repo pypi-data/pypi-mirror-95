f4dfe30b3e9265d888cf5445de0efa966cfc4139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:03 +0000

    Add package rawdatarinator/1.4.0/rawdatarinator-1.4.0-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/rawdatarinator/rawdatarinator-1.4.0-cp37-cp37m-macosx_10_9_x86_64.whl/rawdatarinator/__init__.py
@@ -0,0 +1,5 @@
+'''Bring functions up to correct level.'''
+
+from .twixread_pyx import twixread # pylint: disable=E0611
+from .readcfl import read # pylint: disable=E0611
+from .writecfl import write # pylint: disable=E0611
