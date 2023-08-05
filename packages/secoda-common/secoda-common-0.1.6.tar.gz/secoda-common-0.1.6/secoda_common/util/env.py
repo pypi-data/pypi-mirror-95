dc39026d474568507884a78972dc2c747f07fad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:46 +0000

    Add package secoda-common/0.1.6/secoda-common-0.1.6.tar.gz

---
+++ packages/secoda-common/secoda-common-0.1.6.tar.gz/secoda-common-0.1.6/secoda_common/util/env.py
@@ -0,0 +1,13 @@
+import os
+
+
+def req(key):
+    if key not in os.environ:
+        raise Exception(f'Missing required environment property {key}!')
+
+    return os.environ[key]
+
+
+def opt(key, default):
+    print(f'Missing environment property <{key}> --- Using default value "{default}"')
+    return os.getenv(key, default)
