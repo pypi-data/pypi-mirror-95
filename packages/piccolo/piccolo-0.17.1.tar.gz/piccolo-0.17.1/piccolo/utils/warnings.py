9f7269f425f6bc388c391d469e8cc5179c0b9f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:47 +0000

    Add package piccolo/0.17.1/piccolo-0.17.1.tar.gz

---
+++ packages/piccolo/piccolo-0.17.1.tar.gz/piccolo-0.17.1/piccolo/utils/warnings.py
@@ -0,0 +1,20 @@
+from enum import Enum
+import warnings
+
+import colorama  # type: ignore
+
+
+colorama.init()
+
+
+class Level(Enum):
+    low = colorama.Fore.WHITE
+    medium = colorama.Fore.YELLOW
+    high = colorama.Fore.RED
+
+
+def colored_warning(
+    message: str, stacklevel: int = 3, level: Level = Level.medium
+):
+    colored_message = level.value + message + colorama.Fore.RESET
+    warnings.warn(colored_message, stacklevel=stacklevel)
