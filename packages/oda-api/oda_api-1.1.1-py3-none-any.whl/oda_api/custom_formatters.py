83782ca21343d1748a7be5f94c9d9a47c9436893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:47 +0000

    Add package oda-api/1.1.1/oda_api-1.1.1-py3-none-any.whl

---
+++ packages/oda-api/oda_api-1.1.1-py3-none-any.whl/oda_api/custom_formatters.py
@@ -0,0 +1,20 @@
+from . import colors as C
+import re
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def find_custom_formatter(instrument):
+    if instrument in ("isgri", "jemx"):
+        logger.debug(f"{C.GRAY} selected custom formatter for instrument {instrument} {C.NC}")
+        return custom_progress_formatter
+    else:
+        logger.debug(f"{C.GRAY} NO custom formatter for instrument {instrument} {C.NC}")
+
+def custom_progress_formatter(L):
+    nscw = len(set([l['scwid'] for l in L if re.match("[0-9]{12}\.00[0-9]", l['scwid'])]))
+    ndone = len(set([l['node'] for l in L if l['message'] == 'main done']))
+    nrestored = len(set([l['node'] for l in L if l['message'] == 'restored from cache']))
+    nnodes = len(set([l['node'] for l in L]))
+    return f"in {nscw} SCW so far; nodes ({nnodes}): {ndone} computed {nrestored} restored"
