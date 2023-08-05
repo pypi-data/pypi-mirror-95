e0fe5512e2d5c4392f00a48ed513dea82f07a8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:00 +0000

    Add package my-package-19CS30013/0.0.2/my_package_19CS30013-0.0.2-py2-none-any.whl

---
+++ packages/my-package-19CS30013/my_package_19CS30013-0.0.2-py2-none-any.whl/my_package/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# import data
+# import analysis
+import model
+from model import ObjectDetectionModel
---
+++ packages/my-package-19CS30013/my_package_19CS30013-0.0.2-py2-none-any.whl/my_package/data/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from dataset import Dataset
+# import transforms
