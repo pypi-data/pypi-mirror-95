88aa019524c638d14536eba5c309f128ef67522d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:47 +0000

    Add package pycarrot/0.3.5/pycarrot-0.3.5.tar.gz

---
+++ packages/pycarrot/pycarrot-0.3.5.tar.gz/pycarrot-0.3.5/pycarrot/__init__.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import tkinter.font as tkFont
+import tkinter.ttk as ttk
+import tkinter.scrolledtext as ts 
+import tkinter.messagebox as mb
+from PIL import Image, ImageTk
+
+from .carrot import Carrot
+from .layout import Layout
+# We use . to make sure it is looking in the current dir
