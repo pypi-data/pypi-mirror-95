ceb7182f25cd70a6737fad85eeea21116c194ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:04 +0000

    Add package puipuigym/0.0.0/puipuigym-0.0.0.tar.gz

---
+++ packages/puipuigym/puipuigym-0.0.0.tar.gz/puipuigym-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='puipuigym',
+    version='0.0.0',
+    description='',
+    long_description='',
+    author='Yasuhiro Fujita',
+    author_email='muupan@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
