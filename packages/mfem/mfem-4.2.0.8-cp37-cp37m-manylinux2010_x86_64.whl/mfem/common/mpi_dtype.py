311f81cfb09e1946f3bc481be80a49e4c20948bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:50 +0000

    Add package mfem/4.2.0.8/mfem-4.2.0.8-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/mfem/mfem-4.2.0.8-cp37-cp37m-manylinux2010_x86_64.whl/mfem/common/mpi_dtype.py
@@ -0,0 +1,36 @@
+from mpi4py import MPI
+import traceback
+def get_mpi_datatype(data):
+    '''
+    get MPI datatype from numpy dtype
+    '''
+    #print '!!!chekcing MPI type ',  data.dtype.name
+    s = data.dtype.itemsize  # get data size of element
+    if data.dtype.name.startswith('int'):
+        if s == 8:
+           return MPI.LONG
+        elif s == 4:
+           return MPI.INT            
+        elif s == 2:         
+           return MPI.SHORT
+        else:
+           pass
+    elif data.dtype.name.startswith('float'):       
+        if s == 16:
+           return MPI.LONG_DOUBLE
+        elif s == 8:
+           return MPI.DOUBLE
+        else:
+           pass
+    elif data.dtype.name.startswith('complex'):              
+        if s == 16:
+           return MPI.DOUBLE_COMPLEX
+        elif s == 8:
+           return MPI.COMPLEX
+        else:
+           pass
+    elif data.dtype.name.startswith('bool'):
+        return MPI.BOOL        
+    else:
+        pass
+    assert False, "MPI data type is unknown for " + data.dtype.name
