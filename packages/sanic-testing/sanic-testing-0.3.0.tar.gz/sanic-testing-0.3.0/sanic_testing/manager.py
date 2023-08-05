233df2d03f74327c7730b765585544505600a94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:47 +0000

    Add package sanic-testing/0.3.0/sanic-testing-0.3.0.tar.gz

---
+++ packages/sanic-testing/sanic-testing-0.3.0.tar.gz/sanic-testing-0.3.0/sanic_testing/manager.py
@@ -0,0 +1,10 @@
+from sanic import Sanic  # type: ignore
+
+from sanic_testing.testing import SanicASGITestClient, SanicTestClient
+
+
+class TestManager:
+    def __init__(self, app: Sanic) -> None:
+        self.test_client = SanicTestClient(app)
+        self.asgi_client = SanicASGITestClient(app)
+        app._test_manager = self  # type: ignore
