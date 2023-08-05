a9cdcf48d676ee65d184dd9d51dac5388292fc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:25 +0000

    Add package yyyy-mm-dd/0.1.1/yyyy_mm_dd-0.1.1.tar.gz

---
+++ packages/yyyy-mm-dd/yyyy_mm_dd-0.1.1.tar.gz/yyyy_mm_dd-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='yyyy_mm_dd',
+    packages=find_packages(include=['yyyy_mm_dd']),
+    version='0.1.1',
+    description='Helper functions for easy string-based manipulation of dates in python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Rogerio Chaves',
+    url="https://github.com/rogeriochaves/yyyy_mm_dd",
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=['python-dateutil'],
+)
