dd5b5d84ef94f902d9535bdf641c9c24b8426f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:43 +0000

    Add package itsdangerous/2.0.0rc1/itsdangerous-2.0.0rc1.tar.gz

---
+++ packages/itsdangerous/itsdangerous-2.0.0rc1.tar.gz/itsdangerous-2.0.0rc1/docs/conf.py
@@ -0,0 +1,54 @@
+from pallets_sphinx_themes import get_version
+from pallets_sphinx_themes import ProjectLink
+
+# Project --------------------------------------------------------------
+
+project = "ItsDangerous"
+copyright = "2011 Pallets"
+author = "Pallets"
+release, version = get_version("itsdangerous")
+
+# General --------------------------------------------------------------
+
+master_doc = "index"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "pallets_sphinx_themes",
+    "sphinxcontrib.log_cabinet",
+    "sphinx_issues",
+]
+autoclass_content = "both"
+autodoc_typehints = "description"
+intersphinx_mapping = {"python": ("https://docs.python.org/3/", None)}
+issues_github_path = "pallets/itsdangerous"
+
+# HTML -----------------------------------------------------------------
+
+html_theme = "flask"
+html_theme_options = {"index_sidebar_logo": False}
+html_context = {
+    "project_links": [
+        ProjectLink("Donate to Pallets", "https://palletsprojects.com/donate"),
+        ProjectLink("Website", "https://palletsprojects.com/p/itsdangerous/"),
+        ProjectLink("PyPI releases", "https://pypi.org/project/itsdangerous/"),
+        ProjectLink("Source Code", "https://github.com/pallets/itsdangerous/"),
+        ProjectLink("Issue Tracker", "https://github.com/pallets/itsdangerous/issues/"),
+    ]
+}
+html_sidebars = {
+    "index": ["project.html", "localtoc.html", "searchbox.html"],
+    "**": ["localtoc.html", "relations.html", "searchbox.html"],
+}
+singlehtml_sidebars = {"index": ["project.html", "localtoc.html"]}
+html_static_path = ["_static"]
+html_favicon = "_static/itsdangerous-logo-sidebar.png"
+html_logo = "_static/itsdangerous-logo-sidebar.png"
+html_title = f"{project} Documentation ({version})"
+html_show_sourcelink = False
+
+# LaTeX ----------------------------------------------------------------
+
+latex_documents = [
+    (master_doc, f"{project}-{version}.tex", html_title, author, "manual")
+]
