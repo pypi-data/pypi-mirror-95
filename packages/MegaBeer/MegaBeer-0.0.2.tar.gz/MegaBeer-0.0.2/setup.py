b4efa3c4ed1214e94cb71b2e1ba82adf1816d23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:14 +0000

    Add package MegaBeer/0.0.2/MegaBeer-0.0.2.tar.gz

---
+++ packages/MegaBeer/MegaBeer-0.0.2.tar.gz/MegaBeer-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import pathlib
+
+base_path = pathlib.Path(__file__).parent
+
+readme = (base_path / 'README.md').read_text()
+
+setup(
+    name='MegaBeer',
+    version='0.0.2',
+    description='Open source brewing tools tools',
+    url='https://github.com/jhand1993/MegaBeer',
+    author='Jared Hand',
+    author_email='jared.hand1993@gmail.com',
+    license='GPL V3',
+    packages=find_packages(exclude=('tests')),
+    zip_safe=False,
+    install_requires=['numpy', 'scipy'],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True
+)
