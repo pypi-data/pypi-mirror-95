b2ce88216e088e785038a7ca07e1057ee04ad9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:34 +0000

    Add package pyxll-jupyter/0.1.19/pyxll_jupyter-0.1.19-py3-none-any.whl

---
+++ packages/pyxll-jupyter/pyxll_jupyter-0.1.19-py3-none-any.whl/pyxll_jupyter/magic.py
@@ -0,0 +1,239 @@
+"""
+Magic functions for use when running IPython inside Excel.
+"""
+from IPython.core.magic import Magics, magics_class, line_magic
+from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
+from pyxll import xl_app, plot, XLCell
+import logging
+
+_log = logging.getLogger(__name__)
+
+
+@magics_class
+class ExcelMagics(Magics):
+    """Magic functions for interacting with Excel."""
+
+    xlDown = -4121
+    xlToRight = -4161
+
+    @line_magic
+    @magic_arguments()
+    @argument("-c", "--cell", help="Address of cell to get value of.")
+    @argument("-t", "--type", help="Datatype to convert the value to.")
+    @argument("-f", "--formatter", help="PyXLL Formatter to use when setting the value.")
+    @argument("-x", "--no-auto-resize", action="store_true", help="Don't auto-resize the range.")
+    @argument("value", type=str, help="Value to set in Excel.")
+    def xl_set(self, line):
+        """Set a value to the current selection in Excel."""
+        argv = self._split_args(line)
+        args = self.xl_set.parser.parse_args(argv)
+        value = eval(args.value, self.shell.user_ns, self.shell.user_ns)
+
+        xl = xl_app(com_package="win32com")
+
+        # Get the specified range, or use the current selection
+        if args.cell:
+            selection = xl.Range(args.cell.strip("\"' "))
+        else:
+            selection = xl.Selection
+            if not selection:
+                raise Exception("Nothing selected")
+
+        # Get an XLCell object from the range to make it easier to set the value
+        cell = XLCell.from_range(selection)
+
+        if not args.no_auto_resize:
+            cell = cell.options(auto_resize=True)
+
+        if args.formatter:
+            formatter = eval(args.formatter, self.shell.user_ns, self.shell.user_ns)
+            cell = cell.options(formatter=formatter)
+
+        if args.type:
+            cell = cell.options(type=args.type)
+        else:
+            try:
+                import pandas as pd
+            except ImportError:
+                pd = None
+
+            if pd is not None and isinstance(value, pd.DataFrame):
+                type_kwargs = {}
+                if value.index.name:
+                    type_kwargs["index"] = True
+                cell = cell.options(type="dataframe", type_kwargs=type_kwargs)
+
+        # Finally set the value in Excel
+        cell.value = value
+
+    @line_magic
+    @magic_arguments()
+    @argument("-c", "--cell", help="Address of cell to get value of.")
+    @argument("-t", "--type", help="Datatype to convert the value to.")
+    @argument("-x", "--no-auto-resize", action="store_true", help="Don't auto-resize the range.")
+    def xl_get(self, line):
+        """Get the current selection in Excel into Python."""
+        argv = self._split_args(line)
+        args = self.xl_get.parser.parse_args(argv)
+        xl = xl_app(com_package="win32com")
+
+        # Get the specified range, or use the current selection
+        if args.cell:
+            selection = xl.Range(args.cell.strip("\"' "))
+        else:
+            selection = xl.Selection
+            if not selection:
+                raise Exception("Nothing selected")
+
+        # Expand the range if possible
+        if not args.no_auto_resize:
+            top_left = selection.Cells[1]
+            bottom_left = top_left.GetOffset(selection.Rows.Count-1, 0)
+            bottom_right = top_left.GetOffset(selection.Rows.Count-1, selection.Columns.Count-1)
+
+            # Check to see if there is a value below this range
+            below = bottom_left.GetOffset(1, 0)
+            if below.Value is not None:
+                new_bottom_left = bottom_left.End(self.xlDown)
+
+                # If the initial value is None then navigating down from it will only
+                # go to the next non-empty cell, not right to be bottom of the range.
+                # This happens if the top left cell is empty (above an index) and only
+                # that single cell is selected.
+                if selection.Count == 1 and bottom_left.Value is None:
+                    new_below = new_bottom_left.GetOffset(1, 0)
+                    if new_below.Value is not None:
+                        new_bottom_left = new_bottom_left.End(self.xlDown)
+
+                new_bottom_right = new_bottom_left.GetOffset(0, selection.Columns.Count-1)
+
+                # Check to see if we can expand to the right as well
+                right = new_bottom_right.GetOffset(0, 1)
+                if right.Value is not None:
+                    new_bottom_right = new_bottom_right.End(self.xlToRight)
+
+                    if new_bottom_right.Row < bottom_right.Row:
+                        new_bottom_right = new_bottom_left.GetOffset(0, selection.Columns.Count-1)
+
+                selection = xl.Range(top_left, new_bottom_right)
+
+        # Get an XLCell object from the range to make it easier to get the value
+        cell = XLCell.from_range(selection)
+
+        # If a type was passed use that
+        if args.type:
+            return cell.options(type=args.type).value
+
+        # Otherwise check to see if it looks like a pandas DataFrame
+        try_dataframe = False
+        has_index = False
+        if selection.Rows.Count > 1 and selection.Columns.Count > 1:
+            try:
+                import pandas as pd
+            except ImportError:
+                pd = None
+
+            if pd is not None:
+                # If the top left corner is empty assume the first column is an index.
+                try_dataframe = True
+                top_left = selection.Cells[1].Value
+                if top_left is None:
+                    has_index = True
+
+        # Get the value using PyXLL's dataframe converter, or as a plain value.
+        value = None
+        if try_dataframe:
+            try:
+                type_kwargs = {"index": 1 if has_index else 0}
+                value = cell.options(type="dataframe", type_kwargs=type_kwargs).value
+            except:
+                _log.warning("Error converting selection to DataFrame", exc_info=True)
+
+        if value is None:
+            value = cell.value
+
+        return value
+
+    @line_magic
+    @magic_arguments()
+    @argument("-n", "--name", help="Name of the picture object in Excel to use.")
+    @argument("-c", "--cell", help="Address of cell to use when creating the Picture in Excel.")
+    @argument("-w", "--width", type=float, help="Width in points to use when creating the Picture in Excel.")
+    @argument("-h", "--height", type=float, help="Height in points to use when creating the Picture in Excel.")
+    @argument("figure", type=str, help="Figure to plot.")
+    def xl_plot(self, line):
+        """Plot a figure to Excel in the same way as pyxll.plot.
+
+        The figure is exported as an image and inserted into Excel as a Picture object.
+
+        If the --name argument is used and the picture already exists then it will not
+        be resized or moved.
+        """
+        argv = self._split_args(line)
+        args = self.xl_plot.parser.parse_args(argv)
+        figure = eval(args.figure, self.shell.user_ns, self.shell.user_ns)
+
+        kwargs = {}
+        if args.cell is not None:
+            xl = xl_app(com_package="win32com")
+            cell = xl.Range(args.cell.strip("\"' "))
+            kwargs["top"] = cell.Top
+            kwargs["left"] = cell.Left
+
+        plot(figure,
+             name=args.name,
+             width=args.width,
+             height=args.height,
+             **kwargs)
+
+    @staticmethod
+    def _split_args(line):
+        """This is used instead of the standard arg_split to allow full Python
+        expressions to be used as arguments.
+
+        For example, %xl_plot df.plot(x="x", y="y") should be kept as a single
+        argument and not split by spaces.
+        """
+        line = line.strip()
+        if not line:
+            return []
+
+        open_close = {
+            "(": ")",
+            "{": "}",
+            "[": "]",
+            '"': '"',
+            "'": "'"
+        }
+
+        separators = {
+            " ",
+            "\t"
+        }
+
+        args = [""]
+        stack = []
+        for char in line:
+            # If we're in something already check if it's closed
+            if stack:
+                if char == open_close[stack[-1]]:
+                    stack.pop(-1)
+                    args[-1] += char
+                    continue
+
+            # Check if entering parentheses or a quoted string
+            if char in open_close:
+                stack.append(char)
+                args[-1] += char
+                continue
+
+            # If we're not in the middle of something check for a separator
+            if not stack and char in separators:
+                if args[-1]:
+                    args.append("")
+                continue
+
+            args[-1] += char
+            continue
+
+        return list(filter(None, args))
---
+++ packages/pyxll-jupyter/pyxll_jupyter-0.1.19-py3-none-any.whl/pyxll_jupyter/pyxll.py
@@ -0,0 +1,244 @@
+"""
+Entry points for PyXLL integration.
+
+These are picked up automatically when PyXLL starts to add Jupyter
+functionality to Excel as long as this package is installed.
+
+To install this package use::
+
+    pip install pyxll_jupyter
+
+"""
+from .widget import JupyterQtWidget
+from .qtimports import QApplication, QMessageBox
+from pyxll import xlcAlert, get_config, xl_app, xl_macro, schedule_call
+from functools import partial
+import ctypes.wintypes
+import pkg_resources
+import logging
+import sys
+import os
+
+_log = logging.getLogger(__name__)
+
+
+def _get_qt_app():
+    """Get or create the Qt application"""
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication([])
+    return app
+
+
+def _get_notebook_path(cfg):
+    """Return the path to open the Jupyter notebook in."""
+    # Use the path of the active workbook if use_workbook_dir is set
+    use_workbook_dir = False
+    if cfg.has_option("JUPYTER", "use_workbook_dir"):
+        try:
+            use_workbook_dir = bool(int(cfg.get("JUPYTER", "use_workbook_dir")))
+        except (ValueError, TypeError):
+            _log.error("Unexpected value for JUPYTER.use_workbook_dir.")
+
+    if use_workbook_dir:
+        xl = xl_app(com_package="win32com")
+        wb = xl.ActiveWorkbook
+        if wb is not None and wb.FullName and os.path.exists(wb.FullName):
+            return os.path.dirname(wb.FullName)
+
+    # Otherwise use the path option
+    if cfg.has_option("JUPYTER", "notebook_dir"):
+        path = cfg.get("JUPYTER", "notebook_dir").strip("\"\' ")
+        if os.path.exists(path):
+            return os.path.normpath(path)
+        _log.warning("Notebook path '%s' does not exist" % path)
+
+    # And if that's not set use My Documents
+    CSIDL_PERSONAL = 5  # My Documents
+    SHGFP_TYPE_CURRENT = 0  # Get current, not default value
+
+    buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+    ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
+    return buf.value
+
+
+def _get_jupyter_timeout(cfg):
+    """Return the timeout in seconds to use when starting Jupyter."""
+    timeout = 30.0
+    if cfg.has_option("JUPYTER", "timeout"):
+        try:
+            timeout = float(cfg.get("JUPYTER", "timeout"))
+            _log.debug("Using a timeout of %.1fs for starting the Jupyter notebook." % timeout)
+        except (ValueError, TypeError):
+            _log.error("Unexpected value for JUPYTER.timeout.")
+    return max(timeout, 1.0)
+
+
+def open_jupyter_notebook(*args, initial_path=None, notebook_path=None):
+    """Ribbon action function for opening the Jupyter notebook
+    browser control.
+
+    :param initial_path: Path to open Jupyter in.
+    :param notebook_path: Path of Jupyter notebook to open.
+    """
+    from pyxll import create_ctp
+
+    if initial_path is not None and notebook_path is not None:
+        raise RuntimeError("'initial_path' and 'notebook_path' cannot both be set.")
+
+    if notebook_path is not None:
+        if not os.path.exists(notebook_path):
+            raise RuntimeError("Notebook path '%s' not found." % notebook_path)
+        if not os.path.isfile(notebook_path):
+            raise RuntimeError("Notebook path '%s' is not a file." % notebook_path)
+        notebook_path = os.path.abspath(notebook_path)
+
+    # Create the Qt Application
+    app = _get_qt_app()
+
+    # The create the widget and add it as an Excel CTP
+    cfg = get_config()
+    timeout = _get_jupyter_timeout(cfg)
+
+    if notebook_path is None and initial_path is None:
+        initial_path = _get_notebook_path(cfg)
+    if initial_path and not os.path.exists(initial_path):
+        raise RuntimeError("Directory '%s' does not exist.")
+    if initial_path and not os.path.isdir(initial_path):
+        raise RuntimeError("Path '%s' is not a directory.")
+
+    widget = JupyterQtWidget(initial_path=initial_path,
+                             timeout=timeout,
+                             notebook_path=notebook_path)
+
+    create_ctp(widget, width=800)
+
+
+def set_selection_in_ipython(*args):
+    """Gets the value of the selected cell and copies it to
+    the globals dict in the IPython kernel.
+    """
+    from pyxll import xl_app, XLCell
+
+    try:
+        if not getattr(sys, "_ipython_app", None) or not sys._ipython_kernel_running:
+            raise Exception("IPython kernel not running")
+
+        # Get the current selected range
+        xl = xl_app(com_package="win32com")
+        selection = xl.Selection
+        if not selection:
+            raise Exception("Nothing selected")
+
+        # Check to see if it looks like a pandas DataFrame
+        try_dataframe = False
+        has_index = False
+        if selection.Rows.Count > 1 and selection.Columns.Count > 1:
+            try:
+                import pandas as pd
+            except ImportError:
+                pd = None
+                pass
+
+            if pd is not None:
+                # If the top left corner is empty assume the first column is an index.
+                try_dataframe = True
+                top_left = selection.Cells[1].Value
+                if top_left is None:
+                    has_index = True
+
+        # Get an XLCell object from the range to make it easier to get the value
+        cell = XLCell.from_range(selection)
+
+        # Get the value using PyXLL's dataframe converter, or as a plain value.
+        value = None
+        if try_dataframe:
+            try:
+                type_kwargs = {"index": 1 if has_index else 0}
+                value = cell.options(type="dataframe", type_kwargs=type_kwargs).value
+            except:
+                _log.warning("Error converting selection to DataFrame", exc_info=True)
+
+        if value is None:
+            value = cell.value
+
+        # set the value in the shell's locals
+        sys._ipython_app.shell.user_ns["_"] = value
+        print("\n\n>>> Selected value set as _")
+    except:
+        app = _get_qt_app()
+        QMessageBox.warning(None, "Error", "Error setting selection in Excel")
+        _log.error("Error setting selection in Excel", exc_info=True)
+
+
+@xl_macro
+def OpenJupyterNotebook(path=None):
+    """
+    Open a Jupyter notebook in a new task pane.
+
+    :param path: Path to Jupyter notebook file or directory.
+    :return: True on success
+    """
+    try:
+        if path is not None:
+            if not os.path.isabs(path):
+                # Try and get the absolute path relative to the active workbook
+                xl = xl_app(com_package="win32com")
+                wb = xl.ActiveWorkbook
+                if wb is not None and wb.FullName and os.path.exists(wb.FullName):
+                    abs_path = os.path.join(os.path.dirname(wb.FullName), path)
+                    if os.path.exists(abs_path):
+                        path = abs_path
+            if not os.path.exists(path):
+                raise RuntimeError(f"Path '{path}' not found.")
+
+        initial_path = None
+        notebook_path = None
+        if path is not None:
+            if os.path.isdir(path):
+                initial_path = path
+            elif os.path.isfile(path):
+                notebook_path = path
+            else:
+                raise RuntimeError(f"Something is wrong with {path}")
+
+        # Use schedule_call to actually open the notebook since if this was called
+        # from a Workbook.Open macro Excel may not yet be ready to open a CTP.
+        schedule_call(partial(open_jupyter_notebook,
+                                initial_path=initial_path,
+                                notebook_path=notebook_path))
+
+        return True
+    except Exception as e:
+        xlcAlert(f"Error opening Jupyter notebook: {e}")
+        raise
+
+
+def modules():
+    """Entry point for getting the pyxll modules.
+    Returns a list of module names."""
+    return [
+        __name__
+    ]
+
+
+def ribbon():
+    """Entry point for getting the pyxll ribbon file.
+    Returns a list of (filename, data) tuples.
+    """
+    cfg = get_config()
+
+    disable_ribbon = False
+    if cfg.has_option("JUPYTER", "disable_ribbon"):
+        try:
+            disable_ribbon = bool(int(cfg.get("JUPYTER", "disable_ribbon")))
+        except (ValueError, TypeError):
+            _log.error("Unexpected value for JUPYTER.disable_ribbon.")
+
+    if disable_ribbon:
+        return []
+
+    ribbon = pkg_resources.resource_string(__name__, "resources/ribbon.xml")
+    return [
+        (None, ribbon)
+    ]
