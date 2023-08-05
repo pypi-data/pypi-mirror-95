ea3e594773ae5464612765d00b1b2621bbd3eff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:03 +0000

    Add package LNMarketBot/0.1.8/LNMarketBot-0.1.8-py3-none-any.whl

---
+++ packages/LNMarketBot/LNMarketBot-0.1.8-py3-none-any.whl/LNMarketBot/Bot.py
@@ -0,0 +1,58 @@
+import sys
+import asyncio
+
+
+def background(f):
+    def wrapped(*args, **kwargs):
+        return asyncio.get_event_loop().run_in_executor(None, f,
+                                                        *args, **kwargs)
+
+    return wrapped
+
+
+class Bot:
+
+    def __init__(self):
+        self.strategy = []
+        self.datas = []
+        self.dataIndex = 0
+
+    def run(self):
+        priceGens = []
+        for data in self.datas:
+            priceGens.append(data.dataGenerator())
+        while True:
+            prices = []
+            try:
+                for gen in priceGens:
+                    prices.append(next(gen))
+            except StopIteration:
+                break
+            for strategy in self.strategy:
+                stratPrices = []
+                for index in strategy.dataIndex:
+                    stratPrices.append(prices[index])
+                try:
+                    strategy.broker.processData(stratPrices)
+                    strategy.execute(stratPrices)
+                except ValueError as VErr:
+                    strategy.broker.notifier.notify(str(VErr))
+                    break
+                except Exception as exception:
+                    strategy.broker.notifier.notify(str(exception))
+                    raise exception
+            sys.stdout.flush()
+        for strategy in self.strategy:
+            strategy.stop()
+
+    def addStrategy(self, strategy):
+        if strategy.broker is None:
+            raise ValueError("Add Broker to Strategy first")
+        self.strategy.append(strategy)
+        for data in strategy.datas:
+            try:
+                strategy.dataIndex.append(self.datas.index(data))
+            except ValueError:
+                self.datas.append(data)
+                strategy.dataIndex.append(self.dataIndex)
+                self.dataIndex += 1
---
+++ packages/LNMarketBot/LNMarketBot-0.1.8-py3-none-any.whl/LNMarketBot/__init__.py
@@ -0,0 +1,9 @@
+from .Broker import Broker
+from .Strategy import Strategy
+from .Bot import Bot
+from .Notifier import Notifier, addMessage
+from .Data import Data
+from .KrakenData import KrakenData
+from .CSVData import CSVData
+from .BacktestBroker import BacktestBroker
+from .LNMBroker import LNMBroker
