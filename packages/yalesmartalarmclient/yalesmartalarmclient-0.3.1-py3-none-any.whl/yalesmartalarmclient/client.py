12c0977b8500e0412a11025475f661b7d93eca06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package yalesmartalarmclient/0.3.1/yalesmartalarmclient-0.3.1-py3-none-any.whl

---
+++ packages/yalesmartalarmclient/yalesmartalarmclient-0.3.1-py3-none-any.whl/yalesmartalarmclient/client.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+"""Yale Smart Alarm client is a python client for interacting with the Yale Smart Alarm System API.
+
+See https://github.com/domwillcode/yale-smart-alarm-client for more information.
+"""
+
+import logging
+from .auth import YaleAuth
+from .lock import YaleDoorManAPI
+
+_LOGGER = logging.getLogger(__name__)
+
+YALE_STATE_ARM_FULL = "arm"
+YALE_STATE_ARM_PARTIAL = "home"
+YALE_STATE_DISARM = "disarm"
+
+YALE_LOCK_STATE_LOCKED = "locked"
+YALE_LOCK_STATE_UNLOCKED = "unlocked"
+YALE_LOCK_STATE_DOOR_OPEN = "dooropen"
+YALE_LOCK_STATE_UNKNOWN = "unknown"
+
+YALE_DOOR_CONTACT_STATE_CLOSED = "closed"
+YALE_DOOR_CONTACT_STATE_OPEN = "open"
+YALE_DOOR_CONTACT_STATE_UNKNOWN = "unknown"
+
+
+class AuthenticationError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class YaleSmartAlarmClient:
+    YALE_CODE_RESULT_SUCCESS = '000'
+
+    _ENDPOINT_GET_MODE = "/yapi/api/panel/mode/"
+    _ENDPOINT_SET_MODE = "/yapi/api/panel/mode/"
+    _ENDPOINT_DEVICES_STATUS = "/yapi/api/panel/device_status/"
+    _ENDPOINT_PANIC_BUTTON = "/api/panel/panic"
+
+    _REQUEST_PARAM_AREA = "area"
+    _REQUEST_PARAM_MODE = "mode"
+
+    _DEFAULT_REQUEST_TIMEOUT = 5
+
+    def __init__(self, username, password, area_id=1):
+        self.auth: YaleAuth = YaleAuth(username=username, password=password)
+        self.area_id = area_id
+        self.lock_api: YaleDoorManAPI = YaleDoorManAPI(auth=self.auth)
+
+    # Kept for backwards compatibility.
+    def get_locks_status(self):
+        devices = self.auth.get_authenticated(self._ENDPOINT_DEVICES_STATUS)
+        locks = {}
+        for device in devices['data']:
+            if device['type'] == "device_type.door_lock":
+                state = device['status1']
+                name = device['name']
+                lock_status_str = device['minigw_lock_status']
+                if lock_status_str != '':
+                    lock_status = int(lock_status_str, 16)
+                    closed = ((lock_status & 16) == 16)
+                    locked = ((lock_status & 1) == 1)
+                    if closed is True and locked is True:
+                        state = YALE_LOCK_STATE_LOCKED
+                    elif closed is True and locked is False:
+                        state = YALE_LOCK_STATE_UNLOCKED
+                    elif not closed:
+                        state = YALE_LOCK_STATE_DOOR_OPEN
+                elif "device_status.lock" in state:
+                    state = YALE_LOCK_STATE_LOCKED
+                elif "device_status.unlock" in state:
+                    state = YALE_LOCK_STATE_UNLOCKED
+                else:
+                    state = YALE_LOCK_STATE_UNKNOWN
+                locks[name] = state
+        return locks
+    # end keep for backwards.
+
+    def get_doors_status(self):
+        devices = self.auth.get_authenticated(self._ENDPOINT_DEVICES_STATUS)
+        doors = {}
+        for device in devices['data']:
+            if device['type'] == "device_type.door_contact":
+                state = device['status1']
+                name = device['name']
+                if "device_status.dc_close" in state:
+                    state = YALE_DOOR_CONTACT_STATE_CLOSED
+                elif "device_status.dc_open" in state:
+                    state = YALE_DOOR_CONTACT_STATE_OPEN
+                else:
+                    state = YALE_DOOR_CONTACT_STATE_UNKNOWN
+                doors[name] = state
+        return doors
+
+    def get_armed_status(self):
+        alarm_state = self.auth.get_authenticated(self._ENDPOINT_GET_MODE)
+        return alarm_state.get('data')[0].get('mode')
+
+    def set_armed_status(self, mode):
+        params = {
+            self._REQUEST_PARAM_AREA: self.area_id,
+            self._REQUEST_PARAM_MODE: mode
+        }
+
+        return self.auth.post_authenticated(self._ENDPOINT_SET_MODE, params=params)
+
+    def trigger_panic_button(self):
+        self.auth.post_authenticated(self._ENDPOINT_PANIC_BUTTON)
+
+    def arm_full(self):
+        self.set_armed_status(YALE_STATE_ARM_FULL)
+
+    def arm_partial(self):
+        self.set_armed_status(YALE_STATE_ARM_PARTIAL)
+
+    def disarm(self):
+        self.set_armed_status(YALE_STATE_DISARM)
+
+    def is_armed(self):
+        """Return True or False if the system is armed in any way"""
+        alarm_code = self.get_armed_status()
+
+        if alarm_code == YALE_STATE_ARM_FULL:
+            return True
+
+        if alarm_code == YALE_STATE_ARM_PARTIAL:
+            return True
+
+        return False
