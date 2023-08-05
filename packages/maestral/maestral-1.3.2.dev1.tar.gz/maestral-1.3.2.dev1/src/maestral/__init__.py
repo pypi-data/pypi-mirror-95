98283232b0a60612fd75b0f42fc7d9ce11363c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:05 +0000

    Add package maestral/1.3.2.dev1/maestral-1.3.2.dev1.tar.gz

---
+++ packages/maestral/maestral-1.3.2.dev1.tar.gz/maestral-1.3.2.dev1/src/maestral/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import warnings
+
+__version__ = "1.3.2.dev1"
+__author__ = "Sam Schott"
+__url__ = "https://github.com/SamSchott/maestral"
+
+
+# suppress Python 3.9 warning from rubicon-objc
+warnings.filterwarnings("ignore", module="rubicon", category=UserWarning)
