a208cf8e25bf6a323c4270e897f59f167391f0c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:21 +0000

    Add package boyer/0.0.7/boyer-0.0.7-py3-none-any.whl

---
+++ packages/boyer/boyer-0.0.7-py3-none-any.whl/boyer/_boyer.py
@@ -0,0 +1,32 @@
+import sys as _sys
+import time as _time
+
+__all__ = ['hello', 'delay_print', 'get_int']
+
+def hello(name):
+	string = f"Hello {name}, my name is Andrew Boyer"
+	print(string)
+	return string
+
+def delay_print(s, end=''):
+	for c in s:
+		_sys.stdout.write(c)
+		_sys.stdout.flush()
+		_time.sleep(0.04)
+	print(end)
+
+def get_int(prompt="Enter a number", start=0, finish=0):
+	while True:
+		try:
+			x = int(input(f"{prompt}: "))
+			return x
+		except ValueError:
+			print("Please enter an integer!")
+		if finish != 0: 
+			if start != 0:
+				if x < finish and x > start:
+					return x
+			elif x < finish:
+				return x
+			else:
+				print(f"Enter a value between {start} and {finish}")
