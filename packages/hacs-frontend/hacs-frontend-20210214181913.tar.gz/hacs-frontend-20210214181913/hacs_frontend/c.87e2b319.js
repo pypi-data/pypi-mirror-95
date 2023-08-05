e20f820ff9ca4735280f2fe988d76d9649baf628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:04 +0000

    Add package hacs-frontend/20210214181913/hacs-frontend-20210214181913.tar.gz

---
+++ packages/hacs-frontend/hacs-frontend-20210214181913.tar.gz/hacs-frontend-20210214181913/hacs_frontend/c.87e2b319.js
@@ -0,0 +1,59 @@
+import{P as e,o as t}from"./e.03a03bfd.js";import{P as o}from"./c.d6a20677.js";e({_template:t`
+    <style include="paper-item-shared-styles"></style>
+    <style>
+      :host {
+        @apply --layout-horizontal;
+        @apply --layout-center;
+        @apply --paper-font-subhead;
+
+        @apply --paper-item;
+        @apply --paper-icon-item;
+      }
+
+      .content-icon {
+        @apply --layout-horizontal;
+        @apply --layout-center;
+
+        width: var(--paper-item-icon-width, 56px);
+        @apply --paper-item-icon;
+      }
+    </style>
+
+    <div id="contentIcon" class="content-icon">
+      <slot name="item-icon"></slot>
+    </div>
+    <slot></slot>
+`,is:"paper-icon-item",behaviors:[o]}),e({_template:t`
+    <style>
+      :host {
+        overflow: hidden; /* needed for text-overflow: ellipsis to work on ff */
+        @apply --layout-vertical;
+        @apply --layout-center-justified;
+        @apply --layout-flex;
+      }
+
+      :host([two-line]) {
+        min-height: var(--paper-item-body-two-line-min-height, 72px);
+      }
+
+      :host([three-line]) {
+        min-height: var(--paper-item-body-three-line-min-height, 88px);
+      }
+
+      :host > ::slotted(*) {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+
+      :host > ::slotted([secondary]) {
+        @apply --paper-font-body1;
+
+        color: var(--paper-item-body-secondary-color, var(--secondary-text-color));
+
+        @apply --paper-item-body-secondary;
+      }
+    </style>
+
+    <slot></slot>
+`,is:"paper-item-body"});
---
+++ packages/hacs-frontend/hacs-frontend-20210214181913.tar.gz/hacs-frontend-20210214181913/hacs_frontend/c.c33059b8.js
@@ -0,0 +1,1 @@
+const o="20210214181913";export{o as v};
