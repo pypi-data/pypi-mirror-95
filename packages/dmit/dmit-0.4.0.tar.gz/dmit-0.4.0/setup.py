76a10c8405a8ebd1294bf7cac257cbd06917bc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:02 +0000

    Add package dmit/0.4.0/dmit-0.4.0.tar.gz

---
+++ packages/dmit/dmit-0.4.0.tar.gz/dmit-0.4.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='dmit',
+     version='0.4.0',
+     author="Kasper Hintz",
+     author_email="kah@dmi.dk",
+     description="A Met Tool package",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     packages=setuptools.find_packages(),
+     setup_requires=['wheel'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
