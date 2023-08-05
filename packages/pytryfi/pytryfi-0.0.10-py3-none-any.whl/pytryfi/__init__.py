7e25ede9dd3e9b484603187ad3d8beb5379e8443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:10 +0000

    Add package pytryfi/0.0.10/pytryfi-0.0.10-py3-none-any.whl

---
+++ packages/pytryfi/pytryfi-0.0.10-py3-none-any.whl/pytryfi/__init__.py
@@ -0,0 +1,172 @@
+import logging
+import requests
+
+from pytryfi.const import (API_HOST_URL_BASE, API_LOGIN, API_GRAPHQL)
+from pytryfi.fiUser import FiUser
+from pytryfi.fiPet import FiPet
+from pytryfi.fiBase import FiBase
+from pytryfi.common import query
+
+
+LOGGER = logging.getLogger(__name__)
+
+class PyTryFi(object):
+    """base object for TryFi"""
+
+    def __init__(self, username=None, password=None):
+
+        self._api_host = API_HOST_URL_BASE
+        self._session = requests.Session()
+        self._user_agent = "pyTryFi"
+        self._username = username
+        self._password = password
+        self.login()
+
+        self._currentUser = FiUser(self._userId)
+        self._currentUser.setUserDetails(self._session)
+        petListJSON = query.getPetList(self._session)
+        self._pets = []
+        for pet in petListJSON:
+            p = FiPet(pet['id'])
+            p.setPetDetailsJSON(pet)
+            #get the current location and set it
+            pLocJSON = query.getCurrentPetLocation(self._session,p._petId)
+            p.setCurrentLocation(pLocJSON)
+            #get the daily, weekly and monthly stats and set
+            pStatsJSON = query.getCurrentPetStats(self._session,p._petId)
+            p.setStats(pStatsJSON['dailyStat'],pStatsJSON['weeklyStat'],pStatsJSON['monthlyStat'])
+            LOGGER.debug(f"Adding Pet: {p._name} with Device: {p._device._deviceId}")
+            self._pets.append(p)
+        
+        self._bases = []
+        baseListJSON = query.getBaseList(self._session)
+        for base in baseListJSON:
+            b = FiBase(base['baseId'])
+            b.setBaseDetailsJSON(base)
+            LOGGER.debug(f"Adding Base: {b._name} Online: {b._online}")
+            self._bases.append(b)
+
+    def __str__(self):
+        instString = f"Username: {self.username}"
+        userString = f"{self.currentUser}"
+        baseString = ""
+        petString = ""
+        for b in self.bases:
+            baseString = baseString + f"{b}"
+        for p in self.pets:
+            petString = petString + f"{p}"
+        return f"TryFi Instance - {instString}\n Pets in Home:\n {petString}\n Bases In Home:\n {baseString}"
+        
+    #refresh pet details for all pets
+    def updatePets(self):
+        petListJSON = query.getPetList(self._session)
+        updatedPets = []
+        for pet in petListJSON:
+            p = FiPet(pet['id'])
+            p.setPetDetailsJSON(pet)
+            #get the current location and set it
+            pLocJSON = query.getCurrentPetLocation(self._session,p._petId)
+            p.setCurrentLocation(pLocJSON)
+            #get the daily, weekly and monthly stats and set
+            pStatsJSON = query.getCurrentPetStats(self._session,p._petId)
+            p.setStats(pStatsJSON['dailyStat'],pStatsJSON['weeklyStat'],pStatsJSON['monthlyStat'])
+            LOGGER.debug(f"Adding Pet: {p._name} with Device: {p._device._deviceId}")
+            updatedPets.append(p)
+        self._pets = updatedPets
+
+    def updatePetObject(self, petObj):
+        petId = petObj.petId
+        count = 0
+        for p in self.pets:
+            if p.petId == petId:
+                self._pets.pop(count)
+                self._pets.append(petObj)
+                LOGGER.debug(f"Updating Existing Pet: {petId}")
+                break
+            count = count + 1
+
+    # return the pet object based on petId
+    def getPet(self, petId):
+        for p in self.pets:
+            if petId == p.petId:
+                return p
+        LOGGER.error(f"Cannot find Pet: {petId}")
+        return None
+    
+    #refresh base details
+    def updateBases(self):
+        updatedBases = []
+        baseListJSON = query.getBaseList(self._session)
+        for base in baseListJSON:
+            b = FiBase(base['baseId'])
+            b.setBaseDetailsJSON(base)
+            updatedBases.append(b)
+        self._bases = updatedBases
+
+    # return the pet object based on petId
+    def getBase(self, baseId):
+        for b in self.bases:
+            if baseId == b.baseId:
+                return b
+        LOGGER.error(f"Cannot find Base: {baseId}")
+        return None
+
+    def update(self):
+        self.updateBases()
+        self.updatePets()
+
+    @property
+    def currentUser(self):
+        return self._currentUser
+    @property
+    def pets(self):
+        return self._pets
+    @property
+    def bases(self):
+        return self._bases
+    @property
+    def username(self):
+        return self._username
+    @property
+    def session(self):
+        return self._session
+    @property
+    def cookies(self):
+        return self._cookies
+    @property
+    def userID(self):
+        return self._userID
+    @property
+    def session(self):
+        return self._session
+
+    # login to the api and get a session
+    def login(self):
+        url = API_HOST_URL_BASE + API_LOGIN
+        params={
+                'email' : self._username,
+                'password' : self._password,
+            }
+        
+        LOGGER.debug(f"Logging into TryFi")
+        try:
+            response = self._session.post(url, data=params)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            LOGGER.error(f"Cannot login, error: ({e})")
+            raise requests.RequestException(e)
+        error = None
+        try:
+            error = response.json()['error']
+        except:
+            error = None
+        if error or not response.ok:
+            errorMsg = error['message']
+            LOGGER.error(f"Cannot login, response: ({response.status_code}): {errorMsg} ")
+            raise Exception("TryFiLoginError")
+        #storing cookies but don't need them. Handled by session mgmt
+        self._cookies = response.cookies
+        #store unique userId from login for future use
+        self._userId = response.json()['userId']
+        self._sessionId = response.json()['sessionId']
+        LOGGER.debug(f"Successfully logged in. UserId: {self._userId}")
---
+++ packages/pytryfi/pytryfi-0.0.10-py3-none-any.whl/pytryfi/fiPet.py
@@ -0,0 +1,297 @@
+import logging
+from pytryfi.fiDevice import FiDevice
+from pytryfi.common import query
+from pytryfi.exceptions import *
+from pytryfi.const import PET_ACTIVITY_ONGOINGWALK
+import datetime
+
+LOGGER = logging.getLogger(__name__)
+
+class FiPet(object):
+    def __init__(self, petId):
+        self._petId = petId
+
+    def setPetDetailsJSON(self, petJSON):
+        try:
+            self._name = petJSON['name']
+            self._homeCityState = petJSON['homeCityState']
+            self._yearOfBirth = int(petJSON['yearOfBirth'])
+            self._monthOfBirth = int(petJSON['monthOfBirth'])
+            self._dayOfBirth = int(petJSON['dayOfBirth'])
+            self._gender = petJSON['gender']
+            #weight is in kg
+            self._weight = float(petJSON['weight'])
+            self._breed = petJSON['breed']['name']
+            #track last updated
+            self._lastUpdated = datetime.datetime.now()
+        except TryFiError as e:
+            LOGGER.error(f"Unable to set values for Pet.\nException: {e}\nwhile parsing {petJSON}")
+            raise TryFiError("Unable to set Pet Details")
+        try:
+            self._photoLink = petJSON['photos']['first']['image']['fullSize']
+        except:
+            LOGGER.warning(f"Cannot find photo of your pet. Defaulting to empty string.")
+            self._photoLink = ""
+
+        self._device = FiDevice(petJSON['device']['id'])
+        self._device.setDeviceDetailsJSON(petJSON['device'])
+
+    def __str__(self):
+        return f"Last Updated - {self.lastUpdated} - Pet ID: {self.petId} Name: {self.name} Is Lost: {self.isLost} From: {self.homeCityState} ActivityType: {self.activityType} Located: {self.currLatitude},{self.currLongitude} Last Updated: {self.currStartTime}\n \
+            using Device/Collar: {self._device}"
+    
+    # set the Pet's current location details
+    def setCurrentLocation(self, activityJSON):
+        activityType = activityJSON['__typename']
+        self._activityType = activityType
+        self._areaName = activityJSON['areaName']
+        try:
+            if activityType == PET_ACTIVITY_ONGOINGWALK:
+                positionSize = len(activityJSON['positions'])
+                self._currLongitude = float(activityJSON['positions'][positionSize-1]['position']['longitude'])
+                self._currLatitude = float(activityJSON['positions'][positionSize-1]['position']['latitude'])
+                self._currStartTime = datetime.datetime.fromisoformat(activityJSON['start'].replace('Z', '+00:00'))            
+            else:          
+                self._currLongitude = float(activityJSON['position']['longitude'])
+                self._currLatitude = float(activityJSON['position']['latitude'])
+                self._currStartTime = datetime.datetime.fromisoformat(activityJSON['start'].replace('Z', '+00:00'))
+            try:
+                self._currPlaceName = activityJSON['place']['name']
+                self._currPlaceAddress = activityJSON['place']['address']
+            except:
+                LOGGER.warning("Could not set place, defaulting to Unknown")
+                self._currPlaceName = "UNKNOWN"
+                self._currPlaceAddress = "UNKNOWN"
+            self._lastUpdated = datetime.datetime.now()
+        except TryFiError as e:
+            LOGGER.error(f"Unable to set values Current Location for Pet {self.name}.\nException: {e}\nwhile parsing {activityJSON}")
+            raise TryFiError("Unable to set Pet Location Details")
+
+    # set the Pet's current steps, goals and distance details for daily, weekly and monthly
+    def setStats(self, activityJSONDaily, activityJSONWeekly, activityJSONMonthly):
+        try:
+            #distance is in metres
+            self._dailyGoal = int(activityJSONDaily['stepGoal'])
+            self._dailySteps = int(activityJSONDaily['totalSteps'])
+            self._dailyTotalDistance = float(activityJSONDaily['totalDistance'])
+        except TryFiError as e:
+            LOGGER.error(f"Unable to set values Daily Stats for Pet {self.name}.\nException: {e}\nwhile parsing {activityJSONDaily}")
+            raise TryFiError("Unable to set Pet Daily Stats")
+        try:
+            self._weeklyGoal = int(activityJSONWeekly['stepGoal'])
+            self._weeklySteps = int(activityJSONWeekly['totalSteps'])
+            self._weeklyTotalDistance = float(activityJSONWeekly['totalDistance'])
+        except TryFiError as e:
+            LOGGER.error(f"Unable to set values Weekly Stats for Pet {self.name}.\nException: {e}\nwhile parsing {activityJSONWeekly}")
+            raise TryFiError("Unable to set Pet Weekly Stats")
+        try:
+            self._monthlyGoal = int(activityJSONMonthly['stepGoal'])
+            self._monthlySteps = int(activityJSONMonthly['totalSteps'])
+            self._monthlyTotalDistance = float(activityJSONMonthly['totalDistance'])
+        except TryFiError as e:
+            LOGGER.error(f"Unable to set values Monthly Stats for Pet {self.name}.\nException: {e}\nwhile parsing {activityJSONMonthly}")
+            raise TryFiError("Unable to set Pet Monthly Stats")
+
+        self._lastUpdated = datetime.datetime.now()
+
+
+    # Update the Stats of the pet
+    def updateStats(self, sessionId):
+        try:
+            pStatsJSON = query.getCurrentPetStats(sessionId,self.petId)
+            self.setStats(pStatsJSON['dailyStat'],pStatsJSON['weeklyStat'],pStatsJSON['monthlyStat'])
+            return True
+        except Exception as e:
+            LOGGER.error(f"Could not update stats for Pet {self.name}.\n{e}")
+
+    # Update the Pet's GPS location
+    def updatePetLocation(self, sessionId):
+        try:
+            pLocJSON = query.getCurrentPetLocation(sessionId,self.petId)
+            self.setCurrentLocation(pLocJSON)
+            return True
+        except Exception as e:
+            LOGGER.error(f"Could not update Pet: {self.name}'s location.\n{e}")
+            return False
+    
+    # Update the device/collar details for this pet
+    def updateDeviceDetails(self, sessionId):
+        try:
+            deviceJSON = query.getDevicedetails(sessionId, self.petId)
+            self.device.setDeviceDetailsJSON(deviceJSON['device'])
+            return True
+        except Exception as e:
+            LOGGER.error(f"Could not update Device/Collar information for Pet: {self.name}\n{e}")
+            return False
+
+    # Update all details regarding this pet
+    def updateAllDetails(self, sessionId):
+        self.updateDeviceDetails(sessionId)
+        self.updatePetLocation(sessionId)
+        self.updateStats(sessionId)
+
+    # set the color code of the led light on the pet collar
+    def setLedColorCode(self, sessionId, colorCode):
+        try:
+            moduleId = self.device.moduleId
+            ledColorCode = int(colorCode)
+            setColorJSON = query.setLedColor(sessionId, moduleId, ledColorCode)
+            try:
+                self.device.setDeviceDetailsJSON(setColorJSON['setDeviceLed'])
+            except Exception as e:
+                LOGGER.warning(f"Updated LED Color but could not get current status for Pet: {self.name}\nException: {e}")
+            return True
+        except Exception as e:
+            LOGGER.error(f"Could not complete Led Color request:\n{e}")
+            return False
+    
+    # turn on or off the led light. action = True will enable the light, false turns off the light
+    def turnOnOffLed(self, sessionId, action):
+        try:
+            moduleId = self.device.moduleId
+            onOffResponse = query.turnOnOffLed(sessionId, moduleId, action)
+            try:
+                self.device.setDeviceDetailsJSON(onOffResponse['updateDeviceOperationParams'])
+            except Exception as e:
+                LOGGER.warning(f"Action: {action} was successful however unable to get current status for Pet: {self.name}")
+            return True
+        except Exception as e:
+            LOGGER.error(f"Could not complete LED request:\n{e}")
+            return False
+
+    # set the lost dog mode to Normal or Lost Dog. Action is true for lost dog and false for normal (not lost)
+    def setLostDogMode(self, sessionId, action):
+        try:
+            moduleId = self.device.moduleId
+            petModeResponse = query.setLostDogMode(sessionId, moduleId, action)
+            try:
+                self.device.setDeviceDetailsJSON(petModeResponse['updateDeviceOperationParams'])
+            except Exception as e:
+                LOGGER.warning(f"Action: {action} was successful however unable to get current status for Pet: {self.name}")
+            return True
+        except Exception as e:
+            LOGGER.error(f"Could not complete Lost Dog Mode request:\n{e}")
+            LOGGER.error(f"Could not complete turn on/off light where ledEnable is {action}.\nException: {e}")
+            return False
+
+    @property
+    def device(self):
+        return self._device
+    @property
+    def petId(self):
+        return self._petId
+    @property
+    def name(self):
+        return self._name
+    @property
+    def homeCityState(self):
+        return self._homeCityState
+    @property
+    def yearOfBirth(self):
+        return self._yearOfBirth
+    @property
+    def monthOfBirth(self):
+        return self._monthOfBirth
+    @property
+    def dayOfBirth(self):
+        return self._dayOfBirth
+    @property
+    def gender(self):
+        return self._gender
+    @property
+    def weight(self):
+        return self._weight
+    @property
+    def breed(self):
+        return self._breed
+    @property
+    def photoLink(self):
+        return self._photoLink
+    @property
+    def currLongitude(self):
+        return self._currLongitude
+    @property
+    def currLatitude(self):
+        return self._currLatitude
+    @property
+    def currStartTime(self):
+        return self._currStartTime
+    @property
+    def currPlaceName(self):
+        return self._currPlaceName
+    @property
+    def currPlaceAddress(self):
+        return self._currPlaceAddress
+    @property
+    def currPlaceAddress(self):
+        return self._currPlaceAddress
+    @property
+    def dailyGoal(self):
+        return self._dailyGoal
+    @property
+    def dailySteps(self):
+        return self._dailySteps
+    @property
+    def dailyTotalDistance(self):
+        return self._dailyTotalDistance
+    @property
+    def weeklyGoal(self):
+        return self._weeklyGoal
+    @property
+    def weeklySteps(self):
+        return self._weeklySteps
+    @property
+    def weeklyTotalDistance(self):
+        return self._weeklyTotalDistance
+    @property
+    def monthlyGoal(self):
+        return self._monthlyGoal
+    @property
+    def monthlySteps(self):
+        return self._monthlySteps
+    @property
+    def monthlyTotalDistance(self):
+        return self._monthlyTotalDistance
+    @property
+    def lastUpdated(self):
+        return self._lastUpdated
+    @property
+    def isLost(self):
+        return self.device.isLost
+    @property
+    def activityType(self):
+        return self._activityType
+    @property
+    def areaName(self):
+        return self._areaName
+    
+    def getBirthDate(self):
+        return datetime.datetime(self.yearOfBirth, self.monthOfBirth, self.dayOfBirth)
+    
+    def getDailySteps(self):
+        return self.dailySteps
+    
+    def getDailyGoal(self):
+        return self.dailyGoal
+
+    def getDailyDistance(self):
+        return self.dailyTotalDistance
+
+    def getWeeklySteps(self):
+        return self.weeklySteps
+    
+    def getWeeklyGoal(self):
+        return self.weeklyGoal
+
+    def getWeeklyDistance(self):
+        return self.weeklyTotalDistance
+
+    def getMonthlySteps(self):
+        return self.monthlySteps
+    
+    def getMonthlyGoal(self):
+        return self.monthlyGoal
+
+    def getMonthlyDistance(self):
+        return self.monthlyTotalDistance
+        
