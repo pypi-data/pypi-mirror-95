bbff6005678bc3ef62b5825421ca67ee2b416b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:54 +0000

    Add package ssht00ls/2.0.4/ssht00ls-2.0.4.tar.gz

---
+++ packages/ssht00ls/ssht00ls-2.0.4.tar.gz/ssht00ls-2.0.4/ssht00ls/__main__.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+import os, sys, syst3m ; sys.path.insert(1, syst3m.defaults.get_source_path(__file__, back=2))
+from ssht00ls.classes.config import *
+import ssht00ls
+
+# the cli object class.
+class CLI(cl1.CLI):
+	def __init__(self):
+		
+		# defaults.
+		cl1.CLI.__init__(self,
+			modes={
+				"--create-alias":"Create a ssh alias.",
+				"    --server myserver":"Specify the server's name.",
+				"    --username myuser":"Specify the username.",
+				"    --ip 0.0.0.0":"Specify the server's ip.",
+				"    --port 22":"Specify the server's port.",
+				"    for ssh keys:":"",
+				"    --key /path/to/key/private_key":"Specify the path to the private key.",
+				"    --passphrase 'MyPassphrase123'":"Specify the keys pasphrase (optional).",
+				"    for smart cards:":"",
+				"    --smart-cards":"Enable the smart cards boolean.",
+				"    --pin 123456":"Specify the smart cards pin code (optional).",
+				"--generate":"Generate a ssh key.",
+				"    --path /keys/mykey/":"Specify the keys directory path.",
+				"    --passphrase Passphrase123":"Specify the keys passphrase.",
+				"    --comment 'My Key'":"Specify the keys comment.",
+				"--command <alias> 'ls .'":"Execute a command over ssh.",
+				"--session <alias>":"Start a ssh session.",
+				"    --options '' ":"Specify additional ssh options (optional).",
+				"--pull <path> <alias>:<remote>":"Pull a file / directory.",
+				"    --delete":"Also update the deleted files (optional).",
+				"    --safe":"Enable version control.",
+				"    --forced":"Enable forced mode.",
+				"--push <alias>:<remote> <path>":"Push a file / directory.",
+				"    --delete":"Also update the deleted files (optional).",
+				"    --safe":"Enable version control.",
+				"    --forced":"Enable forced mode.",
+				"--mount <alias>:<remote> <path>":"Mount a remote directory.",
+				"--unmount <path>":"Unmount a mounted remote directory.",
+				"    --sudo":"Root permission required.",
+				"    --forced":"Enable forced mode.",
+				"--index <path> / <alias>:<remote>":"Index the specified path / alias:remote.",
+				"--start-agent":"Start the ssht00ls agent.",
+				"--stop-agent":"Stop the ssht00ls agent.",
+				"--start-daemon <alias>:<remote> <path>":"Start a ssync daemon.",
+				"--stop-daemon <path>":"Stop a ssync daemon.",
+				"--kill <identifier>":"Kill all ssh processes that include the identifier.",
+				"--config":"Edit the ssht00ls configuration file (nano).",
+				"-h / --help":"Show the documentation.",
+			},
+			options={
+				"-j / --json":"Print the response in json format.",
+			},
+			notes={
+				"SSHT00LS_CONFIG":"Specify the $SSHT00LS_CONFIG environment variable to use a different ssht00ls config file.",
+			},
+			alias=ALIAS,
+			executable=__file__,
+		)
+
+		#
+	def start(self):
+
+		# options.
+		json = self.arguments_present(["-j", "--json"])
+		
+		# help.
+		if self.arguments_present(['-h', '--help']):
+			print(self.documentation)
+
+		# check create ssh config.
+		elif self.argument_present('--create-alias'):
+			
+			# create an ssh alias for the key.
+			if not self.argument_present('--smart-card'):
+				key = self.get_argument('--key')
+				response = ssht00ls.aliases.create( 
+					# the alias.
+					alias=self.get_argument('--alias'), 
+					# the username.
+					username=self.get_argument('--username'), 
+					# the public ip of the server.
+					public_ip=self.get_argument('--public-ip'),
+					# the public port of the server.
+					public_port=self.get_argument('--public-port'),
+					# the private ip of the server.
+					private_ip=self.get_argument('--private-ip'),
+					# the private port of the server.
+					private_port=self.get_argument('--private-port'),
+					# the path to the private key.
+					key=key,
+					passphrase=getpass.getpass("Enter the passphrase of key [{key}]:"),
+					# smart card.
+					smart_card=False,)
+
+			# create an ssh alias for a smart card.
+			else:
+				response = ssht00ls.aliases.create( 
+					# the alias.
+					alias=self.get_argument('--alias'), 
+					# the username.
+					username=self.get_argument('--username'), 
+					# the public ip of the server.
+					public_ip=self.get_argument('--public-ip'),
+					# the public port of the server.
+					public_port=self.get_argument('--public-port'),
+					# the private ip of the server.
+					private_ip=self.get_argument('--private-ip'),
+					# the private port of the server.
+					private_port=self.get_argument('--private-port'),
+					# the path to the private key.
+					key=smart_card.path,
+					# smart card.
+					smart_card=True,
+					pin=self.get_argument('--pin', required=False, default=None), )
+
+			# log to console.
+			r3sponse.log(response=response, json=json)
+
+		# check create ssh config.
+		elif self.argument_present('--generate'):
+			
+			# generate a key.
+			passphrase = self.get_passphrase(required=False)
+			if passphrase in ["", None, "null", "None", "none"]: passphrase = None
+			response = ssht00ls.key.generate(
+				path=self.get_argument("--path"), 
+				passphrase=passphrase, 
+				comment=self.get_argument("--comment"),)
+			r3sponse.log(response=response, json=json)
+
+		# kill ssh processes.
+		elif self.argument_present('--kill'):
+			response = ssht00ls.utils.kill_ssh(
+				identifier=self.get_argument("--kill"), 
+				sudo=self.argument_present("--sudo"),)
+			r3sponse.log(response=response, json=json)
+
+		# pull.
+		elif self.argument_present('--pull'):
+			remote = self.get_argument("--pull", index=1)
+			path = self.get_argument("--pull", index=2)
+			if ":" not in remote:
+				r3sponse.log(error=f"Invalid <alias>:<remote> <path> format.", json=json)
+				self.stop()
+			alias,remote = remote.split(":")
+			exclude = None
+			if self.argument_present("--exclude"): 
+				exclude = self.get_argument("--exclude").split(",")
+			elif self.argument_present("--no-exclude"): exclude = []
+			response = ssht00ls.ssync.pull(
+				alias=alias, 
+				remote=remote, 
+				path=path,
+				exclude=exclude, 
+				forced=self.argument_present("--forced"), 
+				delete=self.argument_present("--delete"), 
+				safe=self.argument_present("--safe"), )
+			r3sponse.log(response=response, json=json)
+
+		# push.
+		elif self.argument_present('--push'):
+			path = self.get_argument("--push", index=1)
+			remote = self.get_argument("--push", index=2)
+			if ":" not in remote:
+				r3sponse.log(error=f"Invalid <alias>:<remote> <path>.", json=json)
+				self.stop()
+			alias,remote = remote.split(":")
+			exclude = None
+			if self.argument_present("--exclude"): 
+				exclude = self.get_argument("--exclude").split(",")
+			elif self.argument_present("--no-exclude"): exclude = []
+			response = ssht00ls.ssync.push(
+				alias=alias, 
+				remote=remote, 
+				path=path,
+				exclude=exclude, 
+				forced=self.argument_present("--forced"), 
+				delete=self.argument_present("--delete"), 
+				safe=self.argument_present("--safe"), )
+			r3sponse.log(response=response, json=json)
+
+		# mount.
+		elif self.argument_present('--mount'):
+			remote = self.get_argument("--mount", index=1)
+			path = self.get_argument("--mount", index=2)
+			if ":" not in remote:
+				r3sponse.log(error=f"Invalid <alias>:<remote> <path>.", json=json)
+				self.stop()
+			alias,remote = remote.split(":")
+			response = ssht00ls.ssync.mount(
+				alias=alias, 
+				remote=remote, 
+				path=path,
+				forced=self.argument_present("--forced"), )
+			r3sponse.log(response=response, json=json)
+
+		# unmount.
+		elif self.argument_present('--unmount'):
+			path = self.get_argument("--unmount", index=1)
+			response = ssht00ls.ssync.unmount(
+				path=path,
+				forced=self.argument_present("--forced"), 
+				sudo=self.argument_present("--sudo"), )
+			r3sponse.log(response=response, json=json)
+
+		# index.
+		elif self.argument_present('--index'):
+			index = self.get_argument("--index")
+			if ":" in index:
+				alias,remote = index.split(":")
+				response = ssht00ls.ssync.index(path=remote, alias=alias)
+			else:
+				response = ssht00ls.ssync.index(path=index)
+			r3sponse.log(response=response, json=json)
+
+		# start daemon.
+		elif self.argument_present('--start-daemon'):
+			remote = self.get_argument("--start-daemon", index=1)
+			path = self.get_argument("--start-daemon", index=2)
+			if ":" not in remote:
+				r3sponse.log(error=f"Invalid <alias>:<remote> <path>.", json=json)
+				self.stop()
+			alias,remote = remote.split(":")
+			response = ssht00ls.ssync.daemon(alias=alias, remote=remote, path=path)
+			r3sponse.log(response=response, json=json)
+
+		# stop daemon.
+		elif self.argument_present('--stop-daemon'):
+			path = self.get_argument("--stop-daemon", index=1)
+			response = ssht00ls.ssync.stop_daemon(path)
+			r3sponse.log(response=response, json=json)
+
+		# config.
+		elif self.argument_present('--config'):
+			if json:
+				print(CONFIG.dictionary)
+			else:
+				os.system(f"nano {CONFIG.file_path.path}")
+
+		# invalid.
+		else: self.invalid()
+
+		#
+	def invalid(self):
+		if json:
+			r3sponse.log(error="Selected an invalid mode.", json=json)
+		else:
+			print(self.documentation)
+			print("Selected an invalid mode.")
+		#
+	def stop(self, success=False):
+		if success: sys.exit(1)
+		else:sys.exit(1)
+	def get_passphrase(self, required=True):
+		return self.get_argument("--passphrase", required=required).replace("\\", "").replace("\ ", "")
+# main.
+if __name__ == "__main__":
+	cli = CLI()
+	cli.start()
---
+++ packages/ssht00ls/ssht00ls-2.0.4.tar.gz/ssht00ls-2.0.4/ssht00ls/classes/ssync/__init__.py
@@ -0,0 +1,377 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from ssht00ls.classes.config import *
+from ssht00ls.classes import utils
+from ssht00ls.classes.aliases import aliases
+from ssht00ls.classes.agent import agent
+
+# the ssync object class.
+class SSync(object):
+	def __init__(self):
+		self.exclude = ['__pycache__', '.DS_Store']
+		aliases.sync()
+		self.sync()
+	def mount(self, 
+		# the local path.
+		path=None, 
+		# the remote path.
+		remote=None, 
+		# ssh alias.
+		alias=None,
+		# forced.
+		forced=False,
+		# exclude.
+		exclude=['__pycache__', '.DS_Store'],
+		# accept new host verification keys.
+		accept_new_host_keys=True,
+		# log level.
+		log_level=0,
+	):
+
+		# checks.
+		response = r3sponse.check_parameters({
+			"path:str":path,
+			"remote:str":remote,
+			"alias:str":alias,
+			"exclude:list":exclude,
+			"forced:bool":forced,
+			"accept_new_host_keys:bool":accept_new_host_keys, })
+		if not response.success: return response
+		elif os.path.exists(path):
+			return r3sponse.error_response(f"Path [{path}] already exists.")
+		elif "*mounted*" in str(cache.get(id=path, group="daemons")):
+			return r3sponse.error_response(f"Path [{path}] is already mounted.")
+		path = utils.clean_path(path)
+		remote = utils.clean_path(remote)
+
+		# pull.
+		response = self.pull(
+			path=path, 
+			alias=alias, 
+			remote=remote, 
+			exclude=exclude,
+			forced=forced,
+			delete=True,
+			safe=False,
+			log_level=log_level,)
+		if not r3sponse.success: return response
+
+		# start daemon.
+		p = subprocess.Popen(["python3", SOURCE_PATH, "--daemon", f"{alias}:{remote}", path, "--non-interactive"])
+
+		# handler.
+		return r3sponse.success_response(f"Successfully mounted [{alias}:{remote}] ==> [{path}].")
+
+		#
+	def unmount(self, 
+		# the local path.
+		path=None, 
+		# forced required.
+		forced=False,
+		# sudo required.
+		sudo=False,
+		# log level.
+		log_level=0,
+	):
+
+		# checks.
+		response = r3sponse.check_parameters({
+			"path:str":path,
+			"forced:bool":forced,
+			"sudo:bool":sudo, })
+		if not response.success: return response
+		elif not os.path.exists(path):
+			return r3sponse.error_response(f"Path [{path}] does not exist.")
+		elif not os.path.isdir(path):
+			return r3sponse.error_response(f"Path [{path}] is not a directory.")
+		elif "*mounted*" not in cache.get(id=path, group="daemons"):
+			return r3sponse.error_response(f"Path [{path}] is not mounted.")
+		path = utils.clean_path(path)
+
+		# wait for daemon stop.
+		response = self.stop_daemon(path=path)
+		if not success: return response
+
+		# delete local.
+		"""
+		if sudo: sudo_str = "sudo "
+		else: sudo_str = ""
+		os.system(f"{sudo_str}rm -fr {path}")
+		if os.path.exists(path):
+			return r3sponse.error_response(f"Failed to unmount [{path}].")
+		"""
+
+		# handler.
+		return r3sponse.success_response(f"Successfully unmounted [{path}].")
+
+		#
+	def index(self, path=None, alias=None, log_level=0, accept_new_host_keys=True):
+
+		# checks.
+		if path == None:
+			return r3sponse.error_response(f"Define parameter: path.")
+		path = utils.clean_path(path)
+
+		# remote.
+		if alias != None:
+			
+			# check alias.
+			response = aliases.check(alias)
+			if not response.success: return response
+
+			# check passphrase.
+			if CONFIG["aliases"][alias]["smart_card"] in [True, "true", "True"]:
+				response = ENCRYPTION.decrypt(CONFIG["aliases"][alias]["passphrase"])
+			else:
+				response = ENCRYPTION.decrypt(CONFIG["aliases"][alias]["passphrase"])
+			if not response.success: return response
+			passphrase = response.decrypted.decode()
+			
+			# tests.
+			response = agent.add(path=CONFIG["aliases"][alias]["private_key"], passphrase=passphrase)
+			if not response["success"]: return response
+			response = utils.test_ssht00ls(alias=alias, accept_new_host_keys=accept_new_host_keys)
+			if not response.success: return response
+			response = utils.test_ssh_path(alias=alias, path=path, accept_new_host_keys=accept_new_host_keys)
+			if not response.success: return response
+
+			# index.
+			output = syst3m.utils.__execute_script__(f"printf 'yes' | ssh {alias} ' python3 /usr/local/lib/ssht00ls --index {path} --non-interactive --json ' ")
+			try: response = json.loads(output)
+			except Exception as e: 
+				return r3sponse.error_response(f"Failed to serialize remote {alias} output: {output}")
+			return r3sponse.ResponseObject(response)
+
+		# local.
+		else:
+			if not os.path.exists(path):
+				return r3sponse.error_response(f"Path [{path}] does not exist.")
+			elif not os.path.isdir(path):
+				return r3sponse.error_response(f"Path [{path}] is not a directory.")
+
+			# index.
+			index = {}
+			for path in Files.Directory(path=path).paths(recursive=True):
+				index[path] = Formats.FilePath(path).mtime(format="seconds")
+
+			# handler.
+			return r3sponse.success_response(f"Successfully indexed {len(index)} files from directory [{path}].", {
+				"index":index,
+			})
+
+			#
+	# pull & push.
+	def pull(self,
+		# the local path.
+		path=None, 
+		# the ssht00ls alias.
+		alias=None, 
+		# the remote path.
+		remote=None, 
+		# exlude subpaths (list) (leave None to use default).
+		exclude=None,
+		# update deleted files.
+		delete=False,
+		# forced mode.
+		forced=False,
+		# version control.
+		safe=False,
+		# accept new hosts keys.
+		accept_new_host_keys=True,
+		# log level.
+		log_level=0,
+	):	
+		# check alias.
+		path = utils.clean_path(path)
+		remote = utils.clean_path(remote)
+		response = aliases.check(alias)
+		if not response.success: return response
+		
+		# check passphrase.
+		if CONFIG["aliases"][alias]["smart_card"] in [True, "true", "True"]:
+			response = ENCRYPTION.decrypt(CONFIG["aliases"][alias]["passphrase"])
+		else:
+			response = ENCRYPTION.decrypt(CONFIG["aliases"][alias]["passphrase"])
+		if not response.success: return response
+		passphrase = response.decrypted.decode()
+		
+		# tests.
+		response = agent.add(path=CONFIG["aliases"][alias]["private_key"], passphrase=passphrase)
+		if not response["success"]: return response
+		response = utils.test_ssht00ls(alias=alias, accept_new_host_keys=accept_new_host_keys)
+		if not response.success: return response
+		response = utils.test_ssh_path(alias=alias, path=remote, accept_new_host_keys=accept_new_host_keys)
+		if not response.success: return response
+
+		# pull.
+		if exclude == None: exclude = self.exclude
+		exclude_str = ""
+		for i in exclude: exclude_str += f"--exclude {i} "
+		delete_str = Formats.Boolean(delete).convert(true="--delete", false="")
+		command = f"rsync -azP {alias}:{remote} {path} {exclude_str}{delete_str}"
+		loader = syst3m.console.Loader(f"Pulling [{alias}:{remote}] to [{path}]")
+		output = syst3m.utils.__execute_script__(command)
+		if len(output) > 0 and output[len(output)-1] == "\n": output = output[:-1]
+		cache.set(id=path, data="*mounted*", group="daemons")
+		
+		# handler.
+		if "rsync: " in output or "rsync error: " in output:
+			loader.stop(success=False)
+			print(output)
+			return r3sponse.success_response(f"Failed to pull [{alias}:{remote}] to [{path}].")
+		else:
+			loader.stop()
+			return r3sponse.success_response(f"Successfully pulled [{alias}:{remote}] to [{path}].")
+
+		#
+	def push(self,
+		# the local path.
+		path=None, 
+		# the ssht00ls alias.
+		alias=None, 
+		# the remote path.
+		remote=None, 
+		# exlude subpaths (list) (leave None to use default).
+		exclude=None,
+		# update deleted files.
+		delete=False,
+		# forced mode.
+		forced=False,
+		# version control.
+		safe=False,
+		# accept new hosts keys.
+		accept_new_host_keys=True,
+		# log level.
+		log_level=0,
+	):
+		# check alias.
+		path = utils.clean_path(path)
+		remote = utils.clean_path(remote)
+		response = aliases.check(alias)
+		if not response.success: return response
+		
+		# check passphrase.
+		if CONFIG["aliases"][alias]["smart_card"] in [True, "true", "True"]:
+			response = ENCRYPTION.decrypt(CONFIG["aliases"][alias]["passphrase"])
+		else:
+			response = ENCRYPTION.decrypt(CONFIG["aliases"][alias]["passphrase"])
+		if not response.success: return response
+		passphrase = response.decrypted.decode()
+		
+		# tests.
+		response = agent.add(path=CONFIG["aliases"][alias]["private_key"], passphrase=passphrase)
+		if not response["success"]: return response
+		response = utils.test_ssht00ls(alias=alias, accept_new_host_keys=accept_new_host_keys)
+		if not response.success: return response
+		response = utils.test_ssh_path(alias=alias, path=Formats.FilePath(remote).base(), accept_new_host_keys=accept_new_host_keys)
+		if not response.success: return response
+
+		# push.
+		if exclude == None: exclude = self.exclude
+		exclude_str = ""
+		for i in exclude: exclude_str += f"--exclude {i} "
+		delete_str = Formats.Boolean(delete).convert(true="--delete", false="")
+		command = f"rsync -azP {path} {alias}:{remote} {exclude_str}{delete_str}"
+		loader = syst3m.console.Loader(f"Pushing [{path}] to [{alias}:{remote}].")
+		output = syst3m.utils.__execute_script__(command)
+		if len(output) > 0 and output[len(output)-1] == "\n": output = output[:-1]
+		cache.set(id=path, data="*mounted*", group="daemons")
+		
+		# handler.
+		if "rsync: " in output or "rsync error: " in output:
+			loader.stop(success=False)
+			print(output)
+			return r3sponse.success_response(f"Failed to push [{path}] to [{alias}:{remote}].")
+		else:
+			loader.stop()
+			return r3sponse.success_response(f"Successfully pushed [{path}] to [{alias}:{remote}].")
+
+
+		#
+	# the mounted daemon.
+	def sync(self):
+		for path in Files.Directory(path=f"{cache.path}/daemons/").paths(recursive=False):
+			a=1
+	def daemon(self, 
+		# the ssh alias.
+		alias=None, 
+		# the remote path.
+		remote=None, 
+		# thel local path.
+		path=None, 
+		# settings.
+		start=True,
+	):
+		path = utils.clean_path(path)
+		remote = utils.clean_path(remote)
+		daemon = self.Daemon({
+			"alias":alias,
+			"remote":remote,
+			"path":path,
+			"ssync":self,
+		})
+		if start: daemon.start()
+		return daemon
+	def stop_daemon(self, path, timeout=60, sleeptime=1):
+		cache.set(id=path, data="*stop*", group="daemons")
+		stopped = False
+		for i in range(timeout/sleeptime):
+			status_ = cache.get(id=path, group="daemons")
+			if "*stopped*" in status_ or "*crashed*" in status_:
+				stopped = True
+				break
+			time.sleep(sleeptime)
+		if stopped:
+			return r3sponse.success_response(f"Successfully stopped ssht00ls daemon [{path}].")
+		else:
+			return r3sponse.error_response(f"Failed to stop ssht00ls daemon [{path}].")
+	class Daemon(syst3m.objects.Thread):
+		def __init__(self, attributes={}):
+			syst3m.objects.Thread.__init__(self)
+			self.assign(attributes)
+			self.path = utils.clean_path(self.path)
+			self.id = f"{self.alias}:{self.remote} ==> {self.path}"
+		def run(self):
+
+			# checks.
+			if not os.path.exists(self.path):
+				cache.set(id=self.path, data="*crashed*", group="daemons")
+				raise ValueError(f"SSHT00ls daemon ({self.id}): Path [{self.path}] does not exist.")
+			elif not os.path.isdir(self.path):
+				cache.set(id=self.path, data="*crashed*", group="daemons")
+				raise ValueError(f"SSHT00ls daemon ({self.id}): Path [{self.path}] is not a directory.")
+			elif "*mounted*" not in cache.get(id=path, group="daemons"):
+				cache.set(id=self.path, data="*crashed*", group="daemons")
+				raise ValueError(f"SSHT00ls daemon ({self.id}): Path [{self.path}] is not mounted.")
+
+			# check alias.
+			response = aliases.check(alias)
+			if not response.success: 
+				cache.set(id=self.path, data="*crashed*", group="daemons")
+				raise ValueError(response.error)
+
+			# start.
+			while True:
+
+				# check stop command.
+				if "*stop*" in cache.get(id=self.path, group="daemons"):
+					break
+
+				# if file no longer exists stop daemon.
+				if not os.path.exists(self.path):
+					cache.set(id=self.path, data="*crashed*", group="daemons")
+					raise ValueError(f"SSHT00ls daemon ({self.id}): Mounted directory [{self.path}] from [{self.alias}:{self.remote}] no longer exists.")
+
+			# stop.
+			cache.set(id=self.path, data="*stopped*", group="daemons")
+
+# initialized objects.
+ssync = SSync()
+
+#print(ssync.mount(
+#	alias="dev.vandenberghinc.com",
+#	remote="/tmp/testmount",
+#	path="/tmp/testmount",))
+#quit()
