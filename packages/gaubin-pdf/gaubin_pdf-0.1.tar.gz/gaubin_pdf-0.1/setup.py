29a6371485ee18643e941cd4934c6673589bc007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:38 +0000

    Add package gaubin-pdf/0.1/gaubin_pdf-0.1.tar.gz

---
+++ packages/gaubin-pdf/gaubin_pdf-0.1.tar.gz/gaubin_pdf-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaubin_pdf',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gaubin_pdf'],
+      author = 'Paima Marbun',
+      author_email = 'something@email.com',
+      zip_safe=False)
