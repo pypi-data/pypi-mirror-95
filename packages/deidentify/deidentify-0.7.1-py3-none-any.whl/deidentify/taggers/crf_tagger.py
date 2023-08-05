b7b549e9be6a6f689c361f18a6825f9a933b466a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:29 +0000

    Add package deidentify/0.7.1/deidentify-0.7.1-py3-none-any.whl

---
+++ packages/deidentify/deidentify-0.7.1-py3-none-any.whl/deidentify/taggers/crf_tagger.py
@@ -0,0 +1,48 @@
+import pickle
+from typing import List
+
+from loguru import logger
+
+from deidentify.base import Document
+from deidentify.methods import tagging_utils
+from deidentify.methods.crf import crf_labeler, crf_util
+from deidentify.taggers.base import TextTagger, lookup_model
+from deidentify.tokenizer import Tokenizer
+
+
+class CRFTagger(TextTagger):
+
+    def __init__(self, model, tokenizer: Tokenizer, verbose=False):
+        self.tokenizer = tokenizer
+        self.feature_extractor, self.meta_sentence_filter = crf_util.FEATURE_EXTRACTOR['liu_2015']
+        self.verbose = verbose
+
+        model_file = lookup_model(model)
+        logger.info('Load sklearn-crfsuite model from {}'.format(model_file))
+        with open(model_file, 'rb') as clf_file:
+            self.tagger = pickle.load(clf_file)
+        logger.info('Finish loading crf model.')
+
+    def annotate(self, documents: List[Document]) -> List[Document]:
+        sents, parsed_docs = tagging_utils.standoff_to_sents(
+            docs=documents,
+            tokenizer=self.tokenizer,
+            verbose=self.verbose
+        )
+
+        X_features, _ = crf_labeler.sents_to_features_and_labels(sents, self.feature_extractor)
+        y_pred = self.tagger.predict(X_features, verbose=self.verbose)
+        annotated_docs = tagging_utils.sents_to_standoff(y_pred, parsed_docs)
+        return annotated_docs
+
+    @property
+    def tags(self):
+        bio_tag_names = self.tagger.classes_
+        bio_tag_names.remove('O')
+
+        targets = set()
+        for target in bio_tag_names:
+            name = target.split('-', maxsplit=1)[1]
+            targets.add(name)
+
+        return list(targets)
---
+++ packages/deidentify/deidentify-0.7.1-py3-none-any.whl/deidentify/taggers/flair_tagger.py
@@ -0,0 +1,48 @@
+from typing import List
+
+from flair.models import SequenceTagger
+from loguru import logger
+
+from deidentify.base import Document
+from deidentify.methods.bilstmcrf import flair_utils
+from deidentify.taggers.base import TextTagger, lookup_model
+from deidentify.tokenizer import Tokenizer
+
+
+class FlairTagger(TextTagger):
+
+    def __init__(self, model, tokenizer: Tokenizer, mini_batch_size=256, verbose=False):
+        self.tokenizer = tokenizer
+        self.mini_batch_size = mini_batch_size
+        self.verbose = verbose
+
+        model_file = lookup_model(model)
+        logger.info('Load flair model from {}'.format(model_file))
+        self.tagger = SequenceTagger.load(model_file)
+        logger.info('Finish loading flair model.')
+
+    def annotate(self, documents: List[Document]) -> List[Document]:
+        flair_sents, parsed_docs = flair_utils.standoff_to_flair_sents(
+            docs=documents,
+            tokenizer=self.tokenizer,
+            verbose=self.verbose
+        )
+
+        self.tagger.predict(flair_sents, mini_batch_size=self.mini_batch_size, verbose=self.verbose)
+
+        annotated_docs = flair_utils.flair_sents_to_standoff(flair_sents, parsed_docs)
+        return annotated_docs
+
+    @property
+    def tags(self):
+        bio_tag_names = self.tagger.tag_dictionary.get_items()
+        bio_tag_names.remove('<unk>')
+        bio_tag_names.remove('<START>')
+        bio_tag_names.remove('<STOP>')
+        bio_tag_names.remove('O')
+
+        tags = set()
+        for bio_tag in bio_tag_names:
+            name = bio_tag.split('-', maxsplit=1)[1]
+            tags.add(name)
+        return list(tags)
