78912e096838a4ba4067d0b2812d88223446c5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:44 +0000

    Add package xltpl/0.9.1/xltpl-0.9.1.tar.gz

---
+++ packages/xltpl/xltpl-0.9.1.tar.gz/xltpl-0.9.1/setup.py
@@ -0,0 +1,24 @@
+import os
+from io import open
+from setuptools import setup
+
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+README = os.path.join(CUR_DIR, "README.md")
+with open(README, 'r', encoding='utf-8') as fd:
+    long_description = fd.read()
+
+setup(
+    name = 'xltpl',
+    version = "0.9.1",
+    author = 'Zhang Yu',
+    author_email = 'zhangyu836@gmail.com',
+    url = 'https://github.com/zhangyu836/python-xls-xlsx-template',
+    packages = ['xltpl'],
+	install_requires = ['xlrd >= 1.2.0', 'xlwt >= 1.3.0', 'openpyxl >= 2.6.0', 'jinja2', 'six'],
+    description = ( 'A python module to generate xls/x files from a xls/x template' ),
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    platforms = ["Any platform "],
+    license = 'MIT',
+    keywords = ['Excel', 'xls', 'xlsx', 'spreadsheet', 'workbook', 'template']
+)
