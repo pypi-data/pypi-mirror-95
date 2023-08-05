6d3fd994b2f7992fda5b4fdc4df360563a6e30f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:52 +0000

    Add package django-sockpuppet/0.5.1/django-sockpuppet-0.5.1.tar.gz

---
+++ packages/django-sockpuppet/django-sockpuppet-0.5.1.tar.gz/django-sockpuppet-0.5.1/sockpuppet/js/sockpuppet.js
@@ -0,0 +1,11 @@
+import { Application } from 'stimulus'
+import StimulusReflex from 'stimulus_reflex'
+// because travis had issues with 'sockpuppet-js' we had to do this.
+import WebsocketConsumer from '../../javascript/stimulus-websocket/index'
+
+const application = Application.start()
+const consumer = new WebsocketConsumer(
+  `ws://${window.location.host}/ws/sockpuppet-sync`, {debug: false}
+)
+
+StimulusReflex.initialize(application, { consumer })
