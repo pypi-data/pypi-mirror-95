b8c1748db6f9e272c04f80c3c2da189deb920598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:03 +0000

    Add package odinwolf/1.1.6/odinwolf-1.1.6-py3-none-any.whl

---
+++ packages/odinwolf/odinwolf-1.1.6-py3-none-any.whl/py_pkg/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'odinwolf'
+__description__ = f'A wrapper for the {__title__} package.'
+__version__ = '1.1.6'
+__author__ = 'janus banus'
+__author_email__ = 'janus.banus@gmail.com'
+__license__ = 'Apache 2.0'
+__url__ = 'https://youtube.com/'
