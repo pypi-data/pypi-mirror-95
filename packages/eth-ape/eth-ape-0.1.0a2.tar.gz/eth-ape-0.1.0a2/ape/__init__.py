d48b94bd1fde99f8d1b99c08f5aad7be21d0414e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:31 +0000

    Add package eth-ape/0.1.0a2/eth-ape-0.1.0a2.tar.gz

---
+++ packages/eth-ape/eth-ape-0.1.0a2.tar.gz/eth-ape-0.1.0a2/ape/__init__.py
@@ -0,0 +1,9 @@
+from importlib.metadata import version, PackageNotFoundError
+
+from . import _setup  # NOTE: Do this before anything else
+
+try:
+    __version__ = version("ape")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "<unknown>"
