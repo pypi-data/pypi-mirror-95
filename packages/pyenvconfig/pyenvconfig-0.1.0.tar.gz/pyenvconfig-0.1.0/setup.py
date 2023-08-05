e04d819ef9bad5ef90e2ddf9ff61e2b517387542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:17 +0000

    Add package pyenvconfig/0.1.0/pyenvconfig-0.1.0.tar.gz

---
+++ packages/pyenvconfig/pyenvconfig-0.1.0.tar.gz/pyenvconfig-0.1.0/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+import unittest
+
+import envconfig
+
+
+def test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('tests/', pattern='test_*.py')
+    return test_suite
+
+
+setup(
+    name='pyenvconfig',
+    version=envconfig.__version__,
+    description='Simple environment variable configuration for Python',
+    author='Makram Kamaleddine',
+    url='https://github.com/makramkd/envconfig',
+    keywords='environment-variables env-var configuration',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(),
+    test_suite='setup.test_suite',
+    tests_require=[
+        'coverage',
+        'flake8',
+    ],
+)
