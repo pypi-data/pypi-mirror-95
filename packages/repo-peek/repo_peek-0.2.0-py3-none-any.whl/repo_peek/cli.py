153d1393b9ba0b805a998353f5501fca7ee01e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:02 +0000

    Add package repo-peek/0.2.0/repo_peek-0.2.0-py3-none-any.whl

---
+++ packages/repo-peek/repo_peek-0.2.0-py3-none-any.whl/repo_peek/cli.py
@@ -0,0 +1,28 @@
+"""cli to github peek."""
+
+import typer
+
+from repo_peek.fetch import main
+from . import __version__
+
+app = typer.Typer()
+
+
+@app.command()
+def peek(repo: str):
+    try:
+        typer.secho(f"opening repo: {repo}...", fg=typer.colors.GREEN)
+        main(repo)
+    except Exception:
+       typer.echo(f"failed to peek {repo}")
+       raise typer.Abort()
+
+
+@app.command()
+def info():
+    typer.secho("a tool to peek into a github repo.")
+    typer.secho("version: {}".format(__version__))
+
+
+if __name__ == "__main__":
+    app()
