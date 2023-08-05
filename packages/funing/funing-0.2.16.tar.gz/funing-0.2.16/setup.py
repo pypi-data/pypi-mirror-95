9ded40a61860f7cd625ce80939cd7193c02be9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:27 +0000

    Add package funing/0.2.16/funing-0.2.16.tar.gz

---
+++ packages/funing/funing-0.2.16.tar.gz/funing-0.2.16/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description=fh.read()
+
+setuptools.setup(
+    name="funing",
+    version= '0.2.16',
+    author="larryw3i",
+    author_email="larryw3i@163.com",
+    description="A face recognition gui",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/larryw3i/Funing",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'funing=funing:run',
+        ]
+    },
+    python_requires='>=3.6',
+    install_requires=[
+        'dlib',
+        'face-recognition',
+        'opencv-python',
+        'language_data',
+        'langcodes',
+        'pony',
+        'pyyaml',
+    ],
+    include_package_data = True,
+)
