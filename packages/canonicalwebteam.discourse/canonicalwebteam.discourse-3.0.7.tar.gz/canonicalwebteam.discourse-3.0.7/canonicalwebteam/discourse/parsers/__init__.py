53911286a3de2cab34cfc99545fecd11c67460b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:47 +0000

    Add package canonicalwebteam.discourse/3.0.7/canonicalwebteam.discourse-3.0.7.tar.gz

---
+++ packages/canonicalwebteam.discourse/canonicalwebteam.discourse-3.0.7.tar.gz/canonicalwebteam.discourse-3.0.7/canonicalwebteam/discourse/parsers/__init__.py
@@ -0,0 +1,9 @@
+from canonicalwebteam.discourse.parsers.docs import (  # noqa
+    DocParser,  # noqa
+)
+from canonicalwebteam.discourse.parsers.engage import (  # noqa
+    EngageParser,  # noqa
+)
+from canonicalwebteam.discourse.parsers.tutorials import (  # noqa
+    TutorialParser,  # noqa
+)
