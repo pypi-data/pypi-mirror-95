83c266b31151ef7efdd436611f95ca63e3f9c29c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:35 +0000

    Add package givemestuff/0.1/givemestuff-0.1.tar.gz

---
+++ packages/givemestuff/givemestuff-0.1.tar.gz/givemestuff-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='givemestuff',
+    version='0.1',
+    author="Zhe Wang",
+    author_email="wangzhetju@gmail.com",
+    description="Giveme info",
+    py_modules=['giveme'],
+    install_requires=[
+        'Click',
+        'requires'
+    ],
+    entry_points='''
+        [console_scripts]
+        giveme=main:cli
+    ''',
+)
