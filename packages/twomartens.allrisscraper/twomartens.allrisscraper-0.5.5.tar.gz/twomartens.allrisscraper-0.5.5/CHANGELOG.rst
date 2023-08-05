a3266036d681062167e013fee7d6af8ec9643855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:25 +0000

    Add package twomartens.allrisscraper/0.5.5/twomartens.allrisscraper-0.5.5.tar.gz

---
+++ packages/twomartens.allrisscraper/twomartens.allrisscraper-0.5.5.tar.gz/twomartens.allrisscraper-0.5.5/CHANGELOG.rst
@@ -0,0 +1,59 @@
+Changelog
+=========
+
+This changelog contains a list of versions with their respective high-level changes.
+
+v0.5.5:
+    - fixed missing len of pattern
+v0.5.4:
+    - match video conference starts case insensitive
+v0.5.3:
+    - fixed typo && added additional video conference start string
+v0.5.2:
+    - fixed video conferences
+v0.5.1:
+    - fixed login procedure
+v0.5.0:
+    - added support for not public video conferences
+v0.4.1:
+    - specify window size for internal scraping
+v0.4.0:
+    - added public scraping support
+    - added geckodriver path for internal scraping
+v0.3.7:
+    - changed login link
+v0.3.6:
+    - specified firefox binary to prevent start up issues
+v0.3.5:
+    - covered case where not all types of documents are available
+    - added ort.txt in document output, contains the location of the session
+v0.3.4:
+    - fixed some typos
+    - fixed not working badges
+v0.3.3:
+    - added changelog
+    - converted README to reStructuredText
+v0.3.2:
+    document links now work if more than three documents are available
+v0.3.1:
+    - fixed URL for invited sessions
+    - fixed problems with headless mode for GUI-free systems
+v0.3.0:
+    added support for multiple districts
+v0.2.2:
+    - added support for official abbreviations of committees
+    - detects now all invited sessions, not only every other
+v0.2.1:
+    - finally fixed regional committees
+v0.2.0:
+    - fixed regional committees part one
+v0.1.4:
+    unified versions between README and setup.py
+v0.1.3:
+    corrected link to Git repository
+v0.1.2:
+    added nice badges
+v0.1.1:
+    added main module
+v0.1.0:
+    - initial release
---
+++ packages/twomartens.allrisscraper/twomartens.allrisscraper-0.5.5.tar.gz/twomartens.allrisscraper-0.5.5/setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright 2020 Jim Martens
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""setup.py for allris scraper"""
+
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.rst", "rb") as f:
+    long_desc = f.read().decode()
+
+setup(
+    name="twomartens.allrisscraper",
+    description="Scraper for ALLRIS",
+    long_description=long_desc,
+    long_description_content_type="text/x-rst; charset=UTF-8",
+    author="Jim Martens",
+    author_email="github@2martens.de",
+    url="https://git.2martens.de/2martens/allris-scraper",
+    version="0.5.5",
+    namespace_packages=["twomartens"],
+    packages=find_packages('src', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_dir={'': 'src'},
+    package_data={},
+    entry_points={
+        "console_scripts": ["tm-allrisscraper = twomartens.allrisscraper.main:main"]
+    },
+    python_requires="~=3.7",
+    install_requires=["selenium"],
+    license="Apache License 2.0",
+    include_package_data=True,
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Environment :: Console",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
