8339c714d5a661a872fdf7c4c911e327a6fb1e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:50 +0000

    Add package tion-btle/1.2.1a2/tion_btle-1.2.1a2.tar.gz

---
+++ packages/tion-btle/tion_btle-1.2.1a2.tar.gz/tion_btle-1.2.1a2/setup.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='tion_btle',
+    version='1.2.1a2',
+    long_description="Module for working with Tion breezers",
+    url='https://github.com/TionAPI/tion_python/tree/dev',
+    install_requires=['bluepy==1.3.0'],
+    description='Python module for interacting with Tion breezers',
+    packages=find_packages(),
+)
