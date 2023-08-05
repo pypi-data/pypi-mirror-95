d1ef63b7b68d408c48cf91e1ea0adba88a77b340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:51 +0000

    Add package clibato/0.0.8/clibato-0.0.8.tar.gz

---
+++ packages/clibato/clibato-0.0.8.tar.gz/clibato-0.0.8/clibato/__init__.py
@@ -0,0 +1,6 @@
+from .error import *
+from .logger import Logger
+from .destination import Destination, Directory, Repository
+from .content import Content
+from .config import Config
+from .clibato import Clibato
