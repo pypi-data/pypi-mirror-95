bc0508355f113ee00c4a3bab12e949257f6f9f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:03 +0000

    Add package maestral-qt/1.4.2/maestral-qt-1.4.2.tar.gz

---
+++ packages/maestral-qt/maestral-qt-1.4.2.tar.gz/maestral-qt-1.4.2/maestral_qt/__init__.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 16:23:13 2018
+
+@author: samschott
+"""
+import os
+
+__author__ = "Sam Schott"
+__version__ = "1.4.2"
+__url__ = "https://github.com/SamSchott/maestral"
+
+# add '~/.local/share' to XDG_DATA_DIRS
+# this is needed to find icons installed for the current user only
+XDG_DATA_DIRS = os.environ.get("XDG_DATA_DIRS", "")
+XDG_DATA_DIRS += ":" + os.path.expanduser("~/.local/share")
+os.environ["XDG_DATA_DIRS"] = XDG_DATA_DIRS.strip(":")
