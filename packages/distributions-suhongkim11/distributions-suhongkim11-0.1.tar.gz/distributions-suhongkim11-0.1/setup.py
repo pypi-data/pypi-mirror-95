275328fe6ca10a06be11afbcb7da65e693ac51f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:47 +0000

    Add package distributions-suhongkim11/0.1/distributions-suhongkim11-0.1.tar.gz

---
+++ packages/distributions-suhongkim11/distributions-suhongkim11-0.1.tar.gz/distributions-suhongkim11-0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+        name='distributions-suhongkim11',
+        version='0.1',
+        author='Suhong Kim', 
+        author_email='suhongkim11@gmail.com', 
+        description='Gaussian distributions',
+        packages=setuptools.find_packages(), 
+        classifiers=[
+            'Programming Language :: Python :: 3', 
+            'License :: OSI Approved :: MIT License', 
+        ],
+        python_requires='>=3.6', 
+        zip_safe=False)
