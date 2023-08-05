3256b3d46448054bee09ac8834668672ce88d105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:27 +0000

    Add package khtube/0.9.26/khtube-0.9.26.tar.gz

---
+++ packages/khtube/khtube-0.9.26.tar.gz/khtube-0.9.26/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="khtube", # Replace with your own username
+    version="0.9.26",
+    author="Kodershub",
+    author_email="furqan.ali9500@gmail.com",
+    description="A youtube video downloader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KodersHub/khtube.git",
+    packages=setuptools.find_packages(),
+    install_requires=["youtube_dl", "googledrivedownloader"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
