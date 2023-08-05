6ecf86368225ccb71389fa9c11ecd2b1df2b4a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:35 +0000

    Add package scintillant/0.0.5/scintillant-0.0.5-py3-none-any.whl

---
+++ packages/scintillant/scintillant-0.0.5-py3-none-any.whl/scintillant/__init__.py
@@ -0,0 +1,11 @@
+"""
+Scintillant -a tool for quickly creating skills adapted to work with the
+Dialog service - the central system of the Lilia chat bot.
+"""
+
+GITHUB = "https://github.com"
+BOTTLE_SKILL_TEMPLATE_URL = (
+        GITHUB + "/PaperDevil/bottle-skill-template/archive/develop.zip"
+)
+AIOHTTP_SKILL_TEMPLATE_URL = ""
+FAST_API_SKILL_TEMPLATE_URL = ""
