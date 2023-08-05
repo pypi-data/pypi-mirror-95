36223e8d58d66c17f5acf3fd93df85c134a99143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:30 +0000

    Add package helloTestSecond/0.0.4/helloTestSecond-0.0.4.tar.gz

---
+++ packages/helloTestSecond/helloTestSecond-0.0.4.tar.gz/helloTestSecond-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.4'
+DESCRIPTION = 'A basic hello package'
+
+# Setting up
+setup(
+    name="helloTestSecond",
+    version=VERSION,
+    author="Sven Redlich",
+    author_email="sven.redlich96@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
