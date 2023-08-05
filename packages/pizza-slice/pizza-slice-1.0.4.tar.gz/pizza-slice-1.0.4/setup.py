3716e3a24642cfe0a949daee4587157ced6d797d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:27 +0000

    Add package pizza-slice/1.0.4/pizza-slice-1.0.4.tar.gz

---
+++ packages/pizza-slice/pizza-slice-1.0.4.tar.gz/pizza-slice-1.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="pizza-slice",
+    version="1.0.4",
+    description="A Python package to get weather reports for any location.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lkakada/Prime-Number",
+    author="Kakada Ly",
+    author_email="ly103399@gmail.com",
+    license="MIT",
+    packages=find_packages(where='src'),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "pizza-slice=pizza_slice.run:main",
+        ]
+    },
+)
