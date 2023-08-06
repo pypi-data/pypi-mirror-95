359d65d2cd44f6d2d1a0870c9f848f9840172c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:44 +0000

    Add package musamusa/0.0.1.dev1/musamusa-0.0.1.dev1.tar.gz

---
+++ packages/musamusa/musamusa-0.0.1.dev1.tar.gz/musamusa-0.0.1.dev1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['musamusa']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['rich>=9.11.1,<10.0.0']
+
+setup_kwargs = {
+    'name': 'musamusa',
+    'version': '0.0.1.dev1',
+    'description': 'Tool to display annotated texts.',
+    'long_description': None,
+    'author': 'suizokukan',
+    'author_email': 'suizokukan@orange.fr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
