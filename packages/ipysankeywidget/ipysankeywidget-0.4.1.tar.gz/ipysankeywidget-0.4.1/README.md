0f3878c0d33d8d30f5dc9e472a6167a82340a49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:28 +0000

    Add package ipysankeywidget/0.4.1/ipysankeywidget-0.4.1.tar.gz

---
+++ packages/ipysankeywidget/ipysankeywidget-0.4.1.tar.gz/ipysankeywidget-0.4.1/README.md
@@ -0,0 +1,82 @@
+# IPython Sankey diagram widget [![Binder](https://mybinder.org/badge.svg)](https://mybinder.org/v2/gh/ricklupton/ipysankeywidget/master?urlpath=lab/tree/examples/Simple%20example.ipynb) [![Conda Version](https://img.shields.io/conda/vn/conda-forge/ipysankeywidget.svg)](https://anaconda.org/conda-forge/ipysankeywidget) 
+
+Display Sankey diagrams in IPython / Jupyter notebook using [d3-sankey-diagram](https://github.com/ricklupton/d3-sankey-diagram).
+
+Installation
+------------
+
+Option 1: to install using **pip**:
+
+    $ pip install ipysankeywidget
+
+Then, if using the classic Jupyter Notebook:
+
+    $ jupyter nbextension enable --py --sys-prefix ipysankeywidget
+    $ jupyter nbextension enable --py --sys-prefix widgetsnbextension   # <-- you might have already done this,
+                                                                        #     but it's safe to do again
+Or, if using Jupyterlab:
+
+    $ jupyter labextension install jupyter-sankey-widget @jupyter-widgets/jupyterlab-manager
+
+Option 2: to install using **conda**:
+
+    $ conda install -c conda-forge ipysankeywidget                # (that's it, nbextensions enabled automatically)
+
+Browser support
+---------------
+
+Tested in Firefox and Chrome. [There have been reports](https://github.com/ricklupton/ipysankeywidget/issues/2) that it's not working in Safari.
+    
+Documentation
+-------------
+
+See the
+[d3-sankey-diagram API docs](https://github.com/ricklupton/d3-sankey-diagram/wiki):
+the `value` of the `SankeyWidget` is exactly the same as the `sankey` object
+described there.
+
+To change the figure size, use the ipywidgets Layout object:
+
+```python
+layout = Layout(width="1000", height="1200")
+SankeyWidget(links=data, layout=layout)
+```
+
+Examples
+--------
+
+See notebooks in examples folder for usage examples. You can try these online
+using
+[Binder](https://mybinder.org/v2/gh/ricklupton/ipysankeywidget/master?urlpath=lab/tree/examples/Simple%20example.ipynb),
+or follow the links to static versions on nbviewer:
+- [Simple example](http://nbviewer.jupyter.org/github/ricklupton/ipysankeywidget/blob/master/examples/Simple%20example.ipynb)
+- [Advanced examples](http://nbviewer.jupyter.org/github/ricklupton/ipysankeywidget/blob/master/examples/More%20examples.ipynb)
+- [Linking and Layout](http://nbviewer.jupyter.org/github/ricklupton/ipysankeywidget/blob/master/examples/Linking%20and%20Layout.ipynb)
+- [Exporting Images](http://nbviewer.jupyter.org/github/ricklupton/ipysankeywidget/blob/master/examples/Exporting%20Images.ipynb)
+
+Changelog
+-----------
+
+## v0.3.0
+
+- Now supports Jupyterlab
+
+## v0.2.5
+
+- You can now show link values as SVG text elements. Set `linkLabelFormat` to a [d3-format string](https://github.com/d3/d3-format#locale_format); links whose value is more than `linkLabelMinWidth` will have a label using that format.
+- Upgrade to d3-sankey-diagram version 0.7.3
+- ipysankeywidget can now be installed using conda (thanks to Ali Alsabbah #33)
+- Add a check for duplicate links (thanks to Remi Bois #23)
+
+## v0.2.4
+
+- Add events for clicking on nodes and links: `on_node_clicked` and
+  `on_link_clicked`. These replace the `on_selected` event.
+
+Contributors
+------------
+
+- Nicholas Bollweg
+- Ali Alsabbah
+- Remi Bois
+- Miguel Mendez
---
+++ packages/ipysankeywidget/ipysankeywidget-0.4.1.tar.gz/ipysankeywidget-0.4.1/ipysankeywidget/sankey_widget.py
@@ -0,0 +1,202 @@
+import warnings
+import base64
+
+import ipywidgets as widgets
+from traitlets import (
+    Float,
+    Dict,
+    List,
+    Bool,
+    observe,
+    Unicode,
+)
+
+
+@widgets.register
+class SankeyWidget(widgets.DOMWidget):
+    """Sankey widget"""
+    _view_name = Unicode('SankeyView').tag(sync=True)
+    _model_name = Unicode('SankeyModel').tag(sync=True)
+    _view_module = Unicode('jupyter-sankey-widget').tag(sync=True)
+    _model_module = Unicode('jupyter-sankey-widget').tag(sync=True)
+    _view_module_version = Unicode('^0.4.1').tag(sync=True)
+    _model_module_version = Unicode('^0.4.1').tag(sync=True)
+
+    # Data
+    links = List([]).tag(sync=True)
+    nodes = List([]).tag(sync=True)
+
+    # Layout options
+    order = List(None, allow_none=True).tag(sync=True)
+    groups = List([]).tag(sync=True)
+    rank_sets = List([]).tag(sync=True)
+    align_link_types = Bool(False).tag(sync=True)
+
+    # Rendering options
+    scale = Float(None, allow_none=True).tag(sync=True)
+    margins = Dict({}).tag(sync=True)
+
+    # Link label options
+    linkLabelFormat = Unicode('').tag(sync=True)
+    linkLabelMinWidth = Float(5).tag(sync=True)
+    show_link_info_html = Bool(False).tag(sync=True)
+
+    # Get raster and vector image data back
+    png = Unicode('').tag(sync=True)
+    svg = Unicode('').tag(sync=True)
+
+    def __init__(self, **kwargs):
+        """Constructor"""
+
+        # check for duplicates in links
+        values = set()
+        for i, link in enumerate(kwargs.get('links', [])):
+            linksource = link['source']
+            linktarget = link['target']
+            if 'type' in link:
+                linktype = link['type']
+            else:
+                linktype = None
+            if (linksource, linktarget, linktype) not in values:
+                values.add((linksource, linktarget, linktype))
+            else:
+                raise ValueError('Links has at least one duplicated entry. '
+                                 'The first duplicated item appears at index {0}. '
+                                 'See https://github.com/ricklupton/ipysankeywidget/issues/22'.format(i))
+
+        # Automatically create nodes
+        nodes = kwargs.get('nodes', [])
+        node_ids = {node['id'] for node in nodes}
+        missing_ids = set()
+        for link in kwargs.get('links', []):
+            if link['source'] not in node_ids:
+                missing_ids.add(link['source'])
+            if link['target'] not in node_ids:
+                missing_ids.add(link['target'])
+        kwargs['nodes'] = nodes + [{'id': k} for k in missing_ids]
+
+        super(SankeyWidget, self).__init__(**kwargs)
+
+        self._node_clicked_handlers = widgets.CallbackDispatcher()
+        self._link_clicked_handlers = widgets.CallbackDispatcher()
+
+        self._auto_png_filename = None
+        self._auto_svg_filename = None
+        self.on_msg(self._handle_sankey_msg)
+
+    def on_node_clicked(self, callback, remove=False):
+        """Register a callback to execute when a node is clicked.
+
+        The callback will be called with three arguments: the Sankey widget
+        instance, the node, and the index of the node in the nodes list.
+
+        Parameters
+        ----------
+        remove : bool (optional)
+            Set to true to remove the callback from the list of callbacks.
+
+        """
+        self._node_clicked_handlers.register_callback(callback, remove=remove)
+
+    def on_link_clicked(self, callback, remove=False):
+        """Register a callback to execute when a link is clicked.
+
+        The callback will be called with three arguments: the Sankey widget
+        instance, the link, and the index of the link in the nodes list.
+
+        Parameters
+        ----------
+        remove : bool (optional)
+            Set to true to remove the callback from the list of callbacks.
+
+        """
+        self._link_clicked_handlers.register_callback(callback, remove=remove)
+
+    def _handle_sankey_msg(self, _, content, buffers):
+        """Handle a msg from the front-end.
+
+        Parameters
+        ----------
+        content: dict
+            Content of the msg."""
+        if content.get('event', '') == 'node_clicked':
+            self._node_clicked_handlers(self, content.get('node'))
+        if content.get('event', '') == 'link_clicked':
+            self._link_clicked_handlers(self, content.get('link'))
+
+    @observe("png")
+    def _on_png_data(self, change):
+        if change['type'] != 'change':
+            return
+        if self._auto_png_filename:
+            self.save_png(self._auto_png_filename)
+            self._auto_png_filename = None
+
+    def save_png(self, filename):
+        """Save the diagram to a PNG file.
+
+        The widget must be displayed first before the PNG data is available. To
+        display the widget and save an image at the same time, use
+        `auto_save_png`.
+
+        Parameters
+        ----------
+        filename : string
+        """
+        if self.png:
+            data = base64.decodebytes(bytes(self.png, 'ascii'))
+            with open(filename, 'wb') as f:
+                f.write(data)
+        else:
+            warnings.warn('No png image available! Try auto_save_png() instead?')
+
+    def auto_save_png(self, filename):
+        """Save the diagram to a PNG file, once it has been rendered.
+
+        This waits for the diagram to be rendered, then automatically calls
+        `save_png` for you.
+
+        Parameters
+        ----------
+        filename : string
+        """
+        self._auto_png_filename = filename
+        return self
+
+    @observe("svg")
+    def _on_svg_data(self, change):
+        if change['type'] != 'change':
+            return
+        if self._auto_svg_filename:
+            self.save_svg(self._auto_svg_filename)
+            self._auto_svg_filename = None
+
+    def save_svg(self, filename):
+        """Save the diagram to an SVG file.
+
+        The widget must be displayed first before the SVG data is available. To
+        display the widget and save an image at the same time, use
+        `auto_save_svg`.
+
+        Parameters
+        ----------
+        filename : string
+        """
+        if self.svg:
+            with open(filename, 'wb') as f:
+                f.write(self.svg.encode('utf8'))
+        else:
+            warnings.warn('No svg image available! Try auto_save_svg() instead?')
+
+    def auto_save_svg(self, filename):
+        """Save the diagram to an SVG file, once it has been rendered.
+
+        This waits for the diagram to be rendered, then automatically calls
+        `save_svg` for you.
+
+        Parameters
+        ----------
+        filename : string
+        """
+        self._auto_svg_filename = filename
+        return self
