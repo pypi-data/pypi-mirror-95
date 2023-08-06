865598d220cf48723caab641d84bbd705b82f3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:44 +0000

    Add package h5/0.4.0/h5-0.4.0.tar.gz

---
+++ packages/h5/h5-0.4.0.tar.gz/h5-0.4.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '.'}
+
+packages = \
+['h5']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['h5py>=2.10.0', 'numpy>=1.7,!=1.19.4']
+
+setup_kwargs = {
+    'name': 'h5',
+    'version': '0.4.0',
+    'description': 'H5py utils',
+    'long_description': None,
+    'author': 'jesse',
+    'author_email': 'jesse@dgi.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/dynamic-graphics-inc/dgpy-libs',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
