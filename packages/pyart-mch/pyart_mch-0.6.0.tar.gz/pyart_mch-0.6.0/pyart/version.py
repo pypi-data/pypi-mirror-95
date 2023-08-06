bb412966b2982868cd13e9b8d5404cf7279479a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:40 +0000

    Add package pyart-mch/0.6.0/pyart_mch-0.6.0.tar.gz

---
+++ packages/pyart-mch/pyart_mch-0.6.0.tar.gz/pyart_mch-0.6.0/pyart/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYART SETUP.PY
+short_version = '0.6.0'
+version = '0.6.0'
+full_version = '0.6.0.dev+e13d96c'
+git_revision = 'e13d96ca7a4f0be45aab5cd56efd9f6b90ddfcf0'
+release = False
+
+if not release:
+    version = full_version
