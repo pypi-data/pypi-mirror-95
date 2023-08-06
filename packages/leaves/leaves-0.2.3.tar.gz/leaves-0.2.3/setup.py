3e7993d54758bb764d58e13f57e84daef8f3d1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:34 +0000

    Add package leaves/0.2.3/leaves-0.2.3.tar.gz

---
+++ packages/leaves/leaves-0.2.3.tar.gz/leaves-0.2.3/setup.py
@@ -0,0 +1,29 @@
+"""
+Author:     LanHao
+Date:       2021/2/8 14:49
+Python:     python3.6
+
+"""
+
+from setuptools import setup, find_packages
+
+description = "基于rabbitmq的轻量级rpc调用封装"
+
+setup(
+    name='leaves',
+    version="0.2.3",
+    description=description,
+    long_description=description,
+    author="bigpangl",
+    author_email='bigpangl@163.com',
+    url='https://github.com/bigpangl/leaves',
+    py_modules=['leaves'],
+    install_requires=[
+        "aiormq"
+    ],
+    include_package_data=True,
+    keywords='rpc',
+    classifiers=[
+    ],
+    license="Apache License 2.0"
+)
