7f1ccdc4120b8e989338a484b0c70757102ec8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:54 +0000

    Add package lager/0.15.1/lager-0.15.1-py3-none-any.whl

---
+++ packages/lager/lager-0.15.1-py3-none-any.whl/lager/fastapi.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""FastAPI logging"""
+from typing import List, Optional, Set, Tuple, Union
+
+from lager.logging import intercept
+
+
+__all__ = ['FASTAPI_LOGGERS', 'fastapi_intercept']
+
+FASTAPI_LOGGERS = [
+    'gunicorn',
+    'gunicorn.errors' 'uvicorn',
+    'uvicorn.error',
+    'fastapi',
+    'sqlalchemy',
+]
+
+
+def fastapi_intercept(
+    loggers: Optional[Union[List[str], Set[str], Tuple[str, ...]]] = None
+) -> None:
+    _loggers2intercept = (
+        FASTAPI_LOGGERS if not loggers else sorted(set(*(*FASTAPI_LOGGERS, loggers)))
+    )
+    intercept(_loggers2intercept)
