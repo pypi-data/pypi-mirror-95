44f7777b4681a80ba34b819dae20ac48758eb88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:05 +0000

    Add package J-12-Spotify-recommendation/0.1.8/J-12-Spotify-recommendation-0.1.8.tar.gz

---
+++ packages/J-12-Spotify-recommendation/J-12-Spotify-recommendation-0.1.8.tar.gz/J-12-Spotify-recommendation-0.1.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='J-12-Spotify-recommendation',
+    version='0.1.8',
+    packages=['Scripts', 'my_tests'],
+    url='',
+    license='',
+    author='JonnyMurillo',
+    author_email='jonnymurillo288@gmail.com',
+    description='Create playlist with simple decision tree of your tracks in spotify'
+
+)
