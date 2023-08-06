3d8e31481578e15f9ea18c245194b2f5d8975022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:31 +0000

    Add package mongodantic/0.1.4b2/mongodantic-0.1.4b2.tar.gz

---
+++ packages/mongodantic/mongodantic-0.1.4b2.tar.gz/mongodantic-0.1.4b2/mongodantic/__init__.py
@@ -0,0 +1,10 @@
+from .connection import (
+    init_db_connection_params,
+    set_connection_env,
+    get_connection_env,
+)
+
+
+__version__ = '0.1.4b2'  # prerelease with rework handle connection errors
+
+__author__ = 'bzdvdn'
