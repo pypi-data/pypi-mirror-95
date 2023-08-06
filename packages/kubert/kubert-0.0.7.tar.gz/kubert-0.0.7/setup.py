de994dd4ba94977d39a808b5f4d2f50bcd3e0fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:55 +0000

    Add package kubert/0.0.7/kubert-0.0.7.tar.gz

---
+++ packages/kubert/kubert-0.0.7.tar.gz/kubert-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kubert",
+    version="0.0.7",
+    author="James Jones",
+    author_email="jam.jones@f5.com",
+    description="A Python Kubernetes API client designed to for making easy to register callbacks for watches",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nginx-architects/kubert",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Freely Distributable",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
