884d280a9674d68045de51cc9557ca08e71c4cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:51 +0000

    Add package gloro/0.0.5/gloro-0.0.5.tar.gz

---
+++ packages/gloro/gloro-0.0.5.tar.gz/gloro-0.0.5/gloro/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '0.0.5'
+
+# Disable eager execution if using Tensorflow 2.
+try:
+    from tensorflow.python.framework.ops import disable_eager_execution
+
+    disable_eager_execution()
+except:
+    pass
+
+from gloro.models import GloroNet
+
+from gloro.constants import GLORO_CUSTOM_OBJECTS
