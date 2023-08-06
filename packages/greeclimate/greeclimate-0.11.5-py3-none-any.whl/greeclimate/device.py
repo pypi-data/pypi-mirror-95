7a1b294bb410ac6e3215b73ff52a6adf4edf01ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:32 +0000

    Add package greeclimate/0.11.5/greeclimate-0.11.5-py3-none-any.whl

---
+++ packages/greeclimate/greeclimate-0.11.5-py3-none-any.whl/greeclimate/device.py
@@ -0,0 +1,419 @@
+import asyncio
+import enum
+import logging
+import re
+from enum import IntEnum, unique
+
+import greeclimate.network as network
+from greeclimate.exceptions import DeviceNotBoundError, DeviceTimeoutError
+
+
+class Props(enum.Enum):
+    POWER = "Pow"
+    MODE = "Mod"
+    TEMP_SET = "SetTem"
+    TEMP_SENSOR = "TemSen"
+    TEMP_UNIT = "TemUn"
+    TEMP_BIT = "TemRec"
+    FAN_SPEED = "WdSpd"
+    FRESH_AIR = "Air"
+    XFAN = "Blo"
+    ANION = "Health"
+    SLEEP = "SwhSlp"
+    SLEEP_MODE = "SlpMod"
+    LIGHT = "Lig"
+    SWING_HORIZ = "SwingLfRig"
+    SWING_VERT = "SwUpDn"
+    QUIET = "Quiet"
+    TURBO = "Tur"
+    STEADY_HEAT = "StHt"
+    POWER_SAVE = "SvSt"
+    UNKNOWN_HEATCOOLTYPE = "HeatCoolType"
+
+
+@unique
+class TemperatureUnits(IntEnum):
+    C = 0
+    F = 1
+
+
+@unique
+class Mode(IntEnum):
+    Auto = 0
+    Cool = 1
+    Dry = 2
+    Fan = 3
+    Heat = 4
+
+
+@unique
+class FanSpeed(IntEnum):
+    Auto = 0
+    Low = 1
+    MediumLow = 2
+    Medium = 3
+    MediumHigh = 4
+    High = 5
+
+
+@unique
+class HorizontalSwing(IntEnum):
+    Default = 0
+    FullSwing = 1
+    Left = 2
+    LeftCenter = 3
+    Center = 4
+    RightCenter = 5
+    Right = 6
+
+
+@unique
+class VerticalSwing(IntEnum):
+    Default = 0
+    FullSwing = 1
+    FixedUpper = 2
+    FixedUpperMiddle = 3
+    FixedMiddle = 4
+    FixedLowerMiddle = 5
+    FixedLower = 6
+    SwingUpper = 7
+    SwingUpperMiddle = 8
+    SwingMiddle = 9
+    SwingLowerMiddle = 10
+    SwingLower = 11
+
+
+class DeviceInfo:
+    """Device information class, used to identify and connect
+
+    Attributes
+        ip: IP address (ipv4 only) of the physical device
+        port: Usually this will always be 7000
+        mac: mac address, in the format 'aabbcc112233'
+        name: Name of unit, if available
+    """
+
+    def __init__(self, ip, port, mac, name, brand=None, model=None, version=None):
+        self.ip = ip
+        self.port = port
+        self.mac = mac
+        self.name = name if name else self.name
+        self.brand = brand
+        self.model = model
+        self.version = version
+
+    def __str__(self):
+        return f"Device: {self.name} @ {self.ip}:{self.port} (mac: {self.mac})"
+
+    def __eq__(self, other):
+        """Check equality based on Device Info properties"""
+        if isinstance(other, DeviceInfo):
+            return (
+                self.mac == other.mac
+                and self.name == other.name
+                and self.brand == other.brand
+                and self.model == other.model
+                and self.version == other.version
+            )
+        return False
+
+    def __ne__(self, other):
+        """Check inequality based on Device Info properties"""
+        return not self.__eq__(other)
+
+
+class Device:
+    """Class representing a physical device, it's state and properties.
+
+    Devices must be bound, either by discovering their presence, or supplying a persistent
+    device key which is then used for communication (and encryption) with the unit. See the
+    `bind` function for more details on how to do this.
+
+    Once a device is bound occasionally call `update_state` to request and update state from
+    the HVAC, as it is possible that it changes state from other sources.
+
+    Attributes:
+        power: A boolean indicating if the unit is on or off
+        mode: An int indicating operating mode, see `Mode` enum for possible values
+        target_temperature: The target temperature, ignore if in Auto, Fan or Steady Heat mode
+        temperature_units: An int indicating unit of measurement, see `TemperatureUnits` enum for possible values
+        current_temperature: The current temperature
+        fan_speed: An int indicating fan speed, see `FanSpeed` enum for possible values
+        fresh_air: A boolean indicating if fresh air valve is open, if present
+        xfan: A boolean to enable the fan to dry the coil, only used for cool and dry modes
+        anion: A boolean to enable the ozone generator, if present
+        sleep: A boolean to enable sleep mode, which adjusts temperature over time
+        light: A boolean to enable the light on the unit, if present
+        horizontal_swing: An int to control the horizontal blade position, see `HorizontalSwing` enum for possible values
+        vertical_swing: An int to control the vertical blade position, see `VerticalSwing` enum for possible values
+        quiet: A boolean to enable quiet operation
+        turbo: A boolean to enable turbo operation (heat or cool faster initially)
+        steady_heat: When enabled unit will maintain a target temperature of 8 degrees C
+        power_save: A boolen to enable power save operation
+    """
+
+    def __init__(self, device_info):
+        self._logger = logging.getLogger(__name__)
+
+        self.device_info = device_info
+        self.device_key = None
+
+        """ Device properties """
+        self._hid = None
+        self._version = None
+        self._properties = None
+        self._dirty = []
+
+    async def bind(self, key=None):
+        """Run the binding procedure.
+
+        Binding is a finnicky procedure, and happens in 1 of 2 ways:
+            1 - Without the key, binding must pass the device info structure immediately following
+                the search devices procedure. There is only a small window to complete registration.
+            2 - With a key, binding is implicit and no further action is required
+
+            Both approaches result in a device_key which is used as like a persitent session id.
+
+        Args:
+            key (str): The device key, when provided binding is a NOOP, if None binding will
+                       attempt to negatiate the key with the device.
+
+        Raises:
+            DeviceNotBoundError: If binding was unsuccessful and no key returned
+            DeviceTimeoutError: The device didn't respond
+        """
+
+        if not self.device_info:
+            raise DeviceNotBoundError
+
+        self._logger.info("Starting device binding to %s", str(self.device_info))
+
+        try:
+            if key:
+                self.device_key = key
+            else:
+                self.device_key = await network.bind_device(
+                    self.device_info, announce=False
+                )
+        except asyncio.TimeoutError:
+            raise DeviceTimeoutError
+
+        if not self.device_key:
+            raise DeviceNotBoundError
+        else:
+            self._logger.info("Bound to device using key %s", self.device_key)
+
+    async def request_version(self) -> None:
+        """Request the firmware version from the device."""
+        ret = await network.request_state(["hid"], self.device_info, self.device_key)
+        self._hid = ret.get("hid")
+
+        # Ex: hid = 362001000762+U-CS532AE(LT)V3.31.bin
+        if self._hid:
+            match = re.search(r"(?<=V)([\d.]+)\.bin$", self._hid)
+            self._version = match and match.group(1)
+
+            # Special case firmwares ...
+            if self._hid.endswith("_JDV1.bin"):
+                self._version = "4.0"
+
+    async def update_state(self):
+        """ Update the internal state of the device structure of the physical device """
+        if not self.device_key:
+            await self.bind()
+
+        self._logger.debug("Updating device properties for (%s)", str(self.device_info))
+
+        props = [x.value for x in Props]
+
+        try:
+            if not self._hid:
+                await self.request_version()
+
+            self._properties = await network.request_state(
+                props, self.device_info, self.device_key
+            )
+        except asyncio.TimeoutError:
+            raise DeviceTimeoutError
+
+    async def push_state_update(self):
+        """ Push any pending state updates to the unit """
+        if not self._dirty:
+            return
+
+        if not self.device_key:
+            await self.bind()
+
+        self._logger.debug("Pushing state updates to (%s)", str(self.device_info))
+
+        props = {}
+        for name in self._dirty:
+            value = self._properties.get(name)
+            self._logger.debug("Sending remote state update %s -> %s", name, value)
+            props[name] = value
+
+        self._dirty.clear()
+
+        try:
+            await network.send_state(props, self.device_info, key=self.device_key)
+        except asyncio.TimeoutError:
+            raise DeviceTimeoutError
+
+    def get_property(self, name):
+        """ Generic lookup of properties tracked from the physical device """
+        if self._properties:
+            return self._properties.get(name.value)
+        return None
+
+    def set_property(self, name, value):
+        """ Generic setting of properties for the physical device """
+        if not self._properties:
+            self._properties = {}
+
+        if self._properties.get(name.value) == value:
+            return
+        else:
+            self._properties[name.value] = value
+            if name.value not in self._dirty:
+                self._dirty.append(name.value)
+
+    @property
+    def power(self) -> bool:
+        return bool(self.get_property(Props.POWER))
+
+    @power.setter
+    def power(self, value: int):
+        self.set_property(Props.POWER, int(value))
+
+    @property
+    def mode(self) -> int:
+        return self.get_property(Props.MODE)
+
+    @mode.setter
+    def mode(self, value: int):
+        self.set_property(Props.MODE, int(value))
+
+    @property
+    def target_temperature(self) -> int:
+        return self.get_property(Props.TEMP_SET)
+
+    @target_temperature.setter
+    def target_temperature(self, value: int):
+        self.set_property(Props.TEMP_SET, int(value))
+
+    @property
+    def temperature_units(self) -> int:
+        return self.get_property(Props.TEMP_UNIT)
+
+    @temperature_units.setter
+    def temperature_units(self, value: int):
+        self.set_property(Props.TEMP_UNIT, int(value))
+
+    @property
+    def current_temperature(self) -> int:
+        prop = self.get_property(Props.TEMP_SENSOR)
+        if prop is not None:
+            v = self._version and int(self._version.split(".")[0])
+            if v == 4:
+                return prop
+            elif prop != 0:
+                return prop - 40
+
+        return self.target_temperature
+
+    @property
+    def fan_speed(self) -> int:
+        return self.get_property(Props.FAN_SPEED)
+
+    @fan_speed.setter
+    def fan_speed(self, value: int):
+        self.set_property(Props.FAN_SPEED, int(value))
+
+    @property
+    def fresh_air(self) -> bool:
+        return bool(self.get_property(Props.FRESH_AIR))
+
+    @fresh_air.setter
+    def fresh_air(self, value: bool):
+        self.set_property(Props.FRESH_AIR, int(value))
+
+    @property
+    def xfan(self) -> bool:
+        return bool(self.get_property(Props.XFAN))
+
+    @xfan.setter
+    def xfan(self, value: bool):
+        self.set_property(Props.XFAN, int(value))
+
+    @property
+    def anion(self) -> bool:
+        return bool(self.get_property(Props.ANION))
+
+    @anion.setter
+    def anion(self, value: bool):
+        self.set_property(Props.ANION, int(value))
+
+    @property
+    def sleep(self) -> bool:
+        return bool(self.get_property(Props.SLEEP))
+
+    @sleep.setter
+    def sleep(self, value: bool):
+        self.set_property(Props.SLEEP, int(value))
+        self.set_property(Props.SLEEP_MODE, int(value))
+
+    @property
+    def light(self) -> bool:
+        return bool(self.get_property(Props.LIGHT))
+
+    @light.setter
+    def light(self, value: bool):
+        self.set_property(Props.LIGHT, int(value))
+
+    @property
+    def horizontal_swing(self) -> int:
+        return self.get_property(Props.SWING_HORIZ)
+
+    @horizontal_swing.setter
+    def horizontal_swing(self, value: int):
+        self.set_property(Props.SWING_HORIZ, int(value))
+
+    @property
+    def vertical_swing(self) -> int:
+        return self.get_property(Props.SWING_VERT)
+
+    @vertical_swing.setter
+    def vertical_swing(self, value: int):
+        self.set_property(Props.SWING_VERT, int(value))
+
+    @property
+    def quiet(self) -> bool:
+        return self.get_property(Props.QUIET)
+
+    @quiet.setter
+    def quiet(self, value: bool):
+        self.set_property(Props.QUIET, int(value))
+
+    @property
+    def turbo(self) -> bool:
+        return bool(self.get_property(Props.TURBO))
+
+    @turbo.setter
+    def turbo(self, value: bool):
+        self.set_property(Props.TURBO, int(value))
+
+    @property
+    def steady_heat(self) -> bool:
+        return bool(self.get_property(Props.STEADY_HEAT))
+
+    @steady_heat.setter
+    def steady_heat(self, value: bool):
+        self.set_property(Props.STEADY_HEAT, int(value))
+
+    @property
+    def power_save(self) -> bool:
+        return bool(self.get_property(Props.POWER_SAVE))
+
+    @power_save.setter
+    def power_save(self, value: bool):
+        self.set_property(Props.POWER_SAVE, int(value))
---
+++ packages/greeclimate/greeclimate-0.11.5-py3-none-any.whl/greeclimate/network.py
@@ -0,0 +1,391 @@
+import asyncio
+import base64
+import json
+import logging
+import socket
+from dataclasses import dataclass
+from typing import Any, Dict, Text, Tuple, Union
+
+from Crypto.Cipher import AES
+
+NETWORK_TIMEOUT = 10
+GENERIC_KEY = "a3K8Bx%2r8Y7#xDh"
+
+_LOGGER = logging.getLogger(__name__)
+
+
+IPAddr = Tuple[str, int]
+
+
+@dataclass
+class IPInterface:
+    ip_address: str
+    bcast_address: str
+
+
+class DeviceProtocol2(asyncio.DatagramProtocol):
+    """Event driven device protocol class."""
+
+    def __init__(self, timeout: int = 10, drained: asyncio.Event = None) -> None:
+        """Initialize the device protocol object.
+
+        Args:
+            timeout (int): Packet send timeout
+            drained (asyncio.Event): Packet send drain event signal
+        """
+        self._timeout = timeout
+        self._drained = drained or asyncio.Event()
+        self._drained.set()
+        self._transport = None
+        self._key = GENERIC_KEY
+
+    # This event need to be implement to handle incoming requests
+    def packet_received(self, obj, addr: IPAddr) -> None:
+        """Event called when a packet is received and decoded.
+
+        Args:
+            obj (JSON): Json object with decoded UDP data
+            addr (IPAddr): Endpoint address of the sender
+        """
+        raise NotImplementedError(self)
+
+    @property
+    def device_key(self) -> str:
+        """Sets the encryption key used for device data."""
+        return self._key
+
+    @device_key.setter
+    def device_key(self, value: str):
+        """Gets the encryption key used for device data."""
+        self._key = value
+
+    def close(self) -> None:
+        """Close the UDP transport."""
+        try:
+            self._transport.close()
+        except RuntimeError:
+            pass
+
+    def connection_made(self, transport: asyncio.transports.DatagramTransport) -> None:
+        """Called when the Datagram protocol handler is initialized."""
+        self._transport = transport
+
+    def connection_lost(self, exc: Exception) -> None:
+        """Handle a closed socket."""
+
+        # In this case the connection was closed unexpectedly
+        if exc is not None:
+            _LOGGER.exception("Connection was closed unexpectedly", exc_info=exc)
+
+        if self._transport is not None:
+            self._transport.close()
+            self._transport = None
+
+    def error_received(self, exc: Exception) -> None:
+        """Handle error while sending/receiving datagrams."""
+        raise exc
+
+    def pause_writing(self) -> None:
+        """Stop writing additional data to the transport."""
+        self._drained.clear()
+        super().pause_writing()
+
+    def resume_writing(self) -> None:
+        """Resume writing data to the transport."""
+        self._drained.set()
+        super().resume_writing()
+
+    def datagram_received(self, data: bytes, addr: IPAddr) -> None:
+        """Handle an incoming datagram."""
+        if len(data) == 0:
+            return
+
+        obj = json.loads(data)
+        key = GENERIC_KEY if obj.get("i") == 1 else self._key
+
+        if obj.get("pack"):
+            obj["pack"] = DeviceProtocol2.decrypt_payload(obj["pack"], key)
+
+        _LOGGER.debug("Received packet from %s:\n%s", addr[0], json.dumps(obj))
+
+        self.packet_received(obj, addr)
+
+    async def send(self, obj, addr: IPAddr = None) -> None:
+        """Send encode and send JSON command to the device."""
+        _LOGGER.debug("Sending packet:\n%s", json.dumps(obj))
+
+        if obj.get("pack"):
+            key = GENERIC_KEY if obj.get("i") == 1 else self._key
+            obj["pack"] = DeviceProtocol2.encrypt_payload(obj["pack"], key)
+
+        data_bytes = json.dumps(obj).encode()
+        self._transport.sendto(data_bytes, addr)
+
+        task = asyncio.create_task(self._drained.wait())
+        await asyncio.wait_for(task, self._timeout)
+
+    @staticmethod
+    def decrypt_payload(payload, key=GENERIC_KEY):
+        cipher = AES.new(key.encode(), AES.MODE_ECB)
+        decoded = base64.b64decode(payload)
+        decrypted = cipher.decrypt(decoded).decode()
+        t = decrypted.replace(decrypted[decrypted.rindex("}") + 1 :], "")
+        return json.loads(t)
+
+    @staticmethod
+    def encrypt_payload(payload, key=GENERIC_KEY):
+        def pad(s):
+            bs = 16
+            return s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
+
+        cipher = AES.new(key.encode(), AES.MODE_ECB)
+        encrypted = cipher.encrypt(pad(json.dumps(payload)).encode())
+        encoded = base64.b64encode(encrypted).decode()
+        return encoded
+
+
+class BroadcastListenerProtocol(DeviceProtocol2):
+    """Special protocol handler for when broadcast is needed."""
+
+    def connection_made(self, transport: asyncio.transports.DatagramTransport) -> None:
+        """Called when the Datagram protocol handler is initialized."""
+        super().connection_made(transport)
+
+        sock = transport.get_extra_info("socket")  # type: socket.socket
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+
+class DeviceProtocol(asyncio.DatagramProtocol):
+    def __init__(
+        self, recvq: asyncio.Queue, excq: asyncio.Queue, drained: asyncio.Queue
+    ) -> None:
+        self._loop = asyncio.get_event_loop()
+
+        self._recvq = recvq
+        self._excq = excq
+        self._drained = drained
+
+        self._drained.set()
+
+        # Transports are connected at the time a connection is made.
+        self._transport = None
+
+    def connection_made(self, transport: asyncio.transports.DatagramTransport) -> None:
+
+        self._transport = transport
+
+    def datagram_received(self, data, addr: IPAddr) -> None:
+        self._recvq.put_nowait((data, addr))
+
+    def connection_lost(self, exc) -> None:
+        if exc is not None:
+            self._excq.put_nowait(exc)
+
+        if self._transport is not None:
+            self._transport.close()
+            self._transport = None
+
+    def error_received(self, exc) -> None:
+        self._excq.put_nowait(exc)
+
+    def pause_writing(self) -> None:
+        self._drained.clear()
+        super().pause_writing()
+
+    def resume_writing(self) -> None:
+        self._drained.set()
+        super().resume_writing()
+
+
+# Concepts and code here were taken from https://github.com/jsbronder/asyncio-dgram
+class DatagramStream:
+    def __init__(self, transport, recvq, excq, drained, timeout: int = 120):
+        self._transport = transport
+        self._recvq = recvq
+        self._excq = excq
+        self._drained = drained
+        self._timeout = timeout
+
+    def __del__(self):
+        self.close()
+
+    @property
+    def exception(self):
+        try:
+            exc = self._excq.get_nowait()
+            raise exc
+        except asyncio.queues.QueueEmpty:
+            pass
+
+    @property
+    def socket(self):
+        return self._transport.get_extra_info("socket")
+
+    def close(self):
+        try:
+            self._transport.close()
+        except RuntimeError:
+            pass
+
+    async def send(self, data, addr=None) -> None:
+        _ = self.exception
+        self._transport.sendto(data, addr)
+
+        task = asyncio.create_task(self._drained.wait())
+        await asyncio.wait_for(task, self._timeout)
+
+    async def send_device_data(self, data, key=GENERIC_KEY) -> None:
+        """Send a formatted request to the device."""
+        _LOGGER.debug("Sending packet:\n%s", json.dumps(data))
+
+        if "pack" in data:
+            data["pack"] = DatagramStream.encrypt_payload(data["pack"], key)
+
+        data_bytes = json.dumps(data).encode()
+        await self.send(data_bytes)
+
+    def recv_ready(self):
+        _ = self.exception
+        return not self._recvq.empty()
+
+    async def recv(self):
+        _ = self.exception
+
+        task = asyncio.create_task(self._recvq.get())
+        await asyncio.wait_for(task, self._timeout)
+        return task.result()
+
+    async def recv_device_data(self, key=GENERIC_KEY):
+        """Receive a formatted request from the device."""
+        (data_bytes, addr) = await self.recv()
+        if len(data_bytes) == 0:
+            return
+
+        data = json.loads(data_bytes)
+
+        if "pack" in data:
+            data["pack"] = DatagramStream.decrypt_payload(data["pack"], key)
+
+        _LOGGER.debug("Received packet:\n%s", json.dumps(data))
+        return (data, addr)
+
+    @staticmethod
+    def decrypt_payload(payload, key=GENERIC_KEY):
+        cipher = AES.new(key.encode(), AES.MODE_ECB)
+        decoded = base64.b64decode(payload)
+        decrypted = cipher.decrypt(decoded).decode()
+        t = decrypted.replace(decrypted[decrypted.rindex("}") + 1 :], "")
+        return json.loads(t)
+
+    @staticmethod
+    def encrypt_payload(payload, key=GENERIC_KEY):
+        def pad(s):
+            bs = 16
+            return s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
+
+        cipher = AES.new(key.encode(), AES.MODE_ECB)
+        encrypted = cipher.encrypt(pad(json.dumps(payload)).encode())
+        encoded = base64.b64encode(encrypted).decode()
+        return encoded
+
+
+async def create_datagram_stream(target: IPAddr) -> DatagramStream:
+    loop = asyncio.get_event_loop()
+    recvq = asyncio.Queue()
+    excq = asyncio.Queue()
+    drained = asyncio.Event()
+
+    transport, _ = await loop.create_datagram_endpoint(
+        lambda: DeviceProtocol(recvq, excq, drained), remote_addr=target
+    )
+    return DatagramStream(transport, recvq, excq, drained, timeout=NETWORK_TIMEOUT)
+
+
+async def bind_device(device_info, announce=False):
+    payload = {
+        "cid": "app",
+        "i": 1,
+        "t": "pack",
+        "uid": 0,
+        "tcid": device_info.mac,
+        "pack": {"mac": device_info.mac, "t": "bind", "uid": 0},
+    }
+
+    remote_addr = (device_info.ip, device_info.port)
+    stream = await create_datagram_stream(remote_addr)
+    try:
+        # Binding uses the generic key only
+        if announce:
+            await stream.send_device_data({"t": "scan"})
+            await stream.recv_device_data()
+        await stream.send_device_data(payload)
+        (r, _) = await stream.recv_device_data()
+    except asyncio.TimeoutError as e:
+        raise e
+    except Exception as e:
+        _LOGGER.exception("Encountered an error trying to bind device")
+        raise e
+    finally:
+        stream.close()
+
+    return r["pack"].get("key")
+
+
+async def send_state(property_values, device_info, key=GENERIC_KEY):
+    payload = {
+        "cid": "app",
+        "i": 0,
+        "t": "pack",
+        "uid": 0,
+        "tcid": device_info.mac,
+        "pack": {
+            "opt": list(property_values.keys()),
+            "p": list(property_values.values()),
+            "t": "cmd",
+        },
+    }
+
+    remote_addr = (device_info.ip, device_info.port)
+    stream = await create_datagram_stream(remote_addr)
+    try:
+        await stream.send_device_data(payload, key)
+        (r, _) = await stream.recv_device_data(key)
+    except asyncio.TimeoutError as e:
+        raise e
+    except Exception as e:
+        _LOGGER.exception("Encountered an error sending state to device")
+        raise e
+    finally:
+        stream.close()
+
+    cols = r["pack"]["opt"]
+    dat = r["pack"]["val"]
+    return dict(zip(cols, dat))
+
+
+async def request_state(properties, device_info, key=GENERIC_KEY):
+    payload = {
+        "cid": "app",
+        "i": 0,
+        "t": "pack",
+        "uid": 0,
+        "tcid": device_info.mac,
+        "pack": {"mac": device_info.mac, "t": "status", "cols": list(properties)},
+    }
+
+    remote_addr = (device_info.ip, device_info.port)
+    stream = await create_datagram_stream(remote_addr)
+    try:
+        await stream.send_device_data(payload, key)
+        (r, _) = await stream.recv_device_data(key)
+    except asyncio.TimeoutError as e:
+        raise e
+    except Exception as e:
+        _LOGGER.exception("Encountered an error requesting update from device")
+        raise e
+    finally:
+        stream.close()
+
+    cols = r["pack"]["cols"]
+    dat = r["pack"]["dat"]
+    return dict(zip(cols, dat))
