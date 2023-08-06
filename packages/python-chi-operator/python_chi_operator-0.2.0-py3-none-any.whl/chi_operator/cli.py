7de70e7b84a4c5e0351126eae53e175d5e64ee6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:51 +0000

    Add package python-chi-operator/0.2.0/python_chi_operator-0.2.0-py3-none-any.whl

---
+++ packages/python-chi-operator/python_chi_operator-0.2.0-py3-none-any.whl/chi_operator/cli.py
@@ -0,0 +1,25 @@
+import logging
+
+import click
+
+from .network import network
+from .node import node
+from .user import user
+
+
+@click.group()
+@click.option('-v', '--verbose', 'verbose', count=True)
+@click.pass_context
+def cli(ctx, verbose):
+    ctx.ensure_object(dict)
+    log_level = logging.DEBUG if verbose else logging.INFO
+    logging.basicConfig(level=log_level)
+
+
+cli.add_command(network)
+cli.add_command(node)
+cli.add_command(user)
+
+
+if __name__ == '__main__':
+    cli(obj={})
