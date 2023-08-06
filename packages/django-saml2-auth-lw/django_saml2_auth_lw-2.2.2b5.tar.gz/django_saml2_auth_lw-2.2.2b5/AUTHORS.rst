688f293cf2faa1ba611790e47426e428ae241d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:55 +0000

    Add package django-saml2-auth-lw/2.2.2b5/django_saml2_auth_lw-2.2.2b5.tar.gz

---
+++ packages/django-saml2-auth-lw/django_saml2_auth_lw-2.2.2b5.tar.gz/django_saml2_auth_lw-2.2.2b5/AUTHORS.rst
@@ -0,0 +1,34 @@
+This project is written and maintained by Fang Li and
+various contributors:
+
+
+Django Saml2 Auth Lotterywest
+-----------------
+
+- Fang Li (Original Author of source repository)
+- Ignacio Cabeza
+- Frinze Erin Lapuz
+
+
+
+Pysaml2
+-------
+
+- Roland Hedberg and it's contributors
+
+
+
+Contributors
+------------
+
+- `DSpeichert <https://github.com/DSpeichert>`_
+- `jacobh <https://github.com/jacobh>`_
+- `Gene Wood <http://github.com/gene1wood/>`_
+- `Terry <https://github.com/tpeng>`_
+- `Tim Pierce <https://github.com/qwrrty/>`_ (Adobe Systems)
+- `Tonymke <https://github.com/tonymke/>`_
+- `pintor <https://github.com/pintor>`_
+- `BaconAndEggs <https://github.com/BaconAndEggs>`_
+- `Ryan Mahaffey <https://github.com/mahaffey>`_
+- `ayr-ton <https://github.com/ayr-ton>`_
+_ `kevPo <https://github.com/kevPo>`_
---
+++ packages/django-saml2-auth-lw/django_saml2_auth_lw-2.2.2b5.tar.gz/django_saml2_auth_lw-2.2.2b5/setup.py
@@ -0,0 +1,67 @@
+"""The setup module for django_saml2_auth.
+See:
+https://github.com/fangli/django_saml2_auth
+"""
+
+from codecs import open
+from setuptools import (setup, find_packages)
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django_saml2_auth_lw',
+    description='Django SAML2 Authentication Made Easy. Easily integrate with SAML2 SSO identity providers like Okta',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+
+    url='https://github.com/Lotterywest/django-saml2-auth-lw',
+
+    author='Lotterywest GoRillas Team',
+    author_email='Gorillas@lotterywest.wa.gov.au',
+
+    license='Apache 2.0',
+
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Framework :: Django :: 1.5',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='Django SAML2 Authentication Made Easy, integrate with SAML2 SSO such as Okta easily',
+
+    packages=find_packages(),
+
+    install_requires=['pysaml2>=4.5.0',
+                      'drf-jwt',
+                      'django-rest-auth', ],
+    include_package_data=True,
+)
