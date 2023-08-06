735db61ad90141463ae2c717cd667daf7339da4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:00 +0000

    Add package mathgenerator/1.1.12/mathgenerator-1.1.12-py3-none-any.whl

---
+++ packages/mathgenerator/mathgenerator-1.1.12-py3-none-any.whl/mathgenerator/funcs/geometry/circumference.py
@@ -0,0 +1,14 @@
+from .__init__ import *
+import math
+
+
+def circumferenceCircle(maxRadius=100):
+    r = random.randint(0, maxRadius)
+    circumference = 2 * math.pi * r
+    problem = f"Circumference of circle with radius {r}"
+    solution = circumference
+    return problem, solution
+
+
+circumference = Generator("Circumference", 104, "2*pi*r=", "circumference", circumferenceCircle,
+                          ["maxRadius=100"])
