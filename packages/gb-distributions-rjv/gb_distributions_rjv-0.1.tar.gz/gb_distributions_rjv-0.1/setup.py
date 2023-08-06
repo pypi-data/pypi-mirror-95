4ebf07e5b09d02570b71a0942e8022dec44f02b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:15 +0000

    Add package gb-distributions-rjv/0.1/gb_distributions_rjv-0.1.tar.gz

---
+++ packages/gb-distributions-rjv/gb_distributions_rjv-0.1.tar.gz/gb_distributions_rjv-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gb_distributions_rjv',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gb_distributions_rjv'],
+      zip_safe=False)
