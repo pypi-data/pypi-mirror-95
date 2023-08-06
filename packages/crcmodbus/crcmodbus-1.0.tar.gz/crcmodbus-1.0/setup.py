8cc2ab50b8552a6cc38e1c4597e742aa0c749908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:14 +0000

    Add package crcmodbus/1.0/crcmodbus-1.0.tar.gz

---
+++ packages/crcmodbus/crcmodbus-1.0.tar.gz/crcmodbus-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+        name="crcmodbus",
+        version="1.0",
+        description="",
+        author="jdh99",
+        author_email="jdh821@163.com",
+        url="https://gitee.com/jdhxyy/crc16-python",
+        packages=find_packages(),
+    )
