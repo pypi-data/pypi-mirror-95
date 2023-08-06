06e103dcb5d5b7f69242ebefe8126d8e49e1eb43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:51 +0000

    Add package postgres-table-as-queue-tools/0.0.1/postgres_table_as_queue_tools-0.0.1.tar.gz

---
+++ packages/postgres-table-as-queue-tools/postgres_table_as_queue_tools-0.0.1.tar.gz/postgres_table_as_queue_tools-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Postgres Table-as-queue Tools Module
+
+There are just only several function to getting SQL queries for manage your table as a queue. 
---
+++ packages/postgres-table-as-queue-tools/postgres_table_as_queue_tools-0.0.1.tar.gz/postgres_table_as_queue_tools-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='postgres_table_as_queue_tools',
+    url='https://github.com/vinger4/pg_table_as_queue_tools',
+    author='Evgenii Kozhanov',
+    author_email='evgenii.kozhanov@gmail.com',
+    packages=['pg_table_as_queue_tools'],
+    install_requires=[],
+    version='0.0.1',
+    license='MIT',
+    description='There are just only several function to getting SQL queries for manage your table as a queue.',
+)
