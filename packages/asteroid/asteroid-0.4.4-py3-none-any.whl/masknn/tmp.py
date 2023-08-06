12f8694f07ef17a355e7ff72b77583ab5cfe265b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:46 +0000

    Add package asteroid/0.4.4/asteroid-0.4.4-py3-none-any.whl

---
+++ packages/asteroid/asteroid-0.4.4-py3-none-any.whl/masknn/tmp.py
@@ -0,0 +1,17 @@
+import torch
+from torch import nn
+
+
+class MixtureConsistency(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class GriffinLim(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class MISI(nn.Module):
+    def __init__(self):
+        super().__init__()
