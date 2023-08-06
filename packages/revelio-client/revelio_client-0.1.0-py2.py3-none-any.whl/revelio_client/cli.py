8df6c779e159bd1db6c571a5e7f2c6e98813312b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:33 +0000

    Add package revelio-client/0.1.0/revelio_client-0.1.0-py2.py3-none-any.whl

---
+++ packages/revelio-client/revelio_client-0.1.0-py2.py3-none-any.whl/revelio_client/cli.py
@@ -0,0 +1,16 @@
+"""Console script for revelio_client."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for revelio_client."""
+    click.echo("Replace this message by putting your code into "
+               "revelio_client.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
