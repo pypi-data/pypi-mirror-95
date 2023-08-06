4f70a5f56ba96eb5ae2108af0ab4611bd49ebbd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:36 +0000

    Add package plone.app.vocabularies/4.2.2/plone.app.vocabularies-4.2.2-py2.py3-none-any.whl

---
+++ packages/plone.app.vocabularies/plone.app.vocabularies-4.2.2-py2.py3-none-any.whl/plone/app/vocabularies/actions.py
@@ -0,0 +1,50 @@
+# -*- coding:utf-8 -*-
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone import PloneMessageFactory as _
+from zope.interface import implementer
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+@implementer(IVocabularyFactory)
+class ActionCategoriesVocabulary(object):
+    """Provides an actions categories vocabulary"""
+
+    def __call__(self, context):
+        portal_actions = getToolByName(context, 'portal_actions')
+
+        # Building the list of action categories
+        try:
+            categories = portal_actions.listFilteredActionsFor(context).keys()
+        except Exception:
+            portal = context.portal_url.getPortalObject()
+            categories = portal.portal_actions.objectIds()
+        return SimpleVocabulary(
+            [SimpleTerm(cat, title=cat) for cat in sorted(categories)]
+        )
+
+
+ActionCategoriesVocabularyFactory = ActionCategoriesVocabulary()
+
+
+@implementer(IVocabularyFactory)
+class PortalActionCategoriesVocabulary(object):
+    """Provides portal action categories vocabulary."""
+
+    def __call__(self, context):
+        portal_actions = getToolByName(context, 'portal_actions')
+        categories = portal_actions.objectIds()
+        terms = []
+        for category in sorted(categories):
+            if category == 'controlpanel':
+                continue
+            title = portal_actions.get(category).title
+            if title:
+                title = _(title)
+            else:
+                title = category
+            terms.append(SimpleTerm(category, title=title))
+        return SimpleVocabulary(terms)
+
+PortalActionCategoriesVocabularyFactory = PortalActionCategoriesVocabulary()
---
+++ packages/plone.app.vocabularies/plone.app.vocabularies-4.2.2-py2.py3-none-any.whl/plone/app/vocabularies/tests/test_vocabularies.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+from doctest import DocTestSuite
+from zope.component import hooks
+from zope.component.testing import setUp
+from zope.component.testing import tearDown
+from zope.configuration.xmlconfig import XMLConfig
+
+import doctest
+import re
+import six
+import unittest
+import zope.component
+
+
+def vocabSetUp(self):
+    setUp()
+    import plone.app.vocabularies
+    XMLConfig('meta.zcml', zope.component)()
+    XMLConfig('configure.zcml', plone.app.vocabularies)()
+    hooks.setHooks()
+
+
+def vocabTearDown(self):
+    tearDown()
+    hooks.resetHooks()
+    hooks.setSite()
+
+
+class Py23DocChecker(doctest.OutputChecker):
+
+    def check_output(self, want, got, optionflags):
+        if not six.PY2:
+            want = re.sub("u'(.*?)'", "'\\1'", want)
+            want = re.sub('u"(.*?)"', '"\\1"', want)
+        return doctest.OutputChecker.check_output(self, want, got, optionflags)
+
+
+def test_suite():
+    optionflags = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
+    return unittest.TestSuite(
+        (
+            DocTestSuite(
+                'plone.app.vocabularies.terms'
+            ),
+            DocTestSuite(
+                'plone.app.vocabularies.catalog',
+                setUp=vocabSetUp,
+                tearDown=vocabTearDown,
+                optionflags=optionflags,
+                checker=Py23DocChecker(),
+            ),
+            DocTestSuite(
+                'plone.app.vocabularies.datetimerelated',
+                setUp=vocabSetUp,
+                tearDown=vocabTearDown,
+                optionflags=optionflags,
+                checker=Py23DocChecker(),
+            ),
+            DocTestSuite(
+                'plone.app.vocabularies.groups',
+                setUp=vocabSetUp,
+                tearDown=vocabTearDown,
+                optionflags=optionflags,
+                checker=Py23DocChecker(),
+            ),
+            DocTestSuite(
+                'plone.app.vocabularies.language',
+                setUp=vocabSetUp,
+                tearDown=vocabTearDown,
+                optionflags=optionflags,
+                checker=Py23DocChecker(),
+            ),
+            DocTestSuite(
+                'plone.app.vocabularies.metadatafields',
+                setUp=vocabSetUp,
+                tearDown=vocabTearDown,
+                optionflags=optionflags,
+                checker=Py23DocChecker(),
+            ),
+            DocTestSuite(
+                'plone.app.vocabularies.security',
+                setUp=vocabSetUp,
+                tearDown=vocabTearDown,
+                optionflags=optionflags,
+                checker=Py23DocChecker(),
+            ),
+            DocTestSuite(
+                'plone.app.vocabularies.skins',
+                setUp=vocabSetUp,
+                tearDown=vocabTearDown,
+                optionflags=optionflags,
+                checker=Py23DocChecker(),
+            ),
+            DocTestSuite(
+                'plone.app.vocabularies.types',
+                setUp=vocabSetUp,
+                tearDown=vocabTearDown,
+                optionflags=optionflags,
+                checker=Py23DocChecker(),
+            ),
+            DocTestSuite(
+                'plone.app.vocabularies.users',
+                setUp=vocabSetUp,
+                tearDown=vocabTearDown,
+                optionflags=optionflags,
+                checker=Py23DocChecker(),
+            ),
+            DocTestSuite(
+                'plone.app.vocabularies.workflow',
+                setUp=vocabSetUp,
+                tearDown=vocabTearDown,
+                optionflags=optionflags,
+                checker=Py23DocChecker(),
+            ),
+        )
+    )
