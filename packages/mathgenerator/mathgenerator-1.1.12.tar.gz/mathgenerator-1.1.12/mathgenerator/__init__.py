5b50d7d90f5d3175ea0119db1e35d00c2aa7f837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:00 +0000

    Add package mathgenerator/1.1.12/mathgenerator-1.1.12.tar.gz

---
+++ packages/mathgenerator/mathgenerator-1.1.12.tar.gz/mathgenerator-1.1.12/mathgenerator/__init__.py
@@ -0,0 +1,49 @@
+import sys
+import traceback
+
+import random
+import sympy
+import numpy
+import scipy
+
+genList = []
+
+
+class Generator:
+    def __init__(self, title, id, generalProb, generalSol, func, kwargs):
+        self.title = title
+        self.id = id
+        self.generalProb = generalProb
+        self.generalSol = generalSol
+        self.func = func
+        self.kwargs = kwargs
+
+        (filename, line_number, function_name,
+         text) = traceback.extract_stack()[-2]
+        funcname = filename[filename.rfind('/'):].strip()
+        funcname = funcname[1:-3]
+        subjectname = filename[:filename.rfind('/')].strip()
+        subjectname = subjectname[subjectname.rfind('/'):].strip()
+        subjectname = subjectname[1:]
+        genList.append([id, title, self, funcname, subjectname, kwargs])
+
+    def __str__(self):
+        return str(
+            self.id
+        ) + " " + self.title + " " + self.generalProb + " " + self.generalSol
+
+    def __call__(self, *args, **kwargs):
+        try:
+            return self.func(*args, **kwargs)
+        except TypeError:
+            # If an error is thrown from kwargs, remove the style element
+            # This happens if someone trys to get style='latex' for an
+            del kwargs['style']
+            return self.func(*args, **kwargs)
+
+
+def getGenList():
+    correctedList = genList[-1:] + genList[:-1]
+    # Orders list by id
+    correctedList.sort()
+    return correctedList
---
+++ packages/mathgenerator/mathgenerator-1.1.12.tar.gz/mathgenerator-1.1.12/mathgenerator/funcs/geometry/area_of_circle.py
@@ -0,0 +1,14 @@
+from .__init__ import *
+
+
+def areaCircle(maxRadius=100):
+    r = random.randint(0, maxRadius)
+    pi = 22 / 7
+    area = pi * r * r
+    problem = f"Area of circle with radius {r}"
+    solution = area
+    return problem, solution
+
+
+area_of_circle = Generator("Area of Circle", 112, "pi*r*r=", "area", areaCircle,
+                           ["maxRadius=100"])
