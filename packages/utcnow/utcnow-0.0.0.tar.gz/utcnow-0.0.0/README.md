d52999e9f15a30709fa96a6904a916f0de89fe08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:56 +0000

    Add package utcnow/0.0.0/utcnow-0.0.0.tar.gz

---
+++ packages/utcnow/utcnow-0.0.0.tar.gz/utcnow-0.0.0/README.md
@@ -0,0 +1,15 @@
+# python package in development
+
+*More info soon.*
+
+
+## Installation with `pip`
+Like you would install any other Python package, use `pip`, `poetry`, `pipenv` or your weapon of choice.
+```
+$ pip install <package>
+```
+
+
+## Usage and examples
+
+#### Use-case
---
+++ packages/utcnow/utcnow-0.0.0.tar.gz/utcnow-0.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['utcnow']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'utcnow',
+    'version': '0.0.0',
+    'description': 'under development',
+    'long_description': '# python package in development\n\n*More info soon.*\n\n\n## Installation with `pip`\nLike you would install any other Python package, use `pip`, `poetry`, `pipenv` or your weapon of choice.\n```\n$ pip install <package>\n```\n\n\n## Usage and examples\n\n#### Use-case\n',
+    'author': 'Carl Oscar Aaro',
+    'author_email': 'hello@carloscar.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/kalaspuff',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
