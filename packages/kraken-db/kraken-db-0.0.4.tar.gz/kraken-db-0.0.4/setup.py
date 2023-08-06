ffe73d1914658545f892de2a068487cafe79d5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:08 +0000

    Add package kraken-db/0.0.4/kraken-db-0.0.4.tar.gz

---
+++ packages/kraken-db/kraken-db-0.0.4.tar.gz/kraken-db-0.0.4/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="kraken-db",
+    version="0.0.4",
+    description="Kraken db",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tactik8/kraken_db",
+    author="Tactik8",
+    author_email="info@tactik8.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=['kraken_db'],
+    include_package_data=True,
+    install_requires=['flatten-dict', 'firebase-admin'],
+    
+)
