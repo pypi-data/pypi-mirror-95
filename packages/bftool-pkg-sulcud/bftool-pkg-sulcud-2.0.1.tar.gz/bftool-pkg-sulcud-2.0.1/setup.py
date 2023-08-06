7b380008c8f8e7a2c8998d336735279d4fc58863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:46 +0000

    Add package bftool-pkg-sulcud/2.0.1/bftool-pkg-sulcud-2.0.1.tar.gz

---
+++ packages/bftool-pkg-sulcud/bftool-pkg-sulcud-2.0.1.tar.gz/bftool-pkg-sulcud-2.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bftool-pkg-sulcud",
+    version="2.0.1",
+    author="Antonio Donis",
+    author_email="antoniojosedonishung@gmail.com",
+    description="A python module and script to execute functions inside a custom pool that distribute workers between "
+                "threads and processes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shoriwe/bftool",
+    packages=["bftool"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
