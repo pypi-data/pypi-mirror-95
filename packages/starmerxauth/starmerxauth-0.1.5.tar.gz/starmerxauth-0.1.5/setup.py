4b34262f5e2955d58337cabcada2539286b572c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:42 +0000

    Add package starmerxauth/0.1.5/starmerxauth-0.1.5.tar.gz

---
+++ packages/starmerxauth/starmerxauth-0.1.5.tar.gz/starmerxauth-0.1.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="starmerxauth",
+    version="0.1.5",
+    author="yang",
+    author_email="yangjuan@starmerx.com",
+    description="starmerx verify jwt",
+    license="MIT",
+    url="",  # github
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        #"License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+    ],
+    install_requires=[
+        "PyJWT== 1.7.1",
+        "cryptography== 3.3.2"
+    ],
+    zip_safe=True,
+)
