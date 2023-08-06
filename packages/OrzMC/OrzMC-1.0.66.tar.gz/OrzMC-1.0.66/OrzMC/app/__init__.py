1eae3226d29618879bff06aec1daf414ffdae28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:55 +0000

    Add package OrzMC/1.0.66/OrzMC-1.0.66.tar.gz

---
+++ packages/OrzMC/OrzMC-1.0.66.tar.gz/OrzMC-1.0.66/OrzMC/app/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+
+__all__ = [
+    'OrzMC',
+    'Game',
+    'Client',
+    'Server',
+    'Config',
+    'Console'
+    'Constants',
+    'Downloader',
+    'Tools',
+    'Version',
+    'Nginx',
+    'Daemon',
+]
