a5a997cb51dbdd80af0710be73060fb2a8316cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:39 +0000

    Add package OrzMC/1.0.91/OrzMC-1.0.91.tar.gz

---
+++ packages/OrzMC/OrzMC-1.0.91.tar.gz/OrzMC-1.0.91/OrzMC/core/Spigot.py
@@ -0,0 +1,8 @@
+# -*- coding: utf8 -*-
+class Spigot:
+
+    build_tool_jar = 'https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar'
+    
+    def __init__(self, version):
+        self.server_version = version
+
