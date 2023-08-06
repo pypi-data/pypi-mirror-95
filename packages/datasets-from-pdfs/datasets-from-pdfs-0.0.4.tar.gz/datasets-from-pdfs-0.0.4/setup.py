ab03176ffca6f18e207bfc9c5b4ecbe871d9c898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:03 +0000

    Add package datasets-from-pdfs/0.0.4/datasets-from-pdfs-0.0.4.tar.gz

---
+++ packages/datasets-from-pdfs/datasets-from-pdfs-0.0.4.tar.gz/datasets-from-pdfs-0.0.4/setup.py
@@ -0,0 +1,45 @@
+#     datasets-from-pdfs - Convert single or mass PDFs to datasets
+#     Copyright (C) 2021  Daniel Whitten - danieljwhitten@gmail.com
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datasets-from-pdfs",
+    version="0.0.4",
+    author="Daniel Whitten",
+    author_email="danieljwhitten@gmail.com",
+    description='''A tool to convert single or mass PDFs to datasets for
+    language analysis, including a toolbox of text and NLP pre-processing options''',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/danieljwhitten/datasets-from-pdfs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+) ",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "datasets-from-pdfs = datasets_from_pdfs.__main__:main"
+        ]
+    },
+
+)
