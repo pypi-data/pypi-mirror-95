3f711109fe61b74cb00e3b9462fff523b681197c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:09 +0000

    Add package ixdat/0.0.5/ixdat-0.0.5-py3-none-any.whl

---
+++ packages/ixdat/ixdat-0.0.5-py3-none-any.whl/ixdat/readers/__init__.py
@@ -0,0 +1,19 @@
+"""Import readers and build the READER_CLASSES dictionary for direct import
+
+Constants:
+    READER_CLASSES (dict): Dictionary of {reader_name: ReaderClass} where
+        reader_name is the name of the backend (like "directory") and ReaderClass
+        is the reader class for parsing files.
+"""
+from ..techniques import TECHNIQUE_CLASSES
+from .ec_ms_pkl import EC_MS_CONVERTER
+from .zilien import ZilienTSVReader
+from .biologic import BiologicMPTReader
+from .ixdat_csv import IxdatCSVReader
+
+READER_CLASSES = {
+    "EC_MS": EC_MS_CONVERTER,
+    "zilien": ZilienTSVReader,
+    "biologic": BiologicMPTReader,
+    "ixdat": IxdatCSVReader,
+}
