60818cded0c59ba9c7d924328c5b13aa8bdc1cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:45 +0000

    Add package scholarly/1.1.0/scholarly-1.1.0.tar.gz

---
+++ packages/scholarly/scholarly-1.1.0.tar.gz/scholarly-1.1.0/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='scholarly',
+    version='1.1.0',
+    author='Steven A. Cholewiak, Panos Ipeirotis, Victor Silva',
+    author_email='steven@cholewiak.com, panos@stern.nyu.edu, vsilva@ualberta.ca',
+    description='Simple access to Google Scholar authors and citations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Unlicense',
+
+    url='https://github.com/scholarly-python-package/scholarly',
+    packages=setuptools.find_packages(),
+    keywords=['Google Scholar', 'academics', 'citations'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    install_requires=['arrow',
+                      'beautifulsoup4',
+                      'bibtexparser',
+                      'requests[security]',
+                      'requests[socks]',
+                      'stem',
+                      'fake_useragent',
+                      'PySocks',
+                      'selenium',
+                      'python-dotenv',
+                      'free-proxy',
+                      'sphinx_rtd_theme',
+                      'typing_extensions'
+                      ],
+    test_suite="test_module.py"
+)
