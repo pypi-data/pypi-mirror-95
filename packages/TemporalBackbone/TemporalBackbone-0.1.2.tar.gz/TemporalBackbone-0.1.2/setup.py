cc4a411181b55e20c2bc9aaa448e1e16be598da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:46 +0000

    Add package TemporalBackbone/0.1.2/TemporalBackbone-0.1.2.tar.gz

---
+++ packages/TemporalBackbone/TemporalBackbone-0.1.2.tar.gz/TemporalBackbone-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TemporalBackbone",
+    version="0.1.2",
+    author="Matthieu Nadini",
+    author_email="matthieu.nadini@gmail.com",
+    description="A tool to detect the backbone in temporal networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matnado/TemporalBackbone",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+)
