b6b07bbea79cc9d8f1d7dd3f3c659f4fb906c815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:56 +0000

    Add package riegocloud/0.0.1/riegocloud-0.0.1.tar.gz

---
+++ packages/riegocloud/riegocloud-0.0.1.tar.gz/riegocloud-0.0.1/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+from riegocloud.__init__ import __version__
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="riegocloud",
+    version=__version__,
+    author="Stephan Winter",
+    author_email="riego@finca-panorama.es",
+    url="https://github.com/py-steph/riegocloud",
+    description="Riego Cloud Service",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'ConfigArgParse',
+        'aiohttp',
+        'aiohttp_jinja2',
+        'uvloop; sys_platform == "linux"',
+        'cchardet',
+        'asyncssh',
+    ],
+    include_package_data=True,
+    scripts=['bin/setup_service.sh'],
+    entry_points={
+        'console_scripts': ['riego-cloud=riegocloud.app:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Framework :: AsyncIO",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.7',
+    zip_safe=False,
+)
