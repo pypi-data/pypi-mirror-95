4c9defaad440b506cfdb3b522863a398041acb19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:39 +0000

    Add package ultraplex/1.0.4.3/ultraplex-1.0.4.3.tar.gz

---
+++ packages/ultraplex/ultraplex-1.0.4.3.tar.gz/ultraplex-1.0.4.3/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="ultraplex",
+    version="1.0.4.3",
+    description="fastq demultiplexer",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ulelab/ultraplex.git",
+    author="Oscar Wilkins",
+    author_email="oscar.wilkins@crick.ac.uk",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["ultraplex"],
+    include_package_data=True,
+    install_requires=["cutadapt==2.10"],
+    entry_points={
+        "console_scripts": [
+            "ultraplex = ultraplex.__main__:main",
+        ]
+    },
+)
