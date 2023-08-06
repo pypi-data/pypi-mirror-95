44a4116b8f4bced7afec9f1b9eaa388eeed4bbcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:32 +0000

    Add package greeclimate/0.11.5/greeclimate-0.11.5.tar.gz

---
+++ packages/greeclimate/greeclimate-0.11.5.tar.gz/greeclimate-0.11.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="greeclimate",
+    version="0.11.5",
+    python_requires=">=3.6",
+    install_requires=requirements,
+    author="Clifford Roche",
+    author_email="",
+    description="Discover, connect and control Gree based minisplit systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cmroche/greeclimate",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
