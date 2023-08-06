944f51c7c59a76b8d7f6cca5a2852f6aefb7b9a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:07 +0000

    Add package musamusa/0.0.1.dev5/musamusa-0.0.1.dev5.tar.gz

---
+++ packages/musamusa/musamusa-0.0.1.dev5.tar.gz/musamusa-0.0.1.dev5/musamusa/aboutproject.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################################################################
+#    musamusa Copyright (C) 2021 Suizokukan
+#    Contact: suizokukan _A.T._ orange dot fr
+#
+#    This file is part of musamusa.
+#    musamusa is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    musamusa is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with musamusa.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+"""
+   MusaMusa project : musamusa/aboutproject.py
+
+   Global constants defining the project : version number, author, ...
+
+   ____________________________________________________________________________
+
+
+   * __author__      : (str) project's initial author
+   * __email__       : (str) project's official email
+   * __license__     : (str) project's license
+   * __location__    : (str) project's official site
+   * __projectname__ : (str) Official project's name
+   * __version__     : (str) project's version
+                       To propagate the project's version across all the 
+                       project, see the propagate_versionnumber.py script.
+
+   * THIRDPARTIES_DEPENDENCIES : (list of str) name of the required third parties dependencies
+"""
+
+__author__ = "Xavier Faure (suizokukan)"
+__email__ = "suizokukan@orange.fr"
+__license__ = "GPL-3.0"
+__location__ = "github.com/suizokukan/musamusa"
+__projectname__ = "MusaMusa"
+__version__ = "0.0.1-dev5"
+
+THIRDPARTIES_DEPENDENCIES = ("rich",)
