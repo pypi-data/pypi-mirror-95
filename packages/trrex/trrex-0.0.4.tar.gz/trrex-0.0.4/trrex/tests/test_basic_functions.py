1115d1e1ab1333fb06e1a4a0d6d78198d504e3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:20 +0000

    Add package trrex/0.0.4/trrex-0.0.4.tar.gz

---
+++ packages/trrex/trrex-0.0.4.tar.gz/trrex-0.0.4/trrex/tests/test_basic_functions.py
@@ -0,0 +1,89 @@
+import re
+from string import ascii_letters, punctuation
+
+from hypothesis import example, given
+from hypothesis.strategies import lists, text
+
+from trrex import make
+
+
+def compile(lst, flags=0, left=r"\b", right=r"\b"):
+    return re.compile(make(lst, left, right), flags)
+
+
+def test_findall():
+    words = ["baby", "bad", "bat"]
+    pattern = compile(words)
+    assert pattern.findall("The baby was bitten by the bad bat") == words
+
+
+def test_findall_ignore_case():
+    words = ["baby", "bad", "bat"]
+    pattern = compile(words, flags=re.IGNORECASE)
+    actual = pattern.findall("The BABY was bitten by the bAD Bat")
+    assert words == [w.lower() for w in actual]
+
+
+def test_findall_whitespace():
+    words = ["Ray Steam"]
+    pattern = compile(words)
+    actual = pattern.findall("Ray Steam is Steamboy")
+    assert words == actual
+
+
+def test_match():
+    name = "Ray Steam"
+    pattern = compile([name, "Edward Elric"])
+    match = pattern.match(name)
+    assert match is not None
+
+
+def test_sub():
+    replacements = {"Ray Steam": "Steamboy"}
+    pattern = compile(list(replacements.keys()))
+
+    def replace(match):
+        return replacements.get(match.group(), "")
+
+    actual = pattern.sub(replace, "The kid Ray Steam save the day")
+    assert "The kid Steamboy save the day" == actual
+
+
+def test_findall_punctuation():
+    words = ["bab.y", "b#ad", "b?at"]
+    pattern = compile(words)
+    assert pattern.findall("The bab.y was bitten by the b#ad b?at") == words
+
+
+def test_match_punctuation():
+    word = ":"
+    pattern = compile([word], left="", right="")
+    match = pattern.match(word)
+    assert match is not None
+
+
+def test_findall_emoticon():
+    emoticons = [":)", ":D", ":("]
+    pattern = compile(emoticons, left=r"(?<!\w)", right=r"(?!\w)")
+    assert pattern.findall("The smile :), and the laugh :D and the sad :(") == emoticons
+
+
+@given(text(alphabet=ascii_letters, min_size=1))
+def test_single_string_match(s):
+    pattern = compile([s])
+    assert pattern.match(s) is not None
+
+
+@example(lst=["B", "BA", "B"])
+@given(lists(text(alphabet=ascii_letters, min_size=1)))
+def test_multiple_string_match(lst):
+    pattern = compile(lst)
+    for word in lst:
+        assert pattern.match(word) is not None
+
+
+@given(lists(text(alphabet=ascii_letters + punctuation, min_size=1)))
+def test_multiple_string_match_punctuation(lst):
+    pattern = compile(lst, left="", right="")
+    for word in lst:
+        assert pattern.match(word) is not None
---
+++ packages/trrex/trrex-0.0.4.tar.gz/trrex-0.0.4/trrex/trrex.py
@@ -0,0 +1,119 @@
+from io import StringIO
+from re import escape
+from typing import Dict, Sequence, Tuple
+
+_OPTION: Tuple[Tuple, Dict] = (("?", False), {})
+_OPEN_PARENTHESIS: Tuple[Tuple, Dict] = (("(?:", False), {})
+_CLOSE_PARENTHESIS: Tuple[Tuple, Dict] = ((")", False), {})
+_OPEN_BRACKETS: Tuple[Tuple, Dict] = (("[", False), {})
+_CLOSE_BRACKETS: Tuple[Tuple, Dict] = (("]", False), {})
+_ALTERNATION: Tuple[Tuple, Dict] = (("|", False), {})
+
+
+class _Trie:
+    def __init__(self, words, left=r"\b", right=r"\b"):
+
+        data = {}
+        self.left = left
+        self.right = right
+        self.root = {(left, False): data}
+        for word in set(words):
+            ref = data
+            for char in map(escape, word[:-1]):
+                fk, tk = (char, False), (char, True)
+                key = tk if tk in ref else fk
+                ref[key] = key in ref and ref[key] or {}
+                ref = ref[key]
+            if word:
+                char = escape(word[-1])
+                ref[(char, True)] = ref.pop((char, False), {})  # mark as end
+
+    def _to_regex(self):
+
+        stack = [*self.root.items()]
+        cumulative = StringIO()
+
+        while stack:
+
+            (char, end), children = stack.pop()
+            cumulative.write(char)
+
+            if not children:
+                continue  # skip
+
+            if end:
+                stack.append(_OPTION)
+
+            single, multiple = [], []
+            for key, values in children.items():
+                if values:
+                    multiple.append((key, values))
+                else:
+                    single.append((key, values))
+
+            requires_character_set = len(single) > 1
+            requires_alternation = (multiple and single) or len(multiple) > 1
+            requires_group = requires_alternation or (end and multiple)
+
+            if requires_group:
+                stack.append(_CLOSE_PARENTHESIS)
+
+            if requires_character_set:
+                stack.append(_CLOSE_BRACKETS)
+
+            for child in single:
+                stack.append(child)
+
+            if requires_character_set:
+                stack.append(_OPEN_BRACKETS)
+
+            if requires_alternation and single:
+                stack.append(_ALTERNATION)
+
+            if multiple:
+                head, *tail = multiple
+                stack.append(head)
+
+                for child in tail:
+                    stack.append(_ALTERNATION)
+                    stack.append(child)
+
+            if requires_group:
+                stack.append(_OPEN_PARENTHESIS)
+
+        return cumulative.getvalue()
+
+    def make(self):
+        return f"{self._to_regex()}{self.right}"
+
+
+def make(words: Sequence[str], prefix: str = r"\b", suffix: str = r"\b"):
+    """
+    Create a string that represents a regular expression object from a set of strings
+
+    Parameters
+    ----------
+    words : Sequence[str]
+        Sequence or set of strings to be made into a regex
+
+    prefix : str, optional
+           Left delimiter for pattern
+
+    suffix : str, optional
+            Right delimiter for pattern
+
+    Returns
+    -------
+    String
+            A string representing a regular expression pattern
+
+    Examples
+    --------
+    >>> import re
+    >>> import trrex as tx
+    >>> pattern = tx.make(["baby", "bat", "bad"])
+    >>> re.findall(pattern, "The baby was scared by the bad bat.")
+    ['baby', 'bad', 'bat']
+    """
+
+    return _Trie(words, left=prefix, right=suffix).make()
