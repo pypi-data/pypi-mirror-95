65b2dd1ad14aa647dd678d3fba1662f64f562131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:35 +0000

    Add package modin-spreadsheet/0.1.0/modin-spreadsheet-0.1.0.tar.gz

---
+++ packages/modin-spreadsheet/modin-spreadsheet-0.1.0.tar.gz/modin-spreadsheet-0.1.0/modin_spreadsheet/__init__.py
@@ -0,0 +1,42 @@
+from .grid import (
+    enable,
+    disable,
+    set_defaults,
+    on,
+    off,
+    set_grid_option,
+    show_grid,
+    SpreadsheetWidget,
+)
+from ._version import get_versions
+
+
+def _jupyter_nbextension_paths():
+    return [
+        {
+            "section": "notebook",
+            "src": "static",
+            "dest": "modin_spreadsheet",
+            "require": "modin_spreadsheet/extension",
+        }
+    ]
+
+
+__all__ = [
+    "enable",
+    "disable",
+    "set_defaults",
+    "on",
+    "off",
+    "set_grid_option",
+    "show_grid",
+    "SpreadsheetWidget",
+]
+
+__version__ = get_versions()["version"]
+del get_versions
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
