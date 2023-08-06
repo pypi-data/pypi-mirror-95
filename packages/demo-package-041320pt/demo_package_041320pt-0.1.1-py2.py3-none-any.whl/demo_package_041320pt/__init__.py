75b3a83407f586ebcf0ef982ce5f9aeef39d0626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:28 +0000

    Add package demo-package-041320pt/0.1.1/demo_package_041320pt-0.1.1-py2.py3-none-any.whl

---
+++ packages/demo-package-041320pt/demo_package_041320pt-0.1.1-py2.py3-none-any.whl/demo_package_041320pt/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for demo package 041320pt."""
+
+__author__ = """James Irving"""
+__email__ = 'james.irving@flatironschool.com'
+__version__ = '0.1.1'
+
+import demo_package_041320pt.lz as imports
+import demo_package_041320pt.my_functions as mf
