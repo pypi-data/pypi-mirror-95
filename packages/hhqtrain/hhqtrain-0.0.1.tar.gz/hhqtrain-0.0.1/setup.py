c85102e1089527edbe4ee95d8233f7b5c57c57e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:55 +0000

    Add package hhqtrain/0.0.1/hhqtrain-0.0.1.tar.gz

---
+++ packages/hhqtrain/hhqtrain-0.0.1.tar.gz/hhqtrain-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='hhqtrain',
+      version='0.0.1',
+      description='clinical trial information retriver',
+      url='http://github.com/',
+      author='hhq',
+      author_email='779613975@qq.com',
+      license='MIT',
+      packages=['hhqtrain'],
+      zip_safe=False)
