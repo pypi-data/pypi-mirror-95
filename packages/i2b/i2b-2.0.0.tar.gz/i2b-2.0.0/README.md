ea09ecf317633c44784a2760c7f0189cdc58e9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:00 +0000

    Add package i2b/2.0.0/i2b-2.0.0.tar.gz

---
+++ packages/i2b/i2b-2.0.0.tar.gz/i2b-2.0.0/README.md
@@ -0,0 +1,31 @@
+# IMAGE TO _BASE64 STRING_
+
+## Convert Image into base64 format.
+
+### Supprted image format is:
+
+* _**jpg**_
+* _**png**_
+* _**jpeg**_
+* _**JPG**_
+* _**JPEG**_
+* _**TIF**_
+
+### Install Package
+Install package from
+[pypi.org](https://pypi.org/project/img2b64/)
+```
+pip install img2b64
+```
+### Usage
+
+```
+img2b64.py -h
+usage: img2b64.py [-h] [-i IMG]
+
+Convert Image into base64 format, supported format is jpg and png.
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -i IMG, --img IMG  Input image or image path to convert into base64
+```
---
+++ packages/i2b/i2b-2.0.0.tar.gz/i2b-2.0.0/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='i2b',
+    version='2.0.0',
+    author='Sachin Chavan',
+    author_email='sachinewx@gmail.com',
+    description='Convert image into base64 string and store into the txt file',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='image base64 jpg png jpeg JPG JPEG TIF',
+    url="https://github.com/sachinchavan9/image-encode-decode",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['i2b=img2b64.img2b64:main'],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Artistic Software",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)"
+    ],
+    python_requires='>=3.5',
+    include_package_data=True,
+    zip_safe=False
+)
