cbeb82ac53df3cfa5ba0fc1c651cb7e6c32e2513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:33 +0000

    Add package electronvolt/1.3.0/electronvolt-1.3.0.tar.gz

---
+++ packages/electronvolt/electronvolt-1.3.0.tar.gz/electronvolt-1.3.0/setup.py
@@ -0,0 +1,29 @@
+# test electronvolt.py using command I
+# update README.md
+# update version number in setuptools.setup
+# rm -r __pycache__
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+# rm -r build dist *.egg-info
+# pip install electronvolt -U
+# test module in nteract or terminal at home directory
+# git commit and push
+# pypi.org/project/electronvolt
+# github.com/dw61/electronvolt
+
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="electronvolt",
+    version="1.3.0",
+    author_email="lw7jz@virginia.edu",
+    description="A physical quantity calculator.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dw61/electronvolt",
+    py_modules=["electronvolt"],
+    python_requires='>=3.6',
+)
