701cb5d1cbc16e73d39ac28eabaad978c6174c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:09 +0000

    Add package colab-frp/0.0.1/colab_frp-0.0.1.tar.gz

---
+++ packages/colab-frp/colab_frp-0.0.1.tar.gz/colab_frp-0.0.1/colab-frp/frp_client.py
@@ -0,0 +1,2 @@
+def frp_connect(server_host,server_port,token):
+    print(server_host)
---
+++ packages/colab-frp/colab_frp-0.0.1.tar.gz/colab_frp-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="colab_frp",
+    version="0.0.1",
+    author="argszero",
+    author_email="argszero@gmail.com",
+    description="Google colab frp connector",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/WassimBenzarti/colab-ssh-connector.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True
+)
