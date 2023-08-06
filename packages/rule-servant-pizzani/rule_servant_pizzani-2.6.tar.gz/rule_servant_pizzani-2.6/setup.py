b4b373b670b4c9813a4b1470939c772a7db11abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:05 +0000

    Add package rule-servant-pizzani/2.6/rule_servant_pizzani-2.6.tar.gz

---
+++ packages/rule-servant-pizzani/rule_servant_pizzani-2.6.tar.gz/rule_servant_pizzani-2.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="rule_servant_pizzani",
+	version = "2.6",
+	description = "Rule Servant",
+	packages = ["rule_servant_pizzani"],
+	author = "Bruno Pizzani Mendoza",
+	zip_safe=False)
