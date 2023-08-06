e8620c8e7eee7067880e23b63554590a19e6278e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:09 +0000

    Add package librelingo-types/0.1.0/librelingo_types-0.1.0.tar.gz

---
+++ packages/librelingo-types/librelingo_types-0.1.0.tar.gz/librelingo_types-0.1.0/librelingo_types/__init__.py
@@ -0,0 +1,7 @@
+"""
+Data types to be used in Python packages for LibreLingo
+"""
+
+import librelingo_types.data_types
+
+__version__ = '0.1.0'
---
+++ packages/librelingo-types/librelingo_types-0.1.0.tar.gz/librelingo_types-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['librelingo_types']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'librelingo-types',
+    'version': '0.1.0',
+    'description': 'Data types to be used in Python packages for LibreLingo',
+    'long_description': None,
+    'author': 'Dániel Kántor',
+    'author_email': 'git@daniel-kantor.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
