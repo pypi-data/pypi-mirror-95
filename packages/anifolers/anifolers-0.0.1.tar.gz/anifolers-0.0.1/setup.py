3c33d5ae61186423520b9530992dc9ce68e34471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:51 +0000

    Add package anifolers/0.0.1/anifolers-0.0.1.tar.gz

---
+++ packages/anifolers/anifolers-0.0.1.tar.gz/anifolers-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+with open("README.md", "r")as fh :
+    long_description = fh.read()
+setup(
+    name='anifolers',
+    version='0.0.1',
+    url="https://github.com/RobiMez/Anicons_py",
+    author="robimez",
+    description='Anime icon creation tool!',
+    py_modules=['anicons'],
+    package_dir={'':'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires = [
+        "jikanpy" ,
+        "pyfiglet" ,
+        "PyInquirer" ,
+        "PIL",
+    ]
+)
