7d5dffe3c76f453933f332fde3e1d9fe0ca69aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:59 +0000

    Add package json-converter/0.4.0/json-converter-0.4.0.tar.gz

---
+++ packages/json-converter/json-converter-0.4.0.tar.gz/json-converter-0.4.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="json-converter",
+    version="0.4.0",
+    description="json-converter is a tool for translating/converting a JSON document into another JSON document "
+                "with a different structure.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ebi-ait/json-converter",
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"
+    ],
+)
