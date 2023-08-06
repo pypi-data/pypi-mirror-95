8f10646457e580da2ab70aa015c5dc7f0517c675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:19 +0000

    Add package raisin/0.0.21/raisin-0.0.21-py3-none-any.whl

---
+++ packages/raisin/raisin-0.0.21-py3-none-any.whl/raisin/serialization/compress.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+|=================================================|
+| Compression sans perte inteligente des donnees. |
+| N'est pas encore implementee                    |
+|=================================================|
+"""
+
+def compress_gen(gen, **kwargs):
+	yield from gen
