557f05a3a7575477820f0698437efdacc59ed032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:10 +0000

    Add package re-common/0.1.50/re_common-0.1.50.tar.gz

---
+++ packages/re-common/re_common-0.1.50.tar.gz/re_common-0.1.50/re_common/baselibrary/tools/move_mongo/use_mv.py
@@ -0,0 +1,81 @@
+import click
+
+###########################################
+# 同项目调用基础包
+import datetime
+import gzip
+import json
+import os
+import sys
+import time
+
+filepath = os.path.abspath(__file__)
+pathlist = filepath.split(os.sep)
+pathlist = pathlist[:-5]
+TopPath = os.sep.join(pathlist)
+sys.path.insert(0, TopPath)
+print(TopPath)
+############################################
+
+
+from re_common.baselibrary.tools.move_mongo.move_mongo_table import MoveMongodbColl, MoveMongodbThreadRun, Configs
+
+#
+# class Configs(object):
+#
+#     def __init__(self):
+#         self.db3_path = r"D:\config\mvmg\db3\test_images.db3"
+#         self.db3_encoding = "utf-8"
+#         self.mgdb_conn = "mongodb://192.168.31.30:32417/"
+#         self.mgdb_conn_motor = "mongodb://192.168.31.30:32417/htmljson.wanfang_ref?authSource=htmljson"
+#         self.mgdb_db = "htmljson"
+#         self.mgdb_col = "wanfang_ref"
+#
+#         self.mgdb_conn2_motor = "mongodb://cjrw:vipdatacenter@192.168.31.243:32920,192.168.31.206:32920,192.168.31.208:32920/?authSource=htmljson"
+#         self.mgdb_db2 = "htmljson"
+#         self.mgdb_col2 = "wanfang_ref"
+#
+#         self.mq_name = "mongodb.move.send"
+#         self.mq_name_work = "mongodb.move.worker"
+#
+#         self.error_dir = r"D:\config\mvmg\log"
+
+
+conf = Configs()
+
+
+def one_init():
+    # 第一步 初始化数据到db3文件
+    MoveMongodbColl(conf).one_init()
+
+
+# 第二步 开始转移 使用send_work_recv结构 共开三个进程
+
+def send():
+    MoveMongodbColl(conf).two_send()
+
+
+def works():
+    MoveMongodbThreadRun(1, conf).run()
+
+
+def recv():
+    MoveMongodbColl(conf).two_recv()
+
+
+@click.command()
+@click.option('--name',
+              help='func name')
+def main(name):
+    if name == "send":
+        send()
+    elif name == "one_init":
+        one_init()
+    elif name == "works":
+        works()
+    elif name == "recv":
+        recv()
+
+
+if __name__ == '__main__':
+    main()
