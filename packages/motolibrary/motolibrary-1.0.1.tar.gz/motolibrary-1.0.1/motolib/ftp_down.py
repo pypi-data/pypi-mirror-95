a78c842f09f0d2da81db324565a4c1b43bf6ad34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:51 +0000

    Add package motolibrary/1.0.1/motolibrary-1.0.1.tar.gz

---
+++ packages/motolibrary/motolibrary-1.0.1.tar.gz/motolibrary-1.0.1/motolib/ftp_down.py
@@ -0,0 +1,61 @@
+# ftp_down.py
+# Dependencies: ftplib, pandas
+
+import io
+import os.path
+import ftplib
+import pandas as pd
+from customsol_pkg.errors import FTPConnectionError, FTPDownloadError, FTPEncodingError
+
+
+def ftp_download(hostname, username, password, filename_filter, download_directory='.'):
+    """
+    ----------------------------------------------------------------
+    Fetch information using FTP and save file to local machine.
+    ----------------------------------------------------------------
+    Parameters:
+        hostname : string
+            FTP host name
+        username : string
+            FTP login username
+        password : string
+            FTP login password
+        filename_filter : string
+            Name of file to download from FTP
+        download_directory: string
+            Directory to download file to. CWD is default
+    Returns:
+       NONE
+    Side Effects:
+        Writes and deletes feed file to local machine (in same dir as script)
+            Output file will have same name as FTP file
+            (ie. same as filename_filter parameter)
+    ----------------------------------------------------------------
+    """
+    try:
+        # Login to FTP
+        print(f"Initializing FTP Download from {hostname}...")
+        ftp = ftplib.FTP(hostname)
+    except OSError:
+        raise FTPConnectionError('Unable to establish connect to host')
+    try:
+        print("Attempting FTP Login...")
+        ftp.login(username, password)
+        print("FTP Login Successful!")
+    except ftplib.error_perm as e:
+        raise FTPConnectionError('Incorrect Login.')
+
+    try:
+        ftp.encoding = "utf-8"
+    except Exception as e:
+        raise FTPEncodingError('Unable to enforce UTF-8 Encoding.')
+
+    try:
+        print(f"Starting Download of {filename_filter}...")
+        # Downloads a copy of the FTP file, saving in current dir with same name as filename_filter
+        ftp.retrbinary("RETR " + filename_filter, open(os.path.join(download_directory, filename_filter), 'wb').write)
+        print("File Downloaded Successfully!\n")
+        ftp.quit()
+
+    except ftplib.error_perm as err:
+        raise FTPDownloadError('Unable to open file.')
---
+++ packages/motolibrary/motolibrary-1.0.1.tar.gz/motolibrary-1.0.1/motolib/ftp_up.py
@@ -0,0 +1,57 @@
+# up_ftp.py
+# Dependencies: ftplib
+
+import os
+import ftplib
+from customsol_pkg.errors import FTPConnectionError, FTPUploadError, FTPEncodingError
+
+def ftp_upload_file(hostname, username, password, file_name, upload_name="NOUPLOADNAME"):
+    """
+    -------------------------------------------------------
+    Uploads a given file to FTP
+    -------------------------------------------------------
+    Parameters:
+        hostname : string
+            FTP host name
+        username : string
+            FTP login username
+        password : string
+            FTP login password
+        file_name : string
+            Name of file in dir that will be uploaded
+        upload_name : string
+            !! MUST INCLUDE EXTENSION !!
+            (Optional) Specifies name of FTP upload
+                ie. can be uploaded with different name than local file
+    Returns:
+        True if supplement has been uploaded to FTP
+        False if supplement did not upload to FTP
+    ------------------------------------------------------
+    """
+    try:
+        # Login to FTP
+        print(f"Initializing FTP Download from {hostname}...")
+        ftp = ftplib.FTP(hostname)
+    except OSError:
+        raise FTPConnectionError('Unable to establish connect to host')
+    try:
+        print("Attempting FTP Login...")
+        ftp.login(username, password)
+        print("FTP Login Successful!")
+    except ftplib.error_perm as e:
+        raise FTPConnectionError('Incorrect Login.')
+    try:
+        print(f"Starting Upload of {file_name}...")
+
+        # Store file to FTP with appropriate upload name
+        ftp.storbinary("STOR " + (file_name if upload_name == "NOUPLOADNAME" else upload_name), open(file_name, 'rb'))
+
+        if upload_name == "NONE":
+            print("File Upload Successful!")
+        else:
+            print(f"File Upload Successful! Saved on FTP as {upload_name}")
+
+        ftp.quit()
+
+    except ftplib.all_errors as err:
+        raise FTPUploadError("ERROR: Could not upload to FTP.")
