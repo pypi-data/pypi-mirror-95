30f54334fbf2b026e405d0745ab18e93bcd46676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:18 +0000

    Add package bscscan-python/0.0.1/bscscan_python-0.0.1-py3-none-any.whl

---
+++ packages/bscscan-python/bscscan_python-0.0.1-py3-none-any.whl/bscscan/__init__.py
@@ -0,0 +1,5 @@
+from .bscscan import BscScan
+from .modules.accounts import Accounts as accounts
+from .modules.contracts import Contracts as contracts
+from .modules.stats import Stats as stats
+from .modules.tokens import Tokens as tokens
