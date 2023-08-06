cba108618e2304f273a008c9db513691889305fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:12 +0000

    Add package urbangrammar-graphics/1.1.0/urbangrammar_graphics-1.1.0.tar.gz

---
+++ packages/urbangrammar-graphics/urbangrammar_graphics-1.1.0.tar.gz/urbangrammar_graphics-1.1.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="urbangrammar_graphics",
+    version="1.1.0",
+    author="Martin Fleischmann",
+    author_email="martin@martinfleischmann.net",
+    python_requires=">=3.6",
+    install_requires=["matplotlib", "seaborn", "numpy", "contextily"],
+    description="Visual style for Urban Grammar AI research project",
+    url="https://github.com/urbangrammarai/graphics",
+)
