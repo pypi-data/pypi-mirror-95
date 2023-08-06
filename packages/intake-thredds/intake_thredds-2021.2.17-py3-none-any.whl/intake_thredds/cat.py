13ad8eccda6d66be95d7b05b19451817b706ece9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:46 +0000

    Add package intake-thredds/2021.2.17/intake_thredds-2021.2.17-py3-none-any.whl

---
+++ packages/intake-thredds/intake_thredds-2021.2.17-py3-none-any.whl/intake_thredds/cat.py
@@ -0,0 +1,96 @@
+from intake.catalog import Catalog
+from intake.catalog.local import LocalCatalogEntry
+
+
+class ThreddsCatalog(Catalog):
+    """Intake catalog interface to a thredds catalog.
+
+    Parameters
+    ----------
+    url : str
+        Location of thredds catalog.
+    driver : str
+        Select driver to access data. Choose from 'netcdf' and 'opendap'.
+    **kwargs :
+        Additional keyword arguments are passed through to the
+        :py:class:`~intake.catalog.Catalog` base class.
+
+    Examples
+    --------
+    >>> import intake
+    >>> cat_url = 'https://psl.noaa.gov/thredds/catalog/Datasets/noaa.ersst/catalog.xml'
+    >>> cat = intake.open_thredds_cat(cat_url)
+    """
+
+    name = 'thredds_cat'
+
+    def __init__(self, url: str, driver: str = 'opendap', **kwargs):
+        self.url = url
+        self.driver = driver
+        super().__init__(**kwargs)
+
+    def _load(self):
+        from siphon.catalog import TDSCatalog
+
+        if 'simplecache::' in self.url:
+            if self.driver == 'netcdf':
+                self.cache = True
+                self.url_no_simplecache = self.url.replace('simplecache::', '')
+                self.metadata.update({'fsspec_pre_url': 'simplecache::'})
+            else:
+                raise ValueError(
+                    f'simplecache requires driver="netcdf", found driver="{self.driver}".'
+                )
+        else:
+            self.cache = False
+            self.url_no_simplecache = self.url
+
+        self.cat = TDSCatalog(self.url_no_simplecache)
+        if self.name is None:
+            self.name = self.cat.catalog_name
+        self.metadata.update(self.cat.metadata)
+
+        # sub-cats
+        self._entries = {
+            r.title: LocalCatalogEntry(
+                r.title,
+                'THREDDS cat',
+                'thredds_cat',
+                True,
+                {'url': r.href},
+                [],
+                [],
+                self.metadata,
+                None,
+                catalog=self,
+            )
+            for r in self.cat.catalog_refs.values()
+        }
+
+        def access_urls(ds, self):
+            if self.driver == 'opendap':
+                driver_for_access_urls = 'OPENDAP'
+            elif self.driver == 'netcdf':
+                driver_for_access_urls = 'HTTPServer'
+            url = ds.access_urls[driver_for_access_urls]
+            if 'fsspec_pre_url' in self.metadata.keys():
+                url = f'{self.metadata["fsspec_pre_url"]}{url}'
+            return url
+
+        self._entries.update(
+            {
+                ds.name: LocalCatalogEntry(
+                    ds.name,
+                    'THREDDS data',
+                    self.driver,
+                    True,
+                    {'urlpath': access_urls(ds, self), 'chunks': {}},
+                    [],
+                    [],
+                    {},
+                    None,
+                    catalog=self,
+                )
+                for ds in self.cat.datasets.values()
+            }
+        )
---
+++ packages/intake-thredds/intake_thredds-2021.2.17-py3-none-any.whl/tests/test_cat.py
@@ -0,0 +1,72 @@
+import intake
+import intake_xarray
+import pytest
+import xarray as xr
+
+
+@pytest.fixture(scope='module')
+def thredds_cat_url():
+    """Single file thredds catalog."""
+    return 'https://psl.noaa.gov/thredds/catalog/Datasets/noaa.ersst/catalog.xml'
+
+
+def test_ThreddsCatalog_init_catalog(thredds_cat_url):
+    """Test initialization of ThreddsCatalog."""
+    cat = intake.open_thredds_cat(thredds_cat_url)
+    assert isinstance(cat, intake.catalog.Catalog)
+    assert cat.metadata == cat.cat.metadata
+    assert cat.discover()['container'] == 'catalog'
+
+    assert 'err.mnmean.v3.nc' in cat
+
+    cat = intake.open_thredds_cat(thredds_cat_url, metadata={'random_attribute': 'thredds'})
+    assert 'random_attribute' in cat.metadata
+
+
+@pytest.mark.parametrize('driver', ['netcdf', 'opendap'])
+def test_ThreddsCatalog(thredds_cat_url, driver):
+    """Test entry.to_dask() is xr.Dataset and allows opendap and netcdf as source."""
+    cat = intake.open_thredds_cat(thredds_cat_url, driver=driver)
+    entry = cat['sst.mon.19712000.ltm.v3.nc']
+    if driver == 'opendap':
+        assert isinstance(entry, intake_xarray.opendap.OpenDapSource)
+    elif driver == 'netcdf':
+        assert isinstance(entry, intake_xarray.netcdf.NetCDFSource)
+    d = entry.describe()
+    assert d['name'] == 'sst.mon.19712000.ltm.v3.nc'
+    assert d['container'] == 'xarray'
+    assert d['plugin'] == [driver]
+    if driver == 'opendap':
+        loc = 'dodsC'
+    elif driver == 'netcdf':
+        loc = 'fileServer'
+    assert (
+        d['args']['urlpath']
+        == f'https://psl.noaa.gov/thredds/{loc}/Datasets/noaa.ersst/sst.mon.19712000.ltm.v3.nc'
+    )
+    ds = entry(chunks={}).to_dask()
+    assert isinstance(ds, xr.Dataset)
+
+
+def test_ThreddsCatalog_simplecache_netcdf(thredds_cat_url):
+    """Test that ThreddsCatalog allows simplecache:: in url if netcdf as source."""
+    import os
+
+    import fsspec
+
+    fsspec.config.conf['simplecache'] = {'cache_storage': 'my_caching_folder', 'same_names': True}
+    cat = intake.open_thredds_cat(f'simplecache::{thredds_cat_url}', driver='netcdf')
+    entry = cat['sst.mon.19712000.ltm.v3.nc']
+    ds = entry(chunks={}).to_dask()
+    assert isinstance(ds, xr.Dataset)
+    # test files present
+    cached_file = 'my_caching_folder/sst.mon.19712000.ltm.v3.nc'
+    assert os.path.exists(cached_file)
+    os.remove(cached_file)
+    assert not os.path.exists(cached_file)
+
+
+def test_ThreddsCatalog_simplecache_fails_opendap(thredds_cat_url):
+    """Test that ThreddsCatalog simplecache:: in url with opendap."""
+    with pytest.raises(ValueError, match=r'simplecache requires driver="netcdf"'):
+        intake.open_thredds_cat(f'simplecache::{thredds_cat_url}', driver='opendap')
