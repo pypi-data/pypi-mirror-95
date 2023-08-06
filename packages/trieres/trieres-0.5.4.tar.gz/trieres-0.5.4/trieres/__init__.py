85d0a725c67f078c1a6fd0fb86aa62a61ee3ca12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:50 +0000

    Add package trieres/0.5.4/trieres-0.5.4.tar.gz

---
+++ packages/trieres/trieres-0.5.4.tar.gz/trieres-0.5.4/trieres/__init__.py
@@ -0,0 +1,15 @@
+#  *
+#  *                       cloudFPGA
+#  *     Copyright IBM Research, All Rights Reserved
+#  *    =============================================
+#  *     Created: Feb. 2021
+#  *     Authors: DID
+#  *
+#  *     Description:
+#  *      A initialization file for the trieres lib.
+#  *
+#  *    License:
+#  *     Apache Version 2.0
+
+from .trieres import *
+
