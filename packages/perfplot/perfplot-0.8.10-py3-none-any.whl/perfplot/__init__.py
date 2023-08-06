3119daba3a0e86e1ef025d937eb33e7d6f856343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:46 +0000

    Add package perfplot/0.8.10/perfplot-0.8.10-py3-none-any.whl

---
+++ packages/perfplot/perfplot-0.8.10-py3-none-any.whl/perfplot/__init__.py
@@ -0,0 +1,10 @@
+from perfplot.__about__ import __version__
+from perfplot.main import bench, plot, save, show
+
+__all__ = [
+    "__version__",
+    "bench",
+    "plot",
+    "show",
+    "save",
+]
