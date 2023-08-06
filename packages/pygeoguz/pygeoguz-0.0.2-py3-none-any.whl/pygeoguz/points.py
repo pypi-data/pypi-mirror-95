f633a0212ddb256ce1d437b174f0301f7fafcb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:38 +0000

    Add package pygeoguz/0.0.2/pygeoguz-0.0.2-py3-none-any.whl

---
+++ packages/pygeoguz/pygeoguz-0.0.2-py3-none-any.whl/pygeoguz/points.py
@@ -0,0 +1,17 @@
+class Point3D:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+class Point2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class PointBL:
+    def __init__(self, b, l):
+        self.b = b
+        self.l = l
