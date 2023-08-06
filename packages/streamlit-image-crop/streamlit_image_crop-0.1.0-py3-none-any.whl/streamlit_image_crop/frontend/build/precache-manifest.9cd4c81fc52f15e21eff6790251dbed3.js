910da6f4b3bb81bdea5c2f4ea7d4342d3e5160e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:33 +0000

    Add package streamlit-image-crop/0.1.0/streamlit_image_crop-0.1.0-py3-none-any.whl

---
+++ packages/streamlit-image-crop/streamlit_image_crop-0.1.0-py3-none-any.whl/streamlit_image_crop/frontend/build/precache-manifest.9cd4c81fc52f15e21eff6790251dbed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57789577ba803d51a5f6e64df4122c0e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4101c30710c50438ab7",
+    "url": "./static/css/2.1bce3852.chunk.css"
+  },
+  {
+    "revision": "b4101c30710c50438ab7",
+    "url": "./static/js/2.31d1fd16.chunk.js"
+  },
+  {
+    "revision": "9bf7777849ff7c0e7b1f4ddb70e6eb95",
+    "url": "./static/js/2.31d1fd16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678fb25b8deaea048b3d",
+    "url": "./static/js/main.4737860d.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
