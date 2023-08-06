d1185c500c4c50971e6bc33545f3e801cbab9f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:44 +0000

    Add package d3rlpy/0.70/d3rlpy-0.70-cp36-cp36m-win_amd64.whl

---
+++ packages/d3rlpy/d3rlpy-0.70-cp36-cp36m-win_amd64.whl/d3rlpy/models/torch/__init__.py
@@ -0,0 +1,60 @@
+from .encoders import Encoder, EncoderWithAction
+from .encoders import PixelEncoder, PixelEncoderWithAction
+from .encoders import VectorEncoder, VectorEncoderWithAction
+from .policies import Policy, squash_action
+from .policies import DeterministicPolicy, DeterministicResidualPolicy
+from .policies import NormalPolicy, CategoricalPolicy
+from .q_functions import DiscreteQFunction, ContinuousQFunction
+from .q_functions import DiscreteMeanQFunction, ContinuousMeanQFunction
+from .q_functions import DiscreteQRQFunction, ContinuousQRQFunction
+from .q_functions import DiscreteIQNQFunction, ContinuousIQNQFunction
+from .q_functions import DiscreteFQFQFunction, ContinuousFQFQFunction
+from .q_functions import EnsembleQFunction
+from .q_functions import EnsembleDiscreteQFunction, EnsembleContinuousQFunction
+from .q_functions import compute_max_with_n_actions
+from .q_functions import compute_max_with_n_actions_and_indices
+from .v_functions import ValueFunction
+from .imitators import ConditionalVAE
+from .imitators import Imitator, DiscreteImitator
+from .imitators import DeterministicRegressor, ProbablisticRegressor
+from .dynamics import ProbablisticDynamics, EnsembleDynamics
+from .parameters import Parameter
+
+__all__ = [
+    "Encoder",
+    "EncoderWithAction",
+    "PixelEncoder",
+    "PixelEncoderWithAction",
+    "VectorEncoder",
+    "VectorEncoderWithAction",
+    "Policy",
+    "squash_action",
+    "DeterministicPolicy",
+    "DeterministicResidualPolicy",
+    "NormalPolicy",
+    "CategoricalPolicy",
+    "DiscreteQFunction",
+    "ContinuousQFunction",
+    "DiscreteMeanQFunction",
+    "ContinuousMeanQFunction",
+    "DiscreteQRQFunction",
+    "ContinuousQRQFunction",
+    "DiscreteIQNQFunction",
+    "ContinuousIQNQFunction",
+    "DiscreteFQFQFunction",
+    "ContinuousFQFQFunction",
+    "EnsembleQFunction",
+    "EnsembleDiscreteQFunction",
+    "EnsembleContinuousQFunction",
+    "compute_max_with_n_actions",
+    "compute_max_with_n_actions_and_indices",
+    "ValueFunction",
+    "ConditionalVAE",
+    "Imitator",
+    "DiscreteImitator",
+    "DeterministicRegressor",
+    "ProbablisticRegressor",
+    "EnsembleDynamics",
+    "ProbablisticDynamics",
+    "Parameter",
+]
