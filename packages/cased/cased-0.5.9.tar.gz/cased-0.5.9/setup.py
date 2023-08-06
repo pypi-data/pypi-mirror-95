b414de22faf5494ce7a5c3d773ccc75e0980b3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:25 +0000

    Add package cased/0.5.9/cased-0.5.9.tar.gz

---
+++ packages/cased/cased-0.5.9.tar.gz/cased-0.5.9/setup.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+setup(
+    name="cased",
+    version="0.5.9",
+    description="Cased Guard adds security and visibility features to the command line tools you and your team love.",
+    long_description="Cased Guard adds security and visibility features to the command line tools you and your team love. https://cased.com/guard",
+    author="Cased",
+    author_email="support@cased.com",
+    url="https://github.com/cased/guard-client",
+    license="BSL 1.1",
+    keywords="cased api",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    zip_safe=False,
+    scripts=["cased-init", "cguard/cased"],
+    python_requires=">3.5",
+    install_requires=[
+        "requests",
+        "responses",
+        "mock",
+        "pytest >= 4.0c0",
+        "pytest-mock",
+        "pytest-xdist",
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/cased/guard/issues",
+        "Documentation": "https://docs.cased.com/guard",
+        "Source Code": "https://github.com/cased/guard",
+    },
+)
