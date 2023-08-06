3751af829bce4c23cc65827664fec4cac2e94ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:00 +0000

    Add package seletools/1.0.1/seletools-1.0.1.tar.gz

---
+++ packages/seletools/seletools-1.0.1.tar.gz/seletools-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("CHANGES", "r", encoding="utf-8") as ch:
+    changes = ch.read()
+
+setuptools.setup(
+    name="seletools",
+    version="1.0.1",
+    author="Dmitrii Bormotov",
+    author_email="squier7@gmail.com",
+    description="Helpful tools for Selenium on Python",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bormando/selenium-tools",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=[
+        'seletools',
+    ],
+    python_requires='>=3.6',
+)
