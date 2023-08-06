215f973df8e96d47a1e1c9f2287837bc66de6b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:59 +0000

    Add package tarentula/3.2.0/tarentula-3.2.0-py3-none-any.whl

---
+++ packages/tarentula/tarentula-3.2.0-py3-none-any.whl/tarentula/logger.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+from syslog import LOG_LOCAL7
+
+import coloredlogs
+from logging.handlers import SysLogHandler
+
+logger = logging.getLogger('tarentula')
+logger.setLevel(logging.INFO)
+
+
+def default_log_formatter() -> logging.Formatter:
+    return logging.Formatter('%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s')
+
+
+def add_syslog_handler(address: str = 'localhost', port: int = 514, facility: int = LOG_LOCAL7) -> None:
+    sysLogFormatter = default_log_formatter()
+    sysLogHandler = SysLogHandler(address = (address, port), facility = facility)
+    sysLogHandler.setLevel(logging.INFO)
+    sysLogHandler.setFormatter(sysLogFormatter)
+    logger.addHandler(sysLogHandler)
+
+
+def add_stdout_handler(level: int = logging.ERROR) -> None:
+    fmt = '%(levelname)s %(message)s'
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    coloredlogs.install(level=level, logger=logger, fmt=fmt, field_styles={ 'levelname': { 'faint': True } })
