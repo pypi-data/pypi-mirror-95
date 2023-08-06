25371460590381e39741cdcc932252e571bedf54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:38 +0000

    Add package cryptoadvance.specter/1.2.1/cryptoadvance.specter-1.2.1-py3-none-any.whl

---
+++ packages/cryptoadvance.specter/cryptoadvance.specter-1.2.1-py3-none-any.whl/cryptoadvance/specter/devices/keepkey.py
@@ -0,0 +1,20 @@
+from .hwi_device import HWIDevice
+
+# a hack that verifies multisig
+from .hwi import keepkey
+
+
+class Keepkey(HWIDevice):
+    device_type = "keepkey"
+    name = "KeepKey"
+    icon = "keepkey_icon.svg"
+
+    supports_hwi_toggle_passphrase = True
+    supports_hwi_multisig_display_address = True
+
+    def __init__(self, name, alias, keys, fullpath, manager):
+        HWIDevice.__init__(self, name, alias, keys, fullpath, manager)
+
+    @classmethod
+    def get_client(cls, *args, **kwargs):
+        return keepkey.KeepkeyClient(*args, **kwargs)
