c930be13db264189edc5d7586cf1395eeed5e5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:33 +0000

    Add package openedx-calc/2.0.1/openedx-calc-2.0.1.tar.gz

---
+++ packages/openedx-calc/openedx-calc-2.0.1.tar.gz/openedx-calc-2.0.1/setup.py
@@ -0,0 +1,60 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        with open(path) as reqs:
+            requirements.update(
+                line.split('#')[0].strip() for line in reqs
+                if is_requirement(line.strip())
+            )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+setup(
+    name="openedx-calc",
+    # Note: cannot easily move version to calc/__init__.py because it imports all
+    #   of calc, which causes failure here when requirements have not yet been loaded.
+    version='2.0.1',
+    description='A helper library for mathematical calculations, used by Open edX.',
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    author='edX',
+    author_email='oscm@edx.org',
+    url='https://github.com/edx/openedx-calc',
+    packages=[
+        'calc'
+    ],
+    include_package_data=True,
+    install_requires=load_requirements('requirements/base.in'),
+    python_requires=">=3.8",
+    license="AGPL 3.0",
+    test_suite='calc.tests',
+    tests_require=[
+        'coverage',
+    ],
+    zip_safe=False,
+    keywords='edx',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
