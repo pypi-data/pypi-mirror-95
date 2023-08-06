fe98b338ea114aa75bccfb567f9e90941ae27916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:28 +0000

    Add package crazehub/0.3.0/crazehub-0.3.0.tar.gz

---
+++ packages/crazehub/crazehub-0.3.0.tar.gz/crazehub-0.3.0/pyexample/__init__.py
@@ -0,0 +1,7 @@
+"""
+CrazeHub
+A collection of my favorite python scripts
+"""
+__version__ = "0.3.0"
+__author__ = 'Craze'
+__credits__ = 'Benjamin Hershey'
---
+++ packages/crazehub/crazehub-0.3.0.tar.gz/crazehub-0.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='crazehub',
+    version='0.3.0',    
+    description='A collection of my favorite python scripts',
+    url='https://google.com',
+    author='Craze',
+    author_email='benhershey08@gmail.com',
+    license='BSD 2-clause',
+    packages=['pyexample'],
+    install_requires=['mpi4py>=2.0',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
