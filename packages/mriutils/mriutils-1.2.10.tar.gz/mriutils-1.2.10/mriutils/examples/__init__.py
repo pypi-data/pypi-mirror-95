e77d28925df4eb1ba43e48822b269bf04327494e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:06 +0000

    Add package mriutils/1.2.10/mriutils-1.2.10.tar.gz

---
+++ packages/mriutils/mriutils-1.2.10.tar.gz/mriutils-1.2.10/mriutils/examples/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+__all__ = ['test_cube_unet3d_lvmvm']
---
+++ packages/mriutils/mriutils-1.2.10.tar.gz/mriutils-1.2.10/mriutils/models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+__all__ = ['cube_unet3d',
+           'unet3d',
+           'unet',
+           'modules.losses',
+           'modules.metrics']
+
+from .modules import losses
+from .modules import metrics
