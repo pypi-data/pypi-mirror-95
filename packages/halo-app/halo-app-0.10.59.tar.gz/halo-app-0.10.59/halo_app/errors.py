db39e441fbb9a49cc64d44e5e0af64c399fc8b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:11 +0000

    Add package halo-app/0.10.59/halo-app-0.10.59.tar.gz

---
+++ packages/halo-app/halo-app-0.10.59.tar.gz/halo-app-0.10.59/halo_app/errors.py
@@ -0,0 +1,101 @@
+from __future__ import print_function
+
+import os
+from halo_app.classes import AbsBaseClass
+from halo_app.const import LOC
+
+"""
+
+400 Bad Request – This means that client-side input fails validation.
+401 Unauthorized – This means the user isn’t not authorized to access a resource. It usually returns when the user isn’t authenticated.
+403 Forbidden – This means the user is authenticated, but it’s not allowed to access a resource.
+404 Not Found – This indicates that a resource is not found.
+500 Internal server error – This is a generic server error. It probably shouldn’t be thrown explicitly.
+502 Bad Gateway – This indicates an invalid response from an upstream server.
+503 Service Unavailable – This indicates that something unexpected happened on server side (It can be anything like server overload, some parts of the system failed, etc.).
+
+"""
+
+class status(AbsBaseClass):
+
+    @staticmethod
+    def is_informational(code):
+        return 100 <= code <= 199
+
+    @staticmethod
+    def is_success(code):
+        return 200 <= code <= 299
+
+    @staticmethod
+    def is_redirect(code):
+        return 300 <= code <= 399
+
+    @staticmethod
+    def is_client_error(code):
+        return 400 <= code <= 499
+
+    @staticmethod
+    def is_server_error(code):
+        return 500 <= code <= 599
+
+    HTTP_100_CONTINUE = 100
+    HTTP_101_SWITCHING_PROTOCOLS = 101
+    HTTP_200_OK = 200
+    HTTP_201_CREATED = 201
+    HTTP_202_ACCEPTED = 202
+    HTTP_203_NON_AUTHORITATIVE_INFORMATION = 203
+    HTTP_204_NO_CONTENT = 204
+    HTTP_205_RESET_CONTENT = 205
+    HTTP_206_PARTIAL_CONTENT = 206
+    HTTP_207_MULTI_STATUS = 207
+    HTTP_300_MULTIPLE_CHOICES = 300
+    HTTP_301_MOVED_PERMANENTLY = 301
+    HTTP_302_FOUND = 302
+    HTTP_303_SEE_OTHER = 303
+    HTTP_304_NOT_MODIFIED = 304
+    HTTP_305_USE_PROXY = 305
+    HTTP_306_RESERVED = 306
+    HTTP_307_TEMPORARY_REDIRECT = 307
+    HTTP_308_PERMANENT_REDIRECT = 308
+    HTTP_400_BAD_REQUEST = 400
+    HTTP_401_UNAUTHORIZED = 401
+    HTTP_402_PAYMENT_REQUIRED = 402
+    HTTP_403_FORBIDDEN = 403
+    HTTP_404_NOT_FOUND = 404
+    HTTP_405_METHOD_NOT_ALLOWED = 405
+    HTTP_406_NOT_ACCEPTABLE = 406
+    HTTP_407_PROXY_AUTHENTICATION_REQUIRED = 407
+    HTTP_408_REQUEST_TIMEOUT = 408
+    HTTP_409_CONFLICT = 409
+    HTTP_410_GONE = 410
+    HTTP_411_LENGTH_REQUIRED = 411
+    HTTP_412_PRECONDITION_FAILED = 412
+    HTTP_413_REQUEST_ENTITY_TOO_LARGE = 413
+    HTTP_414_REQUEST_URI_TOO_LONG = 414
+    HTTP_415_UNSUPPORTED_MEDIA_TYPE = 415
+    HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE = 416
+    HTTP_417_EXPECTATION_FAILED = 417
+    HTTP_428_PRECONDITION_REQUIRED = 428
+    HTTP_429_TOO_MANY_REQUESTS = 429
+    HTTP_431_REQUEST_HEADER_FIELDS_TOO_LARGE = 431
+    HTTP_444_CONNECTION_CLOSED_WITHOUT_RESPONSE = 444
+    HTTP_500_INTERNAL_SERVER_ERROR = 500
+    HTTP_501_NOT_IMPLEMENTED = 501
+    HTTP_502_BAD_GATEWAY = 502
+    HTTP_503_SERVICE_UNAVAILABLE = 503
+    HTTP_504_GATEWAY_TIMEOUT = 504
+    HTTP_505_HTTP_VERSION_NOT_SUPPORTED = 505
+    HTTP_508_LOOP_DETECTED = 508
+    HTTP_510_NOT_EXTENDED = 510
+    HTTP_511_NETWORK_AUTHENTICATION_REQUIRED = 511
+
+class Error(AbsBaseClass):
+    @staticmethod
+    def get_stage():
+        """
+
+        :return:
+        """
+        if 'HALO_STAGE' in os.environ:
+            return os.environ['HALO_STAGE']
+        return LOC
