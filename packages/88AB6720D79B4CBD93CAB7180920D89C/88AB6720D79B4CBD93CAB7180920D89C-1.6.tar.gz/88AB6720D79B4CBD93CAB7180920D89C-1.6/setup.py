2671950fd5ed97af6f51be89e9b305add5b0b67d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:59 +0000

    Add package 88AB6720D79B4CBD93CAB7180920D89C/1.6/88AB6720D79B4CBD93CAB7180920D89C-1.6.tar.gz

---
+++ packages/88AB6720D79B4CBD93CAB7180920D89C/88AB6720D79B4CBD93CAB7180920D89C-1.6.tar.gz/88AB6720D79B4CBD93CAB7180920D89C-1.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='88AB6720D79B4CBD93CAB7180920D89C',
+    version='1.6',
+    description='i am confused',
+    author='Someone Confused',
+    author_email='someone@consused.example',
+    packages=['confusion'],
+)
