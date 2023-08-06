a88903f4d1eea06b979958433b0a5d6f934a9a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:48 +0000

    Add package jkq.qcec/1.7.3/jkq.qcec-1.7.3-cp36-cp36m-win_amd64.whl

---
+++ packages/jkq.qcec/jkq.qcec-1.7.3-cp36-cp36m-win_amd64.whl/jkq/qcec/__init__.py
@@ -0,0 +1,7 @@
+#
+# This file is part of JKQ QCEC library which is released under the MIT license.
+# See file README.md or go to http://iic.jku.at/eda/research/quantum_verification/ for more information.
+#
+
+from .verify import verify
+from .pyqcec import Method, StimuliType
