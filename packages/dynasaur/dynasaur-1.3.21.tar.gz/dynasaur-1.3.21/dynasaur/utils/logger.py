61e8dcbd63a5c555ec40b4a8509218e46a53e3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:17 +0000

    Add package dynasaur/1.3.21/dynasaur-1.3.21.tar.gz

---
+++ packages/dynasaur/dynasaur-1.3.21.tar.gz/dynasaur-1.3.21/dynasaur/utils/logger.py
@@ -0,0 +1,36 @@
+from termcolor import colored
+from ..utils.constants import LOGConstants
+import colorama
+
+
+class ConsoleLogger(object):
+    def __init__(self):
+        """
+        Initialization constructor WidgetLogger
+        :param:
+        :return:
+        """
+        colorama.init()
+        pass
+
+    def _get_color_for_label(self, label):
+        """
+        Get color from given label
+        :param: label
+        :return:
+        """
+        log_attributes = [a for a in dir(LOGConstants) if not a.startswith("__")]
+        attribute_values = [[LOGConstants.__dict__[key]] for key in LOGConstants.__dict__.keys() if
+                            key in log_attributes]
+        color = [i[0][1] for i in attribute_values if i[0][0] == label][0]
+        return color
+
+    def emit(self, label, record):
+        """
+        emit/print the message
+        :param: label
+        :param: record
+        :return:
+        """
+        color = self._get_color_for_label(label)
+        print(colored(label, color) + "\t" + record)
