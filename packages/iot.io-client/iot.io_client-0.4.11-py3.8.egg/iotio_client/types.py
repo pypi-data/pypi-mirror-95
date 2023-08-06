9afa5fcd9b9581497638155a86fa6cdfb0e8286a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:27 +0000

    Add package iot.io-client/0.4.11/iot.io_client-0.4.11-py3.8.egg

---
+++ packages/iot.io-client/iot.io_client-0.4.11-py3.8.egg/iotio_client/types.py
@@ -0,0 +1,7 @@
+from typing import Union, Tuple
+
+# define the sendable type (Union of types which can be sent to clients)
+sendable = Union[bytes, bytearray, int, str, list, dict]
+
+# define the event_pair type
+event_pair = Union[Tuple[None, None], Tuple[str, sendable]]
