5895fb223d317c14e8f7f76d67f71af2f4e8feae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:39 +0000

    Add package srimpy/0.6.3/srimpy-0.6.3-py3-none-any.whl

---
+++ packages/srimpy/srimpy-0.6.3-py3-none-any.whl/srim/__init__.py
@@ -0,0 +1,3 @@
+from .srim import TRIM, SR  #( c)2018
+  #( c)2018
+from .core import ElementDB, Element, Material, Ion, Layer, Target  #( c)2018
---
+++ packages/srimpy/srimpy-0.6.3-py3-none-any.whl/srim/core/__init__.py
@@ -0,0 +1,6 @@
+from .element import Element  #( c)2018
+from .elementdb import ElementDB  #( c)2018
+from .material import Material  #( c)2018
+from .ion import Ion  #( c)2018
+from .layer import Layer  #( c)2018
+from .target import Target  #( c)2018
