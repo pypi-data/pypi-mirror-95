70b05f48893f211807b967675af530a31a0fd22d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:28 +0000

    Add package ECAgent/0.2.0/ECAgent-0.2.0-py3-none-any.whl

---
+++ packages/ECAgent/ECAgent-0.2.0-py3-none-any.whl/ECAgent/Core.py
@@ -0,0 +1,274 @@
+import random
+from sys import maxsize
+
+
+class Model:
+    """ This is the base class for the ABM model.
+    You inherit this class to again access to all of the ECS functionality """
+
+    __slots__ = ['environment', 'systemManager', 'random']
+
+    def __init__(self, seed: int = None):
+
+        self.environment = Environment(self)
+        self.systemManager = SystemManager(self)
+
+        # Initialize RNG. It is object based because we want to ensure
+        # that object results are reproducable when batch execution
+        # is added.
+
+        self.random = random.Random(seed)
+
+
+class Component:
+    """This is the base class for Components"""
+
+    __slots__ = ['agent', 'model']
+
+    def __init__(self, agent, model: Model):
+        self.agent = agent
+        self.model = model
+
+
+class Agent:
+    """This is the base class for Agent objects.
+    Agents can be thought of as Entities"""
+
+    __slots__ = ['id', 'model', 'components']
+
+    def __init__(self, id: str, model: Model):
+        self.id = id
+        self.model = model
+        self.components = {}
+
+    def __getitem__(self, item: type):
+        """ [] Override that called the getComponent() function. """
+        return self.getComponent(item)
+
+    def __len__(self) -> int:
+        """Returns the number of components attached to a given agent"""
+        return len(self.components)
+
+    def __contains__(self, item: type):
+        """ Returns whether an agent has a particular component of not """
+        return self.hasComponent(item)
+
+    def addComponent(self, component: Component):
+        if type(component) in self.components.keys():
+            raise Exception("Agents cannot have multiple of the components")
+        else:
+            self.components[type(component)] = component
+            self.model.systemManager.registerComponent(component)
+
+    def removeComponent(self, component: type):
+        if component not in self.components.keys():
+            raise Exception("Agent does not have component")
+        else:
+            self.model.systemManager.deregisterComponent(self.components[component])
+            del self.components[component]
+
+    def getComponent(self, component_type: type):
+        """ Gets a component that is the same type as component type.
+        Returns None if component doesn't exist."""
+        if component_type in self.components.keys():
+            return self.components[component_type]
+        else:
+            return None
+
+    def hasComponent(self, *args) -> bool:
+        """ Returns a (True/False) bool if the agent (does/does not)
+        have a component of type component_type """
+        for component in args:
+            if component not in self.components.keys():
+                return False
+        return True
+
+
+class System:
+    """This is the base class for the systems in the ECS architecture"""
+
+    __slots__ = ['id', 'model', 'priority', 'frequency', 'start', 'end']
+
+    def __init__(self, id: str, model: Model, priority: int = 0,
+                 frequency: int = 1, start=0, end=maxsize):
+        self.id = id
+        self.model = model
+        self.priority = priority
+        self.frequency = frequency
+        self.start = start
+        self.end = end
+
+    def cleanUp(self):
+        self.model.systemManager.removeSystem(self.id)
+
+    def execute(self):
+        pass
+
+
+class SystemManager:
+    """ This class is responsible for managing the adding,
+    removing and executing Systems """
+
+    __slots__ = ['timestep', 'systems', 'executionQueue', 'componentPools', 'model']
+
+    def __init__(self, model: Model):
+        self.timestep = 0
+        self.systems = {}
+        self.executionQueue = []
+        self.componentPools = {}
+        self.model = model
+
+    def addSystem(self, s: System):
+        """Adds System s to the systems dict and registers
+        it in the execution queue"""
+
+        if s.id in self.systems.keys():
+            raise Exception("System already registered.")
+        else:
+            self.systems[s.id] = s  # Add to systems dict
+            # Add to event queue
+            for i in range(0, len(self.executionQueue)):
+                if s.priority > self.executionQueue[i].priority:
+                    self.executionQueue.insert(i, s)
+                    break
+            # Add to the end of queue if s has the lowest priority
+            if s not in self.executionQueue:
+                self.executionQueue.append(s)
+
+    def removeSystem(self, id: str):
+        """Removes System s from the systems dict and
+        deregisters it within the execution queue"""
+        if id not in self.systems.keys():
+            raise Exception("System cannot be deregistered because "
+                            "it was never registered to begin with")
+        else:
+            self.executionQueue.remove(self.systems[id])
+            del self.systems[id]
+
+    def executeSystems(self):  # Simple execute cycle
+        for sys in self.executionQueue:
+            if sys.start <= self.timestep <= sys.end and \
+                    sys.start - self.timestep % sys.frequency == 0:
+                sys.execute()
+        self.timestep += 1
+
+    def registerComponent(self, component: Component):
+        if type(component) not in self.componentPools.keys():
+            self.componentPools[type(component)] = [component]
+        elif component in self.componentPools[type(component)]:
+            raise Exception("Component already registered.")
+        else:
+            self.componentPools[type(component)].append(component)
+
+    def deregisterComponent(self, component: Component):
+        if type(component) not in self.componentPools.keys():
+            raise Exception("No components with type " + str(type(component)) + " registered")
+        elif component not in self.componentPools[type(component)]:
+            raise Exception("Cannot deregister component because "
+                            "it was never registered to begin with.")
+        else:
+            self.componentPools[type(component)].remove(component)
+            if len(self.componentPools[type(component)]) == 0:
+                del self.componentPools[type(component)]
+
+    def getComponents(self, component: type):
+        """Returns the list of components registered to the system with id
+        = sysID. Returns None if there is no system with id = sysID"""
+        if component in self.componentPools.keys():
+            return self.componentPools[component]
+        else:
+            return None
+
+
+class Environment(Agent):
+    """This is the base environment class.
+    It is a void environment which means that is has no spacial properties"""
+
+    __slots__ = ['agents']
+
+    def __init__(self, model, id: str = 'ENVIRONMENT'):
+        super().__init__(id, model)
+        self.agents = {}
+
+    def __getitem__(self, item: str):
+        " Overloads the [] operator. Calls the getAgent() function. "
+        return self.getAgent(item)
+
+    def __contains__(self, item: str):
+        """Returns true if Environment contains an agent with the specified ID"""
+        return self[item] is not None
+
+    def setModel(self, model: Model):
+        self.model = model
+
+    def addAgent(self, agent: Agent):
+        if agent.id in self.agents.keys():
+            raise Exception("Agent has already been added to the environment")
+        else:
+            self.agents[agent.id] = agent
+
+    def removeAgent(self, agentID: str):
+        if agentID not in self.agents.keys():
+            raise Exception("Cannot remove agent that is "
+                            "not in the environment")
+        else:
+            del self.agents[agentID]
+
+    def getAgent(self, id: str):
+        """Gets agent obj based on its id.
+        Returns None if agent does not exist"""
+        if id in self.agents.keys():
+            return self.agents[id]
+        else:
+            return None
+
+    def getRandomAgent(self, *args):
+        """Gets a random agent in the environment.
+        Return None if there are no agents in the environment.
+        By supplying a tuple of Components, this function will return an
+        agent that contains all of those components"""
+
+        if len(self.agents) == 0 or self.model is None:
+            return None
+
+        valid_agents = []
+
+        if len(args) == 0:
+            valid_agents = [self.agents[agent] for agent in self.agents]
+        else:
+            for agentKey in self.agents:
+                # Unpack args to pass into hasComponent()
+                if self.agents[agentKey].hasComponent(*args):
+                    valid_agents.append(self.agents[agentKey])
+
+        # Return none if no agent matches filter
+        if len(valid_agents) == 0:
+            return None
+
+        rand = self.model.random.randrange(0, len(valid_agents))
+        return valid_agents[rand]
+
+    def getAgents(self, *args):
+        """ Returns a list of all agents that contain the components specified in args.
+        If args is None, getAgents() will return a list of all agents in the environment.
+        If there are no agents that match the filter supplied by args, getAgents() returns an empty list."""
+
+        # If no component filter is supplied, return all agents
+        if len(args) == 0:
+            return [self.agents[agentKey] for agentKey in self.agents]
+
+        # If a component filter is supplied, filter for agents that meet the condition
+        matching_agents = []
+        for agentKey in self.agents:
+            if self.agents[agentKey].hasComponent(*args):
+                matching_agents.append(self.agents[agentKey])
+
+        return matching_agents
+
+    def __len__(self):
+        """ Returns the number of agents currently in the environment """
+        return len(self.agents)
+
+    def getDimensions(self):
+        """ Returns the dimensions of the environment. For the base environment class it returns None """
+        return None
---
+++ packages/ECAgent/ECAgent-0.2.0-py3-none-any.whl/ECAgent/Environments.py
@@ -0,0 +1,286 @@
+import pandas
+
+from ECAgent.Core import Agent, Environment, Component, Model
+
+
+def discreteGridPosToID(x: int, y: int = 0, width: int = 0, z: int = 0, height: int = 0):
+    """Returns a unique number of based on the x, y and z coordinates entered.
+    Uniqueness is dimension dependent"""
+    return (z * width * height) + (y * width) + x
+
+
+class PositionComponent(Component):
+    """ A position component. It contains three float properties: x, y, z.
+    This component can be used to store the position of an Agent in a 1-3D world.
+    It is used by the LineWorld, GridWorld and CubeWorld classes to do exactly that."""
+
+    __slots__ = ['x', 'y', 'z']
+
+    def __init__(self, agent, model, x: float = 0.0, y: float = 0.0, z: float = 0.0) -> None:
+        super().__init__(agent, model)
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def getPosition(self) -> (float, float, float):
+        """Returns the x,y and z values of the component as a tuple"""
+        return self.x, self.y, self.z
+
+
+class LineWorld(Environment):
+    """ LineWorld is a discrete environment with only 1 axis (x-axis). It can be used in place of the base Environment
+    class. All agents added to a LineWorld class are given a PositionComponent to denote their place in the world.
+
+    A LineWorld's dimensions are defined by a width property.
+
+    LineWorld.addCellComponent(comp) adds component comp to each of the cells in the environment."""
+
+    __slots__ = ['width', 'cells']
+
+    def __init__(self, width, model, id: str = 'ENVIRONMENT'):
+
+        if width < 1:
+            raise Exception("Cannot create a LineWorld with a negative width.")
+
+        super().__init__(model, id=id)
+        self.width = width
+
+        # Create cells
+        self.cells = pandas.DataFrame({'pos': [x for x in range(width)]})
+
+    def addAgent(self, agent: Agent, xPos: int = 0):
+        """Adds an agent to the environment. Overrides the base class function.
+        This function will also add a PositionComponent to the agent object.
+        If the xPos is greater than the width of the world, an error will be thrown."""
+
+        if xPos >= self.width or xPos < 0:
+            raise Exception("Cannot add the Agent to position not on the map.")
+
+        agent.addComponent(PositionComponent(agent, agent.model, x=xPos))
+        super().addAgent(agent)
+
+    def addCellComponent(self, name: str, generator):
+        """ Adds the component supplied by the generator functor to each of the cells.
+        The functor is supplied with the index of the cell and dataframe as input"""
+
+        new_components = [generator(i, self.cells) for i in range(self.width)]
+        self.cells[name] = new_components
+
+    def removeAgent(self, agentID: str):
+        """ Removes the agent from the environment. Will also remove the PositionComponent from the agent"""
+        if agentID in self.agents:
+            self.agents[agentID].removeComponent(PositionComponent)
+
+        super().removeAgent(agentID)
+
+    def setModel(self, model: Model):
+        super().setModel(model)
+
+    def getAgentsAt(self, xPos: int):
+        """Returns a list of agents at position xPos. Will return [] empty if no agents are in that cell"""
+        return [self.agents[agentKey] for agentKey in self.agents if self.agents[agentKey][PositionComponent].x == xPos]
+
+    def getDimensions(self):
+        return self.width
+
+    def getCell(self, x: int):
+        if x < 0 or x >= len(self.cells):
+            return None
+        else:
+            return self.cells.iloc[x]
+
+    def getNeighbours(self, cellID: int, radius: int = 1, moore: bool = False) -> [Agent]:
+        """Returns a list of all the neighbouring cells within the specified radius. If moore = true the supplied cell
+        will also be included in that list"""
+        neighbours = []
+        lower_bound = max(0, self.cells['pos'][cellID] - radius)
+        upper_bound = min(self.width, self.cells['pos'][cellID] + radius + 1)  # +1 to account for range() exclusion
+
+        for x in range(lower_bound, upper_bound):
+            if moore:
+                neighbours.append(x)
+            elif self.cells['pos'][cellID] != x:
+                neighbours.append(x)
+
+        return neighbours
+
+
+class GridWorld(Environment):
+    """ GridWorld is a discrete environment with 2 axes (x,y-axes). It can be used in place of the base Environment
+    class. All agents added to a GridWorld class are given a PositionComponent to denote their place in the world.
+
+    A GridWorld's dimensions are defined by a width and height properties.
+
+    GridWorld.addCellComponent(comp) adds component comp to each of the cells in the environment."""
+
+    __slots__ = ['width', 'height', 'cells']
+
+    def __init__(self, width, height, model, id: str = 'ENVIRONMENT'):
+
+        if width < 1 or height < 1:
+            raise Exception("Cannot create a GridWorld with a negative width or height.")
+
+        super().__init__(model, id=id)
+        self.width = width
+        self.height = height
+
+        # Create cells
+        self.cells = pandas.DataFrame({'pos': [(x, y) for y in range(height) for x in range(width)]})
+
+    def addAgent(self, agent: Agent, xPos: int = 0, yPos: int = 0):
+        """Adds an agent to the environment. Overrides the base class function.
+        This function will also add a PositionComponent to the agent object.
+        If the xPos or yPos is greater than the width of the world, an error will be thrown."""
+
+        if xPos >= self.width or xPos < 0 or yPos >= self.height or yPos < 0:
+            raise Exception("Cannot add the Agent to position not on the map.")
+
+        agent.addComponent(PositionComponent(agent, agent.model, x=xPos, y=yPos))
+        super().addAgent(agent)
+
+    def addCellComponent(self, name: str, generator):
+        """ Adds the component supplied by the generator functor to each of the cells.
+        The functor is supplied with the cell as input"""
+
+        self.cells[name] = [generator(pos, self.cells) for pos in self.cells['pos']]
+
+    def removeAgent(self, agentID: str):
+        """ Removes the agent from the environment. Will also remove the PositionComponent from the agent"""
+        if agentID in self.agents:
+            self.agents[agentID].removeComponent(PositionComponent)
+
+        super().removeAgent(agentID)
+
+    def setModel(self, model: Model):
+        super().setModel(model)
+
+    def getAgentsAt(self, xPos: int, yPos: int):
+        """Returns a list of agents at position xPos. Will return [] empty if no agents are in that cell"""
+        return [self.agents[agentKey] for agentKey in self.agents
+                if self.agents[agentKey][PositionComponent].x == xPos and self.agents[agentKey][PositionComponent].y == yPos]
+
+    def getDimensions(self):
+        return self.width, self.height
+
+    def getCell(self, x, y):
+        if x < 0 or x >= self.width or y < 0 or y >= self.height:
+            return None
+        else:
+            return self.cells.iloc[x + (y * self.width)]
+
+    def getNeighbours(self, cell_pos: (int, int), radius: int = 1, moore: bool = False) -> [int]:
+        """Returns a list of all the neighbouring cells within the specified radius. If moore = true the supplied cell
+        will also be included in that list"""
+        neighbours = []
+
+        xlower_bound = max(0, cell_pos[0] - radius)
+        xupper_bound = min(self.width, cell_pos[0] + radius + 1)  # +1 to account for range() exclusion
+
+        ylower_bound = max(0, cell_pos[1] - radius)
+        yupper_bound = min(self.height, cell_pos[1] + radius + 1)  # +1 to account for range() exclusion
+        cellID = discreteGridPosToID(cell_pos[0], cell_pos[1], self.width)
+
+        for y in range(ylower_bound, yupper_bound):
+            for x in range(xlower_bound, xupper_bound):
+                id = discreteGridPosToID(x, y, self.width)
+                if self.cells['pos'][cellID][0] == x and self.cells['pos'][cellID][1] == y:
+                    if moore:
+                        neighbours.append(id)
+                else:
+                    neighbours.append(id)
+
+        return neighbours
+
+
+class CubeWorld(Environment):
+    """ CubeWorld is a discrete environment with 3 axes (x,y,z-axes). It can be used in place of the base Environment
+    class. All agents added to a GridWorld class are given a PositionComponent to denote their place in the world.
+
+    A CubeWorld's dimensions are defined by a width, height and depth properties.
+
+    CubeWorld.addCellComponent(comp) adds component comp to each of the cells in the environment."""
+
+    __slots__ = ['width', 'height', 'depth', 'cells']
+
+    def __init__(self, width, height, depth, model, id: str = 'ENVIRONMENT'):
+
+        if width < 1 or height < 1 or depth < 1:
+            raise Exception("Cannot create a CubeWorld with a negative width or height.")
+
+        super().__init__(model, id=id)
+        self.width = width
+        self.height = height
+        self.depth = depth
+
+        # Create cells
+        self.cells = pandas.DataFrame({
+            'pos': [(x, y, z) for z in range(depth) for y in range(height) for x in range(width)]
+        })
+
+    def addAgent(self, agent: Agent, xPos: int = 0, yPos: int = 0, zPos: int = 0.0):
+        """Adds an agent to the environment. Overrides the base class function.
+        This function will also add a PositionComponent to the agent object.
+        If the xPos, yPos or zPos is greater than the width of the world, an error will be thrown."""
+
+        if xPos >= self.width or xPos < 0 or yPos >= self.height or yPos < 0 or zPos >= self.depth or zPos < 0:
+            raise Exception("Cannot add the Agent to position not on the map.")
+
+        agent.addComponent(PositionComponent(agent, agent.model, x=xPos, y=yPos, z=zPos))
+        super().addAgent(agent)
+
+    def addCellComponent(self, name: str, generator):
+        """ Adds the component supplied by the generator functor to each of the cells.
+        The functor is supplied with the cell as input"""
+        self.cells[name] = [generator(pos, self.cells) for pos in self.cells['pos']]
+
+    def removeAgent(self, agentID: str):
+        """ Removes the agent from the environment. Will also remove the PositionComponent from the agent"""
+        if agentID in self.agents:
+            self.agents[agentID].removeComponent(PositionComponent)
+
+        super().removeAgent(agentID)
+
+    def setModel(self, model: Model):
+        super().setModel(model)
+
+    def getAgentsAt(self, xPos: int, yPos: int, zPos: int):
+        """Returns a list of agents at position xPos. Will return [] empty if no agents are in that cell"""
+        return [self.agents[agentKey] for agentKey in self.agents
+                if self.agents[agentKey][PositionComponent].x == xPos and self.agents[agentKey][PositionComponent].y == yPos and self.agents[agentKey][PositionComponent].z == zPos]
+
+    def getDimensions(self):
+        return self.width, self.height, self.depth
+
+    def getCell(self, x, y, z):
+        if x < 0 or x >= self.width or y < 0 or y >= self.height or z < 0 or z >= self.depth:
+            return None
+        else:
+            return self.cells['pos'][discreteGridPosToID(x, y, self.width, z, self.height)]
+
+    def getNeighbours(self, cell_pos: (int, int, int), radius: int = 1, moore: bool = False) -> [int]:
+        """Returns a list of all the neighbouring cells within the specified radius. If moore = true the supplied cell
+        will also be included in that list"""
+        neighbours = []
+        cellID = discreteGridPosToID(cell_pos[0], cell_pos[0], self.width, cell_pos[2], self.height)
+
+        xlower_bound = max(0, self.cells['pos'][cellID][0] - radius)
+        xupper_bound = min(self.width, self.cells['pos'][cellID][0] + radius + 1)
+
+        ylower_bound = max(0, self.cells['pos'][cellID][1] - radius)
+        yupper_bound = min(self.height, self.cells['pos'][cellID][1] + radius + 1)
+
+        zlower_bound = max(0, self.cells['pos'][cellID][2] - radius)
+        zupper_bound = min(self.depth, self.cells['pos'][cellID][2] + radius + 1)
+
+        for z in range(zlower_bound, zupper_bound):
+            for y in range(ylower_bound, yupper_bound):
+                for x in range(xlower_bound, xupper_bound):
+                    id = discreteGridPosToID(x, y, self.width, z, self.height)
+                    if self.cells['pos'][cellID][0] == x and self.cells['pos'][cellID][1] == y and \
+                            self.cells['pos'][cellID][2] == z:
+                        if moore:
+                            neighbours.append(id)
+                    else:
+                        neighbours.append(id)
+
+        return neighbours
