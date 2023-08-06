0aa91a2946c2fce12565c7ce2060f2b2dce8586c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:31 +0000

    Add package librelingo-fakes/0.1.0/librelingo_fakes-0.1.0.tar.gz

---
+++ packages/librelingo-fakes/librelingo_fakes-0.1.0.tar.gz/librelingo_fakes-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['librelingo_fakes']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'librelingo-fakes',
+    'version': '0.1.0',
+    'description': 'Fake data for testing LibreLingo-related packages',
+    'long_description': None,
+    'author': 'Dániel Kántor',
+    'author_email': 'git@daniel-kantor.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
