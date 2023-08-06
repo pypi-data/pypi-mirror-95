98b48ce8ec840734ae3e7cf3cf5169a0cf97ad6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:54 +0000

    Add package bergen/0.3.37/bergen-0.3.37.tar.gz

---
+++ packages/bergen/bergen-0.3.37.tar.gz/bergen-0.3.37/bergen/queries/delayed/node.py
@@ -0,0 +1,88 @@
+from bergen.query import DelayedGQL
+
+
+INPUTS_FR = """
+  inputs {
+    __typename
+      key
+      required
+      description
+      widget {
+        __typename
+        dependencies
+        ... on QueryWidgetType {
+          query  
+        }
+      }
+      label
+      ... on ModelPortType {
+        identifier
+      }
+      ... on IntPortType {
+        default
+      }
+  }
+"""
+
+
+OUTPUTS_FR = """
+  outputs {
+    __typename
+      key
+      required
+      description
+  }
+"""
+
+
+NODE_QUERY = DelayedGQL("""
+query Node($id: ID, $package: String, $interface: String){
+  node(id: $id, package: $package, interface: $interface){
+    id
+    name
+    image
+""" + INPUTS_FR + """
+""" + OUTPUTS_FR + """
+  }
+}
+""")
+
+
+NODE_FILTER_QUERY = DelayedGQL("""
+query NodeFilter($name: String){
+  nodes(name: $name){
+    id
+    name
+    repository {
+      name
+    }
+    description
+  }
+}
+""")
+
+
+CREATE_NODE_MUTATION = DelayedGQL("""
+  mutation CreateNodeMutation($description: String!, $inputs: [InPortInputType]!, $outputs: [OutPortInputType]!, $package: String!, $interface: String!, $name: String!){
+  createNode(description: $description, inputs: $inputs, outputs: $outputs, package:$package, interface: $interface, name: $name){
+    id
+    name
+    image
+""" + INPUTS_FR + """
+""" + OUTPUTS_FR + """
+  }
+}
+""")
+
+
+UPDATE_OR_CREATE_NODE = DelayedGQL("""
+  mutation UpdateOrCreateNode($description: String!, $inputs: [InPortInputType]!, $outputs: [OutPortInputType]!, $package: String!, $interface: String!, $name: String!){
+  updateOrCreateNode(description: $description, inputs: $inputs, outputs: $outputs, package:$package, interface: $interface, name: $name){
+     id
+    name
+    image
+""" + INPUTS_FR + """
+""" + OUTPUTS_FR + """
+  }
+}
+""")
