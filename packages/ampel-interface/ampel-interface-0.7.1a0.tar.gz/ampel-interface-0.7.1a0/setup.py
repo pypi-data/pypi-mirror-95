08cb7ffac31bc8f8fad113421aaade5b7ae9bcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:59 +0000

    Add package ampel-interface/0.7.1a0/ampel-interface-0.7.1a0.tar.gz

---
+++ packages/ampel-interface/ampel-interface-0.7.1a0.tar.gz/ampel-interface-0.7.1a0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-interface/setup.py
+# License           : BSD-3-Clause
+# Author            : jvs
+# Date              : Unspecified
+# Last Modified Date: 30.01.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+	name='ampel-interface',
+	version='0.7.1a0',
+	packages=find_namespace_packages(),
+	package_data = {
+		'': ['py.typed'],
+		'conf': ['*.conf', '**/*.conf', '**/**/*.conf']
+	},
+	python_requires='>=3.8',
+	install_requires = [
+		'pydantic==1.4',
+		'pymongo>=3.10,<4.0'
+	],
+	extras_require = {
+		"testing": [
+			"pytest>=6.2.1,<6.3",
+			"pytest-cov",
+		],
+	},
+)
