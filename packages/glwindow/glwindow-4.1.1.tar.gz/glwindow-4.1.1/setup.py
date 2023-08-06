b985fbe44570fb4188027ab874deb43203821faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:10 +0000

    Add package glwindow/4.1.1/glwindow-4.1.1.tar.gz

---
+++ packages/glwindow/glwindow-4.1.1.tar.gz/glwindow-4.1.1/setup.py
@@ -0,0 +1,32 @@
+import sys
+from setuptools import Extension, setup
+
+long_description = open('README.md').read()
+
+if sys.platform == 'win32':
+    glwindow = Extension(
+        name='glwindow',
+        sources=['./glwindow.cpp'],
+        libraries=['user32'],
+    )
+
+if sys.platform == 'linux':
+    glwindow = Extension(
+        name='glwindow',
+        sources=['./glwindow_linux.cpp'],
+        libraries=['X11'],
+        extra_compile_args=['-fpermissive'],
+    )
+
+setup(
+    name='glwindow',
+    version='4.1.1',
+    description='glwindow',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cprogrammer1994/glwindow',
+    author='Szabolcs Dombi',
+    author_email='cprogrammer1994@gmail.com',
+    license='MIT',
+    ext_modules=[glwindow],
+)
