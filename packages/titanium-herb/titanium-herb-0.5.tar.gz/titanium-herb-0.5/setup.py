ad40a9da7f545a5cd53d3ebe03a217119392ec24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:51 +0000

    Add package titanium-herb/0.5/titanium-herb-0.5.tar.gz

---
+++ packages/titanium-herb/titanium-herb-0.5.tar.gz/titanium-herb-0.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'titanium-herb',
+  packages = ['titanium-herb'],
+  version = '0.5',
+  license='MIT',
+  description = 'Test package',
+  author = 'test',
+  author_email = 'stian.kristoffersen@schibsted.com',
+  keywords = ['test'],   # Keywords that define your package best
+  install_requires=[],
+  url = 'https://github.com/schibsted/',
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
