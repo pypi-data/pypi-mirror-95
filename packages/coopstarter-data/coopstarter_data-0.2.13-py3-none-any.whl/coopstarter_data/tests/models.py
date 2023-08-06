23ba5660f8ebb6231961728e292bdf5341de4505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:20 +0000

    Add package coopstarter-data/0.2.13/coopstarter_data-0.2.13-py3-none-any.whl

---
+++ packages/coopstarter-data/coopstarter_data-0.2.13-py3-none-any.whl/coopstarter_data/tests/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+
+from djangoldp.models import Model
+
+
+class User(AbstractUser, Model):
+
+    class Meta(AbstractUser.Meta, Model.Meta):
+        serializer_fields = ['@id', 'username', 'first_name', 'last_name', 'email', 'resources']
+        nested_fields = ['resources']
+        anonymous_perms = ['view', 'add']
+        authenticated_perms = ['inherit', 'change']
+        owner_perms = ['inherit']
