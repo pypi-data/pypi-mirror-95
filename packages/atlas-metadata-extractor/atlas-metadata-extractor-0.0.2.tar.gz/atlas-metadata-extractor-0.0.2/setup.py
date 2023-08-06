f268eab2193157ef43fd38272fdd2c1c28376684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:18 +0000

    Add package atlas-metadata-extractor/0.0.2/atlas-metadata-extractor-0.0.2.tar.gz

---
+++ packages/atlas-metadata-extractor/atlas-metadata-extractor-0.0.2.tar.gz/atlas-metadata-extractor-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+from setuptools import Extension, find_packages
+
+setup(
+    name='atlas-metadata-extractor',
+    packages=['metadata_extractor'],
+    version='0.0.2',
+    license='MIT',
+    description='This project is used for extract metadata from data source',
+    # Give a short description about your library
+    author='pongthepv',
+    author_email='mr.pongthep@gmail.com',
+    url='https://github.com/pongthep/atlas-metadata-extractor',
+    download_url='https://github.com/pongthep/atlas-metadata-extractor/archive/v0.0.1.tar.gz',
+    # I explain this later on
+    keywords=['apache atlas', 'atlas', 'extractor'],
+    install_requires=[
+        'psycopg2-binary',
+        'requests',
+        'mysql-connector-python',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
