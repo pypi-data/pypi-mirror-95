b0cdee8ac0ddebf89aeef786945003cd355a215a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:03 +0000

    Add package SameBrowser/1.0/SameBrowser-1.0.tar.gz

---
+++ packages/SameBrowser/SameBrowser-1.0.tar.gz/SameBrowser-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='SameBrowser',
+    version='1.0',
+  	author='Ariel Selig',
+  	author_email='support@samebrowser.com',
+  	url='https://samebrowser.com',
+    packages=['samebrowser',],
+    long_description='SameBrowser',
+)
