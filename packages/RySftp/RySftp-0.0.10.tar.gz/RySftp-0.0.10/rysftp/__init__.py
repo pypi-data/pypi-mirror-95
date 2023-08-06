2f71a67ef02239ef49127dcae4cee3222c8e5349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:48 +0000

    Add package RySftp/0.0.10/RySftp-0.0.10.tar.gz

---
+++ packages/RySftp/RySftp-0.0.10.tar.gz/RySftp-0.0.10/rysftp/__init__.py
@@ -0,0 +1,2 @@
+from .sftp import RySftp
+from .utils import moveAndDateStamp, moveAndCopy
---
+++ packages/RySftp/RySftp-0.0.10.tar.gz/RySftp-0.0.10/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+    setuptools.setup(
+        name="RySftp",
+        version="0.0.10",
+        author="Ryan P Joyce",
+        author_email="ryan.joyce.88@gmail.com",
+        description="simple, asynchronous sftp operations",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/riptusk331/rysftp",
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'paramiko>=2.7.2',
+            "Werkzeug>=1.0.1"
+        ],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.8.5",
+    )
