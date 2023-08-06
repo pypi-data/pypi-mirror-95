b0ea14d2871e36d8b8c195b0dd475425425ea28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:18 +0000

    Add package exoctk/0.0.dev654/ExoCTK-0.0.dev654-cp37-cp37m-macosx_10_7_x86_64.whl

---
+++ packages/exoctk/ExoCTK-0.0.dev654-cp37-cp37m-macosx_10_7_x86_64.whl/ExoCTK/__init__.py
@@ -0,0 +1,19 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+The Exoplanet Characterization Tool Kit is a collection of packages used to reduce and analyze observations of transiting exoplanets
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from .core import *
+    from . import ldc
+    # from . import pal
+    from . import tot
+# from . import bar
