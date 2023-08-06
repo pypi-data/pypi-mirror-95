32408f8df96b434c4e907293ab2f88167caaaf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:44 +0000

    Add package trodesnetwork/0.0.2.dev0/trodesnetwork-0.0.2.dev0.tar.gz

---
+++ packages/trodesnetwork/trodesnetwork-0.0.2.dev0.tar.gz/trodesnetwork-0.0.2.dev0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="trodesnetwork",
+    version="0.0.2dev0",
+    description="A library to connect to Trodes over a network",
+    packages=["trodesnetwork"],
+)
+
