93dc2a9e66e14ad6017b3daf2c8ae344917ec5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:09 +0000

    Add package pdok-geopackage-validator/0.5/pdok_geopackage_validator-0.5-py3-none-any.whl

---
+++ packages/pdok-geopackage-validator/pdok_geopackage_validator-0.5-py3-none-any.whl/geopackage_validator/validations/layername_check.py
@@ -0,0 +1,28 @@
+from typing import Iterable, List
+
+from geopackage_validator.validations import validator
+from geopackage_validator.constants import SNAKE_CASE_REGEX
+
+
+def query_layernames(dataset) -> List[str]:
+    return [layer.GetName() for layer in dataset]
+
+
+class LayerNameValidator(validator.Validator):
+    """Layer names must start with a letter, and valid characters are lowercase a-z, numbers or underscores."""
+
+    code = 1
+    level = validator.ValidationLevel.ERROR
+    message = "Error layer: {layer}"
+
+    def check(self) -> Iterable[str]:
+        layernames = query_layernames(self.dataset)
+        return self.check_layernames(layernames)
+
+    def check_layernames(self, layernames: Iterable[str]):
+        assert layernames is not None
+        return [
+            self.message.format(layer=layername)
+            for layername in layernames
+            if not SNAKE_CASE_REGEX.fullmatch(layername)
+        ]
