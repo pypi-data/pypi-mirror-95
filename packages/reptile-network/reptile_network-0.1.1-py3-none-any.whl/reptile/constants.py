326c1799d48f9d270f0e8dfd1d01ed08990249b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:53 +0000

    Add package reptile-network/0.1.1/reptile_network-0.1.1-py3-none-any.whl

---
+++ packages/reptile-network/reptile_network-0.1.1-py3-none-any.whl/reptile/constants.py
@@ -0,0 +1,11 @@
+VERSION = '0.1.1'
+PACKAGE_NAME = 'reptile-network'
+
+REQUIREMENTS = [
+    'cupy==8.4.0',
+    'idx2numpy==1.2.3',
+    'numpy==1.20.1',
+]
+
+AUTHOR = 'sTonKs'
+AUTHOR_EMAIL = 'kisel@internet.ru' # Временно, пока не зарегаем нормальный домен и не сделаем нашу почту корповую
