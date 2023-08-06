ec25bc79b0e578f63125f207fb0c9946980d0152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:28 +0000

    Add package py-app-conf/0.3.1/py-app-conf-0.3.1.tar.gz

---
+++ packages/py-app-conf/py-app-conf-0.3.1.tar.gz/py-app-conf-0.3.1/pyappconf/encoding/ext_yaml.py
@@ -0,0 +1,66 @@
+from pathlib import Path, PosixPath
+
+from yaml.emitter import Emitter
+from yaml.representer import SafeRepresenter
+from yaml.resolver import Resolver
+from yaml.serializer import Serializer
+
+from pyappconf.encoding.general import HasStr
+
+
+def _represent_hasstr(obj, value: HasStr):
+    from yaml.representer import SafeRepresenter
+
+    return SafeRepresenter.represent_str(obj, str(value))
+
+
+class CustomRepresenter(SafeRepresenter):
+    pass
+
+
+CustomRepresenter.add_representer(Path, _represent_hasstr)
+CustomRepresenter.add_representer(PosixPath, _represent_hasstr)
+
+
+class CustomDumper(Emitter, Serializer, CustomRepresenter, Resolver):
+    def __init__(
+        self,
+        stream,
+        default_style=None,
+        default_flow_style=False,
+        canonical=None,
+        indent=None,
+        width=None,
+        allow_unicode=None,
+        line_break=None,
+        encoding=None,
+        explicit_start=None,
+        explicit_end=None,
+        version=None,
+        tags=None,
+        sort_keys=True,
+    ):
+        Emitter.__init__(
+            self,
+            stream,
+            canonical=canonical,
+            indent=indent,
+            width=width,
+            allow_unicode=allow_unicode,
+            line_break=line_break,
+        )
+        Serializer.__init__(
+            self,
+            encoding=encoding,
+            explicit_start=explicit_start,
+            explicit_end=explicit_end,
+            version=version,
+            tags=tags,
+        )
+        CustomRepresenter.__init__(
+            self,
+            default_style=default_style,
+            default_flow_style=default_flow_style,
+            sort_keys=sort_keys,
+        )
+        Resolver.__init__(self)
