0ba7957d6540841d1e87c70677f8845ff4dc3661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:00 +0000

    Add package mapply/0.1.6/mapply-0.1.6-py3-none-any.whl

---
+++ packages/mapply/mapply-0.1.6-py3-none-any.whl/mapply/mapply.py
@@ -0,0 +1,126 @@
+"""Submodule containing code to run PandasObject.apply() in parallel.
+
+Standalone usage (without init):
+::
+
+    import pandas as pd
+    from mapply.mapply import mapply
+
+    df = pd.DataFrame({"A": list(range(100))})
+
+    df["squared"] = mapply(df.A, lambda x: x ** 2, progressbar=False)
+"""
+from functools import partial
+from typing import Any, Callable, Tuple, Union
+
+from mapply.parallel import (
+    DEFAULT_CHUNK_SIZE,
+    DEFAULT_MAX_CHUNKS_PER_WORKER,
+    N_CORES,
+    multiprocessing_imap,
+)
+
+
+def _choose_n_chunks(
+    shape: Tuple[int, ...],
+    axis: int,
+    n_workers: int,
+    chunk_size: int,
+    max_chunks_per_worker: int,
+):
+    """Choose final amount of chunks to be sent to the ProcessPool."""
+    # no sense running parallel if data is too small
+    n_chunks = int(shape[axis] / chunk_size)
+
+    if max_chunks_per_worker:
+        # no sense making too many chunks
+        n_chunks = min(n_chunks, max_chunks_per_worker * N_CORES)
+    if n_chunks < 1 or n_workers == 1 or N_CORES == 1:
+        # no sense running parallel
+        n_chunks = 1
+
+    return n_chunks
+
+
+def mapply(
+    df_or_series: Any,
+    func: Callable,
+    axis: Union[int, str] = 0,
+    *,
+    n_workers: int = -1,
+    chunk_size: int = DEFAULT_CHUNK_SIZE,
+    max_chunks_per_worker: int = DEFAULT_MAX_CHUNKS_PER_WORKER,
+    progressbar: bool = True,
+    args=(),
+    **kwargs
+) -> Any:
+    """Run apply on n_workers. Split in chunks if sensible, gather results, and concat.
+
+    When using :meth:`mapply.init`, the signature of this method will behave the same as
+    :meth:`pandas.DataFrame.apply` or :meth:`pandas.Series.apply`.
+
+    Args:
+        df_or_series: Argument reserved to the class instance, a.k.a. 'self'.
+        func: func to apply to each column or row.
+        axis: Axis along which func is applied.
+        n_workers: Maximum amount of workers (processes) to spawn. Might be lowered
+            depending on chunk_size and max_chunks_per_worker. Will throw a warning if
+            set higher than is sensible (see :meth:`mapply.parallel.sensible_cpu_count`).
+        chunk_size: Minimum amount of columns/rows per chunk. Higher value means a higher
+            threshold to go multi-core. Set to 1 to let max_chunks_per_worker decide.
+        max_chunks_per_worker: Upper limit on amount of chunks per worker. Will lower
+            n_chunks determined by chunk_size if necessary. Set to 0 to skip this check.
+        progressbar: Whether to wrap the chunks in a :meth:`tqdm.auto.tqdm`.
+        args: Additional positional arguments to pass to func.
+        kwargs: Additional keyword arguments to pass to apply/func.
+
+    Returns:
+        Series or DataFrame resulting from applying func along given axis.
+
+    Raises:
+        ValueError: if a Series is passed in combination with axis=1
+    """
+    from numpy import array_split
+    from pandas import Series, concat
+
+    if isinstance(axis, str):
+        axis = ["index", "columns"].index(axis)
+
+    isseries = int(isinstance(df_or_series, Series))
+
+    if isseries and axis == 1:
+        raise ValueError("Passing axis=1 is not allowed for Series")
+
+    opposite_axis = 1 - (isseries or axis)
+
+    n_chunks = _choose_n_chunks(
+        df_or_series.shape,
+        opposite_axis,
+        n_workers,
+        chunk_size,
+        max_chunks_per_worker,
+    )
+
+    dfs = array_split(df_or_series, n_chunks, axis=opposite_axis)
+
+    def run_apply(func, df_or_series, args=(), **kwargs):
+        return df_or_series.apply(func, args=args, **kwargs)
+
+    if not isseries:
+        kwargs["axis"] = axis
+
+    results = multiprocessing_imap(
+        partial(run_apply, func, args=args, **kwargs),
+        dfs,
+        n_workers=n_workers,
+        progressbar=progressbar,
+    )
+
+    if (
+        not isseries
+        and len(results) > 1
+        and results[0].index.equals(df_or_series.index)
+    ):
+        return concat(results, axis=1, copy=False)
+
+    return concat(results, copy=False)
---
+++ packages/mapply/mapply-0.1.6-py3-none-any.whl/mapply/parallel.py
@@ -0,0 +1,103 @@
+"""Submodule containing code to distribute computation over multiple processes using :class:`pathos.multiprocessing.ProcessPool`.
+
+Standalone usage:
+::
+
+    from mapply.parallel import multiprocessing_imap
+
+    def some_heavy_computation(x, power):
+        return pow(x, power)
+
+    multicore_list = multiprocessing_imap(
+        some_heavy_computation,
+        range(100),
+        power=2.5,
+        progressbar=False,
+        n_workers=-1
+    )
+"""
+import logging
+from functools import partial
+from typing import Any, Callable, Iterable, List, Optional
+
+import psutil
+from pathos.multiprocessing import ProcessPool
+from tqdm.auto import tqdm
+
+logger = logging.getLogger(__name__)
+
+
+def sensible_cpu_count() -> int:
+    """Count amount of physical CPUs (+1 on hyperthreading systems to prioritize the workers over e.g. system processes)."""
+    return min(psutil.cpu_count(logical=False) + 1, psutil.cpu_count(logical=True))
+
+
+N_CORES = sensible_cpu_count()
+DEFAULT_CHUNK_SIZE = 100
+DEFAULT_MAX_CHUNKS_PER_WORKER = 8
+
+
+def _choose_n_workers(n_chunks: Optional[int], n_workers: int) -> int:
+    """Choose final amount of workers to be spawned for received input."""
+    if n_workers < 1:
+        n_workers = N_CORES
+    elif n_workers > N_CORES:
+        logger.warning(
+            "Using more workers (%d) than is sensible (%d). For CPU-bound operations, consider lowering n_workers to avoid bottlenecks on the physical CPUs",
+            n_workers,
+            N_CORES,
+        )
+
+    # no sense having more workers than chunks
+    if n_chunks is not None:
+        n_workers = min(n_workers, n_chunks)
+
+    return n_workers
+
+
+def multiprocessing_imap(
+    func: Callable,
+    iterable: Iterable[Any],
+    *,
+    n_workers: int = -1,
+    progressbar: bool = True,
+    args=(),
+    **kwargs
+) -> List[Any]:
+    """Execute func on each element in iterable on n_workers, ensuring order.
+
+    Args:
+        func: Function to apply to each element in iterable.
+        iterable: Input iterable on which to execute func.
+        n_workers: Amount of workers (processes) to spawn.
+        progressbar: Whether to wrap the chunks in a tqdm.auto.tqdm.
+        args: Additional positional arguments to pass to func.
+        kwargs: Additional keyword arguments to pass to func.
+
+    Returns:
+        Results in same order as input iterable.
+    """
+    n_chunks: Optional[int] = tqdm(iterable, disable=True).__len__()  # doesn't exhaust
+    func = partial(func, *args, **kwargs)
+
+    n_workers = _choose_n_workers(n_chunks, n_workers)
+
+    if n_workers <= 1:
+        # no sense spawning pool
+        pool = None
+        stage = map(func, iterable)
+    else:
+        logger.debug("Starting ProcessPool with %d workers", n_workers)
+        pool = ProcessPool(n_workers)
+
+        stage = pool.imap(func, iterable)
+
+    if progressbar:
+        stage = tqdm(stage)
+
+    try:
+        return list(stage)
+    finally:
+        if pool:
+            logger.debug("Closing ProcessPool")
+            pool.clear()
