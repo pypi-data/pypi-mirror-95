d33ea181eba28ffb094999ac170c151c121891b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:57 +0000

    Add package reusable/0.0.8/reusable-0.0.8-py3-none-any.whl

---
+++ packages/reusable/reusable-0.0.8-py3-none-any.whl/reusable/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Python reusable package
+
+A python package with reusable functions and utility classes. 
+"""
+__author__="Ohidur Rahman Bappy"
+__version__="0.0.8"
+__license__ = "MIT"
+__status__ = "Production"
+
+__all__=[
+    'AppConfig','Config','Namespace'
+]
+
+from .config import AppConfig,Config
+from .namespace import Namespace
