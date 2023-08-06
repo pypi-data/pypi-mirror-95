0d821959bae0b5d058c51348779284df8bf5aca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:46 +0000

    Add package postDB/0.2.3/postDB-0.2.3.tar.gz

---
+++ packages/postDB/postDB-0.2.3.tar.gz/postDB-0.2.3/postDB/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "0.2.3"
+__author__ = "Sylte"
+__licence__ = "MIT"
+__copyright__ = "Copyright (c) 2020 Sylte"
+__title__ = "postDB"
+
+import logging
+from collections import namedtuple
+
+from postDB.model.column import Column
+from postDB.model.model import Model
+
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+
+
+def get_version(version: tuple):
+    """Return a cleaned up version number from :data:`VERSION`."""
+    return "%s %s.%s.%s" % (version[3], version[0], version[1], version[2])
+
+
+VERSION = (0, 2, 3, "final", 0)
+version_info = VersionInfo(*VERSION)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
