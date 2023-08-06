bcb5ba903ca110dda48e25f8030a21e885b77871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:44 +0000

    Add package iatikit/3.0.2/iatikit-3.0.2.tar.gz

---
+++ packages/iatikit/iatikit-3.0.2.tar.gz/iatikit-3.0.2/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from os.path import abspath, dirname, join
+
+
+path = abspath(dirname(__file__))
+with open(join(path, 'README.rst')) as f:
+    readme = f.read()
+
+data = {}
+with open(join(path, 'iatikit', '__version__.py')) as f:
+    exec(f.read(), data)
+
+setup(
+    name='iatikit',
+    description='A toolkit for using IATI data.',
+    url='https://iatikit.readthedocs.io',
+    author='Andy Lulham',
+    author_email='a.lulham@gmail.com',
+    version=data.get('__version__'),
+    packages=find_packages(),
+    license='MIT',
+    keywords='IATI',
+    long_description=readme,
+    install_requires=[
+        'configparser',
+        'lxml',
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    setup_requires=['pytest-runner'],
+)
---
+++ packages/iatikit/iatikit-3.0.2.tar.gz/iatikit-3.0.2/tests/test_generic_set.py
@@ -0,0 +1,83 @@
+from os.path import abspath, dirname, join
+from unittest import TestCase
+
+from freezegun import freeze_time
+import pytest
+
+from iatikit.data.registry import Registry
+from iatikit.utils.exceptions import FilterError
+from iatikit.utils.config import CONFIG
+
+
+class TestGenericSet(TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestGenericSet, self).__init__(*args, **kwargs)
+        registry_path = join(dirname(abspath(__file__)),
+                             'fixtures', 'registry')
+        config_dict = {'paths': {'registry': registry_path}}
+        CONFIG.read_dict(config_dict)
+
+    @freeze_time("2015-12-02")
+    def setUp(self):
+        self.registry = Registry()
+
+    def test_set_get(self):
+        org_dataset = self.registry.datasets.get('fixture-org-org')
+        assert org_dataset.name == 'fixture-org-org'
+
+    def test_set_get_unknown(self):
+        unknown_dataset = self.registry.datasets.get('unknown')
+        assert unknown_dataset is None
+
+    def test_set_find(self):
+        org_dataset = self.registry.datasets.find(name='fixture-org-org')
+        assert org_dataset.name == 'fixture-org-org'
+
+    def test_set_not_found(self):
+        with pytest.raises(IndexError):
+            self.registry.datasets.find(name='not-a-dataset')
+
+    def test_set_first(self):
+        first_dataset = self.registry.datasets.first()
+        zero_index_dataset = self.registry.datasets[0]
+        assert first_dataset.name == zero_index_dataset.name
+
+    def test_set_count(self):
+        datasets = self.registry.datasets
+        assert datasets.count() == 5
+
+    def test_set_all(self):
+        dataset_names = [
+            'fixture-org-activities',
+            'fixture-org-activities2',
+            'fixture-org-org',
+            'old-org-acts',
+            'old-org-missing-acts',
+        ]
+        datasets = self.registry.datasets.all()
+        assert isinstance(datasets, list)
+        assert len(datasets) == 5
+        for dataset in datasets:
+            assert dataset.name in dataset_names
+
+    def test_set_filter_chaining(self):
+        act_datasets = self.registry.datasets.filter(filetype='activity')
+        no_datasets = act_datasets.filter(name='fixture-org-org')
+        assert no_datasets.count() == 0
+
+        org_datasets = self.registry.datasets.filter(filetype='organisation')
+        org_dataset = org_datasets.filter(name='fixture-org-org')
+        assert org_dataset.count() == 1
+
+    def test_set_where_chaining(self):
+        act_datasets = self.registry.datasets.where(filetype='activity')
+        no_datasets = act_datasets.where(name='fixture-org-org')
+        assert no_datasets.count() == 0
+
+        org_datasets = self.registry.datasets.where(filetype='organisation')
+        org_dataset = org_datasets.where(name='fixture-org-org')
+        assert org_dataset.count() == 1
+
+    def test_set_unknown_filter(self):
+        with pytest.raises(FilterError):
+            self.registry.datasets.where(unknown_filter='unknown')
