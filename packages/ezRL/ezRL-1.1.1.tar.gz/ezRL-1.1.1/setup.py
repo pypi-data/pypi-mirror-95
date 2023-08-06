62f931ff39afea5daa3e03a11e938da7875e1c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:54 +0000

    Add package ezRL/1.1.1/ezRL-1.1.1.tar.gz

---
+++ packages/ezRL/ezRL-1.1.1.tar.gz/ezRL-1.1.1/setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import setup
+
+with open("./README.md", encoding = "utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name = "ezRL",
+    version = "1.1.1",
+    description = "easy Reinforcement Learning tool for python",
+    author = "ezRL_team",
+    author_email = "ezrl.adm@gmail.com",
+    url = "https://github.co.jp/",
+    packages = ["ezRL", "ezRL/parts/catcher_game", "ezRL/parts/DQN_Agent", "ezRL/parts/normal_cnn"],
+    install_requires = ["relpath"],
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    license = "CC0 v1.0",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
+    ],
+    # entry_points = """
+    #     [console_scripts]
+    #     hoge = hoge:hoge
+    # """
+)
