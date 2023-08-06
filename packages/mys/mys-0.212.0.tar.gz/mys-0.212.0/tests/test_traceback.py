02bc4f9ada7ece134af142b13f4211f35cd1a623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:50 +0000

    Add package mys/0.212.0/mys-0.212.0.tar.gz

---
+++ packages/mys/mys-0.212.0.tar.gz/mys-0.212.0/tests/test_traceback.py
@@ -0,0 +1,139 @@
+import shutil
+import subprocess
+from unittest.mock import patch
+
+import mys.cli
+
+from .utils import Path
+from .utils import TestCase
+from .utils import remove_build_directory
+
+
+class Test(TestCase):
+
+    def run_test_assert(self, name, expected):
+        proc = subprocess.run(['./build/default/test', name],
+                              capture_output=True,
+                              text=True)
+        self.assertNotEqual(proc.returncode, 0)
+        self.assert_in(expected, proc.stderr + proc.stdout)
+
+    def run_app_assert(self, expected, not_expected=None):
+        proc = subprocess.run(['./build/default/app'],
+                              capture_output=True,
+                              text=True)
+        self.assertNotEqual(proc.returncode, 0)
+        self.assert_in(expected, proc.stderr)
+
+        if not_expected is not None:
+            self.assert_not_in(not_expected, proc.stderr + proc.stdout)
+
+    def test_traceback(self):
+        name = 'test_traceback'
+        remove_build_directory(name)
+
+        shutil.copytree('tests/files/traceback', f'tests/build/{name}')
+
+        with Path(f'tests/build/{name}'):
+            try:
+                with patch('sys.argv', ['mys', 'test', '-v']):
+                    mys.cli.main()
+            except SystemExit:
+                pass
+
+            self.run_test_assert(
+                'test_panic',
+                'Traceback (most recent call last):\n'
+                '  File: "./src/lib.mys", line 3 in test_panic\n'
+                '    print(""[1])\n'
+                '\n'
+                'Panic(message="String index 1 is out of range.")\n')
+
+            self.run_test_assert(
+                'test_panic_2',
+                'Traceback (most recent call last):\n'
+                '  File: "./src/lib.mys", line 11 in test_panic_2\n'
+                '    panic_2()\n'
+                '  File: "./src/lib.mys", line 7 in panic_2\n'
+                '    print(""[i])\n'
+                '\n'
+                'Panic(message="String index 10 is out of range.")\n')
+
+            self.run_test_assert(
+                'test_panic_in_except',
+                'Traceback (most recent call last):\n'
+                '  File: "./src/lib.mys", line 21 in test_panic_in_except\n'
+                '    print(b""[11])\n'
+                '\n'
+                'Panic(message="Bytes index 11 is out of range.")\n')
+
+            self.run_test_assert(
+                'test_panic_in_if',
+                'Traceback (most recent call last):\n'
+                '  File: "./src/lib.mys", line 26 in test_panic_in_if\n'
+                '    print(b""[5])\n'
+                '\n'
+                'Panic(message="Bytes index 5 is out of range.")\n')
+
+            self.run_test_assert(
+                'test_panic_in_else',
+                'Traceback (most recent call last):\n'
+                '  File: "./src/lib.mys", line 33 in test_panic_in_else\n'
+                '    print(b""[6])\n'
+                '\n'
+                'Panic(message="Bytes index 6 is out of range.")\n')
+
+            self.run_test_assert(
+                'test_panic_in_for',
+                'Traceback (most recent call last):\n'
+                '  File: "./src/lib.mys", line 38 in test_panic_in_for\n'
+                '    print(b"123"[i])\n'
+                '\n'
+                'Panic(message="Bytes index 3 is out of range.")\n')
+
+            self.run_test_assert(
+                'test_panic_in_while',
+                'Traceback (most recent call last):\n'
+                '  File: "./src/lib.mys", line 43 in test_panic_in_while\n'
+                '    print(b""[10])\n'
+                '\n'
+                'Panic(message="Bytes index 10 is out of range.")\n')
+
+            self.run_test_assert(
+                'test_panic_in_match',
+                'Traceback (most recent call last):\n'
+                '  File: "./src/lib.mys", line 49 in test_panic_in_match\n'
+                '    print(b""[-1])\n'
+                '\n'
+                'Panic(message="Bytes index -1 is out of range.")\n')
+
+            self.run_test_assert(
+                'test_error_in_runtime',
+                'Traceback (most recent call last):\n'
+                '  File: "./src/lib.mys", line 57 in test_error_in_runtime\n'
+                '    raise MyError(1, 2)\n'
+                '\n'
+                'MyError(x=1, y=2)\n')
+
+            # Debug build.
+            with patch('sys.argv', ['mys', 'build', '-o', 'debug']):
+                mys.cli.main()
+
+            self.run_app_assert(
+                'Traceback (most recent call last):\n'
+                '  File: "./src/main.mys", line 5 in main\n'
+                '    raise AnError("hi")\n'
+                '\n'
+                'AnError(message="hi")\n')
+
+            with patch('sys.argv', ['mys', 'clean']):
+                mys.cli.main()
+
+            # Speed build, no tracebacks.
+            with patch('sys.argv', ['mys', 'build']):
+                mys.cli.main()
+
+            self.run_app_assert('AnError(message="hi")\n',
+                                'Traceback (most recent call last):\n'
+                                '  File: "./src/main.mys", line 5 in main\n'
+                                '    raise AnError("hi")\n')
