2433eca69f9e921b668cf4e437769cf7c2ab485a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:07 +0000

    Add package cl1/1.2.9/cl1-1.2.9.tar.gz

---
+++ packages/cl1/cl1-1.2.9.tar.gz/cl1-1.2.9/cl1/classes/exceptions/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# exceptions.
+class EmptyArgumentError(Exception):
+	pass
+class ArgumentError(Exception):
+	pass
+class UnkownArgumentError(Exception):
+	pass
