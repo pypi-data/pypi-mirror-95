088d02b4b83d08beba996a5072db4107bda127fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:30 +0000

    Add package pyactors/0.1.0/pyactors-0.1.0.tar.gz

---
+++ packages/pyactors/pyactors-0.1.0.tar.gz/pyactors-0.1.0/README.md
@@ -0,0 +1,4 @@
+PYACTORS
+========
+
+Python asyncio based implementation of the [actor model](https://en.wikipedia.org/wiki/Actor_model).
---
+++ packages/pyactors/pyactors-0.1.0.tar.gz/pyactors-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    install_requires=["pydantic"],
+    name="pyactors",
+    version="0.1.0",
+    url="http://github.com/DanielSolomon/pyactors",
+    author="Daniel Solomon",
+    author_email="DanielSolomon94.ds@gmail.com",
+    license="MIT",
+    packages=["pyactors"],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
