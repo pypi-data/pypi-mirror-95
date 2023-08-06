e7f7cb3af086baac3292b8dc391bfcf10cdfc792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:05 +0000

    Add package aiozoom/0.0.8.6/aiozoom-0.0.8.6-py3-none-any.whl

---
+++ packages/aiozoom/aiozoom-0.0.8.6-py3-none-any.whl/aiozoom/components/dashboard.py
@@ -0,0 +1,18 @@
+from aiozoom.components.base import Base
+
+
+class Dashboard(Base):
+    """
+    Class describing `Dashboard` logic of Zoom API
+    learn more:
+    https://marketplace.zoom.us/docs/api-reference/zoom-api/dashboards/
+    """
+    async def get_meeting_participants(self, meeting_id: str) -> dict:
+        """
+        Get list of participants with detail information
+
+        Params:
+            meeting_id - unique id of meeting
+        """
+        method = f'metrics/meetings/{meeting_id}/participants?type=pastOne&page_size=100'
+        return await self.base_get_request(method)
---
+++ packages/aiozoom/aiozoom-0.0.8.6-py3-none-any.whl/aiozoom/config/client.py
@@ -0,0 +1,50 @@
+# pylint:disable=(missing-function-docstring)
+import typing as tp
+
+from aiohttp import ClientSession
+
+
+BASE_URL = 'https://api.zoom.us/v2'
+
+class Client:
+    """
+    Async client for making requests
+    """
+
+    def __init__(self, base_url: str = BASE_URL) -> None:
+        self.base_url = base_url
+
+    async def get(self, method: str, *args: tp.Any, **kwargs: tp.Any) -> tp.Any:
+        async with ClientSession() as session:
+            async with session.get(f'{self.base_url}/{method}', *args, **kwargs) as response:
+                data = await response.json()
+                return data
+
+    async def post(self, method: str, *args: tp.Any, **kwargs: tp.Any) -> tp.Any:
+        async with ClientSession() as session:
+            async with session.post(f"{self.base_url}/{method}", *args, **kwargs) as response:
+                data = await response.json()
+                return data
+
+    async def put(self, method: str, *args: tp.Any, **kwargs: tp.Any) -> tp.Any:
+        async with ClientSession() as session:
+            async with session.put(f"{self.base_url}/{method}", *args, **kwargs) as response:
+                raw_response = await response.text()
+                if response.status // 100 == 2 and raw_response == '':
+                    return {'status': 'success'}
+                data = await response.json()
+                return data
+
+    async def patch(self, method: str, *args: tp.Any, **kwargs: tp.Any) -> tp.Any:
+        async with ClientSession() as session:
+            async with session.patch(f"{self.base_url}/{method}", *args, **kwargs) as response:
+                data = await response.json()
+                return data
+
+    async def delete(self, method: str, *args: tp.Any, **kwargs: tp.Any) -> tp.Any:
+        async with ClientSession() as session:
+            async with session.delete(f"{self.base_url}/{method}", *args, **kwargs) as response:
+                data = await response.json()
+                return data
+
+client = Client()
