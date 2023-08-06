ad172cb8639df86b8fcabc42346b85d26b9f411f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:49 +0000

    Add package trieres/0.5.3/trieres-0.5.3.tar.gz

---
+++ packages/trieres/trieres-0.5.3.tar.gz/trieres-0.5.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="trieres", 
+    
+    version="0.5.3",
+    author="Dionysios Diamantopoulos",
+    author_email="did@zurich.ibm.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cloudFPGA/trieres",
+    # this package contains one module,
+    # which resides in the subdirectory mymodule
+    packages=setuptools.find_packages(),
+
+    # make sure the shared library is included
+    package_data={'trieres': ['_trieres.so']},
+    include_package_data=True,
+    
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.7',
+)
