2289c16364c4b547210ae22fd032836dd894a258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:21 +0000

    Add package pyfuseki/1.0.3/pyfuseki-1.0.3.tar.gz

---
+++ packages/pyfuseki/pyfuseki-1.0.3.tar.gz/pyfuseki-1.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyfuseki",
+    version="1.0.3",
+    author="Bin Yu",
+    author_email="yubin_SkyWalker@yeah.net",
+    description="An easy way to mix together OWL and Jena Fuseki.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yubinCloud/pyfuseki",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Artistic License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['httpx', 'rdflib', 'pydantic', 'SPARQLWrapper'],
+    python_requires='>=3.6',
+)
