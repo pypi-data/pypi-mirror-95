41adec363ddf769aec71136c5d5b7428efbf891c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:36 +0000

    Add package pyhesive/0.2.2/pyhesive-0.2.2.tar.gz

---
+++ packages/pyhesive/pyhesive-0.2.2.tar.gz/pyhesive-0.2.2/pyhesive/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+if sys.version_info <= (3, 0):
+    raise ImportError("Must use python3")
+
+from .__about__ import __version__
+from ._mesh import Mesh
+
+__all__ = ["Mesh", "__version__"]
---
+++ packages/pyhesive/pyhesive-0.2.2.tar.gz/pyhesive-0.2.2/setup.py
@@ -0,0 +1,54 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyhesive", # Replace with your own username
+    version="0.2.2",
+    author="Jacob Faibussowitsch",
+    author_email="jacob.fai@gmail.com",
+    description="Insert cohesive elements into any mesh",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/Jfaibussowitsch/pyhesive",
+    packages=setuptools.find_packages(),
+    scripts = ["bin/pyhesive-insert"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Multimedia :: Graphics :: 3D Modeling",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities"
+    ],
+    keywords = [
+        "mesh",
+        "scientific",
+        "engineering",
+        "fem",
+        "finite elements",
+        "fracture",
+        "fracture mechanics",
+        "cohesive elements"
+    ],
+    install_requires = [
+        'numpy',
+        'scipy',
+        'meshio',
+        'pymetis'
+    ],
+    python_requires=">=3.4",
+    tests_require = [
+        "pytest",
+        "pytest-xdist",
+        "pytest-cov",
+        "black",
+        "snakeviz",
+    ],
+    test_suite="pyhesive.test"
+)
