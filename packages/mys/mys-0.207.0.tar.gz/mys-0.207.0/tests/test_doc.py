bdcd08a0b1c84790b6e701e295421df34bc358c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:07 +0000

    Add package mys/0.207.0/mys-0.207.0.tar.gz

---
+++ packages/mys/mys-0.207.0.tar.gz/mys-0.207.0/tests/test_doc.py
@@ -0,0 +1,43 @@
+import shutil
+from html.parser import HTMLParser
+from unittest.mock import patch
+
+import mys.cli
+
+from .utils import Path
+from .utils import TestCase
+from .utils import read_file
+from .utils import remove_build_directory
+
+
+class HTMLText(HTMLParser):
+
+    def __init__(self):
+        super().__init__()
+        self.text = ''
+
+    def handle_data(self, data):
+        self.text += data
+
+
+class Test(TestCase):
+
+    def test_doc(self):
+        name = 'test_doc'
+        remove_build_directory(name)
+
+        shutil.copytree('tests/files/doc', f'tests/build/{name}')
+
+        with Path(f'tests/build/{name}'):
+            with patch('sys.argv', ['mys', '-d', 'doc', '-v']):
+                mys.cli.main()
+
+            self.assert_file_exists('build/doc/html/index.html')
+
+            html_text = HTMLText()
+            html_text.feed(read_file('build/doc/html/index.html'))
+
+            with open('index.txt', 'w') as fout:
+                fout.write(html_text.text)
+
+            self.assert_files_equal('index.txt', '../../files/doc/index.txt')
