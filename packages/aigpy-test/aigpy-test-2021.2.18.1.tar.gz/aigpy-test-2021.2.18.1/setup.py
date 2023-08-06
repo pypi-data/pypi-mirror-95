5f4267c61300a10607b09985bc522fc75cb6d51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:57 +0000

    Add package aigpy-test/2021.2.18.1/aigpy-test-2021.2.18.1.tar.gz

---
+++ packages/aigpy-test/aigpy-test-2021.2.18.1.tar.gz/aigpy-test-2021.2.18.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'aigpy-test',
+    version = '2021.2.18.1',
+    license = "MIT Licence",
+    description = "Python Common Tool",
+
+    author = 'Yaronzz',
+    author_email = "yaronhuang@foxmail.com",
+
+    packages = find_packages(),
+    platforms = "any",
+    include_package_data = True,
+    install_requires=["requests", "colorama", "mutagen"],
+)
