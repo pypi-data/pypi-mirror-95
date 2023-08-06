49251d991ba48f854074ffdc0a9d24ba8d38879e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:52 +0000

    Add package mw2fcitx/0.10.2/mw2fcitx-0.10.2-py3-none-any.whl

---
+++ packages/mw2fcitx/mw2fcitx-0.10.2-py3-none-any.whl/mw2fcitx/build_dict.py
@@ -0,0 +1,30 @@
+import logging
+import sys
+
+from mw2fcitx.pipeline import MWFPipeline
+
+
+def build(config):
+    config["source"] = config["source"] or {}
+    config["tweaks"] = config["tweaks"] or []
+    config["converter"] = config["converter"] or {}
+    config["generator"] = config["generator"] or []
+    pipeline = MWFPipeline(config["source"].get("api_path"))
+    if config["source"].get("api_path") is None:
+        title_file_path = config["source"].get("file_path")
+        if title_file_path is None:
+            logging.error("No api_path or file_path provided. Stop.")
+            sys.exit(1)
+        pipeline.load_titles_from_file(title_file_path,
+                                       **config["source"].get("kwargs"))
+    else:
+        pipeline.fetch_titles(**config["source"].get("kwargs"))
+    pipeline.convert_to_words(config["tweaks"])
+    pipeline.export_words(config["converter"].get("use"),
+                          **config["converter"].get("kwargs"))
+    generators = config["generator"]
+    if not isinstance(generators, list):
+        generators = [generators]
+    for gen in generators:
+        pipeline.generate_dict(gen.get("use"), **gen.get("kwargs"))
+    return pipeline.dict
---
+++ packages/mw2fcitx/mw2fcitx-0.10.2-py3-none-any.whl/mw2fcitx/fetch.py
@@ -0,0 +1,58 @@
+import json
+from urllib.request import Request, urlopen
+from urllib.parse import quote_plus
+
+from mw2fcitx.utils import console
+from mw2fcitx.retry import retry
+
+
+class StatusError(Exception):
+
+    def __init__(self, code):
+        super().__init__("HTTP status is {}".format(code))
+
+
+@retry()
+def open_request(url):
+    return urlopen(
+        Request(
+            url,
+            headers={
+                "User-Agent":
+                    "MW2Fcitx/1.0; github.com/outloudvi/fcitx5-pinyin-moegirl"
+            }))
+
+
+def fetch_as_json(url):
+    res = open_request(url)
+    if res.status == 200:
+        return json.loads(res.read())
+    console.error("Error fetching URL {}".format(url))
+    raise StatusError(res.status)
+
+
+def fetch_all_titles(api_url, **kwargs):
+    limit = kwargs.get("title_limit") or -1
+    console.debug("Fetching titles from {}".format(api_url) +
+                  (" with a limit of {}".format(limit) if limit != -1 else ""))
+    titles = []
+    data = fetch_as_json(api_url + "?action=query&list=allpages&format=json")
+    breakNow = False
+    while True:
+        for i in map(lambda x: x["title"], data["query"]["allpages"]):
+            titles.append(i)
+            if limit != -1 and len(titles) >= limit:
+                breakNow = True
+                break
+        console.debug("Got {} pages".format(len(titles)))
+        if breakNow:
+            break
+        if "continue" in data:
+            data = fetch_as_json(
+                api_url +
+                "?action=query&list=allpages&format=json&aplimit=max&apcontinue={}"
+                .format(quote_plus(data["continue"]["apcontinue"])))
+        else:
+            break
+    console.info("Finished.")
+    return titles
