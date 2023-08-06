65c1b5e76c23701e6e5626c87632441714de89ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:51 +0000

    Add package badar-calc/0.0.2/badar-calc-0.0.2.tar.gz

---
+++ packages/badar-calc/badar-calc-0.0.2.tar.gz/badar-calc-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3.5',
+    ]
+setup(
+  
+    name='badar-calc',
+    version='0.0.2',
+    description='Very basic calculator',
+    url='',
+    author='Badrudin Mohamed Ali',
+    author_email='badrudin.dev@gmail.com',
+    license='MIT',
+    packages=[],
+    classifiers=classifiers,
+    install_requires=[],
+)
