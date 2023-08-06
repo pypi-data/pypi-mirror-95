64c487aafb55ee30a98dda02d6a95b1f52e26e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:25 +0000

    Add package django-tracking-fields/1.3.0/django_tracking_fields-1.3.0-py3-none-any.whl

---
+++ packages/django-tracking-fields/django_tracking_fields-1.3.0-py3-none-any.whl/tracking_fields/tests/urls.py
@@ -0,0 +1,10 @@
+try:
+    from django.urls import re_path
+except ImportError:
+    from django.conf.urls import url as re_path
+from django.contrib import admin
+
+
+urlpatterns = (
+    re_path(r'^admin/', admin.site.urls),
+)
