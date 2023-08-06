63761861120d29d4525fc796c55ba0f251fdf807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:07 +0000

    Add package ts-app/0.0.6/ts_app-0.0.6.tar.gz

---
+++ packages/ts-app/ts_app-0.0.6.tar.gz/ts_app-0.0.6/ts_app/__init__.py
@@ -0,0 +1,18 @@
+from ts_app.index import server
+import os
+from waitress import serve
+import webbrowser
+
+
+def run_app():
+    """Start the app server, and launch a browser to view it."""
+
+    # Open a tab in default browser and point it to the app
+    webbrowser.open('http://localhost:8000')
+
+    # Serve the app using waitress
+    serve(server, host='localhost', port=8000)
+
+    # Remove the temporary data store
+    if os.path.isfile('ts-app-data.temp'):
+        os.remove('ts-app-data.temp')
