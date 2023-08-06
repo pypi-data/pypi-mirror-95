46e92661108c6ec3627a1ec609ae823cef1ce44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:39 +0000

    Add package zwave-js-server-python/0.18.0/zwave_js_server_python-0.18.0-py3-none-any.whl

---
+++ packages/zwave-js-server-python/zwave_js_server_python-0.18.0-py3-none-any.whl/zwave_js_server/model/device_class.py
@@ -0,0 +1,51 @@
+"""
+Model for a Zwave Node's device class.
+
+https://zwave-js.github.io/node-zwave-js/#/api/node?id=deviceclass
+"""
+
+from typing import List, Optional, TypedDict
+
+
+class DeviceClassDataType(TypedDict, total=False):
+    """Represent a device class data dict type."""
+
+    basic: str
+    generic: str
+    specific: str
+    mandatorySupportedCCs: List[str]
+    mandatoryControlCCs: List[str]
+
+
+class DeviceClass:
+    """Model for a Zwave Node's device class."""
+
+    def __init__(self, data: DeviceClassDataType) -> None:
+        """Initialize."""
+        self.data = data
+
+    @property
+    def basic(self) -> Optional[str]:  # TODO: Is this really optional?
+        """Return basic property."""
+        return self.data.get("basic")
+
+    @property
+    def generic(self) -> Optional[str]:
+        """Return generic property."""
+        return self.data.get("generic")
+
+    @property
+    def specific(self) -> Optional[str]:
+        """Return specific property."""
+        return self.data.get("specific")
+
+    @property
+    def mandatory_supported_ccs(self) -> List[str]:
+        """Return mandatorySupportedCCs property."""
+        return self.data.get("mandatorySupportedCCs", [])
+
+    @property
+    def mandatory_controlled_ccs(self) -> List[str]:
+        """Return mandatoryControlledCCs property."""
+        # TODO: either there's a typo in the docs or the implementation
+        return self.data.get("mandatoryControlCCs", [])
