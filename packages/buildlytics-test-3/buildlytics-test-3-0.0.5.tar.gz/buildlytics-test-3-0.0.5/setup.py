e04e4e4aa882d348701b89a0501a0f4a10a9248b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:54 +0000

    Add package buildlytics-test-3/0.0.5/buildlytics-test-3-0.0.5.tar.gz

---
+++ packages/buildlytics-test-3/buildlytics-test-3-0.0.5.tar.gz/buildlytics-test-3-0.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "buildlytics-test-3",
+    packages=['EDALIB'],
+    version="0.0.5",
+    platforms='any',
+    description="EDA Package Test ",
+    install_requires=[
+          'numpy',
+          'pandas',
+          'seaborn'
+      ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering'
+    ],
+    license="MIT"
+)
