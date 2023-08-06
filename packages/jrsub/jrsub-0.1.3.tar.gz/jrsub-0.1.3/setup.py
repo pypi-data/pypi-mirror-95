5cf0bca375363bfbdbba8cc7bd008daf7d865520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:52 +0000

    Add package jrsub/0.1.3/jrsub-0.1.3.tar.gz

---
+++ packages/jrsub/jrsub-0.1.3.tar.gz/jrsub-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jrsub']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['jtran>=0.2.13,<0.3.0', 'tqdm>=4.56.2,<5.0.0']
+
+setup_kwargs = {
+    'name': 'jrsub',
+    'version': '0.1.3',
+    'description': 'Python package for working with Warodai and Yarxi dictionaries',
+    'long_description': '# JRSub\n[![PyPI](https://img.shields.io/pypi/v/jrsub.svg)](https://pypi.python.org/pypi/jrsub)\n[![Build Status](https://travis-ci.com/kateabr/jsub.svg?branch=master)](https://travis-ci.com/kateabr/jsub)',
+    'author': 'Ekaterina Biryukova',
+    'author_email': 'kateabr@yandex.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/kateabr/jrsub',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
