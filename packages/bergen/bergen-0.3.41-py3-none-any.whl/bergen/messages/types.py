8dbd4c8ee6fb9c1273757586078412c24f024375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:50 +0000

    Add package bergen/0.3.41/bergen-0.3.41-py3-none-any.whl

---
+++ packages/bergen/bergen-0.3.41-py3-none-any.whl/bergen/messages/types.py
@@ -0,0 +1,8 @@
+#Base Types
+EXCEPTION = "exception"
+ALLOWANCE = "allowance"
+
+ASSIGNATION = "assignation"
+PROVISION = "provision"
+ASSIGNATION_REQUEST = "assignation_request"
+PROVISION_REQUEST = "provision_request"
