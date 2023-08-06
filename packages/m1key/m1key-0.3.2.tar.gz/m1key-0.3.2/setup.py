1e5cd3d8d1fe7322c3c14f48fd52ca3687d008a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:41 +0000

    Add package m1key/0.3.2/m1key-0.3.2.tar.gz

---
+++ packages/m1key/m1key-0.3.2.tar.gz/m1key-0.3.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3',
+  'Programming Language :: Python :: 3.7',
+  'Programming Language :: Python :: 3.6'
+]
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='m1key',
+    version='0.3.2',
+    description='A resume based package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='',
+    author='Mithilesh Tiwari',
+    author_email='shubhamtiwari.tiwari84@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    entry_points={
+        "console_scripts": ["m1key=m1key.__main__:main"]
+    },
+    keywords='m1key',
+    packages=find_packages(),
+    include_package_data = True,
+    install_requires=['opencv-python','requests','matplotlib','numpy','scikit-learn','Pillow','anytree']
+)
