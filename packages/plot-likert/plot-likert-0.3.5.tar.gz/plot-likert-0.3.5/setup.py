8d3eaa263cbb6bd7dbe60b927827b864832b9a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:00 +0000

    Add package plot-likert/0.3.5/plot-likert-0.3.5.tar.gz

---
+++ packages/plot-likert/plot-likert-0.3.5.tar.gz/plot-likert-0.3.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plot-likert",
+    version="0.3.5",
+    author="nmalkin",
+    description="Library to visualize results from Likert-style survey questions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords = 'plot graph visualize likert survey matplotlib',
+    url="https://github.com/nmalkin/plot-likert",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Visualization"
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+    ],
+)
