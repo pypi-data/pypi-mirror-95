4d98e1ea26ffbdfcb17bc968b8d5a8fde2179de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:52 +0000

    Add package aiplatgo/0.0.0.14/aiplatgo-0.0.0.14.tar.gz

---
+++ packages/aiplatgo/aiplatgo-0.0.0.14.tar.gz/aiplatgo-0.0.0.14/aiplatgo/gcs.py
@@ -0,0 +1,116 @@
+import os
+import re
+from pathlib import Path
+import pandas as pd
+import mproc
+import gcs_helpers.fetch as gfetch
+#
+# CONSTANTS
+#
+FETCH_CMD="gsutil -m -q cp -r {} {}"
+LS_CMD="gsutil ls {}"
+GS='gs://'
+MAX_PROCESSES=64
+
+
+#
+# PUBLIC
+#
+def download_from_storage(
+        dest=None,
+        srcs=None,
+        uri=None,
+        bucket=None,
+        folder=None,
+        dry_run=False ):
+    if dest:
+        dest=_prepare_destination(dest)
+    else:
+        raise ValueError('aiplatgo.gcs: `dest` kwarg must be provided')
+    if not uri:
+        uri="/".join([p for p in ['gs:/',bucket,folder] if p])
+    if isinstance(srcs,str):
+        srcs=[srcs]
+    dests=[]
+    if srcs:
+        for s in srcs:
+            _dest=_prepare_destination(dest)
+            _uri=f'{uri}/{s}'
+            cmd=FETCH_CMD.format(_uri,_dest)
+            print(cmd)
+            if dry_run:
+                print('--dry_run:')
+                cmd=LS_CMD.format(_uri)
+            os.system(cmd)
+            dests.append(_dest)
+    else:
+        cmd=FETCH_CMD.format(uri,dest)
+        print(cmd)
+        if dry_run:
+            print('--dry_run:')
+            cmd=LS_CMD.format(uri)
+        os.system(cmd)
+    return dest, dests
+
+
+def download_from_dataset(
+        dataset,
+        dest_root,
+        bucket=None,
+        keys=['path'],
+        max_processes=MAX_PROCESSES,
+        safe=True,
+        dry_run=False):
+    if isinstance(dataset,str):
+        dataset=[dataset]
+    if isinstance(dataset,list):
+        if isinstance(dataset[0],str):
+            dataset=[pd.read_csv(d) for d in dataset]
+        df=pd.concat(dataset)
+    else:
+        df=dataset
+    uris=df[keys].drop_duplicates().values.tolist()
+    def _down(uris):
+        return download_uris(
+            uris,
+            dest_root=dest_root,
+            bucket=bucket,
+            safe=safe,
+            dry_run=dry_run)
+    return mproc.map_with_threadpool(_down,uris,max_processes=max_processes)
+
+
+def download_uris(uris,dest_root='.',bucket=None,safe=True,dry_run=False):
+    if isinstance(uris,str):
+        uris=[uris]
+    return [download_uri(u,dest_root,bucket=bucket,safe=safe,dry_run=dry_run) for u in uris]
+
+
+def download_uri(uri,dest_root='.',bucket=None,safe=True,dry_run=False):
+    path=re.sub(f'^{GS}','',uri)
+    if bucket:
+        path=re.sub(f'{bucket}/','',path)
+        uri=f'{GS}{bucket}/{path}'
+    else:
+        path="/".join(path.split('/')[1:])
+    dest=Path(f'{dest_root}/{path}')
+    if dry_run or (safe and dest.is_file()):
+        return dest
+    else:
+        dest.parent.mkdir(parents=True, exist_ok=True)
+        return gfetch.blob(path=uri,dest=str(dest))
+
+
+
+#
+# INTERNAL
+#
+def _prepare_destination(*args):
+    dest='/'.join([a for a in args if a])
+    path=Path(dest)
+    if '.' in path.name:
+        path.parent.mkdir(parents=True, exist_ok=True)
+    else:
+        path.mkdir(parents=True, exist_ok=True)
+    return dest
+
---
+++ packages/aiplatgo/aiplatgo-0.0.0.14.tar.gz/aiplatgo-0.0.0.14/aiplatgo/parser.py
@@ -0,0 +1,145 @@
+import re
+from . import utils
+from pprint import pprint
+
+NO_DEFALUT_VALUE='_no_default_value'
+MISSING_KEY_ERROR=(
+    '{} is not in parser.config. '
+    'provide a default value other than None or '
+    'use required=True to allow' )
+
+
+class Parser(object):
+    """ Parser for user args passed through `ai-platform (local) job`
+
+    Args:
+
+        - args_config<str|dict>: config or path to yaml config file with defaults and per method args
+        - config_list<list>: 
+            * a flat list of key/value pairs. note: this is how ai-platform passes user args
+            * keys: config_list[2n]
+            * values: config_list[2n+1]
+        - config<dict|None>: dictionary containing arg key-values to parse (if config_list not provided)
+        - boolify<bool>: if true turn true/false strings into bool True/False
+
+
+    Usage:
+
+    (using click)
+
+    @click.pass_context
+    def run(ctx):
+        parser=Parser('args_config.yaml',config_list=ctx.args)
+        
+        train_model=parser.get('train_model',True)
+        score_model=parser.get('score_model',False)
+        
+        ... 
+
+        model=build_model(
+            optimizer,
+            loss,
+            metrics,
+            model_dir,
+            **parser.args('build_model'))
+
+    """
+    @staticmethod
+    def cli_args(args):
+        return {  _key(args[i]): _value(args[i+1]) 
+                  for i in range(0,len(args),2) }
+
+
+    def __init__(self,args_config,config_list=None,config=None,boolify=True):
+        if isinstance(args_config,str):
+            args_config=utils.read_yaml(args_config)
+        self.args_config=args_config
+        self.defaults=args_config.get('defaults',{})
+        if config_list:
+            config=Parser.cli_args(config_list)
+        self.config=config
+        self.boolify=boolify
+
+
+    def args(self,args_key,config=None,defaults={}):
+        config=config or self.config
+        defaults=defaults or self.defaults
+        _args={}
+        for k in self.args_config.get(args_key,{}):
+            k,v=self._get_key_value(k,config,defaults)
+            _args[k]=v
+        return _args
+
+
+    def get(self,key,default=None,required=False):
+        value=self.config.get(key,default)
+        if required and (value is None):
+            raise KeyError(MISSING_KEY_ERROR.format(key))
+        else:
+            return self._process_value(value)
+
+
+    #
+    # INTERNAL
+    #
+    def _get_key_value(self,key,config,defaults):
+        if isinstance(key,str):
+            default=defaults.get(key,NO_DEFALUT_VALUE)
+            if default==NO_DEFALUT_VALUE:
+                value=config[key]
+            else: 
+                value=config.get(key,default)
+        else:
+            default=list(key.values())[0]
+            key=list(key.keys())[0]
+            value=config.get(key,default)
+        return key, self._process_value(value)
+
+
+    def _process_value(self,value):
+        if self.boolify and isinstance(value,str):
+            if value.lower()=='false':
+                value=False
+            elif value.lower()=='true':
+                value=True
+        return value
+
+
+
+
+
+#
+# INTERNAL
+#
+def _key(key):
+    key=re.sub('^--','',key)
+    return utils.to_underscore(key)
+
+
+def _value(value):
+    try:
+        _v=str(value).lower()
+        if _v=='true':
+            value=True
+        elif _v=='false':
+            value=False
+        elif _v in ['none','null']:
+            value=None
+        elif _is_list_str(value):
+            value=value.split(',')
+            if value[-1] is '':
+                value=value[:-1]
+        else:
+            value=float(value)
+            if (value==value//1): 
+                value=int(value)
+    except:
+        pass
+    return value
+
+
+def _is_list_str(value):
+    if (',' in value) and (' ' not in value):
+        return True
+    else:
+        return False
