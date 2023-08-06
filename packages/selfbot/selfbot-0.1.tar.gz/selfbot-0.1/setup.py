2814fdb668dc656c23dc56cf4f2232883fb9bf92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:03 +0000

    Add package selfbot/0.1/selfbot-0.1.tar.gz

---
+++ packages/selfbot/selfbot-0.1.tar.gz/selfbot-0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'selfbot',         # How you named your package folder (MyLib)
+  packages = ['selfbot'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'epic discord exploit api wrapper',   # Give a short description about your library
+  author = 'YOUR NAME',                   # Type in your name
+  author_email = 'your.email@domain.com',      # Type in your E-Mail
+  url = 'https://github.com/rqzu/selfbot.py',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['discord.py', 'selfbot', 'discord-selfbot'],   # Keywords that define your package best
+  classifiers=[
+    'Programming Language :: Python :: 3.6',
+  ],
+)
