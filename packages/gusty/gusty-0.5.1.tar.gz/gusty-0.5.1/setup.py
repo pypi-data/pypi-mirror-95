7dd5e12a3d2daf29044a1a688f00668236034ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:11 +0000

    Add package gusty/0.5.1/gusty-0.5.1.tar.gz

---
+++ packages/gusty/gusty-0.5.1.tar.gz/gusty-0.5.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gusty",
+    version="0.5.1",
+    author="Chris Cardillo",
+    author_email="cfcardillo23@gmail.com",
+    description="Making DAG construction easier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chriscardillo/gusty",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'apache-airflow',
+          'inflection',
+          'jupytext',
+          'nbformat',
+          'python-frontmatter'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
