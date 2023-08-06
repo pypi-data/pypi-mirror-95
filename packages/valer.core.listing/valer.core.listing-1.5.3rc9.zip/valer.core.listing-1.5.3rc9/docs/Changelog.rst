9f8736041429ab5d09171b43eee5f98e207007ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:09 +0000

    Add package valer.core.listing/1.5.3rc9/valer.core.listing-1.5.3rc9.zip

---
+++ packages/valer.core.listing/valer.core.listing-1.5.3rc9.zip/valer.core.listing-1.5.3rc9/docs/Changelog.rst
@@ -0,0 +1,65 @@
+1.5.3 (unreleased)
+------------------
+
+- #36 Fix unpredictable ZPublisher's AttributeError on MultiSelect submit
+- #31 Dismiss items if cleared by subscribers
+
+
+1.5.2 (2020-08-05)
+------------------
+
+- Missing files added over MANIFEST.in
+
+
+1.5.1 (2020-08-05)
+------------------
+
+- Fixed release package
+
+
+1.5.0 (2020-08-04)
+------------------
+
+- #28 Remove classic listing mode and improve folderitems
+
+
+1.4.0 (2020-03-01)
+------------------
+
+- #25 Added tab index to result input fields
+- #24 Improved column sorting and index lookup
+- #23 Fix column config error
+
+
+1.3.0 (2019-10-26)
+------------------
+
+- #21 Custom Column Configuration
+- #20 Updated build system and JS package versions
+
+
+1.2.0 (2019-07-01)
+------------------
+
+- #19 Omit disabled items when "select all" checkbox is selected
+- #18 Support for string fields (added StringField react component)
+- #17 Send the original query string with API calls
+- #15 Allow custom button CSS definition in transition object
+- #14 Convert URLs/Paths to absolute URLs
+- #11 Notify edited event on set fields
+
+
+1.1.0 (2019-03-30)
+------------------
+
+- #9 Show status messages on API errors
+- #9 Only fetch affected folderitems by UID after a field was updated
+- #7 Hide comment toggle in transposed cell when remarks are disabled
+- #6 Allow to sort columns on catalog metadata columns
+- #5 Detection Limit handling in the Frontend/Backend
+
+
+1.0.0 (2019-02-04)
+------------------
+
+- Initial Release
---
+++ packages/valer.core.listing/valer.core.listing-1.5.3rc9.zip/valer.core.listing-1.5.3rc9/setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = "1.5.3rc9"
+
+with open("docs/About.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("docs/Changelog.rst", "r") as fh:
+    long_description += "\n\n"
+    long_description += "Changelog\n"
+    long_description += "=========\n"
+    long_description += fh.read()
+
+setup(
+    name="valer.core.listing",
+    version=version,
+    description="ReactJS powered listing tables for SENAITE LIMS",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+    ],
+    keywords=['senaite', 'lims', 'opensource', 'reactjs'],
+    
+    author="Valer Group LLC",
+    author_email="valerio.zhang@valer.us",
+    url="https://github.com/valeriozhang/senaite.core.listing",
+    
+    license="GPLv2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    package_dir={"": "src"},
+    namespace_packages=["senaite", "senaite.core"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "valer.core==1.3.5rc9",
+    ],
+    extras_require={
+        "test": [
+            "Products.PloneTestCase",
+            "plone.app.testing",
+            "unittest2",
+        ]
+    },
+    entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
