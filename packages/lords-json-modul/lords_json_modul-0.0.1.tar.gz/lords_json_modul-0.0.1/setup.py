dfa2c392a901221607f4cb1ffcae2b86c69426fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:44 +0000

    Add package lords-json-modul/0.0.1/lords_json_modul-0.0.1.tar.gz

---
+++ packages/lords-json-modul/lords_json_modul-0.0.1.tar.gz/lords_json_modul-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='lords_json_modul',
+    version='0.0.1',
+    description='A very basic json addon.',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Eike Kraus',
+    author_email='LordofGamemode@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='json',
+    packages=find_packages(),
+    install_requires=['json']
+)
