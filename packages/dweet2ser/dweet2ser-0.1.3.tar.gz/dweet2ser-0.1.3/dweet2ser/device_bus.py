ad0719d6442f4540660a4723deeac1b7d5cf6b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:21 +0000

    Add package dweet2ser/0.1.3/dweet2ser-0.1.3.tar.gz

---
+++ packages/dweet2ser/dweet2ser-0.1.3.tar.gz/dweet2ser-0.1.3/dweet2ser/device_bus.py
@@ -0,0 +1,169 @@
+import threading
+import time
+
+from termcolor import colored
+from colorama import init as colorama_init, Fore, Style
+
+from dweet2ser.settings import timestamp, internet_connection, s_print
+
+colorama_init()
+
+
+def _print_device_list(dev_list):
+    """
+    Prints a list of devices in table form.
+    """
+    cols = ["#".ljust(3),
+            "Name".ljust(16),
+            "Type".ljust(10),
+            "Port".ljust(15),
+            "ThingName".ljust(20),
+            "Locked".ljust(10),
+            "Muted".ljust(10)
+            ]
+    header = ''
+    for col in cols:
+        header = header + f"{col}  "
+    print(f"\t{Fore.LIGHTWHITE_EX}{header}{Style.RESET_ALL}")
+
+    for i in range(0, len(dev_list)):
+        num = str(i + 1)
+        d = dev_list[i]
+        if type(d).__name__ == "LocalDevice":
+            print(f"\t"
+                  f"{num.ljust(3)}  "
+                  f"{d.name.ljust(16)}  "
+                  f"{colored(d.type.ljust(10), d.type_color)}  "
+                  f"{d.port_name.ljust(15)}  "
+                  f"{''.ljust(20)}  "
+                  f"{''.ljust(10)}  "
+                  f"{str(d.mute).ljust(10)}  "
+                  )
+        if type(d).__name__ == "RemoteDevice":
+            print(f"\t"
+                  f"{num.ljust(3)}  "
+                  f"{d.name.ljust(16)}  "
+                  f"{colored(d.type.ljust(10), d.type_color)}  "
+                  f"{''.ljust(15)}  "
+                  f"{d.thing_id.ljust(20)}  "
+                  f"{str(d.locked).ljust(10)}  "
+                  f"{str(d.mute).ljust(10)}  "
+                  )
+
+
+class DeviceBus(object):
+    """
+    Holds all the devices in a connection and facilitates communication between them. All DCE devices
+    write to all DTE devices and vice versa.
+    """
+    def __init__(self):
+        self.dce_devices = []
+        self.dte_devices = []
+        self.listen_threads = {}
+
+        self.stream_restarter = threading.Thread(target=self._check_for_crashed_threads)
+        self.stream_restarter.daemon = True
+        self.stream_restarter.start()
+
+    def add_device(self, device):
+        """
+        Adds the given device to the connection, and starts a listening thread if the device is not muted.
+        """
+        if device.mode == "DTE":
+            self.dte_devices.append(device)
+        if device.mode == "DCE":
+            self.dce_devices.append(device)
+        if not device.mute:
+            self.listen_threads[device.name] = threading.Thread(target=self._listen_stream, args=[device])
+            self.listen_threads[device.name].daemon = True
+            self.listen_threads[device.name].start()
+        return True
+
+    def remove_device(self, device_name):
+        """
+        Takes a device name, and removes that device if the name is found
+        """
+        found = False
+        for d in self.dce_devices:
+            if d.name == device_name:
+                found = True
+                d.kill_listen_stream()
+                self.dce_devices.remove(d)
+                print(f"{timestamp()}Device '{d.name}' removed.")
+
+        for d in self.dte_devices:
+            if d.name == device_name:
+                found = True
+                d.kill_listen_stream()
+                self.dte_devices.remove(d)
+                print(f"{timestamp()}Device '{d.name}' removed.")
+
+        if not found:
+            print("Device not found.")
+
+    def print_status(self):
+        """
+        Prints a list of all devices in the connection.
+        """
+        print("\nDCE Devices")
+        _print_device_list(self.dce_devices)
+        print("\nDTE Devices")
+        _print_device_list(self.dte_devices)
+
+    def print_threads(self):
+        print(self.listen_threads)
+
+    def _listen_stream(self, device):
+        """
+        Listens for messages from the given device, then writes to all appropriate devices on bus.
+        """
+        s_print(f"{timestamp()}Listen stream started for {device.name}.")
+
+        for message in device.listen():
+            message = str(message)
+            message_decoded = bytes.fromhex(message).decode('latin-1').rstrip().replace('\r', '')
+
+            s_print(f"\n{timestamp()}Received {colored(device.type, device.type_color)} message from {device.name}:"
+                    f" {Fore.LIGHTWHITE_EX}{message_decoded}{Style.RESET_ALL}")
+
+            if device.mode == "DTE":
+                # Messages from DTE devices get sent to all DCE devices.
+                for d in self.dce_devices:
+                    d.write(message)
+
+            elif device.mode == "DCE":
+                # Messages from DCE devices get sent to all DTE devices.
+                for d in self.dte_devices:
+                    d.write(message)
+
+            else:
+                s_print("Mode not found")
+
+        return True
+
+    def _check_for_crashed_threads(self):
+        """
+        If a thread has thrown an error, attempt to restart it.
+        """
+        while True:
+            for d in self.dce_devices:
+                if d.exc and internet_connection():
+                    # if the thread had an exception, start a new one
+                    self._restart_thread(d)
+            for d in self.dte_devices:
+                if d.exc and internet_connection():
+                    # if the thread had an exception, start a new one
+                    self._restart_thread(d)
+            time.sleep(.01)
+
+    def _restart_thread(self, device):
+        s_print(f"{timestamp()}Reconnecting to {device.name}.")
+        device.kill_listen_stream()  # attempt to kill the listen thread if it is still responding
+        device.exc = False  # reset the exception flag to false
+        device.restart_session()  # start a new Session
+        device.send_message_queue()  # send any messages saved in the queue
+        if not device.mute:  # restart the listen thread if needed
+            self.listen_threads[device.name] = threading.Thread(target=self._listen_stream, args=[device])
+            self.listen_threads[device.name].daemon = True
+            self.listen_threads[device.name].start()
+        return
---
+++ packages/dweet2ser/dweet2ser-0.1.3.tar.gz/dweet2ser-0.1.3/dweet2ser/remote_device.py
@@ -0,0 +1,186 @@
+import datetime
+import threading
+import time
+
+import requests
+from termcolor import colored
+from urllib3.exceptions import ProtocolError
+
+from dweet2ser import dweepy
+from dweet2ser.settings import timestamp, internet_connection, s_print
+
+
+class DeadConnectionError(Exception):
+    pass
+
+
+class RemoteDevice(object):
+    """
+    Implementation of a serial device remotely connected with dweet.io
+    """
+    def __init__(self, thing_id, mode, thing_key=None, name="Remote Device", mute=False):
+        self.name = name
+        self.type = "dweet"
+        self.type_color = "cyan"
+        self.thing_id = thing_id
+        self.locked = False
+        self.thing_key = thing_key
+        self.mute = mute
+        if thing_key is not None:
+            self.locked = True
+        self.mode = mode
+
+        if mode == 'DCE':  # if this is a DCE (device), set the dweet.io keywords appropriately
+            self.write_kw = "from_pc"
+            self.read_kw = "from_device"
+        else:  # if this is a DTE (computer), the dweet.io keywords should be reversed.
+            self.write_kw = "from_device"
+            self.read_kw = "from_pc"
+
+        self._session = requests.Session()
+        self._last_message = ''
+        self._kill_signal = "kill"
+        self._message_queue = []
+        self._started_on_day = datetime.datetime.utcnow().strftime("%Y-%m-%d")
+        self.exc = False
+        self.listening = False
+
+    def write(self, message):
+        """
+        Tries to send a dweet message to the remote device. If there's no internet connection it saves
+        the message to a queue.
+        """
+        if internet_connection() and self._send_dweet({self.write_kw: message}):
+            # check for a connection before trying to send to dweet
+            message_decoded = bytes.fromhex(message).decode('latin-1').rstrip()
+            s_print(f"{timestamp()}{colored(self.type.capitalize(), self.type_color)} sent to {self.name}: "
+                    f"{message_decoded}")
+            return True
+        else:
+            # if there's no connection save the message to resend on reconnect
+            s_print(f"{timestamp()}No connection to {self.name}. Saving message to queue.")
+            self._message_queue.append(message)
+            self.exc = True
+            return False
+
+    def send_message_queue(self):
+        """
+        Attempt to send queued messages.
+        """
+        while len(self._message_queue) > 0 and internet_connection():
+            s_print(f"{timestamp()}Sending queued messages.")
+            self.write(self._message_queue.pop(0))
+            time.sleep(1.2)  # avoid exceeding dweet.io's 1s rate limit
+
+    def _send_dweet(self, content):
+        try:
+            dweepy.dweet_for(self.thing_id, content, key=self.thing_key, session=self._session, timeout=10)
+            return True
+
+        except dweepy.DweepyError as e:
+            s_print(timestamp() + str(e))
+            if str(e) == "Rate limit exceeded, try again in 1 second(s).":
+                s_print(f"{timestamp()}Trying again...")
+                time.sleep(1.5)
+                return self._send_dweet(content)
+            else:
+                return False
+
+        except (ConnectionError, ProtocolError, OSError) as e:
+            s_print(timestamp() + str(e))
+            s_print(f"{timestamp()}Connection to {self.name} lost.")
+            self.exc = True
+            return False
+
+        except Exception as e:
+            s_print(f"{timestamp()}Unexpected error.")
+            s_print(timestamp() + str(e))
+            self.exc = True
+            return False
+
+    def restart_session(self):
+        """
+        Gets a new Session from requests.
+        """
+        self._session = requests.Session()
+
+    def listen(self):
+        """
+        Calls the listen/for/dweets/from function of dweet.io and yields messages from the chunked
+        HTTP response.
+        """
+        self.listening = True
+        # Start a thread to send a message to dweet every 45 seconds.
+        # This is necessary because dweet.io closes the listen response after 60s of inactivity.
+        keepalive_thread = threading.Thread(target=self._keepalive)
+        keepalive_thread.daemon = True
+        keepalive_thread.start()
+
+        for message in self._listen_for_dweets():
+            yield message
+
+    def _listen_for_dweets(self):
+        """ makes a call to dweepy to start a listening stream. error handling needs work
+        """
+        while internet_connection() and self.listening:
+            try:
+                for dweet in dweepy.listen_for_dweets_from(self.thing_id, key=self.thing_key,
+                                                           timeout=90000, session=self._session):
+                    content = dweet["content"]
+                    self._last_message = dweet
+                    if self.read_kw in content:
+                        message = content[self.read_kw]
+                        if message == self._kill_signal:
+                            s_print(f"{timestamp()}Listen stream for {self.name} closed.")
+                            return
+                        yield message
+
+            # if you get an error because dweet closed the connection, open it again.
+            except (ConnectionError, ProtocolError, OSError) as e:
+                s_print(timestamp() + str(e))
+                s_print(f"{timestamp()}Connection closed by dweet, restarting.")
+                self.restart_session()
+                yield self.get_last_message()
+        s_print(f"{timestamp()}Listen stream for {self.name} closed.")
+        self.exc = True
+        return
+
+    def kill_listen_stream(self):
+        """
+        Attempts to manually end the chunked HTTP listening stream.
+        """
+        self._send_dweet({self.read_kw: self._kill_signal})
+        self.listening = False
+
+    def _keepalive(self):
+        """ dweet.io seems to close the connection after 60 seconds of inactivity.
+            This sends a dummy payload every 45s to avoid that.
+        """
+        while self.listening and not self.exc:
+            time.sleep(45)
+            self._send_dweet({"keepalive": 1})
+
+    def get_last_message(self):
+        """
+        Tries to recover the last message sent to dweet.
+        """
+        message = ''
+        if internet_connection():
+            dweet = dweepy.get_latest_dweet_for(self.thing_id, key=self.thing_key, session=self._session)
+            content = dweet[0]["content"]
+            if self.read_kw in content:
+                message = content[self.read_kw]
+        return message
+
+    @staticmethod
+    def _get_dweet_time(dweet):
+        """
+        For future dweet recovery implementation.
+        """
+        created = dweet["created"].replace("T", " ").replace("Z", "")
+        dweet_time = datetime.datetime.fromisoformat(created)
+        return dweet_time
+
+# TODO Create function to retrieve dweets from storage after a connection loss.
+#   Basic idea: store time of last_message_received from listen.
+#   On connection restore, pull all dweets from today and yield all where "created" > last_message_received
