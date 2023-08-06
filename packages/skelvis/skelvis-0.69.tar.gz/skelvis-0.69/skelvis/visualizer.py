f27fbd8fae079ef796ef91d4f33b924ae3f79feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:05 +0000

    Add package skelvis/0.69/skelvis-0.69.tar.gz

---
+++ packages/skelvis/skelvis-0.69.tar.gz/skelvis-0.69/skelvis/visualizer.py
@@ -0,0 +1,87 @@
+from typing import Optional, List
+
+import ipywidgets as widgets
+import k3d
+import numpy as np
+from IPython.display import display
+from k3d.plot import Plot
+
+from .jointset import JointSet, MuPoTSJoints, OpenPoseJoints, CocoExJoints, PanopticJoints, Common14Joints
+from .objects import Skeleton, DrawableSkeleton, Color
+
+
+class SkeletonVisualizer:
+    def __init__(self, joint_set: JointSet, size_scalar: float = 1.0):
+        self.joint_set: JointSet = joint_set
+        self.plot: Optional[Plot] = None
+        self.size_scalar: float = size_scalar
+        self.skeletons: List[DrawableSkeleton] = []
+        self.joint_names_visible = widgets.Checkbox(description="Show Joint Names")
+
+    def visualize(self, skeletons: np.ndarray, colors: List[Color] = None):
+        self.__assert_visualization_arguments(skeletons, colors)
+        number_of_skeletons = skeletons.shape[0]
+        if colors is None:
+            colors = ['default' for _ in range(number_of_skeletons)]
+        self.plot = self.__create_skeleton_plot(skeletons, colors)
+        self.__link_joint_name_visibility_with_checkbox()
+        self.plot.display()
+        display(self.joint_names_visible)
+
+    def __assert_visualization_arguments(self, skeletons: np.ndarray, colors: List[Color]):
+        assert len(skeletons.shape) == 3, 'The \'skeletons\' parameter should be a 3 dimensional numpy array.'
+        if colors is not None:
+            assert skeletons.shape[0] == len(colors),\
+                'The \'skeletons\' and \'colors\' parameters must be the same length.'
+        assert skeletons.shape[1] == self.joint_set.number_of_joints,\
+            'The number of joints of skeletons and the number of joints in the specified joint set must be the same.'
+        assert skeletons.shape[2] == 3, 'The skeleton joint coordinates must be 3 dimensional'
+
+    def __create_skeleton_plot(self, skeletons: np.ndarray, colors: List[Color]) -> Optional[Plot]:
+        skeleton_part_size = self.__calculate_skeleton_part_size(skeletons)
+        skeleton_plot = k3d.plot()
+        for skeleton in map(
+                lambda skeleton_color_tuple: Skeleton(
+                    joint_coordinates=skeleton_color_tuple[0],
+                    joint_set=self.joint_set,
+                    part_size=skeleton_part_size,
+                    color=skeleton_color_tuple[1]),
+                zip(skeletons, colors)):
+            drawable_skeleton = skeleton.to_drawable_skeleton()
+            skeleton_plot += drawable_skeleton
+            self.skeletons.append(drawable_skeleton)
+        return skeleton_plot
+
+    def __calculate_skeleton_part_size(self, skeletons: np.ndarray) -> float:
+        max_values = [abs(skeleton).max() for skeleton in skeletons]
+        return (min(max_values) / 100.0) * self.size_scalar
+
+    def __link_joint_name_visibility_with_checkbox(self):
+        for skeleton in self.skeletons:
+            for joint_name in skeleton.joint_names:
+                widgets.jslink((joint_name, 'visible'), (self.joint_names_visible, 'value'))
+
+
+class MuPoTSVisualizer(SkeletonVisualizer):
+    def __init__(self, size_scalar: float = 1.0):
+        super().__init__(joint_set=MuPoTSJoints(), size_scalar=size_scalar)
+
+
+class OpenPoseVisualizer(SkeletonVisualizer):
+    def __init__(self, size_scalar: float = 1.0):
+        super().__init__(joint_set=OpenPoseJoints(), size_scalar=size_scalar)
+
+
+class CocoExVisualizer(SkeletonVisualizer):
+    def __init__(self, size_scalar: float = 1.0):
+        super().__init__(joint_set=CocoExJoints(), size_scalar=size_scalar)
+
+
+class PanopticVisualizer(SkeletonVisualizer):
+    def __init__(self, size_scalar: float = 1.0):
+        super().__init__(joint_set=PanopticJoints(), size_scalar=size_scalar)
+
+
+class Common14Visualizer(SkeletonVisualizer):
+    def __init__(self, size_scalar: float = 1.0):
+        super().__init__(joint_set=Common14Joints(), size_scalar=size_scalar)
