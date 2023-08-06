c0d745ca93bd9f18207eaee4bbeeb840501338a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:52 +0000

    Add package python-smarttub/0.0.8/python-smarttub-0.0.8.tar.gz

---
+++ packages/python-smarttub/python-smarttub-0.0.8.tar.gz/python-smarttub-0.0.8/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="python-smarttub",
+    version="0.0.8",
+    author="Matt Zimmerman",
+    author_email="mdz@alcor.net",
+    description="API to query and control hot tubs using the SmartTub system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mdz/python-smarttub",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'aiohttp>=3.0.0,<4.0.0',
+        'pyjwt>=1.0.0,<3.0.0',
+        'python-dateutil>=2.0.0,<3.0.0',
+    ],
+    # tests require python >=3.8
+    tests_require=['pytest', 'pytest-asyncio', 'aresponses'],
+    python_requires='>=3.7',
+)
