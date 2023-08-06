2fae925c9c42036df62db9f1d77341159d54ae3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:40 +0000

    Add package platemap/0.0.8/platemap-0.0.8.tar.gz

---
+++ packages/platemap/platemap-0.0.8.tar.gz/platemap-0.0.8/platemap/PlateUtils/assign_source_wells.py
@@ -0,0 +1,43 @@
+from ..plate import Plate
+from .add_volume import add_volume
+import math
+
+
+def assign_source_wells(
+    plate: Plate,
+    input_dict: dict[str, float],
+    fill_to: int = None,
+    alternate_wells=False,
+):
+    useable_liquid_per_well = (
+        fill_to - plate.deadspace if fill_to else plate.well_volume - plate.deadspace
+    )
+    well_index = 0
+    for item in input_dict:
+        for well_needed in range(
+            math.ceil(
+                input_dict[item]
+                / (
+                    fill_to - plate.deadspace
+                    if fill_to
+                    else plate.well_volume - plate.deadspace
+                )
+            )
+        ):
+            if not alternate_wells:
+                plate.set_well_id(plate.wells[well_index], item)
+                add_volume(
+                    plate,
+                    plate.wells[well_index],
+                    fill_to if fill_to else plate.well_volume,
+                    item,
+                )
+            else:
+                plate.set_well_id(plate.wells[well_index * 2], item)
+                add_volume(
+                    plate,
+                    plate.wells[well_index * 2],
+                    fill_to if fill_to else plate.well_volume,
+                    item,
+                )
+            well_index += 1
