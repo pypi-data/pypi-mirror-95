c8e3a597aaba1a93d37c2026b02cd8706ffb73d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:58 +0000

    Add package livetube/1.0/livetube-1.0.tar.gz

---
+++ packages/livetube/livetube-1.0.tar.gz/livetube-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='livetube',
+   version='1.0',
+   description='A module for youtube livestream',
+   author='Sam',
+   author_email='lau.sam745033858@gmail.com',
+   packages=find_packages(),
+   install_requires=['aiohttp']
+)
