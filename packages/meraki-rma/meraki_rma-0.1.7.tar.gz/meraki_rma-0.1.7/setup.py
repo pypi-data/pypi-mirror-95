37bcb23fc18d3635ea651dd3fc46150bb5574650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:46 +0000

    Add package meraki-rma/0.1.7/meraki_rma-0.1.7.tar.gz

---
+++ packages/meraki-rma/meraki_rma-0.1.7.tar.gz/meraki_rma-0.1.7/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['meraki_rma']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['meraki-dashboard-connect>=0.1.1,<0.2.0',
+ 'meraki-env>=0.1.1,<0.2.0',
+ 'meraki-exception>=0.1.0,<0.2.0',
+ 'meraki>=1.4.3,<2.0.0',
+ 'rich>=9.10.0,<10.0.0']
+
+setup_kwargs = {
+    'name': 'meraki-rma',
+    'version': '0.1.7',
+    'description': '',
+    'long_description': None,
+    'author': 'Thomas Christory',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
