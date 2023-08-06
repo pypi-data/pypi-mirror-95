bd13480171b5d9d63d6d62165e86d5554c6b6b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:54 +0000

    Add package ktg/0.0.6/ktg-0.0.6-py3-none-any.whl

---
+++ packages/ktg/ktg-0.0.6-py3-none-any.whl/ktg/telegram.py
@@ -0,0 +1,73 @@
+# --------------------------------------------------------------- Imports ---------------------------------------------------------------- #
+
+# System
+from typing import Optional
+import json
+
+# Pip
+from kcu import request
+
+# Local
+from .parse_mode import ParseMode
+
+# ---------------------------------------------------------------------------------------------------------------------------------------- #
+
+
+
+# ----------------------------------------------------------- class: Telegram ------------------------------------------------------------ #
+
+class Telegram:
+
+    # ------------------------------------------------------------- Init ------------------------------------------------------------- #
+
+    def __init__(
+        self,
+        token: str,
+        chat_id: Optional[str] = None,
+        debug: bool = False
+    ):
+        self.token = token
+        self.chat_id = chat_id
+        self.debug = debug
+
+
+    # -------------------------------------------------------- Public methods -------------------------------------------------------- #
+
+    def send(
+        self,
+        message: str,
+        chat_id: Optional[str] = None,
+        parse_mode: Optional[ParseMode] = None
+    ) -> bool:
+        chat_id = chat_id or self.chat_id
+
+        if not chat_id:
+            if self.debug:
+                print('ERROR: No chat id')
+
+            return False
+
+        res = request.get(
+            'https://api.telegram.org/bot{}/sendMessage'.format(self.token),
+            params={
+                'chat_id': chat_id,
+                'text': message,
+                'parse_mode': (parse_mode or ParseMode.HTML).value
+            }
+        )
+
+        return res and res.status_code == 200
+
+    @classmethod
+    def send_cls(
+        cls,
+        token: str,
+        message: str,
+        chat_id: str,
+        parse_mode: Optional[ParseMode] = None,
+        debug: bool = False
+    ) -> bool:
+        return cls(token, chat_id, debug).send(message, parse_mode=parse_mode)
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------- #
