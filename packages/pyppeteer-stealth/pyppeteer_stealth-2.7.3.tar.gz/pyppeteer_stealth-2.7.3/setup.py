2c3ec3b324c29e7c1ee4a7eb2f93782a38341d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:05 +0000

    Add package pyppeteer-stealth/2.7.3/pyppeteer_stealth-2.7.3.tar.gz

---
+++ packages/pyppeteer-stealth/pyppeteer_stealth-2.7.3.tar.gz/pyppeteer_stealth-2.7.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyppeteer_stealth",
+    version="2.7.3",
+    author="MeiK2333",
+    author_email="meik2333@gmail.com",
+    description="pyppeteer stealth",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MeiK2333/pyppeteer-stealth",
+    packages=setuptools.find_packages(),
+    package_data={"pyppeteer_stealth": ["js/*.js"]},
+)
