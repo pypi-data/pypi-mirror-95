78c28866e0ce25803ed12257d6b2420444c4c533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:46 +0000

    Add package algorithm-toolkit/0.2.0/algorithm_toolkit-0.2.0.tar.gz

---
+++ packages/algorithm-toolkit/algorithm_toolkit-0.2.0.tar.gz/algorithm_toolkit-0.2.0/docs/index.rst
@@ -0,0 +1,38 @@
+.. Algorithm Toolkit documentation master file
+
+=====================================================
+Welcome to the Algorithm Toolkit (ATK) documentation!
+=====================================================
+This project provides an easy way for researchers and developers to develop and share algorithms related to geospatial data and imagery.
+
+The source code for this project lives on `GitHub <https://github.com/BeamIO-Inc/algorithm_toolkit>`_.
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 2
+
+   about
+   quick_start
+   pycharm
+   atk-project
+   dev-environment
+   working-with-algorithms
+   creating-your-first-algorithm
+   working-with-chains
+   chain-ledger
+   tutorial
+   cli
+   api
+   docker
+   algorithm-registry
+   contributing
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/algorithm-toolkit/algorithm_toolkit-0.2.0.tar.gz/algorithm_toolkit-0.2.0/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author="BeamIO, Inc.",
+    author_email="info@beamio.net",
+    name="algorithm_toolkit",
+    description=(
+        "This project provides an easy way for researchers and "
+        "developers to develop and share algorithms related to "
+        "geospatial data and imagery."
+    ),
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BeamIO-Inc/algorithm_toolkit",
+    license="MIT",
+    install_requires=[
+        'configparser',
+        'distro',
+        'flask >= 1.0',
+        'flask-cors',
+        'flask_wtf',
+        'inflect',
+        'numpy',
+        'psutil',
+        'python-dotenv',
+        'requests',
+        'tabulate',
+        'psutil'
+    ],
+    entry_points={
+        'console_scripts': ['alg=cli.cli:cli']
+    },
+    project_urls={
+        "Documentation":
+            "https://algorithm-toolkit.readthedocs.io/en/latest/index.html",
+        "Source Code": "https://github.com/BeamIO-Inc/algorithm_toolkit",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    version='0.2.0'
+)
