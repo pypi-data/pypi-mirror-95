91e872911e8f31f62e50d63e6b9de2a3a00f9856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:38 +0000

    Add package django-qr-toolkit-core/0.7/django-qr-toolkit-core-0.7.tar.gz

---
+++ packages/django-qr-toolkit-core/django-qr-toolkit-core-0.7.tar.gz/django-qr-toolkit-core-0.7/qrtoolkit_core/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import Department, LinkUrl, QRCode, ApiHit
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+
+class LinkUrlSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LinkUrl
+        fields = '__all__'
+
+
+class QRCodeSerializer(serializers.ModelSerializer):
+    urls = LinkUrlSerializer(many=True, read_only=True)
+    department = serializers.PrimaryKeyRelatedField(queryset=Department.objects.all(), required=False, allow_null=True)
+
+    # image_url = serializers.SerializerMethodField(method_name='get_image_url', read_only=True)
+    # download_url = serializers.SerializerMethodField(method_name='get_dl_url', read_only=True)
+
+    # def get_image_url(self, obj):
+    #     path = reverse('code-detail', kwargs={'short_uuid': obj.short_uuid})
+    #     url = self.context['request'].build_absolute_uri(path)
+    #     return url
+    #
+    # def get_dl_url(self, obj):
+    #     path = reverse('code-dl', kwargs={'short_uuid': obj.short_uuid})
+    #     url = self.context['request'].build_absolute_uri(path)
+    #     return url
+
+    class Meta:
+        model = QRCode
+        fields = '__all__'
+        read_only_fields = ['created', 'last_updated']
+
+
+class ApiHitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApiHit
+        fields = '__all__'
