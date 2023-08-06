dad88285b58a5f8ad3b1335c802c48b9349c27d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:07 +0000

    Add package states3/0.5.0/states3-0.5.0.tar.gz

---
+++ packages/states3/states3-0.5.0.tar.gz/states3-0.5.0/states/__init__.py
@@ -0,0 +1,6 @@
+from states.stateful import StatefulObject
+from states.machine import StateMachine, state_machine
+from states.exception import MachineError, TransitionError
+from states.configuration import states, state, transitions, transition, switch, case
+
+# __all__ = ("StateMachine",)
