546773bc42fe8131c08daf41c05b313aea11126e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:49 +0000

    Add package snir/0.1.5/snir-0.1.5.tar.gz

---
+++ packages/snir/snir-0.1.5.tar.gz/snir-0.1.5/snir/__init__.py
@@ -0,0 +1,107 @@
+print("hello from snir's package 0.1.5")
+from playsound import playsound
+
+from sty import Style, RgbFg, Sgr
+from sty import Style, RgbFg
+from sty import fg
+import site
+
+directory = f"{site.getsitepackages()[1]}\\snir\\"
+reset = '\033[0m'
+fg.cyan = Style(RgbFg(0, 255, 255))
+fg.red = Style(RgbFg(255, 0, 0))
+fg.yellow = Style(RgbFg(255, 255, 0))
+fg.green = Style(RgbFg(0, 255, 0))
+fg.blue = Style(RgbFg(0, 70, 255))
+bold_style = '\033[1m'
+underline_style = '\033[4m'
+
+
+def purple(txt):
+    return '\033[95m' + str(txt) + reset
+
+
+def header(txt):
+    return '\033[93m' + str(txt) + reset
+
+
+def bold(txt):
+    return '\033[1m' + str(txt) + reset
+
+
+def underline(txt):
+    return '\033[4m' + str(txt) + reset
+
+
+def blue(txt):
+    return '\033[94m' + str(txt) + reset
+
+
+def green(txt):
+    return '\033[92m' + str(txt) + reset
+
+
+def red(txt):
+    return '\033[91m' + str(txt) + reset
+
+
+def bright_red(txt):
+    return fg.red + txt + reset
+
+
+def black(txt):
+    return '\u001b[30m' + str(txt) + reset
+
+
+def cyan(txt):
+    fg.cyan = Style(RgbFg(0, 255, 255))
+
+    return fg.cyan + txt + reset
+
+
+def error(txt):
+    """identical to fail"""
+    return fg.red + "\033[1m" + "\033[4m" + txt + reset
+
+
+def fail(txt):
+    """identical to error"""
+    return fg.red + "\033[1m" + "\033[4m" + txt + reset
+
+
+def bright_yellow(txt):
+    return fg.yellow + txt + reset
+
+
+def bright_green(txt):
+    return fg.green + txt + reset
+
+
+def bright_blue(txt):
+    return fg.blue + txt + reset
+
+
+def title(txt):
+    return "\033[93m" + txt + reset
+
+
+# todo __________________________________________________________________________________________________
+
+def startup_sound():
+    playsound(directory + "startup.wav", block=True)
+
+
+def error_sound():
+    playsound(directory + "negativebeep.wav", block=True)
+
+
+def success_sound():
+    playsound(directory + "success.wav", block=True)
+
+
+def click_sound():
+    playsound(directory + "first_click.wav", block=True)
+
+
+def click_sound1():
+    playsound(directory + "second_click.wav", block=True)
