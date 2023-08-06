095f22b3147ef5f81a7e2108aeae7d4e0bcfba2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:16 +0000

    Add package wand-cli/2.4.5/wand-cli-2.4.5.tar.gz

---
+++ packages/wand-cli/wand-cli-2.4.5.tar.gz/wand-cli-2.4.5/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requirements = [
+        'requests==2.25.1',
+        'Pygments==2.7.4',
+        'prompt-toolkit==3.0.11',
+        'rich==9.8.2',
+        'argparse'
+        ]
+
+setuptools.setup(
+    name="wand-cli",
+    version="2.4.5",
+    author="vinam",
+    author_email="vinamraj01@gmail.com",
+    description="A cli tool with an in-place terminal editor to compile over 27 languages instantly using the wandbox api.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/v1nam/wandbox-cli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": ["wandbox=wandbox.cli_:main"],
+    },
+    keywords="wandbox cli compile run compiler",
+    install_requires=requirements,
+    zip_safe=False,
+)
