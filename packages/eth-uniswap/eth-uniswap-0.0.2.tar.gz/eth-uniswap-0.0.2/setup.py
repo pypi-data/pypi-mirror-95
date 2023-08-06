1479df8f6735ece840717127b0a26ebf9fbd14ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:57 +0000

    Add package eth-uniswap/0.0.2/eth-uniswap-0.0.2.tar.gz

---
+++ packages/eth-uniswap/eth-uniswap-0.0.2.tar.gz/eth-uniswap-0.0.2/setup.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: GPL-3.0-only
+# © Copyright 2021 Julien Harbulot
+# see: https://github.com/julien-h/python-eth-uniswap
+import setuptools
+
+setuptools.setup(
+    include_package_data=True, version=open("eth_uniswap/resources/version").read()
+)
