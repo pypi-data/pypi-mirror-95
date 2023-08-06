c310f50888bc847b154935c974730e9d5333cc1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:42 +0000

    Add package finvizfinance/0.8.2/finvizfinance-0.8.2-py3-none-any.whl

---
+++ packages/finvizfinance/finvizfinance-0.8.2-py3-none-any.whl/finvizfinance/screener/financial.py
@@ -0,0 +1,22 @@
+from finvizfinance.screener.overview import Overview
+"""
+.. module:: screen.financial
+   :synopsis: screen financial table.
+
+.. moduleauthor:: Tianning Li <ltianningli@gmail.com>
+"""
+
+
+class Financial(Overview):
+    """Financial inherit from overview module.
+    Getting information from the finviz screener financial page.
+    """
+    def __init__(self):
+        """initiate module
+        """
+        self.BASE_URL = 'https://finviz.com/screener.ashx?v=161{signal}{filter}&ft=4{ticker}'
+        self.NUMBER_COL = ['Market Cap', 'Dividend', 'ROA', 'ROE', 'ROI',
+                           'Curr R', 'Quick R', 'LTDebt/Eq', 'Debt/Eq', 'Gross M',
+                           'Oper M', 'Profit M', 'Price', 'Change', 'Volume']
+        self.url = self.BASE_URL.format(signal='', filter='', ticker='')
+        Overview._loadSetting(self)
---
+++ packages/finvizfinance/finvizfinance-0.8.2-py3-none-any.whl/finvizfinance/screener/valuation.py
@@ -0,0 +1,22 @@
+from finvizfinance.screener.overview import Overview
+"""
+.. module:: screen.valuation
+   :synopsis: screen valuation table.
+
+.. moduleauthor:: Tianning Li <ltianningli@gmail.com>
+"""
+
+
+class Valuation(Overview):
+    """Valuation inherit from overview module.
+        Getting information from the finviz screener valuation page.
+    """
+    def __init__(self):
+        """initiate module
+        """
+        self.BASE_URL = 'https://finviz.com/screener.ashx?v=121{signal}{filter}&ft=4{ticker}'
+        self.NUMBER_COL = ['Market Cap', 'P/E', 'Fwd P/E', 'PEG', 'P/S', 'P/B', 'P/C', \
+                           'P/FCF', 'EPS this Y', 'EPS next Y', 'EPS past 5Y', 'EPS next 5Y', \
+                           'Sales past 5Y', 'Price', 'Change', 'Volume']
+        self.url = self.BASE_URL.format(signal='', filter='', ticker='')
+        Overview._loadSetting(self)
