5f2020f3590e4fcf9961bbfe6811889f610eef98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:28 +0000

    Add package eunomia/0.0.1.dev20/eunomia-0.0.1.dev20-py3-none-any.whl

---
+++ packages/eunomia/eunomia-0.0.1.dev20-py3-none-any.whl/eunomia/config/_loader.py
@@ -0,0 +1,254 @@
+from typing import Tuple, List, Union, Optional
+
+from eunomia.config._default import Default
+from eunomia.util._util_dict import recursive_getitem, dict_recursive_update
+from eunomia.config import Option, Group
+from eunomia.config.nodes import ConfigNode
+
+from eunomia.config import keys as K
+from eunomia.config import validate as V
+
+
+# ========================================================================= #
+# Config Loader                                                             #
+# ========================================================================= #
+
+
+class ConfigLoader(object):
+
+    def __init__(self, root_group: Group, overrides: list = None):
+        # check root group
+        if not isinstance(root_group, Group):
+            raise TypeError(f'root_group must be a {Group.__name__}')
+        self._root_group = root_group
+        # has_run
+        self._has_run = False
+        # merged items
+        self._merged_options = {}
+        self._first_merge_from = {}
+        self._merged_config = {}
+        # make defaults overrides
+        self._overrides = self._resolve_overrides_list(overrides)
+        self._overridden = {}
+        # debug
+        self._debug = False
+        self._debug_depth = 0
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # Checks                                                                #
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+    def _pre_merge_checks(self):
+        # 2.1 check all overrides exist in config
+        # NOT NEEDED BECAUSE OF OVERRIDE RESOLVER
+        # if self._overrides:
+        #     for group_keys, (o_grp, o_opts) in self._overrides.items():
+        #         for opt in o_opts:
+        #             if not self._root_group.has_option_recursive(group_keys + (opt.key,)):
+        #                 raise KeyError(f'specified override does not exist in the config: {V.keys_as_abs_config_path(group_keys + (opt.key,))}')
+        pass
+
+    def _post_merge_checks(self):
+        # ===================== #
+        # all values have not yet been resolved in the merge config!
+        # ===================== #
+        # 2.3 make sure all overrides were used!
+        if self._overrides:
+            unused_overrides = set(self._overrides.keys()) - set(self._overridden.keys())
+            if unused_overrides:
+                raise RuntimeError(
+                    f'the following overrides were not used to override defaults listed in the config: {sorted(map(V.keys_as_abs_config_path, unused_overrides))}')
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # Core Algorithm                                                        #
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+    def load_config(self, config_name, return_merged_options=False):
+        """
+        flatten and merge the options lists using DFS, while
+        simultaneously merging the config
+
+        - When config dictionaries are encountered, those are also
+          processed using DFS to obtain substituted values, before
+          being merged into the config.
+            * Keys are not allowed to be substituted values
+        """
+        # ===================== #
+        # make sure we cant run the loader more than once
+        if self._has_run:
+            raise RuntimeError('Loader cannot be run more than once!')
+        self._has_run = True
+        # ===================== #
+        # 1. entry point for dfs, get initial option
+        entry_option = self._root_group.get_option(config_name)
+        if self._debug:
+            print(f'\nentrypoint: {repr(entry_option.abs_path)}')
+        # ===================== #
+        # 2.1. pre checks
+        self._pre_merge_checks()
+        # 2.2. perform dfs & merging and finally resolve values
+        self._visit_option(entry_option, parent_option=None)
+        # 2.3 post checks
+        self._post_merge_checks()
+        # ===================== #
+        # 3. finally resolve all the values in the config
+        self._resolve_all_values()
+        # ===================== #
+
+        # done, return the result
+        if return_merged_options:
+            return self._merged_config, self._merged_options
+
+        return self._merged_config
+
+    def _visit_option(self, option: Option, parent_option: Optional[Option]):
+        if self._debug:
+            print(f'{" "*self._debug_depth*4}> visiting {repr(option.abs_path)} from {repr(parent_option.abs_path) if parent_option else "<user>"}')
+            self._debug_depth += 1
+        # ===================== #
+        # 1. check where to process self, and make sure self is in the options list
+        # by default we want to merge this before children so we can reference its values.
+        handled_defaults = set()
+        defaults = option.get_unresolved_defaults()
+        if not any(default.is_self_pre_resolve for default in defaults):
+            # allow referencing parent values in children
+            defaults = [Default.make_self()] + defaults
+            # allow parent overwriting children values
+            # group_paths = group_paths + [s.OPT_SELF]
+        # ===================== #
+        # 2. process options in order
+        for default in defaults:
+            # normalise the default
+            # -- various different objects as well as substitution need to be handled
+            d_obj, d_options, d_pkg_keys, d_is_self = default.to_resolved_components(option, resolver=self._resolve_value)
+            if self._debug:
+                print(f'{" " * self._debug_depth * 4}? resolved default { {d_obj.abs_path: [o.key for o in d_options]} } from {default._default}')
+            # ===================== #
+            # make sure that self is only ever handled once
+            key = K.OPT_SELF if d_is_self else V.keys_as_abs_config_path(d_obj.keys)
+            if key in handled_defaults:
+                raise RuntimeError(f'resolved default {key} was encountered more than once in option: {option.abs_path}')
+            handled_defaults.add(key)
+            # ===================== #
+            # handle different cases
+            if d_is_self:
+                # ===================== #
+                # 2.a if self is encountered, merge into config. We skip the
+                #     value of the option_name here as it is not needed.
+                self._merge_option(*d_options, pkg_keys=d_pkg_keys, parent_option=parent_option)
+                # ===================== #
+            else:
+                # ===================== #
+                # allow groups/options to be overridden
+                o_obj, o_options = self._resolve_override(option, d_obj, d_options)
+                if o_obj is not d_obj:
+                    raise AssertionError('this should never happen. overridden default options come from different group.')
+                # ===================== #
+                # 2.b dfs through options
+                # visit the next option
+                for o_option in o_options:
+                    self._visit_option(o_option, parent_option=option)
+                # ===================== #
+        if self._debug:
+            self._debug_depth -= 1
+
+    def _merge_option(self, option: Option, pkg_keys: Tuple[str], parent_option: Optional[Option]):
+        # TODO: this should have different modes
+        #       - do not allow from the same group to be merged
+        #       - allow from the same group to be merged
+        #       - only replace existing values on lhs, don't merge
+        #       - only add missing values on lhs, don't merge or replace
+        # ===================== #
+        # 1. check that the group that the option belongs to has not already been merged.
+        #    Then mark the group the option belongs to as visited
+        #    - if the same source is still merging an option, then we allow
+        #      it even if there are multiple
+        # ===================== #
+        # the path to the group containing the option being merged
+        group_keys = option.group_keys
+        # the parent option can merge one or more options from the above group
+        # once a parent option has been set, being the first to encounter a default, a
+        # different parent option cannot merge options from the same group.
+        first_merge_from = self._first_merge_from.setdefault(group_keys, parent_option if parent_option else None)  # none means the user!
+        visitor = "<user>" if first_merge_from is None else repr(first_merge_from.abs_group_path)
+        if first_merge_from is not parent_option:
+            raise RuntimeError(f'The first merge of options from the group {repr(option.abs_group_path)} occurred '
+                               f'from {visitor} a different parent option cannot request a merge: {repr(option.abs_group_path)}')
+        # check that this exact option has not been visited before
+        if group_keys in self._merged_options:
+            if option.key in self._merged_options[group_keys]:
+                raise KeyError(f'option {repr(option.abs_path)} was previously visited by {visitor}')
+        # mark group as visited
+        self._merged_options.setdefault(group_keys, []).append(option.key)
+        # ===================== #
+        # 2. merged the data
+        # ===================== #
+        # 2.1. get the root config object according to the package
+        root = recursive_getitem(self._merged_config, pkg_keys, make_missing=True)
+        # 2.2. merge the option into the config
+        data = option.get_unresolved_data()
+        dict_recursive_update(left=root, right=data, allow_overwrite=True)
+        # ===================== #
+        if self._debug:
+            print(f'{" "*self._debug_depth*4}* merged data from {repr(option.abs_path)} into output config at {pkg_keys}')
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # Resolving Values                                                      #
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+    def _resolve_override(self, option: Option, d_obj: Union[Group, Option], d_options: List[Option]) -> Tuple[Union[Group, Option], List[Option]]:
+        # if there is no listed override, return the usual options to merge
+        d_group_keys = d_obj.group_keys
+        if d_group_keys not in self._overrides:
+            return d_obj, d_options
+        # make sure it has not been overridden before
+        if d_group_keys in self._overridden:
+            raise RuntimeError(f'default lists entry for group: {option.abs_group_path} has already been overridden: "{d_obj.abs_group_path}: {tuple(o.key for o in d_options)}"')
+        self._overridden[d_group_keys] = (d_obj, d_options)
+        # if there is an override, return the override
+        return self._overrides[d_group_keys]
+
+    def _resolve_overrides_list(self, overrides: list) -> dict:
+        if overrides is None:
+            return {}
+        # resolve overrides
+        overrides_dct = {}
+        for override in overrides:
+            o = Default(override)
+            if o.is_self_pre_resolve:
+                raise KeyError(f'override cannot be self: {repr(override)}')
+            try:
+                o_obj, o_options, _, o_is_self = o.to_resolved_components(self._root_group, resolver=None, force_substitute=False)
+            except KeyError as e:
+                raise KeyError(f'could not resolve the override {repr(override)}, are you sure it exists in the config tree?').with_traceback(e.__traceback__)
+            if o_is_self:
+                raise KeyError(f'override cannot resolve to self: {repr(override)}')
+            overrides_dct[o_obj.group_keys] = (o_obj, o_options)
+        # return!
+        return overrides_dct
+
+    def _resolve_value(self, value):
+        # 1. allow interpolation of config objects
+        # 2. TODO: process dictionary syntax with _node_ keys
+        if isinstance(value, ConfigNode):
+            value = value.get_config_value(self._merged_config, self._merged_options, {})
+        return value
+
+    def _resolve_all_values(self):
+        # TODO: this is wrong!
+        # TODO: config is not mutated on the fly, cannot substitute chains of values.
+        self._merged_config = ConfigNode.recursive_get_config_value(
+            self._merged_config,
+            self._merged_options,
+            {},
+            self._merged_config
+        )
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+    # END Loader                                                            #
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+
+# ========================================================================= #
+# End                                                                       #
+# ========================================================================= #
---
+++ packages/eunomia/eunomia-0.0.1.dev20-py3-none-any.whl/eunomia/config/validate.py
@@ -0,0 +1,255 @@
+import keyword as _keyword
+from typing import Union as _Union, Tuple as _Tuple, Dict as _Dict, List as _List
+
+from eunomia.config import keys as _K
+
+
+# ========================================================================= #
+# Keys                                                                      #
+# ========================================================================= #
+
+
+def validate_identifier(key) -> str:
+    if not isinstance(key, str):
+        raise TypeError(f'identifier is not a string type: {type(key)}')
+    if not key:
+        raise ValueError(f'identifier cannot be empty: {repr(key)}')
+    if not str.isidentifier(key):
+        raise ValueError(f'identifier is not a valid python identifier: {repr(key)}')
+    if _keyword.iskeyword(key):
+        raise ValueError(f'identifier is a python keyword: {repr(key)}')
+    return key
+
+
+def validate_identifier_list(keys) -> list:
+    if not isinstance(keys, list):
+        raise TypeError(f'identifier list must be a list type: {type(keys)}')
+    for k in keys:
+        validate_identifier(k)
+    return keys
+
+
+def validate_config_identifier(key) -> str:
+    key = validate_identifier(key)
+    if key in _K.RESERVED_KEYS:
+        raise ValueError(f'identifier is eunomia reserved key: {repr(key)}')
+    if key.startswith('__') and key.endswith('__'):
+        raise ValueError(f'identifier is reserved: {repr(key)}')
+    return key
+
+
+def validate_config_identifier_list(keys) -> list:
+    if not isinstance(keys, list):
+        raise TypeError(f'identifier list must be a list type: {type(keys)}')
+    for k in keys:
+        validate_config_identifier(k)
+    return keys
+
+
+# ========================================================================= #
+# Values                                                                    #
+# ========================================================================= #
+
+
+def _split_path(path: str, sep: str) -> (str, bool):
+    # check and remove prefix
+    is_prefixed = False
+    if path.startswith(sep):
+        is_prefixed, path = True, path[len(sep):]
+    # split path
+    if path:
+        split = str.split(path, sep)
+    else:
+        split = []
+    # check identifiers
+    return validate_config_identifier_list(split), is_prefixed
+
+
+def split_package_path(path: str) -> (str, bool):
+    if path in _K.ALL_PACKAGE_ALIASES:
+        raise RuntimeError('special package keys should be handled separately')
+    keys, is_relative = _split_path(path, '.')
+    return keys, is_relative
+
+
+def split_config_path(path: str) -> (str, bool):
+    keys, is_not_relative = _split_path(path, '/')
+    return keys, not is_not_relative
+
+
+# ========================================================================= #
+# Paths                                                                     #
+# ========================================================================= #
+
+
+def validate_package_path(path) -> str:
+    if not isinstance(path, str):
+        raise TypeError(f'package path is not of type string: {type(path)}')
+    # check for aliases
+    if path in _K.ALL_PACKAGE_ALIASES:
+        return path
+    # try split
+    split_package_path(path)
+    return path
+
+
+def validate_config_path(path) -> str:
+    if not isinstance(path, str):
+        raise TypeError(f'config path is not of type string: {path}')
+    # try split
+    split_config_path(path)
+    return path
+
+
+# ========================================================================= #
+# config values                                                             #
+# ========================================================================= #
+
+
+def validate_option_data(data) -> dict:
+    if data is None:
+        return {}
+
+    from eunomia.config.nodes import ConfigNode
+
+    if isinstance(data, ConfigNode):
+        raise TypeError(f'option data object can never be a config node: {repr(data)}')
+    if not isinstance(data, dict):
+        raise TypeError(f'option data object must be of type dict: {repr(data)}')
+    return _validate_option_data(data)
+
+
+def _validate_option_data(value):
+    if value is None:
+        pass
+    elif isinstance(value, dict):
+        for k, v in value.items():
+            validate_config_identifier(k)
+            _validate_option_data(v)
+    elif isinstance(value, (list, tuple, set)):
+        for v in value:
+            _validate_option_data(v)
+    elif isinstance(value, (int, float, str)):
+        pass
+    else:
+        raise TypeError(f'unsupported data value type: {type(value)} with value: {repr(value)}')
+    return value
+
+
+def validate_option_package(pkg) -> str:
+    if pkg is None:
+        return _K.DEFAULT_PKG
+
+    from eunomia.config.nodes import ConfigNode
+
+    if isinstance(pkg, ConfigNode):
+        raise TypeError(f'option package can never be a config node: {repr(pkg)}')
+    try:
+        return validate_package_path(pkg)
+    except Exception as e:
+        raise ValueError(f'option package is invalid: {repr(pkg)}').with_traceback(e.__traceback__)
+
+
+def validate_option_defaults(defaults, allow_config_nodes=False) -> list:
+    if defaults is None:
+        return []
+
+    from eunomia.config.nodes import ConfigNode
+
+    if isinstance(defaults, ConfigNode):
+        raise TypeError(f'option defaults can never directly be a config node, only its items: {repr(defaults)}')
+    if not isinstance(defaults, list):
+        raise TypeError('defualts must be a list of values')
+
+    try:
+        for default in defaults:
+            validate_defaults_item(default, allow_config_nodes=allow_config_nodes)
+    except Exception as e:
+        raise ValueError(f'option defaults is invalid: {repr(defaults)}').with_traceback(e.__traceback__)
+
+    return defaults
+
+
+def validate_option_type(typ) -> str:
+    if typ is None:
+        return _K.TYPE_OPTION
+
+    from eunomia.config.nodes import ConfigNode
+
+    if isinstance(typ, ConfigNode):
+        raise TypeError(f'option type can never be a config node: {repr(typ)}')
+    if not isinstance(typ, str):
+        raise TypeError(f'option type must be of type string: {repr(typ)}')
+    if typ != _K.TYPE_OPTION:
+        raise ValueError(f'option  type must be: {repr(typ)}')
+    return typ
+
+
+# ========================================================================= #
+# Options                                                                   #
+# ========================================================================= #
+
+
+def validate_defaults_item(default, allow_config_nodes=False) -> _Union[str, _Dict[str, str], _Dict[str, _List[str]]]:
+    from eunomia.config import Group, Option
+    from eunomia.config.nodes import ConfigNode
+
+    # we cant resolve options or defaults here in case the tree is modified after adding!
+    # we can only resolve them at merge time!
+    if isinstance(default, str):
+        if default != _K.OPT_SELF:
+            validate_config_path(default)
+    elif isinstance(default, (Group, Option, ConfigNode)):
+        if not allow_config_nodes:
+            raise TypeError(f'{default.__class__.__name__} have been disabled as the group key for default entry. Invalid: {default}')
+    elif isinstance(default, dict):
+        # split
+        if len(default) != 1:
+            raise ValueError(f'if defaults entry is a dictionary it must have one and only one key. Invalid: {default}')
+        ((group, opts),) = default.items()
+
+        # check key
+        if isinstance(group, str):
+            validate_config_path(group)
+        elif isinstance(group, Group):
+            if not allow_config_nodes:
+                raise TypeError(f'{group.__class__.__name__} have been disabled as the group key for default entry. Invalid: {group}')
+        elif isinstance(group, ConfigNode):
+            raise TypeError(f'substitution is not supported for the group key of a defaults entry: {group}')
+        else:
+            raise TypeError(f'unsupported defaults entry group key type. Invalid: {group}')
+
+        # check value
+        if isinstance(opts, str):
+            if opts != _K.OPT_GLOB:
+                validate_config_path(opts)
+        elif isinstance(opts, (Group, Option, ConfigNode)):
+            if not allow_config_nodes:
+                raise TypeError(f'{opts.__class__.__name__} have been disabled as the option values for default entry. Invalid: {opts}')
+        elif isinstance(opts, list):
+            for opt in opts:
+                if isinstance(opt, str):
+                    validate_config_path(opt)
+                elif isinstance(opt, Option):
+                    if not allow_config_nodes:
+                        raise TypeError(f'{opts.__class__.__name__} have been disabled as the item in option value list for default entry. Invalid: {opts}')
+                else:
+                    raise TypeError(f'unsupported item in defaults entry value list type. Invalid: {opts}')
+        else:
+            raise TypeError(f'unsupported defaults entry group value type {type(opts)} with value {opts}')
+        # done!
+    else:
+        raise TypeError(f'default entry is invalid type {type(default)}, can only be string paths, {Group.__name__}s or {Option.__name__}s, or a dictionary containing those.')
+
+
+def keys_as_abs_config_path(keys: _Union[_Tuple[str], _List[str]]) -> str:
+    return validate_config_path('/' + '/'.join(keys))
+
+
+def keys_as_abs_pkg_path(keys: _Union[_Tuple[str], _List[str]]) -> str:
+    return validate_package_path('.'.join(keys))
+
+
+# ========================================================================= #
+# END                                                                       #
+# ========================================================================= #
