4e6c6dbc2a12d5729d271c8afb42d9a2333a2af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:16 +0000

    Add package amulet-map-editor/0.8.1a2/amulet_map_editor-0.8.1a2-py3-none-any.whl

---
+++ packages/amulet-map-editor/amulet_map_editor-0.8.1a2-py3-none-any.whl/amulet_map_editor/api/opengl/data_types.py
@@ -0,0 +1,7 @@
+from typing import Tuple
+import numpy
+
+
+TransformationMatrix = numpy.ndarray  # 4x4 matrix
+CameraLocationType = Tuple[float, float, float]
+CameraRotationType = Tuple[float, float]
