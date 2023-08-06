dbcbdb86b53a77be4b8006d9cbb224565d0490a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:02 +0000

    Add package solid-backend/0.1.15/solid-backend-0.1.15.tar.gz

---
+++ packages/solid-backend/solid-backend-0.1.15.tar.gz/solid-backend-0.1.15/solid_backend/content/serializers.py
@@ -0,0 +1,32 @@
+from importlib import import_module
+
+from django.conf import settings
+from rest_framework import serializers
+
+from .models import TreeNode
+
+PROFILES_SERIALIZER = None
+
+if hasattr(settings, "PROFILES_SERIALIZER_MODULE"):
+    PROFILES_SERIALIZER = getattr(
+        import_module(settings.PROFILES_SERIALIZER_MODULE),
+        settings.PROFILES_SERIALIZER_NAME,
+    )(many=True)
+
+
+class TreeNodeSerializer(serializers.ModelSerializer):
+    # To use a custom serializer for the profiles field, it must be specified in the
+    # settings with PROFILES_SERIALIZER_MODULE and PROFILES_SERIALIZER_NAME.
+    if PROFILES_SERIALIZER:
+        profiles = PROFILES_SERIALIZER
+
+    class Meta:
+        model = TreeNode
+        fields = ("name", "info", "profiles", "children")
+        depth = 2
+
+    def get_fields(self):
+        # Serialize self-referential children field recursively.
+        fields = super().get_fields()
+        fields["children"] = TreeNodeSerializer(many=True)
+        return fields
---
+++ packages/solid-backend/solid-backend-0.1.15.tar.gz/solid-backend-0.1.15/solid_backend/slideshow/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from solid_backend.photograph.serializers import PhotographSerializer
+from solid_backend.utils.serializers import DynamicExcludeModelSerializer
+
+from .models import Slideshow, SlideshowImage, SlideshowPage
+
+
+class SlideshowImageSerializer(DynamicExcludeModelSerializer):
+    img = PhotographSerializer()
+
+    class Meta:
+        model = SlideshowImage
+        fields = "__all__"
+
+
+class SlideshowPageSerializer(DynamicExcludeModelSerializer):
+    images = SlideshowImageSerializer(exclude="page", many=True)
+
+    class Meta:
+        model = SlideshowPage
+        fields = "__all__"
+
+
+class SlideshowSerializer(serializers.ModelSerializer):
+    img = PhotographSerializer()
+    pages = SlideshowPageSerializer(exclude="show", many=True)
+
+    class Meta:
+        model = Slideshow
+        exclude = ["active"]
