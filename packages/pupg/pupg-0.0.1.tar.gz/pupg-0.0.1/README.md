d9931c82d6bdeaaee346572cffe7463317616919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:53 +0000

    Add package pupg/0.0.1/pupg-0.0.1.tar.gz

---
+++ packages/pupg/pupg-0.0.1.tar.gz/pupg-0.0.1/README.md
@@ -0,0 +1,27 @@
+# PIPUP
+
+Python script for upgrading all python packages, which is still a big missing pip functionality.
+The script runs `pip list -o` command to get all outdated packages.
+Then it creates a thread for all of the found upgradable packages and runs `pip install -U %pckg` on them.
+
+## Installation
+
+`pip install pipu`
+
+## Usage
+
+`python -m pipup [-h] [-l LIMIT] [-c] [-v]`
+
+### optional arguments:
+
+- `-h, --help` - show help message and exit
+- `-l LIMIT, --limit LIMIT` - set default upgrade try limit (default=3) allowing safely abort upgrade of a package after given number of tries
+- `-c, --check` - check if upgrade was successful at the and of the script; simply runs `pip list -o` again and check if the list is zero
+- `-v, --verbose` - not implemented yet
+
+## VERSIONS
+
+- 1.1.0
+  - multiprocessing functionality replaced by threads
+- 1.0.0
+  - created script for installing all packages
---
+++ packages/pupg/pupg-0.0.1.tar.gz/pupg-0.0.1/setup.py
@@ -0,0 +1,50 @@
+from os import listdir
+from os.path import split
+
+from setuptools import setup, find_packages
+
+
+src = 'src'
+name = next(i for i in listdir(src) if len(i.split('.')) == 1)
+version = '0.0.1'
+
+author = 'Roman Stražanec'
+email = 'strazanec.roman@gmail.com'
+url = f'https://github.com/romanstrazanec/{name}'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(name=name,
+      version='0.0.1',
+      description='Upgrade all python packages.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      keywords="pip install upgrade package all",
+      author=author,
+      author_email=email,
+      maintainer=author,
+      maintainer_email=email,
+      license='MIT',
+      url=url,
+      download_url=f'{url}/archive/v{version}.tar.gz',
+      packages=find_packages(where=src),
+      package_dir={'': src},
+      entry_points={
+          'console_scripts': [
+              f'{name} = {name}._cli:main',
+          ],
+      },
+      install_requires=['pip'],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3.7",
+          "Topic :: Other/Nonlisted Topic"
+      ]
+      )
