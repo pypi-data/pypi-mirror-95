b7563a47fda96b40da9b0cd0656cfecfa7f16cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:52 +0000

    Add package cryptocompare/0.7.5/cryptocompare-0.7.5.tar.gz

---
+++ packages/cryptocompare/cryptocompare-0.7.5.tar.gz/cryptocompare-0.7.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('README.md', encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name='cryptocompare',
+    version='0.7.5',
+    description='Wrapper for CryptoCompare.com',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/lagerfeuer/cryptocompare',
+    author='lagerfeuer',
+    author_email='lukas.deutz@mailfence.com',
+    keywords='crypto cryptocurrency wrapper cryptocompare',
+    license='MIT',
+    python_requires='>=3',
+    packages=['cryptocompare'],
+    classifiers=['Programming Language :: Python :: 3'],
+    install_requires=['requests']
+)
