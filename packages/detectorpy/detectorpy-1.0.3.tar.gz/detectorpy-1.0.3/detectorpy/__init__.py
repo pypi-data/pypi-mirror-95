4c10146d29ae7b7fd9aab09f21fb979648644ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:35 +0000

    Add package detectorpy/1.0.3/detectorpy-1.0.3.tar.gz

---
+++ packages/detectorpy/detectorpy-1.0.3.tar.gz/detectorpy-1.0.3/detectorpy/__init__.py
@@ -0,0 +1,7 @@
+from .model import ObjectDetectionModel
+from detectorpy.data.transforms import BlurImage
+from detectorpy.data.transforms import CropImage
+from detectorpy.data.transforms import FlipImage
+from detectorpy.data.transforms import RescaleImage
+from detectorpy.data.transforms import RotateImage
+from detectorpy.data import Dataset
