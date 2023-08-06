54107bbafe8b8eb87b6e70be3122918f38398c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:44 +0000

    Add package messenger-counter/0.3/messenger-counter-0.3.tar.gz

---
+++ packages/messenger-counter/messenger-counter-0.3.tar.gz/messenger-counter-0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='messenger-counter',
+    version='0.3',
+    author='Krzysztof Mizgała',
+    author_email='krzysztof@mizgala.pl',
+    description='Package for counting messages from Facebook Messenger',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/KMChris/messenger-counter',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+    ],
+    python_requires='>=3.5',
+    install_requires=['pandas', 'matplotlib'],
+    py_modules=['MessengerCounter'],
+)
