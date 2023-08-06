e1713b9c45e656c1c9bb2226f3741e6fd13fa383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:35 +0000

    Add package cdk8s-aws-lb-controller-api-object/0.0.1/cdk8s_aws_lb_controller_api_object-0.0.1-py3-none-any.whl

---
+++ packages/cdk8s-aws-lb-controller-api-object/cdk8s_aws_lb_controller_api_object-0.0.1-py3-none-any.whl/cdk8s_aws_lb_controller_api_object/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import cdk8s._jsii
+import cdk8s_plus_17._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@opencdk8s/cdk8s-aws-lb-controller-api-object",
+    "0.0.1",
+    __name__[0:-6],
+    "cdk8s-aws-lb-controller-api-object@0.0.1.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
