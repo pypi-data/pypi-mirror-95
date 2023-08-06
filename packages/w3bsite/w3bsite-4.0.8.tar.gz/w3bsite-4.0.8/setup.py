0016f52b88f8f03f07aadf52aafe20b29dca8787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:27 +0000

    Add package w3bsite/4.0.8/w3bsite-4.0.8.tar.gz

---
+++ packages/w3bsite/w3bsite-4.0.8.tar.gz/w3bsite-4.0.8/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any '.
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='w3bsite',
+	version='4.0.8',
+	description='Some description.',
+	url='http://github.com/vandenberghinc/w3bsite',
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=[
+        #'ssht00ls',
+        #'fil3s',
+        #'encrypti0n',
+        #'syst3m',
+        #'netw0rk',
+        #'cl1',
+        #'r3sponse',
+        'smtplib',
+        'django',
+        'xmltodict',
+        'firebase_admin',
+        'stripe',
+        'requests',
+    ])
