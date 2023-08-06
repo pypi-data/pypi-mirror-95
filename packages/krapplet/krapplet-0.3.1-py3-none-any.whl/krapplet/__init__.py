f238fd95c12b4acd3aee95a98e59827c88ddaadb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:23 +0000

    Add package krapplet/0.3.1/krapplet-0.3.1-py3-none-any.whl

---
+++ packages/krapplet/krapplet-0.3.1-py3-none-any.whl/krapplet/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+krapplet: A password manager written as a gnome-keyring applet
+__init__.py: a start-up helper 
+(c) 2020-2021 Johannes Willem Fernhout, BSD 3-Clause License applies
+"""
+
