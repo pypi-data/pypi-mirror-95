9a0f949515a9e4eefd919afcd8dabe93a2d38c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:50 +0000

    Add package django-priority-batch/3.0.0a2/django-priority-batch-3.0.0a2.tar.gz

---
+++ packages/django-priority-batch/django-priority-batch-3.0.0a2.tar.gz/django-priority-batch-3.0.0a2/docs/CHANGELOG.rst
@@ -0,0 +1,68 @@
+##########
+Change Log
+##########
+
+All notable changes to this project are documented in this file.
+This project adheres to `Semantic Versioning <http://semver.org/>`_.
+
+
+================
+2.2.0 2020-12-20
+================
+
+Changed
+=======
+- Require Django 3.1
+
+
+================
+2.2.0 2020-12-20
+================
+
+Added
+------
+- Add support for Python 3.9
+
+
+================
+2.1.0 2020-02-07
+================
+
+Added
+-----
+- Add support for Python 3.8
+
+
+================
+2.0.0 2019-04-26
+================
+
+Changed
+-------
+- Require Django 2.x
+
+
+================
+1.1.1 2018-10-22
+================
+
+Fixed
+-----
+- Rollback batcher transaction at the beginning of middleware if is left
+  over from previous request
+
+
+================
+1.1.0 2018-10-19
+================
+
+Added
+-----
+- Support using prioritized batcher as a context manager
+
+
+================
+1.0.0 2018-10-19
+================
+
+Initial version
---
+++ packages/django-priority-batch/django-priority-batch-3.0.0a2.tar.gz/django-priority-batch-3.0.0a2/setup.py
@@ -0,0 +1,59 @@
+import os.path
+import setuptools
+
+# Get long description from README.
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+# Get package metadata from '__about__.py' file.
+about = {}
+base_dir = os.path.abspath(os.path.dirname(__file__))
+with open(
+    os.path.join(base_dir, 'src', 'django_priority_batch', '__about__.py'), 'r'
+) as fh:
+    exec(fh.read(), about)
+
+setuptools.setup(
+    name=about['__title__'],
+    use_scm_version=True,
+    description=about['__summary__'],
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author=about['__author__'],
+    author_email=about['__email__'],
+    url=about['__url__'],
+    license=about['__license__'],
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.6, <3.10',
+    install_requires=['Django~=3.1.5'],
+    extras_require={
+        'docs': ['Sphinx', 'sphinx_rtd_theme'],
+        'package': ['twine', 'wheel'],
+        'test': [
+            'black',
+            'pydocstyle',
+            'check-manifest',
+            'readme_renderer',
+            'setuptools_scm',
+            'pytest',
+            'pytest-django',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='django transaction batching prioritization',
+)
