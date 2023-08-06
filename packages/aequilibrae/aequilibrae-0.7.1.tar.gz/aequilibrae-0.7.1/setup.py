a9fd4e3780559f5279e738f6013cb7b027a078b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:14 +0000

    Add package aequilibrae/0.7.1/aequilibrae-0.7.1.tar.gz

---
+++ packages/aequilibrae/aequilibrae-0.7.1.tar.gz/aequilibrae-0.7.1/setup.py
@@ -0,0 +1,49 @@
+import sys
+import os
+import numpy as np
+from setuptools import setup, find_packages
+from setuptools import Extension
+from Cython.Distutils import build_ext
+from aequilibrae.paths.__version__ import release_version
+
+sys.dont_write_bytecode = True
+
+here = os.path.dirname(os.path.realpath(__file__))
+whole_path = os.path.join(here, "aequilibrae/paths", "AoN.pyx")
+ext_module = Extension("aequilibrae.paths.AoN", [whole_path], include_dirs=[np.get_include()])
+
+pkgs = [pkg for pkg in find_packages()]
+
+pkg_data = {
+    "aequilibrae.reference_files": ["spatialite.sqlite", "nauru.zip", "sioux_falls.zip"],
+    "aequilibrae.paths": ["parameters.pxi"],
+    "aequilibrae": ["parameters.yml"],
+    "aequilibrae.project": ["database_specification/tables/*.*", "database_specification/triggers/*.*"],
+}
+loose_modules = ["__version__", "parameters"]
+
+if __name__ == "__main__":
+    setup(
+        name="aequilibrae",
+        version=release_version,
+        # TODO: Fix the requirements and optional requirements to bring directly from the requirements file
+        install_requires=["numpy", "PyQt5", "pyaml", "pandas", "requests", "shapely", "scipy"],
+        packages=pkgs,
+        package_dir={"": "."},
+        py_modules=loose_modules,
+        package_data=pkg_data,
+        zip_safe=False,
+        description="A package for transportation modeling",
+        author="Pedro Camargo",
+        author_email="c@margo.co",
+        url="https://github.com/AequilibraE/aequilibrae",
+        license="See license.txt",
+        classifiers=[
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+        ],
+        cmdclass={"build_ext": build_ext},
+        ext_modules=[ext_module],
+    )
---
+++ packages/aequilibrae/aequilibrae-0.7.1.tar.gz/aequilibrae-0.7.1/tests/aequilibrae/paths/test_graph.py
@@ -0,0 +1,93 @@
+from unittest import TestCase
+import os
+import tempfile
+import numpy as np
+from aequilibrae.paths import Graph
+from os.path import join
+from uuid import uuid4
+from .parameters_test import centroids
+from aequilibrae.project import Project
+from ...data import siouxfalls_project
+from aequilibrae.paths.results import PathResults
+from aequilibrae.paths import binary_version
+
+# Adds the folder with the data to the path and collects the paths to the files
+# lib_path = os.path.abspath(os.path.join('..', '../tests'))
+# sys.path.append(lib_path)
+from ...data import path_test, test_graph, test_network
+from shutil import copytree, rmtree
+
+
+class TestGraph(TestCase):
+    def setUp(self) -> None:
+        os.environ["PATH"] = os.path.join(tempfile.gettempdir(), "temp_data") + ";" + os.environ["PATH"]
+        self.temp_proj_folder = os.path.join(tempfile.gettempdir(), uuid4().hex)
+        copytree(siouxfalls_project, self.temp_proj_folder)
+        self.project = Project()
+        self.project.open(self.temp_proj_folder)
+        self.project.network.build_graphs()
+        self.graph = self.project.network.graphs["c"]
+
+    def tearDown(self) -> None:
+        self.project.close()
+
+    def test_prepare_graph(self):
+        graph = self.project.network.graphs["c"]
+        graph.prepare_graph(np.arange(5) + 1)
+
+    def test_set_graph(self):
+        self.graph.set_graph(cost_field="distance")
+        self.graph.set_blocked_centroid_flows(block_centroid_flows=True)
+        self.assertEqual(self.graph.num_zones, 24, "Number of centroids not properly set")
+        self.assertEqual(self.graph.num_links, 76, "Number of links not properly set")
+        self.assertEqual(self.graph.num_nodes, 24, "Number of nodes not properly set - " + str(self.graph.num_nodes))
+
+    def test_save_to_disk(self):
+        self.graph.save_to_disk(join(path_test, "aequilibrae_test_graph.aeg"))
+        self.graph_id = self.graph.__id__
+        self.graph_version = self.graph.__version__
+
+    def test_load_from_disk(self):
+        self.test_save_to_disk()
+        reference_graph = Graph()
+        reference_graph.load_from_disk(test_graph)
+        reference_graph.__version__ = binary_version
+
+        new_graph = Graph()
+        new_graph.load_from_disk(join(path_test, "aequilibrae_test_graph.aeg"))
+
+    def test_available_skims(self):
+        self.graph.prepare_graph(np.arange(5) + 1)
+        avail = self.graph.available_skims()
+        data_fields = [
+            "distance",
+            "name",
+            "lanes",
+            "capacity",
+            "speed",
+            "b",
+            "free_flow_time",
+            "power",
+            "colum",
+            "volume",
+            "modes",
+        ]
+        for i in data_fields:
+            if i not in avail:
+                self.fail("Skim availability with problems")
+
+    def test_exclude_links(self):
+        # excludes a link before any setting or preparation
+        self.graph.set_blocked_centroid_flows(False)
+
+        self.graph.set_graph("distance")
+        r1 = PathResults()
+        r1.prepare(self.graph)
+        r1.compute_path(20, 21)
+        self.assertEqual(list(r1.path), [62])
+
+        r1 = PathResults()
+        self.graph.exclude_links([62])
+        r1.prepare(self.graph)
+        r1.compute_path(20, 21)
+        self.assertEqual(list(r1.path), [63, 69])
