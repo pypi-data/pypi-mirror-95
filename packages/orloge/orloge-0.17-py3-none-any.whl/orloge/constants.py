188fa979f9c2c5feceb85c64c04a214b382ae132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:18 +0000

    Add package orloge/0.17/orloge-0.17-py3-none-any.whl

---
+++ packages/orloge/orloge-0.17-py3-none-any.whl/orloge/constants.py
@@ -0,0 +1,24 @@
+LpStatusNotSolved = 0
+LpStatusSolved = 1
+LpStatusInfeasible = -1
+LpStatusUnbounded = -2
+LpStatusUndefined = -3
+LpStatusTimeLimit = -4
+LpStatusMemoryLimit = -5
+
+LpSolutionNoSolutionFound = 0
+LpSolutionOptimal = 1
+LpSolutionIntegerFeasible = 2
+LpSolutionInfeasible = -1
+LpSolutionUnbounded = -2
+
+solver_to_solution = \
+   {
+   LpStatusNotSolved : LpSolutionNoSolutionFound
+, LpStatusSolved : LpSolutionOptimal
+, LpStatusInfeasible  : LpSolutionInfeasible
+, LpStatusUnbounded   : LpSolutionUnbounded
+, LpStatusUndefined   : LpSolutionNoSolutionFound
+, LpStatusTimeLimit   : LpSolutionNoSolutionFound
+, LpStatusMemoryLimit : LpSolutionNoSolutionFound
+}
