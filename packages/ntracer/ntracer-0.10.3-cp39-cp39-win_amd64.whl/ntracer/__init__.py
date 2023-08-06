01f2cab0f654afa993e6112dcc9498bf4b532c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:05 +0000

    Add package ntracer/0.10.3/ntracer-0.10.3-cp39-cp39-win_amd64.whl

---
+++ packages/ntracer/ntracer-0.10.3-cp39-cp39-win_amd64.whl/ntracer/__init__.py
@@ -0,0 +1,9 @@
+"""The names :py:class:`.render.Color`, :py:class:`.render.Material`,
+:py:class:`.render.Channel`, :py:class:`.render.ImageFormat`,
+:py:class:`.render.CallbackRenderer`, :py:class:`.render.BlockingRenderer`,
+:py:class:`.wrapper.NTracer`, :py:data:`.wrapper.CUBE` and
+:py:data:`.wrapper.SPHERE` are imported here for convenience."""
+
+
+from ntracer.render import Color,Material,Channel,ImageFormat,CallbackRenderer,BlockingRenderer
+from ntracer.wrapper import NTracer,CUBE,SPHERE
