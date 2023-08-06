677f0effca1e93b7b29d4ce2233d6310c62aeda5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:51 +0000

    Add package masterprograming/0.0.6/masterprograming-0.0.6-py3-none-any.whl

---
+++ packages/masterprograming/masterprograming-0.0.6-py3-none-any.whl/masterprograming/master.py
@@ -0,0 +1,50 @@
+class Master:
+	def __init__(self):
+		print("MasterPrograming.com")
+		print("--------------------")
+
+	def MPInfo(self):
+		print('Welcome To MasterPrograming library 2021')
+		print('Master Programing is a PIP Package that enables people that aren’t well to Build Software By Coding.')
+		print("----------------------------------")
+		print('Objects Names')
+		print('MPHindi - For Learning Python')
+		print('MPSimpleGui - Tkinter Calculator App')
+		print('MPPnr - PNR Checker App')
+		print('MPKeyboard - Onscreen Keyboard App')
+		print('MPCal - Calculator To Add Numbers')
+		print('MPQuiz - Quiz App')
+		print('MPSciCal - Scientific Calculator App')
+		print('MPApp - Tkinter Login And Signup With Database')
+		print('MPTodoApp - Todo list App')
+
+
+	def MPHindi(self):
+		from masterprograming import PyBasic
+
+	def MPSimpleGui(self):
+		from masterprograming import tkintersimplegui
+
+	def MPPnr(self):
+		from masterprograming import pnrgui
+
+	def MPKeyboard(self):
+		from masterprograming import keyboard
+	
+
+	def MPCal(self):
+		from masterprograming import calgui
+
+
+	def MPQuiz(self):
+		from masterprograming import quiz	
+
+	def MPSciCal(self):
+		from masterprograming import sciencecal	
+
+	def MPApp(self):
+		print("Go To This Link -> https://masterprograming.com/tkinter-login-and-registration-form-with-database/")		
+		print('--------------------------------------------------------------------------------------------------')
+
+	def MPTodoApp(self):
+		from masterprograming import todogui
