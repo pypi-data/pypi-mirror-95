11cd8c05f163abbe52acde23b868e0b55ba0e480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:43 +0000

    Add package aursad/0.1.6/aursad-0.1.6.tar.gz

---
+++ packages/aursad/aursad-0.1.6.tar.gz/aursad-0.1.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aursad",
+    version="0.1.6",
+    author="Błażej Leporowski",
+    author_email="bleporowski@outlook.com",
+    description="Accompanying library to the AURSAD dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CptPirx/robo-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+    python_requires='>=3.6',
+)
