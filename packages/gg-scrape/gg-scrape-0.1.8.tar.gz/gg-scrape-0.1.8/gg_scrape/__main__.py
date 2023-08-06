39ad426c2ffc5d80f4734ceb2be089afc851ea7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:57 +0000

    Add package gg-scrape/0.1.8/gg-scrape-0.1.8.tar.gz

---
+++ packages/gg-scrape/gg-scrape-0.1.8.tar.gz/gg-scrape-0.1.8/gg_scrape/__main__.py
@@ -0,0 +1,5 @@
+# this file exists to support calling the package as 'python -m ggs'
+# https://typer.tiangolo.com/tutorial/package/#support-python-m-optional
+
+from gg_scrape.argsparse import ggs
+ggs(prog_name='ggs')
