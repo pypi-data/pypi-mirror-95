07ba141cfe14be594de21e445391f1e366aef5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:43 +0000

    Add package dukeai/0.1.9/dukeai-0.1.9.tar.gz

---
+++ packages/dukeai/dukeai-0.1.9.tar.gz/dukeai-0.1.9/README.md
@@ -0,0 +1,5 @@
+# Duke-AI Development Package
+
+This is a duke development package which has OCR and other duke APIs.
+
+To know more you can visit our [website](http://duke.ai/).
---
+++ packages/dukeai/dukeai-0.1.9.tar.gz/dukeai-0.1.9/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+                    name="dukeai",
+                    version="0.1.9",
+                    author="shubham kothari",
+                    author_email="shubham@duke.ai",
+                    description="Dukeai Development Package",
+                    long_description=long_description,
+                    url="https://duke.ai",
+                    install_requires=['boto3', 'requests'],
+                    packages=setuptools.find_packages(),
+                    classifiers=[
+                                    "Programming Language :: Python :: 3",
+                                    "License :: OSI Approved :: MIT License",
+                                    "Operating System :: OS Independent",
+                                ],
+                )
+
+# pip install twine
+# python setup.py sdist
+# twine upload dist/*
