7d29881c9685ab46718bb65ecbf45b4e4c714782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:07 +0000

    Add package scmer/0.1.0a1/scmer-0.1.0a1.tar.gz

---
+++ packages/scmer/scmer-0.1.0a1.tar.gz/scmer-0.1.0a1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from pathlib import Path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scmer",
+    version="v0.1.0a1",
+    author="Shaoheng Liang",
+    author_email="",
+    description="Manifold preserving marker selection for single-cell data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://scmer.readthedocs.io/",
+    packages=['scmer'], #setuptools.find_packages(),
+    install_requires=[
+        l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
