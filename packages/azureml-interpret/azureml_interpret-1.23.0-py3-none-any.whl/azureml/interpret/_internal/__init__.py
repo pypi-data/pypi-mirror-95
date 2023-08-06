d4a8fd3a31f00e30ea01f89b8164cd5c6be4a995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:19 +0000

    Add package azureml-interpret/1.23.0/azureml_interpret-1.23.0-py3-none-any.whl

---
+++ packages/azureml-interpret/azureml_interpret-1.23.0-py3-none-any.whl/azureml/interpret/_internal/__init__.py
@@ -0,0 +1,5 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Init file for azureml-interpret/azureml/interpret/_internal."""
