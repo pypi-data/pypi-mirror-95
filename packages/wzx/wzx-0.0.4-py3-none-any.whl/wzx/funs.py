081d12972acaaf45c44c486fd97677dfeea2cb10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:11 +0000

    Add package wzx/0.0.4/wzx-0.0.4-py3-none-any.whl

---
+++ packages/wzx/wzx-0.0.4-py3-none-any.whl/wzx/funs.py
@@ -0,0 +1,72 @@
+import sys
+import re
+
+if (sys.version_info[0] < 3):
+    import urllib2
+    import urllib
+    import HTMLParser
+else:
+    import html.parser
+    import urllib.request
+    import urllib.parse
+    import html
+
+agent = {'User-Agent':
+             "Mozilla/4.0 (\
+             compatible;\
+             MSIE 6.0;\
+             Windows NT 5.1;\
+             SV1;\
+             .NET CLR 1.1.4322;\
+             .NET CLR 2.0.50727;\
+             .NET CLR 3.0.04506.30\
+             )"}
+
+
+def unescape(text):
+    if (sys.version_info[0] < 3):
+        parser = HTMLParser.HTMLParser()
+    else:
+        parser = html.parser.HTMLParser()
+    return (html.unescape(text))
+
+
+def translate(to_translate, to_language="auto", from_language="auto"):
+    """Returns the translation using google translate
+    you must shortcut the language you define
+    (French = fr, English = en, Spanish = es, etc...)
+    if not defined it will detect it or use english by default
+    Example:
+    print(translate("salut tu vas bien?", "en"))
+    hello you alright?
+    """
+    errflg = False
+    try:
+        base_link = "http://translate.google.cn/m?tl=%s&sl=%s&q=%s"
+        if (sys.version_info[0] < 3):
+            to_translate = urllib.quote_plus(to_translate)
+            link = base_link % (to_language, from_language, to_translate)
+            request = urllib2.Request(link, headers=agent)
+            raw_data = urllib2.urlopen(request).read()
+        else:
+            to_translate = urllib.parse.quote(to_translate)
+            link = base_link % (to_language, from_language, to_translate)
+            request = urllib.request.Request(link, headers=agent)
+            raw_data = urllib.request.urlopen(request, timeout=2).read()
+    except BaseException as e:
+        errflg = True
+        print(e)
+        print(type(e))
+
+    if not errflg:
+        data = raw_data.decode("utf-8")
+        # expr = r'class="t0">(.*?)<'
+        expr = r'class="result-container">(.*?)<'
+        re_result = re.findall(expr, data)
+        if (len(re_result) == 0):
+            result = ""
+        else:
+            result = unescape(re_result[0])
+        return (result)
+    else:
+        return ("")
