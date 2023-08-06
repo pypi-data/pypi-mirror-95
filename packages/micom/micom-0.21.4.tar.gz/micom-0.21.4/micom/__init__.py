b481ab107c8220d667bc80e70e32c00a8a1b8cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:52 +0000

    Add package micom/0.21.4/micom-0.21.4.tar.gz

---
+++ packages/micom/micom-0.21.4.tar.gz/micom-0.21.4/micom/__init__.py
@@ -0,0 +1,36 @@
+"""Simple init file for mico."""
+
+from micom.community import Community
+from micom.util import load_pickle
+from micom import (
+    algorithms,
+    problems,
+    util,
+    data,
+    duality,
+    elasticity,
+    media,
+    qiime_formats,
+    solution,
+    workflows
+)
+
+
+__all__ = (
+    "Community",
+    "algorithms",
+    "db",
+    "problems",
+    "optcom",
+    "util",
+    "data",
+    "duality",
+    "elasticity",
+    "media",
+    "qiime_formats",
+    "solution",
+    "load_pickle",
+    "workflows",
+)
+
+__version__ = "0.21.4"
