636f513bdd7573fc272680e6e9d88528f3883b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:49 +0000

    Add package adsmsg/1.2.0/adsmsg-1.2.0-py2.py3-none-any.whl

---
+++ packages/adsmsg/adsmsg-1.2.0-py2.py3-none-any.whl/adsmsg/__init__.py
@@ -0,0 +1,21 @@
+from .augmentrecord import AugmentAffiliationRequestRecord, \
+                           AugmentAffiliationRequestRecordList, \
+                           AugmentAffiliationResponseRecord, \
+                           AugmentAffiliationResponseRecordList
+from .bibrecord import BibRecord
+from .denormalized_record import DenormalizedRecord
+from .fulltext_requests import FulltextRequests
+from .fulltext_update import FulltextUpdate
+from .metrics_record import MetricsRecord, \
+                            MetricsRecordList
+from .nonbibrecord import NonBibRecord,\
+                NonBibRecordList, \
+                DataLinksRecord, \
+                DataLinksRecordList
+from .orcid_claims import OrcidClaims
+from .status import Status
+from .citation_changes import CitationChanges, CitationChange
+from .citation_changes_content_type import CitationChangeContentType
+
+
+
