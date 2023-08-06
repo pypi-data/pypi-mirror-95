2241c82bff7f1767e7ac6398e68abf8e5f372e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:58 +0000

    Add package pglast/2.0.dev3/pglast-2.0.dev3-cp39-cp39-manylinux1_x86_64.whl

---
+++ packages/pglast/pglast-2.0.dev3-cp39-cp39-manylinux1_x86_64.whl/pglast/enums/lockdefs.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# :Project:   pglast -- DO NOT EDIT: automatically extracted from lockdefs.h @ 12.1
+# :Author:    Lele Gaifax <lele@metapensiero.it>
+# :License:   GNU General Public License version 3 or later
+# :Copyright: © 2017-2020 Lele Gaifax
+#
+
+from enum import Enum, IntEnum, IntFlag, auto
+
+
+
+# #define-ed constants
+
+NoLock = 0
+
+AccessShareLock = 1
+
+RowShareLock = 2
+
+RowExclusiveLock = 3
+
+ShareUpdateExclusiveLock = 4
+
+ShareLock = 5
+
+ShareRowExclusiveLock = 6
+
+ExclusiveLock = 7
+
+AccessExclusiveLock = 8
+
+MaxLockMode = 8
