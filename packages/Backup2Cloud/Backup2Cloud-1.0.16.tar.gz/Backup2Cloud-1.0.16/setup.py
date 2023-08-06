7f6fbcf9f5a7b0f852ac0bf9697bd93019740faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:27 +0000

    Add package Backup2Cloud/1.0.16/Backup2Cloud-1.0.16.tar.gz

---
+++ packages/Backup2Cloud/Backup2Cloud-1.0.16.tar.gz/Backup2Cloud-1.0.16/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Backup2Cloud",
+    version="1.0.16",
+    author="otapi",
+    description="Backup specific folders to and upload to a cloud provider",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/otapi/Backup2Cloud",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+        'py7zr',
+        'dirtools2'
+       
+    ],
+    python_requires='>=3.7.3',
+    package_data={
+        # If any package contains *.txt files, include them:
+        "": ["*.ini"],
+    }
+) 
