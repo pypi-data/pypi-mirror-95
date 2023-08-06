5ea6b12d368284521e10c725f825dbac0576da18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:57 +0000

    Add package upbit-client/1.1.7.0/upbit_client-1.1.7.0.tar.gz

---
+++ packages/upbit-client/upbit_client-1.1.7.0.tar.gz/upbit_client-1.1.7.0/setup.py
@@ -0,0 +1,43 @@
+
+from setuptools import setup
+from setuptools import find_packages
+
+import upbit
+
+
+version = upbit.__version__
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+
+setup(
+    name='upbit_client',
+    version=version,
+    packages=find_packages(),
+    install_requires=[
+        'bravado>=11.0.2',
+        'PyJWT>=2.0',
+        'websockets>=8.1'
+    ],
+    extras_require={
+        'fido': [
+            'fido>=4.2.1'
+        ]
+    },
+    python_requires='>=3.8',
+    keywords=[
+        'Upbit',
+        'upbit',
+        'upbit-client',
+        'Upbit-Client',
+        'Upbit_client'
+    ],
+    url='https://github.com/uJhin/upbit-client',
+    download_url='https://github.com/uJhin/upbit-client/releases',
+    license='MIT License',
+    author='ujhin',
+    author_email='ujhin942@gmail.com',
+    description='Upbit OPEN API Client',
+    long_description_content_type='text/x-rst',
+    long_description=long_description
+)
