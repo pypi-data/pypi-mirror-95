c31c13cdbe91e79cb9d0e16a3277091c7dcecc04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:35 +0000

    Add package ledger-compliance-py/2.1.5rc1/ledger_compliance_py-2.1.5rc1-py3-none-any.whl

---
+++ packages/ledger-compliance-py/ledger_compliance_py-2.1.5rc1-py3-none-any.whl/LedgerCompliance/types.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+@dataclass
+class LCState:
+	db: str
+	txid: int
+	txhash: bytes
+	
+@dataclass
+class LCIndex:
+	txid: int
+	
+@dataclass
+class LCItem:
+	txid: int
+	key: bytes
+	value: bytes
+
+@dataclass
+class ZItem:
+	txid: int
+	key: bytes
+	value: bytes
+	score: float
+
+    
+@dataclass
+class SafeSetResponse:
+	txid: int
+	verified: bool
+    
+@dataclass
+class SafeGetResponse:
+    txid: int
+    key: bytes
+    value: bytes
+    timestamp: int
+    verified: bool
+    refkey: bytes
+    
+@dataclass    
+class HealthInfo:
+	status: bool
+	version: str
+
+
+# exceptions
+class VerificationException(Exception):
+	pass
+
+class ErrMaxWidthExceeded(Exception):
+	pass
+
+class ErrIllegalArguments(Exception):
+	pass
+
