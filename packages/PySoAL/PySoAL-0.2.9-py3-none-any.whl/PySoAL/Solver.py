7e0a83b05adf709984b32d0b203d07ae4ae291b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:43 +0000

    Add package PySoAL/0.2.9/PySoAL-0.2.9-py3-none-any.whl

---
+++ packages/PySoAL/PySoAL-0.2.9-py3-none-any.whl/PySoAL/Solver.py
@@ -0,0 +1,14 @@
+#  Copyright (c) 2021. Institute for High Voltage Equipment and Grids, Digitalization and Power Economics (IAEW)
+#  RWTH Aachen University
+#  Contact: Thomas Offergeld (t.offergeld@iaew.rwth-aachen.de)
+#  #
+#  This module is part of PySoAL.
+#  #
+#  PySoAL is licensed under the BSD-3-Clause license.
+#  For further information see LICENSE in the project's root directory.
+
+from abc import ABC
+
+
+class Solver(ABC):
+    pass
