99296d632081706ea07283a7865a5472661f1dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:03 +0000

    Add package cloudbender/0.9.8/cloudbender-0.9.8-py2.py3-none-any.whl

---
+++ packages/cloudbender/cloudbender-0.9.8-py2.py3-none-any.whl/cloudbender/exceptions.py
@@ -0,0 +1,14 @@
+class ParameterNotFound(Exception):
+    """My documentation"""
+
+
+class ParameterIllegalValue(Exception):
+    """My documentation"""
+
+
+class InvalidProjectDir(Exception):
+    """My documentation"""
+
+
+class InvalidHook(Exception):
+    """My documentation"""
