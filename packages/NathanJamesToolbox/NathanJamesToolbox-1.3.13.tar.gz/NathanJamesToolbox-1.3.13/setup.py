9fb666331168a2a3e47064de5c6f1220bf2978d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:14 +0000

    Add package NathanJamesToolbox/1.3.13/NathanJamesToolbox-1.3.13.tar.gz

---
+++ packages/NathanJamesToolbox/NathanJamesToolbox-1.3.13.tar.gz/NathanJamesToolbox-1.3.13/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='NathanJamesToolbox',
+    version='1.3.13',
+    description='Collection of tools developed for NathanJames',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/pfajardo-nj/NathanJamesToolbox',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages=['NathanJamesToolbox'],
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'slacker',
+        'datetime',
+        'google.cloud',
+        'pymysql',
+        'selenium'
+    ]
+)
