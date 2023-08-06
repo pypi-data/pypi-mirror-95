40ec61a6799837c14b2f7577d090402fa0ca5486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:14 +0000

    Add package otree/5.0.0a36/otree-5.0.0a36.tar.gz

---
+++ packages/otree/otree-5.0.0a36.tar.gz/otree-5.0.0a36/otree/api.py
@@ -0,0 +1,10 @@
+from otree.models import BaseSubsession, BaseGroup, BasePlayer  # noqa
+from otree.constants import BaseConstants  # noqa
+from otree.views import Page, WaitPage  # noqa
+from otree.currency import Currency, currency_range, safe_json  # noqa
+from otree.bots import Bot, Submission, SubmissionMustFail, expect  # noqa
+from otree import database as models  # noqa
+from otree.forms import widgets  # noqa
+from otree.i18n import extract_otreetemplate  # noqa
+
+cu = c = Currency
