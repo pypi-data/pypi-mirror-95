fee86be8da72a8ec1b6c25a730fddb8e85afe981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:55 +0000

    Add package nakametpy/0.2.4/nakametpy-0.2.4.tar.gz

---
+++ packages/nakametpy/nakametpy-0.2.4.tar.gz/nakametpy-0.2.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+install_requires = [
+    'numpy',
+    'matplotlib',
+    'nose',
+]
+
+packages = [
+    'nakametpy',
+    'tests'
+]
+
+console_scripts = [
+    # 'sample_lib_cli=sample_lib_cli.call:main',
+]
+
+
+setup(
+    name='nakametpy',
+    python_requires='>3.6.0',
+    # version='0.1.0', # (2021.01.19)
+    version='0.2.4', # (2021.02.18)
+    desctiption='Meteorological modules for calculation and colormap.',
+    packages=packages,
+    install_requires=install_requires,
+    url = 'https://github.com/muchojp/NakaMetPy',
+    author = 'Yuki Nakamura',
+    author_email = 'contact.muchiwo@gmail.com',
+    # entry_points={'console_scripts': console_scripts},
+    long_description=open('README.md', encoding='UTF-8').read(),
+    long_description_content_type="text/markdown",
+)
