1e9794c2bb2ed23544edfc62a10a7d58f3948dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:28 +0000

    Add package vistec-ser/0.3.0a1/vistec_ser-0.3.0a1-py3-none-any.whl

---
+++ packages/vistec-ser/vistec_ser-0.3.0a1-py3-none-any.whl/vistec_ser/utils/utils.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import sys
+
+
+def float_feature(list_of_floats):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=list_of_floats))
+
+
+def int64_feature(list_of_ints):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=list_of_ints))
+
+
+def bytestring_feature(list_of_bytestrings):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=list_of_bytestrings))
+
+
+def print_one_line(*args):
+    tf.print("\033[K", end="")
+    tf.print("\r", *args, sep="", end=" ", output_stream=sys.stdout)
