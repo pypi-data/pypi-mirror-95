c95a3e13088277005f3bbedaa61e79dbaf03d3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:55 +0000

    Add package mloq/0.0.52/mloq-0.0.52-py3-none-any.whl

---
+++ packages/mloq/mloq-0.0.52-py3-none-any.whl/mloq/cli/main.py
@@ -0,0 +1,87 @@
+"""Command line interface for mloq."""
+
+from pathlib import Path
+import sys
+from unittest.mock import patch
+
+import click
+import hydra
+from omegaconf import DictConfig
+
+from mloq.files import mloq_yml
+
+
+overwrite_opt = click.option(
+    "--overwrite/--no-overwrite",
+    "-o/ ",
+    default=False,
+    show_default=True,
+    help="Value indicating whether to overwrite existing files.",
+)
+config_file_opt = click.option(
+    "--filename",
+    "-f",
+    "config_file",
+    default=None,
+    show_default=True,
+    help="Name of the repository config file",
+    type=click.Path(exists=True, file_okay=True, dir_okay=True, resolve_path=True),
+)
+only_config_opt = click.option(
+    "--only-config/--everything",
+    "-c/ ",
+    default=False,
+    show_default=True,
+    help="Value indicating whether to not generate all the files except mloq.yml.",
+)
+output_directory_arg = click.argument(
+    "output_directory",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True),
+)
+
+interactive_opt = click.option(
+    "--interactive/--no-interactive",
+    "-i/ ",
+    default=False,
+    show_default=True,
+    help="If True the configuration values will be defined interactively on the command line.",
+)
+
+
+@click.group()
+def cli():
+    """Command line interface for ML Ops Quickstart."""
+    pass
+
+
+@cli.command(context_settings=dict(ignore_unknown_options=True))
+@config_file_opt
+@output_directory_arg
+@overwrite_opt
+@interactive_opt
+@only_config_opt
+@click.argument("hydra_args", nargs=-1, type=click.UNPROCESSED)
+def setup(
+    config_file: str,
+    output_directory: str,
+    overwrite: bool,
+    only_config: bool,
+    interactive: bool,
+    hydra_args: str,
+) -> None:
+    """Entry point of `mloq setup`."""
+    from mloq.cli.setup_cmd import setup_cmd
+
+    config_file = Path(config_file) if config_file else mloq_yml.src
+    hydra_args = ["--config-dir", str(config_file.parent)] + list(hydra_args)
+    config = DictConfig({})
+
+    @hydra.main(config_name=config_file.name)
+    def load_config(loaded_config: DictConfig):
+        nonlocal config
+        config = loaded_config
+
+    with patch("sys.argv", [sys.argv[0]] + list(hydra_args)):
+        load_config()
+
+    exit(setup_cmd(config, output_directory, overwrite, interactive, only_config))
