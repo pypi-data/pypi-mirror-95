97ce05cdbb26c4d56d7bcb8dfcdb7c7b20de55b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:51 +0000

    Add package hetsar-ida-b/0.2/hetsar-ida-b-0.2.tar.gz

---
+++ packages/hetsar-ida-b/hetsar-ida-b-0.2.tar.gz/hetsar-ida-b-0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hetsar-ida-b", # Replace with your own username
+    version="0.2",
+    author="Ida Johnsson",
+    author_email="ida.b.johnsson@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
