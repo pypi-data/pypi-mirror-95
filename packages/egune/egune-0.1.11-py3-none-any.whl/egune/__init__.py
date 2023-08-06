063bcaa1fcb2778d1f0cc08fa79cb89d192d7b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:02 +0000

    Add package egune/0.1.11/egune-0.1.11-py3-none-any.whl

---
+++ packages/egune/egune-0.1.11-py3-none-any.whl/egune/__init__.py
@@ -0,0 +1,11 @@
+from egune.requests import (  # noqa: 401
+    ButtonQuestion,
+    YesNoQuestion,
+    OpenQuestion,
+    Fail,
+    Tell,
+    Do,
+    MultiSelectQuestion,
+    CheckboxQuestion,
+    Form
+)
