b53d1872f115f9e6996a19e4c50e86f8b3cacb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:12 +0000

    Add package handypy/0.2/handypy-0.2.tar.gz

---
+++ packages/handypy/handypy-0.2.tar.gz/handypy-0.2/README.md
@@ -0,0 +1,5 @@
+# HandyPy
+
+![travis](https://travis-ci.com/jiayiliu/handypy.svg?branch=main)  [![codecov](https://codecov.io/gh/jiayiliu/handypy/branch/main/graph/badge.svg?token=SKALHNC98F)](https://codecov.io/gh/jiayiliu/handypy)
+
+Handy python scrips
---
+++ packages/handypy/handypy-0.2.tar.gz/handypy-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='handypy',
+      version='0.2',
+      description='Handy Python scripts',
+      url='http://github.com/jiayiliu/handypy',
+      author='Jiayi Liu',
+      author_email='jiayiliu@users.noreply.github.com',
+      license='MIT',
+      packages=['handypy'],
+      zip_safe=False)
