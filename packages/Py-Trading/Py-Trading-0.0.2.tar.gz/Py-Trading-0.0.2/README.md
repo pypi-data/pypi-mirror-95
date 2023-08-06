c7244d8063cfd9eb943f93a1795f55f75a2b6ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:59 +0000

    Add package Py-Trading/0.0.2/Py-Trading-0.0.2.tar.gz

---
+++ packages/Py-Trading/Py-Trading-0.0.2.tar.gz/Py-Trading-0.0.2/README.md
@@ -0,0 +1,3 @@
+# Pytrading
+This package contains modules with functions that assist day and swing traders by aiding them in keeping track of their positions 
+and trending stocks.
---
+++ packages/Py-Trading/Py-Trading-0.0.2.tar.gz/Py-Trading-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Py-Trading", # Replace with your own username
+    version="0.0.2",
+    author="Joseph Camyre",
+    author_email="jwcamry03@gmail.com",
+    description="A convenient Python module for stock information.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JCamyre/Python-Trading",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
