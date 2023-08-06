07c904aef47617d2e1b03f142dfba782a0880370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:56 +0000

    Add package navio-builder-win/0.1.54/navio_builder_win-0.1.54-py2.py3-none-any.whl

---
+++ packages/navio-builder-win/navio_builder_win-0.1.54-py2.py3-none-any.whl/navio/meta_builder.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.54"
+__version__ = "0.1.54"
+__license__ = "MIT License"
+__website__ = "https://oss.navio.tech/navio-builder/"
+__download_url__ = ('https://github.com/naviotech/navio-builder/archive/'
+                    '{}.tar.gz'.format(__version__))
