2b9a1f2ad72eb78cfd87e9d0568b4bbf2c3940f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:06 +0000

    Add package kodexa/2.3.219/kodexa-2.3.219.tar.gz

---
+++ packages/kodexa/kodexa-2.3.219.tar.gz/kodexa-2.3.219/kodexa/__init__.py
@@ -0,0 +1,19 @@
+"""
+    Kodexa is a Python framework to enable flexible data engineering with semi-structured and unstructured documents and
+    data.
+"""
+from .cloud import RemoteSession, RemotePipeline, RemoteAction, KodexaPlatform
+from .connectors import FolderConnector, FileHandleConnector, UrlConnector, registered_connectors, get_connectors, \
+    get_connector, add_connector, get_source
+from .extractors import *
+from .model import DocumentMetadata, ContentNode, ContentFeature, SourceMetadata, \
+    Document, RemoteStore, DocumentStore
+from .pipeline import Pipeline, PipelineStatistics, PipelineContext
+from .sinks import InMemoryDocumentSink, FolderSink
+from .steps import NodeTagger, NodeTagCopy, TextParser, RollupTransformer, TagsToKeyValuePairExtractor, \
+    KodexaProcessingException
+from .stores import JsonDocumentStore, TableDataStore, DictDataStore, DataStoreHelper, LocalDocumentStore, \
+    RemoteDocumentStore, RemoteTableDataStore, RemoteDictDataStore, RemoteModelStore, LocalModelStore
+from .taxonomy import Taxonomy, Taxon, RemoteTaxonomy
+from .workflow import Workflow, CronSchedule, WebSchedule, StorePublisher
+from .assistant import Assistant
---
+++ packages/kodexa/kodexa-2.3.219.tar.gz/kodexa-2.3.219/kodexa/model/__init__.py
@@ -0,0 +1,13 @@
+"""
+Core Model
+----------
+
+The core model provides the object structure for Documents, ContentNodes and Features which is used as the foundation
+for working with unstructured data in the framework.
+
+Create a new instance of a Document, you will be required to provide a DocumentMetadata object
+
+    >>> document = Document(DocumentMetadata())
+"""
+from .model import DocumentMetadata, ContentNode, ContentFeature, SourceMetadata, \
+    Document, Store, DocumentStore, RemoteStore, ModelStore
