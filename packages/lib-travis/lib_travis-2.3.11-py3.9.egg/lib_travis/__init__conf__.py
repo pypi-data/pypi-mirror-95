7b4fa80a3e9181ba9140d8f58c00a0b99e91b2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:17 +0000

    Add package lib-travis/2.3.11/lib_travis-2.3.11-py3.9.egg

---
+++ packages/lib-travis/lib_travis-2.3.11-py3.9.egg/lib_travis/__init__conf__.py
@@ -0,0 +1,22 @@
+name = "lib_travis"
+title = "travis related utilities"
+version = "v2.3.11"
+url = "https://github.com/bitranox/lib_travis"
+author = "Robert Nowotny"
+author_email = "bitranox@gmail.com"
+shell_command = "lib_travis"
+
+
+def print_info() -> None:
+    print(
+        """\
+
+Info for lib_travis:
+
+    travis related utilities
+
+    Version : v2.3.11
+    Url     : https://github.com/bitranox/lib_travis
+    Author  : Robert Nowotny
+    Email   : bitranox@gmail.com"""
+    )
