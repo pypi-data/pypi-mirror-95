9f44a6019f510dbb9c8abc811ea1c9bb609c95af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:15 +0000

    Add package example-pkg-zyxstudycs/0.0.1/example-pkg-zyxstudycs-0.0.1.tar.gz

---
+++ packages/example-pkg-zyxstudycs/example-pkg-zyxstudycs-0.0.1.tar.gz/example-pkg-zyxstudycs-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-zyxstudycs",
+    version="0.0.1",
+    author="Yixing Zheng",
+    author_email="zhengyixing01@corp.netease.com",
+    description="Try package python code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
