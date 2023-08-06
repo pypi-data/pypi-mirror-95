4c563d807a4ad12d9e9a39f8601b77b8a34e890f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:38 +0000

    Add package stats-advanced/0.0.5/stats_advanced-0.0.5.tar.gz

---
+++ packages/stats-advanced/stats_advanced-0.0.5.tar.gz/stats_advanced-0.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Making statistics easier with python'
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+# Setting up
+setup(
+    name="stats_advanced",
+    version=VERSION,
+    author="Aayushmaan Jain",
+    author_email="<aayushmaan1306@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['statsmodels', 'numpy', 'pandas', 'sklearn', 'matplotlib'],
+    keywords=['python', 'statistics', 'made', 'easy', 'mean', 'median', 'mode', 'standard deviation', 'skewness'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
