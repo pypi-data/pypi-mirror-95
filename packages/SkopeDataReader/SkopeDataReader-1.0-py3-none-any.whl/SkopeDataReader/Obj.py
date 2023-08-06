7cc81db126152a6f183c46d0085741b37b7f4c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:53 +0000

    Add package SkopeDataReader/1.0/SkopeDataReader-1.0-py3-none-any.whl

---
+++ packages/SkopeDataReader/SkopeDataReader-1.0-py3-none-any.whl/SkopeDataReader/Obj.py
@@ -0,0 +1,8 @@
+# this class populates a dictionary with attribute-style access (main.sub.subsub) from a json object
+class Obj(object):
+    def __init__(self, d):
+        for a, b in d.items():
+            if isinstance(b, (list, tuple)):
+                setattr(self, a, [Obj(x) if isinstance(x, dict) else x for x in b])
+            else:
+                setattr(self, a, Obj(b) if isinstance(b, dict) else b)
