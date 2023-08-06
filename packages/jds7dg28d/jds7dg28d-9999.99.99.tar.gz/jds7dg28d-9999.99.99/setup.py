e93d9d6384d6c64324d54550bca2e9683c44a172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:42 +0000

    Add package jds7dg28d/9999.99.99/jds7dg28d-9999.99.99.tar.gz

---
+++ packages/jds7dg28d/jds7dg28d-9999.99.99.tar.gz/jds7dg28d-9999.99.99/setup.py
@@ -0,0 +1,17 @@
+import jds7dg28d
+import setuptools
+
+setuptools.setup(
+    name='jds7dg28d',
+    version='9999.99.99',
+    author='jds7dg28d',
+    author_email='jds7dg28d@gmail.com',
+    description='jds7dg28d',
+    long_description='jds7dg28d',
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+    ]
+    )
