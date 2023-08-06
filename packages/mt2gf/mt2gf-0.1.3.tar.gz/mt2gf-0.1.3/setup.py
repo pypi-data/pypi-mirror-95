bc292edb0fd707f715fe0aae2137594056c7f4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:04 +0000

    Add package mt2gf/0.1.3/mt2gf-0.1.3.tar.gz

---
+++ packages/mt2gf/mt2gf-0.1.3.tar.gz/mt2gf-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+from pathlib import Path
+from os import path
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name='mt2gf',
+    version='0.1.3',
+    author='Yann Mentha',
+    author_email="yann.mentha@gmail.com",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/ymentha14/mturk2gform",
+    packages = find_packages(),
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires= Path("requirements.txt").read_text().splitlines(),
+)
