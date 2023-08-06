5ee8313efb5023847a742d5f024220ae1b04c8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:40 +0000

    Add package okornot/0.2/okornot-0.2.tar.gz

---
+++ packages/okornot/okornot-0.2.tar.gz/okornot-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'okornot', 
+	  version = '0.2', 
+	  description = 'Test OK or Not', 
+	  package = ['okornot'], 
+	  author = ['Hengshi Yu'], 
+	  author_email = 'hengshi@umich.edu', 
+	  zip_safe = False)
