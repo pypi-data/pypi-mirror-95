6b0dd9e243c55d39869803e2ba9e640e0d8a0567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:27 +0000

    Add package ctypeslib2/2.2.3/ctypeslib2-2.2.3-py3-none-any.whl

---
+++ packages/ctypeslib2/ctypeslib2-2.2.3-py3-none-any.whl/ctypeslib/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# ctypeslib package
+
+from pkg_resources import get_distribution, DistributionNotFound
+import os
+import sys
+
+try:
+    _dist = get_distribution('ctypeslib2')
+    # Normalize case for Windows systems
+    # if you are in a virtualenv, ./local/* are aliases to ./*
+    dist_loc = os.path.normcase(os.path.realpath(_dist.location))
+    here = os.path.normcase(os.path.realpath(__file__))
+    if not here.startswith(os.path.join(dist_loc, 'ctypeslib')):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'Please install this project with setup.py'
+else:
+    __version__ = _dist.version
+
+# configure python-clang to use the local clang library
+try:
+    from ctypes.util import find_library
+    # debug for python-haystack travis-ci
+    for version in ["libclang", "clang", "clang-11", "clang-5.0", "clang-4.0", "clang-3.9", "clang-3.8", "clang-3.7"]:
+        if find_library(version) is not None:
+            from clang import cindex
+            cindex.Config.set_library_file(find_library(version))
+            break
+    else:
+        if os.name == "posix" and sys.platform == "darwin":
+            # On darwin, consider that Xcode should be installed in its default path.
+            from clang import cindex
+            cindex.Config.set_library_file('/Applications/Xcode.app/Contents/Frameworks/libclang.dylib')
+except ImportError as e:
+    print(e)
+
+
