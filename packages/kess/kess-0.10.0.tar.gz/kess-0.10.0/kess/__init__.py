baf2539ecbe34b9e161a73adf5ba0637fd80f0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:54 +0000

    Add package kess/0.10.0/kess-0.10.0.tar.gz

---
+++ packages/kess/kess-0.10.0.tar.gz/kess-0.10.0/kess/__init__.py
@@ -0,0 +1,3 @@
+from kess.app import App
+from kess.function import Function
+from kess.exceptions import *
---
+++ packages/kess/kess-0.10.0.tar.gz/kess-0.10.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['kess']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.63.0,<0.64.0', 'httpx>=0.16.1,<0.17.0']
+
+setup_kwargs = {
+    'name': 'kess',
+    'version': '0.10.0',
+    'description': '',
+    'long_description': None,
+    'author': 'majik',
+    'author_email': 'me@yamajik.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
