b9bae4a4f459a75aaa2950cafe10944b99c054b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:01 +0000

    Add package zcscommonlib/0.3.1/zcscommonlib-0.3.1.tar.gz

---
+++ packages/zcscommonlib/zcscommonlib-0.3.1.tar.gz/zcscommonlib-0.3.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='zcscommonlib',
+    packages=find_packages(include=['zcscommonlib']),
+    version='0.3.1',
+    description='A Common Library For Use In Computer Science Projects',
+    long_description="README.MD",
+    long_description_content_type="text/markdown",
+    author='Zandercraft',
+    url='https://github.com/Zandercraft/ZCSCommonLibrary',
+    download_url='https://github.com/Zandercraft/ZCSCommonLibrary/archive/0.2.0.tar.gz',
+    license='MPL 2.0',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==6.2.2'],
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
