c87ea5450c4ae4f44fa1a4c22caee0bd9f7230f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:15 +0000

    Add package Geode-BackgroundMesh/2.5.7/Geode_BackgroundMesh-2.5.7-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/Geode-BackgroundMesh/Geode_BackgroundMesh-2.5.7-cp36-cp36m-manylinux2014_x86_64.whl/geode_backgroundmesh/bmesh_2d.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2021 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core, modifier_surface, cutter_surface, helpers
+from geode_conversion import model
+
+from .geode_backgroundmesh_py_bmesh_2d import *
