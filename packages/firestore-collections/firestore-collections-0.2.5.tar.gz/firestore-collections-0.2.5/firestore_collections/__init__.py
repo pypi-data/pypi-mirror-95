e87ab860e5573316607fa7803221f3b7def9f02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:53 +0000

    Add package firestore-collections/0.2.5/firestore-collections-0.2.5.tar.gz

---
+++ packages/firestore-collections/firestore-collections-0.2.5.tar.gz/firestore-collections-0.2.5/firestore_collections/__init__.py
@@ -0,0 +1,18 @@
+from firestore_collections.collection import Collection
+from firestore_collections.enums import (
+    FirestoreTimestamp,
+    OrderByDirection,
+)
+from firestore_collections.schema import (
+    Schema,
+    SchemaWithOwner,
+)
+
+
+__all__ = [
+    'Collection',
+    'FirestoreTimestamp',
+    'OrderByDirection',
+    'Schema',
+    'SchemaWithOwner',
+]
