eca71a0c00df55342065f27a8361d8dedad0be8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:34 +0000

    Add package labtransformer/0.1/labtransformer-0.1.tar.gz

---
+++ packages/labtransformer/labtransformer-0.1.tar.gz/labtransformer-0.1/labtransformer/__init__.py
@@ -0,0 +1,10 @@
+import numpy as np
+from skimage import color
+class LabTransformer():
+    # here you define the operation it should perform
+    def transform(self, X, y=None, **fit_params):
+        return color.rgb2lab(X.astype(np.uint8)).astype(np.float32)
+
+    # just return self
+    def fit(self, X, y=None, **fit_params):
+        return self
---
+++ packages/labtransformer/labtransformer-0.1.tar.gz/labtransformer-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='labtransformer',
+      version='0.1',
+      packages=['labtransformer'],
+      author='Zoé Goey',
+      author_email='zoe.goey@gmail.com',
+      license='MIT',
+      install_requires=['scikit-image','numpy'],
+      zip_safe=False)
