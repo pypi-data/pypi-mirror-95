721c9cb3986425c3da582f468c4b398fc93f2c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:52 +0000

    Add package expressintegrations/1.0.56/expressintegrations-1.0.56.tar.gz

---
+++ packages/expressintegrations/expressintegrations-1.0.56.tar.gz/expressintegrations-1.0.56/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='expressintegrations',
+    version='1.0.56',
+    author="Express Integrations",
+    author_email="express.integrations@gmail.com",
+    description="Custom Utilities for the Express Integrations platform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/expressintegrations",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
