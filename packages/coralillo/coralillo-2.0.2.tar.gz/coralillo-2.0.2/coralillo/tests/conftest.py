ab4ba5f491be495b1f819ce968368715f205fbe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:31 +0000

    Add package coralillo/2.0.2/coralillo-2.0.2.tar.gz

---
+++ packages/coralillo/coralillo-2.0.2.tar.gz/coralillo-2.0.2/coralillo/tests/conftest.py
@@ -0,0 +1,29 @@
+from coralillo import Engine, Model, fields
+from coralillo.auth import PermissionHolder
+import pytest
+
+from .models import bound_models
+
+
+@pytest.fixture
+def nrm():
+    nrm = Engine()
+
+    nrm.lua.drop(args=['*'])
+
+    bound_models(nrm)
+
+    return nrm
+
+
+@pytest.fixture
+def user(nrm):
+    class User(Model, PermissionHolder):
+        name = fields.Text()
+
+        class Meta:
+            engine = nrm
+
+    return User(
+        name='juan',
+    ).save()
