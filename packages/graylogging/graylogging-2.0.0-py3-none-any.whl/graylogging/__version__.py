faa96e9cbb9d19eb8c1b6ce982cca86ec6d59b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:53 +0000

    Add package graylogging/2.0.0/graylogging-2.0.0-py3-none-any.whl

---
+++ packages/graylogging/graylogging-2.0.0-py3-none-any.whl/graylogging/__version__.py
@@ -0,0 +1,9 @@
+__author__ = "Eric Rochow"
+__author_email__ = "ericrochow+pypi@gmail.com"
+__description__ = (
+    "An extension to the builtin logging module that writes to a Graylog"
+    " server instead of a flat file."
+)
+__title__ = "graylogging"
+__url__ = "https://github.com/ericrochow/graylogging"
+__version__ = "2.0.0"
