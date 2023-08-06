283e5ba0387990bb26872ddd24c942a9b356a62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:45 +0000

    Add package mys/0.209.0/mys-0.209.0.tar.gz

---
+++ packages/mys/mys-0.209.0.tar.gz/mys-0.209.0/mys/lib/mys/errors/base.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../types/object.hpp"
+
+namespace mys {
+
+// The Error trait that all errors must implement.
+class Error : public Object {
+public:
+    // Throw the C++ exception. Needed when re-raising the exception.
+    [[ noreturn ]] virtual void __throw() = 0;
+};
+
+class __Error : public std::exception {
+public:
+    std::shared_ptr<Error> m_error;
+    __Error(const std::shared_ptr<Error>& error) : m_error(error)
+    {
+    }
+};
+
+}
