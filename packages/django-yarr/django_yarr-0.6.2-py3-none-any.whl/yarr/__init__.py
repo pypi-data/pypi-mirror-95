86730809b75191879edb445ba89a4074500622f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:55 +0000

    Add package django-yarr/0.6.2/django_yarr-0.6.2-py3-none-any.whl

---
+++ packages/django-yarr/django_yarr-0.6.2-py3-none-any.whl/yarr/__init__.py
@@ -0,0 +1,17 @@
+"""
+Django Yarr - Yet Another RSS Reader
+"""
+
+__version__ = "0.6.2"
+__license__ = "BSD"
+__author__ = "Richard Terry"
+__credits__ = [
+    "Andrew Rowson (growse)",
+    "Aleksandr Pasechnik (russkey)",
+    "Spencer Herzberg (sherzberg)",
+    "Tom Most (twm)",
+    "Tuk Bredsdorff (tiktuk)",
+    "Chris Franklin (chrisfranklin)",
+    "windedge",
+    "xujl",
+]
---
+++ packages/django-yarr/django_yarr-0.6.2-py3-none-any.whl/yarr/constants.py
@@ -0,0 +1,18 @@
+"""
+Yarr constants
+
+Also defined in JavaScript in utils/constants.js
+"""
+
+ENTRY_UNREAD = 0
+ENTRY_READ = 1
+ENTRY_SAVED = 2
+
+ORDER_DESC = ""
+ORDER_ASC = "asc"
+
+SIDEBAR_DEFAULT = ""
+SIDEBAR_OVERRIDE = "override"
+
+LAYOUT_ARTICLE = ""
+LAYOUT_LIST = "list"
