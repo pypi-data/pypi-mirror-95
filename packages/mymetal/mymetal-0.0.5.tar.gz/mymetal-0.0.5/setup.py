ef4a56e8fcfc1b79b30a846f68e5d59eabaf9310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:38 +0000

    Add package mymetal/0.0.5/mymetal-0.0.5.tar.gz

---
+++ packages/mymetal/mymetal-0.0.5.tar.gz/mymetal-0.0.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5' 
+DESCRIPTION = 'MeBiPred'
+LONG_DESCRIPTION = 'Metal Binding Predictor, using neural network'
+REQUIREMENTS = [line.strip('\n') for line in open('requirements.txt','r')]
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="mymetal", 
+        version=VERSION,
+        author="Ariel Aptekmann",
+        author_email="<arielaptekmann@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=REQUIREMENTS, # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'metal bingind prediction package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ],
+        include_package_data=True,
+        package_data={'': ['aa.csv','ModelPersistency/*','kmer_counts/*']}
+)
