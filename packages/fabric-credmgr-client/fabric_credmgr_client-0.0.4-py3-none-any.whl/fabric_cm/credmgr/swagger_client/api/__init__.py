3084585e5341ec2dd4553da8ed9fbb1a1842d03b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:38 +0000

    Add package fabric-credmgr-client/0.0.4/fabric_credmgr_client-0.0.4-py3-none-any.whl

---
+++ packages/fabric-credmgr-client/fabric_credmgr_client-0.0.4-py3-none-any.whl/fabric_cm/credmgr/swagger_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from fabric_cm.credmgr.swagger_client.api.default_api import DefaultApi
+from fabric_cm.credmgr.swagger_client.api.tokens_api import TokensApi
