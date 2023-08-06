525ba32c24fe34822836725d737b5408191f807d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:13 +0000

    Add package lemon-framework/1.3.2/lemon-framework-1.3.2.tar.gz

---
+++ packages/lemon-framework/lemon-framework-1.3.2.tar.gz/lemon-framework-1.3.2/setup.py
@@ -0,0 +1,37 @@
+import sys 
+import zipfile 
+import os
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file) , os.path.relpath(os.path.join(root, file), path) )
+  
+
+name, version = ("lemon-framework", "1.3.2")
+
+
+if len(sys.argv) <= 2 and sys.argv[1] == "dev_build":
+	zipf = zipfile.ZipFile('lemon/default.zip', 'w', zipfile.ZIP_DEFLATED)
+	zipdir('lemon/default', zipf)
+	zipf.close()
+
+else:
+	import re
+
+	from setuptools import setup, find_packages
+
+	setup(
+		name=name,
+		version=version,
+		author='Joel Schofield',
+		author_email='none@none.none',
+		url='https://github.com/InsaneMiner/Lemon',
+		license='LICENSE',
+		description='Web framework written in python',
+		long_description=open('README.md').read(),
+		packages=["lemon","lemon.libs"],
+		package_data={'': ['default.zip']},
+		include_package_data=True,
+		install_requires = ["urllib3","regex","asyncio","watchdog"],
+	)
