cf368cb8ac2c336bbb6c6aebc5094a287c3f48e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:56 +0000

    Add package pyseparator/0.12/pyseparator-0.12.tar.gz

---
+++ packages/pyseparator/pyseparator-0.12.tar.gz/pyseparator-0.12/pyseparator/__init__.py
@@ -0,0 +1,9 @@
+class pyseparator(object):
+    def __init__(self):
+        pass
+
+    def star(self, num=30):
+        print('*' * num)
+
+    def line(self, num=30):
+        print('-'*num)
---
+++ packages/pyseparator/pyseparator-0.12.tar.gz/pyseparator-0.12/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='pyseparator',
+      version='0.12',
+      description='pyseparator',
+      author='lijun',
+      license='MIT',
+      author_email='asdfzxcvqwer123a@126.com',
+      packages=['pyseparator'],
+      zip_safe=False)
