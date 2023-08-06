7219e00407a3b50506b38cc116a8bc495001fb77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:06 +0000

    Add package mlsauce/0.8.1/mlsauce-0.8.1.tar.gz

---
+++ packages/mlsauce/mlsauce-0.8.1.tar.gz/mlsauce-0.8.1/mlsauce/booster/_boosterc.pyx
@@ -0,0 +1,374 @@
+# cython: wraparound=False
+# cython: boundscheck=False
+# cython: nonecheck=False
+
+# Authors: T. Moudiki
+#
+# License: BSD 3 Clause Clear
+
+import functools
+import pickle
+import numpy as np
+cimport numpy as np
+cimport cython
+import gc
+
+from ..lasso import LassoRegressor
+from ..ridge import RidgeRegressor
+from cython.parallel cimport prange
+from libc.math cimport log, exp, sqrt
+from numpy.linalg import lstsq
+from numpy.linalg import norm
+from scipy.special import expit
+from tqdm import tqdm
+
+
+# 0 - utils -----
+
+# 0 - 1 data structures & funcs -----
+
+# a tuple of doubles
+cdef struct mydoubletuple:
+    double elt1
+    double elt2
+
+DTYPE_double = np.double
+
+DTYPE_int = np.int
+
+ctypedef np.double_t DTYPE_double_t
+
+
+# 0 - 2 data structures & funcs -----
+
+# dropout
+def dropout_func(x, drop_prob=0, seed=123):
+
+    assert 0 <= drop_prob <= 1
+
+    n, p = x.shape
+
+    if drop_prob == 0:
+        return x
+
+    if drop_prob == 1:
+        return np.zeros_like(x)
+
+    np.random.seed(seed)
+    dropped_indices = np.random.rand(n, p) > drop_prob
+
+    return dropped_indices * x / (1 - drop_prob)
+    
+# one-hot encoder for discrete response
+def one_hot_encode(long int[:] y, 
+                   int n_classes):
+    
+    cdef long int i 
+    cdef long int n_obs = len(y)
+    cdef double[:,::1] res = np.zeros((n_obs, n_classes), dtype=DTYPE_double)        
+
+    for i in range(n_obs):
+        res[i, y[i]] = 1
+
+    return np.asarray(res)
+ 
+# 0 - 3 activation functions ----- 
+
+def relu_activation(x):
+    return np.maximum(x, 0)
+
+def relu6_activation(x):
+    return np.minimum(np.maximum(x, 0), 6)
+
+def sigmoid_activation(x):
+    return 1/(1 + np.exp(-x))
+
+def activation_choice(x):
+  activation_options = {
+                "relu": relu_activation,
+                "relu6": relu6_activation,
+                "tanh": np.tanh,
+                "sigmoid": sigmoid_activation
+            }
+  return activation_options[x]  
+
+
+# 1 - classifier ----- 
+
+# 1 - 1 fit classifier ----- 
+
+def fit_booster_classifier(double[:,::1] X, long int[:] y, 
+                           int n_estimators=100, double learning_rate=0.1, 
+                           int n_hidden_features=5, double reg_lambda=0.1, 
+                           double row_sample=1, double col_sample=1,
+                           double dropout=0, double tolerance=1e-6, 
+                           int direct_link=1, int verbose=1,
+                           int seed=123, str backend="cpu", 
+                           str solver="ridge", 
+                           str activation="relu"
+                           ): 
+  
+  cdef long int n
+  cdef int p, n_cols_h_i, n_cols_hh_i
+  cdef int n_classes
+  cdef Py_ssize_t iter
+  cdef dict res
+  cdef double ym
+  cdef double[:] xm, xsd
+  cdef double[:,::1] Y, X_, E, W_i, h_i, hh_i, hidden_layer_i, hhidden_layer_i
+  cdef double current_error
+  
+  
+  n = X.shape[0]
+  p = X.shape[1]
+  res = {}
+  current_error = 1000000.0
+  
+  xm = np.asarray(X).mean(axis=0)
+  xsd = np.asarray(X).std(axis=0)
+  
+  res['direct_link'] = direct_link
+  res['xm'] = np.asarray(xm)
+  res['xsd'] = np.asarray(xsd)
+  res['n_estimators'] = n_estimators
+  res['learning_rate'] = learning_rate
+  res['W_i'] = {}
+  res['fit_obj_i'] = {} 
+  res['col_index_i'] = {}
+  res['loss'] = []
+  res['activation'] = activation
+  
+  X_ = (np.asarray(X) - xm[None, :])/xsd[None, :]
+  n_classes = len(np.unique(y))
+  res['n_classes'] = n_classes
+  
+  Y = one_hot_encode(y, n_classes)
+  Ym = np.mean(Y, axis=0)
+  res['Ym'] = Ym
+  E = Y - Ym
+  iterator = tqdm(range(n_estimators)) if verbose else range(n_estimators)
+
+  solvers = {"ridge": RidgeRegressor(reg_lambda = reg_lambda, backend = backend),
+             "lasso": LassoRegressor(reg_lambda = reg_lambda, backend = backend)}
+  fit_obj = solvers[solver]
+
+  for iter in iterator:
+      
+      np.random.seed(seed + iter*1000)
+    
+      iy = np.sort(np.random.choice(a=range(p), 
+                                    size=np.int(p*col_sample), 
+                                    replace=False), 
+                   kind='quicksort')
+      res['col_index_i'][iter] = iy                     
+      X_iy = np.asarray(X_)[:, iy] # must be X_!
+      W_i = np.random.rand(X_iy.shape[1], n_hidden_features)
+      hhidden_layer_i = dropout_func(x=activation_choice(activation)(np.dot(X_iy, W_i)), 
+                                     drop_prob=dropout, seed=seed)
+      hh_i = np.hstack((X_iy, hhidden_layer_i)) if direct_link else hhidden_layer_i
+      
+      if row_sample < 1:
+      
+        ix = np.sort(np.random.choice(a=range(n), 
+                                    size=np.int(n*row_sample), 
+                                    replace=False), 
+                     kind='quicksort')
+        X_iy_ix = X_iy[ix,:]       
+        hidden_layer_i = dropout_func(x=activation_choice(activation)(np.dot(X_iy_ix, W_i)), 
+                                      drop_prob=dropout, seed=seed)
+        h_i =  np.hstack((X_iy_ix, hidden_layer_i)) if direct_link else hidden_layer_i
+        fit_obj.fit(X = np.asarray(h_i), y = np.asarray(E)[ix,:])
+                                 
+      else:
+      
+        fit_obj.fit(X = np.asarray(hh_i), y = np.asarray(E))
+            
+      E = E - learning_rate*np.asarray(fit_obj.predict(np.asarray(hh_i)))
+      
+      res['W_i'][iter] = np.asarray(W_i)
+            
+      res['fit_obj_i'][iter] = pickle.loads(pickle.dumps(fit_obj, -1))
+
+      current_error = np.linalg.norm(E, ord='fro')
+
+      res['loss'].append(current_error)
+      
+      try:
+        if np.abs(np.flip(np.diff(res['loss'])))[0] <= tolerance:
+          res['n_estimators'] = iter
+          break
+      except:
+        pass
+      
+  return res
+  
+  
+# 1 - 2 predict classifier ----- 
+
+def predict_proba_booster_classifier(object obj, double[:,::1] X):
+
+  cdef int iter, n_estimators, n_classes
+  cdef double learning_rate
+  cdef double[:,::1] preds_sum, out_probs
+  cdef long int n_row_preds 
+  
+
+  n_classes = obj['n_classes']
+  direct_link = obj['direct_link']
+  n_estimators = obj['n_estimators']
+  learning_rate = obj['learning_rate']
+  activation = obj['activation']
+  X_ = (X - obj['xm'][None, :])/obj['xsd'][None, :]
+  n_row_preds = X.shape[0]
+  preds_sum = np.zeros((n_row_preds, n_classes))
+  out_probs = np.zeros((n_row_preds, n_classes))
+  
+  
+  for iter in range(n_estimators):
+  
+    iy = obj['col_index_i'][iter]
+    X_iy = X_[:, iy] # must be X_!
+    W_i = obj['W_i'][iter]
+    hh_i = np.hstack((X_iy, activation_choice(activation)(np.dot(X_iy, W_i)))) if direct_link else activation_choice(activation)(np.dot(X_iy, W_i))
+    preds_sum = preds_sum + learning_rate*np.asarray(obj['fit_obj_i'][iter].predict(np.asarray(hh_i)))
+  
+  out_probs = expit(np.tile(obj['Ym'], n_row_preds).reshape(n_row_preds, n_classes) + np.asarray(preds_sum))
+  
+  out_probs = out_probs/np.sum(out_probs, axis=1)[:, None]
+
+  return np.asarray(out_probs)
+  
+  
+# 2 - regressor -----   
+
+# 2 - 1 fit regressor -----
+  
+def fit_booster_regressor(double[:,::1] X, double[:] y, 
+                           int n_estimators=100, double learning_rate=0.1, 
+                           int n_hidden_features=5, double reg_lambda=0.1, 
+                           double row_sample=1, double col_sample=1,
+                           double dropout=0, double tolerance=1e-6, 
+                           int direct_link=1, int verbose=1, 
+                           int seed=123, str backend="cpu", 
+                           str solver="ridge", str activation="relu"): 
+  
+  cdef long int n
+  cdef int p, n_cols_h_i, n_cols_hh_i
+  cdef int n_classes
+  cdef Py_ssize_t iter
+  cdef dict res
+  cdef double ym
+  cdef double[:] xm, xsd, e
+  cdef double[:,::1] X_, W_i, h_i, hh_i, hidden_layer_i, hhidden_layer_i
+  cdef double current_error
+  
+  
+  n = X.shape[0]
+  p = X.shape[1]
+  res = {}
+  current_error = 1000000.0
+  
+  xm = np.asarray(X).mean(axis=0)
+  xsd = np.asarray(X).std(axis=0)
+  
+  res['direct_link'] = direct_link
+  res['xm'] = np.asarray(xm)
+  res['xsd'] = np.asarray(xsd)
+  res['n_estimators'] = n_estimators
+  res['learning_rate'] = learning_rate
+  res['activation'] = activation
+  res['W_i'] = {}
+  res['fit_obj_i'] = {} 
+  res['col_index_i'] = {}
+  res['loss'] = []
+  
+  X_ = (np.asarray(X) - xm[None, :])/xsd[None, :]
+  n_classes = len(np.unique(y))
+  res['n_classes'] = n_classes
+  
+  ym = np.mean(y)
+  res['ym'] = ym
+  e = y - np.repeat(ym, n)
+  iterator = tqdm(range(n_estimators)) if verbose else range(n_estimators)
+
+  solvers = {"ridge": RidgeRegressor(reg_lambda = reg_lambda, backend = backend),
+             "lasso": LassoRegressor(reg_lambda = reg_lambda, backend = backend)}
+  fit_obj = solvers[solver]
+
+  for iter in iterator:
+      
+      np.random.seed(seed + iter*1000)
+    
+      iy = np.sort(np.random.choice(a=range(p), 
+                                    size=np.int(p*col_sample), 
+                                    replace=False), 
+                   kind='quicksort')
+      res['col_index_i'][iter] = iy                     
+      X_iy = np.asarray(X_)[:, iy] # must be X_!
+      W_i = np.random.rand(X_iy.shape[1], n_hidden_features)
+      hhidden_layer_i = dropout_func(x=activation_choice(activation)(np.dot(X_iy, W_i)), 
+                                     drop_prob=dropout, seed=seed)
+      hh_i = np.hstack((X_iy, hhidden_layer_i)) if direct_link else hhidden_layer_i
+      
+      if row_sample < 1:
+      
+        ix = np.sort(np.random.choice(a=range(n), 
+                                    size=np.int(n*row_sample), 
+                                    replace=False), 
+                     kind='quicksort')
+        X_iy_ix = X_iy[ix,:]       
+        hidden_layer_i = dropout_func(x=activation_choice(activation)(np.dot(X_iy_ix, W_i)), 
+                                      drop_prob=dropout, seed=seed)
+        h_i =  np.hstack((X_iy_ix, hidden_layer_i)) if direct_link else hidden_layer_i
+        n_cols_h_i = h_i.shape[1]
+        fit_obj.fit(X = np.asarray(h_i), y = np.asarray(e)[ix,:])
+
+      else:
+      
+        fit_obj.fit(X = np.asarray(hh_i), y = np.asarray(e))
+            
+      e = e - learning_rate*np.asarray(fit_obj.predict(np.asarray(hh_i)))
+
+      res['W_i'][iter] = np.asarray(W_i)
+      
+      res['fit_obj_i'][iter] = pickle.loads(pickle.dumps(fit_obj, -1))
+
+      current_error = np.linalg.norm(e)
+
+      res['loss'].append(current_error)      
+
+      try:              
+        if np.abs(np.flip(np.diff(res['loss'])))[0] <= tolerance:
+          res['n_estimators'] = iter
+          break      
+      except:
+        pass
+      
+  return res
+  
+# 2 - 2 predict regressor -----
+
+def predict_booster_regressor(object obj, double[:,::1] X):
+
+  cdef int iter, n_estimators, n_classes
+  cdef double learning_rate
+  cdef double[:] preds_sum
+  cdef double[:,::1] hh_i
+
+  n_classes = obj['n_classes']
+  direct_link = obj['direct_link']
+  n_estimators = obj['n_estimators']
+  learning_rate = obj['learning_rate']
+  activation = obj['activation']
+  X_ = (X - obj['xm'][None, :])/obj['xsd'][None, :]
+  preds_sum = np.zeros(X.shape[0])
+  
+  for iter in range(n_estimators):
+  
+    iy = obj['col_index_i'][iter]
+    X_iy = X_[:, iy] # must be X_!
+    W_i = obj['W_i'][iter]
+    hh_i = np.hstack((X_iy, activation_choice(activation)(np.dot(X_iy, W_i)))) if direct_link else activation_choice(activation)(np.dot(X_iy, W_i))    
+    preds_sum = preds_sum + learning_rate*np.asarray(obj['fit_obj_i'][iter].predict(np.asarray(hh_i)))
+  
+  return np.asarray(obj['ym'] + np.asarray(preds_sum))
---
+++ packages/mlsauce/mlsauce-0.8.1.tar.gz/mlsauce-0.8.1/mlsauce/stump/_stumpc.pyx
@@ -0,0 +1,296 @@
+# cython: wraparound=False
+# cython: boundscheck=False
+# cython: nonecheck=False
+
+# Authors: T. Moudiki
+#
+# License: BSD 3 Clause Clear
+
+import functools
+import numpy as np
+cimport numpy as np
+cimport cython
+import gc
+import operator
+import warnings
+import pickle
+
+from collections import Counter
+from cython.parallel cimport prange
+from libc.math cimport log, exp, sqrt, fabs
+from numpy.linalg import lstsq
+from numpy.linalg import norm
+from scipy.special import expit
+
+
+
+# 0 - utils -----
+
+warnings.filterwarnings("ignore")
+
+# returns min(x, y)
+cdef double min_c(double x, double y):
+    if (x < y):
+        return x 
+    return y
+
+# returns max(x, y)
+cdef double max_c(double x, double y):
+    if (x > y):
+        return x 
+    return y
+
+
+# 0 - 1 data structures & funcs -----
+
+# a tuple of doubles
+cdef struct mydoubletuple:
+    double elt1
+    double elt2
+
+ctypedef fused nparray_int:
+    int
+    
+ctypedef fused nparray_double:    
+    double
+    
+ctypedef fused nparray_long:
+    long long
+
+
+# 0 - 2 data structures & funcs -----
+
+# one-hot encoder for discrete response
+def one_hot_encode(long int[:] y, 
+                   int n_classes):
+    
+    cdef long int i 
+    cdef long int n_obs = len(y)
+    cdef double[:,::1] res = np.zeros((n_obs, n_classes))        
+
+    for i in range(n_obs):
+        res[i, y[i]] = 1
+
+    return np.asarray(res)
+
+# average stuff
+cdef double cython_average(double[:] x, double[:] weights):
+    
+    cdef long int i, n 
+    cdef double sum_x    
+    cdef double[:] sum_out 
+    
+    n = len(x)
+    sum_x = 0 
+
+    for i in range(n):        
+      sum_x += x[i]*weights[i]        
+    
+    return sum_x
+
+# obtain histogram bins        
+def histogram_bins(double[:] x, long int n_bins = -1):
+    
+    cdef long int i, n    
+    cdef long int n_classes, n_bins_1 
+    cdef double freq     
+    cdef double[:] res
+    cdef double min_x, max_x
+
+    n = len(x)    
+
+    if n_bins == -1:
+      n_bins = int(sqrt(n))    
+
+    min_x = np.min(x)    
+    max_x = np.max(x)
+
+    freq = (max_x - min_x)/n_bins
+    
+    n_bins_1 = n_bins + 1
+
+    res = np.zeros(n_bins_1)
+
+    for i in range(n_bins_1):
+
+      res[i] = min_x + i*freq
+                                
+    return np.asarray(res)
+
+   
+# 1 - fitting -----    
+
+
+def fit_stump_classifier(double[:,::1] X, long int[:] y, 
+                         double[:] sample_weight=None, 
+                         bins="auto"):
+
+  cdef long int n
+  cdef long int n_up 
+  cdef long int n_down 
+  cdef long int n_preds
+  cdef int n_bins 
+  cdef int p
+  cdef long int i
+  cdef int j, k
+  cdef int best_col
+  cdef double[:] cutpoints
+  cdef double cutpoint_i
+  cdef double best_cutpoint
+  cdef long int n_cutpoints
+  cdef double error_rate, error_rate_cur, error_rate_down, error_rate_up
+  cdef int best_class, class_up, n_classes
+  cdef dict res_class
+  cdef double[:] equal_weights
+
+
+  n = X.shape[0]
+  p = X.shape[1]
+  n_classes = len(np.unique(y))
+  res_class = {}
+  error_rate = 10000.0
+  error_rate_cur = 10000.0
+  best_cutpoint = 0.0
+  i = 0
+  j = 0
+  k = 0
+  if bins == "auto":
+    n_bins = -1
+  else: 
+    n_bins = bins  
+  n_preds = len(y)  
+  equal_weights = np.repeat(1/n_preds, n_preds)
+  
+  X_ = np.asarray(X).T.tolist()
+  
+  if n_classes <= 2:    
+  
+    for j in range(p):
+    
+      X_j = np.asarray(X_[j])
+      
+      cutpoints = histogram_bins(X_j, n_bins) 
+
+      n_cutpoints = len(cutpoints)
+      
+      for i in range(n_cutpoints):
+               
+        cutpoint_i = cutpoints[i] 
+        index_up = (X_j <= cutpoint_i)
+        y_up = np.asarray(y)[index_up]        
+        class_up = Counter(y_up.tolist()).most_common(1)[0][0] # majority vote # majority vote
+
+        preds = class_up*index_up + (1 - class_up)*np.logical_not(index_up)                
+        
+        error_rate_cur = cython_average((preds != y)*1.0, equal_weights) if sample_weight is None else cython_average((preds != y)*1.0, sample_weight)           
+        
+        if error_rate_cur <= error_rate:
+          # print(error_rate_cur)
+          best_col = j
+          best_cutpoint = cutpoint_i
+          error_rate = error_rate_cur
+          best_class = class_up
+         
+    return best_col, best_cutpoint, best_class
+
+# if n_classes > 2:
+
+  Y = one_hot_encode(y, n_classes)
+  Y_ = Y.T.tolist()
+  
+  for k in range(n_classes):
+  
+    y_ = np.asarray(Y_[k])
+    best_col = 0
+    best_cutpoint = 0.0
+    best_class = 0
+    error_rate = 10000.0
+    error_rate_cur = 10000.0
+    best_cutpoint = 0.0
+  
+    for j in range(p):
+    
+      X_j = np.asarray(X_[j])
+           
+      cutpoints = histogram_bins(X_j, n_bins) 
+
+      n_cutpoints = len(cutpoints)      
+ 
+      for i in range(n_cutpoints):
+        
+        try:
+        
+          cutpoint_i = cutpoints[i] 
+          index_up = (X_j <= cutpoint_i)
+          y_up = y_[index_up]          
+          class_up = Counter(y_up.tolist()).most_common(1)[0][0] # majority vote
+  
+          preds = class_up*index_up + (1 - class_up)*np.logical_not(index_up)
+                     
+          error_rate_cur = cython_average((preds != y_)*1.0, equal_weights) if sample_weight is None else cython_average((preds != y_)*1.0, sample_weight)                     
+          
+          if error_rate_cur <= error_rate:
+            # print(error_rate_cur)
+            best_col = j
+            best_cutpoint = cutpoint_i
+            error_rate = error_rate_cur
+            best_class = class_up
+  
+        except:
+        
+          pass
+    
+    res_class[k] = (best_col, best_cutpoint, best_class)
+        
+  return res_class
+  
+  
+  
+# 2 - predict -----   
+
+def predict_proba_twoclass(double[:,::1] X, 
+                             int best_col, 
+                             double best_cutpoint, 
+                             int best_class):
+    X_ = np.asarray(X).T.tolist()
+    X_j = np.asarray(X_[best_col])
+    preds = (X_j <= best_cutpoint)*best_class + (X_j > best_cutpoint)*(1 - best_class)
+    return (np.asarray(one_hot_encode(preds, 2)))
+
+def predict_proba_stump_classifier(object obj, double[:,::1] X):
+  
+  cdef long int n_obs
+  cdef int n_classes
+  cdef double[:,:] raw_probs
+  cdef long int[:] temp_prob
+
+
+  # if n_classes <= 2  
+  if (isinstance(obj, dict) == False):
+    return (predict_proba_twoclass(X, obj[0], obj[1], obj[2]))
+    
+    
+  # else: n_classes > 2
+  n_obs = X.shape[0]
+  n_classes = len(obj)
+  raw_probs = np.zeros((n_obs, n_classes))
+  
+  
+  for k in range(n_classes):
+    # print(f"class {k}")
+    # print(predict_proba_twoclass(X, obj[k][0], obj[k][1], obj[k][2]))
+    temp_prob = np.argmax(predict_proba_twoclass(X, obj[k][0], obj[k][1], obj[k][2]), 
+                              axis=1)
+    for i in range(n_obs):
+      raw_probs[i, k] = temp_prob[i]
+  
+  # print("raw_probs")
+  # print(np.asarray(raw_probs))
+  expit_raw_probs = expit(raw_probs)
+  
+  return (expit_raw_probs/expit_raw_probs.sum(axis=1)[:, None])
+  
+  
+  
+    
+  
