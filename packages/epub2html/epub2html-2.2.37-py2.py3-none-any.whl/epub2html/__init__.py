13abb633a63f46c921f8fa13bed05aed63be6220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:35 +0000

    Add package epub2html/2.2.37/epub2html-2.2.37-py2.py3-none-any.whl

---
+++ packages/epub2html/epub2html-2.2.37-py2.py3-none-any.whl/epub2html/__init__.py
@@ -0,0 +1,6 @@
+from .epub2html import (entry_point,
+                     main,
+                     Epub2Html,
+                     createParse)
+
+__all__ = ['entry_point','createParse', 'main','Epub2Html']
