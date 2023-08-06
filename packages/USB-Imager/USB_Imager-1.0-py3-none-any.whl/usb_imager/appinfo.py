afe1ca6a147610f33d5840321308f71e2686ecdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:43 +0000

    Add package USB-Imager/1.0/USB_Imager-1.0-py3-none-any.whl

---
+++ packages/USB-Imager/USB_Imager-1.0-py3-none-any.whl/usb_imager/appinfo.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Project-Info"""
+
+
+__appname__         = "USB-Imager"
+__version__         = "1.0"
+__license__         = "GPLv3"
+__copyright__       = "\u00A9 2021"
+
+__description__     = "GUI-Application to write bootable disk images to USB key."
+__url_source__      = "https://gitlab.com/skynet-devel/usb-imager"
+__url_pip__         = "https://pypi.org/project/USB-Imager"
+
+__author__          = "skynet-devel"
+__author_email__    = "skynet-devel@mailbox.org"
+
+__contributors__    = ["Rico (testing)"]
+
+__organisation__    = "Skynet-Research"
+__domain__          = "usbimager.app.skynet"
+
+__donation_paypal__ = "skynet-devel@mailbox.org"
+
+
+# Developer version informations
+"""
+pathlib.Path.home() >= Python 3.5
+pathlib >= Python 3.4
+importlib.resources >= 3.7  (not used yet)
+
+Accepts a path-like object (Python >= 3.6):
+    open()              (path-like object not used yet)
+    os.open()           (path-like object not used yet)
+    os.path.getsize()   (path-like object not used yet)
+"""
