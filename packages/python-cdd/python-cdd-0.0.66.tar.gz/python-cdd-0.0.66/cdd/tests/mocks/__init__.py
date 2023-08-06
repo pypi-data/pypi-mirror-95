23a2a637fcc326029d5cafd9401f89bb3a112615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:14 +0000

    Add package python-cdd/0.0.66/python-cdd-0.0.66.tar.gz

---
+++ packages/python-cdd/python-cdd-0.0.66.tar.gz/python-cdd-0.0.66/cdd/tests/mocks/__init__.py
@@ -0,0 +1,14 @@
+"""
+Shared by the mocks. Currently unused, but has some imports mocked for later use…
+"""
+
+"""
+try:
+    import tensorflow as tf
+    import numpy as np
+except ImportError:
+    from collections import namedtuple
+
+    tf = namedtuple('TensorFlow', ('data',))(namedtuple('data', ('Dataset',)))
+    np = namedtuple('numpydoc', ('ndarray',))
+"""
