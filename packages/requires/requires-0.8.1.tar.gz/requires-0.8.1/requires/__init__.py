b15ebf82d0cbc427b23984fb0f6a70f2e3777963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:28 +0000

    Add package requires/0.8.1/requires-0.8.1.tar.gz

---
+++ packages/requires/requires-0.8.1.tar.gz/requires-0.8.1/requires/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""`requires` ~ make modules callable"""
+from funkify import funkify
+from requires._meta import __version__
+from requires.core import (
+    Requirement,
+    RequirementError,
+    RequirementProxy,
+    require,
+    requires,
+)
+
+
+funkify(requires, name="requires")
+
+__all__ = [
+    "__version__",
+    "requires",
+    "require",
+    "RequirementError",
+    "Requirement",
+    "RequirementProxy",
+]
