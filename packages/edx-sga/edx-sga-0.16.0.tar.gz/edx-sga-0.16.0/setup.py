ef79ac373a967f982ac0975f5e3f618ee10edd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:48 +0000

    Add package edx-sga/0.16.0/edx-sga-0.16.0.tar.gz

---
+++ packages/edx-sga/edx-sga-0.16.0.tar.gz/edx-sga-0.16.0/setup.py
@@ -0,0 +1,40 @@
+"""Setup for edx-sga XBlock."""
+import os
+
+from setuptools import find_packages, setup
+
+import edx_sga
+
+
+def package_data(pkg, root_list):
+    """Generic function to find package_data for `pkg` under `root`."""
+    data = []
+    for root in root_list:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+setup(
+    name='edx-sga',
+    version=edx_sga.__version__,
+    description='edx-sga Staff Graded Assignment XBlock',
+    license='Affero GNU General Public License v3 (GPLv3)',
+    url="https://github.com/mitodl/edx-sga",
+    author="MITx",
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'XBlock',
+        'xblock-utils',
+        'web_fragments',
+    ],
+    entry_points={
+        'xblock.v1': [
+            'edx_sga = edx_sga.sga:StaffGradedAssignmentXBlock',
+        ]
+    },
+    package_data=package_data("edx_sga", ["static", "templates"]),
+)
