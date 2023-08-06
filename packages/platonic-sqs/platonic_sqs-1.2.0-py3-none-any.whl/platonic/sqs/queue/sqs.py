53776da891bf2d53e2dc9bc1fed1472258aaf93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:20 +0000

    Add package platonic-sqs/1.2.0/platonic_sqs-1.2.0-py3-none-any.whl

---
+++ packages/platonic-sqs/platonic_sqs-1.2.0-py3-none-any.whl/platonic/sqs/queue/sqs.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, field
+from functools import partial
+
+import boto3
+from mypy_boto3_sqs import Client as SQSClient
+from typecasts import Typecasts, casts
+
+from platonic.const import const
+
+# Max number of SQS messages receivable by single API call.
+MAX_NUMBER_OF_MESSAGES = 10
+
+# Message in its raw form must be shorter than this.
+MAX_MESSAGE_SIZE = 262144
+
+# Max long polling time
+MAX_WAIT_TIME_SECONDS = 20
+
+
+@dataclass
+class SQSMixin:
+    """Common fields for SQS queue classes."""
+
+    url: str
+    typecasts: Typecasts = field(default_factory=const(casts))
+    internal_type: type = field(default=str)
+    client: SQSClient = field(default_factory=partial(boto3.client, 'sqs'))
