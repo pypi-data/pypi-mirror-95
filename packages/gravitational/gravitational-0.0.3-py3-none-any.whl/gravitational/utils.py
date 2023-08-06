d4e21142cdb476ad56c24a1da8571d53ba1fc7f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:44 +0000

    Add package gravitational/0.0.3/gravitational-0.0.3-py3-none-any.whl

---
+++ packages/gravitational/gravitational-0.0.3-py3-none-any.whl/gravitational/utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def magnitude(x):
+    return np.linalg.norm(np.array(x))
+
+def unit_vector(x):
+    return tuple(np.array(x)/magnitude(x))
+
+def random_normal_sphere(center, radius, size, std=1, seed=123):
+    '''creates xyz coordiantes in a sphere'''
+    np.random.seed(seed)
+    x0, y0, z0 = center
+    x = np.random.normal(loc=x0, scale=std, size=size)
+    y = np.random.normal(loc=y0, scale=std, size=size)
+    z = np.random.normal(loc=z0, scale=std, size=size)
+    mag = np.sqrt((x-x0)**2 + (y-y0)**2 + (z-z0)**2)
+    xs = x[np.where(mag<=radius)]
+    ys = y[np.where(mag<=radius)]
+    zs = z[np.where(mag<=radius)]
+    return list(zip(xs,ys,zs))
+
+def random_uniform_sphere(center, radius, size, seed=123):
+    '''creates xyz coordiantes in a sphere'''
+    np.random.seed(seed)
+    x0, y0, z0 = center
+    x = np.random.uniform(low=x0, high=x0+radius, size=size)
+    y = np.random.uniform(low=x0, high=x0+radius, size=size)
+    z = np.random.uniform(low=x0, high=x0+radius, size=size)
+    mag = np.sqrt((x-x0)**2 + (y-y0)**2 + (z-z0)**2)
+    xs = x[np.where(mag<=radius)]
+    ys = y[np.where(mag<=radius)]
+    zs = z[np.where(mag<=radius)]
+    return list(zip(xs,ys,zs))
