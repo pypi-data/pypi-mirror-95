d5475d6a6ea4d8b4dc49e7be89c71ea2c48b3c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:20 +0000

    Add package example-pkg-insistgang/0.0.1/example-pkg-insistgang-0.0.1.tar.gz

---
+++ packages/example-pkg-insistgang/example-pkg-insistgang-0.0.1.tar.gz/example-pkg-insistgang-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-insistgang", # Replace with your own username
+    version="0.0.1",
+    author="insistgang",
+    author_email="insistgang@163.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
