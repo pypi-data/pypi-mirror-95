87b08359e56f9f2a9c189cb4103257252ff6a5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package flake8-pytest-importorskip/1.0.0/flake8-pytest-importorskip-1.0.0.tar.gz

---
+++ packages/flake8-pytest-importorskip/flake8-pytest-importorskip-1.0.0.tar.gz/flake8-pytest-importorskip-1.0.0/flake8_pytest_importorskip/__init__.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict
+
+__version__ = '1.0.0'
+
+
+def patch_pytest_importorskip(
+    logical_line: str,
+    previous_logical: str,
+    _checker_states: Dict[str, Any],
+) -> Any:
+    def _interesting_line(line: str) -> bool:
+        # This misses `from pytest import importorskip`. PR welcome
+        return "pytest.importorskip(" in line
+
+    if "pycodestyle.module_imports_on_top_of_file" not in _checker_states:
+        return ()
+
+    if _interesting_line(logical_line) or _interesting_line(previous_logical):
+        _checker_states["pycodestyle.module_imports_on_top_of_file"][
+            "seen_non_imports"
+        ] = False
+
+    return ()
+
+
+patch_pytest_importorskip.name = "PytestImportSkip"  # type: ignore
+patch_pytest_importorskip.version = __version__  # type: ignore
