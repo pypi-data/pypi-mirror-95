42a33ae5746e765721f4f4a8b53dece74b3ccf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:09 +0000

    Add package librelingo-types/0.1.1/librelingo_types-0.1.1-py3-none-any.whl

---
+++ packages/librelingo-types/librelingo_types-0.1.1-py3-none-any.whl/librelingo_types/data_types.py
@@ -0,0 +1,52 @@
+from collections import namedtuple
+
+Language = namedtuple("Language", [
+    "name",
+    "code",
+])
+
+Course = namedtuple("Course", [
+    "target_language",
+    "source_language",
+    "special_characters",
+    "modules",
+    "license",
+    "dictionary"
+])
+
+License = namedtuple("License", [
+    "name",
+    "full_name",
+    "link",
+])
+
+Module = namedtuple("Module", [
+    "title",
+    "skills",
+])
+
+Skill = namedtuple("Skill", [
+    "name",
+    "id",
+    "words",
+    "phrases",
+    "image_set",
+    "dictionary",
+])
+
+Word = namedtuple("Word", [
+    "in_target_language",
+    "in_source_language",
+    "pictures",
+])
+
+Phrase = namedtuple("Phrase", [
+    "in_target_language",
+    "in_source_language",
+])
+
+DictionaryItem = namedtuple("DictionaryItem", [
+    "word",
+    "definition",
+    "is_in_target_language"
+])
