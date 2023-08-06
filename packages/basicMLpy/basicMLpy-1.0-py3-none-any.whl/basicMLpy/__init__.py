d3cf36d62a8e2c3c94864d6826c99a4fbf9698af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:44 +0000

    Add package basicMLpy/1.0/basicMLpy-1.0-py3-none-any.whl

---
+++ packages/basicMLpy/basicMLpy-1.0-py3-none-any.whl/basicMLpy/__init__.py
@@ -0,0 +1,16 @@
+"""
+basicMLpy 1.0
+=================
+
+
+Provides
+    1. Simple and effective machine learning algorithms for classification and regression
+    2. Easy-to-use functions that allow the user to expand on the package's capabilities
+    3. A Cross-Validation algorithm for the functions presented by the package
+=================
+
+
+For more information on the package, please visit my Github page or the package page at pypi
+Thanks for using the package and feel free to contact me for suggestions! 
+
+"""
