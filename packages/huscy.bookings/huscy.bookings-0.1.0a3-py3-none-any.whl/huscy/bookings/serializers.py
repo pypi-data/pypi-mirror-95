08a38b43f702bb4b24d1d4c52437aa9d475c1f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:25 +0000

    Add package huscy.bookings/0.1.0a3/huscy.bookings-0.1.0a3-py3-none-any.whl

---
+++ packages/huscy.bookings/huscy.bookings-0.1.0a3-py3-none-any.whl/huscy/bookings/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from huscy.bookings import models, services
+
+
+class TimeslotSerializer(serializers.ModelSerializer):
+    active = serializers.ReadOnlyField()
+
+    class Meta:
+        model = models.Timeslot
+        fields = (
+            'active',
+            'id',
+            'session',
+            'start',
+            'end',
+        )
+
+    def create(self, validated_data):
+        return services.add_timeslot(**validated_data)
+
+    def update(self, instance, validated_data):
+        try:
+            return services.move_timeslot(instance, validated_data['start'])
+        except services.BookingExistsException as e:
+            raise ValidationError(e)
