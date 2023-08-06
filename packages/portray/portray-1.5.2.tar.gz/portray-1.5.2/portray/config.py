efe42578d1f1f7dc5c23145895f82252999954cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:13 +0000

    Add package portray/1.5.2/portray-1.5.2.tar.gz

---
+++ packages/portray/portray-1.5.2.tar.gz/portray-1.5.2/portray/config.py
@@ -0,0 +1,226 @@
+"""Defines the configuration defaults and load functions used by `portray`"""
+import ast
+import os
+import re
+import warnings
+from typing import Any, Dict, List, Optional, Union, cast
+from urllib import parse
+
+import mkdocs.config as _mkdocs_config
+import mkdocs.exceptions as _mkdocs_exceptions
+from git import Repo
+from toml import load as toml_load
+
+import _ast
+from portray.exceptions import NoProjectFound
+
+PORTRAY_DEFAULTS = {
+    "docs_dir": "docs",
+    "extra_dirs": ["art", "images", "media"],
+    "output_dir": "site",
+    "port": 8000,
+    "host": "127.0.0.1",
+    "append_directory_to_python_path": True,
+    "include_reference_documentation": True,
+    "labels": {"Cli": "CLI", "Api": "API", "Http": "HTTP", "Pypi": "PyPI"},
+    "extra_markdown_extensions": [],
+}
+
+MKDOCS_DEFAULTS: Dict[str, Any] = {
+    "site_name": os.path.basename(os.getcwd()),
+    "config_file_path": os.getcwd(),
+    "theme": {
+        "name": "material",
+        "palette": {"primary": "green", "accent": "lightgreen"},
+        "custom_dir": os.path.join(os.path.dirname(__file__), "mkdocs_templates"),
+    },
+    "markdown_extensions": [
+        "admonition",
+        "codehilite",
+        "extra",
+        "pymdownx.details",
+        "pymdownx.highlight",
+    ],
+}
+
+PDOCS_DEFAULTS: Dict = {"overwrite": True, "exclude_source": False}
+
+
+def project(directory: str, config_file: str, **overrides) -> dict:
+    """Returns back the complete configuration - including all sub configuration components
+       defined below that `portray` was able to determine for the project
+    """
+    if not (
+        os.path.isfile(os.path.join(directory, config_file))
+        or os.path.isfile(os.path.join(directory, "setup.py"))
+        or "modules" in overrides
+    ):
+        raise NoProjectFound(directory)
+
+    project_config: Dict[str, Any] = {**PORTRAY_DEFAULTS, "directory": directory}
+    if os.path.isfile(os.path.join(directory, "setup.py")):
+        project_config.update(setup_py(os.path.join(directory, "setup.py")))
+
+    project_config.update(toml(os.path.join(directory, config_file)))
+    project_config.update(overrides)
+
+    project_config.setdefault("modules", [os.path.basename(os.getcwd()).replace("-", "_")])
+    project_config.setdefault("pdocs", {}).setdefault("modules", project_config["modules"])
+
+    mkdocs_config = project_config.get("mkdocs", {})
+    mkdocs_config.setdefault(
+        "extra_markdown_extensions", project_config.get("extra_markdown_extensions", [])
+    )
+    project_config["mkdocs"] = mkdocs(directory, **mkdocs_config)
+    if "pdoc3" in project_config:
+        warnings.warn(
+            "pdoc3 config usage is deprecated in favor of pdocs. "
+            "pdoc3 section will be ignored. ",
+            DeprecationWarning,
+        )
+    project_config["pdocs"] = pdocs(directory, **project_config.get("pdocs", {}))
+    return project_config
+
+
+def setup_py(location: str) -> dict:
+    """Returns back any configuration info we are able to determine from a setup.py file"""
+    setup_config = {}
+    try:
+        with open(location) as setup_py_file:
+            for node in ast.walk(ast.parse(setup_py_file.read())):
+                if (
+                    type(node) == _ast.Call
+                    and type(getattr(node, "func", None)) == _ast.Name
+                    and node.func.id == "setup"  # type: ignore
+                ):
+                    for keyword in node.keywords:  # type: ignore
+                        if keyword.arg == "packages":
+                            setup_config["modules"] = ast.literal_eval(keyword.value)
+                            break
+                    break
+    except Exception as error:
+        warnings.warn(f"Error ({error}) occurred trying to parse setup.py file: {location}")
+
+    return setup_config
+
+
+def toml(location: str) -> dict:
+    """Returns back the configuration found within the projects
+       [TOML](https://github.com/toml-lang/toml#toml) config (if there is one).
+
+       Generally this is a `pyproject.toml` file at the root of the project
+       with a `[tool.portray]` section defined.
+    """
+    try:
+        location_exists = os.path.exists(location)
+        if not location_exists:
+            warnings.warn(f'\nNo config file found at location: "{location}"')
+            return {}
+    except Exception as detection_error:
+        warnings.warn(f'\nUnable to check config at "{location}" due to error: {detection_error}')
+
+    try:
+        toml_config = toml_load(location)
+        tools = toml_config.get("tool", {})
+
+        config = tools.get("portray", {})
+        config["file"] = location
+
+        if "modules" not in config:
+            if "poetry" in tools and "name" in tools["poetry"]:
+                config["modules"] = [tools["poetry"]["name"]]
+            elif (
+                "flit" in tools
+                and "metadata" in tools["flit"]
+                and "module" in tools["flit"]["metadata"]
+            ):
+                config["modules"] = [tools["flit"]["metadata"]["module"]]
+
+        return config
+    except Exception as load_config_error:
+        warnings.warn(f'\nConfig file at "{location}" has errors: {load_config_error}')
+
+    return {}
+
+
+def repository(
+    directory: str,
+    repo_url: Optional[str] = None,
+    repo_name: Optional[str] = None,
+    edit_uri: Optional[str] = None,
+    normalize_repo_url: bool = True,
+    **kwargs,
+) -> Dict[str, Optional[str]]:
+    """Returns back any information that can be determined by introspecting the projects git repo
+       (if there is one).
+    """
+    try:
+        if repo_url is None:
+            repo_url = Repo(directory).remotes.origin.url
+        if repo_name is None:
+            match = re.search(r"(:(//)?)([\w\.@\:/\-~]+)(\.git)?(/)?", repo_url)
+            if match:
+                path = match.groups()[2]
+            else:
+                path = repo_url
+            repo_name = path.split("/")[-1]
+            if repo_name.endswith(".git"):
+                repo_name = repo_name[: -len(".git")]
+        if edit_uri is None:
+            if "github" in repo_url or "gitlab" in repo_url:
+                edit_uri = "edit/master/"
+            elif "bitbucket" in repo_url:
+                edit_uri = "src/default/docs/"
+
+        if normalize_repo_url:
+            if repo_url.startswith("git@") and ":" in repo_url:
+                tld, path = repo_url[4:].split(":")
+                repo_url = f"https://{tld}/{path}"
+            elif repo_url.startswith("https://") and "@" in repo_url:
+                repo_url = f"https://{repo_url.split('@')[1]}"
+
+            if repo_url and "github" in repo_url or "gitlab" in repo_url or "bitbucket" in repo_url:
+                repo_url = repo_url.replace(".git", "")
+
+        return {
+            key: value
+            for key, value in {
+                "repo_url": repo_url,
+                "repo_name": repo_name,
+                "edit_uri": edit_uri,
+            }.items()
+            if value
+        }
+
+    except Exception:
+        warnings.warn("Unable to identify `repo_name`, `repo_url`, and `edit_uri` automatically.")
+        return {}
+
+
+def mkdocs(directory: str, **overrides) -> dict:
+    """Returns back the configuration that will be used when running mkdocs"""
+    mkdocs_config: Dict[str, Any] = {
+        **MKDOCS_DEFAULTS,
+        **repository(directory, **overrides),
+        **overrides,
+    }
+    theme = mkdocs_config["theme"]
+    if theme["name"].lower() == "material" and "custom_dir" not in theme:
+        theme["custom_dir"] = MKDOCS_DEFAULTS["theme"]["custom_dir"]
+
+    nav = mkdocs_config.get("nav", None)
+    if nav and hasattr(nav[0], "copy"):
+        mkdocs_config["nav"] = [nav_item.copy() for nav_item in nav]
+
+    mkdocs_config["markdown_extensions"] = mkdocs_config["markdown_extensions"] + mkdocs_config.pop(
+        "extra_markdown_extensions", []
+    )
+
+    return mkdocs_config
+
+
+def pdocs(directory: str, **overrides) -> dict:
+    """Returns back the configuration that will be used when running pdocs"""
+    defaults = {**PDOCS_DEFAULTS}
+    defaults.update(overrides)
+    return defaults
---
+++ packages/portray/portray-1.5.2.tar.gz/portray-1.5.2/portray/render.py
@@ -0,0 +1,220 @@
+"""Defines how to render the current project and project_config using the
+included documentation generation utilities.
+"""
+import os
+import shutil
+import sys
+import tempfile
+from contextlib import contextmanager
+from glob import glob
+from typing import Dict, Iterator, Tuple
+
+import mkdocs.config as mkdocs_config
+import mkdocs.exceptions as _mkdocs_exceptions
+from mkdocs.commands.build import build as mkdocs_build
+from mkdocs.utils import is_markdown_file
+from pdocs import as_markdown as pdocs_as_markdown
+from yaspin import yaspin
+
+from portray.exceptions import DocumentationAlreadyExists
+
+NO_HOME_PAGE = """
+# Nothing here
+
+`portray` uses README.md as your projects home page.
+It appears you do not yet have a README.md file created.
+"""
+
+
+def documentation(config: dict, overwrite: bool = False) -> None:
+    """Renders the entire project given the project config into the config's
+    specified output directory.
+
+    Behind the scenes:
+
+    - A temporary directory is created and your code is copy and pasted there
+    - pdoc is ran over your code with the output sent into the temporary directory
+        as Markdown documents
+    - MkDocs is ran over all of your projects Markdown documents including those
+        generated py pdoc. MkDocs outputs an HTML representation to a new temporary
+        directory.
+    - The html temporary directory is copied into your specified output location
+    - Both temporary directories are deleted.
+    """
+    if os.path.exists(config["output_dir"]):
+        if overwrite:
+            shutil.rmtree(config["output_dir"])
+        else:
+            raise DocumentationAlreadyExists(config["output_dir"])
+
+    with documentation_in_temp_folder(config) as (_, documentation_output):
+        shutil.copytree(documentation_output, config["output_dir"])
+
+
+def pdocs(config: dict) -> None:
+    """Render this project using the specified pdoc config passed into pdoc.
+
+    This rendering is from code definition to Markdown so that
+    it will be compatible with MkDocs.
+    """
+    pdocs_as_markdown(**config)
+
+
+def mkdocs(config: dict):
+    """Render the project's associated Markdown documentation using the specified
+    MkDocs config passed into the MkDocs `build` command.
+
+    This rendering is from `.md` Markdown documents into HTML
+    """
+    config_instance = _mkdocs_config(config)
+    return mkdocs_build(config_instance)
+
+
+@contextmanager
+def documentation_in_temp_folder(config: dict) -> Iterator[Tuple[str, str]]:
+    """Build documentation within a temp folder, returning that folder name before it is deleted."""
+    if config["append_directory_to_python_path"] and not config["directory"] in sys.path:
+        sys.path.append(config["directory"])
+
+    with tempfile.TemporaryDirectory() as input_dir:
+        input_dir = os.path.join(input_dir, "input")
+        os.mkdir(input_dir)
+        with tempfile.TemporaryDirectory() as temp_output_dir:
+
+            with yaspin(
+                text="Copying source documentation to temporary compilation directory"
+            ) as spinner:
+                for root_file in os.listdir(config["directory"]):
+                    root_file_absolute = os.path.join(config["directory"], root_file)
+                    if os.path.isfile(root_file_absolute) and is_markdown_file(root_file_absolute):
+                        shutil.copyfile(root_file_absolute, os.path.join(input_dir, root_file))
+
+                for source_directory in [config["docs_dir"]] + config["extra_dirs"]:
+                    directory_absolute = os.path.join(config["directory"], source_directory)
+                    if os.path.isdir(directory_absolute):
+                        shutil.copytree(
+                            directory_absolute, os.path.join(input_dir, source_directory)
+                        )
+
+                spinner.ok("Done")
+
+            if "docs_dir" not in config["mkdocs"]:
+                config["mkdocs"]["docs_dir"] = input_dir
+            if "site_dir" not in config["mkdocs"]:
+                config["mkdocs"]["site_dir"] = temp_output_dir
+            if "nav" not in config["mkdocs"]:
+                nav = config["mkdocs"]["nav"] = []
+
+                root_docs = sorted(glob(os.path.join(input_dir, "*.md")))
+                readme_doc = os.path.join(input_dir, "README.md")
+                if readme_doc in root_docs:
+                    root_docs.remove(readme_doc)
+                else:
+                    with open(readme_doc, "w") as readme_doc_file:
+                        readme_doc_file.write(NO_HOME_PAGE)
+
+                nav.append({"Home": "README.md"})
+
+                nav.extend(_doc(doc, input_dir, config) for doc in root_docs)
+
+                nav.extend(
+                    _nested_docs(os.path.join(input_dir, config["docs_dir"]), input_dir, config)
+                )
+            else:
+                nav = config["mkdocs"]["nav"]
+                if nav:
+                    index_nav = nav[0]
+                    index_page: str = ""
+                    if index_nav and isinstance(index_nav, dict):
+                        index_page = tuple(index_nav.values())[0]
+                    elif isinstance(index_nav, str):  # pragma: no cover
+                        index_page = index_nav
+
+                    if index_page:
+
+                        destination_index_page = os.path.join(input_dir, "index.md")
+                        if (
+                            index_page != "README.md"
+                            and index_page != "index.md"
+                            and not os.path.exists(destination_index_page)
+                        ):
+                            shutil.copyfile(
+                                os.path.join(input_dir, index_page), destination_index_page
+                            )
+
+            if config["include_reference_documentation"]:
+                with yaspin(text="Auto generating reference documentation using pdocs") as spinner:
+                    if "output_dir" not in config["pdocs"]:
+                        config["pdocs"]["output_dir"] = os.path.join(input_dir, "reference")
+                    pdocs(config["pdocs"])
+                    reference_docs = _nested_docs(config["pdocs"]["output_dir"], input_dir, config)
+                    nav.append({"Reference": reference_docs})  # type: ignore
+                    spinner.ok("Done")
+
+            with yaspin(text="Rendering complete website from Markdown using MkDocs") as spinner:
+                mkdocs(config["mkdocs"])
+                spinner.ok("Done")
+
+            # remove any settings pointing to the temp dirs
+            if config["mkdocs"]["docs_dir"].startswith(input_dir):
+                del config["mkdocs"]["docs_dir"]
+            if config["mkdocs"]["site_dir"].startswith(temp_output_dir):
+                del config["mkdocs"]["site_dir"]
+            if config["pdocs"]["output_dir"].startswith(input_dir):
+                del config["pdocs"]["output_dir"]
+            if config["include_reference_documentation"]:
+                nav.pop()
+
+            yield input_dir, temp_output_dir
+
+
+def _mkdocs_config(config: dict) -> mkdocs_config.Config:
+    config_instance = mkdocs_config.Config(schema=mkdocs_config.DEFAULT_SCHEMA)
+    config_instance.load_dict(config)
+
+    errors, warnings = config_instance.validate()
+    if errors:
+        print(errors)
+        raise _mkdocs_exceptions.ConfigurationError(
+            f"Aborted with {len(errors)} Configuration Errors!"
+        )
+    elif config.get("strict", False) and warnings:  # pragma: no cover
+        print(warnings)
+        raise _mkdocs_exceptions.ConfigurationError(
+            f"Aborted with {len(warnings)} Configuration Warnings in 'strict' mode!"
+        )
+
+    config_instance.config_file_path = config["config_file_path"]
+    return config_instance
+
+
+def _nested_docs(directory: str, root_directory: str, config: dict) -> list:
+    nav = [
+        _doc(doc, root_directory, config) for doc in sorted(glob(os.path.join(directory, "*.md")))
+    ]
+
+    nested_dirs = sorted(glob(os.path.join(directory, "*/")))
+    for nested_dir in nested_dirs:
+        if (
+            len(glob(os.path.join(nested_dir, "*.md")) + glob(os.path.join(nested_dir, "**/*.md")))
+            > 0
+        ):
+            dir_nav = {
+                _label(nested_dir[:-1], config): _nested_docs(nested_dir, root_directory, config)
+            }
+            nav.append(dir_nav)  # type: ignore
+
+    return nav
+
+
+def _label(path: str, config: Dict) -> str:
+    label = os.path.basename(path)
+    if "." in label:
+        label = ".".join(label.split(".")[:-1])
+    label = label.replace("-", " ").replace("_", " ").title()
+    return config["labels"].get(label, label)
+
+
+def _doc(path: str, root_path: str, config: dict) -> Dict[str, str]:
+    path = os.path.relpath(path, root_path)
+    return {_label(path, config): path}
