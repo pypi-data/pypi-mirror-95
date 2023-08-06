9341d7fe1663e7a66e00dddd8cb1eb3cab998537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:41 +0000

    Add package Geode-SimplexRemeshGeosciences/1.2.3/Geode_SimplexRemeshGeosciences-1.2.3-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/Geode-SimplexRemeshGeosciences/Geode_SimplexRemeshGeosciences-1.2.3-cp36-cp36m-manylinux2014_x86_64.whl/geode_simplexremeshgeosciences/surface.py
@@ -0,0 +1,12 @@
+#
+# Copyright (c) 2019 - 2021 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import opengeode_geosciences
+from geode_common import core
+from geode_parameterization import surface
+from geode_simplexremesh import surface
+
+from .geode_simplexremeshgeosciences_py_surface import *
+
