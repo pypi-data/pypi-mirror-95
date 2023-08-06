0b5be5df9e87754e20e0fcbb50ce5a617cef5933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:14 +0000

    Add package h5-info-pkg-inra-phenome/1.1.6/h5-info-pkg-inra-phenome-1.1.6.tar.gz

---
+++ packages/h5-info-pkg-inra-phenome/h5-info-pkg-inra-phenome-1.1.6.tar.gz/h5-info-pkg-inra-phenome-1.1.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="h5-info-pkg-inra-phenome",
+    version="1.1.6",
+    author="Eric David",
+    author_email="eric.david@ephesia-consult.com",
+    description="Utility package for extracting, reading and saving metadata from HDF5 Phenomobile V2 acquisition file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://forgemia.inra.fr/4p/tools/h5info",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'h5py',
+        'Pillow'
+    ]
+)
