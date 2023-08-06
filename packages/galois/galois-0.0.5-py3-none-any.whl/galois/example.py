f0c02519d12a45313105689267516ea83ae71a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:51 +0000

    Add package galois/0.0.5/galois-0.0.5-py3-none-any.whl

---
+++ packages/galois/galois-0.0.5-py3-none-any.whl/galois/example.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def example_function(value):
+    """
+    This is an example function to test the auto-generated documentation
+
+    Parameters
+    ----------
+    value : float
+        Any floating-point value.
+
+    Returns
+    -------
+    float
+        The same value as was inputted.
+    """
+    return np.array(value)
