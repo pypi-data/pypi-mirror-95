fcfff581c044afc5bab1c389f37b75595274d0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:38 +0000

    Add package psstore-ru/1.0.1/psstore-ru-1.0.1.tar.gz

---
+++ packages/psstore-ru/psstore-ru-1.0.1.tar.gz/psstore-ru-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="psstore-ru",
+    version="1.0.1",
+    author="Ian Gabaraev",
+    author_email="hrattisianees@gmail.com",
+    description="Play Station Store Russian Python Interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ian-Gabaraev/psstorereader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+       'requests', 'bs4', 'pyyaml', 'asyncio', 'aiohttp'
+    ],
+    python_requires='>=3.6',
+)
