1ea2ccd4acc59effbecb6399853f9be108d32a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:29 +0000

    Add package Coolapk/0.0.2/Coolapk-0.0.2.tar.gz

---
+++ packages/Coolapk/Coolapk-0.0.2.tar.gz/Coolapk-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="Coolapk",
+    version="0.0.2",
+    author="QQlittleice",
+    author_email="1767523953@qq.com",
+    description="ToolBox for coolapk",
+    long_description="A module for coolapk. \nFunction:\n1.Login Coolapk\n2.Get UserData",
+    long_description_content_type="text/plain",
+    url="https://github.com/1767523953/Coolapk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+)
