0cb6625b0644085420070be5061951ac354978db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:04 +0000

    Add package cryptodataaccess-athanikos/0.5.4/cryptodataaccess_athanikos-0.5.4-py3-none-any.whl

---
+++ packages/cryptodataaccess-athanikos/cryptodataaccess_athanikos-0.5.4-py3-none-any.whl/cryptodataaccess/Rates/RatesRepository.py
@@ -0,0 +1,25 @@
+from cryptodataaccess.Repository import Repository
+
+DATE_FORMAT = "%Y-%m-%d"
+
+
+class RatesRepository(Repository):
+
+    def __init__(self, rates_store):
+        self.rates_store = rates_store
+        super(RatesRepository, self).__init__()
+
+    def fetch_symbols(self):
+        return self.rates_store.fetch_symbols()
+
+    def fetch_symbol_rates(self):
+        return self.rates_store.fetch_symbol_rates()
+
+    def fetch_symbol_rates_for_date(self, dt):
+        return self.rates_store.fetch_symbol_rates_for_date(dt)
+
+    def fetch_latest_prices_to_date(self, before_date):
+        return self.rates_store.fetch_latest_prices_to_date(before_date)
+
+    def fetch_latest_exchange_rates_to_date(self, before_date):
+        return self.rates_store.fetch_latest_exchange_rates_to_date(before_date)
