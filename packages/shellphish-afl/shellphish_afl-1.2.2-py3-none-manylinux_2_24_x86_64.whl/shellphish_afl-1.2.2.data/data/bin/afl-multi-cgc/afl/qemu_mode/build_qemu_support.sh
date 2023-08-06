6b79a0b984ed83d7b01aec1296b86c2e9800f2a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:07 +0000

    Add package shellphish-afl/1.2.2/shellphish_afl-1.2.2-py3-none-manylinux_2_24_x86_64.whl

---
+++ packages/shellphish-afl/shellphish_afl-1.2.2-py3-none-manylinux_2_24_x86_64.whl/shellphish_afl-1.2.2.data/data/bin/afl-multi-cgc/afl/qemu_mode/build_qemu_support.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+#
+# american fuzzy lop - QEMU build script
+# --------------------------------------
+#
+# Written by Andrew Griffiths <agriffiths@google.com> and
+#            Michal Zalewski <lcamtuf@google.com>
+#
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# This script downloads, patches, and builds a version of QEMU with
+# minor tweaks to allow non-instrumented binaries to be run under
+# afl-fuzz.
+#
+# The modifications reside in patches/*. The standalone QEMU binary
+# will be written to ../afl-qemu-trace.
+#
+
+echo "================================================="
+echo "AFL binary-only instrumentation QEMU build script"
+echo "================================================="
+echo
+
+echo "[*] Performing basic sanity checks..."
+
+if [ ! "`uname -s`" = "Linux" ]; then
+
+  echo "[-] Error: QEMU instrumentation is supported only on Linux."
+  exit 1
+
+fi
+
+if [ ! -f "patches/afl-qemu-cpu-inl.h" -o ! -f "../config.h" ]; then
+
+  echo "[-] Error: key files not found - wrong working directory?"
+  exit 1
+
+fi
+
+if [ ! -f "../afl-showmap" ]; then
+
+  echo "[-] Error: ../afl-showmap not found - compile AFL first!"
+  exit 1
+
+fi
+
+
+for i in libtool wget python automake autoconf sha384sum bison iconv; do
+
+  T=`which "$i" 2>/dev/null`
+
+  if [ "$T" = "" ]; then
+
+    echo "[-] Error: '$i' not found, please install first."
+    exit 1
+
+  fi
+
+done
+
+if [ ! -d "/usr/include/glib-2.0/" -a ! -d "/usr/local/include/glib-2.0/" ]; then
+
+  echo "[-] Error: devel version of 'glib2' not found, please install first."
+  exit 1
+
+fi
+
+if echo "$CC" | grep -qF /afl-; then
+
+  echo "[-] Error: do not use afl-gcc or afl-clang to compile this tool."
+  exit 1
+
+fi
+
+echo "[+] Building a multi-CB-ready qemu!"
+
+if [ -d qemu-dev ]; then
+    echo "[*] Reusing the existing qemu-dev dir (dev only! will just run make)"
+    QEMU_DIR=qemu-dev
+    cd $QEMU_DIR || exit 1
+else
+    QEMU_DIR=multicb-qemu
+    rm -rf $QEMU_DIR
+    echo "[*] Cloning our multi-CB QEMU branch..."
+    git clone --branch multicb_afl --depth=1 https://github.com/mechaphish/qemu-cgc $QEMU_DIR || exit 1
+    echo "[+] Checked out."
+    cd $QEMU_DIR || exit 1
+    echo "[*] Configuring QEMU..."
+    ./cgc_configure_afl_opt
+    echo "[+] Configuration complete."
+fi
+
+echo "[*] Attempting to build QEMU (fingers crossed!)..."
+
+make -j || exit 1
+
+echo "[+] Build process successful!"
+
+echo "[*] Copying binary..."
+
+rm -f "../../multicb-qemu"
+cp "i386-linux-user/qemu-i386" "../../multicb-qemu" || exit 1
+echo "[+] Successfully created '../../multicb-qemu'."
+
+echo "[+] All set, you should now be able to use it together with fakeforksrv!"
+
+exit 0
---
+++ packages/shellphish-afl/shellphish_afl-1.2.2-py3-none-manylinux_2_24_x86_64.whl/shellphish_afl-1.2.2.data/data/bin/afl-multi-cgc/fakeforksrv/check-cs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -eu
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PATH="$PATH:../../../darpa/interact/cb-testing/"
+PORT=$(( 2000 + ($RANDOM % 3000) ))
+
+export AFL_PATH=${AFL_PATH-$SCRIPT_DIR/../afl}
+
+POLLS_DIR_2=
+
+if (( $# > 1 )); then
+    POLLS_DIR="$1"
+    shift
+    CBS="$@"
+elif (( $# == 1 )); then
+    SAMPLE_NAME="$1"
+    SAMPLE_DIR="../../../samples/examples/$SAMPLE_NAME"
+    [ -d "$SAMPLE_DIR" ] || SAMPLE_DIR="../../../samples/cqe-challenges/$SAMPLE_NAME"
+    [ -d "$SAMPLE_DIR" ] || { echo "Cannot find sample dir! Run with full paths. Looked in ../../samples/{examples,cqe-challenges}/$SAMPLE_NAME"; exit 2; }
+    POLLS_DIR="$SAMPLE_DIR/poller/for-testing"
+    if [ -d "$POLLS_DIR" ]; then
+        POLLS_DIR_2="$SAMPLE_DIR/poller/for-release/"
+        [ -d "$POLLS_DIR_2" ] || POLLS_DIR_2=
+    else
+        POLLS_DIR="$SAMPLE_DIR/poller/for-release/"
+    fi
+    [ -d "$POLLS_DIR" ] || { echo "Found sample dir but not its poller/for-{testing,release}/? (sample dir: $SAMPLE_DIR)"; exit 1; }
+    CBS="$SAMPLE_DIR/bin/${SAMPLE_NAME}_?"
+    [ -f "$SAMPLE_DIR/bin/${SAMPLE_NAME}_1" ] || { echo "Warning: single-CB! Trying $SAMPLE_DIR/bin/$SAMPLE_NAME"; CBS="$SAMPLE_DIR/bin/${SAMPLE_NAME}"; }
+else
+    echo -e "Usage: $0 POLLS_DIR CB_0 [CB_1] [...]\n       SAMPLE_NAME"
+    exit 1
+fi
+ls "$POLLS_DIR"/*.xml >/dev/null || { echo "No *.xml files in polls dir $POLLS_DIR"; exit 3; }
+ls $CBS >/dev/null || { echo "No CBs $CBS"; exit 3; }
+
+./run_via_fakeforksrv --port $PORT $CBS &
+
+RET=1
+cb-replay --host 127.0.0.1 --port $PORT "$POLLS_DIR"/*.xml | egrep 'failed: [1-9]' || RET=0
+(( $RET == 1 )) || ! [ -n "$POLLS_DIR_2" ] || cb-replay --host 127.0.0.1 --port $PORT "$POLLS_DIR_2"/*.xml | egrep 'failed: [1-9]' || RET=0
+
+kill %
+exit $RET
