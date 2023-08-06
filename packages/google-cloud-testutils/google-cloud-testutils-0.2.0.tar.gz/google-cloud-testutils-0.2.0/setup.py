ae7e62a51ebf9b2c6c655be9eb7cb546c891b03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:08 +0000

    Add package google-cloud-testutils/0.2.0/google-cloud-testutils-0.2.0.tar.gz

---
+++ packages/google-cloud-testutils/google-cloud-testutils-0.2.0.tar.gz/google-cloud-testutils-0.2.0/setup.py
@@ -0,0 +1,64 @@
+# Copyright 2017 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import io
+import os
+import setuptools  # type: ignore
+
+version = "0.2.0"
+
+package_root = os.path.abspath(os.path.dirname(__file__))
+
+readme_filename = os.path.join(package_root, "README.rst")
+with io.open(readme_filename, encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+scripts = (
+    ["lower-bound-checker=test_utils.lower_bound_checker.lower_bound_checker:main"],
+)
+
+setuptools.setup(
+    name="google-cloud-testutils",
+    version=version,
+    long_description=readme,
+    author="Google LLC",
+    author_email="googleapis-packages@google.com",
+    license="Apache 2.0",
+    url="https://github.com/googleapis/python-test-utils",
+    packages=setuptools.PEP420PackageFinder.find(),
+    entry_points={"console_scripts": scripts},
+    platforms="Posix; MacOS X; Windows",
+    include_package_data=True,
+    install_requires=(
+        "google-auth >= 0.4.0",
+        "six>=1.9.0",
+        "click>=7.0.0",
+        "packaging>=19.0",
+    ),
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    zip_safe=False,
+)
