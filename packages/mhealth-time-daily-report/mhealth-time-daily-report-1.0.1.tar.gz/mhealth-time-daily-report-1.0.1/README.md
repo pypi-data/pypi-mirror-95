ed560f2541870fab8251b29888686956f6f25bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:06 +0000

    Add package mhealth-time-daily-report/1.0.1/mhealth-time-daily-report-1.0.1.tar.gz

---
+++ packages/mhealth-time-daily-report/mhealth-time-daily-report-1.0.1.tar.gz/mhealth-time-daily-report-1.0.1/README.md
@@ -0,0 +1,3 @@
+# TIME Study Daily Report
+
+This package is developed exclusively for the longitudinal data collection study called TIME Study. The library is used to compute day level summaries of data collected using smartphones and smartwatches.
---
+++ packages/mhealth-time-daily-report/mhealth-time-daily-report-1.0.1.tar.gz/mhealth-time-daily-report-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='mhealth-time-daily-report',
+    version='1.0.1',
+    description='Package to generate daily reports for TIME study data',
+    url='https://bitbucket.org/mhealthresearchgroup/daily_report_cluster/src/master/',
+    long_description='long_description',
+    long_description_content_type="text/markdown",
+    author='Aditya Ponnada',
+    author_email='ponnada.a@northeastern.edu',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    package_data={'time_daily_report': ['validation_key_ans.json']},
+    include_package_data=True,
+    python_requires='>=3.6'
+)
