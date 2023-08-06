f2dabb15f999fe395cc9e7bf8afe86e66e18e31c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:31 +0000

    Add package daemonocle/1.2.1/daemonocle-1.2.1.tar.gz

---
+++ packages/daemonocle/daemonocle-1.2.1.tar.gz/daemonocle-1.2.1/HISTORY.rst
@@ -0,0 +1,139 @@
+Release History
+---------------
+
+v1.2.1 (2021-02-16)
+~~~~~~~~~~~~~~~~~~~
+
+* Minor tweaks to an undocumented experimental feature
+
+v1.2.0 (2021-02-15)
+~~~~~~~~~~~~~~~~~~~
+
+* Dropped support for Python 2.7 and 3.5 (only >= 3.6 is now supported)
+* Deprecated ``shutdown_callback`` argument in favor of new hooks system.
+  Replace ``shutdown_callback=func`` with ``hooks={'shutdown': func}``.
+* Added ``'start'`` hook that gets called before attempting to start the
+  daemon. This can be useful for checking things or setting things up.
+* Improvements to some undocumented experimental features. :)
+
+v1.1.1 (2020-12-31)
+~~~~~~~~~~~~~~~~~~~
+
+* The ``stop`` action will now clean up the PID file if the worker didn't
+  do it for some reason.
+* A few bug fixes to some undocumented experimental features. :)
+
+v1.1.0 (2020-12-16)
+~~~~~~~~~~~~~~~~~~~
+
+* Official support for Python 3.9
+* Added back official support for Python 3.5
+* Increased test coverage to over 95%.
+* All tests now pass on macOS (Intel) and the GitHub Actions build now runs on
+  macOS 10.15 in addition to Ubuntu.
+* Fixed the ``close_open_files`` option to be much more reliable and consistent
+  across different platforms.
+* Fixed a race condition with the self-reload functionality where the PID file
+  of the parent process was being deleted while the child process was trying to
+  read it.
+* Added ``stdout_file`` and ``stderr_file`` arguments to ``Daemon``. If these
+  arguments are provided when ``detach=True``, STDOUT and STDERR will be
+  redirected to these files. In non-detached mode, these arguments are ignored.
+* When ``chrootdir`` is given, all other paths are now always considered
+  relative to the chroot directory, even with a leading slash.
+* Actions can now take arbitrary arguments, and (on Python 3) CLI options are
+  auto-generated from the function signature. The auto-generated CLI options
+  work best when your action's function signature contains type annotations and
+  default values where applicable.
+* Added ``timeout`` and ``force`` arguments to the built-in ``stop`` action,
+  accessible from the CLI as ``--timeout`` and ``--force``.
+* Added ``json`` and ``fields`` arguments to the built-in ``status`` action,
+  accessible from the CLI as ``--json`` and ``--fields``.
+* Added colored output when the output stream is attached to a terminal.
+* Fixed a bug where the daemon wouldn't respond properly to ``docker stop``
+  when running in a docker container.
+* The worker function can now be a method called ``worker`` on a ``Daemon``
+  subclass.
+* Some more secret experimental stuff. :)
+
+v1.0.2 (2020-07-12)
+~~~~~~~~~~~~~~~~~~~
+
+* Official support for Python 2.7, 3.6, 3.7, and 3.8.
+* Fixed bug checking if a stream is a socket on Python 3.8.
+* Closing open files is now more efficient on systems with a very high limit
+  on the number of open files.
+* Improved detection of running inside a container.
+
+v1.0.1 (2016-04-17)
+~~~~~~~~~~~~~~~~~~~
+
+* No changes in this release. Bumped version only to re-upload to PyPI.
+
+v1.0.0 (2016-04-17)
+~~~~~~~~~~~~~~~~~~~
+
+* Added official support for Python 2.7, 3.3, 3.4, and 3.5.
+* Added a comprehensive suite of unit tests with over 90% code coverage.
+* Dependencies (click and psutil) are no longer pinned to specific versions.
+* Fixed bug with ``atexit`` handlers not being called in intermediate processes.
+* Fixed bug when PID file is a relative path.
+* Fixed bug when STDIN doesn't have a file descriptor number.
+* Fixed bug when running in non-detached mode in a Docker container.
+* A TTY is no longer checked for when deciding how to run in non-detached mode.
+  The behavior was inconsistent across different platforms.
+* Fixed bug when a process stopped before having chance to check if it stopped.
+* Fixed bug where an exception could be raised if a PID file is already gone
+  when trying to remove it.
+* Subdirectories created for PID files now respect the ``umask`` setting.
+* The pre-``umask`` mode for PID files is now ``0o666`` instead of ``0o777``,
+  which will result in a default mode of ``0o644`` instead of ``0o755`` when
+  using the default ``umask`` of ``0o22``.
+
+v0.8 (2014-08-01)
+~~~~~~~~~~~~~~~~~
+
+* Upgraded click to version 2.5.
+* Status action now returns exit code 1 if the daemon is not running.
+
+v0.7 (2014-06-23)
+~~~~~~~~~~~~~~~~~
+
+* Fixed bug that was causing an empty PID file on Python 3.
+* Upgraded click to version 2.1.
+* Open file discriptors are no longer closed by default. This functionality is now optional via the
+  ``close_open_files`` argument to ``Daemon()``.
+* Added ``is_worker`` argument to ``DaemonCLI()`` as well as the ``pass_daemon`` decorator.
+
+v0.6 (2014-06-10)
+~~~~~~~~~~~~~~~~~
+
+* Upgraded click to version 2.0.
+
+v0.5 (2014-06-09)
+~~~~~~~~~~~~~~~~~
+
+* Fixed literal octal formatting to work with Python 3.
+
+v0.4 (2014-05-19)
+~~~~~~~~~~~~~~~~~
+
+* Fixed bug with uptime calculation in status action.
+* Upgraded click to version 0.7.
+
+v0.3 (2014-05-14)
+~~~~~~~~~~~~~~~~~
+
+* Reorganized package and cleaned up code.
+
+v0.2 (2014-05-12)
+~~~~~~~~~~~~~~~~~
+
+* Renamed ``Daemon.get_actions()`` to ``Daemon.list_actions()``.
+* Improvements to documentation.
+* Fixed bug with non-detached mode when parent is in the same process group.
+
+v0.1 (2014-05-11)
+~~~~~~~~~~~~~~~~~
+
+* Initial release.
---
+++ packages/daemonocle/daemonocle-1.2.1.tar.gz/daemonocle-1.2.1/daemonocle/cli.py
@@ -0,0 +1,156 @@
+"""Utilities for building command-line interfaces for your daemons"""
+
+import inspect
+from functools import wraps
+
+import click
+
+from .core import Daemon
+from .helpers import MultiDaemon
+
+
+def _parse_cli_options(func):
+    """Parse click options from a function signature"""
+    options = []
+    for param in inspect.signature(func).parameters.values():
+        if param.kind not in {param.POSITIONAL_OR_KEYWORD, param.KEYWORD_ONLY}:
+            # Only keyword arguments are currently supported
+            continue
+
+        option_name = '--' + param.name.lower().replace('_', '-').strip('-')
+        kwargs = {}
+        if param.annotation in {str, int, float, bool}:
+            # Only basic types are currently supported
+            kwargs['type'] = param.annotation
+
+        if param.default != param.empty:
+            kwargs['default'] = param.default
+        else:
+            # If the param doesn't have a default, then it's required
+            kwargs['required'] = True
+
+        if param.annotation == bool or isinstance(param.default, bool):
+            if param.default is True:
+                # If the default of a boolean option is ``True``, then add a
+                # ``--no-x` off switch
+                option_name += '/--no-' + option_name.lstrip('-')
+            else:
+                # If the default is ``False``, just make it a basic flag
+                kwargs['is_flag'] = True
+
+        args = (option_name, param.name)
+
+        options.append((args, kwargs))
+
+    # Reverse it so the decorators are applied in the correct order
+    return options[::-1]
+
+
+class DaemonCLI(click.MultiCommand):
+    """A Command class for `click <http://click.pocoo.org/>`_.
+
+    This class automatically adds start, stop, restart, and status
+    subcommands for daemons.
+    """
+
+    def __init__(
+            self, callback=None, daemon_params=None, is_worker=True,
+            daemon_class=Daemon, daemon=None, **kwargs):
+        """Create a new DaemonCLI object."""
+        self.daemon_params = daemon_params or {}
+        self.daemon_class = (
+            daemon_class if daemon is None else daemon.__class__)
+        self.is_worker = (
+            is_worker and callback is not None and callable(callback))
+
+        if daemon is None:
+            daemon = self.daemon_class(**self.daemon_params)
+
+        if ((not daemon.worker or not callable(daemon.worker)) and
+                self.is_worker):
+            # If the callback is the worker, then don't pass the
+            # callback to the parent class so we don't call it twice
+            daemon.worker = callback
+            callback = None
+
+        # The context object will be the Daemon object
+        context_settings = {'obj': daemon}
+
+        if not kwargs.get('help'):
+            kwargs['help'] = daemon.worker.__doc__
+
+        super(DaemonCLI, self).__init__(
+            callback=callback, context_settings=context_settings, **kwargs)
+
+    def list_commands(self, ctx):
+        """Get a list of subcommands."""
+        return self.daemon_class.list_actions()
+
+    def get_command(self, ctx, name):
+        """Get a callable command object."""
+        if name not in self.daemon_class.list_actions():
+            return None
+
+        # The context object is a Daemon object
+        daemon = ctx.obj
+
+        action = daemon.get_action(name)
+
+        @wraps(action)
+        def subcommand(*args, **kwargs):
+            return action(*args, **kwargs)
+
+        if name in {'start', 'stop', 'restart'}:
+            if name in {'start', 'restart'}:
+                subcommand = click.option(
+                    '--debug', is_flag=True,
+                    help='Do NOT detach and run in the background.',
+                )(subcommand)
+            if name in {'stop', 'restart'}:
+                subcommand = click.option(
+                    '--force', is_flag=True,
+                    help='Kill the daemon forcefully after the timeout.',
+                )(subcommand)
+                subcommand = click.option(
+                    '--timeout', type=int, default=None,
+                    help=('Number of seconds to wait for the daemon to stop. '
+                          'Overrides "stop_timeout" from daemon definition.'),
+                )(subcommand)
+            if isinstance(daemon, MultiDaemon):
+                subcommand = click.option(
+                    '--worker-id', type=int, default=None,
+                    help='The ID of the worker to {}.'.format(name),
+                )(subcommand)
+        elif name == 'status':
+            subcommand = click.option(
+                '--fields', type=str, default=None,
+                help='Comma-separated list of process info fields to display.',
+            )(subcommand)
+            subcommand = click.option(
+                '--json', is_flag=True,
+                help='Show the status in JSON format.',
+            )(subcommand)
+            if isinstance(daemon, MultiDaemon):
+                subcommand = click.option(
+                    '--worker-id', type=int, default=None,
+                    help='The ID of the worker whose status to get.',
+                )(subcommand)
+        else:
+            # This is a custom action so try to parse the CLI options
+            # by inspecting the function
+            for option_args, option_kwargs in _parse_cli_options(action):
+                subcommand = click.option(
+                    *option_args, **option_kwargs)(subcommand)
+
+        # Make it into a click command
+        subcommand = click.command(name)(subcommand)
+
+        return subcommand
+
+
+def cli(**daemon_params):
+    return click.command(cls=DaemonCLI, daemon_params=daemon_params)
+
+
+# Make a pass decorator for passing the Daemon object
+pass_daemon = click.make_pass_decorator(Daemon)
