85e7f712afd53107dbdca4787157ea80b80a3ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:42 +0000

    Add package revdb/1.0.1/revdb-1.0.1-py2.py3-none-any.whl

---
+++ packages/revdb/revdb-1.0.1-py2.py3-none-any.whl/revdb/__init__.py
@@ -0,0 +1,7 @@
+"""a light layer for revtel mongodb"""
+
+__version__ = "1.0.1"
+
+from revdb.db import connect, connect_v2, make_model_class
+
+__all__ = ["make_model_class", "connect", "connect_v2"]
