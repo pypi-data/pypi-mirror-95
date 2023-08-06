02c818a193a2641131d00ca079482adeb77dd44e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:36 +0000

    Add package stats-advanced/0.0.6/stats_advanced-0.0.6-py3-none-any.whl

---
+++ packages/stats-advanced/stats_advanced-0.0.6-py3-none-any.whl/stats_advanced/__init__.py
@@ -0,0 +1,7 @@
+from stats_advanced.stats_advanced import Mean
+from stats_advanced.stats_advanced import Median
+from stats_advanced.stats_advanced import Mode
+from stats_advanced.stats_advanced import StandardDeviation
+from stats_advanced.stats_advanced import Skewness
+from stats_advanced.stats_advanced import UnivariateRegression
+from stats_advanced.stats_advanced import MultiVariateRegression
