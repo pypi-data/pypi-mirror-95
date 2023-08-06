aacc53a48977065438efe861918fbbc7fe361b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:39 +0000

    Add package seqsim/0.2/seqsim-0.2-py2.py3-none-any.whl

---
+++ packages/seqsim/seqsim-0.2-py2.py3-none-any.whl/seqsim/common.py
@@ -0,0 +1,118 @@
+"""
+Common functions.
+"""
+
+# Import Python standard libraries
+import hashlib
+import random
+from typing import Union, List, Optional, Sequence
+
+# Import 3rd party libraries
+import numpy as np
+
+
+def set_seeds(seed: Union[str, float, int]) -> None:
+    """
+    Set seeds globally from the user provided one.
+
+    The function takes care of reproducibility and allows to use strings and
+    floats as seed for `numpy` as well.
+
+    :param seed: The seed for Python and numpy random number generators.
+    """
+
+    # Set seed for Python RNG
+    random.seed(seed)
+
+    # Allows using strings as numpy seeds, which only takes uint32 or arrays of uint32
+    if isinstance(seed, (str, float)):
+        np_seed = np.frombuffer(
+            hashlib.sha256(str(seed).encode("utf-8")).digest(), dtype=np.uint32
+        )
+    else:
+        np_seed = seed
+
+    # Set the np set
+    np.random.seed(np_seed)
+
+
+# TODO: properly rewrite
+def sequence_find(hay: Sequence, needle: Sequence) -> Optional[int]:
+    """
+    Return the index for starting index of a sub-sequence within a sequence.
+
+    The function is intended to work similarly to the built-in `.find()` method for
+    Python strings, but accepting all types of sequences (including different types
+    for `hay` and `needle`).
+
+    :param hay: The sequence to be searched within.
+    :param needle: The sub-sequence to be located in the sequence.
+    :return: The starting index of the sub-sequence in the sequence, or `None` if not
+             found.
+    """
+    # Cache `needle` length and have it as a tuple already
+    len_needle = len(needle)
+    t_needle = tuple(needle)
+
+    # Iterate over all sub-lists (or sub-tuples) of the correct length and check
+    # for matches
+    for i in range(len(hay) - len_needle + 1):
+        if tuple(hay[i : i + len_needle]) == t_needle:
+            return i
+
+    return None
+
+
+def collect_subseqs(sequence: Sequence, sort: bool = True) -> List[Sequence]:
+    """
+    Collects all possible sub-sequences in a given sequence.
+
+    When sorting is requested, sub-sequences will first be sorted by their length and,
+    later, by comparing one with the other. Mixing types, like strings and integers, can
+    lead to unexpected results and is not suggested if the type cannot be guaranteed.
+
+    Note that this function performs simple comprehensions, neither using padding
+    symbols nor the more complex methods n-gram collection methods ultimately based on
+    `ngram_iter()`.
+
+    Examples
+    --------
+    collect_subseqs('abcde')
+    ['a', 'b', 'c', 'd', 'e', 'ab', 'bc', 'cd', 'de', 'abc', 'bcd', 'cde', 'abcd', 'bcde', 'abcde']
+
+    :param sequence: The sequence that shall be converted into it's ngram-representation.
+    :param sort: Whether to sort the list of ngrams by length and by identity
+        (default: True).
+    :return: A list of all ngrams of the input sequence.
+    """
+
+    # Cache the length of the sequence
+    length = len(sequence)
+
+    # Set the starting index
+    idx = 0
+
+    # define the output list
+    ret = []
+
+    # start the while loop
+    while idx != length and idx < length:
+        # copy the sequence
+        new_sequence = sequence[idx:length]
+
+        # append the sequence to the output list
+        ret += [new_sequence]
+
+        # loop over the new sequence
+        for j in range(1, len(new_sequence)):
+            ret += [new_sequence[:j]]
+            ret += [new_sequence[j:]]
+
+        # increment idx and decrement length
+        idx += 1
+        length -= 1
+
+    if sort:
+        ret = sorted(ret, key=lambda e: (len(e), e))
+
+    return ret
