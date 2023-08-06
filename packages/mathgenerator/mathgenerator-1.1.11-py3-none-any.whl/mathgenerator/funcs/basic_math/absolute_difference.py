3936f32fc5ad73564e90786b09ae51595cb14839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:30 +0000

    Add package mathgenerator/1.1.11/mathgenerator-1.1.11-py3-none-any.whl

---
+++ packages/mathgenerator/mathgenerator-1.1.11-py3-none-any.whl/mathgenerator/funcs/basic_math/absolute_difference.py
@@ -0,0 +1,22 @@
+from .__init__ import *
+
+
+def absoluteDifferenceFunc(maxA=100, maxB=100, style='raw'):
+    a = random.randint(-1 * maxA, maxA)
+    b = random.randint(-1 * maxB, maxB)
+    absDiff = abs(a - b)
+
+    if style == 'latex':
+        problem = "\\(|" + str(a) + "-" + str(b) + "|=\\)"
+        solution = f"\\({absDiff}\\)"
+    else:
+        problem = "|" + str(a) + "-" + str(b) + "|="
+        solution = absDiff
+    return problem, solution
+
+
+absolute_difference = Generator(
+    "Absolute difference between two numbers", 71,
+    "Absolute difference betweeen two numbers a and b =", "|a-b|",
+    absoluteDifferenceFunc,
+    ["maxA=100", "maxB=100"])
