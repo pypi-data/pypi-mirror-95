03c1886837574191f23ebc3791a40a154e57ad7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:50 +0000

    Add package PyPDFForm/0.3.0/PyPDFForm-0.3.0.tar.gz

---
+++ packages/PyPDFForm/PyPDFForm-0.3.0.tar.gz/PyPDFForm-0.3.0/PyPDFForm/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .middleware.wrapper import PyPDFForm
+
+__version__ = "0.3.0"
---
+++ packages/PyPDFForm/PyPDFForm-0.3.0.tar.gz/PyPDFForm-0.3.0/PyPDFForm/middleware/exceptions/base.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Contains base exception"""
+
+
+class BasePyPDFFormException(Exception):
+    """Base exception for PyPDFForm."""
