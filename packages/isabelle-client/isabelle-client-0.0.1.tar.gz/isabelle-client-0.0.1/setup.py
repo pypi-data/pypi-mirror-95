d4d80e710767676dfd5448d036b217597b66584b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:20 +0000

    Add package isabelle-client/0.0.1/isabelle-client-0.0.1.tar.gz

---
+++ packages/isabelle-client/isabelle-client-0.0.1.tar.gz/isabelle-client-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['isabelle_client']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'isabelle-client',
+    'version': '0.0.1',
+    'description': 'A client to Isabelle proof assistant server',
+    'long_description': '# Isabelle Client\n\nA client for [Isabelle](https://isabelle.in.tum.de) server. For more information about the server see part 4 of [the Isabelle system manual](https://isabelle.in.tum.de/dist/Isabelle2020/doc/system.pdf).\n',
+    'author': 'Boris Shminke',
+    'author_email': 'boris@shminke.ml',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/inpefess/isabelle-client',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
