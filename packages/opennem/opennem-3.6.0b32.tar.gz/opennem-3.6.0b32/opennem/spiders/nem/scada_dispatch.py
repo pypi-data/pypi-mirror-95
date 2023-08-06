52bb1e271bc208646868f7f5184b32a6c5989903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:54 +0000

    Add package opennem/3.6.0b32/opennem-3.6.0b32.tar.gz

---
+++ packages/opennem/opennem-3.6.0b32.tar.gz/opennem-3.6.0b32/opennem/spiders/nem/scada_dispatch.py
@@ -0,0 +1,53 @@
+from opennem.pipelines.bulk_insert import BulkInsertPipeline
+from opennem.pipelines.csv import RecordsToCSVPipeline
+from opennem.pipelines.nem.opennem import NemwebUnitScadaOpenNEMStorePipeline
+from opennem.spiders.nemweb import NemwebSpider
+
+
+class NemwebLatestDispatchScada(NemwebSpider):
+    name = "au.nem.latest.dispatch_scada"
+    start_url = "http://www.nemweb.com.au/Reports/CURRENT/Dispatch_SCADA/"
+    limit = 3
+
+    pipelines_extra = set(
+        [
+            NemwebUnitScadaOpenNEMStorePipeline,
+            BulkInsertPipeline,
+            RecordsToCSVPipeline,
+        ]
+    )
+
+
+class NemwebCurrentDispatchScada(NemwebSpider):
+    name = "au.nem.current.dispatch_scada"
+    start_url = "http://www.nemweb.com.au/Reports/CURRENT/Dispatch_SCADA/"
+    limit = 0
+    skip = 2
+
+    pipelines_extra = set(
+        [
+            NemwebUnitScadaOpenNEMStorePipeline,
+            BulkInsertPipeline,
+            RecordsToCSVPipeline,
+        ]
+    )
+
+
+class NemwebArchiveDispatchScada(NemwebSpider):
+    name = "au.nem.archive.dispatch_scada"
+    start_url = "http://www.nemweb.com.au/Reports/ARCHIVE/Dispatch_SCADA/"
+
+    pipelines_extra = set(
+        [
+            NemwebUnitScadaOpenNEMStorePipeline,
+            BulkInsertPipeline,
+            RecordsToCSVPipeline,
+        ]
+    )
+
+    # Archives tend to contain large zips of embedded zips so throttle
+    # to limit memory use
+    custom_settings = {
+        "CONCURRENT_REQUESTS": 4,
+        "CONCURRENT_ITEMS": 1,
+    }
