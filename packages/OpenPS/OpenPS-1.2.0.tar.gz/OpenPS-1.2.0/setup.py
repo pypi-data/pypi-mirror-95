a2a1a0f051ec97e01d21d04c4ced7e4b3ebc8b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:06 +0000

    Add package OpenPS/1.2.0/OpenPS-1.2.0.tar.gz

---
+++ packages/OpenPS/OpenPS-1.2.0.tar.gz/OpenPS-1.2.0/setup.py
@@ -0,0 +1,18 @@
+import codecs
+from setuptools import setup
+
+with codecs.open("README.md", 'r', encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+setup(
+   name='OpenPS',
+   version='1.2.0',
+   description='Open source plugin system.',
+   long_description_content_type="text/markdown",
+   long_description=long_description,
+   license="Apache-2.0",
+   dependency_links=["https://github.com/TheKruger/OpenPS"],
+   author='TheKruger',
+   keywords=["python", "plugin-system"],
+   packages=['OpenPS']
+)
