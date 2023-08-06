738d638d5d62778aae7ecea9eb5dce62985fa93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:10 +0000

    Add package nutritranslate/0.4.1/nutritranslate-0.4.1.tar.gz

---
+++ packages/nutritranslate/nutritranslate-0.4.1.tar.gz/nutritranslate-0.4.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='nutritranslate',
+    packages=find_packages(include=['nutritranslate']),
+    version='0.4.1',
+    description='A service that translates and normalizes food nutrients',
+    author='Renato Campos',
+    author_email='renatoineeve@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ineeve/nutrients-translator",
+    license='MIT',
+    install_requires=[],
+    python_requires='>=3.6'
+)
