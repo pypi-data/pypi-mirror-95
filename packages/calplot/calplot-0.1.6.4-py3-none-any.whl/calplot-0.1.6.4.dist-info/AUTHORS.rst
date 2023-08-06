43709c1e7d063b4cb22f9f4fab1aa8402aa4f303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:58 +0000

    Add package calplot/0.1.6.4/calplot-0.1.6.4-py3-none-any.whl

---
+++ packages/calplot/calplot-0.1.6.4-py3-none-any.whl/calplot-0.1.6.4.dist-info/AUTHORS.rst
@@ -0,0 +1,5 @@
+Calplot is a fork of calmap maintained by Tom Kwok.
+Calmap was written by Martijn Vermaat.
+
+- Tom Kwok <tom@tomkwok.com>
+- Martijn Vermaat <martijn@vermaat.name>
---
+++ packages/calplot/calplot-0.1.6.4-py3-none-any.whl/calplot/__init__.py
@@ -0,0 +1,9 @@
+__version_info__ = ('0', '1', '6', '4')
+__date__ = '20 Feb 2021'
+
+__version__ = '.'.join(__version_info__)
+__author__ = 'Tom Kwok'
+__contact__ = 'tom@tomkwok.com'
+__homepage__ = 'https://github.com/tomkwok/calplot'
+
+from .calplot import yearplot, calplot
