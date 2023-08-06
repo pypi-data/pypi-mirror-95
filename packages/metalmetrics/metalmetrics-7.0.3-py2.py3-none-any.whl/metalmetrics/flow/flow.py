4621ed6bfdbda88b081302774613b81bb7f7edb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:34 +0000

    Add package metalmetrics/7.0.3/metalmetrics-7.0.3-py2.py3-none-any.whl

---
+++ packages/metalmetrics/metalmetrics-7.0.3-py2.py3-none-any.whl/metalmetrics/flow/flow.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import grpc
+
+from concurrent import futures
+from metalmetrics.flow.flow_pb2 import FlowReply
+from metalmetrics.flow.flow_pb2_grpc import (
+    add_FlowProtoServicer_to_server,
+    FlowProtoServicer,
+)
+
+MAX_WORKERS = 10
+
+MSG_PREFIX = "metalmetrics/metrics"
+MSG_SEP = "/"
+
+
+class FlowException(Exception):
+    def __init__(self, info):
+        super().__init__(self)
+        self._info = info
+
+    def __str__(self):
+        return self._info
+
+
+class Flow(object):
+    def __init__(self, config):
+        if config is None:
+            raise FlowException("config invalid")
+        self._config = config
+
+    def _serve(self, routine):
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=MAX_WORKERS))
+        add_FlowProtoServicer_to_server(FlowProto(routine), server)
+        server.add_insecure_port(self._config.listen_url)
+        server.start()
+        server.wait_for_termination()
+
+    def run(self, routine):
+        self._serve(routine)
+
+
+class FlowProto(FlowProtoServicer):
+    def __init__(self, routine):
+        self._routine = routine
+
+    def SendFlow(self, request, _):
+        if len(request.message) == 0 or not request.message.startswith(
+            MSG_PREFIX + MSG_SEP
+        ):
+            return FlowReply(message="")
+        msg = MSG_SEP.split(request.message)
+        if len(msg) == len(MSG_SEP.split(MSG_PREFIX)):
+            buf = self._routine()
+        elif len(msg) == len(MSG_SEP.split(MSG_PREFIX) + 1):
+            buf = self._routine(msg[-1])
+        else:
+            buf = ""
+        return FlowReply(message=buf)
