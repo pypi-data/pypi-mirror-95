da18899d0a03e73a15fbcb2d750207aeb16ec18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:44 +0000

    Add package udacity-distributions-5-exe/0.1/udacity_distributions_5_exe-0.1.tar.gz

---
+++ packages/udacity-distributions-5-exe/udacity_distributions_5_exe-0.1.tar.gz/udacity_distributions_5_exe-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udacity_distributions_5_exe',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udacity_distributions_5_exe'],
+      zip_safe=False)
