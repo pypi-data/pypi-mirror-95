ccb4be8e27bab9feb20a3ee774325e49e0cd5fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:32 +0000

    Add package swat-em/0.6.3/swat_em-0.6.3-py3-none-any.whl

---
+++ packages/swat-em/swat_em-0.6.3-py3-none-any.whl/swat_em/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+__title__ = "SWAT-EM - Specific Winding Analyse Tool for electrical machines"
+__version__ = "0.6.3"
+__author__ = "Martin Baun"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2018-2021 Martin Baun"
+
+from swat_em.datamodel import datamodel, project
