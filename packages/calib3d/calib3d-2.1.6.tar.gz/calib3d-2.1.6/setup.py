38025e01dd9aad9c293f15ae224ebacce349e4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:55 +0000

    Add package calib3d/2.1.6/calib3d-2.1.6.tar.gz

---
+++ packages/calib3d/calib3d-2.1.6.tar.gz/calib3d-2.1.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calib3d',
+    author='Gabriel Van Zandycke',
+    author_email="gabriel.vanzandycke@hotmail.com",
+    url="https://github.com/ispgroupucl/calib3d",
+    licence="LGPL",
+    python_requires='>=3.6',
+    description="Python 3D calibration and homogenous coordinates computation library",
+    version='2.1.6',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "opencv-python"
+    ],
+)
