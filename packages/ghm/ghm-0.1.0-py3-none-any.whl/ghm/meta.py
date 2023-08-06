15bce13e9930d3639b0b1d622a5cbec8b0df7ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:51 +0000

    Add package ghm/0.1.0/ghm-0.1.0-py3-none-any.whl

---
+++ packages/ghm/ghm-0.1.0-py3-none-any.whl/ghm/meta.py
@@ -0,0 +1,7 @@
+NAME = __package__
+DESCRIPTION = "GitHub Mirrorer - Bulk mirror GitHub repositories"
+VERSION = "0.1.0"
+AUTHOR = "Mike Conigliaro"
+URL = "http://conigliaro.org"
+EMAIL = "mike@conigliaro.org"
+COPYRIGHT = f"""Copyright (c) 2021 {AUTHOR}"""
