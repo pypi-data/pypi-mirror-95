5aad5bae8a6daae92339a89467fd174dd9d3670c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:36 +0000

    Add package tikotapi/1.0.1/tikotapi-1.0.1.tar.gz

---
+++ packages/tikotapi/tikotapi-1.0.1.tar.gz/tikotapi-1.0.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['tikotapi']
+setup_kwargs = {
+    'name': 'tikotapi',
+    'version': '1.0.1',
+    'description': 'API by TikOt. Connect to account (testing account): tikotapi.connect(). Next to dashboard: tikotapi.panel_service (panel_tikotshop)("login", "password").',
+    'long_description': None,
+    'author': 'tikotstudio',
+    'author_email': 'tikotstudio@yandex.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.0,<4.0',
+}
+
+
+setup(**setup_kwargs)
