d9bf88ffdee88be5e6d66288d79ccdff83fd2252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:55 +0000

    Add package docdata/0.0.1/docdata-0.0.1-py3-none-any.whl

---
+++ packages/docdata/docdata-0.0.1-py3-none-any.whl/docdata/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Add structured information to the end of your python docstrings."""
+
+from .api import docdata, parse_docdata
+
+__all__ = [
+    'docdata',
+    'parse_docdata',
+]
