d51c8879be46c7d61c91dfc882f7f2812352d49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:41 +0000

    Add package mdpdf/0.0.9/mdpdf-0.0.9-py3-none-any.whl

---
+++ packages/mdpdf/mdpdf-0.0.9-py3-none-any.whl/mdpdf/converter.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+import commonmark
+import fitz  # pymupdf
+
+from . import log
+from .pdf_renderer import PdfRenderer
+
+
+class Converter:
+    def __init__(self, outputFileName):
+        self.parser = commonmark.Parser()
+        self.renderer = PdfRenderer(outputFileName)
+
+    def convert(self, inputFile):
+        log.info(inputFile)
+        mdFile = open(inputFile, "r", encoding="utf-8")
+        entireFile = mdFile.read()
+        ast = self.parser.parse(entireFile)
+        self.renderer.render(ast, inputFile)
+
+    def convertSingle(self, inputFileName):
+        self.convert(inputFileName)
+
+    def convertMultiple(self, inputFileNames):
+        for i in inputFileNames:
+            self.convert(i)
+
+
+def convertMarkdown2Pdf(inputFileName, outputFileName):
+    parser = commonmark.Parser()
+
+    mdFile = open(inputFileName, "r", encoding="utf-8")
+    entireFile = mdFile.read()
+    ast = parser.parse(entireFile)
+
+    renderer = PdfRenderer(outputFileName)
+    renderer.render(ast, inputFileName.parent)
