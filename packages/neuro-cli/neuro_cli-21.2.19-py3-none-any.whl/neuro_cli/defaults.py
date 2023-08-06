bcbe512bf58d8e7f52f079bd926de4f58a715e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:24 +0000

    Add package neuro-cli/21.2.19/neuro_cli-21.2.19-py3-none-any.whl

---
+++ packages/neuro-cli/neuro_cli-21.2.19-py3-none-any.whl/neuro_cli/defaults.py
@@ -0,0 +1,8 @@
+JOB_GPU_NUMBER = 0
+JOB_GPU_MODEL = "nvidia-tesla-k80"
+JOB_CPU_NUMBER = 0.1
+JOB_MEMORY_AMOUNT = "1G"
+JOB_DEBUG_LOCAL_PORT = 31234
+JOB_SSH_USER = "root"
+
+GPU_MODELS = ("nvidia-tesla-k80", "nvidia-tesla-p4", "nvidia-tesla-v100")
