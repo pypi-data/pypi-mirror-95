8966a513a4c3b515a308d1ddd524976a176a8174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:34 +0000

    Add package jinja24doc/1.3.4/jinja24doc-1.3.4-py3-none-any.whl

---
+++ packages/jinja24doc/jinja24doc-1.3.4-py3-none-any.whl/jinja24doc/apidoc.py
@@ -0,0 +1,362 @@
+""" module api documentation reader """
+
+from inspect import signature, getdoc, getmembers, getsource, \
+    isfunction, isroutine, ismethod, isclass, ismodule, isbuiltin, \
+    ismethoddescriptor, isgetsetdescriptor
+from operator import itemgetter
+
+import sys
+import re
+
+# from Python 3.7 re.Pattern type instead of re._pattern_type
+try:
+    from re import Pattern  # type: ignore
+except ImportError:
+    from re import _pattern_type as Pattern  # type: ignore
+
+
+ordering = {'module':       (0, 0),
+            'submodule':    (1, 0),
+            'dependence':   (1, 1),
+            'class':        (2, 0),
+            'property':     (2, 1),
+            'descriptor':   (2, 2),
+            'method':       (2, 3),
+            'staticmethod': (2, 4),
+            'function':     (3, 0),
+            'variable':     (4, 0)}
+
+re_notlink = re.compile(r"(.*?)((<a .*?</a>)|$)", re.S)
+
+re_pep = re.compile(r"\bPEP ([0-9]{1,4})\b")            # pep link
+re_rfc = re.compile(r"\b(RFC )([0-9]+)\b")              # rfc link
+
+
+class Fn:
+    """Support class for naming in module.
+
+    >>> Fn('scope', 'name')
+    scope.name
+    """
+    # pylint: disable=too-few-public-methods
+    def __init__(self, scope, name):
+        self.scope = scope
+        self.name = name
+
+    def __repr__(self):
+        return self.scope+'.'+self.name
+
+
+def local_name(name):
+    """Returns striped name from its parent (module or class).
+
+    Typical use:
+
+        #!jinja
+        {{ local_name('MyClass.__init__') }} {# put __init__ to document #}
+    """
+    dot = name.rfind('.')
+    return name[dot+1:]
+
+
+def property_info(info, delimiter=' | '):
+    """Returns property info from tupple.
+
+    Input tuple must containts flags if property is writable, readable
+    and deletable.
+
+        #!jinja
+        {# return someone like this: WRITE | READ | DELETE #}
+        {{ property_info(info) }}
+    """
+    rv = []
+    if info[0]:
+        rv.append('READ')
+    if info[1]:
+        rv.append('WRITE')
+    if info[2]:
+        rv.append('DELETE')
+    return delimiter.join(rv)
+
+
+def key_doc(a):
+    """Return string sortable item via ordering keys."""
+    o = ordering[a[0]]
+    return str(o[0])+a[1].replace('.', str(o[1]))
+
+
+def _pep(obj):
+    return '<a href="http://www.python.org/dev/peps/pep-{0:04}/">PEP {0}</a>'.\
+        format(int(obj.groups()[0]))
+
+
+def pep_rfc(doc):
+    """Automatic create html links in doc from PEP and RFC notifications."""
+    doc = re_pep.sub(_pep, doc)
+    doc = re_rfc.sub(           # rfc
+        r'<a href="http://www.faqs.org/rfcs/rfc\2/">\1\2</a>', doc)
+    return doc
+
+
+def fix_default_fce(sig):
+    """Replace parameters signature - default function and methods with
+       own __repr__.
+
+    >>> from inspect import signature
+    >>> def foo(fn=print):
+    ...     pass
+    >>> str(fix_default_fce(signature(foo)))
+    '(fn=<built-in function print>)'
+    >>> def boo(fn=foo):
+    ...     pass
+    >>> str(fix_default_fce(signature(boo)))
+    '(fn=jinja24doc.apidoc.foo)'
+    """
+    # TODO: str -> str
+
+    parameters = []
+    for name in sig.parameters:
+        param = sig.parameters[name]
+        dflt = param.default
+        if isfunction(dflt):
+            param = param.replace(default=Fn(dflt.__module__, dflt.__name__))
+        elif ismethod(dflt):
+            param = param.replace(default=Fn(dflt.__objclass__, dflt.__name__))
+        parameters.append(param)
+    return sig.replace(parameters=parameters)
+
+
+class ApiDoc(object):
+
+    def __init__(self):
+        self.re_docs = None
+        self.api_url = ''
+        self.api_keywords = {}
+        self.paths = []
+        self.modules = []
+        self.encoding = 'utf-8'
+
+    def __keyword(self, obj):
+        groups = obj.groups()
+        key = groups[1]
+        args = self.api_keywords[key]
+        tmp = '<a href="%s#%s" title="%s">%s</a>' % \
+              (self.api_url, key, args, key)
+        return groups[0] + tmp + groups[2]
+
+    def __not_in_link(self, obj):
+        groups = obj.groups()
+        if not groups[0]:
+            return groups[1]
+        tmp = self.re_docs.sub(self.__keyword, groups[0])
+        return tmp + groups[1]
+
+    def linked_api(self, doc):
+        """Append link to api to html.
+
+        Function is called by Context.wiki or Context.rst.
+        """
+        if self.re_docs is not None:
+            return re_notlink.sub(self.__not_in_link, doc)
+        else:
+            return doc
+
+    def load_module(self, module):               # jinja function
+        """Get documentation of function, variables and classes from module.
+
+        Example:
+
+            #!jinja
+            {% set api = load_module('module') %}
+            {% for type, name, args, doc = api[0] %}
+                ...
+        """
+        try:
+            ml = module.split('.')
+            module = __import__(module)
+            for it in ml[1:]:                   # if module is submodule
+                module = module.__getattribute__(it)
+        except Exception as e:
+            sys.stderr.write(repr(e)+'\n')
+            return []
+
+        if '__file__' in module.__dict__:
+            _file = module.__file__
+        else:
+            _file = module.__name__
+
+        if _file in self.modules:
+            return []
+        else:
+            self.modules.append(_file)
+
+        try:
+            source = getsource(module)
+        except:
+            source = ''
+
+        dependences = set()
+        doc = []
+        modinfo = {'author': None, 'date': None, 'version': None}
+
+        for name, item in getmembers(module):
+            if isclass(item):                       # module classes
+                if module.__name__ != item.__module__:
+                    dependences.add(item.__module__)
+                    continue
+                # type, name, args, doc
+                doc.append(('class',
+                            item.__name__,
+                            None,
+                            getdoc(item) or ''))
+
+                for nm, it in getmembers(item):     # class members
+                    if ismethod(it) or isfunction(it) \
+                            or ismethoddescriptor(it) \
+                            or isgetsetdescriptor(it):
+                        try:
+                            mtype = 'descriptor'
+                            if isfunction(it):
+                                mtype = 'method'
+                            elif isinstance(it, staticmethod):
+                                mtype = 'staticmethod'
+                                it = it.__func__
+
+                            sig = fix_default_fce(signature(it))
+                            # type, name, args, doc
+                            doc.append((
+                                mtype,
+                                (item.__name__ + '.' +
+                                 it.__name__),
+                                str(sig),
+                                getdoc(it) or ''))
+                        except:
+                            pass
+
+                    elif isinstance(it, property):
+                        # type, name, property info, doc
+                        doc.append((
+                            'property',
+                            item.__name__ + '.' + nm,
+                            (bool(it.fget), bool(it.fset), bool(it.fdel)),
+                            getdoc(it) or ''))
+                    # elif isbuiltin(it):    <- __new__, __subclasshook__
+                    else:
+                        pass
+
+            # module function
+            elif isfunction(item) or isroutine(item):
+                if module.__name__ != item.__module__:
+                    item__module__ = item.__module__
+                    if item.__module__ is None:
+                        item__module__ = "None"
+                    elif item.__module__.startswith(module.__name__):
+                        item__module__ = \
+                            item__module__[len(module.__name__)+1:]
+                    # append to dependences
+                    dependences.add(item__module__)
+                    continue
+
+                if not isbuiltin(item):
+                    sig = fix_default_fce(signature(item))
+                    # type, name, args, doc
+                    doc.append(('function',
+                                name,
+                                str(sig),
+                                getdoc(item) or ''))
+                else:
+                    bdoc = getdoc(item) or ''
+                    if bdoc.find(name) == 0:
+                        eol = bdoc.find('\n')
+                        args = bdoc[len(name):eol]
+                        bdoc = bdoc[eol:].strip()
+                    else:
+                        args = ''
+
+                    # type, name, args, doc
+                    doc.append(('function', name, args, bdoc))
+
+            elif ismodule(item):
+                doc.append(('submodule', name, None, ''))
+            elif isinstance(item, Pattern):
+                if name[0:2] == '__' \
+                        or not re.search(r"\n%s\s*=" % name, source):
+                    continue
+                match = re.search(r"\n#\s*([^\n]*)\n%s\s*=" % name, source)
+                comment = match.groups()[0] if match else ''
+                # type, name, value, doc
+                doc.append(('variable',
+                            name,
+                            "(%s, %s)" % (item.pattern, item.flags),
+                            comment))
+            else:
+                if name[0:2] == '__' \
+                        or not re.search(r"\n%s\s*=" % name, source):
+                    if name in ('__author__', '__date__', '__version__'):
+                        modinfo[name.strip('_')] = repr(item)
+                    continue
+                # get last previous comment start with hash char (#)
+                match = re.search(r"\n#\s*([^\n]*)\n%s\s*=" % name, source)
+                comment = match.groups()[0] if match else ''
+                # type, name, value, doc
+                doc.append(('variable', name, repr(item), comment))
+        # endfor
+
+        doc.append(('module',
+                    module.__name__,
+                    (modinfo['author'], modinfo['date'],
+                     modinfo['version']),
+                    getdoc(module) or ''))
+
+        for name in dependences:
+            doc.append(('dependence', name, None, ''))
+
+        return sorted(doc, key=key_doc)
+    # enddef loadmodule
+
+    def keywords(self, api, api_url="",                 # jinja function
+                 types=('module', 'class', 'method', 'staticmethod',
+                        'descriptor', 'property', 'variable', 'function',
+                        'h1', 'h2', 'h3')):
+        """
+        Fill internal api_url variable from names of modules, classes,
+        functions, methods, variables or h1, h2 and h3 sections. With this,
+        wiki can create links to this functions  or classes.
+
+            #!jinja
+            {% set api = load_module('module') %}
+
+            {# create api rexex for from module where api will be on
+               module_api.html #}
+            {% do keywords(api, 'module_api.html') %}
+
+            {# create api rexex from module where api will be on same page #}
+            {% do keywords(api) %}
+
+            {# another way how to call keywords function without do keyword #}
+            {{ keywords(api) }}    {# keywords function return empty string #}
+
+        Nice to have: there could be nice, if will be arguments in title, so
+        mouseover event show some little detail of functions, methods or
+        variables.
+        """
+        api = sorted(api, key=itemgetter(1), reverse=True)
+        # TODO: dict for mapping names to parametres (for title in href)
+
+        self.re_docs = re.compile(
+            r"(\b)(%s)(\b)" % '|'.join(
+                (name.replace('.', r'\.')
+                    for type, name, args, doc in api if type in types)))
+        if self.re_docs.pattern == r"(\b)()(\b)":  # no keywrods found
+            self.re_docs = None
+        self.api_url = api_url
+
+        self.api_keywords = dict(
+            (name, (args if type != 'module' else None) or type)
+            for type, name, args, doc in api)
+
+        return ''
+    # enddef
+
+
+# endclass
---
+++ packages/jinja24doc/jinja24doc-1.3.4-py3-none-any.whl/jinja24doc/frontend.py
@@ -0,0 +1,227 @@
+"""Frontend command functions.
+
+Module containts some funtcions which are run by command tools.
+"""
+from traceback import format_exc
+from argparse import ArgumentParser
+from sys import path as python_path, version_info, stderr
+from os import path, getcwd
+
+from jinja24doc import __version__
+from jinja24doc.context import Context
+
+STYLESHEET = "../../../../share/jinja24doc/templates/"
+
+
+def build_parser(description):
+    """Create ArgumentParser instance with all suported options."""
+    parser = ArgumentParser(
+        description=description,
+        usage="%(prog)s [options] SOURCE [PATH[:PATH]]")
+
+    parser.add_argument(
+        "source", type=str,
+        help="python module or reStructured text file",
+        metavar="SOURCE")
+    parser.add_argument(
+        "path", default="", type=lambda s: [it for it in s.split(':') if it],
+        nargs='?', metavar="PATH[:PATH]",
+        help="template paths separated by colon")
+    parser.add_argument(
+        "-O", "--output", type=str, metavar="FILE",
+        help="write html to output file insead of stdout")
+    parser.add_argument(
+        "--encoding", default="utf-8", type=str, metavar="STRING",
+        help="write html to output file insead of stdout")
+    parser.add_argument(
+        "--stylesheet",  default="style.css",
+        type=lambda s: [it for it in s.split(',') if it],
+        metavar="FILE[,FILE]",
+        help="stylesheet file name (default style.css)")
+    parser.add_argument(
+        "--embed-stylesheet", action="store_true",
+        help="embed the stylesheet(s) in the output HTML file "
+             "(default: False)")
+    parser.add_argument(
+        "--system-message", action="store_true",
+        help="output system messages to output html (default: False)")
+    link_group = parser.add_mutually_exclusive_group()
+    link_group.add_argument(
+        "--link", default="link", type=str, metavar="STRING",
+        help="the `link' string (default: `link')")
+    link_group.add_argument(
+        "--no-link", action="store_const", const='', dest='link',
+        help="don't create the `link' link for sections.")
+    top_group = parser.add_mutually_exclusive_group()
+    top_group.add_argument(
+        "--top", default="top", type=str, metavar="STRING",
+        help="the `top' string (default: `top')")
+    top_group.add_argument(
+        "--no-top", action="store_const", const='', dest='top',
+        help="don't create the `top' link for sections.")
+    parser.add_argument(
+        "--traceback", action="store_true",
+        help="Enable Python tracebacks.")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true",
+        help="verbose mode")
+    parser.add_argument(
+        "--var", action='append',
+        help="Named variable, which is visible in templates. Could rewrite "
+             "other input variables for context.generate method.")
+    parser.add_argument(
+        '--version', action='version', version='%%(prog)s %s' % __version__)
+    return parser
+
+
+def verbose(args, parser):
+    """Print info message to stderr when verbose option is set."""
+    if args.verbose:
+        stderr.write("%s proccessing %s " % (parser.prog, args.source))
+        if args.output:
+            stderr.write("-> %s\n" % args.output)
+        else:
+            stderr.write("...\n")
+    # endif verbose
+
+
+def embed_stylesheet(args):
+    """Return stylesheets content readed from stylesheets."""
+    embed_stylesheet = ''
+    paths = args.path + [getcwd(),
+                         path.abspath(path.join(path.dirname(__file__),
+                                                STYLESHEET))]
+    for stylesheet in args.stylesheet:
+        stylesheet_path = None
+        for directory in paths:
+            tmp = path.join(directory, stylesheet)
+            if path.exists(tmp):
+                stylesheet_path = tmp
+                break
+        if stylesheet_path is None:
+            raise SystemExit("Stylesheet %s does not exist in path %s" %
+                             (stylesheet, ':'.join(paths)))
+        try:
+            with open(stylesheet_path, 'rt', encoding=args.encoding) as css:
+                embed_stylesheet += '<style source="%s">\n      ' % \
+                                    stylesheet
+                embed_stylesheet += css.read().replace('\n', "\n      ")
+                embed_stylesheet += "</style>\n"
+        except OSError as e:
+            raise SystemExit("Can't read stylesheet %s:\n\t%s" %
+                             (stylesheet_path, e))
+    return embed_stylesheet
+# enddef
+
+
+def process(ctx, source, file_types, **kwargs):
+    """Run Context.generate on right internal template dependend on file_types.
+    """
+    if source[-1] == '/':
+            source = source[:-1]
+    file_name, ext = path.splitext(path.basename(source))
+
+    if path.isdir(source) or ext == '.py':
+        python_path.insert(0, path.abspath(path.dirname(source)))
+        kwargs.update({'title': file_name,
+                       'module': file_name})
+        output = ctx.generate("module.html", **kwargs)
+    else:
+        ctx.paths.insert(0, path.abspath(path.dirname(source)))
+        kwargs['source'] = path.basename(source)
+        output = ctx.generate("text.html", ** kwargs)
+    return output
+
+
+def jinja_cmdline(description=''):
+    """Function called by jinja24doc command tool."""
+    parser = build_parser(description)
+    args = parser.parse_args()
+    try:
+        ctx = Context(args.path, args.encoding)
+        python_path.insert(0, getcwd())
+
+        source = args.source
+        # TODO: styles
+        verbose(args, parser)
+
+        kwargs = {
+            'link': args.link,
+            'top': args.top,
+            'system_message': args.system_message,
+            'encoding': args.encoding
+        }
+        if args.var:    # could be None
+            for kwarg in args.var:
+                key, val = kwarg.split('=', 1)
+                kwargs[key] = val
+
+        output = ctx.generate(source, **kwargs)
+
+        if args.output:
+            with open(args.output, 'w+', encoding=args.encoding) as o:
+                o.write(output)
+        else:
+            if not isinstance(output, str):
+                output = output.encode('utf-8')
+            print(output)
+    except SystemExit as e:
+        if args.traceback:
+            stderr.write("%s\n" % args)
+            stderr.write(format_exc())
+        parser.error(e)
+    except BaseException as e:
+        if args.traceback:
+            stderr.write("%s\n" % args)
+            stderr.write(format_exc())
+        parser.error('Error while proccessing %s' % e)
+# enddef
+
+
+def auto_cmdline(description='', formater='rst', file_types=['.txt']):
+    """Function called by rst24doc and wiki24doc command tools."""
+    parser = build_parser(description)
+    args = parser.parse_args()
+    try:
+        ctx = Context(args.path, args.encoding)
+
+        source = args.source
+        verbose(args, parser)
+
+        kwargs = {
+            'link': args.link,
+            'top': args.top,
+            'system_message': args.system_message,
+            'encoding': args.encoding,
+            'formater': getattr(ctx, formater)
+        }
+        if args.var:    # could be None
+            for kwarg in args.var:
+                key, val = kwarg.split('=', 1)
+                kwargs[key] = val
+
+        if args.embed_stylesheet:
+            kwargs['embed_stylesheet'] = embed_stylesheet(args)
+        else:
+            kwargs['stylesheets'] = args.stylesheet
+
+        output = process(ctx, source, file_types, **kwargs)
+
+        if args.output:
+            with open(args.output, 'w+', encoding=args.encoding) as o:
+                o.write(output)
+        else:
+            if not isinstance(output, str):
+                output = output.encode('utf-8')
+            print(output)
+    except SystemExit as e:
+        if args.traceback:
+            stderr.write("%s\n" % args)
+            stderr.write(format_exc())
+        parser.error(e)
+    except BaseException as e:
+        if args.traceback:
+            stderr.write("%s\n" % args)
+            stderr.write(format_exc())
+        parser.error('Error while proccessing %s' % e)
+# enddef
