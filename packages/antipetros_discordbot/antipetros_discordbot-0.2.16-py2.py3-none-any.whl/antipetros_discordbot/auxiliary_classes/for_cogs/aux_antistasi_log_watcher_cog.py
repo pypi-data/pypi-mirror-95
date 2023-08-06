21630f24c0d57bc3290f54587ae27a71e8372bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:28 +0000

    Add package antipetros_discordbot/0.2.16/antipetros_discordbot-0.2.16-py2.py3-none-any.whl

---
+++ packages/antipetros_discordbot/antipetros_discordbot-0.2.16-py2.py3-none-any.whl/antipetros_discordbot/auxiliary_classes/for_cogs/aux_antistasi_log_watcher_cog.py
@@ -0,0 +1,166 @@
+"""
+[summary]
+
+[extended_summary]
+"""
+
+# region [Imports]
+
+# * Standard Library Imports ------------------------------------------------------------------------------------------------------------------------------------>
+
+import gc
+import os
+import re
+import sys
+import json
+import lzma
+import time
+import queue
+import base64
+import pickle
+import random
+import shelve
+import shutil
+import asyncio
+import logging
+import sqlite3
+import platform
+import importlib
+import subprocess
+import unicodedata
+
+from io import BytesIO
+from abc import ABC, abstractmethod
+from copy import copy, deepcopy
+from enum import Enum, Flag, auto
+from time import time, sleep
+from pprint import pprint, pformat
+from string import Formatter, digits, printable, whitespace, punctuation, ascii_letters, ascii_lowercase, ascii_uppercase
+from timeit import Timer
+from typing import Union, Callable, Iterable
+from inspect import stack, getdoc, getmodule, getsource, getmembers, getmodulename, getsourcefile, getfullargspec, getsourcelines
+from zipfile import ZipFile
+from datetime import tzinfo, datetime, timezone, timedelta
+from tempfile import TemporaryDirectory
+from textwrap import TextWrapper, fill, wrap, dedent, indent, shorten
+from functools import wraps, partial, lru_cache, singledispatch, total_ordering
+from importlib import import_module, invalidate_caches
+from contextlib import contextmanager
+from statistics import mean, mode, stdev, median, variance, pvariance, harmonic_mean, median_grouped
+from collections import Counter, ChainMap, deque, namedtuple, defaultdict
+from urllib.parse import urlparse
+from importlib.util import find_spec, module_from_spec, spec_from_file_location
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from importlib.machinery import SourceFileLoader
+from dateparser import parse as date_parse
+from asyncio import get_event_loop
+# * Third Party Imports ----------------------------------------------------------------------------------------------------------------------------------------->
+
+# import discord
+
+# import requests
+
+# import pyperclip
+
+# import matplotlib.pyplot as plt
+
+# from bs4 import BeautifulSoup
+
+# from dotenv import load_dotenv
+
+# from discord import Embed, File
+
+# from discord.ext import commands, tasks
+
+# from github import Github, GithubException
+
+# from jinja2 import BaseLoader, Environment
+
+# from natsort import natsorted
+
+# from fuzzywuzzy import fuzz, process
+
+from webdav3.client import Client
+
+# * PyQt5 Imports ----------------------------------------------------------------------------------------------------------------------------------------------->
+
+# from PyQt5.QtGui import QFont, QIcon, QBrush, QColor, QCursor, QPixmap, QStandardItem, QRegExpValidator
+
+# from PyQt5.QtCore import (Qt, QRect, QSize, QObject, QRegExp, QThread, QMetaObject, QCoreApplication,
+#                           QFileSystemWatcher, QPropertyAnimation, QAbstractTableModel, pyqtSlot, pyqtSignal)
+
+# from PyQt5.QtWidgets import (QMenu, QFrame, QLabel, QAction, QDialog, QLayout, QWidget, QWizard, QMenuBar, QSpinBox, QCheckBox, QComboBox, QGroupBox, QLineEdit,
+#                              QListView, QCompleter, QStatusBar, QTableView, QTabWidget, QDockWidget, QFileDialog, QFormLayout, QGridLayout, QHBoxLayout,
+#                              QHeaderView, QListWidget, QMainWindow, QMessageBox, QPushButton, QSizePolicy, QSpacerItem, QToolButton, QVBoxLayout, QWizardPage,
+#                              QApplication, QButtonGroup, QRadioButton, QFontComboBox, QStackedWidget, QListWidgetItem, QSystemTrayIcon, QTreeWidgetItem,
+#                              QDialogButtonBox, QAbstractItemView, QCommandLinkButton, QAbstractScrollArea, QGraphicsOpacityEffect, QTreeWidgetItemIterator)
+
+
+# * Gid Imports ------------------------------------------------------------------------------------------------------------------------------------------------->
+
+import gidlogger as glog
+
+# from gidtools.gidfiles import (readit, clearit, readbin, writeit, loadjson, pickleit, writebin, pathmaker, writejson,
+#                                dir_change, linereadit, get_pickled, ext_splitter, appendwriteit, create_folder, from_dict_to_file)
+
+
+# * Local Imports ----------------------------------------------------------------------------------------------------------------------------------------------->
+from antipetros_discordbot.utility.gidtools_functions import bytes2human, pathmaker, readit, writeit
+# endregion[Imports]
+
+# region [TODO]
+
+
+# endregion [TODO]
+
+# region [AppUserData]
+
+
+# endregion [AppUserData]
+
+# region [Logging]
+
+log = glog.aux_logger(__name__)
+log.info(glog.imported(__name__))
+
+# endregion[Logging]
+
+# region [Constants]
+
+THIS_FILE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# endregion[Constants]
+
+
+class LogFile:
+
+    def __init__(self, client: Client, created, etag, isdir, modified, name, path, size):
+        self.client = client
+        self.created = date_parse(created) if created is not None else created
+        self.etag = etag.strip('"')
+        self.isdir = isdir
+        self.modified = date_parse(modified)
+        self.name = name if name is not None else os.path.basename(path)
+        self.path = '/'.join(path.split('/')[6:])
+        self.size = int(size)
+
+    @property
+    def size_pretty(self):
+        return bytes2human(self.size, annotate=True)
+
+    async def content(self):
+        with TemporaryDirectory() as tempdir:
+            new_path = pathmaker(tempdir, os.path.basename(self.path))
+            loop = get_event_loop()
+            await loop.run_in_executor(None, self.client.download_sync, self.path, new_path)
+
+            content = await loop.run_in_executor(None, readit, new_path)
+            return content
+
+            # region[Main_Exec]
+
+
+if __name__ == '__main__':
+    pass
+
+# endregion[Main_Exec]
