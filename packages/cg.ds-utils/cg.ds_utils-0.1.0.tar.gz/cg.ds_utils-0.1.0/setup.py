02d58b48f97c7ffa27eb199a9414c6cb608b093d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:50 +0000

    Add package cg.ds-utils/0.1.0/cg.ds_utils-0.1.0.tar.gz

---
+++ packages/cg.ds-utils/cg.ds_utils-0.1.0.tar.gz/cg.ds_utils-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cg']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'cg.ds-utils',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Seth Woodworth',
+    'author_email': 'seth@sethish.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
