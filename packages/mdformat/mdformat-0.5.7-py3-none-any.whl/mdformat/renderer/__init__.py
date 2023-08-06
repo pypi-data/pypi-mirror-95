8fd19c365aa15f7898fd1789ecb0c512cdae71d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:04 +0000

    Add package mdformat/0.5.7/mdformat-0.5.7-py3-none-any.whl

---
+++ packages/mdformat/mdformat-0.5.7-py3-none-any.whl/mdformat/renderer/__init__.py
@@ -0,0 +1,146 @@
+__all__ = ("MDRenderer", "MARKERS", "LOGGER")
+
+
+import logging
+from typing import Any, Mapping, Optional, Sequence
+
+from markdown_it.common.normalize_url import unescape_string
+from markdown_it.token import Token
+
+from mdformat.renderer import _container_renderers, _token_renderers
+from mdformat.renderer._util import MARKERS, removesuffix
+
+LOGGER = logging.getLogger(__name__)
+
+
+class MDRenderer:
+    """Markdown renderer.
+
+    A renderer class that outputs formatted Markdown. Compatible with
+    `markdown_it.MarkdownIt`.
+    """
+
+    __output__ = "md"
+
+    def __init__(self, parser: Any = None):
+        """__init__ must have `parser` parameter for markdown-it-py
+        compatibility."""
+
+    def render(  # noqa: C901
+        self,
+        tokens: Sequence[Token],
+        options: Mapping[str, Any],
+        env: dict,
+        *,
+        start: int = 0,
+        stop: Optional[int] = None,
+        finalize: bool = True,
+        _recursion_level: int = 0,
+    ) -> str:
+        """Takes token stream and generates Markdown.
+
+        Args:
+            tokens: A list of block tokens to render
+            options: Params of parser instance
+            env: Additional data from parsed input
+            start: Start rendering from this index
+            stop: Stop rendering before this index
+            finalize: replace markers and write references
+        """
+        assert _recursion_level in {
+            0,
+            1,
+        }, "There should be no more than one level of recursion in tokens"
+        text_stack = [""]
+
+        i = start - 1
+        if stop is None:
+            stop = len(tokens)
+        while (i + 1) < stop:
+            i += 1
+            token = tokens[i]
+
+            result = None
+
+            # first check plugins
+            for plugin in options.get("parser_extension", []):
+                output = plugin.render_token(self, tokens, i, options, env)
+                if output is not None:
+                    result, i = output
+                    break
+
+            if result is not None:
+                # if a plugin has handled the token,
+                # we assume that it does not need to open or close a container
+                text_stack[-1] = text_stack[-1] + result
+                continue
+
+            if token.type == "inline":
+                assert (
+                    token.children is not None
+                ), "inline token children can not be None"
+                # inline tokens require nested rendering
+                result = self.render(
+                    token.children,
+                    options,
+                    env,
+                    finalize=finalize,
+                    _recursion_level=_recursion_level + 1,
+                )
+            else:
+                # otherwise use a built-in renderer
+                tkn_renderer = getattr(
+                    _token_renderers, token.type, _token_renderers.default
+                )
+                result = tkn_renderer(tokens, i, options, env)
+
+            # If the token opens a new container block, create a new item for
+            # it in the text stack.
+            if token.nesting == 1:
+                text_stack.append(result)
+            # If the token doesn't change nesting, write in the immediate container
+            # block's stack item.
+            elif token.nesting == 0:
+                text_stack[-1] = text_stack[-1] + result
+            # If the token ends a container block, pop the block's stack item,
+            # format all markdown of that block, and append formatted markdown
+            # to the block's container's stack item.
+            else:  # token.nesting == -1
+                container_result = text_stack.pop() + result
+                container_renderer = getattr(
+                    _container_renderers, token.type, _container_renderers.default
+                )
+                container_result = container_renderer(
+                    container_result, tokens, i, options, env
+                )
+                text_stack[-1] = text_stack[-1] + container_result
+
+        rendered_content = text_stack.pop()
+        assert not text_stack, "Text stack should be empty before returning"
+
+        if finalize and not _recursion_level:
+            rendered_content = removesuffix(rendered_content, MARKERS.BLOCK_SEPARATOR)
+            rendered_content = rendered_content.replace(MARKERS.BLOCK_SEPARATOR, "\n\n")
+
+            if env.get("used_refs"):
+                rendered_content += "\n\n"
+                rendered_content += self._write_references(env)
+
+            rendered_content += "\n"
+
+        return rendered_content
+
+    @staticmethod
+    def _write_references(env: dict) -> str:
+        text = ""
+        for label in sorted(env.get("used_refs", [])):
+            ref = env["references"][label]
+            destination = ref["href"] if ref["href"] else "<>"
+            destination = unescape_string(destination)
+            item = f"[{label.lower()}]: {destination}"
+            title = ref["title"]
+            if title:
+                title = title.replace('"', '\\"')
+                item += f' "{title}"'
+            text += item + "\n"
+        return text.rstrip()
---
+++ packages/mdformat/mdformat-0.5.7-py3-none-any.whl/mdformat/renderer/_util.py
@@ -0,0 +1,187 @@
+import html.entities
+import re
+from typing import Callable, Sequence
+
+from markdown_it.token import Token
+
+from mdformat.renderer import _codepoints
+
+# Regex that finds character references.
+# The reference can be either
+#   1. decimal representation, e.g. &#11;
+#   2. hex representation, e.g. &#x1e;
+#   3. HTML5 entity reference, e.g. &nbsp;
+RE_CHAR_REFERENCE = re.compile(
+    "&("
+    "#[0-9]{1,7}"
+    "|#[Xx][0-9A-Fa-f]{1,6}"
+    "|" + "|".join({c.rstrip(";") for c in html.entities.html5}) + ");"
+)
+
+# key to indicate consecutive numbering of ordered lists
+CONSECUTIVE_KEY = "number"
+
+
+class MARKERS:
+    """A container for markers for unprocessed Markdown.
+
+    Temporary markers that can be used for markdown that is not yet
+    fully processed.
+    """
+
+    # Type ignore, because mypy thinks this is a method. This is not
+    # a method, just a function in the namespace.
+    def _make_autovalue_func() -> Callable[[], str]:  # type: ignore
+        """A closure to make a function that creates unique, even length MARKER
+        values.
+
+        "\x00" is invalid Markdown so a string that contains it can not
+        naturally exist in Markdown. This is why we prefix all markers
+        with it.
+        """
+        count = 0
+        max_val = 99
+        number_len = len(str(max_val))
+
+        def next_value() -> str:
+            nonlocal count
+            if count > max_val:
+                raise Exception("Tried to create too many marker values")
+            marker = "\x00" + str(count).rjust(number_len, "0")
+            count += 1
+            return marker
+
+        return next_value
+
+    _autovalue = _make_autovalue_func()
+
+    LIST_ITEM = _autovalue()
+    LIST_INDENT_FIRST_LINE = _autovalue()
+    LIST_INDENT = _autovalue()
+    # We add BLOCK_SEPARATOR instead of newlines at the end of every block.
+    # We convert it to newlines when closing
+    #   - a list item
+    #   - a list
+    #   - a blockquote
+    #   - the root document
+    BLOCK_SEPARATOR = _autovalue()
+
+
+def index_opening_token(tokens: Sequence[Token], closing_token_idx: int) -> int:
+    """Return index of an opening token.
+
+    Takes token stream and closing token index of a container block as
+    params. Returns index of the corresponding opening token.
+    """
+    closing_tkn = tokens[closing_token_idx]
+    assert closing_tkn.nesting == -1, "Cant find opening token for non closing token"
+    for i in reversed(range(closing_token_idx)):
+        opening_tkn_candidate = tokens[i]
+        if opening_tkn_candidate.level == closing_tkn.level:
+            return i
+    raise ValueError("Invalid token list. Opening token not found.")
+
+
+def find_opening_token(tokens: Sequence[Token], closing_token_idx: int) -> Token:
+    """Return an opening token.
+
+    Takes token stream and closing token index of a container block as
+    params. Returns the corresponding opening token.
+    """
+    opening_token_idx = index_opening_token(tokens, closing_token_idx)
+    return tokens[opening_token_idx]
+
+
+def is_tight_list(tokens: Sequence[Token], closing_token_idx: int) -> bool:
+    closing_tkn = tokens[closing_token_idx]
+    assert closing_tkn.type in {"bullet_list_close", "ordered_list_close"}
+
+    # The list has list items at level +1 so paragraphs in those list
+    # items must be at level +2
+    paragraph_level = closing_tkn.level + 2
+
+    for i in range(
+        index_opening_token(tokens, closing_token_idx) + 1, closing_token_idx
+    ):
+        if tokens[i].level != paragraph_level or tokens[i].type != "paragraph_open":
+            continue
+        is_tight = tokens[i].hidden
+        if not is_tight:
+            return False
+    return True
+
+
+def is_tight_list_item(tokens: Sequence[Token], closing_token_idx: int) -> bool:
+    list_item_closing_tkn = tokens[closing_token_idx]
+    assert list_item_closing_tkn.type == "list_item_close"
+
+    for i in range(closing_token_idx, len(tokens)):
+        if tokens[i].level < list_item_closing_tkn.level:
+            return is_tight_list(tokens, i)
+    raise ValueError("List closing token not found")
+
+
+def longest_consecutive_sequence(seq: str, char: str) -> int:
+    """Return length of the longest consecutive sequence of `char` characters
+    in string `seq`."""
+    assert len(char) == 1
+    longest = 0
+    current_streak = 0
+    for c in seq:
+        if c == char:
+            current_streak += 1
+        else:
+            current_streak = 0
+        if current_streak > longest:
+            longest = current_streak
+    return longest
+
+
+def is_text_inside_autolink(tokens: Sequence[Token], idx: int) -> bool:
+    assert tokens[idx].type == "text"
+    if idx == 0:
+        return False
+    previous_token = tokens[idx - 1]
+    return previous_token.type == "link_open" and previous_token.markup == "autolink"
+
+
+def removesuffix(string: str, suffix: str) -> str:
+    if suffix and string.endswith(suffix):
+        return string[: -len(suffix)]
+    return string
+
+
+def maybe_add_link_brackets(link: str) -> str:
+    """Surround URI with brackets if required by spec."""
+    if not link or (
+        _codepoints.ASCII_CTRL | _codepoints.ASCII_SPACE | {"(", ")"}
+    ).intersection(link):
+        return "<" + link + ">"
+    return link
+
+
+def get_list_marker_type(tokens: Sequence[Token], closing_token_idx: int) -> str:
+    if tokens[closing_token_idx].type == "bullet_list_close":
+        mode = "bullet"
+        primary_marker = "-"
+        secondary_marker = "*"
+    else:
+        mode = "ordered"
+        primary_marker = "."
+        secondary_marker = ")"
+    consecutive_lists_count = 1
+    current_closing_idx = closing_token_idx
+    while True:
+        assert current_closing_idx, "Closing token index can not be 0"
+        opening_token_idx = index_opening_token(tokens, current_closing_idx)
+        if opening_token_idx == 0:
+            return primary_marker if consecutive_lists_count % 2 else secondary_marker
+        prev_idx = opening_token_idx - 1
+        prev_type = tokens[prev_idx].type
+        if (mode == "bullet" and prev_type == "bullet_list_close") or (
+            mode == "ordered" and prev_type == "ordered_list_close"
+        ):
+            consecutive_lists_count += 1
+            current_closing_idx = prev_idx
+        else:
+            return primary_marker if consecutive_lists_count % 2 else secondary_marker
