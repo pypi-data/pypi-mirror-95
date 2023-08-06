1ac075b1ce22e6dd830661e4ef6f2f2b4ea97429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:46 +0000

    Add package DGSD/1.0.2/DGSD-1.0.2.tar.gz

---
+++ packages/DGSD/DGSD-1.0.2.tar.gz/DGSD-1.0.2/dgsd/__init__.py
@@ -0,0 +1,4 @@
+__author__ = 'Anwar Said'
+__email__ = 'anwar.said@itu.edu.pk'
+__version__ = 'v0.0.1'
+from DGSD.main import get_descriptor
---
+++ packages/DGSD/DGSD-1.0.2.tar.gz/DGSD-1.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='DGSD',
+    version='v1.0.2',
+    author='Anwar Said',
+    author_email='anwar.said@itu.edu.pk',
+    description='DGSD: Distributed Graph Representation via Graph Statistical Properties!',
+    long_description='long_description',
+    packages=['dgsd',],
+    url='https://github.com/Anwar-Said/DGSD',
+    download_url = 'https://github.com/Anwar-Said/DGSD/archive/v1.0.2.tar.gz',
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'networkx==2.5'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
