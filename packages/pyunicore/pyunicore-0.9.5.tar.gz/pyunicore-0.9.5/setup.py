329164a27dc23616d8490e4a829619668186a6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:39 +0000

    Add package pyunicore/0.9.5/pyunicore-0.9.5.tar.gz

---
+++ packages/pyunicore/pyunicore-0.9.5.tar.gz/pyunicore-0.9.5/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import versioneer
+
+long_description = open("README.md").read()
+install_requires = [
+    'PyJWT>=1.7',
+    'requests>=2.5'
+]
+
+setup(
+    name="pyunicore",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    author="Bernd Schuller",
+    author_email="b.schuller@fz-juelich.de",
+    description="Python helper functions for using the UNICORE REST API",
+    long_description=long_description,
+    install_requires=install_requires,
+    license="License :: OSI Approved :: BSD",
+    url='http://www.humanbrainproject.eu',
+)
