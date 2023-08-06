536956acc6d38307ca54291fbf7da81b151d2d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:06 +0000

    Add package django-data-browser/3.3.0/django-data-browser-3.3.0.tar.gz

---
+++ packages/django-data-browser/django-data-browser-3.3.0.tar.gz/django-data-browser-3.3.0/data_browser/fe_build/precache-manifest.7d25de35b92cce9fbb768977583433b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fbe85b9e84a607895e2f93b4dfafe8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "99d7f7fddd1d09a265b3",
+    "url": "./static/css/main.d7d51505.chunk.css"
+  },
+  {
+    "revision": "6b4e59d71c0d8ede848a",
+    "url": "./static/js/2.d314ae20.chunk.js"
+  },
+  {
+    "revision": "66486fd8359f40be208121c37c8c9200",
+    "url": "./static/js/2.d314ae20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d7f7fddd1d09a265b3",
+    "url": "./static/js/main.60ff8064.chunk.js"
+  },
+  {
+    "revision": "aba697584663a7cb794c",
+    "url": "./static/js/runtime-main.c3c9eabf.js"
+  }
+]);
