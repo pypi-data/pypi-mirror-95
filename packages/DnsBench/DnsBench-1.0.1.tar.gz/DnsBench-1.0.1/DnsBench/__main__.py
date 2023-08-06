c354c4ef1329c208eeacc3f9d28fa5432f6e8f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:39 +0000

    Add package DnsBench/1.0.1/DnsBench-1.0.1.tar.gz

---
+++ packages/DnsBench/DnsBench-1.0.1.tar.gz/DnsBench-1.0.1/DnsBench/__main__.py
@@ -0,0 +1,28 @@
+from . import dns_provider
+from . import dns_ip
+from . import check_dns
+import os
+import pyautogui
+import eel
+eel.init(os.path.join(os.path.dirname(os.path.realpath(__file__)),'web'))
+
+@eel.expose
+def dnscheck(cache):
+    data = check_dns.resolve_dns(cache)
+    dns = list(data.keys())
+    speed = list(data.values())
+    
+    # You can't suggest system DNS as best since it might be cached
+    best = "For your network {} is best DNS Server for use.".format(dns[1] if dns[0] == "Your system Dns" else dns[0])
+    recmd = "Second recommended is {}.".format(dns[2] if dns[0] == "Your system Dns" else dns[1])
+    return [dns,speed,best,recmd]
+
+def main():
+    try:
+        eel.start('index.html',block=True,size=pyautogui.size())
+    except OSError as e:
+        print(e)
+        exit()
+    
+if __name__ == '__main__':
+    main()
