5ecd800af22a05440beb3d338cb52f562973a185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:50 +0000

    Add package grin3/2.4/grin3-2.4-py3-none-any.whl

---
+++ packages/grin3/grin3-2.4-py3-none-any.whl/grin/__init__.py
@@ -0,0 +1,64 @@
+# Copyright (C) 2017-2021, Raffaele Salmaso <raffaele@salmaso.org>
+# Copyright (C) 2007, Enthought, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#  * Neither the name of Enthought, Inc. nor the names of its contributors may
+#    be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from .grin import get_filenames, get_grin_arg_parser  # noqa: F401
+from .grind import get_grind_arg_parser  # noqa: F401
+from .main import GrepText  # noqa: F401
+from .options import Options  # noqa: F401
+from .recognizer import GZIP_MAGIC, FileRecognizer, get_recognizer  # noqa: F401
+from .utils import get_line_offsets, get_regex, is_binary_string  # noqa: F401
+
+
+def get_version(version=None):
+    "Returns a PEP 386-compliant version number from VERSION."
+    assert len(version) == 5
+    assert version[3] in ("alpha", "beta", "rc", "final")
+
+    # Now build the two parts of the version number:
+    # main = X.Y[.Z]
+    # sub = .devN - for pre-alpha releases
+    #     | {a|b|c}N - for alpha, beta and rc releases
+
+    parts = 2 if version[2] == 0 else 3
+    main = ".".join(str(x) for x in version[:parts])
+
+    sub = ""
+
+    if version[3] != "final":
+        mapping = {"alpha": "a", "beta": "b", "rc": "c"}
+        sub = mapping[version[3]] + str(version[4])
+
+    return str(main + sub)
+
+
+# Constants
+VERSION = (2, 4, 0, "final", 0)
+__version__ = get_version(VERSION)
+__author__ = "Robert Kern"
+__author_email__ = "robert.kern@enthought.com"
+__maintainer__ = "Raffaele Salmaso"
+__maintainer_email__ = "raffaele@salmaso.org"
