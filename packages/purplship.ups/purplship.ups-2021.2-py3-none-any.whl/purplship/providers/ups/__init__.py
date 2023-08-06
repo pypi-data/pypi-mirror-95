11e26c093252efa961eb0da120fee7f7b8b5c007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:58 +0000

    Add package purplship.ups/2021.2/purplship.ups-2021.2-py3-none-any.whl

---
+++ packages/purplship.ups/purplship.ups-2021.2-py3-none-any.whl/purplship/providers/ups/__init__.py
@@ -0,0 +1,9 @@
+from purplship.providers.ups.utils import Settings
+from purplship.providers.ups.address import (
+    parse_address_validation_response,
+    address_validation_request
+)
+from purplship.providers.ups.tracking import (
+    parse_tracking_response,
+    tracking_request,
+)
