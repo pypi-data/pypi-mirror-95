3f27b88ea382cc07061cdd453f2f0435d709bccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:55 +0000

    Add package pymagnet/0.1.0/pymagnet-0.1.0-py3-none-any.whl

---
+++ packages/pymagnet/pymagnet-0.1.0-py3-none-any.whl/pymagnet/magnets/__init__.py
@@ -0,0 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https: // mozilla.org / MPL / 2.0 / .
+# Copyright 2021 Peter Dunne
+
+from ._magnet import *
+from ._magnet1 import *
+from ._magnet2 import *
+from ._magnet3 import *
+from ._fields import *
+from ._routines2 import *
+from ._routines3 import *
