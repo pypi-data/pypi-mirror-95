a9d0346eb870bb94321602110910cda4127b09c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:54 +0000

    Add package diaolinglib/0.0.1/diaolinglib-0.0.1.tar.gz

---
+++ packages/diaolinglib/diaolinglib-0.0.1.tar.gz/diaolinglib-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+    name='diaolinglib',
+    version='0.0.1',
+    author='Wenxin Chen',
+    author_email='minecraft_cwx@163.com',
+    description='凋零工作室研发的数学库,主要可以求面积(详情请看教程)',
+    ong_descripition_content_type="text/markdown",
+    url='https://github.com/',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
