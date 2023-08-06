bfe5acb0ba041340706d724d0497cc367c5e45c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:06 +0000

    Add package nose-launchable/0.2.4/nose_launchable-0.2.4-py3-none-any.whl

---
+++ packages/nose-launchable/nose_launchable-0.2.4-py3-none-any.whl/tests/test_manager.py
@@ -0,0 +1,131 @@
+import unittest
+from .resources.module0 import MockTestClass0
+from .resources.module1 import MockTestClass1
+from .resources.module2 import MockTestClass2
+
+from nose.suite import ContextSuite
+from nose_launchable.manager import parse_test, reorder, get_test_names, subset
+
+
+class TestManager(unittest.TestCase):
+    def setUp(self):
+        self.mock_suite1 = ContextSuite(tests=[ContextSuite(context=MockTestClass1), ContextSuite(context=MockTestClass2)])
+        self.mock_suite0 = ContextSuite(tests=[ContextSuite(context=MockTestClass0), self.mock_suite1])
+
+    def test_parse_test(self):
+        want = {
+            'type': 'tree',
+            'root':
+                {
+                    'type': 'treeNode',
+                    'id': str(id(self.mock_suite0)),
+                    'children':
+                        [
+                            {'type': 'testCaseNode', 'testName': 'tests/resources/module0.py'},
+                            {
+                                'type': 'treeNode',
+                                'id': str(id(self.mock_suite1)),
+                                'children':
+                                    [
+                                        {'type': 'testCaseNode', 'testName': 'tests/resources/module1.py'},
+                                        {'type': 'testCaseNode', 'testName': 'tests/resources/module2.py'}
+                                    ],
+                            }
+                        ],
+                },
+        }
+
+        got = parse_test(self.mock_suite0)
+
+        self.assertEqual(want, got)
+
+    def test_parse_test_empty(self):
+        suite = ContextSuite()
+
+        want = {'type': 'tree', 'root': {'type': 'treeNode', 'id': str(id(suite)), 'children': []}}
+        got = parse_test(suite)
+
+        self.assertEqual(want, got)
+
+    def test_get_test_names(self):
+        want = ['tests/resources/module0.py', 'tests/resources/module1.py', 'tests/resources/module2.py']
+
+        got = get_test_names(self.mock_suite0)
+
+        self.assertEqual(want, got)
+
+    def test_get_test_names_empty(self):
+        suite = ContextSuite()
+
+        want = []
+        got = get_test_names(suite)
+
+        self.assertEqual(want, got)
+
+    def test_reorder(self):
+        want = {
+            'type': 'tree',
+            'root':
+                {
+                    'type': 'treeNode',
+                    'id': str(id(self.mock_suite0)),
+                    'children':
+                        [
+                            {'type': 'testCaseNode', 'testName': 'tests/resources/module0.py'},
+                            {
+                                'type': 'treeNode',
+                                'id': str(id(self.mock_suite1)),
+                                'children':
+                                    [
+                                        {'type': 'testCaseNode', 'testName': 'tests/resources/module1.py'},
+                                        {'type': 'testCaseNode', 'testName': 'tests/resources/module2.py'}
+                                    ],
+                            }
+                        ],
+                },
+        }
+
+        reorder(self.mock_suite0, want)
+
+        got = parse_test(self.mock_suite0)
+        self.assertEqual(want, got)
+
+    def test_reorder_empty(self):
+        test = ContextSuite()
+
+        reorder(test, {'type': 'tree', 'root': {}})
+
+        want = {'type': 'tree', 'root': {'type': 'treeNode', 'id': str(id(test)), 'children': []}}
+        got = parse_test(test)
+
+        self.assertEqual(want, got)
+
+    def test_reorder_unexpected_type(self):
+        test = ContextSuite()
+
+        with self.assertRaises(RuntimeError):
+            reorder(test, {'type': 'graph', 'root': {}})
+
+    def test_subset_full(self):
+        want = ['tests/resources/module2.py', 'tests/resources/module1.py', 'tests/resources/module0.py']
+
+        subset(self.mock_suite0, {w: i for i, w in enumerate(want)})
+
+        got = get_test_names(self.mock_suite0)
+        self.assertEqual(want, got)
+
+    def test_subset_partial(self):
+        want = ['tests/resources/module2.py', 'tests/resources/module0.py']
+
+        subset(self.mock_suite0, {w: i for i, w in enumerate(want)})
+
+        got = get_test_names(self.mock_suite0)
+        self.assertEqual(want, got)
+
+    def test_subset_empty(self):
+        want = []
+
+        subset(self.mock_suite0, {w: i for i, w in enumerate(want)})
+
+        got = get_test_names(self.mock_suite0)
+        self.assertEqual(want, got)
