d3758e1adb8dc3abd299990bccf741e4c1567421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:43 +0000

    Add package ascii-magic/1.5.5/ascii_magic-1.5.5.tar.gz

---
+++ packages/ascii-magic/ascii_magic-1.5.5.tar.gz/ascii_magic-1.5.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ascii_magic",
+    version="1.5.5",
+    author="Leandro Barone",
+    author_email="web@leandrobarone.com.ar",
+    description="Converts pictures into ASCII art",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LeandroBarone/python-ascii_magic",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=['colorama', 'Pillow'],
+)
