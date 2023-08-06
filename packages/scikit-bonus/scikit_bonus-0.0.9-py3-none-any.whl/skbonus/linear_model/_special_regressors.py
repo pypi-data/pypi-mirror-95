d9b6a64e57758a359ce699c40a8394f3e642a1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:48 +0000

    Add package scikit-bonus/0.0.9/scikit_bonus-0.0.9-py3-none-any.whl

---
+++ packages/scikit-bonus/scikit_bonus-0.0.9-py3-none-any.whl/skbonus/linear_model/_special_regressors.py
@@ -0,0 +1,351 @@
+from abc import ABC, abstractmethod
+from typing import Callable, Optional, Tuple
+
+import numpy as np
+from scipy.optimize import minimize
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils.validation import (
+    _check_sample_weight,
+    check_array,
+    check_is_fitted,
+    check_X_y,
+)
+
+
+class BaseScipyMinimizeRegressor(BaseEstimator, RegressorMixin, ABC):
+    """
+    Base class for regressors relying on scipy's minimze method. Derive a class from this one and give it the function to be minimized.
+
+    Parameters
+    ----------
+    fit_intercept : bool, default=True
+        Whether to calculate the intercept for this model. If set
+        to False, no intercept will be used in calculations
+        (i.e. data is expected to be centered).
+
+    copy_X : bool, default=True
+        If True, X will be copied; else, it may be overwritten.
+
+    positive : bool, default=False
+        When set to True, forces the coefficients to be positive.
+
+    Attributes
+    ----------
+    coef_ : np.array of shape (n_features,)
+        Estimated coefficients of the model.
+
+    intercept_ : float
+        Independent term in the linear model. Set to 0.0 if fit_intercept = False.
+
+    Notes
+    -----
+    This implementation uses scipy.optimize.minimize, see
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> np.random.seed(0)
+    >>> X = np.random.randn(100, 4)
+    >>> y = X @ np.array([1, 2, 3, 4])
+    >>> l = LADRegression()
+    >>> l.fit(X, y)
+    LADRegression()
+    >>> l.coef_
+    array([1., 2., 3., 4.])
+
+    >>> import numpy as np
+    >>> np.random.seed(0)
+    >>> X = np.random.randn(100, 4)
+    >>> y = X @ np.array([-1, 2, -3, 4])
+    >>> l = LADRegression(positive=True)
+    >>> l.fit(X, y)
+    LADRegression(positive=True)
+    >>> l.coef_
+    array([0.        , 1.42423304, 0.        , 4.29789588])
+
+    """
+
+    def __init__(
+        self, fit_intercept: bool = True, copy_X: bool = True, positive: bool = False
+    ) -> None:
+        """Initialize."""
+        self.fit_intercept = fit_intercept
+        self.copy_X = copy_X
+        self.positive = positive
+
+    @abstractmethod
+    def _get_objective(
+        self, X: np.array, y: np.array, sample_weight: np.array
+    ) -> Tuple[Callable[[np.array], float], Callable[[np.array], np.array]]:
+        """
+        Produce the loss function to be minimized, and its gradient to speed up computations.
+
+        Parameters
+        ----------
+        X : np.array of shape (n_samples, n_features)
+            The training data.
+
+        y : np.array, 1-dimensional
+            The target values.
+
+        sample_weight : Optional[np.array], default=None
+            Individual weights for each sample.
+
+        Returns
+        -------
+        loss : Callable[[np.array], float]
+            The loss function to be minimized.
+
+        grad_loss : Callable[[np.array], np.array]
+            The gradient of the loss function. Speeds up finding the minimum.
+        """
+        pass
+
+    def fit(
+        self,
+        X: np.array,
+        y: np.array,
+        sample_weight: Optional[np.array] = None,
+    ) -> "BaseScipyMinimizeRegressor":
+        """
+        Fit the model using the L-BFGS-B algorithm.
+
+        Parameters
+        ----------
+        X : np.array of shape (n_samples, n_features)
+            The training data.
+
+        y : np.array, 1-dimensional
+            The target values.
+
+        sample_weight : Optional[np.array], default=None
+            Individual weights for each sample.
+
+        Returns
+        -------
+        Fitted regressor.
+        """
+        X_, grad_loss, loss = self._prepare_inputs(X, sample_weight, y)
+
+        d = X_.shape[1]
+        bounds = [(0, np.inf) for _ in range(d)] if self.positive else None
+        minimize_result = minimize(
+            loss,
+            x0=np.zeros(d),
+            bounds=bounds,
+            method="L-BFGS-B",
+            jac=grad_loss,
+            tol=1e-20,
+        )
+        self.convergence_status_ = minimize_result.message
+
+        if self.fit_intercept:
+            *self.coef_, self.intercept_ = minimize_result.x
+        else:
+            self.coef_ = minimize_result.x
+            self.intercept_ = 0.0
+
+        self.coef_ = np.array(self.coef_)
+
+        return self
+
+    def _prepare_inputs(self, X, sample_weight, y):
+        X, y = check_X_y(X, y)
+        sample_weight = _check_sample_weight(sample_weight, X)
+        n = X.shape[0]
+        if self.copy_X:
+            X_ = X.copy()
+        else:
+            X_ = X
+        if self.fit_intercept:
+            X_ = np.hstack([X_, np.ones(shape=(n, 1))])
+        loss, grad_loss = self._get_objective(X_, y, sample_weight)
+        return X_, grad_loss, loss
+
+    def predict(self, X: np.array) -> np.array:
+        """
+        Predict using the linear model.
+
+        Parameters
+        ----------
+        X : np.array, shape (n_samples, n_features)
+            Samples to get predictions of.
+
+        Returns
+        -------
+        y : np.array, shape (n_samples,)
+            The predicted values.
+        """
+        check_is_fitted(self)
+        X = check_array(X)
+
+        return X @ self.coef_ + self.intercept_
+
+
+class LADRegression(BaseScipyMinimizeRegressor):
+    """
+    Least absolute deviation Regression.
+
+    LADRegression fits a linear model to minimize the residual sum of absolute deviations between
+    the observed targets in the dataset, and the targets predicted by the linear approximation.
+
+    Compared to linear regression, this approach is robust to outliers. You can even
+    optimize for the lowest MAPE (Mean Average Percentage Error), if you pass in np.abs(1/y_train) for the
+    sample_weight keyword when fitting the regressor.
+
+    Parameters
+    ----------
+    fit_intercept : bool, default=True
+        Whether to calculate the intercept for this model. If set
+        to False, no intercept will be used in calculations
+        (i.e. data is expected to be centered).
+
+    copy_X : bool, default=True
+        If True, X will be copied; else, it may be overwritten.
+
+    positive : bool, default=False
+        When set to True, forces the coefficients to be positive.
+
+    Attributes
+    ----------
+    coef_ : np.array of shape (n_features,)
+        Estimated coefficients of the model.
+
+    intercept_ : float
+        Independent term in the linear model. Set to 0.0 if fit_intercept = False.
+
+    Notes
+    -----
+    This implementation uses scipy.optimize.minimize, see
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> np.random.seed(0)
+    >>> X = np.random.randn(100, 4)
+    >>> y = X @ np.array([1, 2, 3, 4])
+    >>> l = LADRegression()
+    >>> l.fit(X, y)
+    LADRegression()
+    >>> l.coef_
+    array([1., 2., 3., 4.])
+
+    >>> import numpy as np
+    >>> np.random.seed(0)
+    >>> X = np.random.randn(100, 4)
+    >>> y = X @ np.array([-1, 2, -3, 4])
+    >>> l = LADRegression(positive=True)
+    >>> l.fit(X, y)
+    LADRegression(positive=True)
+    >>> l.coef_
+    array([0.        , 1.42423304, 0.        , 4.29789588])
+
+    """
+
+    def _get_objective(
+        self, X: np.array, y: np.array, sample_weight: np.array
+    ) -> Tuple[Callable[[np.array], float], Callable[[np.array], np.array]]:
+        def mae_loss(params):
+            return np.mean(sample_weight * np.abs(y - X @ params))
+
+        def grad_mae_loss(params):
+            return -(sample_weight * np.sign(y - X @ params)) @ X / X.shape[0]
+
+        return mae_loss, grad_mae_loss
+
+
+class ImbalancedLinearRegression(BaseScipyMinimizeRegressor):
+    """
+    Linear regression where overestimating is `overestimation_punishment_factor` times worse than underestimating.
+
+    A value of `overestimation_punishment_factor=5` implies that overestimations by the model are penalized with a factor of 5
+    while underestimations have a default factor of 1.
+
+    ImbalancedLinearRegression fits a linear model to minimize the residual sum of squares between
+    the observed targets in the dataset, and the targets predicted by the linear approximation.
+    Compared to normal linear regression, this approach allows for a different treatment of over or under estimations.
+
+    Parameters
+    ----------
+    fit_intercept : bool, default=True
+        Whether to calculate the intercept for this model. If set
+        to False, no intercept will be used in calculations
+        (i.e. data is expected to be centered).
+
+    copy_X : bool, default=True
+        If True, X will be copied; else, it may be overwritten.
+
+    positive : bool, default=False
+        When set to True, forces the coefficients to be positive.
+
+    overestimation_punishment_factor : float, default=1
+        Factor to punish overestimations more (if the value is larger than 1) or less (if the value is between 0 and 1).
+
+    Attributes
+    ----------
+    coef_ : np.array of shape (n_features,)
+        Estimated coefficients of the model.
+
+    intercept_ : float
+        Independent term in the linear model. Set to 0.0 if fit_intercept = False.
+
+    Notes
+    -----
+    This implementation uses scipy.optimize.minimize, see
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> np.random.seed(0)
+    >>> X = np.random.randn(100, 4)
+    >>> y = X @ np.array([1, 2, 3, 4]) + 2*np.random.randn(100)
+    >>> over_bad = ImbalancedLinearRegression(overestimation_punishment_factor=50)
+    >>> over_bad.fit(X, y)
+    ImbalancedLinearRegression(overestimation_punishment_factor=50)
+    >>> over_bad.coef_
+    array([0.36267036, 1.39526844, 3.4247146 , 3.93679175])
+
+    >>> under_bad = ImbalancedLinearRegression(overestimation_punishment_factor=0.01)
+    >>> under_bad.fit(X, y)
+    ImbalancedLinearRegression(overestimation_punishment_factor=0.01)
+    >>> under_bad.coef_
+    array([0.73519586, 1.28698197, 2.61362614, 4.35989806])
+
+    """
+
+    def __init__(
+        self,
+        fit_intercept: bool = True,
+        copy_X: bool = True,
+        positive: bool = False,
+        overestimation_punishment_factor: float = 1.0,
+    ) -> None:
+        """Initialize."""
+        super().__init__(fit_intercept, copy_X, positive)
+        self.overestimation_punishment_factor = overestimation_punishment_factor
+
+    def _get_objective(
+        self, X: np.array, y: np.array, sample_weight: np.array
+    ) -> Tuple[Callable[[np.array], float], Callable[[np.array], np.array]]:
+        def imbalanced_loss(params):
+            return 0.5 * np.mean(
+                sample_weight
+                * np.where(X @ params > y, self.overestimation_punishment_factor, 1)
+                * np.square(y - X @ params)
+            )
+
+        def grad_imbalanced_loss(params):
+            return (
+                -(
+                    sample_weight
+                    * np.where(X @ params > y, self.overestimation_punishment_factor, 1)
+                    * (y - X @ params)
+                )
+                @ X
+                / X.shape[0]
+            )
+
+        return imbalanced_loss, grad_imbalanced_loss
---
+++ packages/scikit-bonus/scikit_bonus-0.0.9-py3-none-any.whl/skbonus/pandas/tests/test_time.py
@@ -0,0 +1,113 @@
+"""Test time classes."""
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from skbonus.pandas.time import (
+    CyclicalEncoder,
+    PowerTrend,
+    SimpleTimeFeatures,
+    SpecialDayBumps,
+)
+
+dfa = SimpleTimeFeatures(
+    second=True,
+    minute=True,
+    hour=True,
+    day_of_week=True,
+    day_of_month=True,
+    day_of_year=True,
+    week_of_month=True,
+    week_of_year=True,
+    month=True,
+    year=True,
+)
+
+sda = SpecialDayBumps(
+    name="black_friday_2018",
+    dates=["2018-11-23"],
+    frequency="d",
+    window=15,
+    p=1,
+    sig=1,
+)
+
+pta = PowerTrend(frequency="d", origin_date="2018-11-01")
+
+ce = CyclicalEncoder()
+
+non_continuous_input = pd.DataFrame(
+    {"A": ["a", "b", "c"], "B": [1, 2, 2], "C": [0, 1, 0]},
+    index=[
+        pd.Timestamp("1988-08-08 11:12:12"),
+        pd.Timestamp("2000-01-01 07:06:05"),
+        pd.Timestamp("1950-12-31"),
+    ],
+)
+
+continuous_input = pd.DataFrame(
+    {"data": range(60)}, index=pd.date_range(start="2018-11-01", periods=60)
+)
+
+minutes = pd.DataFrame({"minute": range(60)})
+
+sda_transformed = sda.fit_transform(continuous_input)
+ce_transformed = ce.fit_transform(minutes)
+
+
+@pytest.mark.parametrize(
+    "function, column_name, result",
+    [
+        (dfa._add_second, "second", [12, 5, 0]),
+        (dfa._add_minute, "minute", [12, 6, 0]),
+        (dfa._add_hour, "hour", [11, 7, 0]),
+        (dfa._add_day_of_week, "day_of_week", [1, 6, 7]),
+        (dfa._add_day_of_month, "day_of_month", [8, 1, 31]),
+        (dfa._add_day_of_year, "day_of_year", [221, 1, 365]),
+        (dfa._add_week_of_month, "week_of_month", [2, 1, 5]),
+        (dfa._add_week_of_year, "week_of_year", [32, 52, 52]),
+        (dfa._add_month, "month", [8, 1, 12]),
+        (dfa._add_year, "year", [1988, 2000, 1950]),
+    ],
+)
+def test_timefeaturesadder(function, column_name, result):
+    """Test SimpleTimeFeatures."""
+    assert function(non_continuous_input)[column_name].tolist() == result
+
+
+@pytest.mark.parametrize(
+    "date, value",
+    [
+        ("2018-11-23", 1.0),
+        ("2018-11-22", 6.065307e-01),
+        ("2018-11-24", 6.065307e-01),
+        ("2018-11-21", 0.1353352832366127),
+        ("2018-11-25", 0.1353352832366127),
+    ],
+)
+def test_specialeventsadder(date, value):
+    """Test the SpecialDayBumps."""
+    np.testing.assert_almost_equal(
+        sda_transformed.loc[date, "black_friday_2018"], value
+    )
+
+
+def test_powertrendadder_fit_transform():
+    """Test the PowerTrendAdder."""
+    assert pta.fit_transform(continuous_input).trend.tolist() == list(range(60))
+
+
+@pytest.mark.parametrize(
+    "index, value",
+    [
+        (0, 1.0),
+        (1, 9.945219e-01),
+        (2, 9.781476e-01),
+        (3, 9.510565e-01),
+        (4, 9.135455e-01),
+    ],
+)
+def test_cyclicalencoder(index, value):
+    """Test the CyclicalEncoder."""
+    np.testing.assert_almost_equal(ce_transformed.minute_cos.loc[index], value)
