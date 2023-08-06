1329f5130780fd6fef161eecec333ffa381fe3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:53 +0000

    Add package timecollider/0.0.1/timecollider-0.0.1.tar.gz

---
+++ packages/timecollider/timecollider-0.0.1.tar.gz/timecollider-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         ='timecollider',
+    version      ='0.0.1',
+    license      = 'MIT Licence',
+    py_modules   =['timecollider'],
+    author       ='Leon Tian',
+    author_email ='iamleon@yeah.net',
+    description  ='timecollider',
+    )
