1bc111feb2c3731a019a71c3c0a3fbec308957cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:14 +0000

    Add package tatsu.py/1.0.1/tatsu.py-1.0.1-py3-none-any.whl

---
+++ packages/tatsu.py/tatsu.py-1.0.1-py3-none-any.whl/tatsu/__init__.py
@@ -0,0 +1,14 @@
+import tatsu.wrapper
+import tatsu.data_structures
+import tatsu.error
+
+wrapper = tatsu.wrapper
+data_structures = tatsu.data_structures
+error = tatsu.error
+
+__all__ = [
+    'wrapper',
+]
+
+name = "tatsumaki.py"
+version = "0.3"
