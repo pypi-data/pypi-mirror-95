5bf3737a3c1290263a0050dacbcc35e549206f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:59 +0000

    Add package OverloadingFixed/1.1/OverloadingFixed-1.1.tar.gz

---
+++ packages/OverloadingFixed/OverloadingFixed-1.1.tar.gz/OverloadingFixed-1.1/setup.py
@@ -0,0 +1,25 @@
+from itertools import takewhile
+import os
+
+import setuptools
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    long_description = str.join('', takewhile(lambda l: not l.startswith('Installation'), f.readlines()[15:]))
+
+setuptools.setup(
+    name = 'OverloadingFixed',
+    version = '1.1',
+    author="L. Pham-Trong",
+    author_email="spam@lucasanss.xyz",
+    description="Function overloading for Python 3",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/toto112358/overloading.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
