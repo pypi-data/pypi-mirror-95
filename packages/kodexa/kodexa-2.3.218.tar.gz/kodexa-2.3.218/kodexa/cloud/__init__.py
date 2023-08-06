3da7d0e5f41e09d121dc33d421c5bc23006fce81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:24 +0000

    Add package kodexa/2.3.218/kodexa-2.3.218.tar.gz

---
+++ packages/kodexa/kodexa-2.3.218.tar.gz/kodexa-2.3.218/kodexa/cloud/__init__.py
@@ -0,0 +1,7 @@
+"""
+Kodexa Platform
+----------
+
+Provides out of the box integration with the Kodexa platform, enabling the universe of content services that are available
+"""
+from .kodexa import RemoteSession, RemotePipeline, RemoteAction, KodexaPlatform
