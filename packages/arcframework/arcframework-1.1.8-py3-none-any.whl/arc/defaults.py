74fbb6367c80369c8eff6cdbffc070374bae359a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:24 +0000

    Add package arcframework/1.1.8/arcframework-1.1.8-py3-none-any.whl

---
+++ packages/arcframework/arcframework-1.1.8-py3-none-any.whl/arc/defaults.py
@@ -0,0 +1,21 @@
+from arc.middleware import Middleware
+from arc.errors import AppException
+import os
+
+
+class DefaultMiddleware(Middleware):
+    def process_request(self, req):
+        print(f"\n[REQUEST][{req.method}] {req.url}")
+
+    def process_response(self, req, res):
+        print(f"\n[RESPONSE] {req.url}")
+
+
+class DefaultExceptionHandler:
+    def __init__(self, app):
+        self.app = app
+
+    def handle_error(self, request, response, error):
+        response.status_code = 500
+        response.body = self.app.custom_template(r"selfpages",
+                                                 "index.html", context={"error": str(error)})
