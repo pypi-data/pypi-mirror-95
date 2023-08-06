bf49cbbd7f9fdb8d8a4cc562435ed17c4b7ca9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:45 +0000

    Add package can-j1939/1.2.5/can-j1939-1.2.5.tar.gz

---
+++ packages/can-j1939/can-j1939-1.2.5.tar.gz/can-j1939-1.2.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages, Extension
+
+exec(open('j1939/version.py').read())
+
+description = open("README.rst").read()
+# Change links to stable documentation
+description = description.replace("/latest/", "/stable/")
+
+setup(
+    name="can-j1939",
+    url="https://github.com/juergenH87/python-can-j1939",
+    version=__version__,
+    packages=find_packages(exclude=['docs', 'examples']),
+    author="Juergen Heilgemeir",
+    description="SAE J1939 stack implementation",
+    keywords="CAN SAE J1939",
+    long_description=description,
+    long_description_content_type='text/x-rst',
+    license="MIT",
+    platforms=["any"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering"
+    ],
+    install_requires=["python-can>=3.0.0"],
+    include_package_data=True,
+
+    # Tests can be run using `python setup.py test`
+    test_suite="nose.collector",
+    tests_require=["nose"]
+)
