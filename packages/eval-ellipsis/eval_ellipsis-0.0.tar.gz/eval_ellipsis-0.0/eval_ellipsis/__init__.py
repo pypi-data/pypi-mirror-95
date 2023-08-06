4b6751c64a44b72abb5009ab6810d0979675aded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:50 +0000

    Add package eval-ellipsis/0.0/eval_ellipsis-0.0.tar.gz

---
+++ packages/eval-ellipsis/eval_ellipsis-0.0.tar.gz/eval_ellipsis-0.0/eval_ellipsis/__init__.py
@@ -0,0 +1,1 @@
+from eval_ellipsis.main import GlobalEllipsis
---
+++ packages/eval-ellipsis/eval_ellipsis-0.0.tar.gz/eval_ellipsis-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='eval_ellipsis',
+    version='0.0',
+    packages=['eval_ellipsis'],
+    license='MIT',
+    author='LDmitriy7',
+    author_email='ldm.work2019@gmail.com',
+    description='Evaluating global level Ellipsis to useful code.'
+)
