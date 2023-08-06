a759cc62b099e3175568d755268184121d1d5736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:35 +0000

    Add package dalle-pytorch/0.1.16/dalle-pytorch-0.1.16.tar.gz

---
+++ packages/dalle-pytorch/dalle-pytorch-0.1.16.tar.gz/dalle-pytorch-0.1.16/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'dalle-pytorch',
+  packages = find_packages(),
+  version = '0.1.16',
+  license='MIT',
+  description = 'DALL-E - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/dalle-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'transformers',
+    'text-to-image'
+  ],
+  install_requires=[
+    'axial_positional_embedding',
+    'einops>=0.3',
+    'torch>=1.6',
+    'torchvision',
+    'vector-quantize-pytorch'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
