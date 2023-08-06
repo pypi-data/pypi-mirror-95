826242f74297d2eb36c659fd31e6e8228014a9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:48 +0000

    Add package pytextspan/0.5.2/pytextspan-0.5.2-cp39-cp39-macosx_10_7_x86_64.whl

---
+++ packages/pytextspan/pytextspan-0.5.2-cp39-cp39-macosx_10_7_x86_64.whl/textspan/__init__.py
@@ -0,0 +1,19 @@
+from textspan.textspan import (
+    get_original_spans,
+    align_spans_by_mapping,
+    align_spans,
+    remove_span_overlaps,
+    remove_span_overlaps_idx,
+    lift_spans_index,
+    lift_span_index,
+)
+
+__all__ = [
+    "get_original_spans",
+    "align_spans_by_mapping",
+    "align_spans",
+    "remove_span_overlaps",
+    "remove_span_overlaps_idx",
+    "lift_span_index",
+    "lift_spans_index",
+]
