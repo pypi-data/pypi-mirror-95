2a4c803538767c73d5dca92a5381c1a36a812129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:32 +0000

    Add package intvalpy-test/0.0.11/intvalpy_test-0.0.11.tar.gz

---
+++ packages/intvalpy-test/intvalpy_test-0.0.11.tar.gz/intvalpy_test-0.0.11/intvalpy_test/linear/system_properties.py
@@ -0,0 +1,158 @@
+import numpy as np
+from scipy.optimize import minimize
+
+from intvalpy_test.MyClass import Interval
+from intvalpy_test.intoper import zeros
+
+
+def Uni(A, b, x=None, maxQ=False, x0=None, tol=1e-12, maxiter=1e3):
+    """
+    Вычисление распознающего функционала Uni.
+    В случае, если maxQ=True то находится максимум функционала.
+
+    Parameters:
+                A: Interval
+                    Матрица ИСЛАУ.
+
+                b: Interval
+                    Вектор правой части ИСЛАУ.
+
+    Optional Parameters:
+                x: float, array_like
+                    Точка в которой вычисляется распознающий функционал.
+                    По умолчанию x равен массиву из нулей.
+
+                maxQ: bool
+                    Если значение параметра равно True, то производится
+                    максимизация функционала.
+
+                x0: float, array_like
+                    Первоначальная догадка.
+
+                tol: float
+                    Погрешность для прекращения оптимизационного процесса.
+
+                maxiter: int
+                    Максимальное количество итераций.
+
+    Returns:
+                out: float, tuple
+                    Возвращается значение распознающего функционала в точке x.
+                    В случае, если maxQ=True, то возвращается кортеж, где
+                    первый элемент -- корректность завершения оптимизации,
+                    второй элемент -- точка оптимума,
+                    третий элемент -- значение функции в этой точке.
+    """
+
+    __uni = lambda x: min(b.rad - (b.mid - A @ x).mig)
+    __minus_uni = lambda x: -__uni(x)
+
+    if maxQ==False:
+        if x is None:
+            x = np.zeros(A.shape[1])
+        return __uni(x)
+    else:
+        from scipy.optimize import minimize
+
+        if x0 is None:
+            x0 = np.zeros(A.shape[1])+1
+        maximize = minimize(__minus_uni, x0, method='Nelder-Mead', tol=tol,
+                            options={'maxiter': maxiter})
+
+        return maximize.success, maximize.x, -maximize.fun
+
+
+def Tol(A, b, x=None, maxQ=False, x0=None, tol=1e-12, maxiter=1e3):
+    """
+    Вычисление распознающего функционала Tol.
+    В случае, если maxQ=True то находится максимум функционала.
+
+    Parameters:
+                A: Interval
+                    Матрица ИСЛАУ.
+
+                b: Interval
+                    Вектор правой части ИСЛАУ.
+
+    Optional Parameters:
+                x: float, array_like
+                    Точка в которой вычисляется распознающий функционал.
+                    По умолчанию x равен массиву из нулей.
+
+                maxQ: bool
+                    Если значение параметра равно True, то производится
+                    максимизация функционала.
+
+                x0: float, array_like
+                    Первоначальная догадка.
+
+                tol: float
+                    Погрешность для прекращения оптимизационного процесса.
+
+                maxiter: int
+                    Максимальное количество итераций.
+
+    Returns:
+                out: float, tuple
+                    Возвращается значение распознающего функционала в точке x.
+                    В случае, если maxQ=True, то возвращается кортеж, где
+                    первый элемент -- корректность завершения оптимизации,
+                    второй элемент -- точка оптимума,
+                    третий элемент -- значение функции в этой точке.
+    """
+
+    __tol = lambda x: min(b.rad - abs(b.mid - A @ x))
+    __minus_tol = lambda x: -__tol(x)
+
+    if maxQ==False:
+        if x is None:
+            x = np.zeros(A.shape[1])
+        return __tol(x)
+    else:
+        from scipy.optimize import minimize
+
+        if x0 is None:
+            x0 = np.zeros(A.shape[1])+1
+        maximize = minimize(__minus_tol, x0, method='Nelder-Mead', tol=tol,
+                            options={'maxiter': maxiter})
+
+        return maximize.success, maximize.x, -maximize.fun
+
+
+def ive(A, b, N=40):
+    """
+    Вычисление меры вариабельности оценки параметров.
+
+    Parameters:
+                A: Interval
+                    Матрица ИСЛАУ.
+
+                b: Interval
+                    Вектор правой части ИСЛАУ.
+
+    Optional Parameters:
+                N: int
+                    Количество угловых матриц для которых вычисляется обусловленность.
+
+    Returns:
+                out: float
+                    Возвращается мера вариабельности IVE.
+    """
+
+    success, _arg_max, _max = Tol(A, b, maxQ=True)
+    if not success:
+        print('Оптимизация функционала Tol завершена некорректно!')
+
+    _inf = A.a
+    _sup = A.b
+    cond = float('inf')
+    angle_A = np.zeros(A.shape, dtype='float64')
+    for _ in range(N):
+        for k in range(A.shape[0]):
+            for l in range(A.shape[1]):
+                angle_A[k, l] = np.random.choice([_inf[k,l], _sup[k,l]])
+        tmp = np.linalg.cond(angle_A)
+        cond = tmp if tmp<cond else cond
+
+    return np.sqrt(A.shape[1]) * _max * cond * \
+           (np.linalg.norm(_arg_max, ord=2)/np.sqrt(sum(abs(b)**2)))
---
+++ packages/intvalpy-test/intvalpy_test-0.0.11.tar.gz/intvalpy_test-0.0.11/setup.py
@@ -0,0 +1,36 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+os.environ['CVXOPT_BUILD_GLPK'] = '1'
+
+readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+with io.open(readme_file, mode='r', encoding='utf-8') as f:
+    README = f.read()
+
+INSTALL_REQUIRES = [
+    'matplotlib',
+    'numpy',
+    'scipy',
+    'lpsolvers'
+]
+
+setup(
+    name='intvalpy_test',
+    version='0.0.11',
+    description='Interval library in Python using classical interval ' + \
+                'arithmetic + Kahan division in some functions',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    keywords=[
+        'Interval arithmetic',
+        'inequality visualization',
+        'optimal solutions'
+    ],
+    author='Андросов Артем Станиславович',
+    author_email='artem.androsov@gmail.com',
+    url='https://github.com/AndrosovAS/intvalpy',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES
+)
