9315c6ad0574273ff78681d1c649c926322de6cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:59 +0000

    Add package apputils-discovery/2.0.2.dev20/apputils_discovery-2.0.2.dev20-py3-none-any.whl

---
+++ packages/apputils-discovery/apputils_discovery-2.0.2.dev20-py3-none-any.whl/apputils/discovery/__init__.py
@@ -0,0 +1,184 @@
+#  Licensed to the Apache Software Foundation (ASF) under one or more
+#  contributor license agreements.  See the NOTICE file distributed with
+#  this work for additional information regarding copyright ownership.
+#  The ASF licenses this file to You under the Apache License, Version 2.0
+#  (the "License"); you may not use this file except in compliance with
+#  the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Github: https://github.com/hapylestat/apputils
+#
+#
+
+import os
+import sys
+from typing import List, Iterable, Tuple
+
+from .arguments import CommandLineOptions
+from .commands import CommandMetaInfo, NoCommandException, CommandArgumentException, \
+  CommandModules, CommandModule, NotImplementedCommandException
+
+
+class CommandsDiscovery(object):
+  def __init__(self,
+               discovery_location_path: str,
+               module_class_path: str,
+               file_pattern: str = "",
+               module_main_fname: str = "__init__"):
+
+    self._discovery_location_path = discovery_location_path
+    self._module_main_fname = module_main_fname
+    self._file_pattern = file_pattern
+    self._options: CommandLineOptions = CommandLineOptions()
+
+    if os.path.isfile(self._discovery_location_path):
+      self._search_dir = os.path.dirname(os.path.abspath(self._discovery_location_path))
+    else:
+      self._search_dir = self._discovery_location_path
+
+    self._module_class_path = module_class_path
+    self._modules = CommandModules(entry_point=module_main_fname)
+
+  @property
+  def search_dir(self) -> str:
+    return self._search_dir
+
+  @classmethod
+  def __collect_modules(cls, path: str, pattern: str = "") -> Iterable[Tuple[str, str, bool, str]]:
+    exclude_list = ("pyc", "__init__.py", "__pycache__")
+    for name in os.listdir(path):
+      full_name = os.path.join(path, name)
+      is_dir = os.path.isdir(full_name)
+      if name.endswith(exclude_list):
+        continue
+
+      if pattern and not name.endswith(pattern):
+        continue
+
+      # command, file/dir name, is_dir, full_name
+      yield name.partition(".")[0], name, is_dir, full_name
+
+  def collect(self):
+    """
+    :rtype CommandsDiscovery
+    """
+    for command, name, is_dir, full_name in self.__collect_modules(self._search_dir, self._file_pattern):
+      if command in self._modules:
+        continue
+
+      sub_commands: List[str or Tuple[str, List[str]]] = []
+      if is_dir:  # currently supporting only 2 levels of sub-command
+        for _command, _name, _is_dir, _full_name in self.__collect_modules(full_name, self._file_pattern):
+          if _is_dir:
+            _sub = [item[0] for item in self.__collect_modules(_full_name, self._file_pattern) if not item[2]]
+            sub_commands.append((_command, _sub))
+          else:
+            sub_commands.append(_command)
+      self._modules.add(self._module_class_path, command, sub_commands)
+
+    return self
+
+  def __inject_help_command(self):
+    from .help import generate_help
+    meta = CommandMetaInfo("help", "this command")
+    meta.arg_builder \
+      .add_default_argument("subcommand", str, "name of the command to show help for", default="@") \
+      .add_default_argument("subcommand_command", str, "Command name of the subcommand", default="@")
+
+    def _print_help(subcommand: str, subcommand_command: str):
+      sys.stdout.write(generate_help(self._modules, self._options, subcommand, subcommand_command))
+
+    self._modules.inject(CommandModule(meta, "discovery", "__internal__", _print_help))
+
+  @property
+  def command_name(self) -> str or None:
+    return self._options.args[0] if self._options.args else None
+
+  @property
+  def command_arguments(self) -> List[str]:
+    return self._options.args[1:] if self._options.args else []
+
+  @property
+  def kwargs_name(self) -> List[str]:
+    return list(self._options.kwargs.keys())
+
+  def _get_command(self, injected_args: dict = None, fail_on_unknown: bool = False) -> CommandModule:
+    if not self._options.args:
+      raise NoCommandException(None, "No command passed, unable to continue")
+
+    command: CommandModule = self._modules[self._options.args[0]]  # [command name]
+    command_args = self._options.args[1:]
+
+    while command_args:
+      if command_args[0] not in command.subcommand_names:
+        break
+      command_name = command_args[0]
+      command_args = command_args[1:]
+
+      command = command.get_subcommand(command_name)  # [command name] {subcommand}
+
+    if command.meta_info.default_sub_command and \
+       command.meta_info.default_sub_command in command.subcommand_names:
+
+      command = command.get_subcommand(command.meta_info.default_sub_command)  # [command name] {subcommand} {sub}
+
+    inj_args = set(injected_args.keys()) if injected_args else set()
+    command.set_argument(command_args, self._options.kwargs, inj_args, fail_on_unknown)
+
+    return command
+
+  def execute_command(self, injected_args: dict = None):
+    try:
+      command = self._get_command(injected_args)
+      command.execute(injected_args)
+    except CommandArgumentException as e:
+      raise NoCommandException(None, f"Application arguments exception: {str(e)}\n")
+
+  async def execute_command_async(self, injected_args: dict = None):
+    try:
+      command = self._get_command()
+      await command.execute_async(injected_args)
+    except CommandArgumentException as e:
+      raise NoCommandException(None, f"Application arguments exception: {str(e)}\n")
+
+  def __get_modules_from_args(self) -> Tuple[str, str]:
+    args = self._options.args
+    if len(args) == 0:
+      return "", ""
+
+    cmd = self._modules.get_command_by_meta_name(args[0])
+    if cmd is None:
+      return "", ""
+
+    if len(args) == 1 or args[1] not in cmd.subcommand_names:
+      return cmd.name, ""
+
+    return cmd.name, args[1]
+
+  def start_application(self, kwargs: dict = None):
+    from .help import generate_help
+
+    # ToDO: add default command to be executed if no passed
+    self.__inject_help_command()
+    try:
+      command = self._get_command(injected_args=kwargs, fail_on_unknown=True)
+      command.execute(injected_args=kwargs)
+    except NotImplementedCommandException:
+      sys.stdout.write(generate_help(self._modules, self._options, *self.__get_modules_from_args()))
+      pass
+    except NoCommandException as e:
+      if e.command_name:
+        sys.stdout.write(generate_help(self._modules, self._options))
+      else:
+        sys.stdout.write("No command provided, use 'help' to check the list of available commands")
+      return
+    except CommandArgumentException as e:
+      sys.stdout.write(f"Application arguments exception: {str(e)}\n")
+      return
