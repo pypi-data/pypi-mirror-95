9bcd9f8371117270dba2e6a4f68edaa25fc28baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:38 +0000

    Add package tiledb-prompt-options/0.1.3/tiledb_prompt_options-0.1.3-py2.py3-none-any.whl

---
+++ packages/tiledb-prompt-options/tiledb_prompt_options-0.1.3-py2.py3-none-any.whl/tiledb_prompt_options/handlers.py
@@ -0,0 +1,28 @@
+import os
+import json
+
+from notebook.base.handlers import APIHandler
+from notebook.utils import url_path_join
+
+import tornado
+from tornado.web import StaticFileHandler
+
+
+class RouteHandler(APIHandler):
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def get(self):
+        TOKEN = os.getenv("TILEDB_REST_TOKEN")
+        self.finish(json.dumps({"token": TOKEN}))
+
+
+def setup_handlers(web_app, url_path):
+    host_pattern = ".*$"
+    base_url = web_app.settings["base_url"]
+
+    # Prepend the base_url so that it works in a jupyterhub setting
+    route_pattern = url_path_join(base_url, url_path)
+    handlers = [(route_pattern, RouteHandler)]
+    web_app.add_handlers(host_pattern, handlers)
