83380bacdc51206c94885ee44513626e49d27e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:27 +0000

    Add package dataclass-factory/2.10.1/dataclass_factory-2.10.1.tar.gz

---
+++ packages/dataclass-factory/dataclass_factory-2.10.1.tar.gz/dataclass_factory-2.10.1/dataclass_factory/generics.py
@@ -0,0 +1,58 @@
+from typing import Any, Dict, Generic, Type, get_type_hints
+
+from .type_detection import (
+    get_self_type_hints, is_generic, is_generic_concrete,
+)
+
+
+def fill_type_args(args: Dict[Type, Type], type_: Type) -> Type:
+    type_ = args.get(type_, type_)
+    if is_generic_concrete(type_):
+        type_args = tuple(
+            args.get(a, a) for a in type_.__args__
+        )
+        type_ = type_.__origin__[type_args]
+    return type_
+
+
+def resolve_hints(type_: Type):
+    if is_generic_concrete(type_):
+        return resolve_concrete_hints(type_)
+    if is_generic(type_):
+        return resolve_generic_hints(type_)
+    return get_type_hints(type_)
+
+
+def resolve_generic_hints(type_: Type):
+    if type_ is Generic:
+        return {}
+    res = {}
+    for base in reversed(type_.__orig_bases__):
+        base_hints = resolve_hints(base)
+        res.update(base_hints)
+    self_hints = get_self_type_hints(type_)
+    res.update(self_hints)
+    return res
+
+
+def resolve_concrete_hints(type_: Type):
+    if type_.__origin__ is Generic:
+        return {}
+    hints = resolve_generic_hints(type_.__origin__)
+    args = dict(zip(type_.__origin__.__parameters__, type_.__args__))
+    res = {
+        name: fill_type_args(args, type_)
+        for name, type_ in hints.items()
+    }
+    return res
+
+
+def resolve_init_hints(type_: Any):
+    if not is_generic_concrete(type_):
+        return get_type_hints(type_.__init__)
+    hints = get_type_hints(type_.__origin__.__init__)
+    args = dict(zip(type_.__self__.__origin__.__parameters__, type_.__self__.__args__))
+    return {
+        name: fill_type_args(args, type_)
+        for name, type_ in hints.items()
+    }
---
+++ packages/dataclass-factory/dataclass_factory-2.10.1.tar.gz/dataclass_factory-2.10.1/dataclass_factory/schema_helpers.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from uuid import UUID
+
+from .common import T
+from .factory import StackedFactory
+from .schema import Schema
+
+try:
+    isotime_schema = Schema(
+        parser=datetime.fromisoformat,  # type: ignore
+        serializer=datetime.isoformat,
+    )
+except AttributeError:
+    pass
+
+
+def type_checker(value, field="type", pre_parse=None):
+    def check_type(data):
+        if value != data[field]:
+            raise ValueError
+        if pre_parse:
+            return pre_parse(data)
+        return data
+
+    return check_type
+
+
+unixtime_schema = Schema(
+    parser=datetime.fromtimestamp,
+    serializer=datetime.timestamp,
+)
+
+uuid_schema = Schema(
+    serializer=UUID.__str__,
+    parser=UUID,
+)
+
+
+def _stub(data: T) -> T:
+    return data
+
+
+stub_schema = Schema(
+    parser=_stub,
+    serializer=_stub,
+)
+
+
+class ClsCheckSchema(Schema[T]):
+    serializer = _stub
+
+    def get_parser(self, cls, stacked_factory: StackedFactory, debug_path: bool):  # type: ignore
+        def cls_check_parser(data):
+            if isinstance(data, cls):
+                return data
+            raise TypeError(f'Argument must be {cls.__name__}')
+
+        return cls_check_parser
