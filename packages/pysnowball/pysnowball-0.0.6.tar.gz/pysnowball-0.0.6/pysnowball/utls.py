6f560f71e03baf7c86db76239d05a4d724b7eca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:57 +0000

    Add package pysnowball/0.0.6/pysnowball-0.0.6.tar.gz

---
+++ packages/pysnowball/pysnowball-0.0.6.tar.gz/pysnowball-0.0.6/pysnowball/utls.py
@@ -0,0 +1,47 @@
+import requests
+import json
+
+import pysnowball.cons as cons
+import pysnowball.token as token
+
+def fetch(url):
+    HEADERS = {'Host': 'stock.xueqiu.com',
+               'Accept': 'application/json',
+               'Cookie': token.get_token(),
+               'User-Agent': 'Xueqiu iPhone 11.8',
+               'Accept-Language': 'zh-Hans-CN;q=1, ja-JP;q=0.9',
+               'Accept-Encoding': 'br, gzip, deflate',
+               'Connection': 'keep-alive'}
+
+    response = requests.get(url,headers=HEADERS)
+
+    # print(url)
+    # print(HEADERS)
+    # print(response)
+    # print(response.content)
+
+    if response.status_code != 200:
+        raise Exception(response.content)
+
+    return json.loads(response.content)
+
+
+def fetch_without_token(url):
+    HEADERS = {'Host': 'stock.xueqiu.com',
+               'Accept': 'application/json',
+               'User-Agent': 'Xueqiu iPhone 11.8',
+               'Accept-Language': 'zh-Hans-CN;q=1, ja-JP;q=0.9',
+               'Accept-Encoding': 'br, gzip, deflate',
+               'Connection': 'keep-alive'}
+
+    response = requests.get(url, headers=HEADERS)
+
+    # print(url)
+    # print(HEADERS)
+    # print(response)
+    # print(response.content)
+
+    if response.status_code != 200:
+        raise Exception(response.content)
+
+    return json.loads(response.content)
