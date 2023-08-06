144ee63d94e110a17c6ffaffa15a3a285804a0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:53 +0000

    Add package bpf4/0.9.0/bpf4-0.9.0.tar.gz

---
+++ packages/bpf4/bpf4-0.9.0.tar.gz/bpf4-0.9.0/bpf4/pyinterp.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+from . import core
+import numpy as np
+OUTBOUND_CACHE = 1
+
+
+class _NumpyInterp(core._FunctionWrap_Object):
+    
+    def __init__(self, interpolator, xs, ys):
+        self._interpolator = interpolator
+        super(_NumpyInterp, self).__init__(interpolator, (xs[0], xs[-1]))
+        self.xs, self.ys = xs, ys
+
+    def mapn_between(self, n, x0, x1, out=None):
+        xs = np.linspace(x0, x1, n)
+        out0 = self._interpolator(xs)
+        if out is not None:
+            out[...] = out0
+            return out
+        return out0
+    
+    def map(self, xs, out=None):
+        if isinstance(xs, int):
+            return self.mapn_between(xs, self.x0, self.x1)
+        return self._interpolator(xs)
+    
+    def points(self):
+        return [self.xs, self.ys]
+    
+    def __getstate__(self):
+        return self.xs, self.ys
+    
+    def _slice(self, x0, x1):
+        return core._BpfCrop_new(self, x0, x1, OUTBOUND_CACHE)
+
+
+class Pchip(_NumpyInterp):
+    """
+    Monotonic Piecewise Cubit Hermite interpolation, similar to matlab's pchip
+    """
+    def __init__(self, xs: np.ndarray, ys: np.ndarray):
+        # from . import wafointerpol
+        # interpolator = wafointerpol.Pchip(xs, ys)
+        from scipy.interpolate import PchipInterpolator
+        interpolator = PchipInterpolator(xs, ys)
+        super().__init__(interpolator, xs, ys)
+
---
+++ packages/bpf4/bpf4-0.9.0.tar.gz/bpf4-0.9.0/setup.py
@@ -0,0 +1,72 @@
+"""
+BPF
+
+A package implementing piece-wise interpolation functions in Cython
+"""
+from __future__ import print_function
+from setuptools import setup, Extension
+import os
+import sys
+
+
+def get_version():
+    d = {}
+    with open("bpf4/version.py") as f:    
+        code = f.read()
+    exec(code, d)        
+    version = d.get('__version__', (0, 0, 0))
+    return ("%d.%d.%d" % version).strip()
+
+long_description = open('README.md').read()
+    
+compiler_args = [] 
+versionstr = get_version()
+
+if sys.platform == 'windows':
+    compiler_args += ["-march=i686"]  # This is needed in windows to compile cleanly
+
+
+class get_numpy_include(str):
+
+    def __str__(self):
+        import numpy
+        return numpy.get_include()
+
+
+def get_includes():
+    return ['bpf4', get_numpy_include()]
+
+
+setup(
+    name = "bpf4",
+    python_requires='>=3.6',
+    setup_requires = [
+        'setuptools>=18.0', 
+        'cython>=0.21', 
+        'numpy>=1.8',
+    ],
+    ext_modules = [
+        Extension("bpf4.core", 
+        sources=["bpf4/core.pyx"], 
+        include_dirs=get_includes(), 
+        extra_link_args=compiler_args, 
+        compiler_args=compiler_args),  
+    ],
+    include_dirs = get_includes(),
+    install_requires = ['numpy>=1.8', 'matplotlib', 'scipy'],
+    packages = ['bpf4'],
+
+    # metadata
+    version          = versionstr,
+    url              = 'https://github.com/gesellkammer/bpf4',
+    download_url     = 'https://github.com/gesellkammer/bpf4', 
+    author           = 'eduardo moguillansky',
+    author_email     = 'eduardo.moguillansky@gmail.com',
+    maintainer       = '',
+    maintainer_email = '',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    description = "Peace-wise interpolation and lazy evaluation in cython"
+)
+
+print("Version: %s" % versionstr)
