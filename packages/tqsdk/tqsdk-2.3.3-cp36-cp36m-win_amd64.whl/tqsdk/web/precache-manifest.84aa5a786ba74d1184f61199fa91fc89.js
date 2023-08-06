498fe2ffccad0eb943484817cb477543594508b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:27 +0000

    Add package tqsdk/2.3.3/tqsdk-2.3.3-cp36-cp36m-win_amd64.whl

---
+++ packages/tqsdk/tqsdk-2.3.3-cp36-cp36m-win_amd64.whl/tqsdk/web/precache-manifest.84aa5a786ba74d1184f61199fa91fc89.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24916e3776bf3c6191bb8e6103bd1727",
+    "url": "web/css/app.78be4739.css"
+  },
+  {
+    "revision": "9e9b0f141d6ba005c001",
+    "url": "web/css/app.868494d8.css"
+  },
+  {
+    "revision": "9ee2d6b3d0496240f9bf",
+    "url": "web/css/chunk-vendors.297c18d6.css"
+  },
+  {
+    "revision": "6c2d8504517dcc8aaa229919fdd9ea3c",
+    "url": "web/d3.min.js"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "web/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "web/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "web/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "web/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "1ad6ec5ee49d9f950a1d2c305d5f05eb",
+    "url": "web/index.html"
+  },
+  {
+    "revision": "5469fec97d87a56937e5c2dd2172a30d",
+    "url": "web/js/app.129c29b2.js"
+  },
+  {
+    "revision": "9e9b0f141d6ba005c001",
+    "url": "web/js/app.a351c666.js"
+  },
+  {
+    "revision": "41e66ab92ba42f031547092d03f27649",
+    "url": "web/js/chunk-vendors.3888dee2.js"
+  },
+  {
+    "revision": "9ee2d6b3d0496240f9bf",
+    "url": "web/js/chunk-vendors.95880041.js"
+  },
+  {
+    "revision": "260485a0902e994592bfde307cd2874e",
+    "url": "web/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "web/robots.txt"
+  }
+]);
