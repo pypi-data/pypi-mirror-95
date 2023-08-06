a41cdb8fed8bcaab63f7ea470e67a73d095f0377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:16 +0000

    Add package pip2spack/1.3a1/pip2spack-1.3a1.tar.gz

---
+++ packages/pip2spack/pip2spack-1.3a1.tar.gz/pip2spack-1.3a1/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("../VERSION", "r") as vr:
+    version_number = vr.read()
+
+with open("../requirements.txt", "r") as req:
+    requirements = []
+    for l in req.readlines():
+        requirements.append(l.rstrip())
+
+setuptools.setup(
+    name="pip2spack",
+    version=version_number,
+    author="Nex Sabre",
+    author_email="nexsabre@protonmail.com",
+    description="Automatically create and update a spack package base on the pypi.org information",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NexSabre/pip2spack",
+    packages=setuptools.find_packages(),
+    package_data={'pip2spack': ['templates/*.j2']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'pip2spack = pip2spack.main.main:main'
+        ],
+    },
+)
