e5b7b6dac23ee307c287c4c67639cc756b6bae77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:55 +0000

    Add package maskpass/0.3.3/maskpass-0.3.3.tar.gz

---
+++ packages/maskpass/maskpass-0.3.3.tar.gz/maskpass-0.3.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="maskpass",
+    version="0.3.3",
+    description="getpass alternative with masking, Spyder support and additional features ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FuturisticGoo/maskpass",
+    author="Aman Anifer",
+    author_email="fgoo.edu@hash.fyi",
+    license="MIT",
+    keywords=["password","cryptography","getpass","getpass3","mask","spyder","input","pynput"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=["maskpass"],
+    include_package_data=True,
+    install_requires=["pynput"],
+    
+)
