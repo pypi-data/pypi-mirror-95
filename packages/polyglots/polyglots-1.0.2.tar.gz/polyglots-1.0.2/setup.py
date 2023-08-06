5904b93cb3f87a98ea891a97f2559f441d5d964f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:47 +0000

    Add package polyglots/1.0.2/polyglots-1.0.2.tar.gz

---
+++ packages/polyglots/polyglots-1.0.2.tar.gz/polyglots-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+setup(
+    name='polyglots',
+    version='1.0.2',
+    description='Create polyglots for different file types',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/donno2048/polyglots',
+    packages=find_packages(),
+    license='MIT',
+    author='Elisha Hollander',
+    classifiers=['Programming Language :: Python :: 3'],
+    entry_points={ 'console_scripts': [ 'polyglots=polyglots.__main__:main' ] }
+)
