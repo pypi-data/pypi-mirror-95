805dfb5f5f4554adb420a1d02174b8518f2c4fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:28 +0000

    Add package pysqream/3.1.3/pysqream-3.1.3.tar.gz

---
+++ packages/pysqream/pysqream-3.1.3.tar.gz/pysqream-3.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup_params = dict(
+    name =          'pysqream',
+    version =       '3.1.3',
+    description =   'DB-API connector for SQream DB', 
+    long_description = long_description,
+    url = "https://github.com/SQream/pysqream",
+    author = "SQream",
+    author_email = "info@sqream.com",
+    packages = ['pysqream'], 
+    classifiers = [
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    keywords = 'database db-api sqream sqreamdb',
+    python_requires = '>=3.6.5'
+)
+
+if __name__ == '__main__':
+    setup(**setup_params)
