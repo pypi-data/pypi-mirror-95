976a0b45783dc69bf77c413b0175cd0a8cfafe36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:38 +0000

    Add package upbit-wrapper/0.0.1/upbit_wrapper-0.0.1-py3-none-any.whl

---
+++ packages/upbit-wrapper/upbit_wrapper-0.0.1-py3-none-any.whl/upbit/__init__.py
@@ -0,0 +1,7 @@
+"""
+Package
+"""
+from upbit import upbit
+
+__all__ = ['upbit']
+__version = "0.0.1"
---
+++ packages/upbit-wrapper/upbit_wrapper-0.0.1-py3-none-any.whl/upbit_wrapper/__init__.py
@@ -0,0 +1,8 @@
+"""
+Package
+"""
+from upbit_wrapper.upbit import Upbit
+from upbit_wrapper.upbit_websocket import UpbitWebSocket
+
+__all__ = ['Upbit','UpbitWebSocket']
+__version = "0.0.1"
