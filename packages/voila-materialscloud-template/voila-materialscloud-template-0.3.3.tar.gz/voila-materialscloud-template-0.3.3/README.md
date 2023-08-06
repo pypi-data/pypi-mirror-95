5d1970cc3b873a4ebe9ca6584b2897ba88f08023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:45 +0000

    Add package voila-materialscloud-template/0.3.3/voila-materialscloud-template-0.3.3.tar.gz

---
+++ packages/voila-materialscloud-template/voila-materialscloud-template-0.3.3.tar.gz/voila-materialscloud-template-0.3.3/README.md
@@ -0,0 +1,28 @@
+# Voilà template for Materials Cloud  
+
+This is the Voilà template for the Materials Cloud project.
+
+It implements the standard Materials Cloud header and CSS.
+
+The template is called `materialscloud` and can be invoked either by using the `--template=materialscloud` `voila` CLI option, setting it explicitly in a Notebook's metadata or in a Jupyter config file.
+See [the Voilà documentation](https://voila.readthedocs.io/en/stable/customize.html#controlling-the-nbconvert-template) on how to invoke templates for more information.
+
+## Try it with Binder!
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/materialscloud-org/voila-materialscloud-template/develop?urlpath=%2Fvoila%2Frender%2Fexample-notebooks%2Fexample.ipynb)
+
+## Installation
+
+```bash
+pip install voila-materialscloud-template
+```
+
+## Usage
+
+```bash
+voila --template=materialscloud example.ipynb
+```
+
+## License
+
+This repository and all files in it are licensed under the [BSD license](LICENSE), copyright (c) by Voilà contributors.
---
+++ packages/voila-materialscloud-template/voila-materialscloud-template-0.3.3.tar.gz/voila-materialscloud-template-0.3.3/setup.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from subprocess import check_call
+
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+
+def post_install():
+    """Copy materialscloud templates' files to Jupyter config location"""
+    for template in ("materialscloud", "materialscloud-iframe"):
+        check_call(['./copy_voila_template.py', template], cwd=Path(__file__).parent.resolve())
+
+class ExtraInstall(install):
+    """Post-installation for installation (install) mode."""
+
+    def run(self):
+        """Install application and copy template files to Jupyter location afterwards."""
+        install.run(self)
+        post_install()
+
+
+class ExtraDevelop(develop):
+    """Post-installation for editable (develop) mode."""
+
+    def run(self):
+        """Install application and copy template files to Jupyter location afterwards."""
+        develop.run(self)
+        post_install()
+
+
+setup(
+    name='voila-materialscloud-template',
+    version='0.3.3',
+    packages=[],
+    author='Dou Du and Casper Welzel Andersen',
+    author_email='dou.du@epfl.ch',
+    url='https://github.com/materialscloud-org/voila-materialscloud-template',
+    license='BSD',
+    description='Voilà template for Materials Cloud that implements the Materials Cloud header and CSS.',
+    long_description=open(Path(__file__).parent.joinpath('README.md').resolve()).read(),
+    long_description_content_type='text/markdown',
+    keywords='voila jupyter materialscloud',
+    cmdclass={
+        'develop': ExtraDevelop,
+        'install': ExtraInstall,
+    },
+    install_requires=['voila~=0.2.6'],
+    extras_require={
+        'dev': open(Path(__file__).parent.joinpath('requirements.txt').resolve()).readlines() + ['pylint']
+    }
+)
