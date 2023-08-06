d324f57d21ac526e9afd12dc3a1a65eb8d8b6496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:22 +0000

    Add package grabcut/0.5.0/grabcut-0.5.0.tar.gz

---
+++ packages/grabcut/grabcut-0.5.0.tar.gz/grabcut-0.5.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, dist
+from setuptools.command.install import install
+
+#class BinaryDistribution(dist.Distribution):
+#    def has_ext_modules(foo):
+#        return True
+
+setup(name='grabcut',
+    version='0.5.0',
+    description='GrabCut implementation for C++ with Python wrappers.',
+    url='https://github.com/luiscarlosgph/grabcut',
+    author='Luis C. Garcia-Peraza Herrera',
+    author_email='luiscarlos.gph@gmail.com',
+    license='MIT',
+    packages=['grabcut'],
+    include_package_data=True,
+    #package_data={'grabcut': ['_grabcut.so']},
+    #distclass=BinaryDistribution
+)
