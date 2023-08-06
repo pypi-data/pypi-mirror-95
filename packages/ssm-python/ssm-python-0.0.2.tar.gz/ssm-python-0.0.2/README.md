93936ba87af73b2fca716a0a368b06bbcb8102b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:36 +0000

    Add package ssm-python/0.0.2/ssm-python-0.0.2.tar.gz

---
+++ packages/ssm-python/ssm-python-0.0.2.tar.gz/ssm-python-0.0.2/README.md
@@ -0,0 +1,42 @@
+# ssm
+Simple Screen Mirror 💻✨✨✨🖥️  
+![screen-mirroring-icon](https://user-images.githubusercontent.com/31606119/107869470-d246af00-6ed1-11eb-8f95-acf910f27dc4.png)
+![ssm](https://user-images.githubusercontent.com/31606119/107876125-63844880-6f07-11eb-9949-cd1b36e5207e.gif)
+
+## How to run
+### 1. Install ssm package
+- ```pip install ssm-python```
+
+### 2. Create server.py & client.py
+- sample codes [here](https://github.com/JHLeeeMe/ssm/tree/master/samples)
+```python
+server:
+    from ssm import ScreenMirrorServer
+
+
+    # ssm_server = ScreenMirrorServer(['<client-ip>', port=7890])
+    ssm_server = ScreenMirrorServer()  # default: all ip & 7890 port
+
+    ssm_server.start()
+
+
+
+client:
+    from ssm import ScreenMirrorClient
+
+
+    ssm_client = ScreenMirrorClient('<server-ip>'[, port=7890, quality=90, cursor=True])
+
+    ssm_client.start()
+
+```
+
+### 3. Run
+1. ***server:*** ```python server.py```
+
+2. ***client:*** ```python client.py```
+
+### 4. Stop
+- ***server:*** ```press ESC```
+
+- ***client:*** ```kill process```
---
+++ packages/ssm-python/ssm-python-0.0.2.tar.gz/ssm-python-0.0.2/ssm/server.py
@@ -0,0 +1,131 @@
+"""Simple Screen Mirror Server
+
+1. receive screen data from client
+2. show client screen
+
+Functions:
+    _screen_size() -> (int, int)
+
+Classes:
+    ScreenMirrorServer
+
+"""
+
+import os
+import struct
+import pickle
+import socket
+
+import cv2
+from Xlib.display import Display
+
+
+def _screen_size() -> (int, int):
+    """Get screen size
+
+    extract screen size in pixels
+
+    Returns:
+        width, height: (int, int)
+
+    """
+    display = Display(display=os.environ['DISPLAY'])
+    width, height = \
+        display.screen().width_in_pixels, display.screen().height_in_pixels
+    return width, height
+
+
+class ScreenMirrorServer:
+    """ScreenMirrorServer
+
+    Attributes:
+        _HOST: str = ''
+            client ip (default: all ip)
+        _PORT: int = 7890
+
+        _WIDTH: int
+            screen x-axis size
+        _HEIGHT: int
+            screen y-axis size
+
+        _server_socket: socket.socket
+            socket (IPv4, TCP)
+
+    Methods:
+        start() -> None
+
+        _receive(conn_socket: socket.socket, addr: (str, int)) -> None
+
+    """
+    def __init__(self, host: str = '', port: int = 7890):
+        self._HOST = host
+        self._PORT = port
+
+        self._WIDTH, self._HEIGHT = _screen_size()
+
+        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server_socket.bind((self._HOST, self._PORT))
+
+    def start(self):
+        self._server_socket.listen()
+        conn_socket, addr = self._server_socket.accept()
+
+        self._receive(conn_socket, addr)
+
+    def _receive(self, conn_socket: socket.socket, addr: (str, int)):
+        """Receive data from client
+
+        1. receive data from client
+        2. unpacking & unpickling received data
+        3. show client screen with cv2.imshow()
+
+        Args:
+            conn_socket: socket.socket
+                connected client socket
+            addr: (str, int)
+                connected client address
+
+        """
+        assert (conn_socket is not None)
+
+        overhead_size = struct.calcsize('>III')
+        payload_bin = b''
+
+        try:
+            while True:
+                while len(payload_bin) < overhead_size:
+                    received_data_bin = conn_socket.recv(8192)
+                    if not received_data_bin:
+                        raise StopIteration
+                    payload_bin += received_data_bin
+
+                packed_payload_size = payload_bin[:overhead_size]
+                payload_bin = payload_bin[overhead_size:]
+
+                width, height, payload_size = \
+                    struct.unpack('>III', packed_payload_size)
+
+                while len(payload_bin) < payload_size:
+                    received_data_bin = conn_socket.recv(8192)
+                    if not received_data_bin:
+                        raise StopIteration
+                    payload_bin += received_data_bin
+
+                encoded_screen_pkl = payload_bin[:payload_size]
+                payload_bin = payload_bin[payload_size:]
+
+                encoded_screen = pickle.loads(encoded_screen_pkl)
+                screen = cv2.imdecode(encoded_screen, flags=cv2.IMREAD_COLOR)
+
+                if (self._WIDTH < width) and (self._HEIGHT < height):
+                    screen = cv2.resize(screen,
+                                        (self._WIDTH, self._HEIGHT),
+                                        interpolation=cv2.INTER_AREA)
+
+                cv2.imshow(winname=str(addr), mat=screen)
+                if cv2.waitKey(1) == 27:
+                    raise StopIteration
+        except StopIteration:
+            conn_socket.close()
+            cv2.destroyWindow(winname=str(addr))
+            print('Mirroring ends...')
