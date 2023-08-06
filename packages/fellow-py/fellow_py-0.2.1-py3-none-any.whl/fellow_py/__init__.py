9994ea4cc97538b7f9cde167d3b9286093e73b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:30 +0000

    Add package fellow-py/0.2.1/fellow_py-0.2.1-py3-none-any.whl

---
+++ packages/fellow-py/fellow_py-0.2.1-py3-none-any.whl/fellow_py/__init__.py
@@ -0,0 +1,5 @@
+"""Import things for easier access."""
+
+from .discover import discover_by_address  # noqa
+from .exceptions import FellowException  # noqa
+from .kettle import StaggEKGPlusKettle  # noqa
