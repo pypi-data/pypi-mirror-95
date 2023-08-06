c0e3c5371fd4f3bf030f9f3ef6b625e848e9d5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:54 +0000

    Add package Mesa-Adapted/0.8.7.3/Mesa_Adapted-0.8.7.3-py3-none-any.whl

---
+++ packages/Mesa-Adapted/Mesa_Adapted-0.8.7.3-py3-none-any.whl/mesa_adapted/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Mesa Agent-Based Modeling Framework
+
+Core Objects: Model, and Agent.
+
+"""
+import datetime
+
+from mesa_adapted.model import Model
+from mesa_adapted.agent import Agent
+
+
+__all__ = ["Model", "Agent"]
+
+__title__ = "mesa"
+__version__ = "0.8.7.3"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright %s Project Mesa Team" % datetime.date.today().year
---
+++ packages/Mesa-Adapted/Mesa_Adapted-0.8.7.3-py3-none-any.whl/mesa_adapted/visualization/modules/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Container for all built-in visualization modules.
+"""
+
+from mesa_adapted.visualization.modules.CanvasGridVisualization import CanvasGrid  # noqa
+from mesa_adapted.visualization.modules.ChartVisualization import ChartModule  # noqa
+from mesa_adapted.visualization.modules.PieChartVisualization import PieChartModule  # noqa
+from mesa_adapted.visualization.modules.BarChartVisualization import BarChartModule  # noqa
+from mesa_adapted.visualization.modules.TextVisualization import TextElement  # noqa
+from mesa_adapted.visualization.modules.HexGridVisualization import CanvasHexGrid  # noqa
+from mesa_adapted.visualization.modules.NetworkVisualization import NetworkModule  # noqa
