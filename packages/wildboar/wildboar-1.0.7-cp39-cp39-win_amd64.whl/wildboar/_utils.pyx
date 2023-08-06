1f975ddd78d2836b9aa704f3da9935feb77f7165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:33 +0000

    Add package wildboar/1.0.7/wildboar-1.0.7-cp39-cp39-win_amd64.whl

---
+++ packages/wildboar/wildboar-1.0.7-cp39-cp39-win_amd64.whl/wildboar/_utils.pyx
@@ -0,0 +1,379 @@
+# cython: cdivision=True
+# cython: boundscheck=False
+# cython: wraparound=False
+# cython: language_level=3
+
+# This file is part of wildboar
+#
+# wildboar is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# wildboar is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# Authors: Isak Samsten
+from __future__ import print_function
+
+import numpy as np
+cimport numpy as np
+
+from libc.math cimport log2
+from libc.math cimport sqrt
+from libc.stdlib cimport realloc
+
+cdef class RollingVariance:
+    def __cinit__(self):
+        self._m = 0.0
+        self._n_samples = 0.0
+        self._s = 0.0
+
+    cdef void _reset(self) nogil:
+        self._m = 0.0
+        self._n_samples = 0.0
+        self._s = 0.0
+        self._sum = 0.0
+
+    def reset(self):
+        self._reset()
+
+    cdef void _add(self, double weight, double value) nogil:
+        cdef double next_m
+        self._n_samples += weight
+        next_m = self._m + (value - self._m) / self._n_samples
+        self._s += (value - self._m) * (value - next_m)
+        self._m = next_m
+        self._sum += weight * value
+
+    def add(self, value, weight=1.0):
+        self._add(weight, value)
+
+    cdef void _remove(self, double weight, double value) nogil:
+        cdef double old_m
+        if self._n_samples == 1.0:
+            self._n_samples = 0.0
+            self._m = 0.0
+            self._s = 0.0
+        else:
+            old_m = (self._n_samples * self._m - value) / (self._n_samples - weight)
+            self._s -= (value - self._m) * (value - old_m)
+            self._m = old_m
+            self._n_samples -= weight
+        self._sum -= weight * value
+
+    def remove(self, value, weight=1.0):
+        self._remove(weight, value)
+
+    @property
+    def n_samples(self):
+        return self._n_samples
+
+    @property
+    def sum(self):
+        return self._sum
+
+    cdef double _mean(self) nogil:
+        return self._m
+
+    @property
+    def mean(self):
+        return self._mean()
+
+    cdef double _variance(self) nogil:
+        return 0.0 if self._n_samples <= 1 else self._s / self._n_samples
+
+    @property
+    def variance(self):
+        return self._variance()
+
+# For debugging
+def print_tree(o, indent=1):
+    if o.is_classification_leaf:
+        print("-" * indent, "leaf: ")
+        print("-" * indent, " proba: ", o.proba)
+    elif o.is_regression_leaf:
+        print("-" * indent, "leaf: ")
+        print("-" * indent, " avg: ", o.regr_val)
+    else:
+        print("-" * indent, "branch:")
+        print("-" * indent, " shapelet: ", o.shapelet.array)
+        print("-" * indent, " threshold: ", o.threshold)
+        print("-" * indent, " left:", end="\n")
+        print_tree(o.left, indent + 1)
+        print("-" * indent, " right:", end="\n")
+        print_tree(o.right, indent + 1)
+
+# For debugging
+cdef void print_c_array_d(object name, double *arr, Py_ssize_t length):
+    print(name, end=": ")
+    for i in range(length):
+        print(arr[i], end=" ")
+    print()
+
+# For debugging
+cdef void print_c_array_i(object name, Py_ssize_t *arr, Py_ssize_t length):
+    print(name, end=": ")
+    for i in range(length):
+        print(arr[i], end=" ")
+    print()
+
+cdef Py_ssize_t label_distribution(const Py_ssize_t *samples,
+                               const double *sample_weights,
+                               Py_ssize_t start,
+                               Py_ssize_t end,
+                               const Py_ssize_t *labels,
+                               Py_ssize_t label_stride,
+                               Py_ssize_t n_labels,
+                               double *n_weighted_samples,
+                               double *dist) nogil:
+    """Computes the label distribution
+
+    :param samples: the samples to include
+    :param sample_weights: 
+    :param start: the start position in samples
+    :param end: the end position in samples
+    :param labels: the labels
+    :param label_stride: the stride in labels
+    :param n_labels: the number labeles
+    :param n_weighted_samples: (out) number of samples according to weight
+    :param dist: (out) label distribution
+    :return: number of classes included in the sample
+    """
+    cdef double sample_weight
+    cdef Py_ssize_t i, j, p, n_pos
+
+    n_pos = 0
+    n_weighted_samples[0] = 0
+    for i in range(start, end):
+        j = samples[i]
+        p = j * label_stride
+
+        if sample_weights != NULL:
+            sample_weight = sample_weights[j]
+        else:
+            sample_weight = 1.0
+
+        dist[labels[p]] += sample_weight
+        n_weighted_samples[0] += sample_weight
+
+    for i in range(n_labels):
+        if dist[i] > 0:
+            n_pos += 1
+
+    return n_pos
+
+cdef inline size_t rand_r(size_t *seed) nogil:
+    """Returns a pesudo-random number based on the seed.
+
+    :param seed: the initial seed (updated)
+    :return: a psudo-random number
+    """
+    seed[0] = seed[0] * 1103515245 + 12345
+    return seed[0] % (<size_t> RAND_R_MAX + 1)
+
+cdef inline size_t rand_int(size_t min_val, size_t max_val, size_t *seed) nogil:
+    """Returns a pseudo-random number in the range [`min_val` `max_val`[
+
+    :param min_val: the minimum value
+    :param max_val: the maximum value
+    :param seed: the seed (updated)
+    """
+    if min_val == max_val:
+        return min_val
+    else:
+        return min_val + rand_r(seed) % (max_val - min_val)
+
+cdef inline double rand_uniform(double low, double high, size_t *random_state) nogil:
+    """Generate a random double in the range [`low` `high`[."""
+    return ((high - low) * <double> rand_r(random_state) / <double> RAND_R_MAX) + low
+
+# Implementation of introsort. Inspired by sklearn.tree
+# implementation. This code is licensed under BSD3 (and not GPLv3)
+#
+# Including:
+#  - argsort
+#  - swap
+#  - median3
+#  - introsort
+#  - sift_down
+#  - heapsort
+
+
+cdef inline void argsort(double *values, Py_ssize_t *samples, Py_ssize_t n) nogil:
+    if n == 0:
+        return
+    cdef Py_ssize_t maxd = 2 * <Py_ssize_t> log2(n)
+    introsort(values, samples, n, maxd)
+
+cdef inline void swap(double *values, Py_ssize_t *samples,
+                      Py_ssize_t i, Py_ssize_t j) nogil:
+    values[i], values[j] = values[j], values[i]
+    samples[i], samples[j] = samples[j], samples[i]
+
+cdef inline double median3(double *values, Py_ssize_t n) nogil:
+    cdef double a = values[0]
+    cdef double b = values[n / 2]
+    cdef double c = values[n - 1]
+    if a < b:
+        if b < c:
+            return b
+        elif a < c:
+            return c
+        else:
+            return a
+    elif b < c:
+        if a < c:
+            return a
+        else:
+            return c
+    else:
+        return b
+
+cdef void introsort(double *values, Py_ssize_t *samples,
+                    Py_ssize_t n, Py_ssize_t maxd) nogil:
+    cdef double pivot, value
+    cdef Py_ssize_t i, l, r
+
+    while n > 1:
+        if maxd <= 0:
+            heapsort(values, samples, n)
+            return
+        maxd -= 1
+
+        pivot = median3(values, n)
+
+        i = l = 0
+        r = n
+        while i < r:
+            value = values[i]
+            if value < pivot:
+                swap(values, samples, i, l)
+                i += 1
+                l += 1
+            elif value > pivot:
+                r -= 1
+                swap(values, samples, i, r)
+            else:
+                i += 1
+
+        introsort(values, samples, l, maxd)
+        values += r
+        samples += r
+        n -= r
+
+cdef inline void sift_down(double *values, Py_ssize_t *samples,
+                           Py_ssize_t start, Py_ssize_t end) nogil:
+    cdef Py_ssize_t child, maxind, root
+    root = start
+    while True:
+        child = root * 2 + 1
+        maxind = root
+        if child < end and values[maxind] < values[child]:
+            maxind = child
+        if child + 1 < end and values[maxind] < values[child + 1]:
+            maxind = child + 1
+
+        if maxind == root:
+            break
+        else:
+            swap(values, samples, root, maxind)
+            root = maxind
+
+cdef void heapsort(double *values, Py_ssize_t *samples, Py_ssize_t n) nogil:
+    cdef Py_ssize_t start, end
+
+    start = (n - 2) / 2
+    end = n
+    while True:
+        sift_down(values, samples, start, end)
+        if start == 0:
+            break
+        start -= 1
+
+    end = n - 1
+    while end > 0:
+        swap(values, samples, 0, end)
+        sift_down(values, samples, 0, end)
+        end = end - 1
+
+cdef int realloc_array(void** ptr, Py_ssize_t old_size, Py_ssize_t ptr_size, Py_ssize_t *capacity)  nogil except -1:
+    cdef void *tmp = ptr[0]
+    if old_size >= capacity[0]:
+        capacity[0] = old_size * 2
+        tmp = realloc(ptr[0], ptr_size * capacity[0])
+        if tmp == NULL:
+            return -1
+    ptr[0] = tmp
+    return 0
+
+cdef int safe_realloc(void** ptr, Py_ssize_t new_size) nogil except -1:
+    cdef void *tmp = ptr[0]
+    tmp = realloc(ptr[0], new_size)
+    if tmp == NULL:
+        return -1
+
+    ptr[0] = tmp
+    return 0
+
+
+cdef void fast_mean_std(
+        Py_ssize_t offset,
+        Py_ssize_t stride,
+        Py_ssize_t length,
+        double* data,
+        double *mean,
+        double* std,
+) nogil:
+    """Update the mean and standard deviation"""
+    cdef double ex = 0
+    cdef double ex2 = 0
+    cdef Py_ssize_t i
+    for i in range(length):
+        current_value = data[offset + i * stride]
+        ex += current_value
+        ex2 += current_value ** 2
+
+    mean[0] = ex / length
+    std[0] = sqrt(ex2 / length - mean[0] * mean[0])
+
+
+def check_array_fast(np.ndarray x, bint ensure_2d=False, bint allow_nd=False, bint c_order=True):
+    """Ensure that the array is valid and with dtype=np.float64.
+
+    Parameters
+    ----------
+    x : ndarray
+        The array to validate.
+
+    ensure_2d : bool, optional
+        Ensure that the array has 2 dimensions.
+
+    allow_nd : bool, optional
+        Allow more than 2 dimensions. Only valid if ensure_2d=False.
+
+    c_order : bool, optional
+        Ensure that the returned array is in row-major order.
+
+    Returns
+    -------
+    x : ndarray
+        Either a copy or the original array validated.
+    """
+    if ensure_2d:
+        if x.ndim != 2:
+            raise ValueError("not 2d, got %rd" % x.ndim)
+    else:
+        if not allow_nd and x.ndim > 1:
+            raise ValueError("not 1d, got %rd" % x.ndim)
+
+    if not x.flags.c_contiguous and c_order:
+        x = np.ascontiguousarray(x, dtype=np.float64)
+    if not x.dtype == np.float64:
+        x = x.astype(np.float64)
+    return x
---
+++ packages/wildboar/wildboar-1.0.7-cp39-cp39-win_amd64.whl/wildboar/datasets/__init__.py
@@ -0,0 +1,550 @@
+# This file is part of wildboar
+#
+# wildboar is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# wildboar is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors: Isak Samsten
+
+import os
+import re
+
+import numpy as np
+from sklearn.utils import deprecated
+
+from ._repository import (
+    ArffBundle,
+    NpyBundle,
+    Bundle,
+    Repository,
+    RepositoryCollection,
+    JSONRepository,
+)
+
+from ._filter import make_filter
+
+__all__ = [
+    "Repository",
+    "JSONRepository",
+    "Bundle",
+    "ArffBundle",
+    "NpyBundle",
+    "set_cache_dir",
+    "get_repository",
+    "clear_cache",
+    "list_repositories",
+    "list_bundles",
+    "get_bundles",
+    "install_repository",
+    "load_dataset",
+    "list_datasets",
+    "load_datasets",
+    "load_all_datasets",
+    "load_two_lead_ecg",
+    "load_synthetic_control",
+    "load_gun_point",
+]
+
+_CACHE_DIR = None
+_REPOSITORIES = RepositoryCollection()
+
+
+def _split_repo_bundle(repo_bundle_name):
+    """Split a repository bundle string of the format {repo}/{bundle}
+
+    Parameters
+    ----------
+    repo_bundle_name : str
+        {repo}/{bundle} name
+
+    Returns
+    -------
+    repository : str
+        Key of the repository
+
+    bundle : str
+        Name of the bundle
+
+    version : str
+        An optional version
+
+    tag : str
+        An optional tag
+    """
+    match = re.match(
+        "(?:([a-zA-Z]+)/([a-zA-Z0-9\-]+))?(?::((?:\d+\.)?(?:\d+\.)?(?:\*|\d+)))?(?::([a-zA-Z\-]+))?$",
+        repo_bundle_name,
+    )
+    if match:
+        repository_name = match.group(1)
+        bundle_name = match.group(2)
+        version = match.group(3)
+        if version:
+            version = re.sub("(\d+\.\d+)((?:\.0+)*)$", "\\1", version)
+        tag = match.group(4)
+        return repository_name, bundle_name, version, tag
+    else:
+        raise ValueError("repository (%s) is not supported" % repo_bundle_name)
+
+
+def set_cache_dir(cache_dir):
+    """Change the global cache directory
+
+    cache_dir : str
+        The cache directory root
+    """
+    global _CACHE_DIR
+    _CACHE_DIR = cache_dir
+
+
+def _default_cache_dir():
+    return _os_cache_path("wildboar") if not _CACHE_DIR else _CACHE_DIR
+
+
+def _os_cache_path(dir):
+    import platform
+
+    if platform.system() == "Windows":
+        cache_dir = os.path.expandvars(r"%LOCALAPPDATA%\cache")
+        return os.path.join(cache_dir, dir)
+    elif platform.system() == "Linux":
+        cache_dir = os.environ.get("XDG_CACHE_HOME")
+        if not cache_dir:
+            cache_dir = ".cache"
+        return os.path.join(os.path.expanduser("~"), cache_dir, dir)
+    elif platform.system() == "Darwin":
+        return os.path.join(os.path.expanduser("~"), "Library", "Caches", dir)
+    else:
+        return os.path.join(os.path.expanduser("~"), ".cache", dir)
+
+
+def load_synthetic_control(merge_train_test=True):
+    """Load the Synthetic_Control dataset
+
+    See Also
+    --------
+    load_dataset : load a named dataset
+    """
+    return load_dataset(
+        "SyntheticControl",
+        repository="wildboar/ucr-tiny",
+        merge_train_test=merge_train_test,
+    )
+
+
+def load_two_lead_ecg(merge_train_test=True):
+    """Load the TwoLeadECG dataset
+
+    See Also
+    --------
+    load_dataset : load a named dataset
+    """
+    return load_dataset(
+        "TwoLeadECG",
+        repository="wildboar/ucr-tiny",
+        merge_train_test=merge_train_test,
+    )
+
+
+def load_gun_point(merge_train_test=True):
+    """Load the GunPoint dataset
+
+    See Also
+    --------
+    load_dataset : load a named dataset
+    """
+    return load_dataset(
+        "GunPoint",
+        repository="wildboar/ucr-tiny",
+        merge_train_test=merge_train_test,
+    )
+
+
+def load_datasets(
+    repository="wildboar/ucr",
+    *,
+    cache_dir=None,
+    create_cache_dir=True,
+    progress=True,
+    force=False,
+    filter=None,
+    **kwargs
+):
+    """Load all datasets as a generator
+
+    Parameters
+    ----------
+    repository : str
+        The repository string
+
+    progress : bool, optional
+        If progress indicator is shown while downloading the repository.
+
+    cache_dir : str, optional
+        The cache directory for downloaded dataset repositories.
+
+    create_cache_dir : bool, optional
+        Create the cache directory if it does not exist.
+
+    force : bool, optional
+        Force re-download of cached repository
+
+    filter: dict or callable, optional
+        Filter the datasets
+
+        - if callable, only yield those datasets for which the callable returns True.
+          ``f(dataset, x, y) -> bool``
+
+        - if dict, filter based on the keys and values
+            - "dataset": regex matching dataset name
+            - "n_samples": comparison spec
+            - "n_timestep": comparison spec
+
+        Comparison spec
+            str of two parts, comparison operator (<, <=, >, >= or =) and a number, e.g., "<100", "<= 200", or ">300"
+
+    kwargs : dict
+        Optional arguments to ``load_dataset``
+
+    Yields
+    ------
+    x : array-like
+        Data samples
+
+    y : array-like
+        Data labels
+
+    Examples
+    --------
+
+    >>> from wildboar.datasets import load_datasets
+    >>> for dataset, (x, y) in load_datasets(repository='wildboar/ucr'):
+    >>>     print(dataset, x.shape, y.shape)
+
+    Print the names of datasets with more than 200 samples
+
+    >>> for dataset, (x, y) in load_datasets(repository='wildboar/ucr', filter={"n_samples": ">200"}):
+    >>>     print(dataset)
+    """
+    for dataset in list_datasets(
+        repository=repository,
+        cache_dir=cache_dir,
+        create_cache_dir=create_cache_dir,
+        progress=progress,
+        force=force,
+    ):
+        x, y = load_dataset(dataset, repository=repository, **kwargs)
+        if filter is None:
+            yield dataset, (x, y)
+        elif hasattr(filter, "__call__"):
+            if filter(dataset, x, y):
+                yield dataset, (x, y)
+        else:
+            if make_filter(filter)(dataset, x, y):
+                yield dataset, (x, y)
+
+
+@deprecated(
+    "the function datasets.load_all_datasets has been deprecated in "
+    "1.0.3 and will be removed in in 1.0.5."
+)
+def load_all_datasets(
+    repository="wildboar/ucr",
+    *,
+    cache_dir=None,
+    create_cache_dir=True,
+    progress=True,
+    force=False,
+    **kwargs
+):
+    return load_datasets(
+        repository,
+        cache_dir=cache_dir,
+        create_cache_dir=create_cache_dir,
+        progress=progress,
+        force=force,
+        **kwargs
+    )
+
+
+def load_dataset(
+    name,
+    *,
+    repository="wildboar/ucr",
+    dtype=None,
+    contiguous=True,
+    merge_train_test=True,
+    cache_dir=None,
+    create_cache_dir=True,
+    progress=True,
+    force=False
+):
+    """Load a dataset from a repository
+
+    Parameters
+    ----------
+    name : str
+        The name of the dataset to load.
+
+    repository : str, optional
+        The data repository formatted as {repository}/{bundle}[:{version}][:{tag}]
+
+    dtype : dtype, optional
+        The data type of the returned data
+
+    contiguous : bool, optional
+        Ensure that the returned dataset is memory contiguous.
+
+    merge_train_test : bool, optional
+        Merge the existing training and testing partitions.
+
+    progress: bool, optional
+        Show a progress bar while downloading a bundle.
+
+    cache_dir: str, optional
+        The directory where downloaded files are cached
+
+    create_cache_dir: bool, optional
+        Create cache directory if missing (default=True)
+
+    force : bool, optional
+        Force re-download of already cached bundle
+
+        ..versionadded :: 1.0.4
+
+    Returns
+    -------
+    x : ndarray
+        The data samples
+
+    y : ndarray
+        The labels
+
+    x_train : ndarray, optional
+        The training samples if ``merge_train_test=False``
+
+    x_test : ndarray, optional
+        The testing samples if ``merge_train_test=False``
+
+    y_train : ndarray, optional
+        The training labels if ``merge_train_test=False``
+
+    y_test : ndarray, optional
+        The testing labels if ``merge_train_test=False``
+
+    Examples
+    --------
+
+    Load a dataset from the default repository
+
+    >>> x, y = load_dataset("SyntheticControl")
+
+    or if original training and testing splits are to be preserved
+
+    >>> x_train, x_test, y_train, y_test = load_dataset("SyntheticControl", merge_train_test=False)
+
+    or for a specific version of the dataset
+
+    >>> x_train, x_test, y_train, y_test = load_dataset("Wafer", repository='wildboar/ucr-tiny:1.0')
+    """
+    (
+        repository_name,
+        bundle_name,
+        bundle_version,
+        bundle_tag,
+    ) = _split_repo_bundle(repository)
+    dtype = dtype or np.float64
+    cache_dir = cache_dir or _default_cache_dir()
+    repository = get_repository(repository_name)
+    ret_val = []
+    x, y, n_train_samples = repository.load_dataset(
+        bundle_name,
+        name,
+        version=bundle_version,
+        tag=bundle_tag,
+        dtype=dtype,
+        cache_dir=cache_dir,
+        force=force,
+        create_cache_dir=create_cache_dir,
+        progress=progress,
+    )
+
+    if merge_train_test:
+        ret_val.append(x)
+        ret_val.append(y)
+    else:
+        if n_train_samples == x.shape[0]:
+            raise ValueError("found no test parts. Set merge_train_test=True.")
+
+        ret_val.append(x[:n_train_samples])
+        ret_val.append(x[n_train_samples:])
+        ret_val.append(y[:n_train_samples])
+        ret_val.append(y[n_train_samples:])
+
+    if contiguous:
+        return [np.ascontiguousarray(x) for x in ret_val]
+    else:
+        return ret_val
+
+
+def list_datasets(
+    repository="wildboar/ucr",
+    *,
+    cache_dir=None,
+    create_cache_dir=True,
+    progress=True,
+    force=False
+):
+    """List the datasets in the repository
+
+    Parameters
+    ----------
+    repository : str or Bundle, optional
+        The data repository
+
+        - if str load a named bundle, format {repository}/{bundle}
+
+    progress: bool, optional
+        Show a progress bar while downloading a bundle.
+
+    cache_dir: str, optional
+        The directory where downloaded files are cached (default='wildboar_cache')
+
+    create_cache_dir: bool, optional
+        Create cache directory if missing (default=True)
+
+    force : bool, optional
+        Force re-download of cached bundle
+
+    Returns
+    -------
+        dataset : set
+            A set of dataset names
+    """
+    (
+        repository_name,
+        bundle_name,
+        bundle_version,
+        bundle_tag,
+    ) = _split_repo_bundle(repository)
+    cache_dir = cache_dir or _default_cache_dir()
+    repository = get_repository(repository_name)
+    return repository.list_datasets(
+        bundle_name,
+        version=bundle_version,
+        tag=bundle_tag,
+        cache_dir=cache_dir,
+        create_cache_dir=create_cache_dir,
+        progress=progress,
+        force=force,
+    )
+
+
+def clear_cache(repository=None, *, cache_dir=None, keep_last_version=True):
+    """Clear the cache by deleting cached datasets
+
+    Parameters
+    ----------
+    repository : str, optional
+        The name of the repository to clear cache.
+
+        - if None, clear cache of all repositories
+
+    cache_dir : str, optional
+        The cache directory
+
+    keep_last_version : bool, optional
+        If true, keep the latest version of each repository.
+    """
+    cache_dir = cache_dir or _default_cache_dir()
+    if repository is None:
+        for repo in _REPOSITORIES:
+            repo.clear_cache(cache_dir=cache_dir, keep_last_version=keep_last_version)
+    else:
+        get_repository(repository).clear_cache(
+            cache_dir=cache_dir, keep_last_version=keep_last_version
+        )
+
+
+def get_repository(repository):
+    """Get repository by name
+
+    Parameters
+    ----------
+    repository : str
+        Repository name
+
+    Returns
+    -------
+    repository : Repository
+        A repository
+    """
+    if repository in _REPOSITORIES:
+        return _REPOSITORIES[repository]
+    else:
+        raise ValueError("repository (%s) does not exist" % repository)
+
+
+def install_repository(repository):
+    """Install repository
+
+    Parameters
+    ----------
+    repository : str or Repository
+        A repository
+    """
+    if isinstance(repository, str):
+        repository = JSONRepository(repository)
+    _REPOSITORIES.append(repository)
+
+
+def get_bundles(repository):
+    """Get all bundles in the repository
+
+    Parameters
+    ----------
+    repository : str
+        Name of the repository
+
+    Returns
+    -------
+    dict : A dict of key Bundle pairs
+    """
+    if repository in _REPOSITORIES:
+        return _REPOSITORIES[repository].get_bundles()
+    else:
+        raise ValueError("repository (%s) does not exist.")
+
+
+def list_bundles(repository):
+    """Get a list of all bundle names in the specified repository.
+
+    Parameters
+    ----------
+    repository : str
+        The name of the repository
+
+    Returns
+    -------
+    bundle : str
+        The name of the bundle
+    """
+    return [key for key, bundle in get_bundles(repository).items()]
+
+
+def list_repositories():
+    """List the key of all installed repositories"""
+    return [repo.name for repo in _REPOSITORIES]
+
+
+# Install the default 'wildboar' repository
+install_repository("https://isaksamsten.github.io/wildboar-datasets/master/repo.json")
