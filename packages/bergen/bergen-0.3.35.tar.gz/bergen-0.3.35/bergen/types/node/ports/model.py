3585e39406199a1a8e9b016599dae08e8e5fc321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:48 +0000

    Add package bergen/0.3.35/bergen-0.3.35.tar.gz

---
+++ packages/bergen/bergen-0.3.35.tar.gz/bergen-0.3.35/bergen/types/node/ports/model.py
@@ -0,0 +1,14 @@
+
+
+from typing import Type
+from bergen.types.node.ports.base import BasePort
+from bergen.types.model import ArnheimModel
+
+class ModelPort(BasePort):
+
+  def __init__(self, modelClass: Type[ArnheimModel], **kwargs) -> None:
+      self.modelClass = modelClass
+      super().__init__("model",**kwargs)
+
+  def serialize(self):
+      return {**super().serialize(),"identifier" : self.modelClass.getMeta().identifier}
