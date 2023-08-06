bb7b67c890a2a64847abc89141b338fe7bc079ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:56 +0000

    Add package fil3s/2.4.3/fil3s-2.4.3.tar.gz

---
+++ packages/fil3s/fil3s-2.4.3.tar.gz/fil3s-2.4.3/fil3s/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from fil3s.classes import *
+
+# default file objects.
+File = Files.File
+Script = Files.Script
+Directory = Files.Directory
+Zip = Files.Zip
+Image = Files.Image
+Bytes = Files.Bytes
+Dictionary = Files.Dictionary
+Array = Files.Array
+
+# default format objects.
+FilePath = Formats.FilePath 
+String = Formats.String 
+Boolean = Formats.Boolean 
+Integer = Formats.Integer 
+Date = Formats.Date
