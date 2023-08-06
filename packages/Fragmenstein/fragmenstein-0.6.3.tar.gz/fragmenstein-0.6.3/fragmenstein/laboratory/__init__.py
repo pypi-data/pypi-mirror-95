9a9a12182e76c543e576cee2e742dd7eccd51e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:34 +0000

    Add package Fragmenstein/0.6.3/fragmenstein-0.6.3.tar.gz

---
+++ packages/Fragmenstein/fragmenstein-0.6.3.tar.gz/fragmenstein-0.6.3/fragmenstein/laboratory/__init__.py
@@ -0,0 +1,5 @@
+# Work in progress!!
+
+from ._process import process  # config for a run
+from .laboratory import Laboratory  # prepping data and analysis
+from .make_pyrosetta_options import make_option_string  # make the pyrosetta init str a dict
