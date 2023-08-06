dd58ccff6d93a5dd0cd9e259312598a871b9d789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:50 +0000

    Add package argtools/0.1.6/argtools-0.1.6.tar.gz

---
+++ packages/argtools/argtools-0.1.6.tar.gz/argtools-0.1.6/setup.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+REQUIRES = ['argparse'] if sys.version_info < (2, 7) else []
+if sys.version_info[0] < 3:
+    REQUIRES.append('six')
+
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+long_description = open(README).read() + '\n\n'
+VERSION = '0.1.6'
+
+setup(name='argtools',
+      version=VERSION,
+      install_requires=REQUIRES,
+      description='A collection of decorators to build command-line tools with minimal effort',
+      long_description_content_type = 'text/markdown',
+      long_description=long_description,
+      classifiers=['Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules'],
+      keywords='argparse subcommand command cli',
+      author='Takahiro Mimori',
+      author_email='takahiro.mimori@gmail.com',
+      py_modules=['argtools'],
+      package_data = {'': ['README.md']},
+      url='https://github.com/m1m0r1/argtools.py',
+      )
