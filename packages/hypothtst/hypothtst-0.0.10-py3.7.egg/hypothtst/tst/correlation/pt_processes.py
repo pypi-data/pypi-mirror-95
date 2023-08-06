6f27eabb5a2e5925caa7e875a267af70f4874050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:48 +0000

    Add package hypothtst/0.0.10/hypothtst-0.0.10-py3.7.egg

---
+++ packages/hypothtst/hypothtst-0.0.10-py3.7.egg/hypothtst/tst/correlation/pt_processes.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy.stats import binom_test
+from algorith.arrays.pt_process.overlap import critical_events
+from algorith.arrays.pt_process.window import critical_interval
+
+def correlation_score(ts1,ts2,w,delt,verbose=False):
+    """
+    ts1 rains on ts2 in a w-window.
+    """
+    interv = critical_interval(ts2,w,delt)
+    evnts = critical_events(ts1,ts2,w)
+    if verbose:
+        print(str(evnts)+","+str(len(ts1))+","+str(interv/delt))
+    return binom_test(evnts,len(ts1),interv/delt,alternative='greater')
+
