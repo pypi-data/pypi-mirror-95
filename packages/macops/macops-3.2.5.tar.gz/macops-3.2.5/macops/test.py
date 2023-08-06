b96deff126a273172c2c03d5e7ae0e688bc66b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:59 +0000

    Add package macops/3.2.5/macops-3.2.5.tar.gz

---
+++ packages/macops/macops-3.2.5.tar.gz/macops-3.2.5/macops/test.py
@@ -0,0 +1,5 @@
+#!usr/bin/env python
+from macops.generate import generate_mac
+from macops.reset import reset_original_mac
+from macops.change import change_mac
+from macops.get import get_current_mac
