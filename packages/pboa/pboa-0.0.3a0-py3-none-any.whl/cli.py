d3ce9acaf81c1ed644f5c0e2b49b0fe311e00a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:49 +0000

    Add package pboa/0.0.3a0/pboa-0.0.3a0-py3-none-any.whl

---
+++ packages/pboa/pboa-0.0.3a0-py3-none-any.whl/cli.py
@@ -0,0 +1,29 @@
+import click
+
+from boa import boa
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("name")
+def new(name):
+    """
+    Creates a new project NAME
+    """
+    boa.new(path=name)
+
+
+@cli.command()
+@click.argument("command")
+def make(command):
+    """
+    Runs the method named COMMAND located in make.py
+    """
+    try:
+        boa.run_make_command(command)
+    except Exception as e:
+        click.echo(e)
