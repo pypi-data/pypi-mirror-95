c4e5cedf83a67516f5b6166b583539629d84867d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:49 +0000

    Add package torchxrayvision/0.0.24/torchxrayvision-0.0.24.tar.gz

---
+++ packages/torchxrayvision/torchxrayvision-0.0.24.tar.gz/torchxrayvision-0.0.24/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="torchxrayvision",
+    version="0.0.24",
+    author="Joseph Paul Cohen",
+    author_email="joseph@josephpcohen.com",
+    description="TorchXRayVision: A library of chest X-ray datasets and models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mlmed/torchxrayvision",
+#    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Medical Science Apps."
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'torch>=1',
+        'torchvision>=0.5',
+        'scikit-image>=0.16',
+        'tqdm>=4',
+        'numpy>=1',
+        'pandas>=1',
+        'pydicom>=1',
+        'requests>=1'
+    ],
+    packages=find_packages(),
+    package_dir={'torchxrayvision': 'torchxrayvision'},
+    package_data={'torchxrayvision': ['data/*.zip','*.gz','*.tgz','*.zip']},
+)
