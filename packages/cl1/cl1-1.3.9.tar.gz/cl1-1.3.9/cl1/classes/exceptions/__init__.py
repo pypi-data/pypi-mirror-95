33b03645e07873370c28d2316f0bc36090f9c7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:16 +0000

    Add package cl1/1.3.9/cl1-1.3.9.tar.gz

---
+++ packages/cl1/cl1-1.3.9.tar.gz/cl1-1.3.9/cl1/classes/exceptions/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# exceptions.
+class ArgumentFormatError(Exception):
+	pass
+class EmptyArgumentError(Exception):
+	pass
+class ArgumentError(Exception):
+	pass
+class UnkownArgumentError(Exception):
+	pass
