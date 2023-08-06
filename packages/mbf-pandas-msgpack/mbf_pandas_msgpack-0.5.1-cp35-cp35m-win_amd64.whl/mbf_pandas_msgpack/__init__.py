d7fcdf671035043a79b6e265d11aab76434b0b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:31 +0000

    Add package mbf-pandas-msgpack/0.5.1/mbf_pandas_msgpack-0.5.1-cp35-cp35m-win_amd64.whl

---
+++ packages/mbf-pandas-msgpack/mbf_pandas_msgpack-0.5.1-cp35-cp35m-win_amd64.whl/mbf_pandas_msgpack/__init__.py
@@ -0,0 +1,29 @@
+# flake8: noqa
+
+# pandas versioning
+import pandas
+
+from distutils.version import LooseVersion
+
+pv = LooseVersion(pandas.__version__)
+
+if pv < "0.19.0":
+    raise ValueError("mbf_pandas_msgpack requires at least pandas 0.19.0")
+_is_pandas_legacy_version = pv.version[1] == 19 and len(pv.version) == 3
+
+from .packers import to_msgpack, read_msgpack
+
+# versioning
+#from ._version import get_versions
+
+#versions = get_versions()
+#__version__ = versions.get("closest-tag", versions["version"])
+#__git_revision__ = versions["full-revisionid"]
+
+__version__ = '0.5.1'
+
+
+pandas.DataFrame.to_msgpack = lambda self, path=None, **kwargs: to_msgpack(path, self, **kwargs)
+
+del pv, LooseVersion, pandas#get_versions, versions, 
+
