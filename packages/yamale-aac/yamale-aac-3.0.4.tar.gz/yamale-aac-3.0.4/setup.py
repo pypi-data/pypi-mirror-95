68bc29bfced3fe71dc3069799be9a4d5018aafae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:54 +0000

    Add package yamale-aac/3.0.4/yamale-aac-3.0.4.tar.gz

---
+++ packages/yamale-aac/yamale-aac-3.0.4.tar.gz/yamale-aac-3.0.4/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+readme = open("README.md", encoding="utf-8").read()
+license = open("LICENSE", encoding="utf-8").read()
+
+setup(
+    name="yamale-aac",
+    version="3.0.4",
+    url="https://github.com/danischm/Yamale",
+    author="Bo Lopker",
+    author_email="blopker@23andme.com",
+    description="A schema and validator for YAML.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["pyyaml"],
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["yamale=yamale.command_line:main"],},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+    ],
+)
