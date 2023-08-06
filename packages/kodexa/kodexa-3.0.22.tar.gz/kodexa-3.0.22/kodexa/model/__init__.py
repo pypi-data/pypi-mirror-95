8ea9a3cf52af9557a93bea537747714ccd041eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:52 +0000

    Add package kodexa/3.0.22/kodexa-3.0.22.tar.gz

---
+++ packages/kodexa/kodexa-3.0.22.tar.gz/kodexa-3.0.22/kodexa/model/__init__.py
@@ -0,0 +1,6 @@
+"""
+Document families allow the organization of documents based on relationships and actors
+"""
+from .document_families import DocumentRelationship, DocumentFamily
+from .model import DocumentMetadata, ContentNode, ContentFeature, SourceMetadata, \
+    Document, Store, DocumentStore, RemoteStore, ModelStore, ContentObject
