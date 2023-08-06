570b2ce8bc360934d9eb063e8f034ddd8e8802fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:38 +0000

    Add package narodmon-python-api/0.0.2/narodmon-python-api-0.0.2.tar.gz

---
+++ packages/narodmon-python-api/narodmon-python-api-0.0.2.tar.gz/narodmon-python-api-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from os import path, getcwd
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open(path.join(getcwd(), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="narodmon-python-api",
+    version="0.0.2",
+    author="Ilya Vereshchagin",
+    author_email="i.vereshchagin@gmail.com",
+    description="Narodmon python API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wwakabobik/narodmon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'requests',
+    ],
+)
