2a3ca8ae0a9d703108923a2f689a8f22c2e17962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:00 +0000

    Add package homemade-PWBW/0.0.1/homemade-PWBW-0.0.1.tar.gz

---
+++ packages/homemade-PWBW/homemade-PWBW-0.0.1.tar.gz/homemade-PWBW-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+    name="homemade-PWBW",
+    version="0.0.1",
+    author="haowen zhang",
+    author_email="806670753@qq.com",
+    description="这只是一个快捷方法库",
+    long_descripion_content_type="text/markdown",
+    url="https://github.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
