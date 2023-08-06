02a19bcee124e90e26578b8df72d06af615f939b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:54 +0000

    Add package py-manga/0.1.4/py-manga-0.1.4.tar.gz

---
+++ packages/py-manga/py-manga-0.1.4.tar.gz/py-manga-0.1.4/pymanga/parsers/adv_search_parser.py
@@ -0,0 +1,59 @@
+import re
+
+def parse_results(soup):
+    """
+    Parse search results from the advanced search page.
+
+    Parameters
+    ----------
+    content : BeautifulSoup
+        BeautifulSoup object of the search page content.
+
+    Returns
+    -------
+    results : list of dicts
+        List of search results.
+        ::
+
+            [
+                {
+                    'name': 'Series Name',
+                    'id': 'Series ID',
+                    'rating': 'Average Rating',
+                    'summary': 'Short summary',
+                    'thumbnail': 'Thumbnail link' or None,
+                    'year': 'Year manga started releasing',
+                    'is_adult': True or False
+                    # note! mangaupdates categorizes whole genres like 'Yuri' as adult
+                    # so use this at your own discretion :)
+                    # if this is true, thumbnail will always be None.
+                }
+            ]
+    """
+    results = []
+    for series in soup.find_all('div',attrs={'class':'col-12 col-lg-6 p-3 text'}):
+        box = series.div.find_all('div',recursive=False)
+
+        info = box[1].div.find_all('div',recursive=False)
+
+        ratings_text = re.search(r'(?P<year>\d{4})*(?: - )*(?:(?P<rating>\d+\.(\d+)) \/ 10\.0)*',info[3].get_text())
+
+        thumbnail = box[0].find('img')
+        is_adult = False
+        if not thumbnail:
+            is_adult = bool(box[0].find(lambda tag: tag.get_text() == 'AdultContent'))
+        else:
+            thumbnail = thumbnail['src']
+            # when a manga is considered to contain 'Adult Content', mangaupdates replaces the thumbnail with a divsoup that renders to a centered 'Adult Content' on a white background.
+
+        results.append({
+            'name': info[0].a.u.b.get_text(),
+            'id': info[0].a['href'].replace('https://www.mangaupdates.com/series.html?id=',''),
+            'genres': info[1].a['title'].split(', '),
+            'summary': info[2].get_text(),
+            'year': ratings_text.group('year'),
+            'rating': ratings_text.group('rating'),
+            'thumbnail': thumbnail,
+            'is_adult': is_adult
+        })
+    return results
---
+++ packages/py-manga/py-manga-0.1.4.tar.gz/py-manga-0.1.4/pymanga/parsers/releases_parsers.py
@@ -0,0 +1,52 @@
+def parse_releases(content):
+    """
+    Parse latest releases of a manga
+
+    Parameters
+    ----------
+    content : BeautifulSoup
+        BeautifulSoup object of the releases page content.
+
+    Returns
+    -------
+    releases : list of dicts
+        List of latest releases of a manga.
+        List is ordered latest-to-oldest
+        ::
+
+            [
+                {
+                    'chapter': 'chapter number',
+                    'vol': 'volume number' or None,
+                    'series': {
+                        'name': 'Manga Name',
+                        'id': 'Manga ID'
+                    },
+                    'group': {
+                        'name': 'Scanlation Group Name',
+                        'id': 'Scanlation Group ID'
+                    }
+                }
+            ]
+    """
+    releases = content.find_all('div',class_='text',recursive=False)[:-1]
+    results = []
+
+    for i in range(0,len(releases),5):
+        release = {}
+
+        release['series'] = {
+            'name': releases[i+1].get_text(),
+            'id': releases[i+1].a['href'].replace('https://www.mangaupdates.com/series.html?id=','')
+        }
+
+        vol = releases[i+2].get_text()
+        release['vol'] = vol if vol else None
+        release['chapter'] = releases[i+3].get_text()
+        release['group'] = {
+            'name': releases[i+4].get_text(),
+            'id': releases[i+4].a['href'].replace('https://www.mangaupdates.com/groups.html?id=','')
+        }
+        results.append(release)
+
+    return results
