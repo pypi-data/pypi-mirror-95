0cccdc11b6364e6bd2d0e522cde83a70fb54fb1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:41 +0000

    Add package dukeai/0.2.1/dukeai-0.2.1-py3-none-any.whl

---
+++ packages/dukeai/dukeai-0.2.1-py3-none-any.whl/dukeai/config.py
@@ -0,0 +1,9 @@
+pdf_ocr_funtion_arn = "arn:aws:lambda:us-east-1:689835822208:function:ocr-pdf-dev-lambda"
+img_ocr_funtion_arn = "arn:aws:lambda:us-east-1:689835822208:function:ocr-img-dev-lambda"
+
+# base_url = "https://cjuwzz07ri.execute-api.us-east-1.amazonaws.com/prod"
+base_url = "https://api.duke.ai/api"
+
+duke_auto_lambda_arn = "arn:aws:lambda:us-east-1:689835822208:function:Bare-Duke-dev-Auto"
+orchatect_auto_lambda_arn = "arn:aws:lambda:us-east-1:689835822208:function:Bare-Orchatect-dev-Auto"
+ralock_auto_lambda_arn = "arn:aws:lambda:us-east-1:689835822208:function:Bare-Ralock-dev-Auto"
