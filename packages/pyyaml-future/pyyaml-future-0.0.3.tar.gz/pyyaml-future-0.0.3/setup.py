a4de10c299357b67cb58cb24a1d318a907ef167d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:10 +0000

    Add package pyyaml-future/0.0.3/pyyaml-future-0.0.3.tar.gz

---
+++ packages/pyyaml-future/pyyaml-future-0.0.3.tar.gz/pyyaml-future-0.0.3/setup.py
@@ -0,0 +1,49 @@
+version = '0.0.3'
+
+from setuptools import setup
+import pathlib
+
+root = pathlib.Path(__file__).parent.resolve()
+
+long_description = \
+  (root / '.long_description.md') \
+  .read_text(encoding='utf-8')
+
+setup(
+  name = 'pyyaml-future',
+  version = version,
+  description = 'Use YAML 1.3 Features in PyYAML (YAML 1.1)',
+  license = 'MIT',
+  url = 'https://github.com/yaml/pyyaml-future',
+
+  author = 'Ingy döt Net',
+  author_email = 'ingy@ingy.net',
+
+  packages = ['yamlfuture'],
+  package_dir = {'': 'lib'},
+  #package_data = {
+  #    '': ['ReadMe.md'],
+  #},
+
+  python_requires = '>=3.6, <4',
+  install_requires = [
+    'pyyaml',
+  ],
+
+  keywords = ['yaml', 'future'],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3 :: Only',
+  ],
+
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
+  # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+)
