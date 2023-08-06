ea9381cb56645c8fd903442d9263686c9eb8c3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:10 +0000

    Add package styled-str/0.1.0/styled-str-0.1.0.tar.gz

---
+++ packages/styled-str/styled-str-0.1.0.tar.gz/styled-str-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="styled-str",
+    version="0.1.0",
+    url="https://github.com/jeremyephron/styled-str",
+    author="Jeremy Ephron",
+    author_email="jeremyephron@gmail.com",
+    description="Colored text output in Python using ANSI escape sequences.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS'
+    )
+)
