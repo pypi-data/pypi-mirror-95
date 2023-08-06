0e122a6546e30f646bcc589d722419d562660935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:58 +0000

    Add package distprob/0.1/distprob-0.1.tar.gz

---
+++ packages/distprob/distprob-0.1.tar.gz/distprob-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distprob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author='irishwy_2',
+      packages=['distprob'],
+      zip_safe=False)
