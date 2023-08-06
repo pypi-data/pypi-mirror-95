8987631c04ffa4238df2e42a2661d4ae146da646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:42 +0000

    Add package py-maia/1.0/py-maia-1.0.tar.gz

---
+++ packages/py-maia/py-maia-1.0.tar.gz/py-maia-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+about = open('README.md', 'r').read()
+setup(
+    name='py-maia',
+    version='1.0',
+    description="",
+    long_description=about,
+    license='MIT',
+    author='Arorior',
+    url='https://github.com/Arorior/maia',
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+)
